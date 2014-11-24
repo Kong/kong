77069651d73976babafbbc9b1cd3be9f4a60ab4b
thefosk <marco@mashape.com>
2014-11-24 15:42:36 -0800

    refactor

--- lib/resty/apenode/base/handler.lua
+++ lib/resty/apenode/base/handler.lua
@@ -3,7 +3,6 @@
 local access = require "resty.apenode.base.access"
 local header_filter = require "resty.apenode.base.header_filter"
 local log = require "resty.apenode.base.log"
-local utils = require "resty.apenode.utils"
 
 
 local _M = { _VERSION = '0.1' }
@@ -40,7 +39,7 @@ end
 
 
 function _M.log()
-	utils.create_timer(log.execute, ngx.ctx.log_message)
+	log.execute()
 end
 
 
--- lib/resty/apenode/base/log.lua
+++ lib/resty/apenode/base/log.lua
@@ -2,12 +2,18 @@
 
 
 local cjson = require "cjson"
+local utils = require "resty.apenode.utils"
 
 
 local _M = { _VERSION = '0.1' }
 
 
-function _M.execute(premature, message)
+function _M.execute()
+	utils.create_timer(_M.log, ngx.ctx.log_message)
+end
+
+
+function _M.log(premature, message)
 	-- TODO: Log the information
 	ngx.log(ngx.DEBUG, cjson.encode(message))
 end
