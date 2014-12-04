96a9239cbd3b6488a0af51052183a4c597b5b984
thibaultCha <thibaultcha@me.com>
2014-12-03 19:00:54 -0800

    Web tests (pending)

---
+++ spec/web/apis_spec.lua
@@ -0,0 +1,13 @@
+local http = require("socket.http")
+
+describe("/apis/ #web", function()
+	pending("Later.")
+
+	it("should pass", function()
+		local result, respcode, respheaders, respstatus = http.request {
+			method = "GET",
+			url = "http://httpbin.org/get"
+		}
+		assert.True(respcode == 200)
+	end)
+end)
