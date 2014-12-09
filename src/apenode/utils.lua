608a758b3b88986ae362d646107b5dfb84455c3c
thefosk <marco@mashape.com>
2014-12-09 14:16:38 -0800

    utils

--- src/apenode/utils.lua
+++ src/apenode/utils.lua
@@ -110,6 +110,31 @@ function _M.http_call(method, url, querystring, body, cb)
   end
 end
 
+local epoch = {year=1970, month=1, day=1, hour=0, min=0, sec=0, isdst=false }
+local function gmtime(t)
+   t.isdst =  false
+   return os.time(t) - os.time(epoch)
+end
+
+function _M.get_timestamps(now)
+  local _now = math.floor(now) -- Convert milliseconds to seconds. Milliseconds in openresty are in decimal places
+  local date = os.date("!*t", _now) -- In milliseconds
+
+  local second = _now
+  date.sec = 0
+  local minute = gmtime(date)
+  date.min = 0
+  local hour = gmtime(date)
+  date.hour = 0
+  local day = gmtime(date)
+  date.day = 1
+  local month = gmtime(date)
+  date.month = 1
+  local year = gmtime(date)
+
+  return {second=second * 1000, minute=minute * 1000, hour=hour * 1000,day=day * 1000, month=month * 1000, year=year * 1000}
+end
+
 function _M.get(url, querystring, cb)
   if type(querystring) == "function" then
     cb = querystring
