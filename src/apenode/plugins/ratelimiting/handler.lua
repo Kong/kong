ca36dcb3b1c053b037b31670b3c9bcd77baa4f4d
thefosk <marco@mashape.com>
2014-12-23 15:59:07 -0800

    more plugins schemas

--- src/apenode/plugins/ratelimiting/handler.lua
+++ src/apenode/plugins/ratelimiting/handler.lua
@@ -3,7 +3,20 @@
 local BasePlugin = require "apenode.base_plugin"
 local access = require "apenode.plugins.ratelimiting.access"
 
+local function check_period(v)
+  if v == "second" or v == "minute" or v == "hour" or v == "day" or v == "day" or v == "month" or v == "year" then
+    return true
+  else
+    return false, "Available values are: second, minute, hour, day, month, year"
+  end
+end
+
 local RateLimitingHandler = BasePlugin:extend()
+
+RateLimitingHandler["_SCHEMA"] = {
+  limit = { type = "number", required = true },
+  period = { type = "string", required = true, func = check_period }
+}
 
 function RateLimitingHandler:new()
   RateLimitingHandler.super.new(self, "ratelimiting")
