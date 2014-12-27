793a4de5a63b5a4ce1b6fe23c31de730968e0147
thefosk <marco@mashape.com>
2014-12-26 16:46:58 -0800

    metrics

--- src/apenode/plugins/ratelimiting/access.lua
+++ src/apenode/plugins/ratelimiting/access.lua
@@ -3,14 +3,24 @@
 local _M = {}
 
 function _M.execute(conf)
+  local authenticated_entity_id = nil
+  if ngx.ctx.authenticated_entity then
+    authenticated_entity_id = ngx.ctx.authenticated_entity.id
+  else
+    authenticated_entity_id = ngx.var.remote_addr -- Use the IP if there is not authenticated entity
+  end
+
   local period = conf.period
   local limit = conf.limit
 
   local timestamps = utils.get_timestamps(ngx.now())
-  local current_usage = dao.metrics:retrieve_metric(ngx.ctx.api.id,
-                                                    ngx.ctx.authenticated_entity.id,
-                                                    "requests." .. period,
-                                                    timestamps[period])
+
+  local current_usage = dao.metrics:find_one({
+    api_id = ngx.ctx.api.id,
+    application_id = authenticated_entity_id,
+    name = "requests." .. period,
+    timestamp = timestamps[period]
+  })
 
   if current_usage then current_usage = current_usage.value else current_usage = 0 end
 
@@ -25,7 +35,7 @@ function _M.execute(conf)
   -- TODO: this could also be done asynchronously in a timer maybe if specified in the conf (more performance, less security)?
   for k,v in pairs(timestamps) do
     dao.metrics:increment_metric(ngx.ctx.api.id,
-                                ngx.ctx.authenticated_entity.id,
+                                authenticated_entity_id,
                                 "requests." .. k,
                                 v, 1)
   end
