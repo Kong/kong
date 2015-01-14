b405107f98caf0995e0d330b701be418a0df3bc6
thefosk <marco@mashape.com>
2015-01-14 00:07:53 -0800

    @thibaultcha wip on cassandra factory

--- src/apenode/dao/cassandra/factory.lua
+++ src/apenode/dao/cassandra/factory.lua
@@ -2,27 +2,46 @@
 local Object = require "classic"
 
 local Faker = require "apenode.tools.faker"
+local Migrations = require "apenode.tools.migrations"
 local Apis = require "apenode.dao.cassandra.apis"
 local Metrics = require "apenode.dao.cassandra.metrics"
+local Plugins = require "apenode.dao.cassandra.plugins"
 local Accounts = require "apenode.dao.cassandra.accounts"
 local Applications = require "apenode.dao.cassandra.applications"
-local Plugins = require "apenode.dao.cassandra.plugins"
 
 local CassandraFactory = Object:extend()
 
-function CassandraFactory:new(configuration)
-  -- Build factory
-  self.apis = Apis(configuration)
-  self.metrics = Metrics(configuration)
-  self.accounts = Accounts(configuration)
-  self.applications = Applications(configuration)
-  self.plugins = Plugins(configuration)
+-- Instanciate an SQLite DAO.
+-- @param properties The parsed apenode configuration
+function CassandraFactory:new(properties)
+  self.type = "cassandra"
+  self.migrations = Migrations(self)
+
+  self.apis = Apis(properties)
+  self.metrics = Metrics(properties)
+  self.plugins = Plugins(properties)
+  self.accounts = Accounts(properties)
+  self.applications = Applications(properties)
 end
 
-function CassandraFactory:create_schema()
-  --error("Cannot create Cassandra schema")
+--
+-- Migrations
+--
+function CassandraFactory:migrate(callback)
+  self.migrations:migrate(callback)
 end
 
+function CassandraFactory:rollback(callback)
+  self.migrations:rollback(callback)
+end
+
+function CassandraFactory:reset(callback)
+  self.migrations:reset(callback)
+end
+
+--
+-- Seeding
+--
 function CassandraFactory:seed(random, number)
   Faker.seed(self, random, number)
 end
@@ -32,7 +51,28 @@ function CassandraFactory.fake_entity(type, invalid)
 end
 
 function CassandraFactory:drop()
-  --error("Can't drop Cassandra")
+  --TODO
+end
+
+--
+-- Utilities
+--
+function CassandraFactory:prepare()
+
+end
+
+function CassandraFactory:execute(stmt)
+
+end
+
+function CassandraFactory:close()
+  self.apis:finalize()
+  self.metrics:finalize()
+  self.plugins:finalize()
+  self.accounts:finalize()
+  self.applications:finalize()
+
+  self._db:close()
 end
 
 return CassandraFactory
