3e5c9de5c01895ee85085cc7211de9a00dc80ada
thibaultCha <thibaultcha@me.com>
2015-01-22 18:02:57 -0800

    Custom validate functions (with DAO) test

---
+++ src/apenode/models/validator.lua
@@ -0,0 +1,92 @@
+local rex = require "rex_pcre" -- Why? Lua has built in pattern which should do the job too
+
+local _M = {}
+
+local function add_error(errors, k, v)
+  if not errors then errors = {} end
+
+  if errors[k] then
+    local list = {}
+    table.insert(list, errors[k])
+    table.insert(list, v)
+    errors[k] = list
+  else
+    errors[k] = v
+  end
+  return errors
+end
+
+-- Validate a table against a given schema
+-- @param boolean is_update Ignores read_only fields during the validation if true
+-- @return A filtered, valid table if success, nil if error
+-- @return table A list of encountered errors during the validation
+function _M.validate(t, schema, is_update)
+  local result, errors = {}
+
+  -- Check the given table against a given schema
+  for k,v in pairs(schema) do
+    -- Set default value for the filed if given
+    if not t[k] and v.default ~= nil then
+      if type(v.default) == "function" then
+        t[k] = v.default()
+      else
+        t[k] = v.default
+      end
+
+    -- Check required fields are set
+    elseif v.required and (t[k] == nil or t[k] == "") then
+      errors = add_error(errors, k, k.." is required")
+
+    -- Check we're not passing an id
+    elseif t[k] and v.type == "id" then
+      errors = add_error(errors, k, k.." is an id and cannot be set")
+
+    -- Check types (number/string) of the field
+    elseif t[k] and type(t[k]) ~= v.type then
+      if v.type ~= "id" and v.type ~= "timestamp" then
+        errors = add_error(errors, k, k.." should be a "..v.type)
+      end
+
+    -- Check field against a custom function
+    elseif t[k] and v.func then
+      local success, err = v.func(t[k])
+      if not success then
+        errors = add_error(errors, k, err)
+      end
+    end
+
+    -- Check field against a regex if specified
+    if t[k] and v.regex then
+      if not rex.match(t[k], v.regex) then
+        errors = add_error(errors, k, k.." has an invalid value")
+      end
+    end
+
+    -- Check if field's value is unique
+    --[[
+    if t[k] and v.unique and not is_update then
+      local data, err = self._find_one({[k] = t[k]}, self._dao)
+      if data ~= nil then
+        errors = add_error(errors, k, k.." with value ".."\""..t[k].."\"".." already exists")
+      end
+    end
+    --]]
+
+    result[k] = t[k]
+  end
+
+  -- Check for unexpected fields in the entity
+  for k,v in pairs(t) do
+    if not schema[k] then
+      errors = add_error(errors, k, k.." is an unknown field")
+    end
+  end
+
+  if errors then
+    result = nil
+  end
+
+  return result, errors
+end
+
+return _M
