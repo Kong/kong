e5361565cef5c9906b807a2255f6692e564d89d3
thibaultCha <thibaultcha@me.com>
2015-01-28 14:23:52 -0800

    Update validation tests

--- spec/unit/validations_spec.lua
+++ spec/unit/validations_spec.lua
@@ -8,126 +8,80 @@ describe("Validation", function()
     -- grab a pair of glasses, this stuff can literally explode.
     local collection = "custom_object"
     local schema = {
-      id = { type = "id", read_only = true },
-
-      string = { type = "string",
-                 required = true,
-                 func = check_account_id },
-
-      url = { type = "string",
-              required = true,
-              regex = "(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])" },
-
-      date = { type = "timestamp",
-               default = 123456 },
-
-      default = { type = "string",
-                  default = function() return "default" end  },
-
-      number = { type = "number",
-                 func = function(n) if n == 123 then return true else return false, "The value should be 123" end end }
+      { _ = "id", type = "id" },
+      { _ = "string", required = true },
+      { _ = "url", required = true, regex = "(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])" },
+      { _ = "date", default = 123456 },
+      { _ = "default", default = function() return "default" end  }
     }
 
     it("should confirm a valid entity is valid", function()
       local values = { string = "httpbin entity", url = "httpbin.org" }
 
-      local res_values, err = validate(values, schema)
+      local valid, err = validate(values, schema)
       assert.falsy(err)
-      assert.truthy(res_values)
+      assert.truthy(valid)
     end)
 
     it("should set default values if those are variables or functions specified in the validator", function()
       -- Variables
       local values = { string = "httpbin entity", url = "httpbin.org" }
 
-      local res_values, err = validate(values, schema)
+      local valid, err = validate(values, schema)
       assert.falsy(err)
-      assert.truthy(res_values)
-      assert.are.same(123456, res_values.date)
+      assert.truthy(valid)
+      assert.are.same(123456, values.date)
 
       -- Functions
       local values = { string = "httpbin entity", url = "httpbin.org" }
 
-      local res_values, err = validate(values, schema)
+      local valid, err = validate(values, schema)
       assert.falsy(err)
-      assert.truthy(res_values)
-      assert.are.same("default", res_values.default)
+      assert.truthy(valid)
+      assert.are.same("default", values.default)
     end)
 
     it("should override default values if specified", function()
       -- Variables
       local values = { string = "httpbin entity", url = "httpbin.org", date = 654321 }
 
-      local res_values, err = validate(values, schema)
+      local valid, err = validate(values, schema)
       assert.falsy(err)
-      assert.truthy(res_values)
-      assert.are.same(654321, res_values.date)
+      assert.truthy(valid)
+      assert.are.same(654321, values.date)
 
       -- Functions
       local values = { string = "httpbin entity", url = "httpbin.org", default = "abcdef" }
 
-      local res_values, err = validate(values, schema)
+      local valid, err = validate(values, schema)
       assert.falsy(err)
-      assert.truthy(res_values)
-      assert.are.same("abcdef", res_values.default)
+      assert.truthy(valid)
+      assert.are.same("abcdef", values.default)
     end)
 
     it("should validate a field against a regex", function()
       local values = { string = "httpbin entity", url = "httpbin_!" }
 
-      local res_values, err = validate(values, schema)
-      assert.falsy(res_values)
+      local valid, err = validate(values, schema)
+      assert.falsy(valid)
       assert.are.same("url has an invalid value", err.url)
-    end)
-
-    it("should return an error when passing the wrong type", function()
-      local values = { string = 123, url = "httpbin.org" }
-
-      local res_values, err = validate(values, schema)
-      assert.falsy(res_values)
-      assert.are.same("string should be a string", err.string)
     end)
 
     it("should return error when unexpected values are included in the schema", function()
       local values = { string = "httpbin entity", url = "httpbin.org", unexpected = "abcdef" }
 
-      local res_values, err = validate(values, schema)
-      assert.falsy(res_values)
+      local valid, err = validate(values, schema)
+      assert.falsy(valid)
       assert.are.same("unexpected is an unknown field", err.unexpected)
-    end)
-
-    it("should validate against a custom function", function()
-      -- Success
-      local values = { string = "httpbin entity", url = "httpbin.org", number = 123 }
-
-      local res_values, err = validate(values, schema)
-      assert.falsy(err)
-      assert.truthy(res_values)
-
-      -- Error
-      local values = { string = "httpbin entity", url = "httpbin.org", number = 456 }
-
-      local res_values, err = validate(values, schema)
-      assert.falsy(res_values)
-      assert.are.same("The value should be 123", err.number)
-    end)
-
-    it("should return errors if trying to pass a property of type id", function()
-      local values = { id = 1, string = "httpbin entity", url = "httpbin.org" }
-
-      local res_values, err = validate(values, schema)
-      assert.falsy(res_values)
-      assert.truthy(err)
-      assert.are.same("id is read only", err.id)
     end)
 
     it("should be able to return multiple errors at once", function()
       local values = { id = 1, string = "httpbin entity", url = "httpbin.org", unexpected = "abcdef" }
 
-      local res_values, err = validate(values, schema)
-      assert.falsy(res_values)
+      local valid, err = validate(values, schema)
+      assert.falsy(valid)
       assert.truthy(err)
-      assert.are.same("id is read only", err.id)
+      --assert.are.same("id is read only", err.id)
       assert.are.same("unexpected is an unknown field", err.unexpected)
     end)
 
