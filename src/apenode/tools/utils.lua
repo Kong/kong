3b318725057ed164e753f510be1d44f4fe36f271
thibaultCha <thibaultcha@me.com>
2015-02-05 17:06:23 -0800

    Cleanup

--- src/apenode/tools/utils.lua
+++ src/apenode/tools/utils.lua
@@ -28,42 +28,26 @@ function _M.reverse_table(arr)
   return reversed
 end
 
--- Builds a querystring from a table, separated by `&`
--- @param tab The key/value parameters
--- @param key The parent key if the value is multi-dimensional (optional)
--- @return a string representing the built querystring
-function _M.build_query(tab, key)
-  if ngx then
-    return ngx.encode_args(tab)
+-- Add an error message to a key/value table
+-- Can accept a nil argument, and if is nil, will initialize the table
+--
+-- @param {table|nil} errors Table to attach the error to
+-- @param {string} k Key of the error
+-- @param v Value of the error
+-- @return {table} errors
+function _M.add_error(errors, k, v)
+  if not errors then errors = {} end
+
+  if errors and errors[k] then
+    local list = {}
+    table.insert(list, errors[k])
+    table.insert(list, v)
+    errors[k] = list
   else
-    local query = {}
-    local keys = {}
-
-    for k in pairs(tab) do
-      keys[#keys+1] = k
-    end
-
-    table.sort(keys)
-
-    for _,name in ipairs(keys) do
-      local value = tab[name]
-      if key then
-        name = string.format("%s[%s]", tostring(key), tostring(name))
-      end
-      if type(value) == "table" then
-        query[#query+1] = _M.build_query(value, name)
-      else
-        local value = tostring(value)
-        if value ~= "" then
-          query[#query+1] = string.format("%s=%s", name, value)
-        else
-          query[#query+1] = name
-        end
-      end
-    end
-
-    return table.concat(query, "&")
+    errors[k] = v
   end
+
+  return errors
 end
 
 --
@@ -90,8 +74,17 @@ end
 --
 -- Date utils
 --
+
+-- Returns a UNIX timestamp formatted in Coordinated Universal Time
+-- If ngx is available, will use ngx.now() stripping out the milliseconds
+-- Otherwise, will use os.time
+-- @return {number} Current UTC timestamp
 function _M.get_utc()
-  return os.time(os.date("!*t", os.time()))
+  if ngx then
+    return math.floor(ngx.now())
+  else
+    return os.time(os.date("!*t"))
+  end
 end
 
 --[[
@@ -255,6 +248,45 @@ end
 --
 -- HTTP calls utils
 --
+
+-- Builds a querystring from a table, separated by `&`
+-- @param tab The key/value parameters
+-- @param key The parent key if the value is multi-dimensional (optional)
+-- @return a string representing the built querystring
+function _M.build_query(tab, key)
+  if ngx then
+    return ngx.encode_args(tab)
+  else
+    local query = {}
+    local keys = {}
+
+    for k in pairs(tab) do
+      keys[#keys+1] = k
+    end
+
+    table.sort(keys)
+
+    for _,name in ipairs(keys) do
+      local value = tab[name]
+      if key then
+        name = string.format("%s[%s]", tostring(key), tostring(name))
+      end
+      if type(value) == "table" then
+        query[#query+1] = _M.build_query(value, name)
+      else
+        local value = tostring(value)
+        if value ~= "" then
+          query[#query+1] = string.format("%s=%s", name, value)
+        else
+          query[#query+1] = name
+        end
+      end
+    end
+
+    return table.concat(query, "&")
+  end
+end
+
 local function http_call(options)
   -- Set Host header accordingly
   if not options.headers["host"] then
@@ -274,6 +306,7 @@ local function http_call(options)
   return resp[1], code, headers
 end
 
+-- GET methpd
 function _M.get(url, querystring, headers)
   if not headers then headers = {} end
 
@@ -288,20 +321,7 @@ function _M.get(url, querystring, headers)
   }
 end
 
-function _M.delete(url, querystring, headers)
-  if not headers then headers = {} end
-
-  if querystring then
-    url = string.format("%s?%s", url, _M.build_query(querystring))
-  end
-
-  return http_call {
-    method = "DELETE",
-    url = url,
-    headers = headers
-  }
-end
-
+-- POST methpd
 function _M.post(url, form, headers)
   if not headers then headers = {} end
   if not form then form = {} end
@@ -315,6 +335,21 @@ function _M.post(url, form, headers)
     url = url,
     headers = headers,
     source = ltn12.source.string(body)
+  }
+end
+
+-- DELETE methpd
+function _M.delete(url, querystring, headers)
+  if not headers then headers = {} end
+
+  if querystring then
+    url = string.format("%s?%s", url, _M.build_query(querystring))
+  end
+
+  return http_call {
+    method = "DELETE",
+    url = url,
+    headers = headers
   }
 end
 
