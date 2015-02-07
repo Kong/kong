cea9ca50a387c753ab36283d3efbcee72be70d4f
thefosk <marco@mashape.com>
2015-02-06 19:08:43 -0800

    adding constant

--- src/apenode/plugins/authentication/schema.lua
+++ src/apenode/plugins/authentication/schema.lua
@@ -1,20 +1,22 @@
 local stringy = require "stringy"
 local utils = require "apenode.tools.utils"
 
+local BASIC = "basic"
+
 local function check_authentication_key_names(names, plugin_value)
   if names and type(names) ~= "table" then
     return false, "You need to specify an array"
   end
 
-  if plugin_value.authentication_type == "basic" or names and utils.table_size(names) > 0 then
+  if plugin_value.authentication_type == BASIC or (names and utils.table_size(names) > 0) then
     return true
   else
-    return false, "You need to specify a query or header name for this authentication type"
+    return false, "This field is not available for \""..BASIC.."\" authentication"
   end
 end
 
 return {
-  authentication_type = { type = "string", required = true, enum = {"query", "basic", "header"} },
-  authentication_key_names = { type = "table", required = true, func = check_authentication_key_names },
+  authentication_type = { type = "string", required = true, enum = {"query", BASIC, "header"} },
+  authentication_key_names = { type = "table", func = check_authentication_key_names },
   hide_credentials = { type = "boolean", default = false }
 }
