f321e201885cf4cfd91405edee2c007ef2aaa229
thibaultCha <thibaultcha@me.com>
2015-03-03 18:18:40 -0800

    Unified nginx and kong configurations
    kong/yml now contains the nginx configuration as a string, and bin/kong
    is responsible for extracting it before running nginx.
    
    config.lua handles anything related to configuration files (creating new
    config for environments or extracting the nginx config).

--- spec/unit/utils_spec.lua
+++ spec/unit/utils_spec.lua
@@ -4,20 +4,26 @@ local cjson = require "cjson"
 describe("Utils #utils", function()
 
   describe("Cache", function()
+
     it("should return a valid API cache key", function()
       assert.are.equal("apis/httpbin.org", utils.cache_api_key("httpbin.org"))
     end)
+
     it("should return a valid PLUGIN cache key", function()
       assert.are.equal("plugins/authentication/api123/app123", utils.cache_plugin_key("authentication", "api123", "app123"))
       assert.are.equal("plugins/authentication/api123", utils.cache_plugin_key("authentication", "api123"))
     end)
+
     it("should return a valid Application cache key", function()
       assert.are.equal("applications/username", utils.cache_application_key("username"))
     end)
+
   end)
 
   describe("HTTP", function()
+
     describe("GET", function()
+
       it("should return a valid GET response", function()
         local response, status, headers = utils.get("http://httpbin.org/get", {name = "Mark"}, {Custom = "pippo"})
         assert.are.equal(200, status)
@@ -27,9 +33,11 @@ describe("Utils #utils", function()
         assert.are.equal("Mark", parsed_response.args.name)
         assert.are.equal("pippo", parsed_response.headers.Custom)
       end)
+
     end)
 
     describe("POST", function()
+
       it("should return a valid POST response", function()
         local response, status, headers = utils.post("http://httpbin.org/post", {name = "Mark"}, {Custom = "pippo"})
         assert.are.equal(200, status)
@@ -39,9 +47,11 @@ describe("Utils #utils", function()
         assert.are.equal("Mark", parsed_response.form.name)
         assert.are.equal("pippo", parsed_response.headers.Custom)
       end)
+
     end)
 
     describe("PUT", function()
+
       it("should return a valid PUT response", function()
         local response, status, headers = utils.put("http://httpbin.org/put", {name="Mark"}, {Custom = "pippo"})
         assert.are.equal(200, status)
@@ -51,18 +61,21 @@ describe("Utils #utils", function()
         assert.are.equal("Mark", parsed_response.json.name)
         assert.are.equal("pippo", parsed_response.headers.Custom)
       end)
+
     end)
 
     describe("DELETE", function()
+
       it("should return a valid DELETE response", function()
-      local response, status, headers = utils.delete("http://httpbin.org/delete", {name = "Mark"}, {Custom = "pippo"})
-      assert.are.equal(200, status)
-      assert.truthy(headers)
-      assert.truthy(response)
-      local parsed_response = cjson.decode(response)
-      assert.are.equal("Mark", parsed_response.args.name)
-      assert.are.equal("pippo", parsed_response.headers.Custom)
+        local response, status, headers = utils.delete("http://httpbin.org/delete", {name = "Mark"}, {Custom = "pippo"})
+        assert.are.equal(200, status)
+        assert.truthy(headers)
+        assert.truthy(response)
+        local parsed_response = cjson.decode(response)
+        assert.are.equal("Mark", parsed_response.args.name)
+        assert.are.equal("pippo", parsed_response.headers.Custom)
       end)
+
     end)
   end)
 
