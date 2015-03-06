25292f8551a6a7adb46332b1ec83b5a49cee7978
thefosk <marco@mashape.com>
2015-03-05 18:48:33 -0800

    JSON errors for API

--- src/kong/web/app.lua
+++ src/kong/web/app.lua
@@ -9,7 +9,6 @@ local Applications = require "kong.web.routes.applications"
 app = lapis.Application()
 
 app:get("/", function(self)
-
   local db_plugins, err = dao.plugins:find_distinct()
   if err then
     ngx.log(ngx.ERR, err)
@@ -29,6 +28,26 @@ end)
 
 app.handle_404 = function(self)
   return utils.not_found()
+end
+
+app.handle_error = function(self, err, trace)
+  ngx.log(ngx.ERR, err.."\n"..trace)
+
+  local iterator, iter_err = ngx.re.gmatch(err, ".+:\\d+:\\s*(.+)")
+  if iter_err then
+    ngx.log(ngx.ERR, err)
+  end
+
+  local m, err = iterator()
+  if err then
+    ngx.log(ngx.ERR, err)
+  end
+
+  if m and table.getn(m) > 0 then
+    utils.show_error(500, m[1])
+  else
+    ngx.log(ngx.ERR, "Can't parse error")
+  end
 end
 
 -- Load controllers
