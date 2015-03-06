50110c123d4dc41d03c2289a0fc7bf343017a3fd
thefosk <marco@mashape.com>
2015-03-05 16:06:52 -0800

    Using os.execute

--- spec/spec_helpers.lua
+++ spec/spec_helpers.lua
@@ -35,7 +35,7 @@ function _M.start_kong(conf_file, skip_wait)
   local conf_file = conf_file and conf_file or TEST_CONF_FILE
   local result, exit_code = _M.os_execute(KONG_BIN.." -c "..conf_file.." start")
   if not skip_wait then
-    _M.os_execute("while ! [ `ps aux | grep nginx | grep -c -v grep` -gt 0 ]; do sleep 1; done")
+    os.execute("while ! [ `ps aux | grep nginx | grep -c -v grep` -gt 0 ]; do sleep 1; done")
   end
   return result, exit_code
 end
@@ -43,7 +43,7 @@ end
 function _M.stop_kong(conf_file)
   local conf_file = conf_file and conf_file or TEST_CONF_FILE
   local exit_code = _M.os_execute(KONG_BIN.." -c "..conf_file.." stop")
-  _M.os_execute("while [ `ps aux | grep nginx | grep -c -v grep` -gt 0 ]; do sleep 1; done")
+  os.execute("while [ `ps aux | grep nginx | grep -c -v grep` -gt 0 ]; do sleep 1; done")
 end
 
 function _M.prepare_db()
