232a812f1bf8d7a82217b5d8d246449aa64d621a
thefosk <marco@mashape.com>
2015-03-12 21:02:34 -0700

    removing inspect

--- src/kong/plugins/networklog/schema.lua
+++ src/kong/plugins/networklog/schema.lua
@@ -1,7 +1,4 @@
 local function check_tcp(v, t)
-  local inspect = require "inspect"
-  print(inspect(v))
-  print(inspect(t))
   if t and t.type == "tcp" and v == nil then
     return false, "This property is required for the \"tcp\" type"
   end
