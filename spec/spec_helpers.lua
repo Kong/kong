a5e186058c1ca9c9d316d79ad1b5c0408cdd000a
Thibault Charbonnier <thibaultcha@me.com>
2015-03-17 11:25:19 -0700

    style: cleaning specs and spec_helpers

--- spec/spec_helpers.lua
+++ spec/spec_helpers.lua
@@ -13,10 +13,9 @@ local configuration, dao_factory = utils.load_configuration_and_dao(TEST_CONF_FI
 local migrations = Migrations(dao_factory)
 local faker = Faker(dao_factory)
 
-local _M = {
-  KONG_BIN = KONG_BIN
-}
+local _M = {}
 
+_M.KONG_BIN = KONG_BIN
 _M.CONF_FILE = TEST_CONF_FILE
 _M.PROXY_URL = PROXY_URL
 _M.API_URL = API_URL
