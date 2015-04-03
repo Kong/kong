b1a69ed9b0d0bd404f909f60f06ba5bb4714ae70
thefosk <marco@mashape.com>
2015-04-02 20:29:11 -0700

    closes #97

--- spec/integration/proxy/realip_spec.lua
+++ spec/integration/proxy/realip_spec.lua
@@ -13,7 +13,7 @@ uuid.seed()
 local STUB_GET_URL = spec_helper.STUB_GET_URL
 local TEST_CONF = "kong_TEST.yml"
 
-describe("Logging Plugins #proxy", function()
+describe("Real IP", function()
 
   setup(function()
     spec_helper.prepare_db()
@@ -25,45 +25,41 @@ describe("Logging Plugins #proxy", function()
     spec_helper.reset_db()
   end)
 
-  describe("Invalid API", function()
+  it("should parse the correct IP", function()
+    local uuid,_ = string.gsub(uuid(), "-", "")
 
-    it("should log to file", function()
-      local uuid,_ = string.gsub(uuid(), "-", "")
+    -- Making the request
+    local response, status, headers = http_client.get(STUB_GET_URL, {apikey = "apikey123"}, {host = "test.com", ["X-Forwarded-For"] = "4.4.4.4, 1.1.1.1, 5.5.5.5", file_log_uuid = uuid})
+    assert.are.equal(200, status)
 
-      -- Making the request
-      local response, status, headers = http_client.get(STUB_GET_URL, {apikey = "apikey123"}, {host = "test.com", ["X-Forwarded-For"] = "4.4.4.4, 1.1.1.1, 5.5.5.5", file_log_uuid = uuid})
-      assert.are.equal(200, status)
-
-      -- Reading the log file and finding the entry
-      local configuration = yaml.load(IO.read_file(TEST_CONF))
-      assert.truthy(configuration)
-      local error_log = IO.read_file(configuration.nginx_working_dir.."/logs/error.log")
-      local line
-      local lines = stringy.split(error_log, "\n")
-      for _, v in ipairs(lines) do
-        if string.find(v, uuid) then
-          line = v
-          break
-        end
+    -- Reading the log file and finding the entry
+    local configuration = yaml.load(IO.read_file(TEST_CONF))
+    assert.truthy(configuration)
+    local error_log = IO.read_file(configuration.nginx_working_dir.."/logs/error.log")
+    local line
+    local lines = stringy.split(error_log, "\n")
+    for _, v in ipairs(lines) do
+      if string.find(v, uuid) then
+        line = v
+        break
       end
-      assert.truthy(line)
+    end
+    assert.truthy(line)
 
-      -- Matching the Json 
-      local iterator, iter_err = rex.gmatch(line, "\\s+({.+})\\s+")
-      if not iterator then
-        error(iter_err)
-      end
-      local m, err = iterator()
-      if err then
-        error(err)
-      end
-      assert.truthy(m)
+    -- Matching the Json 
+    local iterator, iter_err = rex.gmatch(line, "\\s+({.+})\\s+")
+    if not iterator then
+      error(iter_err)
+    end
+    local m, err = iterator()
+    if err then
+      error(err)
+    end
+    assert.truthy(m)
 
-      local log_message = cjson.decode(m)
-      assert.are.same("4.4.4.4", log_message.ip)
-      assert.are.same(uuid, log_message.request.headers.file_log_uuid)
-    end)
-
+    local log_message = cjson.decode(m)
+    assert.are.same("4.4.4.4", log_message.ip)
+    assert.are.same(uuid, log_message.request.headers.file_log_uuid)
   end)
 
 end)
