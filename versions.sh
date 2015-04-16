15441ef872c46696562266470435007b019451e8
Thibault Charbonnier <thibaultcha@me.com>
2015-04-17 00:09:13 +0200

    style: polishing versions.sh

--- versions.sh
+++ versions.sh
@@ -1,11 +1,11 @@
-# Travis versions
+# Travis only
 CASSANDRA_VERSION=2.1.2
 
-# Travis and package versions
+# Travis and package builder
 LUAROCKS_VERSION=2.2.1
 OPENRESTY_VERSION=1.7.10.2rc0
 
-# Package versions
+# Package builder only
 LUA_VERSION=5.1.5
 PCRE_VERSION=8.36
 KONG_VERSION=0.1.1beta-2
