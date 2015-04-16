24cb70949cf92cf787ef1785686bf527431d7882
Thibault Charbonnier <thibaultcha@me.com>
2015-04-16 14:52:53 +0200

    chore: unify dependencies versions

--- .travis/setup_cassandra.sh
+++ .travis/setup_cassandra.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-CASSANDRA_BASE=apache-cassandra-$CASSANDRA
+source ./versions.sh
+
+CASSANDRA_BASE=apache-cassandra-$CASSANDRA_VERSION
 
 sudo rm -rf /var/lib/cassandra/*
-curl http://www.us.apache.org/dist/cassandra/2.1.2/$CASSANDRA_BASE-bin.tar.gz | tar xz
+curl -v http://www.us.apache.org/dist/cassandra/$CASSANDRA_VERSION/$CASSANDRA_BASE-bin.tar.gz | tar xz
 sudo sh $CASSANDRA_BASE/bin/cassandra
