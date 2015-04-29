5a3354b5cc4cd8cbaab015d62a64a02611f3222e
Thibault Charbonnier <thibaultcha@me.com>
2015-04-29 16:40:51 +0200

    fix: friendlier 'API not fond' error message
    - 'API not found with host %s' is the new error message, allowing the
      user to better understand what is happening
    - Minor changes to the resolver plugin: no need to strip the header +
      tests

--- kong/resolver/handler.lua
+++ kong/resolver/handler.lua
@@ -1,21 +1,29 @@
+-- Kong resolver core-plugin
+--
+-- This core-plugin is executed before any other, and allows to map a Host header
+-- to an API added to Kong. If the API was found, it will set the $backend_url variable
+-- allowing nginx to proxy the request as defined in the nginx configuration.
+--
+-- Executions: 'access', 'header_filter'
+
 local access = require "kong.resolver.access"
 local header_filter = require "kong.resolver.header_filter"
 local BasePlugin = require "kong.plugins.base_plugin"
 
-local CoreHandler = BasePlugin:extend()
+local ResolverHandler = BasePlugin:extend()
 
-function CoreHandler:new()
-  CoreHandler.super.new(self, "resolver")
+function ResolverHandler:new()
+  ResolverHandler.super.new(self, "resolver")
 end
 
-function CoreHandler:access(conf)
-  CoreHandler.super.access(self)
+function ResolverHandler:access(conf)
+  ResolverHandler.super.access(self)
   access.execute(conf)
 end
 
-function CoreHandler:header_filter(conf)
-  CoreHandler.super.header_filter(self)
+function ResolverHandler:header_filter(conf)
+  ResolverHandler.super.header_filter(self)
   header_filter.execute(conf)
 end
 
-return CoreHandler
+return ResolverHandler
