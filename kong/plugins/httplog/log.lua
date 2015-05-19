b239d56b1050f746585d63ec50bc2341b72ef249
Shashi Ranjan <shashi@mashape.com>
2015-05-19 16:34:00 -0700

    http logging using tcp sockets

--- kong/plugins/httplog/log.lua
+++ kong/plugins/httplog/log.lua
@@ -1,12 +1,64 @@
 local cjson = require "cjson"
-local http_client = require "kong.tools.http_client"
-
-local APPLICATION_JSON_TYPE = "application/json"
+local url = require "socket.url"
 
 local _M = {}
 
+-- Generates http payload .
+-- @param `method` http method to be used to send data
+-- @param `parsed_url` contains the host details     
+-- @param `message`  Message to be logged
+-- @return `payload` http payload
+local function generate_post_payload(method, parsed_url, message)
+  local body = cjson.encode(message);
+  local payload = string.format("%s %s HTTP/1.1\r\nHost: %s\r\nConnection: Keep-Alive\r\nContent-Type: application/json\r\nContent-Length: %s\r\n\r\n%s", 
+    method:upper(), parsed_url.path, parsed_url.host, string.len(body), body)
+  return payload
+end
+
+-- parse host url  .
+-- @param `url`  host url     
+-- @return a table with host details like domain name, port, path etc
+local function parse_url(url)
+  local parsed_url = url.parse(parsed_url)
+  if not parsed_url.port then
+    if parsed_url.scheme == "http" then 
+      parsed_url.port = 80
+     elseif parsed_url.scheme == "https" then
+      parsed_url.port = 443
+     end
+  end
+  return parsed_url
+end
+
+-- Log to a end point.
+-- @param `premature` 
+-- @param `conf`      
+-- @param `message`  Message to be logged
 local function log(premature, conf, message)
-  local response, status, headers = http_client.post(conf.logging_url, cjson.encode(message), { ["content-type"] = APPLICATION_JSON_TYPE })
+  local ok, err
+  local parsed_url = parse_url(conf.logging_url)
+  local host = parsed_url.host
+  local port = tonumber(parsed_url.port)
+  
+  local sock = ngx.socket.tcp()
+  sock:settimeout(conf.timeout)
+
+  ok, err = sock:connect(host, port)
+  if not ok then
+    ngx.log(ngx.ERR, "failed to connect to "..host..":"..tostring(port)..": ", err)
+    return
+  end
+
+  ok, err = sock:send(generate_post_payload(conf.method, parsed_url, message).."\r\n")
+  if not ok then
+    ngx.log(ngx.ERR, "failed to send data to "..host..":"..tostring(port)..": ", err)
+  end
+
+  ok, err = sock:setkeepalive(conf.keepalive)
+  if not ok then
+    ngx.log(ngx.ERR, "failed to keepalive to "..host..":"..tostring(port)..": ", err)
+    return
+  end
 end
 
 function _M.execute(conf)
--- kong/plugins/httplog/schema.lua
+++ kong/plugins/httplog/schema.lua
@@ -1,3 +1,6 @@
 return {
-  logging_url = { required = true, type = "string" }
+  logging_url = { required = true, type = "string" },
+  method = { require = false, default = "POST", enum = { "POST", "PUT", "PATCH" } },
+  timeout = { required = false, default = 10000, type = "number" },
+  keepalive = { required = false, default = 60000, type = "number" }
 }
