b6b6ed588a4d6e713a223fb7d50a2056f92bea8f
thefosk <marco@mashape.com>
2015-05-21 02:27:16 -0700

    removing print

--- kong/plugins/ssl/access.lua
+++ kong/plugins/ssl/access.lua
@@ -3,9 +3,6 @@ local responses = require "kong.tools.responses"
 local _M = {}
 
 function _M.execute(conf)
-  print(conf.only_https)
-  print(ngx.var.scheme:lower())
-
   if conf.only_https and ngx.var.scheme:lower() ~= "https" then
     ngx.header["connection"] = { "Upgrade" }
     ngx.header["upgrade"] = "TLS/1.0, HTTP/1.1"
