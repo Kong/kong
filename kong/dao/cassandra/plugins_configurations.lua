cd59b343fa5411adeeeb7e37a57f86db1be66e3a
Thibault Charbonnier <thibaultcha@me.com>
2015-05-27 00:05:04 +0200

    feat(schemas) auto-required for sub-schemas
    This allows for ex the plugins_configurations `value` property to only
    be required if a plugin really has a configuration (schema.lua).

--- kong/dao/cassandra/plugins_configurations.lua
+++ kong/dao/cassandra/plugins_configurations.lua
@@ -19,7 +19,7 @@ local SCHEMA = {
   api_id = { type = constants.DATABASE_TYPES.ID, required = true, foreign = true, queryable = true },
   consumer_id = { type = constants.DATABASE_TYPES.ID, foreign = true, queryable = true, default = constants.DATABASE_NULL_ID },
   name = { type = "string", required = true, queryable = true, immutable = true },
-  value = { type = "table", required = true, schema = load_value_schema },
+  value = { type = "table", schema = load_value_schema },
   enabled = { type = "boolean", default = true },
   created_at = { type = constants.DATABASE_TYPES.TIMESTAMP }
 }
