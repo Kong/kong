6f52711e5d53812569959c2665e91a62cee69188
thefosk <marco@mashape.com>
2015-05-26 16:54:36 -0700

    travis

--- .travis/setup_kong.sh
+++ .travis/setup_kong.sh
@@ -10,5 +10,10 @@ sudo apt-get install wget curl tar make gcc unzip git liblua5.1-0-dev
 # Installing dependencies required for Kong
 sudo apt-get install sudo netcat lua5.1 openssl libpcre3 dnsmasq
 
-# Installing Kong
+# Installing Kong and its dependencies
 sudo dpkg -i ./.travis/kong-*.precise_all.deb
+
+# Removing Kong only
+sudo luarocks remove kong --force
+sudo rm -rf /etc/kong
+sudo rm -rf /usr/local/kong
