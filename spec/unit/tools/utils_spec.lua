1d6ac5812ab0001fdc9e29ed90afba7ddabce880
Thibault Charbonnier <thibaultcha@me.com>
2015-06-05 15:03:17 +0200

    refactor: cleaning up utils from useless functions
    Former-commit-id: b68227145a63f7426fa6965f158d3a9b2306d19d

--- spec/unit/tools/utils_spec.lua
+++ spec/unit/tools/utils_spec.lua
@@ -2,80 +2,6 @@ local utils = require "kong.tools.utils"
 
 describe("Utils", function()
   describe("tables", function()
-    describe("#sort_table_iter()", function()
-
-      it("should sort a table in ascending order by its keys without order set", function()
-        local t = { [1] = "one", [3] = "three", [2] = "two" }
-        local keyset = {}
-        for k,v in utils.sort_table_iter(t) do
-          table.insert(keyset, k)
-        end
-
-        assert.are.same({1, 2, 3}, keyset)
-      end)
-
-      it("should sort a table in ascending order by its keys with ascending order set", function()
-        local t = { [1] = "one", [3] = "three", [2] = "two" }
-        local keyset = {}
-        for k,v in utils.sort_table_iter(t, utils.sort.ascending) do
-          table.insert(keyset, k)
-        end
-
-        assert.are.same({1, 2, 3}, keyset)
-      end)
-
-      it("should sort a table in descending order by its keys with descending order set", function()
-        local t = { [1] = "one", [3] = "three", [2] = "two" }
-        local keyset = {}
-        for k,v in utils.sort_table_iter(t, utils.sort.descending) do
-          table.insert(keyset, k)
-        end
-
-        assert.are.same({3, 2, 1}, keyset)
-      end)
-
-      it("should sort an array in ascending order by its keys without order set", function()
-        local t = { 3, 1, 2 }
-        local keyset = {}
-        for k,v in utils.sort_table_iter(t) do
-          table.insert(keyset, k)
-        end
-
-        assert.are.same({1, 2, 3}, keyset)
-      end)
-
-      it("should sort an array in ascending order by its keys with ascending order set", function()
-        local t = { 3, 1, 2 }
-        local keyset = {}
-        for k,v in utils.sort_table_iter(t, utils.sort.ascending) do
-          table.insert(keyset, k)
-        end
-
-        assert.are.same({1, 2, 3}, keyset)
-      end)
-
-      it("should sort an array in descending order by its keys with descending order set", function()
-        local t = { 3, 1, 2 }
-        local keyset = {}
-        for k,v in utils.sort_table_iter(t, utils.sort.descending) do
-          table.insert(keyset, k)
-        end
-
-        assert.are.same({3, 2, 1}, keyset)
-      end)
-
-    end)
-
-    describe("#is_empty()", function()
-
-      it("should return true for empty table, false otherwise", function()
-        assert.True(utils.is_empty({}))
-        assert.is_not_true(utils.is_empty({ foo = "bar" }))
-        assert.is_not_true(utils.is_empty({ "foo", "bar" }))
-      end)
-
-    end)
-
     describe("#table_size()", function()
 
       it("should return the size of a table", function()
@@ -83,15 +9,6 @@ describe("Utils", function()
         assert.are.same(1, utils.table_size({ foo = "bar" }))
         assert.are.same(2, utils.table_size({ foo = "bar", bar = "baz" }))
         assert.are.same(2, utils.table_size({ "foo", "bar" }))
-      end)
-
-    end)
-
-    describe("#reverse_table()", function()
-
-      it("should reverse an array", function()
-        local arr = { "a", "b", "c", "d" }
-        assert.are.same({ "d", "c", "b", "a" }, utils.reverse_table(arr))
       end)
 
     end)
