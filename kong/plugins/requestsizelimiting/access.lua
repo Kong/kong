e13774ebf6c4b44b2864b015f430e5d6fe69a5bf
ajk <ajk@adobe.com>
2015-06-09 22:26:23 +0530

    style(requestsizelimiting) typo + used ngx.req.headers

--- kong/plugins/requestsizelimiting/access.lua
+++ kong/plugins/requestsizelimiting/access.lua
@@ -5,10 +5,9 @@ local _M = {}
 
 local CONTENT_LENGTH = "content-length"
 
-local function check_size(length, allowed_size)
+local function check_size(length, allowed_size, headers)
   local allowed_bytes_size = allowed_size * 100000
   if length > allowed_bytes_size then
-    local headers = ngx.req.get_headers()
     if headers.expect and stringy.strip(headers.expect:lower()) == "100-continue" then
       return response.send(417, "Request size limit exceeded")
     else
@@ -25,13 +24,13 @@ end
 function _M.execute(conf)
   local headers = ngx.req.get_headers()
   if headers[CONTENT_LENGTH] then
-    check_size(tonumber(headers[CONTENT_LENGTH]), conf.allowed_payload_size)
+    check_size(tonumber(headers[CONTENT_LENGTH]), conf.allowed_payload_size, headers)
   else
     -- If the request body is too big, this could consume too much memory (to check)
     ngx.req.read_body()
     local data = ngx.req.get_body_data()
     if data then
-      check_size(string.len(data), conf.allowed_payload_size)
+      check_size(string.len(data), conf.allowed_payload_size, headers)
     end
   end
 
