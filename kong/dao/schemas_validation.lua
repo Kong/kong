42cd98a9f440cb52ea8f050237839338ac3588ac
Thibault Charbonnier <thibaultcha@me.com>
2015-06-13 22:55:05 +0200

    chore(dao) generated drop query + cleanup update
    Former-commit-id: c1770df216e0a76687b9e5f44f230d4a428e266d

--- kong/dao/schemas_validation.lua
+++ kong/dao/schemas_validation.lua
@@ -14,15 +14,15 @@ local POSSIBLE_TYPES = {
   timestamp = true
 }
 
-local types_validation = {
+local custom_types_validation = {
   ["id"] = function(v) return type(v) == "string" end,
   ["timestamp"] = function(v) return type(v) == "number" end,
   ["array"] = function(v) return utils.is_array(v) end
 }
 
 local function validate_type(field_type, value)
-  if types_validation[field_type] then
-    return types_validation[field_type](value)
+  if custom_types_validation[field_type] then
+    return custom_types_validation[field_type](value)
   end
   return type(value) == field_type
 end
@@ -44,27 +44,30 @@ function _M.validate(t, schema, options)
   -- Check the given table against a given schema
   for column, v in pairs(schema) do
 
-    -- [DEFAULT] Set default value for the field if given
-    if t[column] == nil and v.default ~= nil then
-      if type(v.default) == "function" then
-        t[column] = v.default(t)
-      else
-        t[column] = v.default
+    if not options.is_update then
+      -- [DEFAULT] Set default value for the field if given
+      if t[column] == nil and v.default ~= nil then
+        if type(v.default) == "function" then
+          t[column] = v.default(t)
+        else
+          t[column] = v.default
+        end
       end
-    -- [IMMUTABLE] check immutability of a field if updating
-    elseif options.is_update and t[column] ~= nil and v.immutable and not v.required then
-      errors = utils.add_error(errors, column, column.." cannot be updated")
+
+      -- [INSERT_VALUE]
+      if v.dao_insert_value and type(options.dao_insert) == "function" then
+        t[column] = options.dao_insert(v)
+      end
+    else
+      -- [IMMUTABLE] check immutability of a field if updating
+      if options.is_update and t[column] ~= nil and v.immutable and not v.required then
+        errors = utils.add_error(errors, column, column.." cannot be updated")
+      end
     end
 
-    -- [INSERT_VALUE]
-    if v.dao_insert_value and not options.is_update and type(options.dao_insert) == "function" then
-      t[column] = options.dao_insert(v)
-    end
-
-    if v.type ~= nil and t[column] ~= nil then
-      -- [TYPE] Check if type is valid. Boolean and Numbers as strings are accepted and converted
+    -- [TYPE] Check if type is valid. Boolean and Numbers as strings are accepted and converted
+    if t[column] ~= nil and v.type ~= nil then
       local is_valid_type
-
       -- ALIASES: number, timestamp, boolean and array can be passed as strings and will be converted
       if type(t[column]) == "string" then
         t[column] = stringy.strip(t[column])
@@ -94,7 +97,7 @@ function _M.validate(t, schema, options)
     end
 
     -- [ENUM] Check if the value is allowed in the enum.
-    if v.enum and t[column] ~= nil then
+    if t[column] ~= nil and v.enum then
       local found = false
       for _, allowed in ipairs(v.enum) do
         if allowed == t[column] then
@@ -115,18 +118,16 @@ function _M.validate(t, schema, options)
       end
     end
 
-    -- [SCHEMA] Validate a sub-schema from a table or retrived by a function
+    -- [SCHEMA] Validate a sub-schema from a table or retrieved by a function
     if v.schema then
       local sub_schema, err
       if type(v.schema) == "function" then
         sub_schema, err = v.schema(t)
+        if err then -- could not retrieve sub schema
+          errors = utils.add_error(errors, column, err)
+        end
       else
         sub_schema = v.schema
-      end
-
-      if err then
-        -- could not retrieve sub schema
-        errors = utils.add_error(errors, column, err)
       end
 
       if sub_schema then
@@ -153,20 +154,24 @@ function _M.validate(t, schema, options)
       end
     end
 
-    -- [REQUIRED] Check that required fields are set. Now that default and most other checks
-    -- have been run.
-    if v.required and (t[column] == nil or t[column] == "") then
-      errors = utils.add_error(errors, column, column.." is required")
+    if not options.is_update then
+      -- [REQUIRED] Check that required fields are set. Now that default and most other checks
+      -- have been run.
+      if v.required and (t[column] == nil or t[column] == "") then
+        errors = utils.add_error(errors, column, column.." is required")
+      end
     end
 
-    -- [FUNC] Check field against a custom function only if there is no error on that field already
-    if v.func and type(v.func) == "function" and (errors == nil or errors[column] == nil) then
-      local ok, err, new_fields = v.func(t[column], t)
-      if not ok and err then
-        errors = utils.add_error(errors, column, err)
-      elseif new_fields then
-        for k, v in pairs(new_fields) do
-          t[k] = v
+    if (options.is_update and t[column] ~= nil) or not options.is_update then
+      -- [FUNC] Check field against a custom function only if there is no error on that field already
+      if v.func and type(v.func) == "function" and (errors == nil or errors[column] == nil) then
+        local ok, err, new_fields = v.func(t[column], t)
+        if not ok and err then
+          errors = utils.add_error(errors, column, err)
+        elseif new_fields then
+          for k, v in pairs(new_fields) do
+            t[k] = v
+          end
         end
       end
     end
