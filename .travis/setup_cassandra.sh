7a2de9f459d18fda4983b0cad8bed29f16379ede
thefosk <marco@mashape.com>
2015-06-24 19:30:13 -0700

    using new container infrastructure

--- .travis/setup_cassandra.sh
+++ .travis/setup_cassandra.sh
@@ -2,6 +2,6 @@
 
 CASSANDRA_BASE=apache-cassandra-$CASSANDRA_VERSION
 
-sudo rm -rf /var/lib/cassandra/*
+rm -rf /var/lib/cassandra/*
 curl http://apache.spinellicreations.com/cassandra/$CASSANDRA_VERSION/$CASSANDRA_BASE-bin.tar.gz | tar xz
-sudo sh $CASSANDRA_BASE/bin/cassandra
+sh $CASSANDRA_BASE/bin/cassandra
