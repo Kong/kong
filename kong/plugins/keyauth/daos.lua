0f4f776223a7e0ee147514b5f8841640266df350
thefosk <marco@mashape.com>
2015-06-26 17:54:19 -0700

    Closes #48

--- kong/plugins/keyauth/daos.lua
+++ kong/plugins/keyauth/daos.lua
@@ -1,4 +1,13 @@
+local utils = require "kong.tools.utils"
+local stringy = require "stringy"
 local BaseDao = require "kong.dao.cassandra.base_dao"
+
+local function generate_if_missing(v, t, column)
+  if not v or stringy.strip(v) == "" then
+    return true, nil, { [column] = utils.random_string()}
+  end
+  return true
+end
 
 local SCHEMA = {
   primary_key = {"id"},
@@ -6,7 +15,7 @@ local SCHEMA = {
     id = { type = "id", dao_insert_value = true },
     created_at = { type = "timestamp", dao_insert_value = true },
     consumer_id = { type = "id", required = true, foreign = "consumers:id" },
-    key = { type = "string", required = true, unique = true, queryable = true }
+    key = { type = "string", required = false, unique = true, queryable = true, func = generate_if_missing }
   }
 }
 
