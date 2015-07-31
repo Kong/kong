89b66672b3d0042d855fa3a23edee5ef02365e39
thefosk <marco@mashape.com>
2015-07-30 17:42:48 -0700

    Tests improvements + CircleCI badge

---
+++ .ci/setup_kong.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+KONG_VERSION=0.4.1
+
+sudo apt-get update
+
+# Installing dependencies required to build development rocks
+sudo apt-get install wget curl tar make gcc unzip git liblua5.1-0-dev
+
+# Installing dependencies required for Kong
+sudo apt-get install sudo netcat lua5.1 openssl libpcre3 dnsmasq
+
+# Installing Kong and its dependencies
+sudo apt-get install lsb-release
+
+KONG_FILE="kong-"$KONG_VERSION"."`lsb_release -cs`"_all.deb"
+curl -L -o $KONG_FILE https://github.com/Mashape/kong/releases/download/$KONG_VERSION/$KONG_FILE
+sudo dpkg -i $KONG_FILE
+
+sudo luarocks remove kong --force
+sudo rm -rf /etc/kong
