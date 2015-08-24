982f7622abf41add3ca1018da8c1eea772c8ba2e
Thijs Schreijer <thijs@thijsschreijer.nl>
2015-08-24 21:21:43 +0200

    fix lint warning

--- spec/unit/constants_spec.lua
+++ spec/unit/constants_spec.lua
@@ -2,8 +2,8 @@ local constants = require("kong.constants")
 
 describe("Constants", function()
   it("Tests accessing database error codes", function()
-    assert.has_no_error(function() local a = constants.DATABASE_ERROR_TYPES.DATABASE end)
-    assert.has_error(function() local a = constants.DATABASE_ERROR_TYPES.ThIs_TyPe_DoEs_NoT_ExIsT end)
+    assert.has_no_error(function() local _ = constants.DATABASE_ERROR_TYPES.DATABASE end)
+    assert.has_error(function() local _ = constants.DATABASE_ERROR_TYPES.ThIs_TyPe_DoEs_NoT_ExIsT end)
   end)
 
 end)
