7f9526f9eb51c2c68828eaea719f4a63f14ef0de
Thibault Charbonnier <thibaultcha@me.com>
2015-08-26 16:42:04 -0700

    feat(api) support old basic-auth and key-auth API routes

--- kong/plugins/key-auth/api.lua
+++ kong/plugins/key-auth/api.lua
@@ -1,51 +1,57 @@
 local crud = require "kong.api.crud_helpers"
 
+local global_route = {
+  before = function(self, dao_factory, helpers)
+    crud.find_consumer_by_username_or_id(self, dao_factory, helpers)
+    self.params.consumer_id = self.consumer.id
+  end,
+
+  GET = function(self, dao_factory, helpers)
+    crud.paginated_set(self, dao_factory.keyauth_credentials)
+  end,
+
+  PUT = function(self, dao_factory)
+    crud.put(self.params, dao_factory.keyauth_credentials)
+  end,
+
+  POST = function(self, dao_factory)
+    crud.post(self.params, dao_factory.keyauth_credentials)
+  end
+}
+
+local single_route = {
+  before = function(self, dao_factory, helpers)
+    crud.find_consumer_by_username_or_id(self, dao_factory, helpers)
+    self.params.consumer_id = self.consumer.id
+
+    local data, err = dao_factory.keyauth_credentials:find_by_keys({ id = self.params.id })
+    if err then
+      return helpers.yield_error(err)
+    end
+
+    self.credential = data[1]
+    if not self.credential then
+      return helpers.responses.send_HTTP_NOT_FOUND()
+    end
+  end,
+
+  GET = function(self, dao_factory, helpers)
+    return helpers.responses.send_HTTP_OK(self.credential)
+  end,
+
+  PATCH = function(self, dao_factory)
+    crud.patch(self.params, self.credential, dao_factory.keyauth_credentials)
+  end,
+
+  DELETE = function(self, dao_factory)
+    crud.delete(self.credential, dao_factory.keyauth_credentials)
+  end
+}
+
 return {
-  ["/consumers/:username_or_id/key-auth/"] = {
-    before = function(self, dao_factory, helpers)
-      crud.find_consumer_by_username_or_id(self, dao_factory, helpers)
-      self.params.consumer_id = self.consumer.id
-    end,
-
-    GET = function(self, dao_factory, helpers)
-      crud.paginated_set(self, dao_factory.keyauth_credentials)
-    end,
-
-    PUT = function(self, dao_factory)
-      crud.put(self.params, dao_factory.keyauth_credentials)
-    end,
-
-    POST = function(self, dao_factory)
-      crud.post(self.params, dao_factory.keyauth_credentials)
-    end
-  },
-
-  ["/consumers/:username_or_id/key-auth/:id"] = {
-    before = function(self, dao_factory, helpers)
-      crud.find_consumer_by_username_or_id(self, dao_factory, helpers)
-      self.params.consumer_id = self.consumer.id
-
-      local data, err = dao_factory.keyauth_credentials:find_by_keys({ id = self.params.id })
-      if err then
-        return helpers.yield_error(err)
-      end
-
-      self.credential = data[1]
-      if not self.credential then
-        return helpers.responses.send_HTTP_NOT_FOUND()
-      end
-    end,
-
-    GET = function(self, dao_factory, helpers)
-      return helpers.responses.send_HTTP_OK(self.credential)
-    end,
-
-    PATCH = function(self, dao_factory)
-      crud.patch(self.params, self.credential, dao_factory.keyauth_credentials)
-    end,
-
-    DELETE = function(self, dao_factory)
-      crud.delete(self.credential, dao_factory.keyauth_credentials)
-    end
-  }
+  ["/consumers/:username_or_id/key-auth/"] = global_route,
+  ["/consumers/:username_or_id/key-auth/:id"] = single_route,
+  -- Deprecated in 0.5.0, maintained for backwards compatibility.
+  ["/consumers/:username_or_id/keyauth/"] = global_route,
+  ["/consumers/:username_or_id/keyauth/:id"] = single_route
 }
