558607ca1ce9e4ab38a4e2a5ddbb0d8bc987696a
Thijs Schreijer <thijs@thijsschreijer.nl>
2015-08-27 00:48:01 +0200

    doc updates, and some minor refactors for readability

--- kong/tools/utils.lua
+++ kong/tools/utils.lua
@@ -1,3 +1,6 @@
+---
+-- Module containing some general utility functions
+
 local uuid = require "uuid"
 
 -- This is important to seed the UUID generator
@@ -5,17 +8,16 @@ uuid.seed()
 
 local _M = {}
 
--- Generates a random unique string
--- @param `no_hypens` (Optional) optionally remove hypens from the output
--- @return `string`   The random string
+--- Generates a random unique string
+-- @return string  The random string (a uuid without hyphens)
 function _M.random_string()
-  local res = uuid()
-  return res:gsub("-", "")
+  return uuid():gsub("-", "")
 end
 
--- Calculates a table size
--- @param `t`       The table to use
--- @return `number` The size
+--- Calculates a table size.
+-- All entries both in array and hash part.
+-- @param t The table to use
+-- @return number The size
 function _M.table_size(t)
   local res = 0
   for _ in pairs(t) do
@@ -24,10 +26,11 @@ function _M.table_size(t)
   return res
 end
 
--- Merges two table together
--- @param `t1`      The first table
--- @param `t2`      The second table
--- @return `table`  The final table
+--- Merges two table together.
+-- A new table is created with a non-recursive copy of the provided tables
+-- @param t1 The first table
+-- @param t2 The second table
+-- @return The (new) merged table
 function _M.table_merge(t1, t2)
   local res = {}
   for k,v in pairs(t1) do res[k] = v end
@@ -35,10 +38,10 @@ function _M.table_merge(t1, t2)
   return res
 end
 
--- Checks if a value exists in a table
--- @param `arr`      The table to use
--- @param `val`      The value to check
--- @return `boolean` Returns true if the table contains the value
+--- Checks if a value exists in a table.
+-- @param arr The table to use
+-- @param val The value to check
+-- @return Returns `true` if the table contains the value, `false` otherwise
 function _M.table_contains(arr, val)
   for _, v in pairs(arr) do
     if v == val then
@@ -48,9 +51,10 @@ function _M.table_contains(arr, val)
   return false
 end
 
--- Checks if a table is an array and not an associative array
--- @param `t`        The table to use
--- @return `boolean` Returns true if the table is an array
+--- Checks if a table is an array and not an associative array.
+-- *** NOTE *** string-keys containing integers are considered valid array entries!
+-- @param t The table to check
+-- @return Returns `true` if the table is an array, `false` otherwise
 function _M.is_array(t)
   local i = 0
   for _ in pairs(t) do
@@ -60,9 +64,10 @@ function _M.is_array(t)
   return true
 end
 
--- Deep copies a table into another table
--- @param `orig`     The table to copy
--- @return `table`   Returns a copy of the input table
+--- Deep copies a table into a new table.
+-- Tables used as keys are also deep copied, as are metatables
+-- @param orig The table to copy
+-- @return Returns a copy of the input table
 function _M.deep_copy(orig)
   local copy
   if type(orig) == "table" then
@@ -78,12 +83,12 @@ function _M.deep_copy(orig)
 end
 
 
--- Add an error message to a key/value table.
--- Can accept a nil argument, and if is nil, will initialize the table.
--- @param `errors`  (Optional) Can be nil. Table to attach the error to. If nil, the table will be created.
--- @param `k`       Key on which to insert the error in the `errors` table.
--- @param `v`       Value of the error
--- @return `errors` The `errors` table with the new error inserted.
+--- Add an error message to a key/value table.
+-- If the key already exists, a sub table is created with the original and the new value.
+-- @param errors (Optional) Table to attach the error to. If `nil`, the table will be created.
+-- @param k Key on which to insert the error in the `errors` table.
+-- @param v Value of the error
+-- @return The `errors` table with the new error inserted.
 function _M.add_error(errors, k, v)
   if not errors then errors = {} end
 
@@ -99,11 +104,12 @@ function _M.add_error(errors, k, v)
   return errors
 end
 
--- Try to load a module and do not throw an error if the module was not found.
--- Will throw an error if the loading failed for another reason (ex: syntax error).
--- @param `module_name` Path of the module to load (ex: kong.plugins.keyauth.api).
--- @return `loaded`     A boolean indicating wether the module was successfully loaded or not.
--- @return `module`     The retrieved module (not loaded).
+--- Try to load a module.
+-- Will not throw an error if the module was not found, but will throw an error if the 
+-- loading failed for another reason (eg: syntax error).
+-- @param module_name Path of the module to load (ex: kong.plugins.keyauth.api).
+-- @return success A boolean indicating wether the module was found.
+-- @return module The retrieved module.
 function _M.load_module_if_exists(module_name)
   local status, res = pcall(require, module_name)
   if status then
