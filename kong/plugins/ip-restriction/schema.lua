bf546b28da91af5a2c441175cd67d0beaef676f8
thefosk <marco@mashape.com>
2015-09-20 12:32:06 -0700

    Adding check to schemas

--- kong/plugins/ip-restriction/schema.lua
+++ kong/plugins/ip-restriction/schema.lua
@@ -29,6 +29,8 @@ return {
   self_check = function(schema, plugin_t, dao, is_update)
     if utils.table_size(plugin_t.whitelist) > 0 and utils.table_size(plugin_t.blacklist) > 0 then
       return false, DaoError("You cannot set both a whitelist and a blacklist", constants.DATABASE_ERROR_TYPES.SCHEMA)
+    elseif utils.table_size(plugin_t.whitelist) == 0 and utils.table_size(plugin_t.blacklist) == 0 then
+      return false, DaoError("You must set at least a whitelist or blacklist", constants.DATABASE_ERROR_TYPES.SCHEMA)
     end
     return true
   end
