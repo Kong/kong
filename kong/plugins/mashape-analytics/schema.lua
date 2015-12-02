87e15dd8265d2560c008130212ac6c49013f449b
Thibault Charbonnier <thibaultcha@me.com>
2015-12-01 20:51:17 -0800

    hotfix(mashape-analytics) improve ALF buffer under load
    As reported by #750, the buffer is vulnerable to a heavy load because of
    its `sending_queue` of batches pending for sending. It also does not
    handle the new 207 HTTP status code returned by the collector in case of
    invalid ALFs.
    
    Changes:
    - handle 207 HTTP status code by discarding the batch. Some ALFs in it
      will have been saved, and the invalid one(s) should not be retried.
    - implement a maximum size (in MB) for the sending_queue, as suggested
      by #750. Originally, I was about to implement such a size limitation
      by "number of batches pending", but it would not be intuitive for
      users to know what fits best their use case, because ALF sizes varies
      from one API to another, and one endpoint to another. By defining it
      in MB it is easier for users to chose a value. The default value of
      10MB has been chosen after performing some benchmarking, and should
      handle from 300 to 500 req/s depending on the ALFs sizes.
      When the `sending_queue` has reached its limit, the current ALFs in
      the buffer will be **discarded**.
    - implement a retry policy. Instead of insisting on retrying to send batches
      when the collector cannot be reached, a delay is computed an
      exponentially increases on each failure to connect to the collector.
      That delay is shared by all workers. This avoids to load the collector
      when it is having difficulties and saves up bandwidth on Kong's side.
      As soon as the collector can be reached again, the delay is reset.
      Currently, the minimum retry delay is 1s and the maximum is 60s. Those
      values cannot be configured.
    - no more line jumps in logs printing responses from the collector.

--- kong/plugins/mashape-analytics/schema.lua
+++ kong/plugins/mashape-analytics/schema.lua
@@ -1,12 +1,13 @@
 return {
   fields = {
-    service_token = { type = "string", required = true },
-    environment = { type = "string" },
-    batch_size = { type = "number", default = 100 },
-    log_body = { type = "boolean", default = false },
-    delay = { type = "number", default = 2 },
-    host = { required = true, type = "string", default = "socket.analytics.mashape.com" },
-    port = { required = true, type = "number", default = 80 },
-    path = { required = true, type = "string", default = "/1.0.0/batch" }
+    service_token = {type = "string", required = true},
+    environment = {type = "string"},
+    batch_size = {type = "number", default = 100},
+    log_body = {type = "boolean", default = false},
+    delay = {type = "number", default = 2},
+    sending_queue_size = {type = "number", default = 10}, -- in mb
+    host = {required = true, type = "string", default = "socket.analytics.mashape.com"},
+    port = {required = true, type = "number", default = 80},
+    path = {required = true, type = "string", default = "/1.0.0/batch"}
   }
 }
--- spec/plugins/mashape-analytics/buffer_spec.lua
+++ spec/plugins/mashape-analytics/buffer_spec.lua
@@ -1,15 +1,20 @@
 require "kong.tools.ngx_stub"
 
-local ALFBuffer = require "kong.plugins.mashape-analytics.buffer"
 local json = require "cjson"
+local utils = require "kong.tools.utils"
+local ALFBuffer = require "kong.plugins.mashape-analytics.buffer"
 
+local MB = 1024 * 1024
 local ALF_STUB = {hello = "world"}
+local STUB_LEN = string.len(json.encode(ALF_STUB))
 local CONF_STUB = {
   batch_size = 100,
-  delay = 2
+  delay = 2,
+  host = "",
+  port = "",
+  path = "",
+  sending_queue_size = 10
 }
-local str = json.encode(ALF_STUB)
-local STUB_LEN = string.len(str)
 
 describe("ALFBuffer", function()
   local alf_buffer
@@ -17,8 +22,12 @@ describe("ALFBuffer", function()
   it("should create a new buffer", function()
     alf_buffer = ALFBuffer.new(CONF_STUB)
     assert.truthy(alf_buffer)
-    assert.equal(100, alf_buffer.MAX_ENTRIES)
-    assert.equal(2, alf_buffer.AUTO_FLUSH_DELAY)
+    assert.equal(100, alf_buffer.max_entries)
+    assert.equal(2, alf_buffer.auto_flush_delay)
+    assert.equal(10 * MB, alf_buffer.max_queue_size)
+    assert.equal("", alf_buffer.host)
+    assert.equal("", alf_buffer.port)
+    assert.equal("", alf_buffer.path)
   end)
 
   describe(":add_alf()", function()
@@ -64,42 +73,58 @@ describe("ALFBuffer", function()
         alf_buffer.flush:revert()
       end)
     end)
-    it("should call :flush() when reaching its max size", function()
-      -- How many stubs to reach the limit?
-      local COMMA_LEN = string.len(",")
-      local JSON_ARR_LEN = string.len("[]")
-      local max_n_stubs = math.ceil(ALFBuffer.MAX_BUFFER_SIZE / (STUB_LEN + COMMA_LEN)) -- + the comma after each ALF in the JSON payload
+    describe("max collector payload size", function()
+      local MAX_BUFFER_SIZE
+      setup(function()
+        -- reduce the max collector payload size to 1MB
+        -- for testing purposes.
+        MAX_BUFFER_SIZE = ALFBuffer.MAX_BUFFER_SIZE
+        ALFBuffer.MAX_BUFFER_SIZE = 0.5 * MB
+      end)
+      teardown(function()
+        ALFBuffer.MAX_BUFFER_SIZE = MAX_BUFFER_SIZE
+      end)
+      it("should call :flush() when reaching its max size", function()
+        -- How many stubs to reach the limit?
+        local COMMA_LEN = string.len(",")
+        local JSON_ARR_LEN = string.len("[]")
+        local max_n_stubs = math.ceil(ALFBuffer.MAX_BUFFER_SIZE / (STUB_LEN + COMMA_LEN)) -- + the comma after each ALF in the JSON payload
 
-      -- Create a new buffer
-      local buffer = ALFBuffer.new({batch_size = max_n_stubs + 100, delay = 2})
+        -- Create a new buffer with a batch_size big enough so that it does not prevent us from testing this behavior
+        local buffer_options = utils.table_merge(CONF_STUB, {batch_size = max_n_stubs + 100})
+        local buffer = ALFBuffer.new(buffer_options)
 
-      local s = spy.on(buffer, "flush")
+        spy.on(buffer, "flush")
+        finally(function()
+          buffer.flush:revert()
+        end)
 
-      -- Add max_n_stubs - 1 entries
-      for i = 1, max_n_stubs - 1 do
+        -- Add max_n_stubs - 1 entries
+        for i = 1, max_n_stubs - 1 do
+          buffer:add_alf(ALF_STUB)
+        end
+
+        assert.spy(buffer.flush).was_not_called()
+
+        -- We should have `(max_n_stubs - 1) * (STUB_LEN + COMMA_LEN) + JSON_ARR_LEN - COMMA_LEN` because no comma for latest object`
+        -- as our current buffer size.
+        assert.equal((max_n_stubs - 1) * (STUB_LEN + COMMA_LEN) + JSON_ARR_LEN - COMMA_LEN, buffer:get_size())
+
+        -- adding one more entry
         buffer:add_alf(ALF_STUB)
-      end
+        assert.spy(buffer.flush).was.called(1)
+      end)
+      it("should drop an ALF if it is too big by itself", function()
+        local str = string.rep(".", ALFBuffer.MAX_BUFFER_SIZE)
+        local huge_alf = {foo = str}
 
-      assert.spy(s).was_not_called()
+        local buffer = ALFBuffer.new(CONF_STUB)
 
-      -- We should have `(max_n_stubs - 1) * (STUB_LEN + COMMA_LEN) + JSON_ARR_LEN - COMMA_LEN` because no comma for latest object`
-      -- as our current buffer size.
-      assert.equal((max_n_stubs - 1) * (STUB_LEN + COMMA_LEN) + JSON_ARR_LEN - COMMA_LEN, buffer:get_size())
+        buffer:add_alf(huge_alf)
 
-      -- adding one more entry
-      buffer:add_alf(ALF_STUB)
-      assert.spy(s).was.called()
-    end)
-    it("should drop an ALF if it is too big by itself", function()
-      local str = string.rep(".", ALFBuffer.MAX_BUFFER_SIZE)
-      local huge_alf = {foo = str}
-
-      local buffer = ALFBuffer.new(CONF_STUB)
-
-      buffer:add_alf(huge_alf)
-
-      assert.equal(0, buffer.entries_size)
-      assert.equal(0, #buffer.entries)
+        assert.equal(0, buffer.entries_size)
+        assert.equal(0, #buffer.entries)
+      end)
     end)
     describe(":flush()", function()
       it("should have emptied the current buffer and added a payload to be sent", function()
