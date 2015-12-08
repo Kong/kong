94c59bdaa20d7dc792fc41cb45dd0d204fdd35c1
Neil Mansilla <neil@mansilla.com>
2015-12-07 16:37:40 -0800

    Change default keepalive (improves ingest perf)

--- kong/plugins/runscope-log/schema.lua
+++ kong/plugins/runscope-log/schema.lua
@@ -4,7 +4,7 @@ return {
     bucket_key = { required = true, type = "string" },
     access_token = { required = true, default = "", type = "string" },
     timeout = { default = 10000, type = "number" },
-    keepalive = { default = 60000, type = "number" },
+    keepalive = { default = 30, type = "number" },
     log_body = { default = false, type="boolean" }
   }
 }
