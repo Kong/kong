709971ba6233c513d3182774e5adb0a84b6a7abd
thefosk <marco@mashape.com>
2016-01-12 15:42:53 -0800

    serf in travis

---
+++ .ci/setup_serf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+if [ "$TEST_SUITE" == "unit" ]; then
+  echo "Exiting, no integration tests"
+  exit
+fi
+
+mkdir -p $SERF_DIR
+
+if [ ! "$(ls -A $SERF_DIR)" ]; then
+  pushd $SERF_DIR
+  wget https://releases.hashicorp.com/serf/$SERF_VERSION/serf_$SERF_VERSION_linux_amd64.zip
+  unzip serf_$SERF_VERSION_linux_amd64.zip
+  popd
+fi
