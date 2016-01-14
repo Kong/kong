75745d453bac7937e9b2ac51882c873339b2996f
Neil Mansilla <neil@mansilla.com>
2016-01-13 16:13:30 -0800

    Simplified timings for request & response

--- kong/plugins/log-serializers/runscope.lua
+++ kong/plugins/log-serializers/runscope.lua
@@ -8,32 +8,33 @@ function _M.serialize(ngx)
 
   -- timers
   -- @see core.handler for their definition
-  local req_send_time = ngx.ctx.KONG_PROXY_LATENCY or -1
-  local req_wait_time = ngx.ctx.KONG_WAITING_TIME or -1
-  local req_receive_time = ngx.ctx.KONG_RECEIVE_TIME or -1
+--  local req_send_time = ngx.ctx.KONG_PROXY_LATENCY or -1
+--  local req_wait_time = ngx.ctx.KONG_WAITING_TIME or -1
+--  local req_receive_time = ngx.ctx.KONG_RECEIVE_TIME or -1
 
   -- Compute the total time. If some properties were unavailable
   -- (because the proxying was aborted), then don't add the value.
-  local req_time = 0
-  for _, timer in ipairs({req_send_time, req_wait_time, req_receive_time}) do
-    if timer > 0 then
-      req_time = req_time + timer
-    end
-  end
+----  local req_time = 0
+--  for _, timer in ipairs({req_send_time, req_wait_time, req_receive_time}) do
+--    if timer > 0 then
+  --    req_time = req_time + timer
+  --  end
+--  end
   return {
     request = {
       url = ngx.var.scheme.."://"..ngx.var.host..":"..ngx.var.server_port..ngx.var.request_uri,
       method = ngx.req.get_method(),
       headers = ngx.req.get_headers(),
-      body = ngx.ctx.runscope.req_body
+      body = ngx.ctx.runscope.req_body,
+      timestamp = ngx.req.start_time()
     },
     response = {
       status = ngx.status,
       headers = ngx.resp.get_headers(),
       size_bytes = ngx.var.bytes_sent,
       body = ngx.ctx.runscope.res_body,
-      timestamp = ngx.req_start_time,
-      response_time = req_time / 1000
+      timestamp = ngx.now(),
+      response_time = ngx.now() - ngx.req.start_time()
     }
   }
 end
