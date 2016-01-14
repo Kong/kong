c182004da608d854516cda4216235b7a2371c892
Thibault Charbonnier <thibaultcha@me.com>
2016-01-13 16:06:07 -0800

    fix(file-log) better permission for file creation
    It is more common to create a log file with 644 permissions. This needed
    an update of lua-system-constants.

--- kong/plugins/file-log/log.lua
+++ kong/plugins/file-log/log.lua
@@ -11,12 +11,13 @@ local string_len = string.len
 local O_CREAT = system_constants.O_CREAT()
 local O_WRONLY = system_constants.O_WRONLY()
 local O_APPEND = system_constants.O_APPEND()
-local S_IWUSR = system_constants.S_IWUSR()
 local S_IRUSR = system_constants.S_IRUSR()
-local S_IXUSR = system_constants.S_IXUSR()
+local S_IWUSR = system_constants.S_IWUSR()
+local S_IRGRP = system_constants.S_IRGRP()
+local S_IROTH = system_constants.S_IROTH()
 
 local oflags = bit.bor(O_WRONLY, O_CREAT, O_APPEND)
-local mode = bit.bor(S_IWUSR, S_IRUSR, S_IXUSR)
+local mode = bit.bor(S_IRUSR, S_IWUSR, S_IRGRP, S_IROTH)
 
 ffi.cdef[[
 int open(char * filename, int flags, int mode);
