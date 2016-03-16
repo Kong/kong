3a48461b98335e8a53032761edd0c9a56660254e
thefosk <marco@mashape.com>
2016-03-15 17:50:15 -0700

    Fixing DAO query in cluster event handling

--- kong/dao/schemas/nodes.lua
+++ kong/dao/schemas/nodes.lua
@@ -5,5 +5,8 @@ return {
     name = { type = "string" },
     created_at = { type = "timestamp", dao_insert_value = true },
     cluster_listening_address = { type = "string", required = true }
-  }
+  },
+  marshall_event = function(self, t)
+    return { name = t.name }
+  end
 }
