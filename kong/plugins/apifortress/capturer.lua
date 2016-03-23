850df72292b88659913933ffef0e9fdefa12e44a
theirish <theirish81@gmail.com>
2016-03-23 11:17:29 +0100

    improved body capituring performance

--- kong/plugins/apifortress/capturer.lua
+++ kong/plugins/apifortress/capturer.lua
@@ -1,27 +1,19 @@
--- ©2016 API Fortress Inc.
 -- Captures the response payloads as they go through the wire
 local _M = {}
 
-local function read_response_body()
+local function capture()
   local chunk, eof = ngx.arg[1], ngx.arg[2]
-  local buffered = ngx.ctx.buffered
-  if not buffered then
-    buffered = {}
-    ngx.ctx.buffered = buffered
+  local captured_body = ngx.ctx.captured_body
+  if not captured_body then
+    captured_body = {}
+    ngx.ctx.captured_body = captured_body
   end
   if chunk ~= "" then
-    buffered[#buffered + 1] = chunk
+    captured_body[#captured_body + 1] = chunk
   end
   if eof then
-    local response_body = table.concat(buffered)
-    return response_body
+    ngx.ctx.captured_body = table.concat(captured_body)
   end
-  return nil
-end
-
-local function capture()
-  local body = read_response_body()
-  ngx.ctx.captured_body = body
 end
 
 function _M.execute(config)
