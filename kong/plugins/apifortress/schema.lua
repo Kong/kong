abcd291acf6146bc83def4a4ceb340b47b65af30
theirish <theirish81@gmail.com>
2016-03-24 13:11:33 +0100

    fixed validation and indentation

--- kong/plugins/apifortress/schema.lua
+++ kong/plugins/apifortress/schema.lua
@@ -1,15 +1,23 @@
 -- The plugin schema
 
 local function validateKeys(value)
-	return #value > 0
+  return #value > 0
+end
+
+local function validateThreshold(value)
+  return value > 0
+end
+
+local function validateProjectId(value)
+  return value > 0
 end
 
 return {
   fields = {
-   	apikey = { required = true, type = "string", func = validateKeys },
-		secret = { required = true, type = "string", func = validateKeys },
-		endpoint = {required = true, type = "string", func = validateKeys},
-		projectId = {required = true , type = "number"},
-    threshold = {required = true, type = "number"}
+    apikey = { required = true, type = "string", func = validateKeys },
+    secret = { required = true, type = "string", func = validateKeys },
+    endpoint = {required = true, type = "string", func = validateKeys },
+    projectId = {required = true , type = "number", func = validateProjectId },
+    threshold = {required = true, type = "number", func = validateThreshold }
   }
 }
