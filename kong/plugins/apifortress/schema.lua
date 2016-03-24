7b4df47e649f05481eff9a35d18b0022d35bf71c
theirish <theirish81@gmail.com>
2016-03-24 12:31:12 +0100

    improved validation

--- kong/plugins/apifortress/schema.lua
+++ kong/plugins/apifortress/schema.lua
@@ -1,7 +1,7 @@
 -- The plugin schema
 
 local function validateKeys(value)
-	return string.len(value) > 0
+	return #value > 0
 end
 
 return {
@@ -9,7 +9,7 @@ return {
    	apikey = { required = true, type = "string", func = validateKeys },
 		secret = { required = true, type = "string", func = validateKeys },
 		endpoint = {required = true, type = "string", func = validateKeys},
-		projectId = {required = true , type = "number", func = validateKeys},
-    threshold = {required = true, type = "number", func = validateKeys}
+		projectId = {required = true , type = "number"},
+    threshold = {required = true, type = "number"}
   }
 }
