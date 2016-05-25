f32f8854e3a4f1482fa50152ea92b89f97b76b29
zhangzheng <18601254750.163.com>
2016-05-25 16:02:00 +0800

    feat(plugins/influxdb) deep copy of table before modifying it

--- kong/plugins/influxdb-log/schema.lua
+++ kong/plugins/influxdb-log/schema.lua
@@ -1,4 +1,8 @@
-local influxdb_log_schema = require "kong.plugins.http-log.schema"
+local utils = require "kong.tools.utils"
+local http_log_schema = require "kong.plugins.http-log.schema"
+
+local influxdb_log_schema = utils.deep_copy(http_log_schema)
 
 influxdb_log_schema.fields.content_type.default = "application/x-www-form-urlencoded"
+
 return influxdb_log_schema
