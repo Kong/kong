71e27245fcffe7531270647d14e77bf68077f301
Thibault Charbonnier <thibaultcha@me.com>
2016-06-14 17:53:31 -0700

    tests(syslog) fix linting and boilerplate

--- spec/03-plugins/syslog/log_spec.lua
+++ spec/03-plugins/syslog/log_spec.lua
@@ -1,80 +1,74 @@
+local helpers = require "spec.helpers"
 local utils = require "kong.tools.utils"
 local cjson = require "cjson"
-local helpers = require "spec.helpers"
-local exec = require("pl.utils").executeex
+local pl_stringx = require "pl.stringx"
 
-describe("Syslog #ci", function()
-
+describe("Plugin: syslog", function()
   local client, platform
-  
   setup(function()
     helpers.dao:truncate_tables()
     assert(helpers.prepare_prefix())
 
     local api1 = assert(helpers.dao.apis:insert {
-        request_host = "logging.com", 
-        upstream_url = "http://mockbin.com"
-      })
+      request_host = "logging.com",
+      upstream_url = "http://mockbin.com"
+    })
     local api2 = assert(helpers.dao.apis:insert {
-        request_host = "logging2.com", 
-        upstream_url = "http://mockbin.com"
-      })
+      request_host = "logging2.com",
+      upstream_url = "http://mockbin.com"
+    })
     local api3 = assert(helpers.dao.apis:insert {
-        request_host = "logging3.com", 
-        upstream_url = "http://mockbin.com"
-      })
-  
+      request_host = "logging3.com",
+      upstream_url = "http://mockbin.com"
+    })
+
     assert(helpers.dao.plugins:insert {
       api_id = api1.id,
-      name = "syslog", 
-      config = { 
-        log_level = "info", 
+      name = "syslog",
+      config = {
+        log_level = "info",
         successful_severity = "warning",
         client_errors_severity = "warning",
         server_errors_severity = "warning"
-      },
+      }
     })
 
     assert(helpers.dao.plugins:insert {
       api_id = api2.id,
-      name = "syslog", 
-      config = { 
-        log_level = "err", 
+      name = "syslog",
+      config = {
+        log_level = "err",
         successful_severity = "warning",
         client_errors_severity = "warning",
         server_errors_severity = "warning"
-      },
+      }
     })
-      
+
     assert(helpers.dao.plugins:insert {
       api_id = api3.id,
-      name = "syslog", 
-      config = { 
-        log_level = "warning", 
+      name = "syslog",
+      config = {
+        log_level = "warning",
         successful_severity = "warning",
         client_errors_severity = "warning",
         server_errors_severity = "warning"
-      },
+      }
     })
-    
-    local success, code, _
-    success, code, platform, _ = exec("/bin/uname")
-    if code ~= 0 then
-      success, code, platform, _ = exec("/usr/bin/uname")
-    end
-    assert(code == 0, "Failed to retrieve platform name")
+
+    local ok, _, stdout = helpers.execute("uname")
+    assert(ok, "failed to retrieve platform name")
     assert(helpers.start_kong())
+    platform = pl_stringx.strip(stdout)
   end)
 
   teardown(function()
-    if client then client:close() end
     helpers.stop_kong()
   end)
 
   before_each(function()
     client = assert(helpers.http_client("127.0.0.1", helpers.test_conf.proxy_port))
   end)
-  
+
   after_each(function()
     if client then client:close() end
   end)
@@ -82,49 +76,42 @@ describe("Syslog #ci", function()
   local function do_test(host, expecting_same)
     local uuid = utils.random_string()
 
-    -- Making the request
-    local response = assert( client:send {
-        method = "GET",
-        path = "/request",
-        headers = {
-          host = host, 
-          sys_log_uuid = uuid,
-        },
-      })
-    
-    assert.has.res_status(200, response)
-    
+    local response = assert(client:send {
+      method = "GET",
+      path = "/request",
+      headers = {
+        host = host,
+        sys_log_uuid = uuid,
+      }
+    })
+    assert.res_status(200, response)
+
     if platform == "Darwin" then
-      local success, code, output, errout = exec("syslog -k Sender kong | tail -1")
+      local _, code, stdout = helpers.execute("syslog -k Sender kong | tail -1")
       assert.equal(0, code)
-      local message = {}
-      for w in string.gmatch(output,"{.*}") do
-        table.insert(message, w)
-      end
-      local log_message = cjson.decode(message[1])
+
+      local msg = string.match(stdout, "{.*}")
+      local json = cjson.decode(msg)
+
       if expecting_same then
-        assert.equal(uuid, log_message.request.headers.sys_log_uuid)
+        assert.equal(uuid, json.request.headers["sys-log-uuid"])
       else
-        assert.not_equal(uuid, log_message.request.headers.sys_log_uuid)
+        assert.not_equal(uuid, json.request.headers["sys-log-uuid"])
       end
-    else
-      if expecting_same then
-        local success, code, output, errout = exec("find /var/log -type f -mmin -5 2>/dev/null | xargs grep -l "..uuid)
-        assert.are.equal(0, code)
-        assert.is.True(#output > 0)
-      end
+    elseif expecting_same then
+      local _, code, stdout = helpers.execute("find /var/log -type f -mmin -5 2>/dev/null | xargs grep -l "..uuid)
+      assert.equal(0, code)
+      assert.True(#stdout > 0)
     end
   end
 
-  it("should log to syslog if log_level is lower", function()
+  it("logs to syslog if log_level is lower", function()
     do_test("logging.com", true)
   end)
-
-  it("should not log to syslog if the log_level is higher", function()
+  it("does not log to syslog if log_level is higher", function()
     do_test("logging2.com", false)
   end)
-
-  it("should log to syslog if log_level is the same", function()
+  it("logs to syslog if log_level is the same", function()
     do_test("logging3.com", true)
   end)
 end)
