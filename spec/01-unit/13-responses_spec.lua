ef7c6ef038a6461d7a85159ed636b7a46dae61d6
Thijs Schreijer <thijs@mashape.com>
2016-07-08 14:49:23 +0200

    tests(responses/resolver) cleanup & refactor

--- spec/01-unit/13-responses_spec.lua
+++ spec/01-unit/13-responses_spec.lua
@@ -2,60 +2,59 @@ local meta = require "kong.meta"
 local responses = require "kong.tools.responses"
 
 describe("Response helpers", function()
+  local old_ngx = _G.ngx
+  local snapshot
+  local stubbed_ngx
 
-  local _ngx = _G.ngx
   before_each(function()
     snapshot = assert:snapshot()
-    local stub = {
+    stubbed_ngx = {
       header = {},
-      say = function(...) return _ngx.say(...) end,
-      exit = function(...) return _ngx.say(...) end,
+      say = function(...) return old_ngx.say(...) end,
+      exit = function(...) return old_ngx.exit(...) end,
+      log = function(...) return old_ngx.log(...) end,
     }
-    _G.ngx = setmetatable({}, {
-      __index = function(self, key)
-        -- lookup a stub, if it fails, fallback to original ngx table
-        return stub[key] or _ngx[key]
-      end
-    })
-    assert.stub(stub, "say")
-    assert.stub(stub, "exit")
+    _G.ngx = setmetatable(stubbed_ngx, {__index = old_ngx})
+    stub(stubbed_ngx, "say")
+    stub(stubbed_ngx, "exit")
+    stub(stubbed_ngx, "log")
   end)
 
   after_each(function()
     snapshot:revert()
-    _G.ngx = _ngx
+    _G.ngx = old_ngx
   end)
 
-  it("should have a list of the main http status codes used in Kong", function()
+  it("has a list of the main http status codes used in Kong", function()
     assert.is_table(responses.status_codes)
   end)
-  it("should be callable via `send_HTTP_STATUS_CODE`", function()
+  it("is callable via `send_HTTP_STATUS_CODE`", function()
     for status_code_name, status_code in pairs(responses.status_codes) do
       assert.has_no.errors(function()
         responses["send_"..status_code_name]()
       end)
     end
   end)
-  it("should set the correct ngx values and call ngx.say and ngx.exit", function()
+  it("sets the correct ngx values and call ngx.say and ngx.exit", function()
     responses.send_HTTP_OK("OK")
     assert.equal(ngx.status, responses.status_codes.HTTP_OK)
     assert.equal(meta._NAME.."/"..meta._VERSION, ngx.header["Server"])
     assert.stub(ngx.say).was.called() -- set custom content
     assert.stub(ngx.exit).was.called() -- exit nginx (or continue to the next context if 200)
   end)
-  it("should send the content as a JSON string with a `message` property if given a string", function()
+  it("send the content as a JSON string with a `message` property if given a string", function()
     responses.send_HTTP_OK("OK")
     assert.stub(ngx.say).was.called_with("{\"message\":\"OK\"}")
   end)
-  it("should send the content as a JSON string if given a table", function()
+  it("sends the content as a JSON string if given a table", function()
     responses.send_HTTP_OK({ success = true })
     assert.stub(ngx.say).was.called_with("{\"success\":true}")
   end)
-  it("should send the content as passed if `raw` is given", function()
+  it("sends the content as passed if `raw` is given", function()
     responses.send_HTTP_OK("OK", true)
     assert.stub(ngx.say).was.called_with("OK")
   end)
-  it("should call `ngx.exit` with the corresponding status_code", function()
+  it("calls `ngx.exit` with the corresponding status_code", function()
     for status_code_name, status_code in pairs(responses.status_codes) do
       assert.has_no.errors(function()
         responses["send_"..status_code_name]()
@@ -63,7 +62,7 @@ describe("Response helpers", function()
       end)
     end
   end)
-  it("should call `ngx.log` if and only if a 500 status code range was given", function()
+  it("calls `ngx.log` if and only if a 500 status code range was given", function()
     responses.send_HTTP_BAD_REQUEST()
     assert.stub(ngx.log).was_not_called()
 
@@ -93,8 +92,8 @@ describe("Response helpers", function()
     end)
   end)
 
-  describe("#send()", function()
-    it("should send a custom status code", function()
+  describe("send()", function()
+    it("sends a custom status code", function()
       responses.send(415, "Unsupported media type")
       assert.stub(ngx.say).was.called_with("{\"message\":\"Unsupported media type\"}")
       assert.stub(ngx.exit).was.called_with(415)
