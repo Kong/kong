78852776630899fb61e681afcdda45fc49fcde8e
Thibault Charbonnier <thibaultcha@me.com>
2016-07-12 16:22:22 -0700

    fix(tests) cleanup tcp/udp tests and helpers

--- spec/03-plugins/tcp-log/01-tcp-log_spec.lua
+++ spec/03-plugins/tcp-log/01-tcp-log_spec.lua
@@ -4,51 +4,49 @@ local helpers = require "spec.helpers"
 local TCP_PORT = 35000
 local HTTP_DELAY_PORT = 35003
 
-describe("Plugin: tcp (log)", function()
-
+describe("Plugin: tcp-log (log)", function()
   local client
-  
+
   setup(function()
     helpers.kill_all()
     helpers.prepare_prefix()
 
     local api1 = assert(helpers.dao.apis:insert {
-      name = "tests-tcp-logging", 
-      request_host = "tcp_logging.com", 
+      request_host = "tcp_logging.com",
       upstream_url = "http://mockbin.com",
     })
     local api2 = assert(helpers.dao.apis:insert {
-      name = "tests-tcp-logging2", 
-      request_host = "tcp_logging2.com", 
+      request_host = "tcp_logging2.com",
       upstream_url = "http://127.0.0.1:"..HTTP_DELAY_PORT,
     })
 
     -- plugin 1
     assert(helpers.dao.plugins:insert {
       api_id = api1.id,
-      name = "tcp-log", 
+      name = "tcp-log",
       config = {
-        host = "127.0.0.1", 
+        host = "127.0.0.1",
         port = TCP_PORT
       },
     })
     -- plugin 2
     assert(helpers.dao.plugins:insert {
       api_id = api2.id,
-      name = "tcp-log", 
+      name = "tcp-log",
       config = {
-        host = "127.0.0.1", 
+        host = "127.0.0.1",
         port = TCP_PORT
       },
     })
 
     assert(helpers.start_kong())
-    client = assert(helpers.proxy_client())
+    client = helpers.proxy_client()
   end)
 
   teardown(function()
     if client then client:close() end
     helpers.stop_kong()
+    helpers.clean_prefix()
   end)
 
   it("logs to TCP", function()
@@ -57,8 +55,8 @@ describe("Plugin: tcp (log)", function()
     -- Making the request
     local r = assert(client:send {
       method = "GET",
-      path = "/request", 
-      headers = { 
+      path = "/request",
+      headers = {
         host = "tcp_logging.com"
       },
     })
@@ -66,12 +64,12 @@ describe("Plugin: tcp (log)", function()
 
     -- Getting back the TCP server input
     local ok, res = thread:join()
-    assert.is_true(ok)
-    assert.is.string(res)
+    assert.True(ok)
+    assert.is_string(res)
 
     -- Making sure it's alright
     local log_message = cjson.decode(res)
-    assert.same("127.0.0.1", log_message.client_ip)
+    assert.equal("127.0.0.1", log_message.client_ip)
   end)
 
   it("logs proper latencies", function()
@@ -82,24 +80,23 @@ describe("Plugin: tcp (log)", function()
     local r = assert(client:send {
       method = "GET",
       path = "/request/delay",
-      headers = { 
-        host = "tcp_logging2.com" 
+      headers = {
+        host = "tcp_logging2.com"
       }
     })
 
     assert.response(r).has.status(200)
     -- Getting back the TCP server input
     local ok, res = tcp_thread:join()
-    assert.is_true(ok)
-    assert.is.string(res)
+    assert.True(ok)
+    assert.is_string(res)
 
     -- Making sure it's alright
     local log_message = cjson.decode(res)
 
-    assert.truthy(log_message.latencies.proxy < 3000)
-    assert.truthy(log_message.latencies.request >= log_message.latencies.kong + log_message.latencies.proxy)
+    assert.True(log_message.latencies.proxy < 3000)
+    assert.True(log_message.latencies.request >= log_message.latencies.kong + log_message.latencies.proxy)
 
     http_thread:join()
   end)
-
 end)
