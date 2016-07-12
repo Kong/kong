dfbe85b0ed0f5a1f4f72b501d3bb2f5e29237675
thefosk <marco@mashape.com>
2016-07-12 14:52:49 -0700

    Fixing set_timeout in serf script, and increasing it to 5000

--- kong/cmd/utils/prefix_handler.lua
+++ kong/cmd/utils/prefix_handler.lua
@@ -25,7 +25,7 @@ fi
 CMD="\
 local http = require 'resty.http' \
 local client = http.new() \
-client:settimeout(3000) \
+client:set_timeout(5000) \
 client:connect('%s', %d) \
 client:request { \
   method = 'POST', \
