b5aa88e13052e90049c2a37d2fde6300ff16a74a
Thibault Charbonnier <thibaultcha@me.com>
2016-07-27 19:11:41 -0700

    fix(lint) remove unused variables from #1402

--- kong/tools/database_cache.lua
+++ kong/tools/database_cache.lua
@@ -127,10 +127,8 @@ function _M.all_apis_by_dict_key()
 end
 
 function _M.get_or_set(key, cb)
-  local value, err
-
   -- Try to get the value from the cache
-  value = _M.get(key)
+  local value = _M.get(key)
   if value then return value end
 
   local lock, err = resty_lock:new("cache_locks", {
@@ -148,12 +146,12 @@ function _M.get_or_set(key, cb)
     ngx_log(ngx.ERR, "failed to acquire cache lock: ", err)
   end
 
-  -- Lock acquired. Since in the meantime another worker may have 
+  -- Lock acquired. Since in the meantime another worker may have
   -- populated the value we have to check again
   value = _M.get(key)
   if not value then
     -- Get from closure
-    value, err = cb()
+    value = cb()
     if value then
       local ok, err = _M.set(key, value)
       if not ok then
@@ -163,7 +161,7 @@ function _M.get_or_set(key, cb)
   end
 
   local ok, err = lock:unlock()
-  if not ok then
+  if not ok and err then
     ngx_log(ngx.ERR, "failed to unlock: ", err)
   end
 
