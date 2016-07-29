895888f270bed8156e0ff837b56dc7bcdc5ee598
Max Metral <max@pyralis.com>
2016-07-29 11:37:37 -0400

    Try and use bcrypt on the provision_key

--- kong-0.8.3-0.rockspec
+++ kong-0.8.3-0.rockspec
@@ -12,6 +12,7 @@ description = {
 }
 dependencies = {
   "luasec ~> 0.5-2",
+  "bcrypt-ffi ~> 1.0.0",
   "luasocket ~> 2.0.2-6",
   "penlight ~> 1.3.2",
   "lua-resty-http ~> 0.07-0",
--- kong/plugins/oauth2/schema.lua
+++ kong/plugins/oauth2/schema.lua
@@ -1,5 +1,8 @@
 local utils = require "kong.tools.utils"
 local stringy = require "stringy"
+local bcrypt = require "bcrypt"
+
+local BCRYPT_ROUNDS = 12
 
 local function generate_if_missing(v, t, column)
   if not v or stringy.strip(v) == "" then
@@ -15,12 +18,16 @@ local function check_mandatory_scope(v, t)
   return true
 end
 
+local function marshall_event(conf)
+end
+
 return {
   no_consumer = true,
   fields = {
     scopes = { required = false, type = "array" },
     mandatory_scope = { required = true, type = "boolean", default = false, func = check_mandatory_scope },
     provision_key = { required = false, unique = true, type = "string", func = generate_if_missing },
+    provision_key_hash = { required = false, unique = false, type = "string" },
     token_expiration = { required = true, type = "number", default = 7200 },
     enable_authorization_code = { required = true, type = "boolean", default = true },
     enable_implicit_grant = { required = true, type = "boolean", default = false },
@@ -28,5 +35,12 @@ return {
     enable_password_grant = { required = true, type = "boolean", default = false },
     hide_credentials = { type = "boolean", default = false },
     accept_http_if_already_terminated = { required = false, type = "boolean", default = false }
-  }
+  },
+  self_check = function (self, config, dao, is_update)
+    if not is_update and config.provision_key then
+      config.provision_key_hash = bcrypt.digest(config.provision_key, BCRYPT_ROUNDS)
+      config.provision_key = nil
+    end
+    return true
+  end
 }
