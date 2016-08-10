ab7795d39f0d8224673e89c4ff2efab48897967e
shashi ranjan <shashiranjan84@gmail.com>
2016-08-09 23:20:12 -0700

    feat(plugin/datadog) added new metric upstream_latency and tagging support (#1473)

--- kong/plugins/datadog/handler.lua
+++ kong/plugins/datadog/handler.lua
@@ -11,54 +11,58 @@ local string_gsub = string.gsub
 local ipairs = ipairs
 
 local gauges = {
-  request_size = function (api_name, message, logger)
+  request_size = function (api_name, message, logger, tags)
     local stat = api_name..".request.size"
-    logger:gauge(stat, message.request.size, 1)
+    logger:gauge(stat, message.request.size, 1, tags)
   end,
-  response_size = function (api_name, message, logger)
+  response_size = function (api_name, message, logger, tags)
     local stat = api_name..".response.size"
-    logger:gauge(stat, message.response.size, 1)
+    logger:gauge(stat, message.response.size, 1, tags)
   end,
-  status_count = function (api_name, message, logger)
+  status_count = function (api_name, message, logger, tags)
     local stat = api_name..".request.status."..message.response.status
-    logger:counter(stat, 1, 1)
+    logger:counter(stat, 1, 1, tags)
   end,
-  latency = function (api_name, message, logger)
+  latency = function (api_name, message, logger, tags)
     local stat = api_name..".latency"
-    logger:gauge(stat, message.latencies.request, 1)
+    logger:gauge(stat, message.latencies.request, 1, tags)
   end,
-  request_count = function (api_name, message, logger)
+  request_count = function (api_name, message, logger, tags)
     local stat = api_name..".request.count"
-    logger:counter(stat, 1, 1)
+    logger:counter(stat, 1, 1, tags)
   end,
-  unique_users = function (api_name, message, logger)
+  unique_users = function (api_name, message, logger, tags)
     if message.authenticated_entity ~= nil and message.authenticated_entity.consumer_id ~= nil then
       local stat = api_name..".user.uniques"
-      logger:set(stat, message.authenticated_entity.consumer_id)
+      logger:set(stat, message.authenticated_entity.consumer_id, tags)
     end
   end,
-  request_per_user = function (api_name, message, logger)
+  request_per_user = function (api_name, message, logger, tags)
     if message.authenticated_entity ~= nil and message.authenticated_entity.consumer_id ~= nil then
       local stat = api_name.."."..string_gsub(message.authenticated_entity.consumer_id, "-", "_")..".request.count"
-      logger:counter(stat, 1, 1)
+      logger:counter(stat, 1, 1, tags)
     end
+  end,
+  upstream_latency = function (api_name, message, logger, tags)
+    local stat = api_name..".upstream_latency"
+    logger:gauge(stat, message.latencies.proxy, 1, tags)
   end
 }
 
 local function log(premature, conf, message)
   if premature then return end
-
   local logger, err = statsd_logger:new(conf)
   if err then
     ngx.log(ngx.ERR, "failed to create Statsd logger: ", err)
     return
   end
-
+  
   local api_name = string_gsub(message.api.name, "%.", "_")
   for _, metric in ipairs(conf.metrics) do
     local gauge = gauges[metric]
     if gauge then
-      gauge(api_name, message, logger)
+      
+      gauge(api_name, message, logger, conf.tags[metric])
     end
   end
 
