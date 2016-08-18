22a83028d8910b01c7dbb7ee80f2b025119477eb
Thijs Schreijer <thijs@mashape.com>
2016-08-18 21:52:36 +0200

    fix typo

--- kong/plugins/acl/schema.lua
+++ kong/plugins/acl/schema.lua
@@ -9,7 +9,7 @@ return {
   self_check = function(schema, plugin_t, dao, is_update)
     if next(plugin_t.whitelist or {}) and next(plugin_t.blacklist or {}) then
       return false, Errors.schema "You cannot set both a whitelist and a blacklist"
-    elseif not (next(plugin_t.whitelist {}) or next(plugin_t.blacklist or {})) then
+    elseif not (next(plugin_t.whitelist or {}) or next(plugin_t.blacklist or {})) then
       return false, Errors.schema "You must set at least a whitelist or blacklist"
     end
     return true
