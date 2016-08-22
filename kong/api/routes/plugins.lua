8a2d755cbb536f027a71290b9f7e74a9cf5d6bc7
Marco Palladino <marco@mashape.com>
2016-08-22 16:04:26 -0700

    Fixing the response value of /plugins/enabled (#1529)

--- kong/api/routes/plugins.lua
+++ kong/api/routes/plugins.lua
@@ -74,8 +74,12 @@ return {
 
   ["/plugins/enabled"] = {
     GET = function(self, dao_factory, helpers)
+      local enabled_plugins = {}
+      for k, v in pairs(singletons.configuration.plugins) do
+        if v then table.insert(enabled_plugins, k) end
+      end
       return helpers.responses.send_HTTP_OK {
-        enabled_plugins = singletons.configuration.plugins
+        enabled_plugins = enabled_plugins
       }
     end
   }
