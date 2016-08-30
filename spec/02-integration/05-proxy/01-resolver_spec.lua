e1a8da0b8654b7102e40947fecd81a3ae6f7be2f
Marco Palladino <marco@mashape.com>
2016-08-30 15:08:15 -0700

    test(resolver) dashes in subdomain (#1565)

--- spec/02-integration/05-proxy/01-resolver_spec.lua
+++ spec/02-integration/05-proxy/01-resolver_spec.lua
@@ -22,6 +22,14 @@ describe("Resolver", function()
       request_host = "wildcard.*",
       upstream_url = "http://mockbin.com"
     })
+    assert(helpers.dao.apis:insert {
+      request_host = "*.my-test.wildcard.com",
+      upstream_url = "http://mockbin.com"
+    })
+    assert(helpers.dao.apis:insert {
+      request_host = "*.my-test.another-wildcard.com",
+      upstream_url = "http://mockbin.com"
+    })
     -- request_path
     assert(helpers.dao.apis:insert {
       request_path = "/status/200",
@@ -149,6 +157,26 @@ describe("Resolver", function()
           path = "/status/200",
           headers = {
             ["Host"] = "subdomain.wildcard.com"
+          }
+        })
+        assert.res_status(200, res)
+      end)
+      it("subdomain with dash", function()
+        local res = assert(client:send {
+          method = "GET",
+          path = "/status/200",
+          headers = {
+            ["Host"] = "host.my-test.wildcard.com"
+          }
+        })
+        assert.res_status(200, res)
+      end)
+      it("another subdomain with dash", function()
+        local res = assert(client:send {
+          method = "GET",
+          path = "/status/200",
+          headers = {
+            ["Host"] = "host.my-test.another-wildcard.com"
           }
         })
         assert.res_status(200, res)
