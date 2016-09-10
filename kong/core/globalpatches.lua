02921586a914637570d0f4c86f9edcf581476eac
Thibault Charbonnier <thibaultcha@me.com>
2016-09-09 17:30:25 -0700

    fix(globalpatches) only log randomseed error when not already seeded

--- kong/core/globalpatches.lua
+++ kong/core/globalpatches.lua
@@ -17,16 +17,19 @@ local seed
 -- unique seed for Nginx workers).
 -- luacheck: globals math
 _G.math.randomseed = function()
-  if ngx.get_phase() ~= "init_worker" then
-    ngx.log(ngx.ERR, "math.randomseed() must be called in init_worker")
-  elseif not seed then
+  if not seed then
+    if ngx.get_phase() ~= "init_worker" then
+      ngx.log(ngx.ERR, "math.randomseed() must be called in init_worker")
+      return
+    end
+
     seed = ngx.time() + ngx.worker.pid()
     ngx.log(ngx.DEBUG, "random seed: ", seed, " for worker n", ngx.worker.id(),
                        " (pid: ", ngx.worker.pid(), ")")
     randomseed(seed)
   else
     ngx.log(ngx.DEBUG, "attempt to seed random number generator, but ",
-                       "already seeded")
+                       "already seeded with ", seed)
   end
 
   return seed
