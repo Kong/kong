8b944951bdaee32e8e932f28155bdb65af972d00
zhangzheng <18601254750.163.com>
2016-09-27 17:42:53 +0800

    fix http-log, support set parsed_url.query and content-type

--- kong/plugins/http-log/handler.lua
+++ kong/plugins/http-log/handler.lua
@@ -16,8 +16,8 @@ local HTTPS = "https"
 -- @return `body` http payload
 local function generate_post_payload(method, parsed_url, body)
   return string.format(
-    "%s %s HTTP/1.1\r\nHost: %s\r\nConnection: Keep-Alive\r\nContent-Type: application/json\r\nContent-Length: %s\r\n\r\n%s",
-    method:upper(), parsed_url.path, parsed_url.host, #body, body)
+    "%s %s HTTP/1.1\r\nHost: %s\r\nConnection: Keep-Alive\r\nContent-Type: %s\r\nContent-Length: %s\r\n\r\n%s",
+    method:upper(), parsed_url.path..(parsed_url.query or ""), parsed_url.host, content_type, #body, body)
 end
 
 -- Parse host url
