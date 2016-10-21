236622a727563f9fac40e4375c7e9eccef6b94d4
Thibault Charbonnier <thibaultcha@me.com>
2016-10-21 11:05:19 -0700

    chore(ci) bump Luarocks to 2.4.1

--- .ci/setup_env.sh
+++ .ci/setup_env.sh
@@ -1,5 +1,4 @@
 set -e
-set -x
 
 #---------
 # Download
@@ -90,8 +89,6 @@ export SERF_PATH=$SERF_INSTALL/serf # for our test instance (not in default bin/
 export PATH=$PATH:$OPENRESTY_INSTALL/nginx/sbin:$OPENRESTY_INSTALL/bin:$LUAROCKS_INSTALL/bin:$SERF_INSTALL
 
 eval `luarocks path`
-
-luarocks purge --tree=$LUAROCKS_INSTALL
 
 # -------------------------------------
 # Install ccm & setup Cassandra cluster
