0e183cbe96ad1ab1d12a2477355ec816458e1186
Thibault Charbonnier <thibaultcha@me.com>
2016-10-22 14:21:43 -0700

    chore(travis) only run ccm on C* integration tests

--- .ci/setup_env.sh
+++ .ci/setup_env.sh
@@ -94,7 +94,7 @@ eval `luarocks path`
 # -------------------------------------
 # Install ccm & setup Cassandra cluster
 # -------------------------------------
-if [[ "$TEST_SUITE" != "unit" ]] && [[ "$TEST_SUITE" != "lint" ]]; then
+if [[ "$TEST_SUITE" != "unit" ]] && [[ "$TEST_SUITE" != "lint" ]] && [[ "$KONG_DATABASE" == "cassandra" ]] ; then
   pip install --user PyYAML six
   git clone https://github.com/pcmanus/ccm.git
   pushd ccm
