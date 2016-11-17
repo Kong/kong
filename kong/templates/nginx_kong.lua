b468e59547db4e42b5c1d3f8b865f2b235ccd401
thefosk <marco@mashape.com>
2016-11-16 21:31:15 -0800

    feat(proxy) supports websockets

--- kong/templates/nginx_kong.lua
+++ kong/templates/nginx_kong.lua
@@ -63,6 +63,11 @@ upstream kong_upstream {
     keepalive ${{UPSTREAM_KEEPALIVE}};
 }
 
+map $http_upgrade $connection_upgrade {
+    default upgrade;
+    '' close;
+}
+
 server {
     server_name kong;
     listen ${{PROXY_LISTEN}};
@@ -89,10 +94,13 @@ server {
             kong.access()
         }
 
+        proxy_http_version 1.1;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
         proxy_set_header Host $upstream_host;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection $connection_upgrade;
         proxy_pass_header Server;
         proxy_pass $upstream_url;
 
