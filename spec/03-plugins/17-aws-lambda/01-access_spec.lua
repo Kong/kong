ce647bbddac3797e2c26e971b467c1f8bcc425f1
thefosk <marco@mashape.com>
2016-11-20 19:12:10 -0800

    tests other invocation types

--- spec/03-plugins/17-aws-lambda/01-access_spec.lua
+++ spec/03-plugins/17-aws-lambda/01-access_spec.lua
@@ -5,19 +5,51 @@ describe("Plugin: AWS Lambda (access)", function()
   setup(function()
     assert(helpers.start_kong())
 
-    local api = assert(helpers.dao.apis:insert {
+    local api1 = assert(helpers.dao.apis:insert {
       request_host = "lambda.com",
+      upstream_url = "http://httpbin.org"
+    })
+    local api2 = assert(helpers.dao.apis:insert {
+      request_host = "lambda2.com",
+      upstream_url = "http://httpbin.org"
+    })
+    local api3 = assert(helpers.dao.apis:insert {
+      request_host = "lambda3.com",
       upstream_url = "http://httpbin.org"
     })
     
     assert(helpers.dao.plugins:insert {
       name = "aws-lambda",
-      api_id = api.id,
+      api_id = api1.id,
       config = {
         aws_key = "AKIAIDPNYYGMJOXN26SQ",
         aws_secret = "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",
         aws_region = "us-east-1",
         function_name = "kongLambdaTest"
+      }
+    })
+
+    assert(helpers.dao.plugins:insert {
+      name = "aws-lambda",
+      api_id = api2.id,
+      config = {
+        aws_key = "AKIAIDPNYYGMJOXN26SQ",
+        aws_secret = "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",
+        aws_region = "us-east-1",
+        function_name = "kongLambdaTest",
+        invocation_type = "Event"
+      }
+    })
+
+    assert(helpers.dao.plugins:insert {
+      name = "aws-lambda",
+      api_id = api3.id,
+      config = {
+        aws_key = "AKIAIDPNYYGMJOXN26SQ",
+        aws_secret = "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",
+        aws_region = "us-east-1",
+        function_name = "kongLambdaTest",
+        invocation_type = "DryRun"
       }
     })
   end)
@@ -97,6 +129,40 @@ describe("Plugin: AWS Lambda (access)", function()
     local body = assert.res_status(200, res)
     assert.is_string(res.headers["x-amzn-RequestId"])
     assert.equal([["from_querystring"]], body)
+  end)
+  it("invokes a Lambda function with POST params and Event invocation_type", function()
+    local res = assert(client:send {
+      method = "POST",
+      path = "/post",
+      headers = {
+        ["Host"] = "lambda2.com",
+        ["Content-Type"] = "application/x-www-form-urlencoded"
+      },
+      body = {
+        key1 = "some_value_post1",
+        key2 = "some_value_post2",
+        key3 = "some_value_post3"
+      }
+    })
+    assert.res_status(202, res)
+    assert.is_string(res.headers["x-amzn-RequestId"])
+  end)
+  it("invokes a Lambda function with POST params and DryRun invocation_type", function()
+    local res = assert(client:send {
+      method = "POST",
+      path = "/post",
+      headers = {
+        ["Host"] = "lambda3.com",
+        ["Content-Type"] = "application/x-www-form-urlencoded"
+      },
+      body = {
+        key1 = "some_value_post1",
+        key2 = "some_value_post2",
+        key3 = "some_value_post3"
+      }
+    })
+    assert.res_status(204, res)
+    assert.is_string(res.headers["x-amzn-RequestId"])
   end)
 
 end)
