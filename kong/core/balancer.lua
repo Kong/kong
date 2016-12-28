c46fefcc65df937069b2a9c63c41c4f27321bcb0
Thijs Schreijer <thijs@mashape.com>
2016-12-29 00:27:16 +0100

    diff

--- kong/core/balancer.lua
+++ kong/core/balancer.lua
@@ -66,7 +66,7 @@ local function load_upstream_into_memory(upstream_id)
   if not upstream then
     return nil, err
   end
-  
+
   return upstream[1]  -- searched by id, so only 1 row in the returned set
 end
 
