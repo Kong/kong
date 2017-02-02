a4f7292922d81117f04e7170ad1bdedca42a6011
Thijs Schreijer <thijs@mashape.com>
2017-02-02 12:32:29 +0100

    fix(auth): key-auth takes consumer id for anonymous

--- kong/plugins/key-auth/schema.lua
+++ kong/plugins/key-auth/schema.lua
@@ -1,3 +1,13 @@
+local utils = require "kong.tools.utils"
+
+local function check_user(anonymous)
+  if anonymous == "" or utils.is_valid_uuid(anonymous) then
+    return true
+  end
+  
+  return false, "the anonymous user must be empty or a valid uuid"
+end
+
 local function default_key_names(t)
   if not t.key_names then
     return {"apikey"}
@@ -9,6 +19,6 @@ return {
   fields = {
     key_names = {required = true, type = "array", default = default_key_names},
     hide_credentials = {type = "boolean", default = false},
-    anonymous = {type = "boolean", default = false}
+    anonymous = {type = "string", default = "", func = check_user},
   }
 }
