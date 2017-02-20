9d2ba14b4fe144c8c9d7d843803266fc38c553e7
Jeremy D Monin <jdmonin@nand.net>
2017-02-20 06:05:02 -0500

    hotfix(response-ratelimiting): use header values from table, or split from string

--- kong/plugins/response-ratelimiting/header_filter.lua
+++ kong/plugins/response-ratelimiting/header_filter.lua
@@ -14,16 +14,12 @@ local _M = {}
 local function parse_header(header_value, limits)
   local increments = {}
   if header_value then
-    if (type(header_value) == "table") then
-      -- multiple headers with same name: combine per RFC 2616 section 4.2
-      local hval_str = header_value[1]
-      local i
-      for i = 2,#header_value do
-        hval_str = hval_str .. "," .. header_value[i]
-      end
-      header_value = hval_str
+    local parts
+    if type(header_value) == "table" then
+      parts = header_value
+    else
+      parts = utils.split(header_value, ",")
     end
-    local parts = utils.split(header_value, ",")
     for _, v in ipairs(parts) do
       local increment_parts = utils.split(v, "=")
       if #increment_parts == 2 then
