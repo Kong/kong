91fdd522cc3a8ed4d1f60631a257ad969be3be34
Paul Austin <paul.austin@revolsys.com>
2017-03-06 10:13:30 -0800

    Implemented changes as requested in pull request

--- kong/plugins/request-termination/schema.lua
+++ kong/plugins/request-termination/schema.lua
@@ -4,27 +4,25 @@ local utils = require "kong.tools.utils"
 return {
   no_consumer = true,
   fields = {
-    status_code = { type = "number" },
+    status_code = { type = "number", default = 503 },
     message = { type = "string" },
     content_type = { type = "string" },
     body = { type = "string" },
   },
   self_check = function(schema, plugin_t, dao, is_updating)
-    local errors
-  
     if plugin_t.status_code then
       if plugin_t.status_code < 100 or plugin_t.status_code > 599 then
-        return false, "status_code must be between 100..599"
+        return false, Errors.schema("status_code must be between 100..599")
       end
     end
     
     if plugin_t.message then
       if plugin_t.content_type or plugin_t.body then
-        return false, "message cannot be used with content_type or body"
+        return false, Errors.schema("message cannot be used with content_type or body")
       end
     else
       if plugin_t.content_type and not plugin_t.body then
-        return false, "content_type requires a body"
+        return false, Errors.schema("content_type requires a body")
       end
     end
     
