dc8cc254eaa6fa71f3a42414ef427a2a077beae9
Robert Paprocki <robert@cryptobells.com>
2017-03-21 10:41:22 -0700

    feat(admin) get a list of active targets per upstream

--- spec/02-integration/03-admin_api/09-targets_routes_spec.lua
+++ spec/02-integration/03-admin_api/09-targets_routes_spec.lua
@@ -259,4 +259,65 @@ describe("Admin API", function()
       end)
     end)
   end)
+
+  describe("/upstreams/{upstream}/targets/active/", function()
+    describe("only shows active targets", function()
+      local upstream_name3 = "example.com"
+
+      before_each(function()
+        local upstream3 = assert(helpers.dao.upstreams:insert {
+          name = upstream_name3,
+        })
+
+        -- two target inserts, resulting in a 'down' target
+        assert(helpers.dao.targets:insert {
+          target = "api-1:80",
+          weight = 10,
+          upstream_id = upstream3.id,
+        })
+        assert(helpers.dao.targets:insert {
+          target = "api-1:80",
+          weight = 0,
+          upstream_id = upstream3.id,
+        })
+
+        -- three target inserts, resulting in an 'up' target
+        assert(helpers.dao.targets:insert {
+          target = "api-2:80",
+          weight = 10,
+          upstream_id = upstream3.id,
+        })
+        assert(helpers.dao.targets:insert {
+          target = "api-2:80",
+          weight = 0,
+          upstream_id = upstream3.id,
+        })
+        assert(helpers.dao.targets:insert {
+          target = "api-2:80",
+          weight = 10,
+          upstream_id = upstream3.id,
+        })
+
+        -- and an insert of a separate active target
+        assert(helpers.dao.targets:insert {
+          target = "api-3:80",
+          weight = 10,
+          upstream_id = upstream3.id,
+        })
+      end)
+
+      it("only shows active targets", function()
+        local res = assert(client:send {
+          method = "GET",
+          path = "/upstreams/" .. upstream_name3 .. "/targets/active/",
+        })
+        assert.response(res).has.status(200)
+        local json = assert.response(res).has.jsonbody()
+        assert.equal(2, #json.data)
+        assert.equal(2, json.total)
+        assert.equal("api-3:80", json.data[1].target)
+        assert.equal("api-2:80", json.data[2].target)
+      end)
+    end)
+  end)
 end)
