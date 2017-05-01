60707b2b0f9e5dbb6c8a7f0f30d59e2ad1873c0e
Robert Paprocki <robert@cryptobells.com>
2017-05-01 09:34:35 -0700

    refactor(*) remove abuse of func to generate default values

--- kong/plugins/key-auth/daos.lua
+++ kong/plugins/key-auth/daos.lua
@@ -1,12 +1,5 @@
 local utils = require "kong.tools.utils"
 
-local function generate_if_missing(v, t, column)
-  if not v or utils.strip(v) == "" then
-    return true, nil, {[column] = utils.random_string()}
-  end
-  return true
-end
-
 local SCHEMA = {
   primary_key = {"id"},
   table = "keyauth_credentials",
@@ -14,7 +7,7 @@ local SCHEMA = {
     id = {type = "id", dao_insert_value = true},
     created_at = {type = "timestamp", immutable = true, dao_insert_value = true},
     consumer_id = {type = "id", required = true, foreign = "consumers:id"},
-    key = {type = "string", required = false, unique = true, func = generate_if_missing}
+    key = {type = "string", required = false, unique = true, default = utils.random_string}
   },
   marshall_event = function(self, t)
     return {id = t.id, consumer_id = t.consumer_id, key = t.key}
