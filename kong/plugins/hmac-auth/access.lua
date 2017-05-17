a89c0c6094fa6ff70a616c48ae20ddb623f350cf
thefosk <marco@mashape.com>
2017-05-17 16:24:33 -0700

    refactor(plugins) removing ngx.req.raw_header() for HTTP/2 compatibility

--- kong/plugins/hmac-auth/access.lua
+++ kong/plugins/hmac-auth/access.lua
@@ -13,6 +13,7 @@ local ngx_sha1 = ngx.hmac_sha1
 local ngx_set_header = ngx.req.set_header
 local ngx_get_headers = ngx.req.get_headers
 local ngx_log = ngx.log
+local fmt = string.format
 
 local split = utils.split
 
@@ -68,7 +69,8 @@ local function create_hash(request, hmac_params, headers)
     if not header_value then
       if header == "request-line" then
         -- request-line in hmac headers list
-        signing_string = signing_string .. split(request.raw_header(), "\r\n")[1]
+        local request_line = fmt("%s %s HTTP/%s", ngx.req.get_method(), ngx.var.uri, ngx.req.http_version())
+        signing_string = signing_string .. request_line
       else
         signing_string = signing_string .. header .. ":"
       end
