1d22b16845a2983cd7b6b24bc4f70eb0aebd6aec
Erran Carey <me@errancarey.com>
2017-05-31 23:20:00 +0100

    feat(aws-lambda) Add support for setting the unhandled response code
    Adds a new `unhandled_status` configuration option for the aws-lambda
    plugin. If set the response status will be overridden if the
    `X-Amzn-Function-Error` lambda invoke respoonse header was set to
    `Unhandled`.
    
    This gives a new feature to users of the plugin allowing their API to
    return non 20x codes. The previous functionality of responding with
    Amazon's Lambda Invoke unhandled response status of 200 (RequestResponse),
    202 (Event), or 204 (DryRun) remains.

--- kong/plugins/aws-lambda/schema.lua
+++ kong/plugins/aws-lambda/schema.lua
@@ -1,3 +1,11 @@
+local function check_status(status)
+  if status and (status < 100 or status > 999) then
+    return false, "unhandled_status must be within 100 - 999."
+  end
+
+  return true
+end
+
 return {
   fields = {
     timeout = {type = "number", default = 60000, required = true },
@@ -14,5 +22,6 @@ return {
     log_type = {type = "string", required = true, default = "Tail", 
                        enum = {"Tail", "None"}},
     port = { type = "number", default = 443 },
+    unhandled_status = { type = "number", func = check_status },
   }
 }
