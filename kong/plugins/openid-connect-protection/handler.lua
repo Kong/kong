240940f911f792dc834fce3bd29df9de10b00e8b
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2017-06-01 13:17:35 +0300

    added openid-connect-protection plugin

---
+++ kong/plugins/openid-connect-protection/handler.lua
@@ -0,0 +1,80 @@
+local BasePlugin = require "kong.plugins.base_plugin"
+local responses  = require "kong.tools.responses"
+local oic        = require "kong.openid-connect"
+local session    = require "resty.session"
+
+
+local log        = ngx.log
+
+
+local NOTICE     = ngx.NOTICE
+local ERR        = ngx.ERR
+
+
+local OICProtectionHandler = BasePlugin:extend()
+
+function OICProtectionHandler:new()
+  OICProtectionHandler.super.new(self, "openid-connect-protection")
+end
+
+
+function OICProtectionHandler:init_worker()
+  OICProtectionHandler.super.init_worker(self)
+end
+
+
+function OICProtectionHandler:access(conf)
+  OICProtectionHandler.super.access(self)
+  if not self.oic then
+    log(NOTICE, "loading openid connect configuration")
+
+    local o, err = oic.new {
+      issuer        = conf.issuer,
+      scope         = conf.scopes or { "openid" },
+      claims        = conf.claims or { "iss", "sub", "aud", "azp", "exp", "iat" },
+      leeway        = conf.leeway                     or 0,
+      http_version  = conf.http_version               or 1.1,
+      ssl_verify    = conf.ssl_verify == nil and true or conf.ssl_verify,
+      timeout       = conf.timeout                    or 10000,
+      max_age       = conf.max_age,
+      domains       = conf.domains,
+    }
+
+    if not o then
+      log(ERR, err)
+      return responses.send_HTTP_INTERNAL_SERVER_ERROR()
+    end
+
+    self.oic = o
+  end
+
+  local s, present = session.open()
+
+  if present then
+    local data = s.data
+
+    local encoded, err = self.oic.token:verify(data.tokens, { nonce = data.nonce })
+    if not encoded then
+      log(NOTICE, err)
+      s:destroy()
+      return responses.send_HTTP_UNAUTHORIZED()
+    else
+      -- TODO: check required scopes
+
+      s:start() -- TODO: move this to header_filter
+      -- TODO: append access token as a bearer token
+      -- TODO: append jwk to header
+      -- TODO: handle refreshing the access token
+      -- TODO: require a new authentication when the previous is too far in the past
+    end
+
+  else
+    -- TODO: add WWW-Authenticate header
+    return responses.send_HTTP_UNAUTHORIZED()
+  end
+end
+
+
+OICProtectionHandler.PRIORITY = 1000
+
+return OICProtectionHandler
---
+++ kong/plugins/openid-connect-protection/schema.lua
@@ -0,0 +1,49 @@
+return {
+  no_consumer = true,
+  fields      = {
+    issuer        = {
+      required    = true,
+      type        = "url",
+    },
+    scopes        = {
+      required    = false,
+      type        = "array",
+      default     = { "openid" },
+    },
+    claims        = {
+      required    = false,
+      type        = "array",
+      enum        = { "iss", "sub", "aud", "azp", "exp", "iat", "auth_time", "at_hash", "alg", "nbf", "hd" },
+      default     = { "iss", "sub", "aud", "azp", "exp", "iat" },
+    },
+    domains       = {
+      required    = false,
+      type        = "array",
+    },
+    max_age       = {
+      required    = false,
+      type        = "number",
+    },
+    leeway        = {
+      required    = false,
+      type        = "number",
+      default     = 0,
+    },
+    http_version  = {
+      required    = false,
+      type        = "number",
+      enum        = { 1.0, 1.1 },
+      default     = 1.1,
+    },
+    ssl_verify    = {
+      required    = false,
+      type        = "boolean",
+      default     = true,
+    },
+    timeout       = {
+      required    = false,
+      type        = "number",
+      default     = 10000,
+    },
+  },
+}
