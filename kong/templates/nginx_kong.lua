ba496bcee5d873c01554d5e9c8aaa4367b65d58b
thefosk <marco@mashape.com>
2017-06-15 09:47:05 -0700

    hotfix(core) bring back x-forwarded-host

--- kong/templates/nginx_kong.lua
+++ kong/templates/nginx_kong.lua
@@ -121,6 +121,7 @@ server {
 
         proxy_http_version 1.1;
         proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-Host $host;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto $scheme;
         proxy_set_header Host $upstream_host;
