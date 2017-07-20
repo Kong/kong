3cf035fa37fd84fb7ef75acb652a1abc1559b3b1
kikito <kikito@gmail.com>
2017-07-20 11:48:19 -0700

    perf (dao) improve performance of DAO:cache_key

--- spec/02-integration/03-dao/09-cache_key_spec.lua
+++ spec/02-integration/03-dao/09-cache_key_spec.lua
@@ -6,7 +6,7 @@ describe("<dao>:cache_key()", function()
       local consumer_id = "59c7fb5e-3430-11e7-b51f-784f437104fa"
 
       local cache_key = helpers.dao.consumers:cache_key(consumer_id)
-      assert.equal("consumers:" .. consumer_id, cache_key)
+      assert.equal("consumers:" .. consumer_id .. "::::", cache_key)
     end)
 
     it("(Plugins)", function()
@@ -15,17 +15,17 @@ describe("<dao>:cache_key()", function()
       local consumer_id = "59c7fb5e-3430-11e7-b51f-784f437104fa"
 
       local cache_key = helpers.dao.plugins:cache_key(name)
-      assert.equal("plugins:" .. name, cache_key)
+      assert.equal("plugins:" .. name .. "::::", cache_key)
 
       cache_key = helpers.dao.plugins:cache_key(name, api_id)
-      assert.equal("plugins:" .. name .. ":" .. api_id, cache_key)
+      assert.equal("plugins:" .. name .. ":" .. api_id .. ":::", cache_key)
 
       cache_key = helpers.dao.plugins:cache_key(name, api_id, consumer_id)
-      assert.equal("plugins:" .. name .. ":" .. api_id .. ":" .. consumer_id,
+      assert.equal("plugins:" .. name .. ":" .. api_id .. ":" .. consumer_id .. "::",
                    cache_key)
 
       cache_key = helpers.dao.plugins:cache_key(name, nil, consumer_id)
-      assert.equal("plugins:" .. name .. "::" .. consumer_id, cache_key)
+      assert.equal("plugins:" .. name .. "::" .. consumer_id .. "::", cache_key)
     end)
   end)
 end)
