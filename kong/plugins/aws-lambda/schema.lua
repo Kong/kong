07bc71d31d8416790466d09b862005cb06959c15
Robert Paprocki <robert@cryptobells.com>
2017-08-04 08:35:50 -0700

    style(aws-lambda) clean up schema
    This schema file has been changing often enough that cleaning it up
    to reduce future diff load is worth the two minutes of mashing
    tab-enter.

--- kong/plugins/aws-lambda/schema.lua
+++ kong/plugins/aws-lambda/schema.lua
@@ -8,22 +8,77 @@ end
 
 return {
   fields = {
-    timeout = {type = "number", default = 60000, required = true },
-    keepalive = {type = "number", default = 60000, required = true },
-    aws_key = {type = "string", required = true},
-    aws_secret = {type = "string", required = true},
-    aws_region = {type = "string", required = true, enum = {
-                  "us-east-1", "us-east-2", "us-west-1", "us-west-2", 
-                  "ap-northeast-1", "ap-northeast-2", "ap-southeast-1", 
-                  "ap-southeast-2", "ap-south-1", "ca-central-1", "eu-central-1", 
-                  "eu-west-1", "eu-west-2", "sa-east-1"}},
-    function_name = {type="string", required = true},
-    qualifier = {type = "string"},
-    invocation_type = {type = "string", required = true, default = "RequestResponse", 
-                       enum = {"RequestResponse", "Event", "DryRun"}},
-    log_type = {type = "string", required = true, default = "Tail", 
-                       enum = {"Tail", "None"}},
-    port = { type = "number", default = 443 },
-    unhandled_status = { type = "number", func = check_status },
-  }
+    timeout = {
+      type = "number",
+      default = 60000,
+      required = true,
+    },
+    keepalive = {
+      type = "number",
+      default = 60000,
+      required = true,
+    },
+    aws_key = {
+      type = "string",
+      required = true,
+    },
+    aws_secret = {
+      type = "string",
+      required = true,
+    },
+    aws_region = {
+      type = "string",
+      required = true,
+      enum = {
+        "us-east-1",
+        "us-east-2",
+        "us-west-1",
+        "us-west-2",
+        "ap-northeast-1",
+        "ap-northeast-2",
+        "ap-southeast-1",
+        "ap-southeast-2",
+        "ap-south-1",
+        "ca-central-1",
+        "eu-central-1",
+        "eu-west-1",
+        "eu-west-2",
+        "sa-east-1",
+      },
+    },
+    function_name = {
+      type= "string",
+      required = true,
+    },
+    qualifier = {
+      type = "string",
+    },
+    invocation_type = {
+      type = "string",
+      required = true,
+      default = "RequestResponse",
+      enum = {
+        "RequestResponse",
+        "Event",
+        "DryRun",
+      }
+    },
+    log_type = {
+      type = "string",
+      required = true,
+      default = "Tail",
+      enum = {
+        "Tail",
+        "None",
+      }
+    },
+    port = {
+      type = "number",
+      default = 443,
+    },
+    unhandled_status = {
+      type = "number",
+      func = check_status,
+    },
+  },
 }
