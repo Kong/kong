0e3f2bfb73bc7d89629ed518bf1e20a4a0df6249
Manuel Gutjahr <manuel.gutjahr@at.redbullmediahouse.com>
2017-08-07 11:09:52 +0200

    style(aws-lambda) Remove unused variables / Add assertions

--- spec/03-plugins/23-aws-lambda/02-schema_spec.lua
+++ spec/03-plugins/23-aws-lambda/02-schema_spec.lua
@@ -19,11 +19,6 @@ describe("Plugin: AWS Lambda (schema)", function()
     port             = 443,
   }
 
-  local DEFAULTS_IAM_ROLE_AUTH = {
-    function_name = DEFAULTS.function_name,
-    aws_region = DEFAULTS.aws_region
-  }
-
   it("accepts nil Unhandled Response Status Code", function()
     local entity = utils.table_merge(DEFAULTS, { unhandled_status = nil })
     local ok, err = validate_entity(entity, aws_lambda_schema)
@@ -53,28 +48,37 @@ describe("Plugin: AWS Lambda (schema)", function()
   end)
 
   it("errors with no aws_key and use_ec2_iam_role set to false", function()
-    local entity = utils.table_merge(DEFAULTS_IAM_ROLE_AUTH, { aws_key = "" })
+    local entity = utils.table_merge(DEFAULTS, { aws_key = "" })
     local ok, err, self_err = validate_entity(entity, aws_lambda_schema)
+    assert.is_nil(err)
     assert.equal("You need to set aws_key and aws_secret or need to use EC2 IAM roles", self_err.message)
     assert.False(ok)
   end)
 
   it("errors with empty aws_secret and use_ec2_iam_role set to false", function()
-    local entity = utils.table_merge(DEFAULTS_IAM_ROLE_AUTH, { aws_secret = "" })
+    local entity = utils.table_merge(DEFAULTS, { aws_secret = "" })
     local ok, err, self_err = validate_entity(entity, aws_lambda_schema)
+    assert.is_nil(err)
     assert.equal("You need to set aws_key and aws_secret or need to use EC2 IAM roles", self_err.message)
     assert.False(ok)
   end)
 
   it("errors with empty aws_secret or aws_key and use_ec2_iam_role set to false", function()
-    local ok, err, self_err = validate_entity(DEFAULTS_IAM_ROLE_AUTH, aws_lambda_schema)
+    local entity = utils.table_merge(DEFAULTS, {})
+    entity.aws_key = nil
+    entity.aws_secret = nil
+    local ok, err, self_err = validate_entity(entity, aws_lambda_schema)
+    assert.is_nil(err)
     assert.equal("You need to set aws_key and aws_secret or need to use EC2 IAM roles", self_err.message)
     assert.False(ok)
   end)
 
   it("ok if aws_key or aws_secret is missing but use_ec2_iam_role is set to true", function()
-    local entity = utils.table_merge(DEFAULTS_IAM_ROLE_AUTH, { use_ec2_iam_role = true })
-    local ok, err= validate_entity(entity, aws_lambda_schema)
+    local entity = utils.table_merge(DEFAULTS, { use_ec2_iam_role = true })
+    entity.aws_key = nil
+    entity.aws_secret = nil
+    local ok, err = validate_entity(entity, aws_lambda_schema)
+    assert.is_nil(err)
     assert.True(ok)
   end)
 
