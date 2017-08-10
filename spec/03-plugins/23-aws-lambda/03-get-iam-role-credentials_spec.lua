3fba98bb9e4dac632164795531b42c2d0895b733
Manuel Gutjahr <manuel.gutjahr@at.redbullmediahouse.com>
2017-08-10 19:43:53 +0200

    tests(aws-lambda) Add test for non-existing metadata service

--- spec/03-plugins/23-aws-lambda/03-get-iam-role-credentials_spec.lua
+++ spec/03-plugins/23-aws-lambda/03-get-iam-role-credentials_spec.lua
@@ -13,9 +13,18 @@ describe("Plugin: aws-lambda", function()
     end)
 
     describe("IAM Metadata service", function()
-        it("should fetch credentials from metadata service", function()
-            local iam_role_credentials = get_credentials_from_iam_role('127.0.0.1', 9999)
+        it("should return error if metadata service is not running on provided endpoint", function()
+            local iam_role_credentials, err = get_credentials_from_iam_role('192.0.2.0', 1234)
 
+            assert.is_nil(iam_role_credentials)
+            assert.is_not_nil(err)
+            assert.equal("timeout", err)
+        end)
+
+        it("should fetch credentials from metadata service", function()
+            local iam_role_credentials, err = get_credentials_from_iam_role('127.0.0.1', 9999)
+
+            assert.is_nil(err)
             assert.equal("test_iam_access_key_id", iam_role_credentials.access_key)
             assert.equal("test_iam_secret_access_key", iam_role_credentials.secret_key)
             assert.equal("test_session_token", iam_role_credentials.session_token)
