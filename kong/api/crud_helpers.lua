Kong/kong

commit cc4ffc30397b0a1291c456f39840d88ba00f7b98

tests(admin) add tests for Admin API post-processing

kong/api/crud_helpers.lua
@@ -193,7 +193,7 @@ function _M.get(primary_keys, dao_collection, post_process)
 end
 
 --- Insertion of an entity.
-function _M.post(params, dao_collection, post_process)
+function _M.post(params, dao_collection, success, post_process)
   local data, err = dao_collection:insert(params)
   if err then
     return app_helpers.yield_error(err)

spec/fixtures/custom_plugins/kong/plugins/admin-api-post-process/api.lua
@@ -1,0 +1,53 @@
+local crud = require "kong.api.crud_helpers"
+
+
+local function uppercase_table_values(tbl)
+  local result = {}
+
+  for k, v in pairs(tbl) do
+    if type(v) == "string" then
+      result[k] = string.upper(v)
+
+    elseif type(v) == "table" then
+      result[k] = uppercase_table_values(tbl)
+
+    else
+      result[k] = v
+    end
+  end
+
+  return result
+end
+
+
+return {
+  ["/consumers/post_processed"] = {
+    GET = function(self, dao_factory)
+      crud.paginated_set(self, dao_factory.consumers, uppercase_table_values)
+    end,
+
+    POST = function(self, dao_factory)
+      crud.post(self.params, dao_factory.consumers, nil,
+                uppercase_table_values)
+    end,
+  },
+
+  ["/consumers/:username_or_id/post_processed"] = {
+    before = function(self, dao_factory, helpers)
+      crud.find_consumer_by_username_or_id(self, dao_factory, helpers)
+    end,
+
+    GET = function(self, dao_factory, helpers)
+      crud.get(self.consumer, dao_factory.consumers, uppercase_table_values)
+    end,
+
+    PATCH = function(self, dao_factory)
+      crud.patch(self.params, dao_factory.consumers, self.consumer,
+                 uppercase_table_values)
+    end,
+
+    PUT = function(self, dao_factory)
+      crud.put(self.params, dao_factory.consumers, uppercase_table_values)
+    end,
+  },
+}

spec/02-integration/03-admin_api/10-post_processing_spec.lua
@@ -1,0 +1,120 @@
+local helpers = require "spec.helpers"
+local cjson   = require "cjson"
+
+
+local function it_content_types(title, fn)
+  local test_form_encoded = fn("application/x-www-form-urlencoded")
+  local test_json = fn("application/json")
+  it(title .. " with application/www-form-urlencoded", test_form_encoded)
+  it(title .. " with application/json", test_json)
+end
+
+
+describe("Admin API post-processing", function()
+  local client
+
+  setup(function()
+    assert(helpers.start_kong {
+      custom_plugins = "admin-api-post-process"
+    })
+
+    client = assert(helpers.admin_client())
+  end)
+
+  teardown(function()
+    if client then
+      client:close()
+    end
+
+    helpers.stop_kong()
+  end)
+
+  before_each(function()
+    helpers.dao:truncate_tables()
+    assert(helpers.dao.consumers:insert({
+      username = "michael",
+      custom_id = "landon",
+    }))
+  end)
+
+  it_content_types("post-processes paginated sets", function(content_type)
+    return function()
+      local res = assert(client:send {
+        method = "GET",
+        path = "/consumers/post_processed",
+        headers = { ["Content-Type"] = content_type }
+      })
+      local body = assert.res_status(200, res)
+      local json = cjson.decode(body).data[1]
+      assert.equal("MICHAEL", json.username)
+      assert.equal("LANDON", json.custom_id)
+    end
+  end)
+
+  it_content_types("post-processes crud.post", function(content_type)
+    return function()
+      local res = assert(client:send {
+        method = "POST",
+        path = "/consumers/post_processed",
+        body = {
+          username = "devon",
+          custom_id = "miles",
+        },
+        headers = { ["Content-Type"] = content_type }
+      })
+      local body = assert.res_status(201, res)
+      local json = cjson.decode(body)
+      assert.equal("DEVON", json.username)
+      assert.equal("MILES", json.custom_id)
+    end
+  end)
+
+  it_content_types("post-processes crud.get", function(content_type)
+    return function()
+      local res = assert(client:send {
+        method = "GET",
+        path = "/consumers/michael/post_processed",
+        headers = { ["Content-Type"] = content_type }
+      })
+      local body = assert.res_status(200, res)
+      local json = cjson.decode(body)
+      assert.equal("MICHAEL", json.username)
+      assert.equal("LANDON", json.custom_id)
+    end
+  end)
+
+  it_content_types("post-processes crud.patch", function(content_type)
+    return function()
+      local res = assert(client:send {
+        method = "PATCH",
+        path = "/consumers/michael/post_processed",
+        body = {
+          custom_id = "knight",
+        },
+        headers = { ["Content-Type"] = content_type }
+      })
+      local body = assert.res_status(200, res)
+      local json = cjson.decode(body)
+      assert.equal("MICHAEL", json.username)
+      assert.equal("KNIGHT", json.custom_id)
+    end
+  end)
+
+  it_content_types("post-processes crud.put", function(content_type)
+    return function()
+      local res = assert(client:send {
+        method = "PUT",
+        path = "/consumers/michael/post_processed",
+        body = {
+          username = "garthe",
+          custom_id = "knight",
+        },
+        headers = { ["Content-Type"] = content_type }
+      })
+      local body = assert.res_status(201, res)
+      local json = cjson.decode(body)
+      assert.equal("GARTHE", json.username)
+      assert.equal("KNIGHT", json.custom_id)
+    end
+  end)
+end)

spec/fixtures/custom_plugins/kong/plugins/admin-api-post-process/handler.lua
@@ -1,0 +1,16 @@
+-- a plugin fixture to test post-processing on the admin api
+local BasePlugin = require "kong.plugins.base_plugin"
+
+
+local AdminApiPostProcess = BasePlugin:extend()
+
+
+AdminApiPostProcess.PRIORITY = 1000
+
+
+function AdminApiPostProcess:new()
+  AdminApiPostProcess.super.new(self, "admin-api-post-process")
+end
+
+
+return AdminApiPostProcess

spec/fixtures/custom_plugins/kong/plugins/admin-api-post-process/schema.lua
@@ -1,0 +1,3 @@
+return {
+  fields = {}
+}

commit 3bff217e2f2d980419e100e05961974d6d58542e

hotfix(reports) propagate API events
Follow-up fix for cc4ffc30397b0a1291c456f39840d88ba00f7b98 which
introduced a new, unused argument and broke the reports feature for
post-creation of APIs and plugins.

kong/api/crud_helpers.lua
@@ -203,7 +203,7 @@ function _M.get(primary_keys, dao_collection, post_process)
 end
 
 --- Insertion of an entity.
-function _M.post(params, dao_collection, success, post_process)
+function _M.post(params, dao_collection, post_process)
   local data, err = dao_collection:insert(params)
   if err then
     return app_helpers.yield_error(err)

spec/fixtures/custom_plugins/kong/plugins/admin-api-post-process/api.lua
@@ -27,8 +27,7 @@ return {
     end,
 
     POST = function(self, dao_factory)
-      crud.post(self.params, dao_factory.consumers, nil,
-                uppercase_table_values)
+      crud.post(self.params, dao_factory.consumers, uppercase_table_values)
     end,
   },
 