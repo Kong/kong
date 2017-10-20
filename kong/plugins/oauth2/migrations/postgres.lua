6696e6db0c2907a5ab88b8418d1a99b0c7bcbf42
supraja01 <ssuresh.zteam@zuora.com>
2017-10-19 17:05:44 -0700

    feat(oauth2) new parameter auth_header_name
    Adds to the `oauth2` plugin a new parameter `auth_header_name` to define the header name
    to use. By default its value is `"authorization"`.
    
    From #2928
    
    Signed-off-by: Hisham Muhammad <hisham@gobolinux.org>

--- kong/plugins/oauth2/migrations/postgres.lua
+++ kong/plugins/oauth2/migrations/postgres.lua
@@ -1,3 +1,5 @@
+local plugin_config_iterator = require("kong.dao.migrations.helpers").plugin_config_iterator
+
 return {
   {
     name = "2015-08-03-132400_init_oauth2",
@@ -164,5 +166,23 @@ return {
     down = [[
       ALTER TABLE oauth2_credentials ADD CONSTRAINT oauth2_credentials_client_secret_key UNIQUE(client_secret);
     ]],
-  }
+  },
+  {
+    name = "2017-10-19-set_auth_header_name_default",
+    up = function(_, _, dao)
+      for ok, config, update in plugin_config_iterator(dao, "oauth2") do
+        if not ok then
+          return config
+        end
+        if config.auth_header_name == nil then
+          config.auth_header_name = "authorization"
+          local _, err = update(config)
+          if err then
+            return err
+          end
+        end
+      end
+    end,
+    down = function(_, _, dao) end  -- not implemented
+  },
 }
