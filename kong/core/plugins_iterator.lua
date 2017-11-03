d9399b07592adb61609522494d06afdafdea33fa
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2017-11-03 20:32:46 +0200

    feat(plugins-iterator) implement iterator based on route and service (replacing api)

--- kong/core/plugins_iterator.lua
+++ kong/core/plugins_iterator.lua
@@ -1,15 +1,22 @@
-local responses = require "kong.tools.responses"
-local singletons = require "kong.singletons"
+local responses    = require "kong.tools.responses"
+local singletons   = require "kong.singletons"
+
 
 local setmetatable = setmetatable
+local ipairs       = ipairs
+
 
 -- Loads a plugin config from the datastore.
 -- @return plugin config table or an empty sentinel table in case of a db-miss
-local function load_plugin_into_memory(api_id, consumer_id, plugin_name)
+local function load_plugin_into_memory(route_id,
+                                       service_id,
+                                       consumer_id,
+                                       plugin_name)
   local rows, err = singletons.dao.plugins:find_all {
-    api_id = api_id,
-    consumer_id = consumer_id,
-    name = plugin_name
+             name = plugin_name,
+         route_id = route_id,
+       service_id = service_id,
+      consumer_id = consumer_id,
   }
   if err then
     error(err)
@@ -17,29 +24,42 @@ local function load_plugin_into_memory(api_id, consumer_id, plugin_name)
 
   if #rows > 0 then
     for _, row in ipairs(rows) do
-      if api_id == row.api_id and consumer_id == row.consumer_id then
+      if    route_id == row.route_id    and
+          service_id == row.service_id  and
+         consumer_id == row.consumer_id then
         return row
       end
     end
   end
-
-  return nil
+  -- insert a cached value to not trigger too many DB queries.
+  return { null = true }  -- works because: `.enabled == nil`
 end
+
 
 --- Load the configuration for a plugin entry in the DB.
 -- Given an API, a Consumer and a plugin name, retrieve the plugin's
 -- configuration if it exists. Results are cached in ngx.dict
--- @param[type=string] api_id ID of the API being proxied.
+-- @param[type=string] route_id ID of the route being proxied.
+-- @param[type=string] service_id ID of the service being proxied.
 -- @param[type=string] consumer_id ID of the Consumer making the request (if any).
 -- @param[type=stirng] plugin_name Name of the plugin being tested for.
 -- @treturn table Plugin retrieved from the cache or database.
-local function load_plugin_configuration(api_id, consumer_id, plugin_name)
+local function load_plugin_configuration(route_id,
+                                         service_id,
+                                         consumer_id,
+                                         plugin_name)
   local plugin_cache_key = singletons.dao.plugins:cache_key(plugin_name,
-                                                            api_id,
+                                                            route_id,
+                                                            service_id,
                                                             consumer_id)
-  local plugin, err = singletons.cache:get(plugin_cache_key, nil,
+
+  local plugin, err = singletons.cache:get(plugin_cache_key,
+                                           nil,
                                            load_plugin_into_memory,
-                                           api_id, consumer_id, plugin_name)
+                                           route_id,
+                                           service_id,
+                                           consumer_id,
+                                           plugin_name)
   if err then
     ngx.ctx.delay_response = false
     return responses.send_HTTP_INTERNAL_SERVER_ERROR(err)
@@ -49,9 +69,9 @@ local function load_plugin_configuration(api_id, consumer_id, plugin_name)
   end
 end
 
+
 local function get_next(self)
-  local i = self.i
-  i = i + 1
+  local i = self.i + 1
 
   local plugin = self.loaded_plugins[i]
   if not plugin then
@@ -64,35 +84,84 @@ local function get_next(self)
 
   -- load the plugin configuration in early phases
   if self.access_or_cert_ctx then
-    local api = self.api
+
+    local route        = self.route
+    local service      = self.service
+    local consumer     = ctx.authenticated_consumer
+
+    if consumer then
+      local schema = plugin.schema
+      if schema and schema.no_consumer then
+        consumer = nil
+      end
+    end
+
+    local    route_id = route    and    route.id or nil
+    local  service_id = service  and  service.id or nil
+    local consumer_id = consumer and consumer.id or nil
+
+    local plugin_name = plugin.name
+
     local plugin_configuration
 
-    local consumer = ctx.authenticated_consumer
-    if consumer then
-      local consumer_id = consumer.id
-      local schema      = plugin.schema
+    repeat
 
-      if schema and not schema.no_consumer then
-        if api then
-          plugin_configuration = load_plugin_configuration(api.id, consumer_id, plugin.name)
-        end
-        if not plugin_configuration then
-          plugin_configuration = load_plugin_configuration(nil, consumer_id, plugin.name)
+      if route_id and service_id and consumer_id then
+        plugin_configuration = load_plugin_configuration(route_id, service_id, consumer_id, plugin_name)
+        if plugin_configuration then
+          break
         end
       end
-    end
 
-    if not plugin_configuration then
-      -- Search API specific, or global
-      if api then
-        plugin_configuration = load_plugin_configuration(api.id, nil, plugin.name)
+      if route_id and consumer_id then
+        plugin_configuration = load_plugin_configuration(route_id, nil, consumer_id, plugin_name)
+        if plugin_configuration then
+          break
+        end
       end
-      if not plugin_configuration then
-        plugin_configuration = load_plugin_configuration(nil, nil, plugin.name)
-      end
-    end
 
-    ctx.plugins_for_request[plugin.name] = plugin_configuration
+      if service_id and consumer_id then
+        plugin_configuration = load_plugin_configuration(nil, service_id, consumer_id, plugin_name)
+        if plugin_configuration then
+          break
+        end
+      end
+
+      if route_id and service_id then
+        plugin_configuration = load_plugin_configuration(route_id, service_id, nil, plugin_name)
+        if plugin_configuration then
+          break
+        end
+      end
+
+      if consumer_id then
+        plugin_configuration = load_plugin_configuration(nil, nil, consumer_id, plugin_name)
+        if plugin_configuration then
+          break
+        end
+      end
+
+      if route_id then
+        plugin_configuration = load_plugin_configuration(route_id, nil, nil, plugin_name)
+        if plugin_configuration then
+          break
+        end
+      end
+
+      if service_id then
+        plugin_configuration = load_plugin_configuration(nil, service_id, nil, plugin_name)
+        if plugin_configuration then
+          break
+        end
+      end
+
+      plugin_configuration = load_plugin_configuration(nil, nil, nil, plugin_name)
+
+    until true
+
+    if plugin_configuration then
+      ctx.plugins_for_request[plugin.name] = plugin_configuration
+    end
   end
 
   -- return the plugin configuration
@@ -104,7 +173,9 @@ local function get_next(self)
   return get_next(self) -- Load next plugin
 end
 
+
 local plugin_iter_mt = { __call = get_next }
+
 
 --- Plugins for request iterator.
 -- Iterate over the plugin loaded for a request, stored in
@@ -123,12 +194,14 @@ local function iter_plugins_for_req(loaded_plugins, access_or_cert_ctx)
   local plugin_iter_state = {
     i                     = 0,
     ctx                   = ctx,
-    api                   = ctx.api,
+    route                 = ctx.route,
+    service               = ctx.service,
     loaded_plugins        = loaded_plugins,
     access_or_cert_ctx    = access_or_cert_ctx,
   }
 
   return setmetatable(plugin_iter_state, plugin_iter_mt)
 end
+
 
 return iter_plugins_for_req
