be0421b916660255ba8e0d6582842fedb137e89e
kinman <kinman@users.noreply.github.com>
2017-12-03 15:43:21 -0800

    style(vitals) clean up vitals module (#85)
    Move to local functions those that are not called outside the module.
    Try to group functions logically.

--- kong/vitals/init.lua
+++ kong/vitals/init.lua
@@ -44,34 +44,6 @@ local worker_count = ngx.worker.count()
 local _M = {}
 local mt = { __index = _M }
 
---[[
-  Returns the names of tables created by the vitals module, mainly
-  for use in `kong migrations reset`.
-
-  Add to this list when you create a new vitals table.
- ]]
-function _M.table_names(dao)
-
-  -- tables common across both dbs
-  local table_names = {
-    "vitals_consumers",
-    "vitals_node_meta",
-    "vitals_stats_hours",
-    "vitals_stats_minutes",
-    "vitals_stats_seconds",
-  }
-  local table_count = #table_names
-
-  if dao.db_type == "postgres" then
-    -- pick up the tables created at runtime
-    for i, v in ipairs(pg_strat.dynamic_table_names(dao)) do
-      table_names[table_count+i] = v
-    end
-  end
-
-  return table_names
-end
-
 ffi.cdef[[
   typedef uint32_t time_t;
 
@@ -91,18 +63,20 @@ local vitals_metrics_t_size      = ffi.sizeof("vitals_metrics_t")
 local const_vitals_metrics_t_ptr = ffi.typeof("const vitals_metrics_t*")
 
 
--- generate the initializers for an array of vitals_metrics_t structs
--- of size `sz`. this table will be fed as the third param of the ffi.new()
--- call to generate our vitals_metrics_t[?]. the first two elements are
--- initialized to 0, as they are just incrementing counters. the third and
--- fourth initializers (for proxy_latency_min and proxy_latency_max,
--- respectively) serve as sentinel values. we call math.min/max on these values
--- so they must be initialized to some value that is handle in Lua land as a
--- number. when we prepare to push stats to our strategy, we check for the
--- presence of this sentinel value; if it exists, we never called the path to
--- which the values are associated, meaning that the vitals strategy expects
--- these to be nil. the final element is the timestamp associated with the
--- bucket
+--[[
+  generate the initializers for an array of vitals_metrics_t structs
+  of size `sz`. this table will be fed as the third param of the ffi.new()
+  call to generate our vitals_metrics_t[?]. the first two elements are
+  initialized to 0, as they are just incrementing counters. the third and
+  fourth initializers (for proxy_latency_min and proxy_latency_max,
+  respectively) serve as sentinel values. we call math.min/max on these values
+  so they must be initialized to some value that is handle in Lua land as a
+  number. when we prepare to push stats to our strategy, we check for the
+  presence of this sentinel value; if it exists, we never called the path to
+  which the values are associated, meaning that the vitals strategy expects
+  these to be nil. the final element is the timestamp associated with the
+  bucket
+]]
 local function metrics_t_arr_init(sz)
   local t = new_tab(sz, 0)
 
@@ -113,22 +87,6 @@ local function metrics_t_arr_init(sz)
   end
 
   return t
-end
-
-
--- acquire a flush local
-function _M:flush_lock()
-  local ok, err = self.shm:safe_add(FLUSH_LOCK_KEY, true,
-                                    self.flush_interval - 0.01)
-  if not ok then
-    if err ~= "exists" then
-      log(ERR, "failed to acquire vitals flush lock: ", err)
-    end
-
-    return false
-  end
-
-  return true
 end
 
 
@@ -255,32 +213,37 @@ persistence_handler = function(premature, self)
 end
 
 
--- returns formatted vitals seconds or minutes data based on the query_type, level, and node_id
-function _M:get_stats(query_type, level, node_id)
-  if query_type ~= "minutes" and query_type ~= "seconds" then
-    return nil, "Invalid query params: interval must be 'minutes' or 'seconds'"
-  end
+local function parse_dictionary_key(key)
+  -- split on |
+  local p = key:find("|", 1, true)
+  local id = key:sub(1, p - 1)
+  p = p + 1
+  local timestamp = tonumber(key:sub(p))
 
-  if level ~= "cluster" and level ~= "node" then
-    return nil, "Invalid query params: level must be 'cluster' or 'node'"
-  end
-
-
-  if not utils.is_valid_uuid(node_id) and node_id ~= nil then
-    return nil, "Invalid query params: invalid node_id"
-  end
-
-  local res, err = self.strategy:select_stats(query_type, level, node_id)
-
-  if err then
-    log(WARN, _log_prefix, err)
-    return {}
-  end
-
-  return self:convert_stats(res)
+  return id, timestamp
 end
 
 
+-- converts Kong stats to format expected by Vitals API
+local function convert_stats(vitals, res)
+  local stats = {}
+
+  for _, row in ipairs(res) do
+    stats[row.node_id] = stats[row.node_id] or {}
+
+    stats[row.node_id][vitals.strategy:get_timestamp_str(row.at)] = {
+      row.l2_hit,
+      row.l2_miss,
+      row.plat_min or json_null,
+      row.plat_max or json_null,
+    }
+  end
+
+  return stats
+end
+
+
+-- converts customer stats to format expected by Vitals API
 local function convert_customer_stats(vitals, res)
   local stats = {}
 
@@ -293,112 +256,7 @@ local function convert_customer_stats(vitals, res)
 end
 
 
---[[
-For use by the Vitals API to retrieve consumer stats
-(currently total request count per consumer).
-opts includes the following:
-  consumer_id = <consumer uuid>,
-  duration    = <"seconds" or "minutes">,
-  level       = <"node" or "cluster">,
-  node_id     = <node uuid (optional)>
-
-return value is a table:
-{
-  meta = {
-    consumer = {
-      id = <uuid>,
-    },
-    node = {
-      id       = <uuid>,
-    }, -- an empty table if node_id wasn't provided in opts
-    interval = "seconds",
-  },
-  stats = {
-    <node_id> = { <- node_id is a node uuid or "cluster"
-      ts = count,
-      ts = count,
-      ...
-    },
-  }
-}
-
-]]
-function _M:get_consumer_stats(opts)
-  if not opts.consumer_id or not opts.duration or not opts.level then
-    return nil, "Invalid query params: consumer_id, duration, and level are required"
-  end
-
-  if opts.duration ~= "seconds" and opts.duration ~= "minutes" then
-    return nil, "Invalid query params: duration must be 'minutes' or 'seconds'"
-  end
-
-  if opts.level ~= "node" and opts.level ~= "cluster" then
-    return nil, "Invalid query params: level must be 'node' or 'cluster'"
-  end
-
-  if opts.node_id and not utils.is_valid_uuid(opts.node_id) then
-    return nil, "Invalid query params: invalid node_id"
-  end
-
-  local query_opts = {
-    consumer_id = opts.consumer_id,
-    duration    = opts.duration == "seconds" and 1 or 60,
-    level       = opts.level,
-    node_id     = opts.node_id,
-  }
-
-  local res, _ = self.strategy:select_consumer_stats(query_opts)
-
-  if not res then
-    return nil, "Failed to retrieve stats for consumer " .. opts.consumer_id
-  end
-
-  local retval = {
-    meta = {
-      interval = opts.duration,
-      consumer = {
-        id = opts.consumer_id,
-      },
-    },
-    stats = convert_customer_stats(self, res)
-  }
-
-  if opts.node_id then
-    retval.meta.node = {
-      id = opts.node_id,
-    }
-  end
-
-  return retval
-end
-
-
--- converts the db query result into a standardized format for the admin API
-function _M:convert_stats(res)
-  local stats = {}
-
-  -- convert [timestamp, node_id, l2_hit, l2_miss, latency_min, latency_max]
-  -- to {
-  --   node_id = {
-  --     timestamp = [hit, miss, latency_min, latency_max]
-  --   }
-  -- }
-  for _, row in ipairs(res) do
-    stats[row.node_id] = stats[row.node_id] or {}
-
-    stats[row.node_id][self.strategy:get_timestamp_str(row.at)] = {
-      row.l2_hit,
-      row.l2_miss,
-      row.plat_min or json_null,
-      row.plat_max or json_null,
-    }
-  end
-
-  return stats
-end
-
-
-function _M:build_flush_key()
+local function build_flush_key(vitals)
   local timestamp = time()
 
   -- hack around minor timing differences among worker processes. its possible
@@ -409,16 +267,32 @@ function _M:build_flush_key()
   --
   -- TODO this whole situation can be avoided by synchronizing the flush key
   -- amongst the node workers via worker events
-  if timestamp % self.flush_interval ~= 0 then
+  if timestamp % vitals.flush_interval ~= 0 then
     log(DEBUG, _log_prefix, "tried to build flush key at invalid timestamp")
 
-    if (timestamp + 1) % self.flush_interval == 0 then
+    if (timestamp + 1) % vitals.flush_interval == 0 then
       log(DEBUG, _log_prefix, "cheat timestamp up 1 sec")
       timestamp = timestamp + 1
     end
   end
 
   return FLUSH_LIST_KEY .. timestamp
+end
+
+
+-- acquire a lock for flushing counters to the database
+function _M:flush_lock()
+  local ok, err = self.shm:safe_add(FLUSH_LOCK_KEY, true,
+    self.flush_interval - 0.01)
+  if not ok then
+    if err ~= "exists" then
+      log(ERR, "failed to acquire vitals flush lock: ", err)
+    end
+
+    return false
+  end
+
+  return true
 end
 
 
@@ -519,17 +393,6 @@ function _M:merge_worker_data(flush_key)
 end
 
 
-local function parse_dictionary_key(key)
-  -- split on |
-  local p = key:find("|", 1, true)
-  local id = key:sub(1, p - 1)
-  p = p + 1
-  local timestamp = tonumber(key:sub(p))
-
-  return id, timestamp
-end
-
-
 function _M:flush_consumer_counters()
   log(DEBUG, _log_prefix, "flushing consumer counters")
   local keys = consumers_dict:get_keys()
@@ -589,7 +452,7 @@ function _M:flush_counters()
     local buf = ffi.string(self.counters.metrics,
                            vitals_metrics_t_size * self.flush_interval)
 
-    flush_key = self:build_flush_key()
+    flush_key = build_flush_key(self)
     log(DEBUG, _log_prefix, flush_key)
 
     local ok, err = self.shm:rpush(flush_key, buf)
@@ -635,35 +498,25 @@ function _M:flush_counters()
 end
 
 
-function _M:cache_accessed(hit_lvl, key, value)
-  if not self:enabled() then
-    return "vitals not enabled"
-  end
-
-  local counter_name
-
-  if hit_lvl == 2 then
-    counter_name = "l2_hits"
-  elseif hit_lvl == 3 then
-    counter_name = "l2_misses"
-  end
-
-  if counter_name then
-    self:increment_counter(counter_name)
-  end
-
-  return "ok"
-end
-
-
-function _M:increment_counter(counter_name)
-  local bucket, err = self:current_bucket()
+local function increment_counter(vitals, counter_name)
+  local bucket, err = vitals:current_bucket()
 
   if bucket then
-   self.counters.metrics[bucket][counter_name] = self.counters.metrics[bucket][counter_name] + 1
+    vitals.counters.metrics[bucket][counter_name] = vitals.counters.metrics[bucket][counter_name] + 1
   else
     log(DEBUG, _log_prefix, err)
   end
+end
+
+
+function _M:reset_counters(counters)
+  local counters = counters or self.counters
+
+  counters.start_at = time()
+  counters.metrics  = ffi.new(vitals_metrics_t_arr_type, self.flush_interval,
+                        metrics_t_arr_init(self.flush_interval))
+
+  return counters
 end
 
 
@@ -679,14 +532,167 @@ function _M:current_bucket()
 end
 
 
-function _M:reset_counters(counters)
-  local counters = counters or self.counters
+--[[
+                         FOR THE VITALS API
+  Functions in this section are called by the Vitals (Admin) API.
+ ]]
+function _M:get_stats(query_type, level, node_id)
+  if query_type ~= "minutes" and query_type ~= "seconds" then
+    return nil, "Invalid query params: interval must be 'minutes' or 'seconds'"
+  end
 
-  counters.start_at = time()
-  counters.metrics  = ffi.new(vitals_metrics_t_arr_type, self.flush_interval,
-                        metrics_t_arr_init(self.flush_interval))
+  if level ~= "cluster" and level ~= "node" then
+    return nil, "Invalid query params: level must be 'cluster' or 'node'"
+  end
 
-  return counters
+
+  if not utils.is_valid_uuid(node_id) and node_id ~= nil then
+    return nil, "Invalid query params: invalid node_id"
+  end
+
+  local res, err = self.strategy:select_stats(query_type, level, node_id)
+
+  if err then
+    log(WARN, _log_prefix, err)
+    return {}
+  end
+
+  return convert_stats(self, res)
+end
+
+
+--[[
+For use by the Vitals API to retrieve consumer stats
+(currently total request count per consumer).
+opts includes the following:
+  consumer_id = <consumer uuid>,
+  duration    = <"seconds" or "minutes">,
+  level       = <"node" or "cluster">,
+  node_id     = <node uuid (optional)>
+
+return value is a table:
+{
+  meta = {
+    consumer = {
+      id = <uuid>,
+    },
+    node = {
+      id       = <uuid>,
+    }, -- an empty table if node_id wasn't provided in opts
+    interval = "seconds",
+  },
+  stats = {
+    <node_id> = { <- node_id is a node uuid or "cluster"
+      ts = count,
+      ts = count,
+      ...
+    },
+  }
+}
+
+]]
+function _M:get_consumer_stats(opts)
+  if not opts.consumer_id or not opts.duration or not opts.level then
+    return nil, "Invalid query params: consumer_id, duration, and level are required"
+  end
+
+  if opts.duration ~= "seconds" and opts.duration ~= "minutes" then
+    return nil, "Invalid query params: duration must be 'minutes' or 'seconds'"
+  end
+
+  if opts.level ~= "node" and opts.level ~= "cluster" then
+    return nil, "Invalid query params: level must be 'node' or 'cluster'"
+  end
+
+  if opts.node_id and not utils.is_valid_uuid(opts.node_id) then
+    return nil, "Invalid query params: invalid node_id"
+  end
+
+  local query_opts = {
+    consumer_id = opts.consumer_id,
+    duration    = opts.duration == "seconds" and 1 or 60,
+    level       = opts.level,
+    node_id     = opts.node_id,
+  }
+
+  local res, _ = self.strategy:select_consumer_stats(query_opts)
+
+  if not res then
+    return nil, "Failed to retrieve stats for consumer " .. opts.consumer_id
+  end
+
+  local retval = {
+    meta = {
+      interval = opts.duration,
+      consumer = {
+        id = opts.consumer_id,
+      },
+    },
+    stats = convert_customer_stats(self, res)
+  }
+
+  if opts.node_id then
+    retval.meta.node = {
+      id = opts.node_id,
+    }
+  end
+
+  return retval
+end
+
+
+--[[
+                         INTERFACES TO KONG CORE
+  Functions in this section are called by Kong core when Vitals is enabled.
+  In general, no errors should percolate up from these functions -- trap and
+  log here so that core does not have to do any exception handling for Vitals.
+ ]]
+
+--[[
+  Returns the names of tables created by the vitals module, mainly for use in
+  `kong migrations reset`. Add to this list when you create a new vitals table.
+ ]]
+function _M.table_names(dao)
+
+  -- tables common across both dbs
+  local table_names = {
+    "vitals_consumers",
+    "vitals_node_meta",
+    "vitals_stats_hours",
+    "vitals_stats_minutes",
+    "vitals_stats_seconds",
+  }
+  local table_count = #table_names
+
+  if dao.db_type == "postgres" then
+    -- pick up the tables created at runtime
+    for i, v in ipairs(pg_strat.dynamic_table_names(dao)) do
+      table_names[table_count+i] = v
+    end
+  end
+
+  return table_names
+end
+
+
+function _M:cache_accessed(hit_lvl, key, value)
+  if not self:enabled() then
+    return "vitals not enabled"
+  end
+
+  local counter_name
+
+  if hit_lvl == 2 then
+    counter_name = "l2_hits"
+  elseif hit_lvl == 3 then
+    counter_name = "l2_misses"
+  end
+
+  if counter_name then
+    increment_counter(self, counter_name)
+  end
+
+  return "ok"
 end
 
 
