Kong/kong

commit 3a60016153e337e184db3343c0866557838ee222

feat(reports) send database_version and redis_version (#3085)
In order to offer better support for the different versions of each
database Kong is compatible with, we need to be able to make decisions
based on field usage data.

This commit adds the Cassandra/PostgreSQL/Redis version as part of the
`PING` signal, when `anonymous_reports` is enabled. We only retrieve the
first two digits of the version number to avoid high cardinality values
as much as possible, and to stay concise in our UDP packet.

We introduce tests for the version retrieval of both datastores
(PostgreSQL and Cassandra).

Unfortunately, there exist no tests for the reports `ping` signal, and
the `redis_version` retrieval is even more cumbersome to test. Those
have been tested manually for now (yuck).

Signed-off-by: Hisham Muhammad <hisham@gobolinux.org>

spec/02-integration/03-dao/01-factory_spec.lua
@@ -19,4 +19,38 @@ helpers.for_each_dao(function(kong_conf)
       assert.equal(factory.daos.plugins, factory.plugins)
     end)
   end)
+
+  describe(":init() + :infos()", function()
+    it("returns DB info + 'unknown' for version if missing", function()
+      local factory = assert(Factory.new(kong_conf))
+      local info = factory:infos()
+
+      if kong_conf.database == "postgres" then
+        assert.same({
+          desc = "database",
+          name = kong_conf.pg_database,
+          version = "unknown",
+        }, info)
+
+      elseif kong_conf.database == "cassandra" then
+        assert.same({
+          desc = "keyspace",
+          name = kong_conf.cassandra_keyspace,
+          version = "unknown",
+        }, info)
+
+      else
+        error("unknown database")
+      end
+    end)
+
+    it("returns DB version if :init() called", function()
+      local factory = assert(Factory.new(kong_conf))
+      assert(factory:init())
+
+      local info = factory:infos()
+      assert.is_string(info.version)
+      assert.not_equal("unknown", info.version)
+    end)
+  end)
 end)

kong/dao/db/cassandra.lua
@@ -105,8 +105,13 @@ local function extract_major(release_version)
   return match(release_version, "^(%d+)%.%d")
 end
 
+local function extract_major_minor(release_version)
+  return match(release_version, "^(%d+%.%d+)")
+end
+
 local function cluster_release_version(peers)
-  local first_release_version
+  local major_minor_version
+  local major_version
   local mismatch
 
   for i = 1, #peers do
@@ -115,14 +120,17 @@ local function cluster_release_version(peers)
       return nil, 'no release_version for peer ' .. peers[i].host
     end
 
-    local major_version = extract_major(release_version)
-    if not major_version then
+    local peer_major_version = extract_major(release_version)
+    if not peer_major_version then
       return nil, 'failed to extract major version for peer ' .. peers[i].host ..
                   ' version: ' .. tostring(peers[i].release_version)
     end
+
     if i == 1 then
-      first_release_version = major_version
-    elseif major_version ~= first_release_version then
+      major_version = peer_major_version
+      major_minor_version = extract_major_minor(release_version)
+
+    elseif peer_major_version ~= major_version then
       mismatch = true
       break
     end
@@ -139,10 +147,14 @@ local function cluster_release_version(peers)
     return nil, concat(err_t, " ")
   end
 
-  return tonumber(first_release_version)
+  return {
+    major = major_version,
+    major_minor = major_minor_version,
+  }
 end
 
 _M.extract_major = extract_major
+_M.extract_major_minor = extract_major_minor
 _M.cluster_release_version = cluster_release_version
 
 function _M:init()
@@ -155,10 +167,13 @@ function _M:init()
   if err then return nil, err
   elseif not peers then return nil, 'no peers in shm' end
 
-  self.release_version, err = cluster_release_version(peers)
-  if not self.release_version then
+  local res, err = cluster_release_version(peers)
+  if not res then
     return nil, err
   end
+
+  self.major_version_n = tonumber(res.major)
+  self.major_minor_version = res.major_minor
 
   return true
 end
@@ -166,7 +181,8 @@ end
 function _M:infos()
   return {
     desc = "keyspace",
-    name = self.cluster_options.keyspace
+    name = self.cluster_options.keyspace,
+    version = self.major_minor_version or "unknown",
   }
 end
 
@@ -657,14 +673,14 @@ end
 function _M:current_migrations()
   local q_keyspace_exists, q_migrations_table_exists
 
-  if not self.release_version then
+  if not self.major_version_n then
     local ok, err = self:init()
     if not ok then
       return nil, err
     end
   end
 
-  if self.release_version == 3 then
+  if self.major_version_n == 3 then
     q_keyspace_exists = [[
       SELECT * FROM system_schema.keyspaces
       WHERE keyspace_name = ?

kong/core/reports.lua
@@ -1,6 +1,5 @@
 local cjson = require "cjson.safe"
 local utils = require "kong.tools.utils"
-local singletons = require "kong.singletons"
 local constants = require "kong.constants"
 
 
@@ -22,6 +21,7 @@ local BUFFERED_REQUESTS_COUNT_KEYS = "events:requests"
 
 
 local _buffer = {}
+local _ping_infos = {}
 local _enabled = false
 local _unique_str = utils.random_string()
 local _buffer_immutable_idx
@@ -158,11 +158,10 @@ local function ping_handler(premature)
     n_requests = 0
   end
 
-  send_report("ping", {
-    requests = n_requests,
-    unique_id = _unique_str,
-    database = singletons.configuration.database
-  })
+  _ping_infos.requests = n_requests
+  _ping_infos.unique_id = _unique_str
+
+  send_report("ping", _ping_infos)
 
   local ok, err = kong_dict:incr(BUFFERED_REQUESTS_COUNT_KEYS, -n_requests, n_requests)
   if not ok then
@@ -179,6 +178,9 @@ return {
     end
 
     create_timer(PING_INTERVAL, ping_handler)
+  end,
+  add_ping_value = function(k, v)
+    _ping_infos[k] = v
   end,
   log = function()
     if not _enabled then

kong/dao/db/postgres.lua
@@ -79,11 +79,34 @@ end
 function _M:infos()
   return {
     desc = "database",
-    name = self:clone_query_options().database
+    name = self:clone_query_options().database,
+    version = self.major_minor_version or "unknown",
   }
 end
 
 local do_clean_ttl
+
+function _M.extract_major_minor(release_version)
+  return match(release_version, "^(%d+%.%d+)")
+end
+
+function _M:init()
+  local res, err = self:query("SHOW server_version;")
+  if not res then
+    return nil, err
+  end
+
+  if #res < 1 or not res[1].server_version then
+    return nil, Errors.db("could not retrieve server_version")
+  end
+
+  self.major_minor_version = _M.extract_major_minor(res[1].server_version)
+  if not self.major_minor_version then
+    return nil, Errors.db("could not extract major.minor version")
+  end
+
+  return true
+end
 
 function _M:init_worker()
   local ok, err = timer_at(TTL_CLEANUP_INTERVAL, do_clean_ttl, self)

kong/init.lua
@@ -120,7 +120,13 @@ local function load_plugins(kong_conf, dao)
   -- add reports plugin if not disabled
   if kong_conf.anonymous_reports then
     local reports = require "kong.core.reports"
+
+    local db_infos = dao:infos()
+    reports.add_ping_value("database", kong_conf.database)
+    reports.add_ping_value("database_version", db_infos.version)
+
     reports.toggle(true)
+
     sorted_plugins[#sorted_plugins+1] = {
       name = "reports",
       handler = reports,

kong/plugins/rate-limiting/policies/init.lua
@@ -12,6 +12,7 @@ local get_local_key = function(api_id, identifier, period_date, name)
   return fmt("ratelimit:%s:%s:%s:%s", api_id, identifier, period_date, name)
 end
 
+local _retrieved_redis_version
 local EXPIRATIONS = {
   second = 1,
   minute = 60,
@@ -96,6 +97,31 @@ return {
         if not ok then
           ngx_log(ngx.ERR, "failed to change Redis database: ", err)
           return nil, err
+        end
+      end
+
+      if not _retrieved_redis_version then
+        _retrieved_redis_version = true
+
+        if singletons.configuration.anonymous_reports then
+          local reports = require "kong.core.reports"
+          local redis_version
+
+          -- we will run this branch for each worker's first hit, but never
+          -- again. Hopefully someday Redis will be made a first class citizen
+          -- in Kong and its integration can be tied deeper into the core,
+          -- avoiding such "hacks". This logic should work for Redis >= 2.4.
+          local res, err = red:info("server")
+          if type(res) ~= "string" then
+            -- could be nil or ngx.null
+            ngx_log(ngx.ERR, "failed to retrieve Redis version: ", err)
+
+          else
+            -- retrieved first 2 digits only
+            redis_version = res:match("redis_version:(%d+%.%d+).-\r\n")
+          end
+
+          reports.add_ping_value("redis_version", redis_version or "unknown")
         end
       end
 

spec/01-unit/01-db/02-cassandra_spec.lua
@@ -11,6 +11,16 @@ describe("cassandra_db", function()
     end)
   end)
 
+  describe("extract_major_minor()", function()
+    it("extract major and minor version digits", function()
+      assert.equal("3.7", cassandra_db.extract_major_minor("3.7"))
+      assert.equal("3.7", cassandra_db.extract_major_minor("3.7.12"))
+      assert.equal("2.1", cassandra_db.extract_major_minor("2.1.14"))
+      assert.equal("2.10", cassandra_db.extract_major_minor("2.10"))
+      assert.equal("10.0", cassandra_db.extract_major_minor("10.0"))
+    end)
+  end)
+
   describe("cluster_release_version()", function()
     it("extracts major release_version from available peers", function()
       local release_version = assert(cassandra_db.cluster_release_version {
@@ -27,7 +37,10 @@ describe("cassandra_db", function()
           release_version = "3.1.2",
         }
       })
-      assert.equal(3, release_version)
+      assert.same({
+        major       = "3",
+        major_minor = "3.7",
+      }, release_version)
 
       local release_version = assert(cassandra_db.cluster_release_version {
         {
@@ -43,7 +56,10 @@ describe("cassandra_db", function()
           release_version = "2.2.4",
         }
       })
-      assert.equal(2, release_version)
+      assert.same({
+        major       = "2",
+        major_minor = "2.14",
+      }, release_version)
     end)
     it("errors with different major versions", function()
       local release_version, err = cassandra_db.cluster_release_version {

spec/01-unit/01-db/03-postgres_spec.lua
@@ -1,0 +1,16 @@
+local postgres_db = require "kong.dao.db.postgres"
+
+
+describe("postgres_db", function()
+  describe("extract_major_minor()", function()
+    it("extract major and minor version digits", function()
+      assert.equal("9.4", postgres_db.extract_major_minor("9.4.11"))
+      assert.equal("9.4", postgres_db.extract_major_minor("9.4"))
+      assert.equal("9.5", postgres_db.extract_major_minor("9.5.6"))
+      assert.equal("9.6", postgres_db.extract_major_minor("9.6.10"))
+      assert.equal("9.10", postgres_db.extract_major_minor("9.10"))
+      assert.equal("10.0", postgres_db.extract_major_minor("10.0.1"))
+      assert.equal("10.0", postgres_db.extract_major_minor("10.0"))
+    end)
+  end)
+end)

commit 8010beb94bec72ef49d0e2a8c3ebac3265acf145

hotfix(migrations) check major C* version against new field
Regression introduced by #3085.

This highlights the dangers of using the DAO in our migrations. While
each migration should be frozen in time an always considered valid given
its parents have previously run, using our DAO in the migrations
introduces a time factor as the DAO undergoes changes over time that
might make previously written migrations suddenly become invalid
(sometimes in worst cases than this, if the schema of entities has
changed).

kong/dao/migrations/cassandra.lua
@@ -319,7 +319,7 @@ return {
     up = function(db, kong_config)
       local keyspace_name = kong_config.cassandra_keyspace
 
-      if db.release_version < 3 then
+      if db.major_version_n < 3 then
         local rows, err = db:query([[
           SELECT *
           FROM system.schema_columns

spec/02-integration/03-dao/01-factory_spec.lua
@@ -52,5 +52,14 @@ helpers.for_each_dao(function(kong_conf)
       assert.is_string(info.version)
       assert.not_equal("unknown", info.version)
     end)
+
+    if kong_conf.database == "cassandra" then
+      it("[cassandra] sets the 'major_version_n' field on the DB", function()
+        local factory = assert(Factory.new(kong_conf))
+        assert(factory:init())
+
+        assert.is_number(factory.db.major_version_n)
+      end)
+    end
   end)
 end)