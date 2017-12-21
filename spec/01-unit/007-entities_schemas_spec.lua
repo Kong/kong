Kong/kong

commit 3e240b8722141d65b84ac69ab5c2bbc3522eb69d

hotfix(healthchecks) disable circuit-breaker functionality by default
To avoid unexpected behaviors for users, this makes all health counter
thresholds to be zero by default, effectively making the circuit-breaker
functionality of passive healthchecks to be disabled by default.
Users need to opt-in by supplying threshold values to enable the
cut-off of targets based on analysis of ongoing traffic.

This commit requires functionality introduced in lua-resty-healthcheck 0.3.0.

kong/dao/schemas/upstreams.lua
@@ -63,30 +63,30 @@ local healthchecks_defaults = {
     concurrency = 10,
     http_path = "/",
     healthy = {
-      interval = 0, -- 0 = disabled by default
+      interval = 0,  -- 0 = probing disabled by default
       http_statuses = { 200, 302 },
-      successes = 2,
+      successes = 0, -- 0 = disabled by default
     },
     unhealthy = {
-      interval = 0, -- 0 = disabled by default
+      interval = 0, -- 0 = probing disabled by default
       http_statuses = { 429, 404,
                         500, 501, 502, 503, 504, 505 },
-      tcp_failures = 2,
-      timeouts = 3,
-      http_failures = 5,
+      tcp_failures = 0,  -- 0 = disabled by default
+      timeouts = 0,      -- 0 = disabled by default
+      http_failures = 0, -- 0 = disabled by default
     },
   },
   passive = {
     healthy = {
       http_statuses = { 200, 201, 202, 203, 204, 205, 206, 207, 208, 226,
                         300, 301, 302, 303, 304, 305, 306, 307, 308 },
-      successes = 5,
+      successes = 0,
     },
     unhealthy = {
       http_statuses = { 429, 500, 503 },
-      tcp_failures = 2,
-      timeouts = 7,
-      http_failures = 5,
+      tcp_failures = 0,  -- 0 = circuit-breaker disabled by default
+      timeouts = 0,      -- 0 = circuit-breaker disabled by default
+      http_failures = 0, -- 0 = circuit-breaker disabled by default
     },
   },
 }

commit f3d887d85dfffc203775cb38da599ca6bef536d1

hotfix(upstreams) ensure 0 is a valid value for health check fields
A regression introduced in 3e240b8722141d65b84ac69ab5c2bbc3522eb69d.

kong/dao/schemas/upstreams.lua
@@ -14,10 +14,23 @@ local function check_nonnegative(arg)
   end
 end
 
-
 local function check_positive_int(t)
   if t < 1 or t > 2^31 - 1 or math.floor(t) ~= t then
     return false, "must be an integer between 1 and " .. 2^31 - 1
+  end
+
+  return true
+end
+
+local function check_positive_int_or_zero(t)
+  if t == 0 then
+    return true
+  end
+
+  local ok = check_positive_int(t)
+  if not ok then
+    return false, "must be 0 (disabled), or an integer between 1 and "
+                  .. 2 ^31 - 1
   end
 
   return true
@@ -96,10 +109,10 @@ local funcs = {
   timeout = check_nonnegative,
   concurrency = check_positive_int,
   interval = check_nonnegative,
-  successes = check_positive_int,
-  tcp_failures = check_positive_int,
-  timeouts = check_positive_int,
-  http_failures = check_positive_int,
+  successes = check_positive_int_or_zero,
+  tcp_failures = check_positive_int_or_zero,
+  timeouts = check_positive_int_or_zero,
+  http_failures = check_positive_int_or_zero,
   http_path = check_http_path,
   http_statuses = check_http_statuses,
 }

spec/01-unit/007-entities_schemas_spec.lua
@@ -754,6 +754,7 @@ describe("Entities Schemas", function()
       local tests = {
         {{ active = { timeout = -1 }}, "greater than or equal to 0" },
         {{ active = { concurrency = 0.5 }}, "must be an integer" },
+        {{ active = { concurrency = 0 }}, "must be an integer" },
         {{ active = { concurrency = -10 }}, "must be an integer" },
         {{ active = { http_path = "" }}, "is empty" },
         {{ active = { http_path = "ovo" }}, "must be prefixed with slash" },
@@ -764,8 +765,8 @@ describe("Entities Schemas", function()
         {{ active = { healthy = { http_statuses = { 99 }}}}, "status code" },
         {{ active = { healthy = { http_statuses = { 1000 }}}}, "status code" },
         {{ active = { healthy = { http_statuses = { 111.314 }}}}, "must be an integer" },
-        {{ active = { healthy = { successes = 0.5 }}}, "must be an integer" },
-        {{ active = { healthy = { successes = 0 }}}, "must be an integer" },
+        {{ active = { healthy = { successes = 0.5 }}}, "must be 0 (disabled), or an integer" },
+        --{{ active = { healthy = { successes = 0 }}}, "must be an integer" },
         {{ active = { healthy = { successes = -1 }}}, "an integer between" },
         {{ active = { unhealthy = { interval = -1 }}}, "greater than or equal to 0" },
         {{ active = { unhealthy = { http_statuses = 404 }}}, "not an array" },
@@ -773,36 +774,37 @@ describe("Entities Schemas", function()
         {{ active = { unhealthy = { http_statuses = { -1 }}}}, "status code" },
         {{ active = { unhealthy = { http_statuses = { 99 }}}}, "status code" },
         {{ active = { unhealthy = { http_statuses = { 1000 }}}}, "status code" },
-        {{ active = { unhealthy = { tcp_failures = 0.5 }}}, "must be an integer" },
-        {{ active = { unhealthy = { tcp_failures = 0 }}}, "must be an integer" },
+        {{ active = { unhealthy = { tcp_failures = 0.5 }}}, "must be 0 (disabled), or an integer" },
+        --{{ active = { unhealthy = { tcp_failures = 0 }}}, "must be an integer" },
         {{ active = { unhealthy = { tcp_failures = -1 }}}, "an integer between" },
-        {{ active = { unhealthy = { timeouts = 0.5 }}}, "must be an integer" },
-        {{ active = { unhealthy = { timeouts = 0 }}}, "must be an integer" },
+        {{ active = { unhealthy = { timeouts = 0.5 }}}, "must be 0 (disabled), or an integer" },
+        --{{ active = { unhealthy = { timeouts = 0 }}}, "must be an integer" },
         {{ active = { unhealthy = { timeouts = -1 }}}, "an integer between" },
-        {{ active = { unhealthy = { http_failures = 0.5 }}}, "must be an integer" },
+        {{ active = { unhealthy = { http_failures = 0.5 }}}, "must be 0 (disabled), or an integer" },
         {{ active = { unhealthy = { http_failures = -1 }}}, "an integer between" },
         {{ passive = { healthy = { http_statuses = 404 }}}, "not an array" },
         {{ passive = { healthy = { http_statuses = { "ovo" }}}}, "not a number" },
         {{ passive = { healthy = { http_statuses = { -1 }}}}, "status code" },
         {{ passive = { healthy = { http_statuses = { 99 }}}}, "status code" },
         {{ passive = { healthy = { http_statuses = { 1000 }}}}, "status code" },
-        {{ passive = { healthy = { successes = 0.5 }}}, "must be an integer" },
-        {{ passive = { healthy = { successes = 0 }}}, "must be an integer" },
+        {{ passive = { healthy = { successes = 0.5 }}}, "must be 0 (disabled), or an integer" },
+        --{{ passive = { healthy = { successes = 0 }}}, "must be an integer" },
         {{ passive = { healthy = { successes = -1 }}}, "an integer between" },
         {{ passive = { unhealthy = { http_statuses = 404 }}}, "not an array" },
         {{ passive = { unhealthy = { http_statuses = { "ovo" }}}}, "not a number" },
         {{ passive = { unhealthy = { http_statuses = { -1 }}}}, "status code" },
         {{ passive = { unhealthy = { http_statuses = { 99 }}}}, "status code" },
         {{ passive = { unhealthy = { http_statuses = { 1000 }}}}, "status code" },
-        {{ passive = { unhealthy = { tcp_failures = 0.5 }}}, "must be an integer" },
-        {{ passive = { unhealthy = { tcp_failures = 0 }}}, "must be an integer" },
+        {{ passive = { unhealthy = { tcp_failures = 0.5 }}}, "must be 0 (disabled), or an integer" },
+        --{{ passive = { unhealthy = { tcp_failures = 0 }}}, "must be an integer" },
         {{ passive = { unhealthy = { tcp_failures = -1 }}}, "an integer between" },
-        {{ passive = { unhealthy = { timeouts = 0.5 }}}, "must be an integer" },
-        {{ passive = { unhealthy = { timeouts = 0 }}}, "must be an integer" },
+        {{ passive = { unhealthy = { timeouts = 0.5 }}}, "must be 0 (disabled), or an integer" },
+        --{{ passive = { unhealthy = { timeouts = 0 }}}, "must be an integer" },
         {{ passive = { unhealthy = { timeouts = -1 }}}, "an integer between" },
-        {{ passive = { unhealthy = { http_failures = 0.5 }}}, "must be an integer" },
-        {{ passive = { unhealthy = { http_failures = 0 }}}, "must be an integer" },
+        {{ passive = { unhealthy = { http_failures = 0.5 }}}, "must be 0 (disabled), or an integer" },
+        --{{ passive = { unhealthy = { http_failures = 0 }}}, "must be an integer" },
         {{ passive = { unhealthy = { http_failures = -1 }}}, "an integer between" },
+        --]]
       }
       for _, test in ipairs(tests) do
         local entity = {
@@ -821,7 +823,7 @@ describe("Entities Schemas", function()
 
         local valid, errors = validate_entity(entity, upstreams_schema)
         assert.is_false(valid)
-        assert.match(test[2], errors[field_name])
+        assert.match(test[2], errors[field_name], nil, true)
       end
 
       -- tests for success
@@ -858,10 +860,23 @@ describe("Entities Schemas", function()
 
     end)
 
+    it("creates an upstream with the default values", function()
+      local default = upstreams_schema.fields.healthchecks.default
+      local entity = {
+        name = "x",
+        healthchecks = default,
+      }
+
+      local valid, errors = validate_entity(entity, upstreams_schema)
+      assert.is_nil(errors)
+      assert.is_true(valid)
+    end)
+
     it("should require (optional) slots in a valid range", function()
       local valid, errors, check, _
       local data = { name = "valid.host.name" }
-      valid, _, _ = validate_entity(data, upstreams_schema)
+      valid, errors, _ = validate_entity(data, upstreams_schema)
+      assert.is_nil(errors)
       assert.is_true(valid)
       assert.equal(slots_default, data.slots)
 