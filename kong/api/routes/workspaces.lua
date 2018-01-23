ac896b6ef017c0812cb693bea17c209df1adaa02
Robert Paprocki <robert@cryptobells.com>
2018-01-22 21:11:18 -0800

    feat(workspaces) Admin API endpoints for workspace <-> entities
    Partial commit of eba05d640d320e9ba96336c1aad70a6e90da509a.

--- kong/api/routes/workspaces.lua
+++ kong/api/routes/workspaces.lua
@@ -1,4 +1,7 @@
-local crud = require "kong.api.crud_helpers"
+local cjson = require "cjson"
+local crud  = require "kong.api.crud_helpers"
+local utils = require "kong.tools.utils"
+local rbac  = require "kong.rbac"
 
 return {
   ["/workspaces/"] = {
@@ -33,4 +36,186 @@ return {
       crud.delete(self.workspace, dao_factory.workspaces)
     end,
   },
+
+  ["/workspaces/:workspace_name_or_id/entities"] = {
+    before = function(self, dao_factory, helpers)
+      self.params.workspace_name_or_id = ngx.unescape_uri(self.params.workspace_name_or_id)
+      crud.find_workspace_by_name_or_id(self, dao_factory, helpers)
+    end,
+
+    GET = function(self, dao_factory, helpers)
+      if self.params.resolve then
+        local entities_hash = rbac.resolve_workspace_entities({ self.workspace.id })
+
+        local e = setmetatable({}, cjson.empty_array_mt)
+        for i = 1, #entities_hash do
+          e[i] = entities_hash[i]
+        end
+
+        return helpers.responses.send_HTTP_OK(e)
+      else
+        local entities, err = dao_factory.workspace_entities:find_all({
+          workspace_id = self.workspace.id,
+        })
+        if err then
+          return helpers.yield_error(err)
+        end
+
+        return helpers.responses.send_HTTP_OK({
+          data = entities,
+          total = #entities,
+        })
+      end
+    end,
+
+    POST = function(self, dao_factory, helpers)
+      if not self.params.entities then
+        return helpers.responses.send_HTTP_BAD_REQUEST("must provide >= entity")
+      end
+
+
+      -- duplication check
+      local existing_entities, err = dao_factory.workspace_entities:find_all({
+        workspace_id = self.workspace.id,
+      })
+      if err then
+        return helpers.yield_error(err)
+      end
+
+
+      -- which one of these are workspace references?
+      local ws_ref = {}
+
+
+      local entity_ids = utils.split(self.params.entities, ",")
+
+      for i = 1, #entity_ids do
+        local e = entity_ids[i]
+
+        if not utils.is_valid_uuid(e) then
+          helpers.responses.send_HTTP_BAD_REQUEST("'" .. e .. "' is not a valid UUID")
+        end
+
+        -- duplication check
+        for j = 1, #existing_entities do
+          if e == existing_entities[i].entity_id then
+            local err = "Entity '" .. entity_ids[j] .. "' already associated " ..
+                        "with workspace '" .. self.workspace.id .. "'"
+            return helpers.responses.send_HTTP_CONFLICT(err)
+
+          end
+        end
+
+        -- circular reference check
+        local refs, err = dao_factory.workspace_entities:find_all({
+          workspace_id = e,
+          entity_id = self.workspace.id,
+        })
+        if err then
+          return helpers.yield_error(err)
+        end
+        if #refs > 0 then
+          local err = "Attempted to create circular reference (workspace " ..
+                      "'" .. e .. "' already references '" ..
+                      self.workspace.id .. "')"
+          return helpers.responses.send_HTTP_CONFLICT(err)
+        end
+
+        local ws_ref_row, err = dao_factory.workspaces:find({id = e})
+        if err then
+          return helpers.yield_error(err)
+        end
+        if ws_ref_row then
+          ws_ref[e] = true
+        end
+      end
+
+
+      -- yayyyy, no fuckup! now do the thing
+      local res = {}
+      for i = 1, #entity_ids do
+        local row, err = dao_factory.workspace_entities:insert({
+          workspace_id = self.workspace.id,
+          entity_id = entity_ids[i],
+          entity_type = ws_ref[entity_ids[i]] and "workspace" or "entity",
+        })
+        if err then
+          helpers.yield_error(err)
+        end
+
+        table.insert(res, row)
+      end
+
+
+      return helpers.responses.send_HTTP_CREATED(res)
+    end,
+
+    DELETE = function(self, dao_factory, helpers)
+      if not self.params.entities then
+        return helpers.responses.send_HTTP_BAD_REQUEST("must provide >= entity")
+      end
+
+
+      local entity_ids = utils.split(self.params.entities, ",")
+
+      for i = 1, #entity_ids do
+        local e = entity_ids[i]
+
+        local _, err = dao_factory.workspace_entities:delete({
+          workspace_id = self.workspace.id,
+          entity_id = e,
+        })
+        if err then
+          return helpers.yield_error(err)
+        end
+      end
+
+      return helpers.responses.send_HTTP_NO_CONTENT()
+    end,
+  },
+
+  ["/workspaces/:workspace_name_or_id/entities/:entity_id"] = {
+    before = function(self, dao_factory, helpers)
+      self.params.workspace_name_or_id = ngx.unescape_uri(self.params.workspace_name_or_id)
+      crud.find_workspace_by_name_or_id(self, dao_factory, helpers)
+    end,
+
+    GET = function(self, dao_factory, helpers)
+      local e, err = dao_factory.workspace_entities:find({
+        workspace_id = self.workspace.id,
+        entity_id = self.params.entity_id,
+      })
+      if err then
+        return helpers.yield_error(err)
+      end
+
+      return e and helpers.responses.send_HTTP_OK(e) or
+                   helpers.responses.send_HTTP_NOT_FOUND()
+    end,
+
+    DELETE = function(self, dao_factory, helpers)
+      local e, err = dao_factory.workspace_entities:find({
+        workspace_id = self.workspace.id,
+        entity_id = self.params.entity_id,
+      })
+      if err then
+        return helpers.yield_error(err)
+      end
+
+      if e then
+        local _, err = dao_factory.workspace_entities:delete({
+          workspace_id = self.workspace.id,
+          entity_id = self.params.entity_id,
+        })
+        if err then
+          return helpers.yield_error(err)
+        end
+
+        return helpers.responses.send_HTTP_NO_CONTENT()
+
+      else
+        return helpers.responses.send_HTTP_NOT_FOUND()
+      end
+    end,
+  }
 }
---
+++ spec/02-integration/04-admin_api/13-workspaces_spec.lua
@@ -0,0 +1,483 @@
+local dao_helpers = require "spec.02-integration.03-dao.helpers"
+local DAOFactory  = require "kong.dao.factory"
+local helpers     = require "spec.helpers"
+local cjson       = require "cjson"
+local utils       = require "kong.tools.utils"
+
+dao_helpers.for_each_dao(function(kong_config)
+
+describe("(#" .. kong_config.database .. ") Admin API workspaces", function()
+  local client, dao
+
+  setup(function()
+    dao = assert(DAOFactory.new(kong_config))
+
+    dao:truncate_tables()
+    helpers.run_migrations(dao)
+    assert(helpers.start_kong({
+      database = kong_config.database
+    }))
+
+    client = assert(helpers.admin_client())
+  end)
+
+  teardown(function()
+    if client then
+      client:close()
+    end
+
+    dao:truncate_tables()
+
+    helpers.stop_kong()
+  end)
+
+  describe("/workspaces", function()
+    describe("POST", function()
+      it("creates a new workspace", function()
+        local res = assert(client:send {
+          method = "POST",
+          path   = "/workspaces",
+          body   = {
+            name = "foo",
+          },
+          headers = {
+            ["Content-Type"] = "application/json",
+          }
+        })
+
+        local body = assert.res_status(201, res)
+        local json = cjson.decode(body)
+
+        assert.is_true(utils.is_valid_uuid(json.id))
+        assert.equals("foo", json.name)
+        assert.is_nil(json.comment)
+      end)
+
+      it("handles unique constraint conflicts", function()
+        local res = assert(client:send {
+          method = "POST",
+          path   = "/workspaces",
+          body   = {
+            name = "foo",
+          },
+          headers = {
+            ["Content-Type"] = "application/json",
+          }
+        })
+
+        assert.res_status(409, res)
+      end)
+    end)
+
+    describe("GET", function()
+      setup(function()
+        local res = assert(client:send {
+          method = "POST",
+          path   = "/workspaces",
+          body   = {
+            name = "bar",
+          },
+          headers = {
+            ["Content-Type"] = "application/json",
+          }
+        })
+
+        assert.res_status(201, res)
+      end)
+
+      it("retrieves a list of workspaces", function()
+        local res = assert(client:send {
+          method = "GET",
+          path   = "/workspaces",
+        })
+
+        local body = assert.res_status(200, res)
+        local json = cjson.decode(body)
+
+        assert.equals(2, json.total)
+        assert.equals(2, #json.data)
+      end)
+    end)
+  end)
+
+  describe("/workspaces/:workspace", function()
+    describe("PATCH", function()
+      it("updates an existing entity", function()
+        local res = assert(client:send {
+          method = "PATCH",
+          path   = "/workspaces/foo",
+          body   = {
+            comment = "foo comment",
+          },
+          headers = {
+            ["Content-Type"] = "application/json",
+          }
+        })
+
+        local body = assert.res_status(200, res)
+        local json = cjson.decode(body)
+
+        assert.equals("foo comment", json.comment)
+      end)
+    end)
+
+    describe("GET", function()
+      it("retrieves a single workspace", function()
+        local res = assert(client:send {
+          method = "GET",
+          path   = "/workspaces/foo",
+        })
+
+        local body = assert.res_status(200, res)
+        local json = cjson.decode(body)
+
+        assert.equals("foo", json.name)
+      end)
+
+      it("sends the appropriate status on an invalid entity", function()
+        local res = assert(client:send {
+          method = "GET",
+          path   = "/workspaces/baz",
+        })
+
+        assert.res_status(404, res)
+      end)
+    end)
+
+    describe("DELETE", function()
+      it("removes a workspace", function()
+        local res = assert(client:send {
+          method = "DELETE",
+          path   = "/workspaces/bar",
+        })
+
+        assert.res_status(204, res)
+      end)
+
+      it("sends the appropriate status on an invalid entity", function()
+        local res = assert(client:send {
+          method = "DELETE",
+          path   = "/workspaces/bar",
+        })
+
+        assert.res_status(404, res)
+      end)
+    end)
+  end)
+
+  describe("/workspaces/:workspace/entites", function()
+    local uuid1, uuid2
+
+    setup(function()
+      -- yayyyyyyy determinism!
+      uuid1, uuid2 = "182f2cc8-008e-11e8-ba89-0ed5f89f718b",
+                     "182f2f2a-008e-11e8-ba89-0ed5f89f718b"
+
+      local w = dao.workspaces:find_all({
+        name = "foo",
+      })
+      w = w[1].id
+
+      assert(dao.workspace_entities:insert({
+        workspace_id = w,
+        entity_id = uuid1,
+        entity_type = "entity",
+      }))
+      assert(dao.workspace_entities:insert({
+        workspace_id = w,
+        entity_id = uuid2,
+        entity_type = "entity",
+      }))
+    end)
+
+    describe("GET", function()
+      it("returns a list of entites associated with the workspace", function()
+        local res = assert(client:send {
+          method = "GET",
+          path = "/workspaces/foo/entities",
+        })
+
+        local body = assert.res_status(200, res)
+        local json = cjson.decode(body)
+
+        table.sort(json.data, function(a, b) return a.entity_id < b.entity_id end)
+
+        assert.equals(2, json.total)
+        assert.equals(2, #json.data)
+        assert.equals(uuid1, json.data[1].entity_id)
+        assert.equals(uuid2, json.data[2].entity_id)
+      end)
+
+      it("resolves entity relationships when specified", function()
+        local res = assert(client:send {
+          method = "GET",
+          path = "/workspaces/foo/entities?resolve",
+        })
+
+        local body = assert.res_status(200, res)
+        local json = cjson.decode(body)
+
+        table.sort(json)
+
+        assert.equals(2, #json)
+        assert.equals(uuid1, json[1])
+        assert.equals(uuid2, json[2])
+      end)
+    end)
+
+    describe("POST", function()
+      describe("creates a new relationship", function()
+        it("as an entity", function()
+          local e = utils.uuid()
+
+          local res = assert(client:send {
+            method = "POST",
+            path = "/workspaces/foo/entities",
+            body = {
+              entities = e,
+            },
+            headers = {
+              ["Content-Type"] = "application/json",
+            }
+          })
+
+          local body = assert.res_status(201, res)
+          local json = cjson.decode(body)
+
+          assert.equals(e, json[1].entity_id)
+          assert.equals("entity", json[1].entity_type)
+        end)
+
+        describe("as a workspace", function()
+          local w
+
+          setup(function()
+            w = assert(dao.workspaces:insert({
+              name = "bar",
+            }))
+          end)
+
+          it("", function()
+            local res = assert(client:send {
+              method = "POST",
+              path = "/workspaces/foo/entities",
+              body = {
+                entities = w.id,
+              },
+              headers = {
+                ["Content-Type"] = "application/json",
+              }
+            })
+
+            local body = assert.res_status(201, res)
+            local json = cjson.decode(body)
+
+            assert.equals(w.id, json[1].entity_id)
+            assert.equals("workspace", json[1].entity_type)
+          end)
+        end)
+      end)
+
+      describe("handles errors", function()
+        it("on duplicate association", function()
+          local res = assert(client:send {
+            method = "POST",
+            path = "/workspaces/foo/entities",
+            body = {
+              entities = "182f2cc8-008e-11e8-ba89-0ed5f89f718b",
+            },
+            headers = {
+              ["Content-Type"] = "application/json",
+            },
+          })
+
+          local body = assert.res_status(409, res)
+          local json = cjson.decode(body)
+
+          assert.matches("Entity '182f2cc8-008e-11e8-ba89-0ed5f89f718b' " ..
+                         "already associated with workspace", json.message, nil,
+                         true)
+        end)
+
+        it("on invalid UUID", function()
+          local res = assert(client:send {
+            method = "POST",
+            path = "/workspaces/foo/entities",
+            body = {
+              entities = "nop",
+            },
+            headers = {
+              ["Content-Type"] = "application/json",
+            },
+          })
+
+          local body = assert.res_status(400, res)
+          local json = cjson.decode(body)
+
+          assert.equals("'nop' is not a valid UUID", json.message)
+        end)
+
+        it("on circular reference", function()
+          local bar_id = dao.workspaces:find_all({
+            name = "bar",
+          })[1].id
+          local foo_id = dao.workspaces:find_all({
+            name = "foo",
+          })[1].id
+
+          local res = assert(client:send {
+            method = "POST",
+            path = "/workspaces/bar/entities",
+            body = {
+              entities = foo_id,
+            },
+            headers = {
+              ["Content-Type"] = "application/json",
+            }
+          })
+
+          local body = assert.res_status(409, res)
+          local json = cjson.decode(body)
+
+          assert.equals("Attempted to create circular reference " ..
+                        "(workspace '" .. foo_id .. "' already references " ..
+                        "'" .. bar_id .. "')", json.message)
+        end)
+
+        it("without inserting some valid rows prior to failure", function()
+          local n = dao.workspace_entities:find_all({
+            workspace_id = dao.workspaces:find_all({ name = "foo" })[1].id
+          })
+          n = #n
+
+          local res = assert(client:send {
+            method = "POST",
+            path = "/workspaces/foo/entities",
+            body = {
+              entities = utils.uuid() .. ",nop",
+            },
+            headers = {
+              ["Content-Type"] = "application/json",
+            },
+          })
+
+          local body = assert.res_status(400, res)
+          local json = cjson.decode(body)
+
+          assert.equals("'nop' is not a valid UUID", json.message)
+
+          local new_n = dao.workspace_entities:find_all({
+            workspace_id = dao.workspaces:find_all({ name = "foo" })[1].id
+          })
+          new_n = #new_n
+          assert.same(n, new_n)
+        end)
+      end)
+    end)
+
+    describe("DELETE", function()
+      it("removes a relationship", function()
+        local n = dao.workspace_entities:find_all({
+          workspace_id = dao.workspaces:find_all({ name = "foo" })[1].id
+        })
+        n = #n
+
+        local res = assert(client:send {
+          method = "DELETE",
+          path = "/workspaces/foo/entities",
+          body = {
+            entities = "182f2cc8-008e-11e8-ba89-0ed5f89f718b",
+          },
+          headers = {
+            ["Content-Type"] = "application/json",
+          },
+        })
+
+        assert.res_status(204, res)
+
+        local new_n = dao.workspace_entities:find_all({
+          workspace_id = dao.workspaces:find_all({ name = "foo" })[1].id
+        })
+        new_n = #new_n
+        assert.equals(n - 1, new_n)
+      end)
+
+      it("sends the appropriate status on an invalid entity", function()
+        local res = assert(client:send {
+          method = "DELETE",
+          path = "/workspaces/baz/entities",
+          body = {
+            entities = utils.uuid(),
+          },
+          headers = {
+            ["Content-Type"] = "application/json",
+          },
+        })
+
+        assert.res_status(404, res)
+      end)
+    end)
+  end)
+
+  describe("/workspaces/:workspace/entites/:entity", function()
+    local w_id, e_id
+
+    setup(function()
+      w_id = dao.workspaces:find_all({ name = "foo" })[1].id
+      e_id = utils.uuid()
+
+      assert(dao.workspace_entities:insert({
+        workspace_id = w_id,
+        entity_id = e_id,
+        entity_type = "entity",
+      }))
+    end)
+
+    describe("GET", function()
+      it("returns a single relation representation", function()
+        local res = assert(client:send {
+          method = "GET",
+          path = "/workspaces/foo/entities/" .. e_id,
+        })
+
+        local body = assert.res_status(200, res)
+        local json = cjson.decode(body)
+
+        assert.equals(json.workspace_id, w_id)
+        assert.equals(json.entity_id, e_id)
+        assert.equals(json.entity_type, "entity")
+      end)
+
+      it("sends the appropriate status on an invalid entity", function()
+        local res = assert(client:send {
+          method = "GET",
+          path = "/workspaces/foo/entities/" .. utils.uuid(),
+        })
+
+        assert.res_status(404, res)
+      end)
+    end)
+
+    describe("DELETE", function()
+      it("removes a single relation representation", function()
+        local res = assert(client:send {
+          method = "DELETE",
+          path = "/workspaces/foo/entities/" .. e_id,
+        })
+
+        assert.res_status(204, res)
+      end)
+
+      it("sends the appropriate status on an invalid entity", function()
+        local res = assert(client:send {
+          method = "DELETE",
+          path = "/workspaces/foo/entities/" .. utils.uuid(),
+        })
+
+        assert.res_status(404, res)
+      end)
+    end)
+  end)
+end)
+
+end)
