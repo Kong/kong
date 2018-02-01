Kong/kong

commit 04f967ba95040e66298f454b5f48b0aabf4f8923

chore(makefile) uninstall step before installing in 'dev' rule (#3170)

Makefile
@@ -15,7 +15,9 @@ endif
 install:
 	@luarocks make OPENSSL_DIR=$(OPENSSL_DIR) CRYPTO_DIR=$(OPENSSL_DIR)
 
-dev: install
+dev:
+	@luarocks remove kong
+	@luarocks make OPENSSL_DIR=$(OPENSSL_DIR) CRYPTO_DIR=$(OPENSSL_DIR)
 	@for rock in $(DEV_ROCKS) ; do \
 	  if luarocks list --porcelain $$rock | grep -q "installed" ; then \
 	    echo $$rock already installed, skipping ; \

commit e97ff3da9b9adaf37a54c909d3dbae16163e0338

chore(makefile) ignore luarocks remove failures
Oftentimes, this command will fail because Kong is not installed. We now
avoid having to call make with the -k option as a workaround.

This is a regression introduced by 04f967b.

Makefile
@@ -16,7 +16,7 @@ install:
 	@luarocks make OPENSSL_DIR=$(OPENSSL_DIR) CRYPTO_DIR=$(OPENSSL_DIR)
 
 dev:
-	@luarocks remove kong
+	-@luarocks remove kong
 	@luarocks make OPENSSL_DIR=$(OPENSSL_DIR) CRYPTO_DIR=$(OPENSSL_DIR)
 	@for rock in $(DEV_ROCKS) ; do \
 	  if luarocks list --porcelain $$rock | grep -q "installed" ; then \