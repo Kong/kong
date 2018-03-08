Kong/kong

commit de6390e34c5301aa4c965d52f08e00ab81f91efa

fix(cache) ensure soft errors from callbacks are caught
The callback run inside a `pcall` and the error checking will only
deal with the `pcall` failing, not with a `pcall` success and
`returning nil + err`.

This is temporary until the mlcache dependency is updated to
support callback-returned errors.

From #3150

kong/cache.lua
@@ -32,6 +32,30 @@ local function log(lvl, ...)
 end
 
 
+-- Temporary fix to convert soft callback errors into hard ones.
+-- FIXME: use upstream mlcache lib instead of local copy
+local soft_to_hard
+do
+  local s2h_cache = setmetatable({}, { __mode = "k" })
+  
+  local function create_wrapper(cb)
+    s2h_cache[cb] = function(...)
+      local result, err = cb(...)
+      if err then
+        error(err)
+      end
+      return result
+    end
+    return s2h_cache[cb]
+  end
+
+
+  soft_to_hard = function(cb)
+    return s2h_cache[cb] or create_wrapper(cb)
+  end
+end
+
+
 local _M = {}
 local mt = { __index = _M }
 
@@ -107,7 +131,7 @@ function _M:get(key, opts, cb, ...)
 
   --log(DEBUG, "get from key: ", key)
 
-  local v, err = self.mlcache:get(key, opts, cb, ...)
+  local v, err = self.mlcache:get(key, opts, soft_to_hard(cb), ...)
   if err then
     return nil, "failed to get from node cache: " .. err
   end

spec/02-integration/03-dao/10-cache_spec.lua
@@ -1,0 +1,81 @@
+local kong_cache = require "kong.cache"
+local kong_cluster_events = require "kong.cluster_events"
+local Factory = require "kong.dao.factory"
+local helpers = require "spec.helpers"
+local worker_events = require "resty.worker.events"
+local create_unique_key = require("kong.tools.utils").uuid
+
+
+local HARD_ERROR = { err = "hard:" .. create_unique_key() }
+local SOFT_ERROR = { err = "soft:" .. create_unique_key() }
+
+
+local cb_call_count
+
+
+local function load_into_memory(value_to_return)
+  cb_call_count = cb_call_count + 1
+  if value_to_return == HARD_ERROR then
+    return error(value_to_return.err)
+  elseif value_to_return == SOFT_ERROR then
+    return nil, value_to_return.err
+  else
+    return value_to_return
+  end
+end
+
+
+describe("dao in-memory cache", function()
+
+  local cache, key
+
+  setup(function()
+    assert(worker_events.configure {
+      shm = "kong_process_events",
+    })
+    local dao_factory = assert(Factory.new(helpers.test_conf))
+    local cluster_events = assert(kong_cluster_events.new {
+      dao = dao_factory,
+    })
+    cache = kong_cache.new {
+      cluster_events = cluster_events,
+      worker_events = worker_events,
+    }
+  end)
+
+  before_each(function()
+    cb_call_count = 0
+    key = create_unique_key()
+  end)
+
+
+  it("handles soft callback errors", function()
+    for _ = 1, 2 do
+      local value, err = cache:get(key, nil, load_into_memory, SOFT_ERROR)
+      assert.is_nil(value)
+      assert.matches(SOFT_ERROR.err, err, nil, true)
+    end
+    assert.equals(2, cb_call_count)
+  end)
+
+
+  it("handles hard callback errors", function()
+    for _ = 1, 2 do
+      local value, err = cache:get(key, nil, load_into_memory, HARD_ERROR)
+      assert.is_nil(value)
+      assert.matches(HARD_ERROR.err, err, nil, true)
+    end
+    assert.equals(2, cb_call_count)
+  end)
+
+
+  it("handles nil as return value", function()
+    for _ = 1, 2 do
+      local value, err = cache:get(key, nil, load_into_memory, nil)
+      assert.is_nil(value)
+      assert.is_nil(err)
+    end
+    assert.equals(1, cb_call_count)
+  end)
+
+end)

commit 1daebc1dc6bb46bdcac889dc5f602ef30cc59fd0

fix(cache) do not use callback as hard-error wrapper keys
Part of the fixes for #3277 for a regression introduced in
de6390e34c5301aa4c965d52f08e00ab81f91efa.

While the soft_to_hard error wrapper tries its best to be GC-friendly by
not keeping references on the DB cache callbacks as they are used as keys
to index their corresponding soft_to_hard_error wrappers.

Unfortunately, because the created closure holds a reference to the
callback itself, said callback cannot be garbage-collected.

This is especially troublesome for callbacks that are generated on the
fly, like so:

    local cache, err = cache:get("key", opts, function()
        return "cached value"
    end)

The above anonymous function should be garbage-collected, but the
soft_to_hard_error still holds a reference to it.
This example could be observed prior to the parent commit, in which we
replaced the cache callback to not create a function on the fly anymore.

The drawback of this change is a larger memory footprint, the same
cache callback will result in several copies of the same
soft_to_hard_error function, if they are invoked with several caching
keys.

kong/cache.lua
@@ -37,21 +37,21 @@ end
 local soft_to_hard
 do
   local s2h_cache = setmetatable({}, { __mode = "k" })
-  
-  local function create_wrapper(cb)
-    s2h_cache[cb] = function(...)
+
+  local function create_wrapper(key, cb)
+    s2h_cache[key] = function(...)
       local result, err = cb(...)
       if err then
         error(err)
       end
       return result
     end
-    return s2h_cache[cb]
+    return s2h_cache[key]
   end
 
 
-  soft_to_hard = function(cb)
-    return s2h_cache[cb] or create_wrapper(cb)
+  soft_to_hard = function(key, cb)
+    return s2h_cache[key] or create_wrapper(key, cb)
   end
 end
 
@@ -131,7 +131,7 @@ function _M:get(key, opts, cb, ...)
 
   --log(DEBUG, "get from key: ", key)
 
-  local v, err = self.mlcache:get(key, opts, soft_to_hard(cb), ...)
+  local v, err = self.mlcache:get(key, opts, soft_to_hard(key, cb), ...)
   if err then
     return nil, "failed to get from node cache: " .. err
   end