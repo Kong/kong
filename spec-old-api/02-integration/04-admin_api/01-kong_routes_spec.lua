0ceff8aac890cc14060dea45ea84a31a797127ed
Hisham Muhammad <hisham@gobolinux.org>
2018-03-08 16:39:28 -0800

    tests(spec-old-api) remove replicated tests
    Removes tests that are identical to the ones in spec/

--- spec-old-api/02-integration/04-admin_api/01-kong_routes_spec.lua
+++ spec-old-api/02-integration/04-admin_api/01-kong_routes_spec.lua
@@ -3,7 +3,7 @@ local cjson = require "cjson"
 
 local DAOFactory = require "kong.dao.factory"
 
-local dao_helpers = require "spec-old-api.02-integration.03-dao.helpers"
+local dao_helpers = require "spec.02-integration.03-dao.helpers"
 
 describe("Admin API - Kong routes", function()
   describe("/", function()
