Kong/kong

commit 976dd8726df69cb874157db7f8d7fa15b0a1d9b1

feat(config) add 'headers' property to replace '*_tokens'
As new headers are introduced by Kong's core, instead of introducing a
config option for each and every header or set of headers, an array of
these values can be now specified using the `headers` config option.

This offers a fine grained controlled and does not blow up the number of
configuration options.

The goal here is to move towards a  simpler and easier to understand
configuration, similar to 1b9976f60 (#3147).

From #3300

Signed-off-by: Thibault Charbonnier <thibaultcha@me.com>

kong/conf_loader.lua
@@ -17,6 +17,16 @@ local DEFAULT_PATHS = {
   "/etc/kong.conf"
 }
 
+local headers = constants.HEADERS
+local header_key_to_name = {
+  ["server_tokens"] = "server_tokens",
+  ["latency_tokens"] = "latency_tokens",
+  [string.lower(headers.VIA)] = headers.VIA,
+  [string.lower(headers.SERVER)] = headers.SERVER,
+  [string.lower(headers.PROXY_LATENCY)] = headers.PROXY_LATENCY,
+  [string.lower(headers.UPSTREAM_LATENCY)] = headers.UPSTREAM_LATENCY,
+}
+
 local PREFIX_PATHS = {
   nginx_pid = {"pids", "nginx.pid"},
   nginx_err_logs = {"logs", "error.log"},
@@ -61,8 +71,7 @@ local CONF_INFERENCES = {
   nginx_user = {typ = "string"},
   nginx_worker_processes = {typ = "string"},
   upstream_keepalive = {typ = "number"},
-  server_tokens = {typ = "boolean"},
-  latency_tokens = {typ = "boolean"},
+  headers = {typ = "array"},
   trusted_ips = {typ = "array"},
   real_ip_header = {typ = "string"},
   real_ip_recursive = {typ = "ngx_boolean"},
@@ -275,6 +284,14 @@ local function check_and_infer(conf)
     end)
     if not ok then
       errors[#errors + 1] = err
+    end
+  end
+
+  if conf.headers then
+    for _, token in ipairs(conf.headers) do
+      if token ~= "off" and not header_key_to_name[string.lower(token)] then
+        errors[#errors+1] = "headers: invalid entry '" .. tostring(token) .. "'"
+      end
     end
   end
 
@@ -585,6 +602,35 @@ local function load(path, custom_conf)
         break
       end
     end
+  end
+
+  -- load headers configuration
+  do
+    local headers_enabled = {}
+
+    for _, v in pairs(header_key_to_name) do
+      headers_enabled[v] = false
+    end
+
+    if #conf.headers > 0 and conf.headers[1] ~= "off" then
+      for _, token in ipairs(conf.headers) do
+        if token ~= "off" then
+          headers_enabled[header_key_to_name[string.lower(token)]] = true
+        end
+      end
+    end
+
+    if headers_enabled.server_tokens then
+      headers_enabled[headers.VIA] = true
+      headers_enabled[headers.SERVER] = true
+    end
+
+    if headers_enabled.latency_tokens then
+      headers_enabled[headers.PROXY_LATENCY] = true
+      headers_enabled[headers.UPSTREAM_LATENCY] = true
+    end
+
+    conf.headers = headers_enabled
   end
 
   -- load absolute paths

spec/02-integration/05-proxy/13-server_tokens_spec.lua
@@ -6,7 +6,9 @@ local default_server_header = _KONG._NAME .. "/" .. _KONG._VERSION
 
 
 for _, strategy in helpers.each_strategy() do
-  describe("Server Tokens [#" .. strategy .. "]", function()
+describe("headers [#" .. strategy .. "]", function()
+
+  describe("Server/Via", function()
     local proxy_client
     local bp
 
@@ -74,10 +76,86 @@ for _, strategy in helpers.each_strategy() do
 
     end)
 
-    describe("(with server_tokens = on)", function()
+    describe("(with headers = Via)", function()
 
       setup(start {
-        server_tokens = "on",
+        headers = "Via",
+      })
+
+      teardown(helpers.stop_kong)
+
+      it("should return Kong 'Via' header but not touch 'Server' header when request was proxied", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "headers-inspect.com",
+          }
+        })
+
+        assert.res_status(200, res)
+        assert.equal(default_server_header, res.headers["via"])
+        assert.not_equal(default_server_header, res.headers["server"])
+      end)
+
+      it("should not return Kong 'Via' header or Kong 'Via' header when no API matched (no proxy)", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "404.com",
+          }
+        })
+
+        assert.res_status(404, res)
+        assert.is_nil(res.headers["via"])
+        assert.is_nil(res.headers["server"])
+      end)
+
+    end)
+
+    describe("(with headers = Server)", function()
+
+      setup(start {
+        headers = "Server",
+      })
+
+      teardown(helpers.stop_kong)
+
+      it("should not return Kong 'Via' header but not change the 'Server' header when request was proxied", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "headers-inspect.com",
+          }
+        })
+
+        assert.res_status(200, res)
+        assert.not_equal(default_server_header, res.headers["server"])
+        assert.is_nil(res.headers["via"])
+      end)
+
+      it("should return Kong 'Server' header but not the Kong 'Via' header when no API matched (no proxy)", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "404.com",
+          }
+        })
+
+        assert.res_status(404, res)
+        assert.equal(default_server_header, res.headers["server"])
+        assert.is_nil(res.headers["via"])
+      end)
+
+    end)
+
+    describe("(with headers = server_tokens)", function()
+
+      setup(start {
+        headers = "server_tokens",
       })
 
       teardown(helpers.stop_kong)
@@ -112,10 +190,10 @@ for _, strategy in helpers.each_strategy() do
 
     end)
 
-    describe("(with server_tokens = off)", function()
+    describe("(with no server_tokens in headers)", function()
 
       setup(start {
-        server_tokens = "off",
+        headers = "off",
       })
 
       teardown(helpers.stop_kong)
@@ -153,7 +231,7 @@ for _, strategy in helpers.each_strategy() do
   end)
 
 
-  describe("Latency Tokens", function()
+  describe("X-Kong-Proxy-Latency/X-Kong-Upstream-Latency", function()
     local proxy_client
     local bp
 
@@ -222,10 +300,10 @@ for _, strategy in helpers.each_strategy() do
 
     end)
 
-    describe("(with latency_tokens = on)", function()
+    describe("(with headers = latency_tokens)", function()
 
       setup(start {
-        latency_tokens = "on",
+        headers = "latency_tokens",
       })
 
       teardown(helpers.stop_kong)
@@ -260,10 +338,86 @@ for _, strategy in helpers.each_strategy() do
 
     end)
 
-    describe("(with latency_tokens = off)", function()
+    describe("(with headers = X-Kong-Upstream-Latency)", function()
 
       setup(start {
-        latency_tokens = "off",
+        headers = "X-Kong-Upstream-Latency",
+      })
+
+      teardown(helpers.stop_kong)
+
+      it("should return 'X-Kong-Upstream-Latency' header but not 'X-Kong-Proxy-Latency' when request was proxied", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "headers-inspect.com"
+          }
+        })
+
+        assert.res_status(200, res)
+        assert.is_not_nil(res.headers[constants.HEADERS.UPSTREAM_LATENCY])
+        assert.is_nil(res.headers[constants.HEADERS.PROXY_LATENCY])
+      end)
+
+      it("should not return any latency header when no API matched (no proxy)", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "404.com",
+          }
+        })
+
+        assert.res_status(404, res)
+        assert.is_nil(res.headers[constants.HEADERS.UPSTREAM_LATENCY])
+        assert.is_nil(res.headers[constants.HEADERS.PROXY_LATENCY])
+      end)
+
+    end)
+
+    describe("(with headers = X-Kong-Proxy-Latency)", function()
+
+      setup(start {
+        headers = "X-Kong-Proxy-Latency",
+      })
+
+      teardown(helpers.stop_kong)
+
+      it("should return 'X-Kong-Proxy-Latency' header but not 'X-Kong-Upstream-Latency' when request was proxied", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "headers-inspect.com"
+          }
+        })
+
+        assert.res_status(200, res)
+        assert.is_nil(res.headers[constants.HEADERS.UPSTREAM_LATENCY])
+        assert.is_not_nil(res.headers[constants.HEADERS.PROXY_LATENCY])
+      end)
+
+      it("should not return any latency header when no API matched (no proxy)", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "404.com",
+          }
+        })
+
+        assert.res_status(404, res)
+        assert.is_nil(res.headers[constants.HEADERS.UPSTREAM_LATENCY])
+        assert.is_nil(res.headers[constants.HEADERS.PROXY_LATENCY])
+      end)
+
+    end)
+
+    describe("(with no latency_tokens in headers)", function()
+
+      setup(start {
+        headers = "off",
       })
 
       teardown(function()
@@ -299,5 +453,158 @@ for _, strategy in helpers.each_strategy() do
       end)
 
     end)
+
+    describe("(with headers='server_tokens, X-Kong-Proxy-Latency')", function()
+
+      setup(start{
+        headers = "server_tokens, X-Kong-Proxy-Latency",
+      })
+
+      teardown(helpers.stop_kong)
+
+      it("should return Kong 'Via' and 'X-Kong-Proxy-Latency' header but not change the 'Server' header when request was proxied", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "headers-inspect.com",
+          }
+        })
+
+        assert.res_status(200, res)
+        assert.not_equal(default_server_header, res.headers["server"])
+        assert.equal(default_server_header, res.headers["via"])
+        assert.is_not_nil(res.headers[constants.HEADERS.PROXY_LATENCY])
+      end)
+
+      it("should return Kong 'Server' header but not the Kong 'Via' or 'X-Kong-Proxy-Latency' header when no API matched (no proxy)", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "404.com",
+          }
+        })
+
+        assert.res_status(404, res)
+        assert.equal(default_server_header, res.headers["server"])
+        assert.is_nil(res.headers["via"])
+        assert.is_nil(res.headers[constants.HEADERS.PROXY_LATENCY])
+      end)
+
+    end)
+
+    describe("(with headers='server_tokens, off, X-Kong-Proxy-Latency')", function()
+
+      setup(start{
+        headers = "server_tokens, off, X-Kong-Proxy-Latency",
+      })
+
+      teardown(helpers.stop_kong)
+
+      it("should return Kong 'Via' and 'X-Kong-Proxy-Latency' header as 'off' will not take effect", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "headers-inspect.com",
+          }
+        })
+
+        assert.res_status(200, res)
+        assert.not_equal(default_server_header, res.headers["server"])
+        assert.equal(default_server_header, res.headers["via"])
+        assert.is_not_nil(res.headers[constants.HEADERS.PROXY_LATENCY])
+      end)
+
+      it("should return Kong 'Server' header as 'off' will not take effect", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "404.com",
+          }
+        })
+
+        assert.res_status(404, res)
+        assert.equal(default_server_header, res.headers["server"])
+        assert.is_nil(res.headers["via"])
+        assert.is_nil(res.headers[constants.HEADERS.PROXY_LATENCY])
+      end)
+
+    end)
   end)
+
+  describe("case insensitiveness", function()
+    local proxy_client
+    local bp
+
+    local function start(config)
+      return function()
+        bp.routes:insert {
+          hosts = { "headers-inspect.com" },
+        }
+
+        config = config or {}
+        config.database = strategy
+        config.nginx_conf = "spec/fixtures/custom_nginx.template"
+
+        assert(helpers.start_kong(config))
+      end
+    end
+
+    setup(function()
+      bp = helpers.get_db_utils(strategy)
+    end)
+
+    before_each(function()
+      proxy_client = helpers.proxy_client()
+    end)
+
+    after_each(function()
+      if proxy_client then
+        proxy_client:close()
+      end
+    end)
+
+    describe("are case insensitive", function()
+
+      setup(start{
+        headers = "serVer_TokEns, x-kOng-pRoXy-lAtency"
+      })
+
+      teardown(helpers.stop_kong)
+
+      it("should return Kong 'Via' and 'X-Kong-Proxy-Latency' header", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "headers-inspect.com",
+          }
+        })
+
+        assert.res_status(200, res)
+        assert.not_equal(default_server_header, res.headers["server"])
+        assert.equal(default_server_header, res.headers["via"])
+        assert.is_not_nil(res.headers[constants.HEADERS.PROXY_LATENCY])
+      end)
+
+      it("should return Kong 'Server' header", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            host  = "404.com",
+          }
+        })
+
+        assert.res_status(404, res)
+        assert.equal(default_server_header, res.headers["server"])
+        assert.is_nil(res.headers["via"])
+        assert.is_nil(res.headers[constants.HEADERS.PROXY_LATENCY])
+      end)
+    end)
+  end)
+end)
 end

kong/core/error_handlers.lua
@@ -1,4 +1,5 @@
 local singletons = require "kong.singletons"
+local constants = require "kong.constants"
 
 local find = string.find
 local format = string.format
@@ -57,8 +58,8 @@ return function(ngx)
   local status = ngx.status
   message = BODIES["s" .. status] and BODIES["s" .. status] or format(BODIES.default, status)
 
-  if singletons.configuration.server_tokens then
-    ngx.header["Server"] = SERVER_HEADER
+  if singletons.configuration.headers[constants.HEADERS.SERVER] then
+    ngx.header[constants.HEADERS.SERVER] = SERVER_HEADER
   end
 
   ngx.header["Content-Type"] = content_type

kong/core/handler.lua
@@ -698,21 +698,24 @@ return {
       local header = ngx.header
 
       if ctx.KONG_PROXIED then
-        if singletons.configuration.latency_tokens then
+        if singletons.configuration.headers[constants.HEADERS.UPSTREAM_LATENCY] then
           header[constants.HEADERS.UPSTREAM_LATENCY] = ctx.KONG_WAITING_TIME
-          header[constants.HEADERS.PROXY_LATENCY]    = ctx.KONG_PROXY_LATENCY
         end
 
-        if singletons.configuration.server_tokens then
-          header["Via"] = server_header
+        if singletons.configuration.headers[constants.HEADERS.PROXY_LATENCY] then
+          header[constants.HEADERS.PROXY_LATENCY] = ctx.KONG_PROXY_LATENCY
+        end
+
+        if singletons.configuration.headers[constants.HEADERS.VIA] then
+          header[constants.HEADERS.VIA] = server_header
         end
 
       else
-        if singletons.configuration.server_tokens then
-          header["Server"] = server_header
+        if singletons.configuration.headers[constants.HEADERS.SERVER] then
+          header[constants.HEADERS.SERVER] = server_header
 
         else
-          header["Server"] = nil
+          header[constants.HEADERS.SERVER] = nil
         end
       end
     end

kong.conf.default
@@ -190,15 +190,25 @@
                                  # process. When this number is exceeded, the
                                  # least recently used connections are closed.
 
-#server_tokens = on              # Enables or disables emitting Kong version on
-                                 # error pages and in the "Server" or "Via"
-                                 # (in case the request was proxied) response
-                                 # header field.
-
-#latency_tokens = on             # Enables or disables emitting Kong latency
-                                 # information in the "X-Kong-Proxy-Latency"
-                                 # and "X-Kong-Upstream-Latency" response
-                                 # header fields.
+#headers = server_tokens, latency_tokens
+                                 # Specify Kong-specific headers that should
+                                 # be injected in responses to the client.
+                                 # Acceptable values are:
+                                 # - `server_tokens`: inject 'Via' and 'Server'
+                                 #   headers.
+                                 # - `latency_tokens`: inject
+                                 #   'X-Kong-Proxy-Latency' and
+                                 #   'X-Kong-Upstream-Latency' headers.
+                                 # - `X-Kong-<header-name>`: only inject this
+                                 #   specific the header when applicable.
+                                 #
+                                 # Example:
+                                 #   headers = via, latency_tokens
+                                 #
+                                 # This value can be set to `off`, which
+                                 # prevents Kong from injecting any of these
+                                 # headers. Note that plugins can still inject
+                                 # headers.
 
 #trusted_ips =                   # Defines trusted IP addresses blocks that are
                                  # known to send correct X-Forwarded-* headers.

kong/constants.lua
@@ -59,7 +59,9 @@ return {
     CONSUMER_GROUPS = "X-Consumer-Groups",
     FORWARDED_HOST = "X-Forwarded-Host",
     FORWARDED_PREFIX = "X-Forwarded-Prefix",
-    ANONYMOUS = "X-Anonymous-Consumer"
+    ANONYMOUS = "X-Anonymous-Consumer",
+    VIA = "Via",
+    SERVER = "Server"
   },
   RATELIMIT = {
     PERIODS = {

kong/templates/kong_defaults.lua
@@ -25,8 +25,7 @@ ssl_ciphers = ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-EC
 admin_ssl_cert = NONE
 admin_ssl_cert_key = NONE
 upstream_keepalive = 60
-server_tokens = on
-latency_tokens = on
+headers = server_tokens, latency_tokens
 trusted_ips = NONE
 real_ip_header = X-Real-IP
 real_ip_recursive = off

spec-old-api/02-integration/05-proxy/12-server_tokens_spec.lua
@@ -80,7 +80,7 @@ describe("Server Tokens", function()
 
     setup(start {
       nginx_conf    = "spec/fixtures/custom_nginx.template",
-      server_tokens = "on",
+      headers = "server_tokens",
     })
 
     teardown(helpers.stop_kong)
@@ -119,7 +119,7 @@ describe("Server Tokens", function()
 
     setup(start {
       nginx_conf    = "spec/fixtures/custom_nginx.template",
-      server_tokens = "off",
+      headers = "off",
     })
 
     teardown(helpers.stop_kong)
@@ -212,7 +212,7 @@ describe("Latency Tokens", function()
 
     setup(start {
       nginx_conf = "spec/fixtures/custom_nginx.template",
-      latency_tokens = "on",
+      headers = "latency_tokens",
     })
 
     teardown(helpers.stop_kong)
@@ -251,7 +251,7 @@ describe("Latency Tokens", function()
 
     setup(start {
       nginx_conf     = "spec/fixtures/custom_nginx.template",
-      latency_tokens = "off",
+      headers = "off",
     })
 
     teardown(function()

commit 75e071ba5fff88fdce7bff1a67a2c6537e62ae89

hotfix(config) allow specifying 'headers' via config file
This fixes a bug introduced in 976dd872 (#3300) where the `headers`
config option were not properly written to `.kong_env`. This would be an
issue when the value was specified from the configuration file (vs. via
environment variables).

From #3419

Signed-off-by: Thibault Charbonnier <thibaultcha@me.com>

kong/conf_loader.lua
@@ -607,31 +607,33 @@ local function load(path, custom_conf)
 
   -- load headers configuration
   do
-    local headers_enabled = {}
+    local enabled_headers = {}
 
     for _, v in pairs(header_key_to_name) do
-      headers_enabled[v] = false
+      enabled_headers[v] = false
     end
 
     if #conf.headers > 0 and conf.headers[1] ~= "off" then
       for _, token in ipairs(conf.headers) do
         if token ~= "off" then
-          headers_enabled[header_key_to_name[string.lower(token)]] = true
+          enabled_headers[header_key_to_name[string.lower(token)]] = true
         end
       end
     end
 
-    if headers_enabled.server_tokens then
-      headers_enabled[headers.VIA] = true
-      headers_enabled[headers.SERVER] = true
+    if enabled_headers.server_tokens then
+      enabled_headers[headers.VIA] = true
+      enabled_headers[headers.SERVER] = true
     end
 
-    if headers_enabled.latency_tokens then
-      headers_enabled[headers.PROXY_LATENCY] = true
-      headers_enabled[headers.UPSTREAM_LATENCY] = true
+    if enabled_headers.latency_tokens then
+      enabled_headers[headers.PROXY_LATENCY] = true
+      enabled_headers[headers.UPSTREAM_LATENCY] = true
     end
 
-    conf.headers = headers_enabled
+    conf.enabled_headers = setmetatable(enabled_headers, {
+      __tostring = function() return "" end,
+    })
   end
 
   -- load absolute paths

kong/error_handlers.lua
@@ -58,7 +58,7 @@ return function(ngx)
   local status = ngx.status
   message = BODIES["s" .. status] and BODIES["s" .. status] or format(BODIES.default, status)
 
-  if singletons.configuration.headers[constants.HEADERS.SERVER] then
+  if singletons.configuration.enabled_headers[constants.HEADERS.SERVER] then
     ngx.header[constants.HEADERS.SERVER] = SERVER_HEADER
   end
 

kong/runloop/handler.lua
@@ -713,20 +713,20 @@ return {
       local header = ngx.header
 
       if ctx.KONG_PROXIED then
-        if singletons.configuration.headers[constants.HEADERS.UPSTREAM_LATENCY] then
+        if singletons.configuration.enabled_headers[constants.HEADERS.UPSTREAM_LATENCY] then
           header[constants.HEADERS.UPSTREAM_LATENCY] = ctx.KONG_WAITING_TIME
         end
 
-        if singletons.configuration.headers[constants.HEADERS.PROXY_LATENCY] then
+        if singletons.configuration.enabled_headers[constants.HEADERS.PROXY_LATENCY] then
           header[constants.HEADERS.PROXY_LATENCY] = ctx.KONG_PROXY_LATENCY
         end
 
-        if singletons.configuration.headers[constants.HEADERS.VIA] then
+        if singletons.configuration.enabled_headers[constants.HEADERS.VIA] then
           header[constants.HEADERS.VIA] = server_header
         end
 
       else
-        if singletons.configuration.headers[constants.HEADERS.SERVER] then
+        if singletons.configuration.enabled_headers[constants.HEADERS.SERVER] then
           header[constants.HEADERS.SERVER] = server_header
 
         else

spec/02-integration/05-proxy/13-server_tokens_spec.lua
@@ -1,5 +1,6 @@
 local helpers = require "spec.helpers"
 local constants = require "kong.constants"
+local cjson = require "cjson"
 
 
 local default_server_header = _KONG._NAME .. "/" .. _KONG._VERSION
@@ -492,6 +493,25 @@ describe("headers [#" .. strategy .. "]", function()
         assert.is_nil(res.headers[constants.HEADERS.PROXY_LATENCY])
       end)
 
+      it("can be specified via configuration file", function()
+        -- A regression test added with https://github.com/Kong/kong/pull/3419
+        -- to ensure that the `headers` configuration value can be specified
+        -- via the configuration file (vs. environment variables as the rest
+        -- of this test suite uses).
+        -- This regression occured because of the dumping of config values into
+        -- .kong_env (and the lack of serialization for the `headers` table).
+        assert(helpers.kong_exec("restart -c spec/fixtures/headers.conf"))
+
+        local admin_client = helpers.admin_client()
+        local res = assert(admin_client:send {
+          method = "GET",
+          path   = "/",
+        })
+        local body = assert.res_status(200, res)
+        local json = cjson.decode(body)
+        assert.equal("server_tokens", json.configuration.headers[1])
+        assert.equal("X-Kong-Proxy-Latency", json.configuration.headers[2])
+      end)
     end)
 
     describe("(with headers='server_tokens, off, X-Kong-Proxy-Latency')", function()

spec/fixtures/headers.conf
@@ -1,0 +1,28 @@
+# 1st digit is 9 for our test instances
+admin_listen = 127.0.0.1:9001
+proxy_listen = 0.0.0.0:9000, 0.0.0.0:9443 ssl
+
+ssl_cert = spec/fixtures/kong_spec.crt
+ssl_cert_key = spec/fixtures/kong_spec.key
+
+admin_ssl_cert = spec/fixtures/kong_spec.crt
+admin_ssl_cert_key = spec/fixtures/kong_spec.key
+
+dns_resolver = 8.8.8.8
+database = postgres
+pg_host = 127.0.0.1
+pg_port = 5432
+pg_database = kong_tests
+cassandra_keyspace = kong_tests
+cassandra_timeout = 10000
+anonymous_reports = off
+
+dns_hostsfile = spec/fixtures/hosts
+
+nginx_worker_processes = 1
+nginx_optimizations = off
+
+prefix = servroot
+log_level = debug
+
+headers = server_tokens, X-Kong-Proxy-Latency