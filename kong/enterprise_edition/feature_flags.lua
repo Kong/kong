cc5e574e098f61e55460699cbe99fec2c3f6280b
hbagdi <hbagdi@users.noreply.github.com>
2018-05-03 16:00:09 -0700

    feat(rate-limiting) add feature flag based redis restriction (#291)
    * feat(*rate-limiting) add feature flag based redis restriction
    
    This is part of a series of patches being merged in from  kong-cloud
    into kong-ee.
    This commit adds two feature flags to restrict the counter
    storage policy to redis only and also supply redis parameters via
    feature values.
    
    * [squash] cloud_client->redis_namespace; style
    
    * [squash] remove required attribute on strategy since we populate it

--- kong/enterprise_edition/feature_flags.lua
+++ kong/enterprise_edition/feature_flags.lua
@@ -7,10 +7,19 @@ local pl_file = require "pl.file"
 local FLAGS = {
   HMAC_AUTH_DISABLE_VALIDATE_REQUEST_BODY = "hmac_auth_disable_validate_request_body",
   KEY_AUTH_DISABLE_KEY_IN_BODY = "key_auth_disable_key_in_body",
+  RATE_LIMITING_RESTRICT_REDIS_ONLY = "rate_limiting_restrict_redis_only",
+  RESPONSE_RATELIMITING_RESTRICT_REDIS_ONLY = "response_ratelimiting_restrict_redis_only",
+  RATE_LIMITING_ADVANCED_RESTRICT_REDIS_ONLY = "rate_limiting_advanced_restrict_redis_only",
 }
 
 
-local VALUES = {}
+local VALUES = {
+  REDIS_HOST = "redis_host",
+  REDIS_PORT = "redis_port",
+  REDIS_NAMESPACE = "redis_namespace",
+}
+
+
 local loaded_conf = {}
 
 
