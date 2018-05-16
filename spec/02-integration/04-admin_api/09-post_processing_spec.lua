0e3cde3a46a2012507f95db8ab4217993b249be8
Raimon Grau <raimonster@gmail.com>
2018-05-16 13:53:44 +0100

    tests(workspaces) fix post_processing tests

--- spec/02-integration/04-admin_api/09-post_processing_spec.lua
+++ spec/02-integration/04-admin_api/09-post_processing_spec.lua
@@ -33,10 +33,14 @@ describe("Admin API post-processing", function()
   before_each(function()
     helpers.dao:truncate_tables()
     helpers.register_consumer_relations(helpers.dao)
-    assert(helpers.dao.consumers:insert({
-      username = "michael",
-      custom_id = "landon",
-    }))
+    helpers.with_default_ws(
+      helpers.dao.workspaces.find_all({name = "default"}),
+      function()
+        assert(helpers.dao.consumers:insert({
+          username = "michael",
+          custom_id = "landon",
+        }))
+    end)
   end)
 
   it_content_types("post-processes paginated sets", function(content_type)
