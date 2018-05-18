b8dd09b9c4c9a85b79a4b9c9e1fefbf0063c1651
Travis Raines <traines@konghq.com>
2018-05-18 15:46:06 -0700

    fix(aws-lambda) strip Connection header if client is using HTTP/2
    We currently access Lambda over HTTP/1.1, and it returns a Connection
    header as such. The Connection header is not used in HTTP/2 (it is
    superseded by other protocol features) and including it violates RFC
    7540 8.1.2.2. Per the same "an intermediary transforming an HTTP/1.x
    message to HTTP/2 will need to remove any header fields nominated by the
    Connection header field, along with the Connection header field itself."

--- kong/plugins/aws-lambda/handler.lua
+++ kong/plugins/aws-lambda/handler.lua
@@ -129,13 +129,12 @@ function AWSLambdaHandler:access(conf)
   AWSLambdaHandler.super.access(self)
 
   local upstream_body = new_tab(0, 6)
+  local var = ngx.var
 
   if conf.forward_request_body or conf.forward_request_headers
     or conf.forward_request_method or conf.forward_request_uri
   then
     -- new behavior to forward request method, body, uri and their args
-    local var = ngx.var
-
     if conf.forward_request_method then
       upstream_body.request_method = var.request_method
     end
@@ -229,6 +228,10 @@ function AWSLambdaHandler:access(conf)
 
   local content = res:read_body()
   local headers = res.headers
+
+  if var.http2 then
+    headers["Connection"] = ""
+  end
 
   local ok, err = client:set_keepalive(conf.keepalive)
   if not ok then
