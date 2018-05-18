b46a0577f4eb9347ab5472fc6e362681f8e30f65
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2018-05-18 18:52:31 +0300

    chore(ci) apply Kong/openresty-patches on setup

--- .ci/setup_env.sh
+++ .ci/setup_env.sh
@@ -6,9 +6,10 @@ set -e
 #---------
 OPENSSL_DOWNLOAD=$DOWNLOAD_CACHE/openssl-$OPENSSL
 OPENRESTY_DOWNLOAD=$DOWNLOAD_CACHE/openresty-$OPENRESTY
+OPENRESTY_PATCHES_DOWNLOAD=$DOWNLOAD_CACHE/openresty-patches-master
 LUAROCKS_DOWNLOAD=$DOWNLOAD_CACHE/luarocks-$LUAROCKS
 
-mkdir -p $OPENSSL_DOWNLOAD $OPENRESTY_DOWNLOAD $LUAROCKS_DOWNLOAD
+mkdir -p $OPENSSL_DOWNLOAD $OPENRESTY_DOWNLOAD $OPENRESTY_PATCHES_DOWNLOAD $LUAROCKS_DOWNLOAD
 
 if [ ! "$(ls -A $OPENSSL_DOWNLOAD)" ]; then
   pushd $DOWNLOAD_CACHE
@@ -19,6 +20,12 @@ fi
 if [ ! "$(ls -A $OPENRESTY_DOWNLOAD)" ]; then
   pushd $DOWNLOAD_CACHE
     curl -s -S -L https://openresty.org/download/openresty-$OPENRESTY.tar.gz | tar xz
+  popd
+fi
+
+if [ ! "$(ls -A $OPENRESTY_PATCHES_DOWNLOAD)" ]; then
+  pushd $DOWNLOAD_CACHE
+    curl -s -S -L https://github.com/Kong/openresty-patches/archive/master.tar.gz | tar xz
   popd
 fi
 
@@ -56,6 +63,13 @@ if [ ! "$(ls -A $OPENRESTY_INSTALL)" ]; then
   )
 
   pushd $OPENRESTY_DOWNLOAD
+    if [ -d $OPENRESTY_PATCHES_DOWNLOAD/patches/$OPENRESTY ]; then
+      pushd bundle
+        for patch_file in $(ls -1 $OPENRESTY_PATCHES_DOWNLOAD/patches/$OPENRESTY/*.patch); do
+          patch -p1 < $patch_file 2> build.log || (cat build.log && exit 1)
+        done
+      popd
+    fi
     ./configure ${OPENRESTY_OPTS[*]} &> build.log || (cat build.log && exit 1)
     make &> build.log || (cat build.log && exit 1)
     make install &> build.log || (cat build.log && exit 1)
