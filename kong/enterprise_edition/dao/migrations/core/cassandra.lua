70558a9da163e562ad2b250bd5de9072e5c899d1
Guilherme Salazar <gsz@acm.org>
2018-06-01 14:11:42 -0300

    chore(*) remove traces of old rbac
    Remove traces of old RBAC introduced in 0.32 for
    compatibility with services and routes.

--- kong/enterprise_edition/dao/migrations/core/cassandra.lua
+++ kong/enterprise_edition/dao/migrations/core/cassandra.lua
@@ -400,36 +400,4 @@ return {
       return portal.update_consumers(dao, CONSUMERS.TYPE.PROXY)
     end,
   },
-  {
-    name = "2018-05-15-100000_rbac_routes_services",
-    up = function(_, _, dao)
-      local rbac = require "kong.rbac"
-      local bxor = require("bit").bxor
-
-      for _, resource in ipairs {
-        "routes",
-        "services",
-      } do
-        local resource, err = rbac.register_resource(resource, dao)
-        if not resource then
-          return err
-        end
-
-        for _, p in ipairs({ "read-only", "full-access" }) do
-          local perm, err = dao.rbac_perms:find_all({
-            name = p,
-          })
-          if err then
-            return err
-          end
-          perm = perm[1]
-          perm.resources = bxor(perm.resources, 2 ^ (resource.bit_pos - 1))
-          local ok, err = dao.rbac_perms:update(perm, { id = perm.id })
-          if not ok then
-            return err
-          end
-        end
-      end
-    end
-  },
 }
