52e493c43c9e12a46a4f6e25830bf485839dc772
Raimon Grau <raimonster@gmail.com>
2018-06-10 22:57:36 +0100

    tests(loggly plugin) scope inserts in default ws

--- spec/03-plugins/07-loggly/01-log_spec.lua
+++ spec/03-plugins/07-loggly/01-log_spec.lua
@@ -10,8 +10,9 @@ for _, strategy in helpers.each_strategy() do
     local proxy_client
 
     setup(function()
-      local bp = helpers.get_db_utils(strategy)
+      local bp, _, dao = helpers.get_db_utils(strategy)
 
+      helpers.with_current_ws(nil, function()
       local route1 = bp.routes:insert {
         hosts = { "logging.com" },
       }
@@ -75,6 +76,7 @@ for _, strategy in helpers.each_strategy() do
           key  = "123456789"
         }
       }
+      end, dao)
 
       assert(helpers.start_kong({
         database   = strategy,
