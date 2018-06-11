8c8f7a3b195ab5524bd5e5df6d3636b9259dcc1a
Raimon Grau <raimonster@gmail.com>
2018-06-11 11:51:14 +0100

    tests(request size plugin) scope inserts in default ws

--- spec/03-plugins/13-request-size-limiting/01-access_spec.lua
+++ spec/03-plugins/13-request-size-limiting/01-access_spec.lua
@@ -11,8 +11,9 @@ for _, strategy in helpers.each_strategy() do
     local proxy_client
 
     setup(function()
-      local bp = helpers.get_db_utils(strategy)
+      local bp, _, dao = helpers.get_db_utils(strategy)
 
+      helpers.with_current_ws(nil, function()
       local route = bp.routes:insert {
         hosts = { "limit.com" },
       }
@@ -24,6 +25,7 @@ for _, strategy in helpers.each_strategy() do
           allowed_payload_size = TEST_SIZE
         }
       }
+      end, dao)
 
       assert(helpers.start_kong({
         database   = strategy,
