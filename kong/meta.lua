0cad24aa462a65418c5bcc3ad1c648137241ce31
Thibault Charbonnier <thibaultcha@me.com>
2018-06-19 11:16:33 -0700

    chore(*) bump version to 0.14.0rc1

--- kong/meta.lua
+++ kong/meta.lua
@@ -1,8 +1,8 @@
 local version = setmetatable({
   major = 0,
-  minor = 13,
-  patch = 1,
-  --suffix = ""
+  minor = 14,
+  patch = 0,
+  suffix = "rc1"
 }, {
   __tostring = function(t)
     return string.format("%d.%d.%d%s", t.major, t.minor, t.patch,
