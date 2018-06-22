d30e5c3733baf42b06944aacf7c72b5d02c9d126
Raimon Grau (rgrau) <raimonster@gmail.com>
2018-06-22 19:46:50 +0100

    fix(rbac) migrations typo fix (#446)
    drop the rbac_user_roles table

--- kong/enterprise_edition/dao/migrations/core/postgres.lua
+++ kong/enterprise_edition/dao/migrations/core/postgres.lua
@@ -186,7 +186,7 @@ return {
     name = "2018-04-18-110000_old_rbac_cleanup",
     up = [[
       DROP TABLE IF EXISTS rbac_users;
-      DROP TABLE IF EXISTS rbac_users_roles;
+      DROP TABLE IF EXISTS rbac_user_roles;
       DROP TABLE IF EXISTS rbac_roles;
       DROP TABLE IF EXISTS rbac_perms;
       DROP TABLE IF EXISTS rbac_role_perms;
