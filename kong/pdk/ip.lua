1dcab8ddb738d3cd3c29e7eca16bd10b718cc3ed
Enrique García Cota <kikito@gmail.com>
2018-07-02 18:42:07 +0200

    doc(pdk) add ldoc for pdk ip module

--- kong/pdk/ip.lua
+++ kong/pdk/ip.lua
@@ -1,5 +1,32 @@
+---
+-- Trusted IPs module
+--
+-- This module can be used to determine whether or not a given IP address is
+-- in the range of trusted IP addresses defined by the `trusted_ips` configuration
+-- property.
+--
+-- Trusted IP addresses are those that are known to send correct replacement
+-- addresses for clients (as per the chosen header field, e.g. X-Forwarded-*).
+--
+-- See [docs.konghq.com/latest/configuration/#trusted_ips](https://docs.konghq.com/latest/configuration/#trusted_ips)
+--
+-- @module kong.ip
 local ip = require "resty.mediador.ip"
 
+---
+-- Depending on the `trusted_ips` configuration property,
+-- this function will return whether a given ip is trusted or not
+--
+-- Both ipv4 and ipv6 are supported.
+--
+-- @function kong.ip.is_trusted
+-- @phases
+-- @tparam string address A string representing an IP address
+-- @treturn boolean `true` if the IP is trusted, `false` otherwise
+-- @usage
+-- if kong.ip.is_trusted("1.1.1.1") then
+--   kong.log("The IP is trusted")
+-- end
 
 local function new(self)
   local _IP = {}
