351ea1c3f3cdf955caabd70afa62abd8c23601f1
kinman <kinman@users.noreply.github.com>
2018-07-12 13:37:33 -0700

    tests(ee) move ee-specific tests out of ce files (INTF-135) (#521)

---
+++ spec/01-unit/02-ee/07-conf_loader_spec.lua
@@ -0,0 +1,97 @@
+local conf_loader = require "kong.conf_loader"
+local helpers = require "spec.helpers"
+
+describe("Configuration loader - enterprise", function()
+  it("loads the defaults", function()
+    local conf = assert(conf_loader())
+    assert.same({"0.0.0.0:8002", "0.0.0.0:8445 ssl"}, conf.admin_gui_listen)
+    assert.same({"0.0.0.0:8003", "0.0.0.0:8446 ssl"}, conf.portal_gui_listen)
+    assert.same({"0.0.0.0:8004", "0.0.0.0:8447 ssl"}, conf.portal_api_listen)
+    assert.equal("logs/admin_gui_access.log", conf.admin_gui_access_log)
+    assert.equal("logs/admin_gui_error.log", conf.admin_gui_error_log)
+    assert.is_nil(conf.admin_gui_ssl_cert)
+    assert.is_nil(conf.admin_gui_ssl_cert_key)
+    assert.is_nil(conf.portal_gui_ssl_cert)
+    assert.is_nil(conf.portal_gui_ssl_cert_key)
+    assert.is_nil(conf.portal_api_ssl_cert)
+    assert.is_nil(conf.portal_api_ssl_cert_key)
+    assert.is_nil(getmetatable(conf))
+  end)
+
+  it("loads a given file, with higher precedence", function()
+    local conf = assert(conf_loader(helpers.test_conf_path))
+    -- defaults
+    assert.equal("on", conf.nginx_daemon)
+    -- overrides
+    assert.same({"0.0.0.0:9002"}, conf.admin_gui_listen)
+    assert.same({"0.0.0.0:9003", "0.0.0.0:9446 ssl"}, conf.portal_gui_listen)
+    assert.equal("http://127.0.0.1:9003", conf.portal_gui_url)
+    assert.same({"0.0.0.0:9004", "0.0.0.0:9447 ssl"}, conf.portal_api_listen)
+    assert.equal("http://127.0.0.1:9004", conf.portal_api_url)
+    assert.is_nil(getmetatable(conf))
+  end)
+
+  it("extracts flags, ports and listen ips from proxy_listen/admin_listen", function()
+    local conf = assert(conf_loader())
+    -- portal is disabled by default
+    assert.equal(nil, conf.portal_gui_listeners)
+    assert.equal(nil, conf.portal_api_listeners)
+
+    assert.equal("0.0.0.0", conf.admin_gui_listeners[1].ip)
+    assert.equal(8002, conf.admin_gui_listeners[1].port)
+    assert.equal(false, conf.admin_gui_listeners[1].ssl)
+    assert.equal(false, conf.admin_gui_listeners[1].http2)
+    assert.equal("0.0.0.0:8000", conf.proxy_listeners[1].listener)
+
+    assert.equal("0.0.0.0", conf.admin_gui_listeners[2].ip)
+    assert.equal(8445, conf.admin_gui_listeners[2].port)
+    assert.equal(true, conf.admin_gui_listeners[2].ssl)
+    assert.equal(false, conf.admin_gui_listeners[2].http2)
+    assert.equal("0.0.0.0:8445 ssl", conf.admin_gui_listeners[2].listener)
+  end)
+
+  it("attaches prefix paths", function()
+    local conf = assert(conf_loader())
+    assert.equal("/usr/local/kong/ssl/admin-gui-kong-default.crt", conf.admin_gui_ssl_cert_default)
+    assert.equal("/usr/local/kong/ssl/admin-gui-kong-default.key", conf.admin_gui_ssl_cert_key_default)
+    assert.equal("/usr/local/kong/ssl/admin-gui-kong-default.csr", conf.admin_gui_ssl_cert_csr_default)
+    assert.equal("/usr/local/kong/ssl/portal-gui-kong-default.crt", conf.portal_gui_ssl_cert_default)
+    assert.equal("/usr/local/kong/ssl/portal-gui-kong-default.key", conf.portal_gui_ssl_cert_key_default)
+    assert.equal("/usr/local/kong/ssl/portal-gui-kong-default.csr", conf.portal_gui_ssl_cert_csr_default)
+    assert.equal("/usr/local/kong/ssl/portal-api-kong-default.crt", conf.portal_api_ssl_cert_default)
+    assert.equal("/usr/local/kong/ssl/portal-api-kong-default.key", conf.portal_api_ssl_cert_key_default)
+    assert.equal("/usr/local/kong/ssl/portal-api-kong-default.csr", conf.portal_api_ssl_cert_csr_default)
+  end)
+
+  describe("validations", function()
+    it("enforces enforce_rbac values", function()
+      local conf, _, errors = conf_loader(nil, {
+        enforce_rbac = "foo",
+      })
+      assert.equal(1, #errors)
+      assert.is_nil(conf)
+    end)
+
+    it("enforces listen addresses format", function()
+      local conf, err = conf_loader(nil, {
+        admin_gui_listen = "127.0.0.1"
+      })
+      assert.is_nil(conf)
+      assert.equal("admin_gui_listen must be of form: [off] | <ip>:<port> [ssl] [http2] [proxy_protocol], [... next entry ...]", err)
+
+      conf, err = conf_loader(nil, {
+        portal = "on",
+        portal_gui_listen = "127.0.0.1"
+      })
+      assert.is_nil(conf)
+      assert.equal("portal_gui_listen must be of form: [off] | <ip>:<port> [ssl] [http2] [proxy_protocol], [... next entry ...]", err)
+
+      conf, err = conf_loader(nil, {
+        portal = "on",
+        portal_api_listen = "127.0.0.1"
+      })
+      assert.is_nil(conf)
+      assert.equal("portal_api_listen must be of form: [off] | <ip>:<port> [ssl] [http2] [proxy_protocol], [... next entry ...]", err)
+    end)
+  end)
+end)
---
+++ spec/02-integration/04-admin_api/52-ee_kong_routes_spec.lua
@@ -0,0 +1,205 @@
+local helpers = require "spec.helpers"
+local cjson = require "cjson"
+local dao_helpers = require "spec.02-integration.03-dao.helpers"
+local proxy_prefix = require("kong.enterprise_edition.proxies").proxy_prefix
+local enums = require "kong.enterprise_edition.dao.enums"
+local utils = require "kong.tools.utils"
+local ee_helpers = require "spec.ee_helpers"
+
+
+describe("Admin API - ee-specific Kong routes", function()
+  dao_helpers.for_each_dao(function(kong_conf)
+    describe("/userinfo with db " .. kong_conf.database, function()
+
+      local strategy = kong_conf.database
+      local client
+      local dao
+      local bp
+
+      after_each(function()
+        helpers.stop_kong()
+      end)
+
+      teardown(function()
+        -- this is just truncating tables, a side effect
+        helpers.get_db_utils(strategy)
+      end)
+
+      it("return 404 on user info when admin_auth is off", function()
+        helpers.get_db_utils(strategy)
+
+        assert(helpers.start_kong({
+          database = strategy,
+        }))
+
+        client = assert(helpers.proxy_client())
+
+        local res = assert(client:send {
+          method = "GET",
+          path = "/" .. proxy_prefix .. "/admin/userinfo",
+        })
+        assert.res_status(404, res)
+      end)
+
+      it("returns 403 with admin_auth = on, invalid credentials", function()
+        local _
+        bp, _, dao = helpers.get_db_utils(strategy)
+
+        assert(helpers.start_kong({
+          database = strategy,
+          admin_gui_auth = 'basic-auth'
+        }))
+
+        client = assert(helpers.proxy_client())
+
+        bp.consumers:insert {
+          username = "hawk",
+          type = enums.CONSUMERS.TYPE.ADMIN,
+          status = enums.CONSUMERS.STATUS.APPROVED,
+        }
+
+        local res = assert(client:send {
+          method = "GET",
+          path = "/" .. proxy_prefix .. "/admin/userinfo",
+          ["Authorization"] = "Basic " .. ngx.encode_base64("iam:invalid"),
+        })
+
+        assert.res_status(401, res)
+      end)
+
+      it("returns user info of admin consumer with no rbac", function()
+        local _
+        bp, _, dao = helpers.get_db_utils(strategy)
+
+        assert(helpers.start_kong({
+          database = strategy,
+          admin_gui_auth = 'basic-auth',
+        }))
+
+        local consumer = bp.consumers:insert {
+          username = "hawk",
+          type = enums.CONSUMERS.TYPE.ADMIN,
+          status = enums.CONSUMERS.STATUS.APPROVED,
+        }
+
+        assert(dao.basicauth_credentials:insert {
+          username    = "hawk",
+          password    = "kong",
+          consumer_id = consumer.id,
+        })
+
+        client = assert(helpers.proxy_client())
+
+        local res = assert(client:send {
+          method = "GET",
+          path = "/" .. proxy_prefix .. "/admin/userinfo",
+          headers = {
+            ["Authorization"] = "Basic " .. ngx.encode_base64("hawk:kong"),
+          }
+        })
+
+        res = assert.res_status(200, res)
+        local json = cjson.decode(res)
+        assert.equals(json.consumer.username, 'hawk')
+        assert.equals(json.consumer.status, enums.CONSUMERS.STATUS.APPROVED)
+        assert.equals(json.consumer.type, enums.CONSUMERS.TYPE.ADMIN)
+        assert.is_true(utils.is_valid_uuid(json.consumer.id))
+
+        assert.is_nil(json.rbac_user)
+      end)
+
+      it("returns user info of admin consumer with rbac", function()
+        local _
+        bp, _, dao = helpers.get_db_utils(strategy)
+
+        assert(helpers.start_kong({
+          database = strategy,
+          admin_gui_auth = "basic-auth",
+          enforce_rbac = "both",
+        }))
+
+        local super_admin = ee_helpers.register_rbac_resources(dao)
+
+        client = assert(helpers.proxy_client())
+
+        local consumer = bp.consumers:insert {
+          username = "hawk",
+          type = enums.CONSUMERS.TYPE.ADMIN,
+          status = enums.CONSUMERS.STATUS.APPROVED,
+        }
+
+        assert(dao.basicauth_credentials:insert {
+          username    = "hawk",
+          password    = "kong",
+          consumer_id = consumer.id,
+        })
+
+        assert(dao.consumers_rbac_users_map:insert {
+          consumer_id = consumer.id,
+          user_id = super_admin.id
+        })
+
+        local res = assert(client:send {
+          method = "GET",
+          path = "/" .. proxy_prefix .. "/admin/userinfo",
+          headers = {
+            ["Authorization"] = "Basic " .. ngx.encode_base64("hawk:kong"),
+            ["Kong-Admin-Token"] = super_admin.user_token,
+          }
+        })
+
+        res = assert.res_status(200, res)
+        local json = cjson.decode(res)
+
+        assert.same(consumer, json.consumer)
+        assert.same(super_admin, json.rbac_user)
+      end)
+
+      it("is whitelisted", function()
+        local _
+        bp, _, dao = helpers.get_db_utils(strategy)
+
+        assert(helpers.start_kong({
+          database = strategy,
+          admin_gui_auth = "basic-auth",
+          enforce_rbac = "both",
+        }))
+
+        local super_admin = ee_helpers.register_rbac_resources(dao)
+
+        client = assert(helpers.proxy_client())
+
+        local consumer = bp.consumers:insert {
+          username = "hawk",
+          type = enums.CONSUMERS.TYPE.ADMIN,
+          status = enums.CONSUMERS.STATUS.APPROVED,
+        }
+
+        assert(dao.basicauth_credentials:insert {
+          username    = "hawk",
+          password    = "kong",
+          consumer_id = consumer.id,
+        })
+
+        assert(dao.consumers_rbac_users_map:insert {
+          consumer_id = consumer.id,
+          user_id = super_admin.id
+        })
+
+        local res = assert(client:send {
+          method = "GET",
+          path = "/" .. proxy_prefix .. "/admin/userinfo",
+          headers = {
+            ["Authorization"] = "Basic " .. ngx.encode_base64("hawk:kong"),
+          }
+        })
+
+        res = assert.res_status(200, res)
+        local json = cjson.decode(res)
+
+        assert.same(consumer, json.consumer)
+        assert.same(super_admin, json.rbac_user)
+      end)
+    end)
+  end)
+end)
