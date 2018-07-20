Kong/kong

commit 5a312ce74f7c911aad2e84d36eb2039078a0ffc0

feat(rbac) create default RBAC user when necessary (#451)
* feat(rbac) create default RBAC user when necessary

This is a rework of #134 on the new RBAC model.

A rbac_users with `super-admin` RBAC role is created if necessary.
The migration takes care of a case where a 0.32 migration(now deleted)
could have already created a `kong_admin` user.

Signed-off-by: Harry Bagdi <harrybagdi@gmail.com>

* remove nop

Signed-off-by: Harry Bagdi <harrybagdi@gmail.com>

kong/rbac/migrations/01_defaults.lua
@@ -99,6 +99,39 @@ return {
         end
       end
 
+      -- Setup and Admin user by default if ENV var is set
+      local super_user_role = role
+
+      if os.getenv("KONG_RBAC_INITIAL_ADMIN_PASS") ~= nil then
+      -- an old migration in 0.32 (the migration is deleted)
+      -- could have already created a kong_admin user, do not overwrite
+        local user, err = dao.rbac_users:find_all({ name = "kong_admin" })
+        if err then
+          return err
+        end
+
+        if user[1] then
+          return
+        end
+
+        local user, err = dao.rbac_users:insert({
+          id = utils.uuid(),
+          name = "kong_admin",
+          user_token = os.getenv("KONG_RBAC_INITIAL_ADMIN_PASS"),
+          enabled = true,
+          comment = "Initial RBAC Secure User"
+        })
+        if err then
+          return err
+        end
+        local _, err = dao.rbac_user_roles:insert({
+          user_id = user.id,
+          role_id = super_user_role.id
+        })
+        if err then
+          return err
+        end
+      end
 
 
       -- add endpoint permissions to the super admin role

commit ec98ae84626eac6c3e2a242b541ac2d732d95601

fix(migration) create superadmin permissions if kong_admin exists
This fixes a bug introduced in 5a312ce74 with a cloud specific
RBAC migration where if a `kong_admin` already exists (from 0.32) in the
database, the super-admin permissions will not
be created.

For CT-306
From #544

kong/rbac/migrations/01_defaults.lua
@@ -112,9 +112,28 @@ return {
         end
       end
 
-      -- Setup and Admin user by default if ENV var is set
-      local super_user_role = role
+      -- add endpoint permissions to the super admin role
+      ok, err = dao.rbac_role_endpoints:insert({
+        role_id = role.id,
+        workspace = "*",
+        endpoint = "*",
+        actions = action_bits_all, -- all actions
+      })
+      if not ok then
+        return err
+      end
 
+      ok, err = dao.rbac_role_entities:insert({
+        role_id = role.id,
+        entity_id = "*",
+        entity_type = "wildcard",
+        actions = action_bits_all,
+      })
+      if not ok then
+        return err
+      end
+
+      -- Setup and Admin user by default if ENV var is set
       if os.getenv("KONG_RBAC_INITIAL_ADMIN_PASS") ~= nil then
       -- an old migration in 0.32 (the migration is deleted)
       -- could have already created a kong_admin user, do not overwrite
@@ -139,33 +158,11 @@ return {
         end
         local _, err = dao.rbac_user_roles:insert({
           user_id = user.id,
-          role_id = super_user_role.id
+          role_id = role.id
         })
         if err then
           return err
         end
-      end
-
-
-      -- add endpoint permissions to the super admin role
-      ok, err = dao.rbac_role_endpoints:insert({
-        role_id = role.id,
-        workspace = "*",
-        endpoint = "*",
-        actions = action_bits_all, -- all actions
-      })
-      if not ok then
-        return err
-      end
-
-      ok, err = dao.rbac_role_entities:insert({
-        role_id = role.id,
-        entity_id = "*",
-        entity_type = "wildcard",
-        actions = action_bits_all,
-      })
-      if not ok then
-        return err
       end
   end
 }