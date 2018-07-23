a0da7bbad6cf964219fb543e94b5181f29153eb9
Colin Hutchinson <chutchic@gmail.com>
2018-07-23 21:14:56 +0000

    misc(travis) pause travis until we're ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ else
   /wait-for-it.sh $KONG_CASSANDRA_CONTACT_POINTS:7000 
 fi
 
+printenv | grep KONG
 make dev
 /kong/bin/kong prepare
-/kong/bin/kong migrations up && tail -f /dev/null
+/kong/bin/kong migrations up && echo 'done' && tail -f /dev/null
