7864171517c3ae3443fec9a82358d38fbaf74032
Enrique García Cota <kikito@gmail.com>
2018-07-28 00:42:33 +0200

    feat(schema) remove requirement of endpoint key to be unique
    The targets.target field is an endpoint key that is not unique

--- kong/db/schema/metaschema.lua
+++ kong/db/schema/metaschema.lua
@@ -140,7 +140,6 @@ local meta_errors = {
   FIELDS_ARRAY = "each entry in fields must be a sub-table",
   FIELDS_KEY = "each key in fields must be a string",
   ENDPOINT_KEY = "value must be a field name",
-  ENDPOINT_KEY_UNIQUE = "endpoint key must be a unique field",
   TTL_RESERVED = "ttl is a reserved field name when ttl is enabled",
   TTL_CREATED_AT = "ttl can only be enabled on entities that have a 'created_at' timestamp field",
   SUBSCHEMA_KEY = "value must be a field name",
@@ -159,6 +158,7 @@ local required_attributes = {
 local attribute_types = {
   between = {
     ["integer"] = true,
+    ["number"] = true,
   },
   len_eq = {
     ["array"]  = true,
@@ -329,11 +329,7 @@ local MetaSchema = Schema.new({
       local found = false
       for _, item in ipairs(schema.fields) do
         local k = next(item)
-        local field = item[k]
         if schema.endpoint_key == k then
-          if not field.unique then
-            errors["endpoint_key"] = meta_errors.ENDPOINT_KEY_UNIQUE
-          end
           found = true
           break
         end
