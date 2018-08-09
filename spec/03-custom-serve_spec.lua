db8ad05beef8c43aae270693a1424b76b99a2b23
James Callahan <james@konghq.com>
2018-08-09 11:40:37 +1000

    chore(spec) fix mixed tabs/spaces

--- spec/03-custom-serve_spec.lua
+++ spec/03-custom-serve_spec.lua
@@ -5,31 +5,31 @@ describe("Plugin: prometheus (custom server)",function()
 
   describe("with custom nginx server block", function()
     setup(function()
-    	local bp = helpers.get_db_utils()
+      local bp = helpers.get_db_utils()
 
-    	local service = bp.services:insert {
-    	  name = "mock-service",
-    	  host = helpers.mock_upstream_host,
-    	  port = helpers.mock_upstream_port,
-    	  protocol = helpers.mock_upstream_protocol,
-    	}
+      local service = bp.services:insert {
+        name = "mock-service",
+        host = helpers.mock_upstream_host,
+        port = helpers.mock_upstream_port,
+        protocol = helpers.mock_upstream_protocol,
+      }
 
-    	bp.routes:insert {
-    	  protocols = { "http" },
-    	  paths = { "/" },
-    	  service = service,
-    	}
+      bp.routes:insert {
+        protocols = { "http" },
+        paths = { "/" },
+        service = service,
+      }
 
-    	bp.plugins:insert {
-    	  name = "prometheus"
-    	}
+      bp.plugins:insert {
+        name = "prometheus"
+      }
 
       assert(helpers.start_kong({
         nginx_conf = "spec/fixtures/prometheus/valid_nginx.template",
         plugins = "bundled, prometheus",
       }))
 
-    	proxy_client = helpers.proxy_client()
+      proxy_client = helpers.proxy_client()
     end)
     teardown(function()
       if proxy_client then
@@ -40,22 +40,22 @@ describe("Plugin: prometheus (custom server)",function()
     end)
 
     it("metrics can be read from a different port", function()
-    	local res = assert(proxy_client:send {
-    	  method  = "GET",
-    	  path    = "/status/200",
-    	  headers = {
-    	    host = helpers.mock_upstream_host,
-    	  }
-    	})
-    	assert.res_status(200, res)
+      local res = assert(proxy_client:send {
+        method  = "GET",
+        path    = "/status/200",
+        headers = {
+          host = helpers.mock_upstream_host,
+        }
+      })
+      assert.res_status(200, res)
 
-			local client = helpers.http_client("127.0.0.1", 9542)
-    	local res = assert(client:send {
-    	  method  = "GET",
-    	  path    = "/metrics",
-    	})
-    	local body = assert.res_status(200, res)
-    	assert.matches('kong_http_status{code="200",service="mock-service"} 1', body, nil, true)
+      local client = helpers.http_client("127.0.0.1", 9542)
+      local res = assert(client:send {
+        method  = "GET",
+        path    = "/metrics",
+      })
+      local body = assert.res_status(200, res)
+      assert.matches('kong_http_status{code="200",service="mock-service"} 1', body, nil, true)
     end)
   end)
 end)
