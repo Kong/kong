4d189b58889b70666156cddd35a81cbe829391b3
Thibault Charbonnier <thibaultcha@me.com>
2018-08-31 13:46:04 -0700

    feat(schema) support for fields with 'function' type
    Also add tests for the Entity schema subclass.

--- kong/db/schema/init.lua
+++ kong/db/schema/init.lua
@@ -107,15 +107,16 @@ local validation_errors = {
 
 
 Schema.valid_types = {
-  array   = true,
-  set     = true,
-  string  = true,
-  number  = true,
-  boolean = true,
-  integer = true,
-  foreign = true,
-  map     = true,
-  record  = true,
+  array        = true,
+  set          = true,
+  string       = true,
+  number       = true,
+  boolean      = true,
+  integer      = true,
+  foreign      = true,
+  map          = true,
+  record       = true,
+  ["function"] = true,
 }
 
 
@@ -688,8 +689,6 @@ function Schema:validate_field(field, value)
     end
 
   elseif field.type == "function" then
-    -- TODO: this type should only be used/visible from the
-    -- metaschema to validate the 'custom_validator'
     if type(value) ~= "function" then
       return nil, validation_errors.FUNCTION
     end
