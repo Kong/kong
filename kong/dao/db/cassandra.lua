4796640ba6f8a294990fa1e7bd4c595ca21c9fec
Raimon Grau (rgrau) <raimonster@gmail.com>
2018-09-06 19:24:42 +0100

    fix(dao) make 'drop_schema()' aware of new-dao tables
    consumers, certificates, and snis are dropped when drop_schema is
    called (for example, when `kong migrations reset` is called from cli)
    
    From #3745

--- kong/dao/db/cassandra.lua
+++ kong/dao/db/cassandra.lua
@@ -30,7 +30,14 @@ _M.dao_insert_values = {
   end
 }
 
-_M.additional_tables = { "cluster_events", "routes", "services" }
+_M.additional_tables = {
+  "cluster_events",
+  "routes",
+  "services",
+  "consumers",
+  "certificates",
+  "snis",
+}
 
 function _M.new(kong_config)
   local self = _M.super.new()
