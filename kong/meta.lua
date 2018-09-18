454476f3841648b691b6566a74f6f25fb97454a2
Thibault Charbonnier <thibaultcha@me.com>
2018-09-17 23:09:55 -0700

    release: 1.0.0rc1

--- kong/meta.lua
+++ kong/meta.lua
@@ -1,8 +1,8 @@
 local version = setmetatable({
-  major = 0,
-  minor = 14,
-  patch = 1,
-  --suffix = ""
+  major = 1,
+  minor = 0,
+  patch = 0,
+  suffix = "rc1"
 }, {
   __tostring = function(t)
     return string.format("%d.%d.%d%s", t.major, t.minor, t.patch,
