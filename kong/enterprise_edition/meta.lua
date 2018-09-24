75d1c1653d3eb8ff91d34838283b09fa91d3faae
Guilherme Salazar <gsz@acm.org>
2018-09-24 14:59:55 -0700

    refactor(meta) split version number out of `package` - FT-135 (#598)
    * refactor(meta) split version number out of package
    
    This commit refactors the Enterprise meta module to:
    
    - Split version number out of the `package` table, adding its own
    metamethod for formatting
    - Make the `Z` component in the version a number instead of a string;
    all version components are numbers and their formatting is taken care
    of in its `__tostring` metamethod
    - The `package` is now conceptually made of the version number and a
    `suffix`, which can accommodate different needs: e.g., it may be
    "Internal Preview", during internal previews period; "Customer Name",
    for custom builds
    
    Splitting the `suffix` from the `Z` component has a number of
    advantages:
    - It avoids confusion - i.e., distinguishing the patch component number
    from, say, a customer name
    - It allows for easier/more reliable build-time patching, which will aid
    in internal previews and custom builds automation
    
    * style(meta) add comment to suffix field
    
    As we rely on the `suffix` variable in Kong Distributions, add a
    cross-reference comment to alert future readers.

--- kong/enterprise_edition/meta.lua
+++ kong/enterprise_edition/meta.lua
@@ -1,8 +1,26 @@
-local package = {
+local strip = (require "pl.stringx").strip
+
+local version = setmetatable({
   x = 0,
   y = 34,
-  z = "-dev",
-}
+  z = 0,
+}, {
+  __tostring = function(t)
+    return string.format("%d.%d%s", t.x, t.y, t.z > 0 and "-" .. t.z or "")
+  end
+})
+
+local package = setmetatable({
+  -- Note: line below is changed at build time in kong-distributions
+  -- build-kong.sh script to add certain suffixes; e.g., internal previews
+  -- automation add a suffix "internal-preview" at build time
+  suffix = "dev",
+}, {
+  __tostring = function(t)
+    local suffix = strip(t.suffix)
+    return string.format("%s%s", version, suffix ~= "" and "-" .. suffix or "")
+  end
+})
 
 local features = {
   _iteration = 1,
@@ -10,11 +28,7 @@ local features = {
 
 return {
   versions = {
-    package  = setmetatable(package, {
-      __tostring = function(t)
-        return string.format("%d.%d%s", t.x, t.y, t.z or "")
-      end,
-    }),
+    package  = package,
     features = setmetatable(features, {
       __tostring = function(t)
         return string.format("v%d", t._iteration)
