d18e1a5b045fa7d4185380f0f1b8dacd6d2e0892
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2018-10-04 20:44:01 +0300

    refactor(acl) remove kong.tools.* dependency from acl plugin

--- kong/plugins/acl/handler.lua
+++ kong/plugins/acl/handler.lua
@@ -1,27 +1,28 @@
 local BasePlugin = require "kong.plugins.base_plugin"
-local responses = require "kong.tools.responses"
 local constants = require "kong.constants"
-local pl_tablex = require "pl.tablex"
+local tablex = require "pl.tablex"
 local groups = require "kong.plugins.acl.groups"
 
-local table_concat = table.concat
-local set_header = ngx.req.set_header
-local ngx_error = ngx.ERR
-local ngx_log = ngx.log
-local EMPTY = pl_tablex.readonly {}
+
+local setmetatable = setmetatable
+local concat = table.concat
+local kong = kong
+
+
+local EMPTY = tablex.readonly {}
 local BLACK = "BLACK"
 local WHITE = "WHITE"
 
+
 local mt_cache = { __mode = "k" }
 local config_cache = setmetatable({}, mt_cache)
-
 
 
 local ACLHandler = BasePlugin:extend()
 
 
 ACLHandler.PRIORITY = 950
-ACLHandler.VERSION = "0.1.1"
+ACLHandler.VERSION = "0.2.0"
 
 
 function ACLHandler:new()
@@ -44,16 +45,17 @@ function ACLHandler:access(conf)
   -- get the consumer/credentials
   local consumer_id = groups.get_current_consumer_id()
   if not consumer_id then
-    ngx_log(ngx_error, "[acl plugin] Cannot identify the consumer, add an ",
-                       "authentication plugin to use the ACL plugin")
-    return responses.send_HTTP_FORBIDDEN("You cannot consume this service")
+    kong.log.err("Cannot identify the consumer, add an authentication ",
+                 "plugin to use the ACL plugin")
+    return kong.response.exit(403, { message = "You cannot consume this service" })
   end
 
   -- get the consumer groups, since we need those as cache-keys to make sure
   -- we invalidate properly if they change
   local consumer_groups, err = groups.get_consumer_groups(consumer_id)
   if not consumer_groups then
-    return responses.send_HTTP_INTERNAL_SERVER_ERROR(err)
+    kong.log.err(err)
+    return kong.response.exit(500, { message = "An unexpected error occurred" })
   end
 
   -- 'to_be_blocked' is either 'true' if it's to be blocked, or the header
@@ -71,8 +73,8 @@ function ACLHandler:access(conf)
     if to_be_blocked == false then
       -- we're allowed, convert 'false' to the header value, if needed
       -- if not needed, set dummy value to save mem for potential long strings
-      to_be_blocked = conf.hide_groups_header and "" 
-                      or table_concat(consumer_groups, ", ")
+      to_be_blocked = conf.hide_groups_header and ""
+                      or concat(consumer_groups, ", ")
     end
 
     -- update cache
@@ -80,12 +82,14 @@ function ACLHandler:access(conf)
   end
 
   if to_be_blocked == true then -- NOTE: we only catch the boolean here!
-    return responses.send_HTTP_FORBIDDEN("You cannot consume this service")
+    return kong.response.exit(403, { message = "You cannot consume this service" })
   end
 
   if not conf.hide_groups_header then
-    set_header(constants.HEADERS.CONSUMER_GROUPS, to_be_blocked)
+    kong.service.request.set_header(constants.HEADERS.CONSUMER_GROUPS,
+                                    to_be_blocked)
   end
 end
+
 
 return ACLHandler
