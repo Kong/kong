03330fd4f9c51c44eb7f4812f8328a7b277a1fd5
Raimon Grau (rgrau) <raimonster@gmail.com>
2018-10-09 00:35:27 +0100

    fix(dao) properly validate rbac on delete (#628)
    When __skip_rbac is true, there's no need to test anything on
    rbac. In that case, ngx.ctx is not needed to be present (fixes
    migrations and tests where no request is involved).

--- kong/dao/dao.lua
+++ kong/dao/dao.lua
@@ -601,8 +601,8 @@ function DAO:delete(tbl, options)
   end
 
   if not options.__skip_rbac and
-    not rbac.validate_entity_operation(primary_keys, self.table) or
-    not rbac.check_cascade(associated_entites, ngx.ctx.rbac)  then
+    (not rbac.validate_entity_operation(primary_keys, self.table) or
+     not rbac.check_cascade(associated_entites, ngx.ctx.rbac))  then
     return ret_error(self.db.name, nil, Errors.forbidden({
       username = ngx.ctx.rbac.user.name,
       action = rbac.readable_action(ngx.ctx.rbac.action)
