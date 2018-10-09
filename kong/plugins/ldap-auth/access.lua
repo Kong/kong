e335610f72a185b8cfe9d7c126afd8809441796a
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2018-10-09 17:50:26 +0300

    refactor(ldap-auth) remove kong.tools.* dependency from ldap-auth plugin

--- kong/plugins/ldap-auth/access.lua
+++ kong/plugins/ldap-auth/access.lua
@@ -1,22 +1,20 @@
-local responses = require "kong.tools.responses"
 local constants = require "kong.constants"
 local singletons = require "kong.singletons"
 local ldap = require "kong.plugins.ldap-auth.ldap"
 
+
+local kong = kong
+local decode_base64 = ngx.decode_base64
+local tostring =  tostring
 local match = string.match
 local lower = string.lower
 local upper = string.upper
 local find = string.find
 local sub = string.sub
 local fmt = string.format
-local ngx_log = ngx.log
-local request = ngx.req
-local ngx_error = ngx.ERR
+local tcp = ngx.socket.tcp
 local md5 = ngx.md5
-local decode_base64 = ngx.decode_base64
-local ngx_socket_tcp = ngx.socket.tcp
-local ngx_set_header = ngx.req.set_header
-local tostring =  tostring
+
 
 local AUTHORIZATION = "authorization"
 local PROXY_AUTHORIZATION = "proxy-authorization"
@@ -26,6 +24,7 @@ local ldap_config_cache = setmetatable({}, { __mode = "k" })
 
 
 local _M = {}
+
 
 local function retrieve_credentials(authorization_header_value, conf)
   local username, password
@@ -38,19 +37,23 @@ local function retrieve_credentials(authorization_header_value, conf)
       username, password = match(decoded_cred, "(.+):(.+)")
     end
   end
+
   return username, password
 end
+
 
 local function ldap_authenticate(given_username, given_password, conf)
   local is_authenticated
   local err, suppressed_err, ok
 
-  local sock = ngx_socket_tcp()
+  local sock = tcp()
+
   sock:settimeout(conf.timeout)
+
   ok, err = sock:connect(conf.ldap_host, conf.ldap_port)
   if not ok then
-    ngx_log(ngx_error, "[ldap-auth] failed to connect to ", conf.ldap_host,
-            ":", tostring(conf.ldap_port),": ", err)
+    kong.log.err("failed to connect to ", conf.ldap_host, ":",
+                   tostring(conf.ldap_port), ": ", err)
     return nil, err
   end
 
@@ -59,6 +62,7 @@ local function ldap_authenticate(given_username, given_password, conf)
     if not success then
       return false, err
     end
+
     local _, err = sock:sslhandshake(true, conf.ldap_host, conf.verify_ldap_host)
     if err ~= nil then
       return false, fmt("failed to do SSL handshake with %s:%s: %s",
@@ -71,37 +75,39 @@ local function ldap_authenticate(given_username, given_password, conf)
 
   ok, suppressed_err = sock:setkeepalive(conf.keepalive)
   if not ok then
-    ngx_log(ngx_error, "[ldap-auth] failed to keepalive to ", conf.ldap_host, ":",
-            tostring(conf.ldap_port), ": ", suppressed_err)
+    kong.log.err("failed to keepalive to ", conf.ldap_host, ":",
+                   tostring(conf.ldap_port), ": ", suppressed_err)
   end
+
   return is_authenticated, err
 end
 
 local function load_credential(given_username, given_password, conf)
   local ok, err = ldap_authenticate(given_username, given_password, conf)
   if err ~= nil then
-    ngx_log(ngx_error, err)
+    kong.log.err(err)
   end
 
   if ok == nil then
     return nil
   end
+
   if ok == false then
     return false
   end
-  return {username = given_username, password = given_password}
+
+  return { username = given_username, password = given_password }
 end
 
 
 local function cache_key(conf, username, password)
   if not ldap_config_cache[conf] then
     ldap_config_cache[conf] = md5(fmt("%s:%u:%s:%s:%u",
-      lower(conf.ldap_host),
-      conf.ldap_port,
-      conf.base_dn,
-      conf.attribute,
-      conf.cache_ttl
-    ))
+                                      lower(conf.ldap_host),
+                                      conf.ldap_port,
+                                      conf.base_dn,
+                                      conf.attribute,
+                                      conf.cache_ttl))
   end
 
   return fmt("ldap_auth_cache:%s:%s:%s", ldap_config_cache[conf],
@@ -110,8 +116,7 @@ end
 
 
 local function authenticate(conf, given_credentials)
-  local given_username, given_password = retrieve_credentials(given_credentials,
-                                                              conf)
+  local given_username, given_password = retrieve_credentials(given_credentials, conf)
   if given_username == nil then
     return false
   end
@@ -120,12 +125,15 @@ local function authenticate(conf, given_credentials)
     ttl = conf.cache_ttl,
     neg_ttl = conf.cache_ttl
   }, load_credential, given_username, given_password, conf)
+
   if err or credential == nil then
-    return responses.send_HTTP_INTERNAL_SERVER_ERROR(err)
+    kong.log.err(err)
+    return kong.response.exit(500, { message = "An unexpected error occurred" })
   end
 
   return credential and credential.password == given_password, credential
 end
+
 
 local function load_consumer(consumer_id, anonymous)
   local result, err = kong.db.consumers:select { id = consumer_id }
@@ -135,38 +143,49 @@ local function load_consumer(consumer_id, anonymous)
     end
     return nil, err
   end
+
   return result
 end
 
+
 local function set_consumer(consumer, credential)
+  local shared_ctx = kong.ctx.shared
+  local ngx_ctx = ngx.ctx -- TODO: for bc only
 
   if consumer then
     -- this can only be the Anonymous user in this case
-    ngx_set_header(constants.HEADERS.CONSUMER_ID, consumer.id)
-    ngx_set_header(constants.HEADERS.CONSUMER_CUSTOM_ID, consumer.custom_id)
-    ngx_set_header(constants.HEADERS.CONSUMER_USERNAME, consumer.username)
-    ngx_set_header(constants.HEADERS.ANONYMOUS, true)
-    ngx.ctx.authenticated_consumer = consumer
+    kong.service.request.set_header(constants.HEADERS.CONSUMER_ID, consumer.id)
+    kong.service.request.set_header(constants.HEADERS.CONSUMER_CUSTOM_ID, consumer.custom_id)
+    kong.service.request.set_header(constants.HEADERS.CONSUMER_USERNAME, consumer.username)
+    kong.service.request.set_header(constants.HEADERS.ANONYMOUS, true)
+
+    shared_ctx.authenticated_consumer = consumer
+    ngx_ctx.authenticated_consumer = consumer -- TODO: for bc only
+
     return
   end
 
-  -- here we have been authenticated by ldap
-  ngx_set_header(constants.HEADERS.CREDENTIAL_USERNAME, credential.username)
-  ngx.ctx.authenticated_credential = credential
+  if credential then
+    -- here we have been authenticated by ldap
+    kong.service.request.set_header(constants.HEADERS.CREDENTIAL_USERNAME, credential.username)
+
+    ngx_ctx.authenticated_credential = credential
+  end
 
   -- in case of auth plugins concatenation, remove remnants of anonymous
-  ngx.ctx.authenticated_consumer = nil
-  ngx_set_header(constants.HEADERS.ANONYMOUS, nil)
-  ngx_set_header(constants.HEADERS.CONSUMER_ID, nil)
-  ngx_set_header(constants.HEADERS.CONSUMER_CUSTOM_ID, nil)
-  ngx_set_header(constants.HEADERS.CONSUMER_USERNAME, nil)
+  shared_ctx.authenticated_consumer = nil
+  ngx_ctx.authenticated_consumer = nil -- TODO: for bc only
 
+  kong.service.request.clear_header(constants.HEADERS.ANONYMOUS)
+  kong.service.request.clear_header(constants.HEADERS.CONSUMER_ID)
+  kong.service.request.clear_header(constants.HEADERS.CONSUMER_CUSTOM_ID)
+  kong.service.request.clear_header(constants.HEADERS.CONSUMER_USERNAME)
 end
 
+
 local function do_authentication(conf)
-  local headers = request.get_headers()
-  local authorization_value = headers[AUTHORIZATION]
-  local proxy_authorization_value = headers[PROXY_AUTHORIZATION]
+  local authorization_value = kong.request.get_header(AUTHORIZATION)
+  local proxy_authorization_value = kong.request.get_header(PROXY_AUTHORIZATION)
 
   -- If both headers are missing, return 401
   if not (authorization_value or proxy_authorization_value) then
@@ -177,8 +196,11 @@ local function do_authentication(conf)
       scheme = upper(scheme)
     end
 
-    ngx.header["WWW-Authenticate"] = scheme .. ' realm="kong"'
-    return false, {status = 401}
+    return false, {
+      status = 401,
+      message = "Unauthorized",
+      headers = { ["WWW-Authenticate"] = scheme .. ' realm="kong"' }
+    }
   end
 
   local is_authorized, credential = authenticate(conf, proxy_authorization_value)
@@ -187,12 +209,12 @@ local function do_authentication(conf)
   end
 
   if not is_authorized then
-    return false, {status = 403, message = "Invalid authentication credentials"}
+    return false, {status = 403, message = "Invalid authentication credentials" }
   end
 
   if conf.hide_credentials then
-    request.clear_header(AUTHORIZATION)
-    request.clear_header(PROXY_AUTHORIZATION)
+    kong.service.request.clear_header(AUTHORIZATION)
+    kong.service.request.clear_header(PROXY_AUTHORIZATION)
   end
 
   set_consumer(nil, credential)
@@ -202,11 +224,20 @@ end
 
 
 function _M.execute(conf)
+  if conf.anonymous then
+    local shared_ctx = kong.ctx.shared
+    if shared_ctx.authenticated_credential then
+      -- we're already authenticated, and we're configured for using anonymous,
+      -- hence we're in a logical OR between auth methods and we're already done.
+      return
+    end
 
-  if ngx.ctx.authenticated_credential and conf.anonymous then
-    -- we're already authenticated, and we're configured for using anonymous,
-    -- hence we're in a logical OR between auth methods and we're already done.
-    return
+    local ngx_ctx = ngx.ctx -- TODO: for bc only
+    if ngx_ctx.authenticated_credential then
+      -- we're already authenticated, and we're configured for using anonymous,
+      -- hence we're in a logical OR between auth methods and we're already done.
+      return
+    end
   end
 
   local ok, err = do_authentication(conf)
@@ -218,11 +249,14 @@ function _M.execute(conf)
                                                       load_consumer,
                                                       conf.anonymous, true)
       if err then
-        responses.send_HTTP_INTERNAL_SERVER_ERROR(err)
+        kong.log.err(err)
+        return kong.response.exit(500, { message = "An unexpected error occurred" })
       end
+
       set_consumer(consumer, nil)
+
     else
-      return responses.send(err.status, err.message)
+      return kong.response.exit(err.status, { message = err.message }, err.headers)
     end
   end
 end
