36084ce69649f0fb09f4703082e2947c91a8c421
Enrique García Cota <kikito@gmail.com>
2018-10-17 00:25:25 +0200

    feat(azure-functions) update to new db & pdk

--- kong/plugins/azure-functions/schema.lua
+++ kong/plugins/azure-functions/schema.lua
@@ -1,17 +1,23 @@
 return {
+  name = "azure-functions",
   fields = {
-    -- connection basics
-    timeout       = { type = "number",  default  = 600000},
-    keepalive     = { type = "number",  default  = 60000 },
-    https         = { type = "boolean", default  = true  },
-    https_verify  = { type = "boolean", default  = false },
-    -- authorization
-    apikey        = { type = "string"                    },
-    clientid      = { type = "string"                    },
-    -- target/location
-    appname       = { type = "string",  required = true  },
-    hostdomain    = { type = "string",  required = true, default = "azurewebsites.net" },
-    routeprefix   = { type = "string",  default = "api"  },
-    functionname  = { type = "string",  required = true  },
-  }
+    { config = {
+        type = "record",
+        fields = {
+          -- connection basics
+          { timeout       = { type = "number",  default  = 600000}, },
+          { keepalive     = { type = "number",  default  = 60000 }, },
+          { https         = { type = "boolean", default  = true  }, },
+          { https_verify  = { type = "boolean", default  = false }, },
+          -- authorization
+          { apikey        = { type = "string"                    }, },
+          { clientid      = { type = "string"                    }, },
+          -- target/location
+          { appname       = { type = "string",  required = true  }, },
+          { hostdomain    = { type = "string",  required = true, default = "azurewebsites.net" }, },
+          { routeprefix   = { type = "string",  default = "api"  }, },
+          { functionname  = { type = "string",  required = true  }, },
+        },
+    }, },
+  },
 }
