c77b5647268714c77322ac94615213114c147927
James Callahan <james@konghq.com>
2018-10-18 18:30:05 +1100

    Release v0.0.6

--- kong/plugins/zipkin/opentracing.lua
+++ kong/plugins/zipkin/opentracing.lua
@@ -12,7 +12,7 @@ local public = require "kong.tools.public"
 local ngx_set_header = ngx.req.set_header
 
 local OpenTracingHandler = BasePlugin:extend()
-OpenTracingHandler.VERSION = "scm"
+OpenTracingHandler.VERSION = "0.0.6"
 
 -- We want to run first so that timestamps taken are at start of the phase
 -- also so that other plugins might be able to use our structures
