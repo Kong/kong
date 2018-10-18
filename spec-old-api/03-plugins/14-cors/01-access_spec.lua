0eaa9acdda696f188646da2ddd29bceb8d71a28b
thefosk <marco@konghq.com>
2018-10-18 15:37:44 -0700

    fix(cors) anchor flat string validation of requests origins
    @thibaultcha:
    
    Prior to this patch, configuring the plugin with, e.g.:
    
        "config": {
          "origins": [
            "http://my-site.com",
            "http://my-other-site.com"
          ]
        }
    
    Would make the following request Origin validate comparison and
    injecting the ACAO response header:
    
        curl -X OPTIONS \
          http://localhost:8000 \
          -H 'Origin: http://my-site.com.bad-guys.com'
    
    This patch's main goal is to anchor the comparison regex in order to
    ensure we match the full configured origin.
    
    Additionally, we now ensure that protocol, host, and port are all taken
    in consideration when comparing origins, as per RFC 3654 on "Comparing
    Origins".
    
    Fix #3832
    From #3872
    
    Signed-off-by: Thibault Charbonnier <thibaultcha@me.com>

--- spec-old-api/03-plugins/14-cors/01-access_spec.lua
+++ spec-old-api/03-plugins/14-cors/01-access_spec.lua
@@ -321,11 +321,11 @@ describe("Plugin: cors (access)", function()
         method = "GET",
         headers = {
           ["Host"] = "cors6.com",
-          ["Origin"] = "http://www.example.com"
+          ["Origin"] = "example.com"
         }
       })
       assert.res_status(200, res)
-      assert.equal("http://www.example.com", res.headers["Access-Control-Allow-Origin"])
+      assert.equal("example.com", res.headers["Access-Control-Allow-Origin"])
 
       local domains = {
         ["example.com"] = true,
@@ -354,7 +354,7 @@ describe("Plugin: cors (access)", function()
         method = "GET",
         headers = {
           ["Host"] = "cors6.com",
-          ["Origin"] = "http://www.example.net"
+          ["Origin"] = "example.net"
         }
       })
       assert.res_status(200, res)
