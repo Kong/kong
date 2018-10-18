8366d3e18289fc63fd0517a6b0305e8f13985992
Colin Hutchinson <chutchic@gmail.com>
2018-10-18 19:37:37 +0000

    chore(ci) use counter metric type instead of gauge which is the default

--- .ci/track_failures.sh
+++ .ci/track_failures.sh
@@ -12,6 +12,6 @@ appkey = $DD_APP_KEY
 EOL
 
 cat ~/output/* | grep 'FAILED  ' | grep -v listed | grep -v ms > collated_output.txt
-cat collated_output.txt | sed 's/^.*spec\///; s/\.lua.*$//' | sed 's/^/travis_ci.kong.failure./' > parsed_output.txt
+cat collated_output.txt | sed 's/^.*spec\///; s/\.lua.*$//' | sed 's/^//' > parsed_output.txt
 cat parsed_output.txt | sed 's/-/_/g' | sed 's/[^a-zA-Z0-9_]/\./g' > dd_compatible.txt
-<dd_compatible.txt xargs -I % dog metric post % 1
+<dd_compatible.txt xargs -I % dog metric post travis_ci.kong.failure --type count --tags "test:%" 1
