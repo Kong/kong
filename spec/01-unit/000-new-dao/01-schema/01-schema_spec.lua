66e5a48cf6df70febd88261e542d53de0db4cd8b
Thibault Charbonnier <thibaultcha@me.com>
2018-10-29 14:04:37 -0700

    feat(schema) new 'conditional_at_least_one_of' entity checker

--- spec/01-unit/000-new-dao/01-schema/01-schema_spec.lua
+++ spec/01-unit/000-new-dao/01-schema/01-schema_spec.lua
@@ -1231,6 +1231,82 @@ describe("schema", function()
 
     end)
 
+    describe("entity_checkers", function()
+
+      describe("conditional_at_least_one_of", function()
+        local Test = Schema.new({
+          fields = {
+            { a = { type = "number" }, },
+            { b = { type = "string" }, },
+            { c = { type = "string" }, },
+          },
+          entity_checks = {
+            { conditional_at_least_one_of = { if_field = "a",
+                                              if_match = { gt = 0 },
+                                              then_at_least_one_of = { "b", "c" }}
+            },
+          }
+        })
+
+        it("sanity", function()
+          local ok, errs = Test:validate_insert({ a = 1 })
+          assert.is_nil(ok)
+          assert.same({
+            "at least one of these fields must be non-empty: 'b', 'c'"
+          }, errs["@entity"])
+
+          local ok, errs = Test:validate_insert({ a = 1, b = "foo" })
+          assert.is_nil(errs)
+          assert.truthy(ok)
+        end)
+
+        it("does not run when condition is evaluated to false", function()
+          local ok, errs = Test:validate_insert({ a = 0 })
+          assert.is_nil(errs)
+          assert.truthy(ok)
+        end)
+
+        it("does not run when the 'if_field' is missing", function()
+          local ok, errs = Test:validate_insert({ b = "foo" })
+          assert.is_nil(errs)
+          assert.truthy(ok)
+        end)
+
+        it("supports an 'else' clause", function()
+          local Test = Schema.new({
+            fields = {
+              { a = { type = "number" }, },
+              { b = { type = "string" }, },
+              { c = { type = "string" }, },
+              { d = { type = "string" }, },
+            },
+            entity_checks = {
+              { conditional_at_least_one_of = { if_field = "a",
+                                                if_match = { gt = 0 },
+                                                then_at_least_one_of = { "b", "c" },
+                                                else_match = { ne = 0 },
+                                                else_then_at_least_one_of = { "c", "d" }, }
+              },
+            }
+          })
+
+          local ok, errs = Test:validate_insert({ a = -1 })
+          assert.is_nil(ok)
+          assert.same({
+            "at least one of these fields must be non-empty: 'c', 'd'"
+          }, errs["@entity"])
+
+          local ok, errs = Test:validate_insert({ a = -1, d = "foo" })
+          assert.is_nil(errs)
+          assert.truthy(ok)
+
+          local ok, errs = Test:validate_insert({ a = 0 })
+          assert.is_nil(errs)
+          assert.truthy(ok)
+        end)
+      end)
+
+    end)
   end)
 
   describe("validate_primary_key", function()
