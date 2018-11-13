3ef9941a14b59cb58b76a7f9bcab9a03a6749424
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2018-11-13 15:46:38 +0200

    fix(response-transformer) make plugin defensive against possible errors and nil header values

--- spec/03-plugins/16-response-transformer/01-header_transformer_spec.lua
+++ spec/03-plugins/16-response-transformer/01-header_transformer_spec.lua
@@ -1,10 +1,41 @@
-local header_transformer = require "kong.plugins.response-transformer.header_transformer"
+local CONTENT_LENGTH = "Content-Length"
+local CONTENT_TYPE = "Content-Type"
+local JSON = "application/json"
+local JSON_UTF8 = "application/json; charset=utf-8"
+local FORM = "application/x-www-form-urlencoded; charset=utf-8"
 
 
-local CONTENT_LENGTH     = "content-length"
+local function get_headers(headers)
+  _G.ngx.resp.get_headers = function()
+    return headers
+  end
+
+  _G.ngx.header = headers
+
+  return headers
+end
 
 
 describe("Plugin: response-transformer", function()
+  local header_transformer
+
+  setup(function()
+    _G.ngx = {
+      headers_sent = false,
+      resp = {
+      }
+    }
+    _G.kong = {
+      response = require "kong.pdk.response".new(),
+      ctx = {
+        core = {
+          phase = 0x00000200,
+        }
+      }
+    }
+
+    header_transformer = require "kong.plugins.response-transformer.header_transformer"
+  end)
   describe("execute_headers()", function()
     describe("remove", function()
       local conf  = {
@@ -23,14 +54,14 @@ describe("Plugin: response-transformer", function()
         }
       }
       it("all the headers", function()
-        local ngx_headers = {h1 = "value1", h2 = {"value2a", "value2b"}}
-        header_transformer.transform_headers(conf, ngx_headers)
-        assert.same({}, ngx_headers)
+        local headers = get_headers({ h1 = "value1", h2 = { "value2a", "value2b" } })
+        header_transformer.transform_headers(conf, headers)
+        assert.same({}, headers)
       end)
       it("sets content-length nil", function()
-        local ngx_headers = {h1 = "value1", h2 = {"value2a", "value2b"}, [CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-        header_transformer.transform_headers(conf, ngx_headers)
-        assert.is_nil(ngx_headers[CONTENT_LENGTH])
+        local headers = get_headers({ h1 = "value1", h2 = {"value2a", "value2b"}, [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+        header_transformer.transform_headers(conf, headers)
+        assert.is_nil(headers[CONTENT_LENGTH])
       end)
     end)
     describe("replace", function()
@@ -50,19 +81,19 @@ describe("Plugin: response-transformer", function()
         }
       }
       it("header if the header only exists", function()
-        local req_ngx_headers = {h1 = "value1", h2 = {"value2a", "value2b"}}
-        header_transformer.transform_headers(conf, req_ngx_headers)
-        assert.same({h1 = "v1", h2 = "value:2"}, req_ngx_headers)
+        local headers = get_headers({ h1 = "value1", h2 = { "value2a", "value2b" } })
+        header_transformer.transform_headers(conf, headers)
+        assert.same({h1 = "v1", h2 = "value:2"}, headers)
       end)
       it("does not add a new header if the header does not already exist", function()
-        local req_ngx_headers = {h2 = {"value2a", "value2b"}}
-        header_transformer.transform_headers(conf, req_ngx_headers)
-        assert.same({h2 = "value:2"}, req_ngx_headers)
+        local headers = get_headers({ h2 = { "value2a", "value2b" } })
+        header_transformer.transform_headers(conf, headers)
+        assert.same({h2 = "value:2"}, headers)
       end)
       it("sets content-length nil", function()
-        local ngx_headers = {h1 = "value1", h2 = {"value2a", "value2b"}, [CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-        header_transformer.transform_headers(conf, ngx_headers)
-        assert.is_nil(ngx_headers[CONTENT_LENGTH])
+        local headers = get_headers({ h1 = "value1", h2 = {"value2a", "value2b"}, [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+        header_transformer.transform_headers(conf, headers)
+        assert.is_nil(headers[CONTENT_LENGTH])
       end)
     end)
     describe("add", function()
@@ -82,19 +113,19 @@ describe("Plugin: response-transformer", function()
         }
       }
       it("header if the header does not exists", function()
-        local req_ngx_headers = {h1 = "v1"}
-        header_transformer.transform_headers(conf, req_ngx_headers)
-        assert.same({h1 = "v1", h2 = "v2"}, req_ngx_headers)
+        local headers = get_headers({ h1 = "v1" })
+        header_transformer.transform_headers(conf, headers)
+        assert.same({h1 = "v1", h2 = "v2"}, headers)
       end)
       it("does not add a new header if the header already exist", function()
-        local req_ngx_headers = {h1 = "v1", h2 = "v3"}
-        header_transformer.transform_headers(conf, req_ngx_headers)
-        assert.same({h1 = "v1", h2 = "v3"}, req_ngx_headers)
+        local headers = get_headers({ h1 = "v1", h2 = "v3" })
+        header_transformer.transform_headers(conf, headers)
+        assert.same({h1 = "v1", h2 = "v3"}, headers)
       end)
       it("sets content-length nil", function()
-        local ngx_headers = {h1 = "v1", [CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-        header_transformer.transform_headers(conf, ngx_headers)
-        assert.is_nil(ngx_headers[CONTENT_LENGTH])
+        local headers = get_headers({ h1 = "v1", [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+        header_transformer.transform_headers(conf, headers)
+        assert.is_nil(headers[CONTENT_LENGTH])
       end)
     end)
     describe("append", function()
@@ -114,19 +145,19 @@ describe("Plugin: response-transformer", function()
         }
       }
       it("header if the header does not exists", function()
-        local req_ngx_headers = {}
-        header_transformer.transform_headers(conf, req_ngx_headers)
-        assert.same({"v2"}, req_ngx_headers["h1"])
+        local headers = get_headers({})
+        header_transformer.transform_headers(conf, headers)
+        assert.same({"v2"}, headers["h1"])
       end)
       it("header if the header already exist", function()
-        local req_ngx_headers = {h1 = "v1"}
-        header_transformer.transform_headers(conf, req_ngx_headers)
-        assert.same({h1 = {"v1", "v2"}}, req_ngx_headers)
+        local headers = get_headers({ h1 = "v1" })
+        header_transformer.transform_headers(conf, headers)
+        assert.same({h1 = {"v1", "v2"}}, headers)
       end)
       it("sets content-length nil", function()
-        local ngx_headers = {h1 = "v1", [CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-        header_transformer.transform_headers(conf, ngx_headers)
-        assert.is_nil(ngx_headers[CONTENT_LENGTH])
+        local headers = get_headers({ h1 = "v1", [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+        header_transformer.transform_headers(conf, headers)
+        assert.is_nil(headers[CONTENT_LENGTH])
       end)
     end)
     describe("performing remove, replace, add, append together", function()
@@ -146,14 +177,14 @@ describe("Plugin: response-transformer", function()
         }
       }
       it("transforms all headers", function()
-        local req_ngx_headers = {h1 = "v1", h2 = "v2"}
-        header_transformer.transform_headers(conf, req_ngx_headers)
-        assert.same({h2 = "v3", h3 = {"v3", "v4"}}, req_ngx_headers)
+        local headers = get_headers({ h1 = "v1", h2 = "v2" })
+        header_transformer.transform_headers(conf, headers)
+        assert.same({h2 = "v3", h3 = {"v3", "v4"}}, headers)
       end)
       it("sets content-length nil", function()
-        local ngx_headers = {h1 = "v1", [CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-        header_transformer.transform_headers(conf, ngx_headers)
-        assert.is_nil(ngx_headers[CONTENT_LENGTH])
+        local headers = get_headers({ h1 = "v1", [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+        header_transformer.transform_headers(conf, headers)
+        assert.is_nil(headers[CONTENT_LENGTH])
       end)
     end)
     describe("content-type json", function()
@@ -177,25 +208,25 @@ describe("Plugin: response-transformer", function()
           }
         }
         it("sets content-length nil if application/json passed", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.is_nil(ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+          header_transformer.transform_headers(conf, headers)
+          assert.is_nil(headers[CONTENT_LENGTH])
         end)
         it("sets content-length nil if application/json and charset passed", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json; charset=utf-8"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.is_nil(ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON_UTF8 })
+          header_transformer.transform_headers(conf, headers)
+          assert.is_nil(headers[CONTENT_LENGTH])
         end)
         it("does not set content-length nil if content-type not json", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/x-www-form-urlencoded; charset=utf-8"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.equals('100', ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = FORM })
+          header_transformer.transform_headers(conf, headers)
+          assert.equals('100', headers[CONTENT_LENGTH])
         end)
         it("does not set content-length nil if any of json not set", function()
           conf.remove.json = {}
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.equals('100', ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+          header_transformer.transform_headers(conf, headers)
+          assert.equals('100', headers[CONTENT_LENGTH])
         end)
       end)
       describe("replace", function()
@@ -218,25 +249,25 @@ describe("Plugin: response-transformer", function()
           }
         }
         it("sets content-length nil if application/json passed", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.is_nil(ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+          header_transformer.transform_headers(conf, headers)
+          assert.is_nil(headers[CONTENT_LENGTH])
         end)
         it("sets content-length nil if application/json and charset passed", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json; charset=utf-8"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.is_nil(ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON_UTF8 })
+          header_transformer.transform_headers(conf, headers)
+          assert.is_nil(headers[CONTENT_LENGTH])
         end)
         it("does not set content-length nil if content-type not json", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/x-www-form-urlencoded; charset=utf-8"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.equals('100', ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = FORM })
+          header_transformer.transform_headers(conf, headers)
+          assert.equals('100', headers[CONTENT_LENGTH])
         end)
         it("does not set content-length nil if any of json not set", function()
           conf.replace.json = {}
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.equals('100', ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+          header_transformer.transform_headers(conf, headers)
+          assert.equals('100', headers[CONTENT_LENGTH])
         end)
       end)
       describe("add", function()
@@ -259,25 +290,25 @@ describe("Plugin: response-transformer", function()
           }
         }
         it("set content-length nil if application/json passed", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.is_nil(ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+          header_transformer.transform_headers(conf, headers)
+          assert.is_nil(headers[CONTENT_LENGTH])
         end)
         it("set content-length nil if application/json and charset passed", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json; charset=utf-8"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.is_nil(ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON_UTF8 })
+          header_transformer.transform_headers(conf, headers)
+          assert.is_nil(headers[CONTENT_LENGTH])
         end)
         it("does not set content-length nil if content-type not json", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/x-www-form-urlencoded; charset=utf-8"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.equals('100', ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = FORM })
+          header_transformer.transform_headers(conf, headers)
+          assert.equals('100', headers[CONTENT_LENGTH])
         end)
         it("does not set content-length nil if any of json not set", function()
           conf.add.json = {}
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.equals('100', ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+          header_transformer.transform_headers(conf, headers)
+          assert.equals('100', headers[CONTENT_LENGTH])
         end)
       end)
       describe("append", function()
@@ -300,25 +331,25 @@ describe("Plugin: response-transformer", function()
           }
         }
         it("set content-length nil if application/json passed", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.is_nil(ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+          header_transformer.transform_headers(conf, headers)
+          assert.is_nil(headers[CONTENT_LENGTH])
         end)
         it("set content-length nil if application/json and charset passed", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json; charset=utf-8"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.is_nil(ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON_UTF8 })
+          header_transformer.transform_headers(conf, headers)
+          assert.is_nil(headers[CONTENT_LENGTH])
         end)
         it("does not set content-length nil if content-type not json", function()
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/x-www-form-urlencoded; charset=utf-8"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.equals('100', ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = FORM })
+          header_transformer.transform_headers(conf, headers)
+          assert.equals('100', headers[CONTENT_LENGTH])
         end)
         it("does not set content-length nil if any of json not set", function()
           conf.append.json = {}
-          local ngx_headers = {[CONTENT_LENGTH] = "100", ["content-type"] = "application/json"}
-          header_transformer.transform_headers(conf, ngx_headers)
-          assert.equals('100', ngx_headers[CONTENT_LENGTH])
+          local headers = get_headers({ [CONTENT_LENGTH] = "100", [CONTENT_TYPE] = JSON })
+          header_transformer.transform_headers(conf, headers)
+          assert.equals('100', headers[CONTENT_LENGTH])
         end)
       end)
     end)
