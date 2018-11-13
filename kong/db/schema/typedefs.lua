816331945a0456d7994be23b4c89aa9462e1ea40
James Callahan <james@konghq.com>
2018-11-13 18:32:57 +1100

    feat(schema) add `cidr` typedef
    Factored out of ip-restriction plugin

--- kong/db/schema/typedefs.lua
+++ kong/db/schema/typedefs.lua
@@ -3,7 +3,7 @@
 local utils = require "kong.tools.utils"
 local openssl_pkey = require "openssl.pkey"
 local openssl_x509 = require "openssl.x509"
-
+local iputils = require "resty.iputils"
 local Schema = require("kong.db.schema")
 local socket_url = require("socket.url")
 
@@ -36,6 +36,18 @@ local function validate_ip(ip)
 
   if res.type == "name" then
     return nil, "not an ip address: " .. ip
+  end
+
+  return true
+end
+
+
+local function validate_cidr(ip)
+  local _, err = iputils.parse_cidr(ip)
+
+  -- It's an error only if the second variable is a string
+  if type(err) == "string" then
+    return nil, "invalid cidr range: " .. err
   end
 
   return true
@@ -161,6 +173,12 @@ typedefs.host = Schema.define {
 typedefs.ip = Schema.define {
   type = "string",
   custom_validator = validate_ip,
+}
+
+
+typedefs.cidr = Schema.define {
+  type = "string",
+  custom_validator = validate_cidr,
 }
 
 
