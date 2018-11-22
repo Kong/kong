2896e365a817a2f2f7253abc980c83500afa4b32
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2018-11-22 21:46:59 +0200

    tests(*) fix ctx-checker plugin and add support for arrays to it

--- spec/fixtures/custom_plugins/kong/plugins/ctx-checker/schema.lua
+++ spec/fixtures/custom_plugins/kong/plugins/ctx-checker/schema.lua
@@ -2,8 +2,10 @@ return {
   fields = {
     ctx_set_field   = { type = "string" },
     ctx_set_value   = { type = "string", default = "set_by_ctx_checker" },
+    ctx_set_array   = { type = "array"  },
     ctx_check_field = { type = "string" },
     ctx_check_value = { type = "string" },
+    ctx_check_array = { type = "array"  },
     ctx_kind        = { type = "string", default = "ngx.ctx" },
     ctx_throw_error = { type = "boolean", default = false },
   }
