e44a158b9f5d905ee1b7ec2701527aeb60dec05d
Darren Jennings <dmjenn02@gmail.com>
2018-12-14 16:19:25 -0800

    chore(session) make global variables local

--- kong/plugins/session/handler.lua
+++ kong/plugins/session/handler.lua
@@ -30,7 +30,7 @@ function KongSessionHandler:header_filter(conf)
   -- if session exists and the data in the session matches the ctx then
   -- don't worry about saving the session data or sending cookie
   if s and s.present then
-    cid, cred_id = session.retrieve_session_data(s)
+    local cid, cred_id = session.retrieve_session_data(s)
     if cred_id == credential_id and cid == consumer_id
     then
       return
