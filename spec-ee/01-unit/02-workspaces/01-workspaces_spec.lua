b30e714a3f54069d38873988e531c5b43bdb70ba
Raimon Grau <raimonster@gmail.com>
2018-12-21 13:39:44 +0000

    tests(workspaces) collision tests use routes
    Changed apis to routes in collision tests

--- spec-ee/01-unit/02-workspaces/01-workspaces_spec.lua
+++ spec-ee/01-unit/02-workspaces/01-workspaces_spec.lua
@@ -82,62 +82,58 @@ describe("workspaces", function()
     end)
   end)
 
-  it("is able to detect a matching [host, uri, method ] in the router", function()
-    local Router = require "kong.core.api_router"
+  it("is able to detect a matching host, uri, method  in the router", function()
+    local Router = require "kong.core.router"
 
-    local apis = {
+    local s = {{id = "dc0a9bdd-b1e0-4c6d-9218-6e9f1e0a9e6b"}}
+    local routes = {
       {
-        created_at = 1521209668855,
+        service = s,
         headers = {
           host = { "myapi1" }
         },
-        hosts = { "myapi1" },
-        http_if_terminated = false,
-        https_only = false,
+        route = {
+        service = {id = "b39c5805-e51f-4159-918a-8b49069bc00c"},
+        created_at = 1521209668855,
+        paths = {"/"},
         id = "cd3205b8-5e52-4951-829d-fee3e38949b2",
-        name = "foo2",
         preserve_host = false,
-        retries = 5,
-        strip_uri = true,
-        upstream_connect_timeout = 60000,
-        upstream_read_timeout = 60000,
-        upstream_send_timeout = 60000,
-        upstream_url = "https://requestb.in/w2r6y3w2",
         workspace = {{ id = "default"}}
+        },
       },
       {
-        created_at = 1521494974461,
+        service = {id = "24b959a6-da08-4937-9b0e-ecb999c61a35"},
         headers = {
           host = { "myapi1" }
         },
-        hosts = {"myapi1"},
-        http_if_terminated = false,
-        https_only = false,
+        route = {
+        created_at = 1521494974461,
         id = "6b4d66b6-f615-44fe-bfec-116a6a37bdf1",
         name = "blabla",
         preserve_host = false,
-        retries = 5,
-        strip_uri = true,
-        upstream_connect_timeout = 60000,
-        upstream_read_timeout = 60000,
-        upstream_send_timeout = 60000,
-        upstream_url = "https://requestb.in/w2r6y3w2",
-        workspaces = {{ id = "foo"}}
+        workspaces = {{ id = "foo"}},
+        },
       },
       {
+        service = {id = "0bc89bbd-7ea7-4566-b474-8fcadf6ff955d"},
+        route = {
         name = "api-1",
         methods = { "POST", "PUT", "GET" },
-        uris = { "/my-api" },
-        workspaces = {{ id = "ws1"}} ,
+        paths = { "/my-api" },
+        workspaces = {{ id = "ws1"}}
+        }
       },
       {
-        name = "api-2",
-        methods = { "POST", "PUT", "GET" },
-        uris = { "/my-api2" },
-        workspaces = {{id = "ws2"}} ,
+        service = {id = "781a63e9-2778-4a34-a17f-a35c443bc7f1"},
+        route = {
+          name = "api-2",
+          methods = { "POST", "PUT", "GET" },
+          paths = { "/my-api2" },
+          workspaces = {{id = "ws2"}}} ,
       }
     }
-    local r = Router.new(apis)
+
+    local r = assert(Router.new(routes))
     local matched_route = r.select("GET", "/","")
     local ws1 = {id = "ws1"}
     local ws2 = {id = "ws2"}
@@ -151,16 +147,16 @@ describe("workspaces", function()
 
     matched_route = workspaces.match_route(r, "GET", "/my-api", "")
     assert.truthy(matched_route)
-    assert.truthy(workspaces.is_route_in_ws(matched_route.api, ws1))
+    assert.truthy(workspaces.is_route_in_ws(matched_route.route, ws1))
 
     matched_route = workspaces.match_route(r, "GET", "/my-api2", "")
     assert.truthy(matched_route)
-    assert.truthy(workspaces.is_route_in_ws(matched_route.api, ws2))
-    assert.falsy(workspaces.is_route_in_ws(matched_route.api, ws1))
+    assert.truthy(workspaces.is_route_in_ws(matched_route.route, ws2))
+    assert.falsy(workspaces.is_route_in_ws(matched_route.route, ws1))
   end)
 
   describe("is_route_in_ws accepts", function()
-    local single_api, multiple_api
+    local single_route, multiple_route
     local ws1 = {id = "ws1"}
     local ws2 = {id = "ws2"}
 
@@ -168,13 +164,13 @@ describe("workspaces", function()
       single_route = {
         name = "api-2",
         methods = { "POST", "PUT", "GET" },
-        uris = { "/my-api2" },
+        paths = { "/my-api2" },
         workspaces = {{id = "ws1"}},
       }
       multiple_route = {
         name = "api-2",
         methods = { "POST", "PUT", "GET" },
-        uris = { "/my-api2" },
+        paths = { "/my-api2" },
         workspaces = {{ id = "ws1" }, { id = "ws2"}},
       }
     end)
@@ -193,103 +189,115 @@ describe("workspaces", function()
   end)
 
   describe("adding a route", function()
-      local apis = {
-      {
-        headers = {
-          host = { "myapi1" }
-        },
-        hosts = { "myapi1" },
-        uris = {"/yeah"},
-        methods = { "POST", "PUT", "GET" },
-        name = "foo2",
-        upstream_url = "https://requestb.in/w2r6y3w2",
-        workspaces = {{ id = "default"}}
-      }, {
-        headers = {
-          host = { "myapi1" }
-        },
-        hosts = { "myapi1" },
-        name = "blabla",
-        upstream_url = "https://requestb.in/w2r6y3w2",
-        workspaces = {{ id = "foo"}}
-       }, {
-        name = "api-1",
-        methods = { "POST", "PUT", "GET" },
-        uris = { "/my-api" },
-        workspaces = {{ id = "ws1"}} ,
-       }, {
-        name = "api-2",
-        methods = { "POST", "PUT", "GET" },
-        uris = { "/my-api2" },
-        workspaces = {{ id = "ws2" }} ,
-      }, {
-        headers = {
-          host = { "*" }
-        },
-        hosts = { "*" },
-        name = "api-3",
-        methods = { "POST", "PUT", "GET" },
-        uris = { "/my-api3" },
-        workspaces = {{ id = "ws3" }} ,
-      }, {
-        hosts = { "host4" },
-        name = "api-4",
-        methods = { "POST", "PUT", "GET" },
-        uris = { "/api4" },
-        workspaces = {{ id = "ws4" }} ,
-      }, {
-        name = "api-5",
-        hosts = ngx.null,
-        methods = { "POST", "PUT", "GET" },
-        uris = { "/my-api5" },
-        workspaces = {{ id = "ws2" }} ,
+    local routes
+    setup(function()
+      routes = {
+        {
+          headers = {
+            host = { "myapi1" }
+          },
+          hosts = { "myapi1" },
+          paths = {"/yeah"},
+          methods = { "POST", "PUT", "GET" },
+          name = "foo2",
+          upstream_url = "https://requestb.in/w2r6y3w2",
+          workspaces = {{ id = "default"}}
+        }, {
+          headers = {
+            host = { "myapi1" }
+          },
+          hosts = { "myapi1" },
+          name = "blabla",
+          paths = {"/"},
+          upstream_url = "https://requestb.in/w2r6y3w2",
+          workspaces = {{ id = "foo"}}
+           }, {
+          name = "api-1",
+          methods = { "POST", "PUT", "GET" },
+          paths = { "/my-api" },
+          workspaces = {{ id = "ws1"}} ,
+              }, {
+          name = "api-2",
+          methods = { "POST", "PUT", "GET" },
+          paths = { "/my-api2" },
+          workspaces = {{ id = "ws2" }} ,
+                 }, {
+          headers = {
+            host = { "*" }
+          },
+          hosts = { "*" },
+          name = "api-3",
+          methods = { "POST", "PUT", "GET" },
+          paths = { "/my-api3" },
+          workspaces = {{ id = "ws3" }} ,
+                    }, {
+          hosts = { "host4" },
+          name = "api-4",
+          methods = { "POST", "PUT", "GET" },
+          paths = { "/api4" },
+          workspaces = {{ id = "ws4" }} ,
+                       }, {
+          name = "api-5",
+          hosts = nil,
+          methods = { "POST", "PUT", "GET" },
+          paths = { "/my-api5" },
+          workspaces = {{ id = "ws2" }} ,
+                          }
       }
-    }
+
+      local s = {{id = "dc0a9bdd-b1e0-4c6d-9218-6e9f1e0a9e6b"}}
+      for i, r in ipairs(routes) do
+        routes[i] = {route = r, service = s}
+      end
+
+    end)
 
     it("selects routes correctly", function()
-      local Router = require "kong.core.api_router"
-      local r = Router.new(apis)
-      assert.equal("api-2", workspaces.match_route(r, "GET", "/my-api2", "h1").api.name)
+      local Router = require "kong.core.router"
+
+      local r = assert(Router.new(routes))
+
+      assert.equal("api-2", workspaces.match_route(r, "GET", "/my-api2", "h1").route.name)
     end)
 
     it("adds root route to an empty router", function()
-      local Router = require "kong.core.api_router"
+      local Router = require "kong.core.router"
       local r = Router.new({})
       assert.truthy(workspaces.validate_route_for_ws(r, "GET", "/", "bla"))
     end)
 
     it("adds route in the same ws", function()
-      local Router = require "kong.core.api_router"
-      local r = Router.new(apis)
+      local Router = require "kong.core.router"
+      local r = Router.new(routes)
       assert.truthy(workspaces.validate_route_for_ws(r, "GET", "/api4", "host4", {id = "ws4"}))
     end)
 
     it("ADD route in different ws, no host in existing one", function()
-      local Router = require "kong.core.api_router"
-      local r = Router.new(apis)
+      local Router = require "kong.core.router"
+      local r = Router.new(routes)
       assert.falsy(workspaces.validate_route_for_ws(r, "GET", "/my-api2", "hi", {id = "ws3"}))
     end)
 
     it("NOT add route in different ws, with same wildcard host", function()
-      local Router = require "kong.core.api_router"
-      local r = Router.new(apis)
+      local Router = require "kong.core.router"
+      local r = Router.new(routes)
       assert.equal("api-3", workspaces.match_route(r, "GET", "/my-api3",
-                                                   "h*").api.name)
+                                                   "h*").route.name)
       assert.falsy(
         workspaces.validate_route_for_ws(r, "GET", "/my-api3", "*", {id = "ws4"}))
     end)
 
     it("ADD route in different ws, with different wildcard host", function()
-      local Router = require "kong.core.api_router"
-      local r = Router.new(apis)
-      assert.equal("api-3", workspaces.match_route(r, "GET", "/my-api3", "h*").api.name)
+      local Router = require "kong.core.router"
+      local r = Router.new(routes)
+      assert.equal("api-3", workspaces.match_route(r, "GET", "/my-api3", "h*").route.name)
       assert.truthy(workspaces.validate_route_for_ws(r, "GET", "/my-api3", "*.foo.com", {id = "ws4"}))
     end)
 
     it("NOT add route in different ws, with full host in the conflicting route", function()
-      local Router = require "kong.core.api_router"
-      local r = Router.new(apis)
-      assert.equal("api-4", workspaces.match_route(r, "GET", "/api4", "host4").api.name)
+      local Router = require "kong.core.router"
+      local r = Router.new(routes)
+      assert.equal("api-4", workspaces.match_route(r, "GET", "/api4", "host4").route.name)
       assert.falsy(workspaces.validate_route_for_ws(r, "GET", "/api4", "host4", {id = "different"}))
     end)
 
