e125bd70b09b47452a8c1de45afc40f10bb6dbc3
Thibault Charbonnier <thibaultcha@users.noreply.github.com>
2019-01-03 17:51:12 -0800

    chore(*) bump license year to 2019
    From #4154

--- kong/tools/utils.lua
+++ kong/tools/utils.lua
@@ -4,7 +4,7 @@
 -- NOTE: Before implementing a function here, consider if it will be used in many places
 -- across Kong. If not, a local function in the appropriate module is preferred.
 --
--- @copyright Copyright 2016-2018 Kong Inc. All rights reserved.
+-- @copyright Copyright 2016-2019 Kong Inc. All rights reserved.
 -- @license [Apache 2.0](https://opensource.org/licenses/Apache-2.0)
 -- @module kong.tools.utils
 
