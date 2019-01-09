5613c9babea2d35bd99c98f566c0177cc29ffc96
wubins <418522791@qq.com>
2019-01-09 17:44:04 +0800

    Add AWS China region to plugin schema
    Add   "cn-north-1" and  "cn-northwest-1" to the list of allowed regions to permit AWS Lambda
    functions to be invoked from the AWS China region.

--- kong/plugins/aws-lambda/schema.lua
+++ kong/plugins/aws-lambda/schema.lua
@@ -5,6 +5,8 @@ local REGIONS = {
   "ap-south-1",
   "ap-southeast-1", "ap-southeast-2",
   "ca-central-1",
+  "cn-north-1",
+  "cn-northwest-1",
   "eu-central-1",
   "eu-west-1", "eu-west-2",
   "sa-east-1",
