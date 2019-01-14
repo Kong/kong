2977d05f9954116516a55d59a949ceaca51b66e0
James Callahan <james@konghq.com>
2019-01-14 15:21:18 +1100

    kong/plugins/zipkin/schema.lua: run_on is a top level field not part of config

--- kong/plugins/zipkin/schema.lua
+++ kong/plugins/zipkin/schema.lua
@@ -3,10 +3,10 @@ local typedefs = require "kong.db.schema.typedefs"
 return {
 	name = "zipkin",
 	fields = {
+		{ run_on = typedefs.run_on { default = "all" } },
 		{ config = {
 				type = "record",
 				fields = {
-					{ run_on = typedefs.run_on { default = "all", one_of = { "all" } } },
 					{ http_endpoint = typedefs.url{ required = true } },
 					{ sample_ratio = { type = "number",
 					                   default = 0.001,
