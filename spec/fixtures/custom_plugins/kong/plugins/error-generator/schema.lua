464564223460c892c0484e765c757860a6eaf427
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2019-01-24 13:50:54 +0200

    tests(*) add preread support to error generator test plugins

--- spec/fixtures/custom_plugins/kong/plugins/error-generator/schema.lua
+++ spec/fixtures/custom_plugins/kong/plugins/error-generator/schema.lua
@@ -2,6 +2,7 @@ return {
   fields = {
     certificate   = { type = "boolean", required = false, default = false },
     rewrite       = { type = "boolean", required = false, default = false },
+    preread       = { type = "boolean", required = false, default = false },
     access        = { type = "boolean", required = false, default = false },
     header_filter = { type = "boolean", required = false, default = false },
     body_filter   = { type = "boolean", required = false, default = false },
