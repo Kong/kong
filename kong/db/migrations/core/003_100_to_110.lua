2595bf627bea2cf8c090dceb825e0584f791bae1
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2019-01-29 17:05:40 +0200

    chore(db) drop un-used `upsert_ttl` postgres function

--- kong/db/migrations/core/003_100_to_110.lua
+++ kong/db/migrations/core/003_100_to_110.lua
@@ -7,6 +7,9 @@ return {
       UPDATE upstreams SET created_at = DATE_TRUNC('seconds', created_at);
       UPDATE targets   SET created_at = DATE_TRUNC('milliseconds', created_at);
 
+
+      DROP FUNCTION IF EXISTS "upsert_ttl" (TEXT, UUID, TEXT, TEXT, TIMESTAMP WITHOUT TIME ZONE);
+
     ]],
   },
 
