2e7681ce1a662b3978858518a394408ed5dbd655
Guilherme Salazar <gsz@acm.org>
2019-01-29 08:46:19 -0800

    fix(vitals) add vitals shms to stream template

--- kong/templates/nginx_kong_stream.lua
+++ kong/templates/nginx_kong_stream.lua
@@ -13,6 +13,11 @@ lua_shared_dict stream_kong_process_events 5m;
 lua_shared_dict stream_kong_cluster_events 5m;
 lua_shared_dict stream_kong_healthchecks   5m;
 lua_shared_dict stream_kong_rate_limiting_counters 12m;
+lua_shared_dict stream_kong_vitals_counters 50m;
+lua_shared_dict stream_kong_vitals_lists     1m;
+
+
+
 > if database == "cassandra" then
 lua_shared_dict stream_kong_cassandra      5m;
 > end
