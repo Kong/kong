24283993249fe1d99bdd56f9ede100cddfefc2cd
Hisham Muhammad <hisham@gobolinux.org>
2019-02-14 12:57:00 -0200

    chore(ci) enable database=off tests in Travis

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -4,15 +4,20 @@ set -e
 export BUSTED_ARGS="-o gtest -v --exclude-tags=flaky,ipv6"
 
 if [ "$KONG_TEST_DATABASE" == "postgres" ]; then
-    export TEST_CMD="bin/busted $BUSTED_ARGS,cassandra"
+    export TEST_CMD="bin/busted $BUSTED_ARGS,cassandra,off"
 elif [ "$KONG_TEST_DATABASE" == "cassandra" ]; then
     export KONG_TEST_CASSANDRA_KEYSPACE=kong_tests
     export KONG_TEST_DB_UPDATE_PROPAGATION=1
-    export TEST_CMD="bin/busted $BUSTED_ARGS,postgres"
+    export TEST_CMD="bin/busted $BUSTED_ARGS,postgres,off"
+else
+    export TEST_CMD="bin/busted $BUSTED_ARGS,postgres,cassandra"
 fi
 
 if [ "$TEST_SUITE" == "integration" ]; then
     eval "$TEST_CMD" spec/02-integration/
+fi
+if [ "$TEST_SUITE" == "dbless" ]; then
+    eval "$TEST_CMD" spec/02-integration/05-proxy
 fi
 if [ "$TEST_SUITE" == "plugins" ]; then
     eval "$TEST_CMD" spec/03-plugins/
