2e4a8b890e2195b1cf144df8edba8a296ede5ffd
Enrique García Cota <kikito@gmail.com>
2019-02-20 13:23:56 +0100

    release: 1.1.0rc1

--- kong/meta.lua
+++ kong/meta.lua
@@ -1,8 +1,8 @@
 local version = setmetatable({
   major = 1,
-  minor = 0,
-  patch = 3,
-  --suffix = ""
+  minor = 1,
+  patch = 0,
+  suffix = "rc1",
 }, {
   __tostring = function(t)
     return string.format("%d.%d.%d%s", t.major, t.minor, t.patch,
