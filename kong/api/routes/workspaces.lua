14ad43153e8e1cf30dcfc4ae73afc7d145ec6c8f
Guilherme Salazar <gsz@acm.org>
2019-02-22 11:29:54 -0800

    refactor(workspaces) leverage endpoints autogeneration
    Changelog:
    - Leverage endpoints autogeneration for workspaces CRUD operations
    - Replace usage of CRUD helpers with kong.api.endpoints
    - Update endpoints to use `parent` callbacks to perform Portal post
    process actions
    
    Known limitations:
    - Post process on PATCH isn't supported by CE; I needed to add it to
    kong.api.endpoints. patch_entity_endpoint now takes in a 4th formal
    paremeter post_process; we should PR that upstream at some point
    - Why isn't there Portal post-process on PUT? It most likely needs o be
    implemented

--- kong/api/routes/workspaces.lua
+++ kong/api/routes/workspaces.lua
@@ -1,15 +1,14 @@
-local cjson = require "cjson"
 local crud  = require "kong.api.crud_helpers"
 local utils = require "kong.tools.utils"
-local rbac  = require "kong.rbac"
 local workspaces = require "kong.workspaces"
 local singletons = require "kong.singletons"
+local endpoints = require "kong.api.endpoints"
 
 -- FT-258: To block some endpoints of being wrongly called from a
 -- workspace namespace, we enforce that some workspace endpoints are
 -- called either from the default workspace or from a different
 -- workspace but having the same workspace passed as a parameter.
-local function ensure_valid_workspace(self, dao_factory, helpers)
+local function ensure_valid_workspace(self, helpers)
   local api_workspace = self.workspace
   local namespace_workspace = ngx.ctx.workspaces[1]
 
@@ -29,129 +28,109 @@ local function ensure_valid_workspace(self, dao_factory, helpers)
 end
 
 
+-- dev portal post-process: perform portal checks and create files
+-- for the created/updated workspace if needed
+local function portal_post_process(workspace, helpers)
+  local dao = singletons.dao
+
+  local workspace, err = crud.portal_crud.check_initialized(workspace, dao)
+  if err then
+    return helpers.yield_error(err)
+  end
+
+  return workspace
+end
+
+
 return {
-  ["/workspaces/"] = {
-    before = function(self, dao_factory, helpers)
+  ["/workspaces"] = {
+    before = function(self, _, helpers)
       -- FT-258
       if ngx.ctx.workspaces[1].name ~= workspaces.DEFAULT_WORKSPACE then
         helpers.responses.send_HTTP_NOT_FOUND()
       end
     end,
-    GET = function(self, dao_factory, helpers)
-      crud.paginated_set(self, dao_factory.workspaces)
-    end,
 
-    PUT = function(self, dao_factory)
-      crud.put(self.params, dao_factory.workspaces)
-    end,
-
-    POST = function(self, dao_factory, helpers)
-      crud.post(self.params, dao_factory.workspaces, function(workspace)
-        local workspace, err = crud.portal_crud.check_initialized(workspace, dao_factory)
-        if err then
-          return helpers.yield_error(err)
-        end
-
-        return workspace
-      end)
-    end,
+    POST = function(self, _, helpers, parent)
+      return parent(portal_post_process, helpers)
+    end
   },
 
-  ["/workspaces/:workspace_name_or_id"] = {
-    before = function(self, dao_factory, helpers)
-      self.params.workspace_name_or_id = ngx.unescape_uri(self.params.workspace_name_or_id)
-      crud.find_workspace_by_name_or_id(self, dao_factory, helpers)
-
-      ensure_valid_workspace(self, dao_factory, helpers)
+  ["/workspaces/:workspaces"] = {
+    before = function(self, db, helpers)
+      self.workspace = endpoints.select_entity(self, db, db["workspaces"].schema)
+      if not self.workspace then
+        return helpers.responses.send_HTTP_NOT_FOUND()
+      end
+      ensure_valid_workspace(self, helpers)
     end,
 
-    GET = function(self, dao_factory, helpers)
-      ensure_valid_workspace(self, dao_factory, helpers)
-      return helpers.responses.send_HTTP_OK(self.workspace)
-    end,
-
-    PATCH = function(self, dao_factory, helpers)
+    PATCH = function(self, _, helpers, parent)
       -- disallow changing workspace name
       if self.params.name and self.params.name ~= self.workspace.name then
         return helpers.responses.send_HTTP_BAD_REQUEST("Cannot rename a workspace")
       end
 
-      crud.patch(self.params, dao_factory.workspaces, self.workspace, function(workspace)
-        local workspace, err = crud.portal_crud.check_initialized(workspace, dao_factory)
-        if err then
-          return helpers.yield_error(err)
-        end
-
-        return workspace
-      end)
+      return parent(portal_post_process, helpers)
     end,
 
-    DELETE = function(self, dao_factory, helpers)
+    -- XXX PORTAL: why wasn't there a post_process for portal on PUT?
+
+    DELETE = function(self, db, helpers, parent)
       if self.workspace.name == workspaces.DEFAULT_WORKSPACE then
         return helpers.responses.send_HTTP_BAD_REQUEST("Cannot delete default workspace")
       end
 
-      local results, err = dao_factory.workspace_entities:find_all({
+      local results, err = workspaces.dao_wrappers.find_all(db.workspace_entities, {
+        workspace_id = self.workspace.id,
+      })
+      if err then
+        return helpers.yield_error(err)
+      end
+      if #results > 0 then
+        return helpers.responses.send_HTTP_BAD_REQUEST("Workspace is not empty")
+      end
+
+      return parent()
+    end,
+  },
+
+  ["/workspaces/:workspaces/entities"] = {
+    before = function(self, db, helpers)
+      self.workspace = endpoints.select_entity(self, db, db["workspaces"].schema)
+      if not self.workspace then
+        return helpers.responses.send_HTTP_NOT_FOUND()
+      end
+    end,
+
+    GET = function(self, db, helpers)
+      ensure_valid_workspace(self, helpers)
+
+      local entities, err = workspaces.dao_wrappers.find_all(db.workspace_entities, {
         workspace_id = self.workspace.id,
       })
       if err then
         return helpers.yield_error(err)
       end
 
-      if #results > 0 then
-        return helpers.responses.send_HTTP_BAD_REQUEST("Workspace is not empty")
-      end
-      crud.delete(self.workspace, dao_factory.workspaces)
-    end,
-  },
-
-  ["/workspaces/:workspace_name_or_id/entities"] = {
-    before = function(self, dao_factory, helpers)
-      self.params.workspace_name_or_id = ngx.unescape_uri(self.params.workspace_name_or_id)
-      crud.find_workspace_by_name_or_id(self, dao_factory, helpers)
+      return helpers.responses.send_HTTP_OK({
+        data = entities,
+        total = #entities,
+      })
     end,
 
-    GET = function(self, dao_factory, helpers)
-      ensure_valid_workspace(self, dao_factory, helpers)
-      if self.params.resolve then
-        local entities_hash = rbac.resolve_workspace_entities({ self.workspace.id })
-
-        local e = setmetatable({}, cjson.empty_array_mt)
-        for i = 1, #entities_hash do
-          e[i] = entities_hash[i]
-        end
-
-        return helpers.responses.send_HTTP_OK(e)
-      else
-        local entities, err = dao_factory.workspace_entities:find_all({
-          workspace_id = self.workspace.id,
-        })
-        if err then
-          return helpers.yield_error(err)
-        end
-
-        return helpers.responses.send_HTTP_OK({
-          data = entities,
-          total = #entities,
-        })
-      end
-    end,
-
-    POST = function(self, dao_factory, helpers)
-
+    POST = function(self, db, helpers)
       if not self.params.entities then
         return helpers.responses.send_HTTP_BAD_REQUEST("must provide >= entity")
       end
 
-
-      -- duplication check
-      local existing_entities, err = dao_factory.workspace_entities:find_all({
+      local dao = db.workspace_entities
+      local existing_entities, err = workspaces.dao_wrappers.find_all(dao, {
         workspace_id = self.workspace.id,
       })
       if err then
         return helpers.yield_error(err)
       end
-
 
       local entity_ids = utils.split(self.params.entities, ",")
 
@@ -170,21 +149,6 @@ return {
             return helpers.responses.send_HTTP_CONFLICT(err)
 
           end
-        end
-
-        -- circular reference check
-        local refs, err = dao_factory.workspace_entities:find_all({
-          workspace_id = e,
-          entity_id = self.workspace.id,
-        })
-        if err then
-          return helpers.yield_error(err)
-        end
-        if #refs > 0 then
-          local err = "Attempted to create circular reference (workspace " ..
-                      "'" .. e .. "' already references '" ..
-                      self.workspace.id .. "')"
-          return helpers.responses.send_HTTP_CONFLICT(err)
         end
       end
 
@@ -212,7 +176,7 @@ return {
       return helpers.responses.send_HTTP_CREATED(res)
     end,
 
-    DELETE = function(self, dao_factory, helpers)
+    DELETE = function(self, db, helpers)
       if not self.params.entities then
         return helpers.responses.send_HTTP_BAD_REQUEST("must provide >= entity")
       end
@@ -221,7 +185,8 @@ return {
 
       for i = 1, #entity_ids do
         local e = entity_ids[i]
-        local ws_e, err = dao_factory.workspace_entities:find_all({
+        local dao = db.workspace_entities
+        local ws_e, err = workspaces.dao_wrappers.find_all(dao, {
           workspace_id = self.workspace.id,
           entity_id = e,
         })
@@ -234,17 +199,21 @@ return {
         end
 
         for _, row in ipairs(ws_e) do
-          local _, err = dao_factory.workspace_entities:delete(row)
+          local _, err = db.workspace_entities:delete({
+            entity_id = row.entity_id,
+            workspace_id = row.workspace_id,
+            unique_field_name = row.unique_field_name,
+          })
           if err then
             return helpers.yield_error(err)
           end
         end
 
-        workspaces.inc_counter(dao_factory, self.workspace.id, ws_e[1].entity_type, { id = e }, -1)
+        workspaces.inc_counter(db, self.workspace.id, ws_e[1].entity_type, { id = e }, -1)
 
         -- find out if the entity is still in any workspace
         local rows
-        rows, err = dao_factory.workspace_entities:find_all({
+        rows, err = workspaces.dao_wrappers.find_all(dao, {
           entity_id = e, -- get the first result's entity_type;
                                            -- we can do that given the result in
                                            -- ws_e is for the same entity_id
@@ -260,7 +229,7 @@ return {
           local entity_type = ws_e[1].entity_type
 
           -- which dao is that entity part of?
-          local dao = dao_factory[entity_type] or singletons.db.daos[entity_type]
+          local dao = db[entity_type] or singletons.db.daos[entity_type]
 
           local _, err = workspaces.run_with_ws_scope({}, dao.delete, dao, {
             id = e,
@@ -275,14 +244,16 @@ return {
     end,
   },
 
-  ["/workspaces/:workspace_name_or_id/entities/:entity_id"] = {
-    before = function(self, dao_factory, helpers)
-      self.params.workspace_name_or_id = ngx.unescape_uri(self.params.workspace_name_or_id)
-      crud.find_workspace_by_name_or_id(self, dao_factory, helpers)
+  ["/workspaces/:workspaces/entities/:entity_id"] = {
+    before = function(self, db, helpers)
+      self.workspace = endpoints.select_entity(self, db, db["workspaces"].schema)
+      if not self.workspace then
+        return helpers.responses.send_HTTP_NOT_FOUND()
+      end
     end,
 
-    GET = function(self, dao_factory, helpers)
-      local e, err = dao_factory.workspace_entities:find_all({
+    GET = function(self, db, helpers)
+      local e, err = workspaces.dao_wrappers.find_all(db.workspace_entities, {
         workspace_id = self.workspace.id,
         entity_id = self.params.entity_id,
       })
@@ -297,12 +268,12 @@ return {
       e.unique_field_name = nil
       e.unique_field_value = nil
 
-      return e and helpers.responses.send_HTTP_OK(e) or
-        helpers.responses.send_HTTP_NOT_FOUND()
+      return e and helpers.responses.send_HTTP_OK(e)
+                or helpers.responses.send_HTTP_NOT_FOUND()
     end,
 
-    DELETE = function(self, dao_factory, helpers)
-      local e, err = dao_factory.workspace_entities:find_all({
+    DELETE = function(self, db, helpers)
+      local e, err = workspaces.dao_wrappers.find_all(db.workspace_entities, {
         workspace_id = self.workspace.id,
         entity_id = self.params.entity_id,
       })
@@ -314,7 +285,11 @@ return {
       end
 
       for _, row in ipairs(e) do
-        local _, err = dao_factory.workspace_entities:delete(row)
+        local _, err = db.workspace_entities:delete({
+          entity_id = row.entity_id,
+          workspace_id = row.workspace_id,
+          unique_field_name = row.unique_field_name,
+        })
         if err then
           return helpers.yield_error(err)
         end
@@ -323,15 +298,17 @@ return {
       return helpers.responses.send_HTTP_NO_CONTENT()
     end,
   },
-  ["/workspaces/:workspace_name_or_id/meta"] = {
-    before = function(self, dao_factory, helpers)
-      self.params.workspace_name_or_id =
-        ngx.unescape_uri(self.params.workspace_name_or_id)
-      crud.find_workspace_by_name_or_id(self, dao_factory, helpers)
-      ensure_valid_workspace(self, dao_factory, helpers)
+  ["/workspaces/:workspaces/meta"] = {
+    before = function(self, db, helpers)
+      self.workspace = endpoints.select_entity(self, db, db["workspaces"].schema)
+      if not self.workspace then
+        return helpers.responses.send_HTTP_NOT_FOUND()
+      end
+
+      ensure_valid_workspace(self, helpers)
     end,
 
-    GET = function(self, dao_factory, helpers)
+    GET = function(self, _, helpers)
       local counts, err = workspaces.counts(self.workspace.id)
       if not counts then
         helpers.responses.send_HTTP_INTERNAL_SERVER_ERROR(err)
