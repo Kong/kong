671beb1475963db27ebac1e80716fe82501e9d2a
Guilherme Salazar <gsz@acm.org>
2019-03-25 16:04:26 -0700

    chore(request-validator) update to new schema format

--- kong/plugins/request-validator/schema.lua
+++ kong/plugins/request-validator/schema.lua
@@ -1,5 +1,4 @@
 local NewErrors = require "kong.db.errors"
-local OldErrors = require "kong.dao.errors"
 local metaschema = require "kong.plugins.request-validator.metaschema"
 local utils = require "kong.plugins.request-validator.utils"
 
@@ -8,25 +7,39 @@ local gen_schema = utils.gen_schema
 
 
 return {
+  name = "request-validator",
+
   fields = {
-    body_schema = {
-      type = "string",
-      required = true,
-    }
+    { config = {
+        type = "record",
+        fields = {
+          { body_schema = {
+            type = "string",
+            required = true,
+          }},
+        },
+      }
+    },
   },
-  self_check = function(schema, plugin_t, dao, is_updating)
-    local schema, err = gen_schema(plugin_t.body_schema)
-    if err then
-      return false, OldErrors.schema(err)
-    end
 
-    -- validate against metaschema
-    local ok
-    ok, err = metaschema:validate(schema)
-    if not ok then
-      return false, NewErrors:schema_violation(err)
-    end
+  entity_checks = {
+    { custom_entity_check = {
+      field_sources = { "config" },
+      fn = function(entity)
+        local schema, err = gen_schema(entity.config.body_schema)
+        if err then
+          return false, err
+        end
 
-    return true
-  end
+        -- validate against metaschema
+        local ok
+        ok, err = metaschema:validate(schema)
+        if not ok then
+          return false, NewErrors:schema_violation(err)
+        end
+
+      return true
+    end
+    }},
+  }
 }
