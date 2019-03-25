6da5639d77bfab5192aeadf31e050d7a85042860
Guilherme Salazar <gsz@acm.org>
2019-03-25 15:15:07 -0700

    chore(rate-limiting-advanced) update to new schema format

--- kong/plugins/rate-limiting-advanced/schema.lua
+++ kong/plugins/rate-limiting-advanced/schema.lua
@@ -1,19 +1,4 @@
-local Errors = require "kong.dao.errors"
-local utils  = require "kong.tools.utils"
 local redis  = require "kong.enterprise_edition.redis"
-local errors = require "kong.dao.errors"
-
-local function validate_rl(value)
-  for i = 1, #value do
-    local x = tonumber(value[i])
-
-    if not x then
-      return false, "size/limit values must be numbers"
-    end
-  end
-
-  return true
-end
 
 
 local function check_shdict(name)
@@ -26,95 +11,70 @@ end
 
 
 return {
+  name = "rate-limiting-advanced",
   fields = {
-    identifier = {
-      type = "string",
-      enum = { "ip", "credential", "consumer" },
-      required = true,
-      default = "consumer",
-    },
-    window_size = {
-      type = "array",
-      required = true,
-      func = validate_rl,
-    },
-    window_type = {
-      type = "string",
-      enum = { "fixed", "sliding" },
-      default = "sliding",
-    },
-    limit = {
-      type = "array",
-      required = true,
-      func = validate_rl,
-    },
-    sync_rate = {
-      type = "number",
-      required = true,
-    },
-    namespace = {
-      type = "string",
-      default = utils.random_string,
-    },
-    strategy = {
-      type = "string",
-      enum = { "cluster", "redis", },
-      required = true,
-      default = "cluster",
-    },
-    redis = {
-      type = "table",
-      schema = redis.config_schema,
-    },
-    dictionary_name = {
-      type = "string",
-      required = true,
-      default = "kong_rate_limiting_counters",
-    },
-    hide_client_headers = {
-      type = "boolean",
-      default = false,
-    },
+    { config = {
+        type = "record",
+        fields = {
+          { identifier = {
+            type = "string",
+            one_of = { "ip", "credential", "consumer" },
+            default = "consumer",
+            required = true,
+          }},
+          { window_size = {
+            type = "array",
+            elements = {
+              type = "number",
+            },
+            required = true,
+          }},
+          { window_type = {
+            type = "string",
+            one_of = { "fixed", "sliding" },
+            default = "sliding",
+          }},
+          { limit = {
+            type = "array",
+            elements = {
+              type = "number",
+            },
+            required = true,
+          }},
+          { sync_rate = {
+            type = "number",
+            required = true,
+          }},
+          { namespace = {
+            type = "string",
+            auto = true,
+          }},
+          { strategy = {
+            type = "string",
+            one_of = { "cluster", "redis", },
+            default = "cluster",
+            required = true,
+          }},
+          { dictionary_name = {
+            type = "string",
+            default = "kong_rate_limiting_counters",
+            required = true,
+          }},
+          { hide_client_headers = {
+            type = "boolean",
+            default = false,
+          }},
+          { redis = redis.config_schema},
+        },
+      },
+    }
   },
-  self_check = function(schema, plugin_t, dao, is_updating)
-    if plugin_t.dictionary_name ~= nil then
-      local ok, err = check_shdict(plugin_t.dictionary_name)
-      if not ok then
-        return false, errors.schema(err)
-      end
-    end
 
-    -- empty semi-optional redis config, needs to be cluster strategy
-    if plugin_t.strategy == "redis" then
-      if not plugin_t.redis then
-        return false, Errors.schema("No redis config provided")
-      end
-    end
+  check = function(entity)
+    local config = entity.config
 
-    -- on update we dont need to enforce re-defining window_size and limit
-    -- e.g., skip the next checks if this is an update and the request body
-    -- did not contain either the window_size or limit values. the reason for
-    -- this is the check that is executing here looks at the request body, not
-    -- the entity post-update. so to simplify PATCH requests we do not want to
-    -- force the user to define window_size and limit when they don't need to
-    -- (say, they are only updating sync_rate)
-    --
-    -- if this request is an update, and window_size and/or limit are being
-    -- updated, then we do want to execute the manual checks on these entities.
-    -- in such case the condition below evaluates to false, and we fall through
-    -- to the next series of checks
-    if is_updating and not plugin_t.window_size and not plugin_t.limit then
+    if not config.limit or not config.window_size then
       return true
-    end
-
-    if not plugin_t.window_size or not plugin_t.limit then
-      return false, Errors.schema(
-                    "Both window_size and limit must be provided")
-    end
-
-    if #plugin_t.window_size ~= #plugin_t.limit then
-      return false, Errors.schema(
-                    "You must provide the same number of windows and limits")
     end
 
     -- sort the window_size and limit arrays by limit
@@ -123,17 +83,41 @@ return {
     -- of this array of pairs. finally, we re-assign the plugin_t configuration
     -- elements directly based off the sorted temp table
     local t = {}
-    for i, v in ipairs(plugin_t.limit) do
-      t[i] = { plugin_t.limit[i], plugin_t.window_size[i] }
+    for i, v in ipairs(config.limit) do
+      t[i] = { config.limit[i], config.window_size[i] }
     end
 
     table.sort(t, function(a, b) return tonumber(a[1]) < tonumber(b[1]) end)
 
     for i = 1, #t do
-      plugin_t.limit[i] = tonumber(t[i][1])
-      plugin_t.window_size[i] = tonumber(t[i][2])
+      config.limit[i] = tonumber(t[i][1])
+      config.window_size[i] = tonumber(t[i][2])
     end
 
     return true
   end,
+
+  entity_checks = {
+    { custom_entity_check = {
+      field_sources = { "config" },
+      fn = function(entity)
+        if entity.config.strategy == "memory" then
+          local ok, err = check_shdict(entity.config.dictionary_name)
+          if not ok then
+            return nil, err
+          end
+        end
+
+        if #entity.config.window_size ~= #entity.config.limit then
+          return nil, "You must provide the same number of windows and limits"
+        end
+
+        return true
+      end
+    }},
+    { conditional = {
+      if_field = "config.strategy", if_match = { eq = "redis" },
+      then_field = "config.redis.host", then_match = { required = true },
+    }},
+  },
 }
