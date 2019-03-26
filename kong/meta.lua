1701c80e314088658200b7c1880a42720b9e9ed4
Hisham Muhammad <hisham@gobolinux.org>
2019-03-26 17:04:07 -0400

    release: 1.1.0
    Signed-off-by: Hisham Muhammad <hisham@gobolinux.org>

--- kong/meta.lua
+++ kong/meta.lua
@@ -2,7 +2,7 @@ local version = setmetatable({
   major = 1,
   minor = 1,
   patch = 0,
-  suffix = "rc2",
+  --suffix = "",
 }, {
   __tostring = function(t)
     return string.format("%d.%d.%d%s", t.major, t.minor, t.patch,
