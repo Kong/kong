Kong/kong

commit ca077e8d8d668495c18cece338567f1c97981f8f

refactor(dao) call delete_entity_relation from the dao
Avoid calling said function from multiple places.

kong/db/dao/init.lua
@@ -632,6 +632,19 @@ local function generate_foreign_key_methods(schema)
           return nil, tostring(err_t), err_t
         end
 
+        -- XXX workspaces
+        local err = workspaces.delete_entity_relation(self.schema.name, entity)
+        if err then
+          return nil, self.errors:database_error("could not delete Route relationship " ..
+          "with Workspace: " .. err)
+        end
+
+        err = rbac.delete_role_entity_permission("routes", entity)
+        if err then
+          return nil, self.errors:database_error("could not delete Route relationship " ..
+          "with Role: " .. err)
+        end
+
         self:post_crud_event("delete", entity)
         propagate_cascade_delete_events(cascade_entries)
 
@@ -1114,6 +1127,19 @@ function DAO:delete(primary_key, options)
   _, err_t = self.strategy:delete(primary_key, options)
   if err_t then
     return nil, tostring(err_t), err_t
+  end
+
+  -- XXX workspaces
+  local err = workspaces.delete_entity_relation(self.schema.name, entity)
+  if err then
+    return nil, self.errors:database_error("could not delete Route relationship " ..
+    "with Workspace: " .. err)
+  end
+
+  err = rbac.delete_role_entity_permission("routes", entity)
+  if err then
+    return nil, self.errors:database_error("could not delete Route relationship " ..
+    "with Role: " .. err)
   end
 
   self:post_crud_event("delete", entity)

kong/db/strategies/cassandra/routes.lua
@@ -73,20 +73,6 @@ function _Routes_ee:delete(primary_key, options)
     end
   end
 
-  if ok and ws then
-    local err = workspaces.delete_entity_relation("routes", primary_key)
-    if err then
-      return nil, self.errors:database_error("could not delete Route relationship " ..
-                                             "with Workspace: " .. err)
-    end
-
-    err = rbac.delete_role_entity_permission("routes", primary_key)
-    if err then
-      return nil, self.errors:database_error("could not delete Route relationship " ..
-                                             "with Role: " .. err)
-    end
-  end
-
   return true, nil, primary_key
 end
 

kong/db/strategies/cassandra/services.lua
@@ -172,20 +172,6 @@ function _Services_ee:delete(primary_key, options)
   local ok2, err2 = delete_cascade_ws(connector, "oauth2_tokens", oauth2_tokens_list, errors, ws)
   local ok3, err3 = delete_cascade_ws(connector, "oauth2_authorization_codes", oauth2_codes_list, errors, ws)
 
-  if ok and ws then
-    local err = workspaces.delete_entity_relation("services", primary_key)
-    if err then
-      return nil, self.errors:database_error("could not delete Service relationship " ..
-                                             "with Workspace: " .. err)
-    end
-
-    err = rbac.delete_role_entity_permission("services", primary_key)
-    if err then
-      return nil, self.errors:database_error("could not delete Service relationship " ..
-                                             "with Role: " .. err)
-    end
-  end
-
   return ok1 and ok2 and ok3, err1 or err2 or err3, primary_key
 end
 

kong/db/strategies/postgres/routes.lua
@@ -64,20 +64,6 @@ function _Routes_ee:delete(primary_key, options)
     end
   end
 
-  if ok and ws then
-    local err = workspaces.delete_entity_relation("routes", {id = primary_key.id})
-    if err then
-      return nil, self.errors:database_error("could not delete Route relationship " ..
-                                             "with Workspace: " .. err)
-    end
-
-    err = rbac.delete_role_entity_permission("routes", {id = primary_key.id})
-    if err then
-      return nil, self.errors:database_error("could not delete Route relationship " ..
-                                             "with Role: " .. err)
-    end
-  end
-
   return true, nil, primary_key
 end
 

kong/db/strategies/postgres/services.lua
@@ -97,21 +97,6 @@ function _Services_ee:delete(primary_key, options)
     return false, err1 or err2 or err3
   end
 
-  -- delete workspace relationship
-  if ok and ws then
-    local err = workspaces.delete_entity_relation("services", primary_key)
-    if err then
-      return nil, self.errors:database_error("could not delete Services relationship " ..
-                                             "with Workspace: " .. err)
-    end
-
-    err = rbac.delete_role_entity_permission("services", primary_key)
-    if err then
-      return nil, self.errors:database_error("could not delete Services relationship " ..
-                                             "with Role: " .. err)
-    end
-  end
-
   return true, nil, primary_key
 end
 

commit b12bc2837fa9aae03031e649f0e0cc48ed26f9c5

feat(rbac) make dao :each and :delete rbac aware
* By calling self.page instead of strategy.page searches go through the
rbac validation.

* Adding rbac checks to delete to find rbac conflicts on cascade
  entities.

* Fix typo in delete_role_entity_permission to work with foreign keys
  that are primary keys on the related table. Also fix a typo making
  cascade only work for routes (introduced in
  ca077e8d8d668495c18cece338567f1c97981f8f)

* Add `skip_rbac = true` on call sites that need it and end up calling
  each. Now it has to be explicit.

kong/db/dao/init.lua
@@ -217,6 +217,34 @@ local function check_update(self, key, entity, options, name)
 end
 
 
+-- Iterator wrapper that applies rbac to any dao iterator unless
+-- skip_rbac is passed.
+local function rbacced_iterator(iterator, self, options)
+  if options and options.skip_rbac then
+    return iterator
+  end
+
+  return function()
+    local res, t, page = iterator()
+    if not res then
+      return res, t, page
+    end
+
+    local valid = rbac.validate_entity_operation(res, self.schema.name)
+    while not valid do
+      res, t, page = iterator()
+      if not res then
+        return res, t, page
+      end
+
+      valid = rbac.validate_entity_operation(res, self.schema.name)
+    end
+
+    return res, t, page
+  end
+end
+
+
 local function find_cascade_delete_entities(self, entity)
   local constraints = self.schema:get_constraints()
   local entries = {}
@@ -368,7 +396,7 @@ local function generate_foreign_key_methods(schema)
           return strategy[page_method_name](strategy, foreign_key, size, offset, options)
         end
 
-        return iteration.by_row(self, pager, size)
+        return rbacced_iterator(iteration.by_row(self, pager, size), self, options)
       end
 
     elseif field.unique or schema.endpoint_key == name then
@@ -896,6 +924,13 @@ function DAO:each(size, options)
 end
 
 
+do
+  local each = DAO.each
+  DAO.each = function(self, size, options)
+    return rbacced_iterator(each(self, size, options), self, options)
+  end
+end
+
 function DAO:insert(entity, options)
   validate_entity_type(entity)
 
@@ -1198,6 +1233,17 @@ function DAO:delete(primary_key, options)
 
   local cascade_entries = find_cascade_delete_entities(self, primary_key)
 
+  if (not options or not options.skip_rbac) and  -- not skipping rbac
+    (not rbac.validate_entity_operation(entity, self.schema.name) or
+     not rbac.check_cascade(cascade_entries, ngx.ctx.rbac))  then
+      -- operation or cascading not allowed
+      local err_t = self.errors:unauthorized_operation({
+        username = ngx.ctx.rbac.user.name,
+        action = rbac.readable_action(ngx.ctx.rbac.action)
+      })
+      return nil, tostring(err_t), err_t
+  end
+
   local _
   _, err_t = self.strategy:delete(primary_key, options)
   if err_t then
@@ -1211,9 +1257,9 @@ function DAO:delete(primary_key, options)
     "with Workspace: " .. err)
   end
 
-  err = rbac.delete_role_entity_permission("routes", entity)
+  err = rbac.delete_role_entity_permission(self.schema.name, entity)
   if err then
-    return nil, self.errors:database_error("could not delete Route relationship " ..
+    return nil, self.errors:database_error("could not delete entity relationship " ..
     "with Role: " .. err)
   end
 

kong/rbac/init.lua
@@ -106,11 +106,11 @@ local function retrieve_relationship_ids(entity_id, entity_name, factory_key)
   if factory_key == "workspace_entities" then
     relationship_ids, err = workspaces.compat_find_all(factory_key, {
       [entity_name .. "_id"] = entity_id
-    })
+    }, { skip_rbac = true })
   else
     relationship_ids, err = workspaces.compat_find_all(factory_key, {
       [entity_name] = { id = entity_id }
-    })
+    }, { skip_rbac = true })
   end
   if err then
     log(ngx.ERR, "err retrieving relationship via id ", entity_id, ": ", err)
@@ -327,6 +327,8 @@ end
 
 -- XXX EE should return 2nd value for error
 local function get_role_entities(db, role, opts)
+  opts = opts or {}
+  opts.skip_rbac = true
   local res = {}
   for role in db.rbac_role_entities:each_for_role({id = role.id}, nil,  opts) do
     table.insert(res, role)
@@ -337,6 +339,8 @@ end
 _M.get_role_entities = get_role_entities
 
 local function get_role_endpoints(db, role, opts)
+  opts = opts or {}
+  opts.skip_rbac = true
   local res = {}
   for role in db.rbac_role_endpoints:each_for_role({id = role.id}, nil, opts) do
     table.insert(res, role)
@@ -639,11 +643,17 @@ local function delete_role_entity_permission(table_name, entity)
   end
 
   local entity_id = schema.primary_key[1]
+  if schema.fields[entity_id].type == "foreign" then
+    entity_id = entity_id .. "_id"
+  end
 
-  local role_entities = db.rbac_role_entities:select_all({
+  local role_entities, err, err_t = db.rbac_role_entities:select_all({
     entity_id = entity[entity_id],
     entity_type = table_name
   })
+  if err then
+    return err_t
+  end
 
   for _, role_entity in ipairs(role_entities) do
     local _, err, err_t = db.rbac_role_entities:delete({

kong/workspaces/dao_wrappers.lua
@@ -12,10 +12,14 @@ end
 
 -- Temporary wrapper around :each implementing arbitrary filtering,
 -- used throughout workspaces implementation
-function _M.compat_find_all(dao_name, filt)
+function _M.compat_find_all(dao_name, filt, opts)
   local dao = singletons.db[dao_name]
 
   filt = filt or {}
+  if opts then
+    opts.skip_rbac = opts.skip_rbac or filt.__skip_rbac
+  end
+
   filt.__skip_rbac = nil -- remove skip_rbac from here
 
   local rows = {}
@@ -25,7 +29,7 @@ function _M.compat_find_all(dao_name, filt)
     filtering = true
   end
 
-  for row, err in dao:each() do
+  for row, err in dao:each(nil, opts) do
     if err then
       return nil, err
     end

spec-ee/02-integration/08-new-dao/02-rbac_iterator.lua
@@ -1,0 +1,44 @@
+local rbac = require "kong.rbac"
+local helpers        = require "spec.helpers"
+
+
+describe("each():", function()
+  local bp, db
+  lazy_setup(function()
+    bp, db  = helpers.get_db_utils("postgres")
+    for i=1, 3 do
+      bp.services:insert()
+    end
+  end)
+
+  it("calls rbac validate function", function()
+    spy.on(rbac, "validate_entity_operation")
+    for row, err in db.services:each() do end
+    assert.spy(rbac.validate_entity_operation).was_called(3)
+  end)
+
+  it("does not call rbac validate function when skip_rbac = true =", function()
+    spy.on(rbac, "validate_entity_operation")
+    for row, err in db.services:each(nil, {skip_rbac = true}) do end
+    assert.spy(rbac.validate_entity_operation).was_called(0)
+  end)
+
+  it("obeys rbac.validate_entity_operation", function()
+    local old_val = rbac.validate_entity_operation
+
+    local test_cases = {{ true, 3 }, { false, 0 }}
+
+    for _, tc in ipairs(test_cases) do
+      rbac.validate_entity_operation = function() return tc[1] end
+      local count = 0
+      for row, err in db.services:each() do
+        count = count + 1
+      end
+
+      assert.equal(count, tc[2])
+    end
+
+    rbac.validate_entity_operation = old_val
+  end)
+
+end)