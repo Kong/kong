ef032676d65372d1a809fd1f2c8c62750f4158f3
eskerda <eskerda@gmail.com>
2019-04-08 15:41:38 +0200

    chore(collector) upgrade schema to new dao

--- kong/plugins/brain/schema.lua
+++ kong/plugins/brain/schema.lua
@@ -1,15 +1,21 @@
 return {
+  name = "brain",
   fields = {
-    environment = {type = "string"},
-    retry_count = {type = "number", default = 10},
-    queue_size = {type = "number", default = 1000},
-    flush_timeout = {type = "number", default = 2},
-    log_bodies = {type = "boolean", default = false},
-    service_token = {type = "string", required = true},
-    connection_timeout = {type = "number", default = 30},
-    host = {type = "string", required = true, default = "collector.brain.kong.com"},
-    port = {type = "number", required = true, default = 443},
-    https = {type = "boolean", default = true},
-    https_verify = {type = "boolean", default = false}
-  }
+    { config = {
+      type = "record",
+      fields = {
+        { environment = { type = "string" } },
+        { retry_count = { type = "number", default = 10 } },
+        { queue_size = { type = "number", default = 1000 } },
+        { flush_timeout = { type = "number", default = 2 } },
+        { log_bodies = { type = "boolean", default = false } },
+        { service_token = { type = "string", required = true } },
+        { connection_timeout = { type = "number", default = 30 } },
+        { host = { type = "string", required = true, default = "collector.brain.kong.com" } },
+        { port = { type = "number", required = true, default = 443 } },
+        { https = { type = "boolean", default = true } },
+        { https_verify = { type = "boolean", default = false } },
+      },
+    }},
+  },
 }
