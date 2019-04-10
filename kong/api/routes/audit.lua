9c6e87644e472db5641db8fd5c6f44aca27c9373
Guilherme Salazar <gsz@acm.org>
2019-04-10 12:37:13 -0700

    fix(audit) next page prefix
    Correctly set `next` prefix, for request audit and object endpoints.
    Previously to this, it was incorrectly hardcoded to `/audit/requests`.

--- kong/api/routes/audit.lua
+++ kong/api/routes/audit.lua
@@ -15,7 +15,7 @@ end
 
 -- Copy of `kong.api.endpoints.get_collection_endpoint`
 -- Needed as the endpoint name differs from the entity's schema
-local function get_collection_endpoint(schema)
+local function get_collection_endpoint(schema, next_page_prefix)
 
   return function(self, db, helpers)
     local args = self.args.uri
@@ -30,7 +30,8 @@ local function get_collection_endpoint(schema)
       return endpoints.handle_error(err_t)
     end
 
-    local next_page = offset and fmt("/audit/requests?offset=%s",
+    local next_page = offset and fmt("/%s?offset=%s",
+                                     next_page_prefix,
                                      escape_uri(offset))
                               or null
 
@@ -46,14 +47,16 @@ return {
   ["/audit/requests"] = {
     schema = kong.db.audit_requests.schema,
     methods = {
-      GET = get_collection_endpoint(kong.db.audit_requests.schema),
+      GET = get_collection_endpoint(kong.db.audit_requests.schema,
+                                    "audit/requests"),
     }
   },
 
   ["/audit/objects"] = {
     schema = kong.db.audit_requests.schema,
     methods = {
-      GET = get_collection_endpoint(kong.db.audit_objects.schema),
+      GET = get_collection_endpoint(kong.db.audit_objects.schema,
+                                    "audit/objects"),
     }
   },
 }
