ad1abdfab9de6bd738a28a853d619111f0434316
Guilherme Salazar <gsz@acm.org>
2019-04-10 12:41:11 -0700

    tests(integration) add audit log paging tests

---
+++ spec-ee/02-integration/07-audit-log/02-audit_log_api_spec.lua
@@ -0,0 +1,104 @@
+local helpers = require "spec.helpers"
+local cjson = require "cjson"
+
+
+for _, strategy in helpers.each_strategy() do
+  describe("audit_log API with #" .. strategy, function()
+    local admin_client
+
+    setup(function()
+      helpers.get_db_utils(strategy)
+
+      assert(helpers.start_kong({
+        database   = strategy,
+        nginx_conf = "spec/fixtures/custom_nginx.template",
+        audit_log  = "on",
+        audit_log_ignore_paths = "/audit/requests",
+        audit_log_ignore_tables = "workspace_entities",
+      }))
+    end)
+
+    teardown(function()
+      helpers.stop_kong()
+    end)
+
+    before_each(function()
+      admin_client = helpers.admin_client()
+    end)
+
+    after_each(function()
+      admin_client:close()
+    end)
+
+    describe("audit requests", function()
+      -- Assert paging behavior - given we have custom logic for paging in
+      -- audit endpoints
+      it("returns paged results", function()
+        assert.res_status(200, admin_client:get("/services"))
+        assert.res_status(200, admin_client:get("/services"))
+        assert.res_status(200, admin_client:get("/services"))
+
+        local res, json
+
+        res = assert.res_status(200, admin_client:send({
+          path = "/audit/requests",
+          query = {size = 2}
+        }))
+        json = cjson.decode(res)
+        assert.same(2, #json.data)
+
+        assert.matches("^/audit/requests", json.next)
+
+        res = assert.res_status(200, admin_client:send({
+          path = "/audit/requests",
+          query = {size = 2, offset = json.offset}
+        }))
+        json = cjson.decode(res)
+        assert.same(1, #json.data)
+      end)
+    end)
+
+    describe("audit objects", function()
+      -- Assert paging behavior - given we have custom logic for paging in
+      -- audit endpoints
+      it("returns paged results", function()
+        assert.res_status(201, admin_client:post("/consumers", {
+          body = {
+            username = "c1"
+          },
+          headers = {["Content-Type"] = "application/json"}
+        }))
+        assert.res_status(201, admin_client:post("/consumers", {
+          body = {
+            username = "c2"
+          },
+          headers = {["Content-Type"] = "application/json"}
+        }))
+        assert.res_status(201, admin_client:post("/consumers", {
+          body = {
+            username = "c3"
+          },
+          headers = {["Content-Type"] = "application/json"}
+        }))
+
+        local res, json
+
+        res = assert.res_status(200, admin_client:send({
+          path = "/audit/objects",
+          query = {size = 2}
+        }))
+        json = cjson.decode(res)
+        assert.same(2, #json.data)
+
+        assert.matches("^/audit/objects", json.next)
+
+        res = assert.res_status(200, admin_client:send({
+          path = "/audit/objects",
+          query = {size = 2, offset = json.offset}
+        }))
+        json = cjson.decode(res)
+        assert.same(1, #json.data)
+      end)
+    end)
+  end)
+end
