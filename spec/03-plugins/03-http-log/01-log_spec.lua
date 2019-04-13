4ac3b4bf36fd1844ce2e7ebbb695845d984fd0eb
Enrique García Cota <kikito@gmail.com>
2019-04-13 03:27:04 +0200

    fix(plugins) simplify logging in http-log
    This commit replaces the plugins/log-buffering folder with a single
    kong/tools/batch_queue.lua file, fixing one bug in http-log along the way.
    
    Bug: the old implemenation of buffers used a module-level global
    table called retry_delays to store delays per-buffer, using the buffer ids
    (a string) as key on that table. The http-log module created many buffers
    (one per route) bug gave all the same name (`http-log`). As a result, the
    `retry_delays` value was overwritten by many buffers. This could lead to
    loss of data.
    
    Bug: send_delay config option was ignored (was reset to 0 by the plugin)
    when creating buffers. The new implementation does not reset it to 0.
    
    Highlights of current refactor:
    
    * Renamed "LogBuffer" to "BatchQueue", and "send" to "process".
    * Now there is a single queue "per plugin config", instead of one buffer
      "per route". This is more efficient on resources, as it creates less
      queues and shares timers.
    * The module-level `retry_delays` variable was replaced by an instance-level
      variable: self.retry_delays
    * As a result, queues don't need to have names any more.
    * BatchQueue has only 1 mandatory parameter, called `process`. This replaces
      the old "sender" abstraction.
    * The optional "opts" parameter can be used to provide configuration. Two
      options have been renamed (they remain unchanged in the http-log
      config for backwards compatibility):
      * `queue_size` is now `max_batch_size`
      * `send_delay` is now `process_delay`
    * The "producers" abstraction has been replaced by `self.current_batch`
    * Queues do not concern themselves with byte sizes any more, they only deal
      with "entities", in the abstract. The 2MB size limit has been removed.
    * Compacting several entities into a single body/payload" (i.e. for sending)
      is now a responsibility of the `process` function. Previously this was
      done in the "producers". Notice how the `process` function is created
      in the http-log handler.
    * `add_entry` was renamed to `add`
    * `self.timer_flush_pending` was renamed to `self.flush_scheduled` and
      `self.timer_send_pending` was renamed to `self.process_scheduled`, to
      better reflect how they function
    * `delayed_flush` was renamed to `flush` and `run_sender` was renamed
      to `process`.
    * The `adjust_retries` private function was merged into the `send`
      private function.
    * The public method `Buffer:send` was merged into the `Buffer:flush` method.
    * Logging is now done via `kong.log` in the plugin and via `ngx.log` in
      the queue. This is no longer a config option

--- spec/03-plugins/03-http-log/01-log_spec.lua
+++ spec/03-plugins/03-http-log/01-log_spec.lua
@@ -137,6 +137,9 @@ for _, strategy in helpers.each_strategy() do
         }
       }
 
+      local test_error_log_path = helpers.test_conf.nginx_err_logs
+      os.execute(":> " .. test_error_log_path)
+
       assert(helpers.start_kong({
         database = strategy,
         nginx_conf = "spec/fixtures/custom_nginx.template",
@@ -183,50 +186,6 @@ for _, strategy in helpers.each_strategy() do
           assert.same("127.0.0.1", log_message.client_ip)
           return true
         end
-      end, 10)
-    end)
-
-    it("logs to HTTP with a buffer", function()
-      for i = 1, 10 do
-        local client = helpers.proxy_client()
-        local res = assert(client:send({
-          method = "GET",
-          path = "/status/" .. tostring(200 + i),
-          headers = {
-            ["Host"] = "http_queue_logging.test"
-          }
-        }))
-        assert.res_status(200 + i, res)
-        client:close()
-      end
-
-      helpers.wait_until(function()
-        local client = assert(helpers.http_client(helpers.mock_upstream_host, helpers.mock_upstream_port))
-        local res = assert(client:send {
-          method  = "GET",
-          path    = "/read_log/http_queue",
-          headers = {
-            Accept = "application/json"
-          }
-        })
-        local raw = assert.res_status(200, res)
-        local body = cjson.decode(raw)
-        if #body.log.entries ~= 2 then
-          return false
-        end
-        local status = 200
-        for _, entry in ipairs(body.log.entries) do
-          local json = cjson.decode(entry.request.postData.text)
-          if #json ~= 5 then
-            return false
-          end
-          for _, item in ipairs(json) do
-            assert.same("127.0.0.1", item.client_ip)
-            status = status + 1
-            assert.same(status, item.response.status)
-          end
-        end
-        return true
       end, 10)
     end)
 
@@ -283,10 +242,9 @@ for _, strategy in helpers.each_strategy() do
         local found = false
 
         for line in logs:gmatch("[^\r\n]+") do
-          if line:find("send(): failed request to " ..
+          if line:find("failed to process entries: .* " ..
                        helpers.mock_upstream_ssl_host .. ":" ..
-                       helpers.mock_upstream_ssl_port .. ": timeout",
-                       0, true)
+                       helpers.mock_upstream_ssl_port .. ": timeout")
           then
             found = true
 
@@ -298,7 +256,7 @@ for _, strategy in helpers.each_strategy() do
         if found then
             return true
         end
-      end, 0.2)
+      end, 2)
     end)
 
     it("adds authorization if userinfo is present", function()
@@ -333,7 +291,276 @@ for _, strategy in helpers.each_strategy() do
     end)
   end)
 
-  describe("enabled globally", function()
+
+  -- test both with a single worker for a deterministic test,
+  -- and with multiple workers for a concurrency test
+  for _, workers in ipairs({1, 4}) do
+    describe("Plugin: http-log (log) queue (worker_processes = " .. workers .. ") [#" .. strategy .. "]", function()
+      local proxy_client
+
+      lazy_setup(function()
+        local bp = helpers.get_db_utils(strategy, {
+          "routes",
+          "services",
+          "plugins",
+        })
+
+        local service = bp.services:insert{
+          protocol = "http",
+          host     = helpers.mock_upstream_host,
+          port     = helpers.mock_upstream_port,
+        }
+
+        local route = bp.routes:insert {
+          hosts   = { "http_queue_logging.test" },
+          service = service
+        }
+
+        bp.plugins:insert {
+          route = { id = route.id },
+          name     = "http-log",
+          config   = {
+            queue_size = 5,
+            http_endpoint = "http://" .. helpers.mock_upstream_host
+                                      .. ":"
+                                      .. helpers.mock_upstream_port
+                                      .. "/post_log/http_queue"
+          }
+        }
+
+        local route2 = bp.routes:insert {
+          hosts   = { "http_queue_logging2.test" },
+          service = service
+        }
+
+        bp.plugins:insert {
+          route = { id = route2.id },
+          name     = "http-log",
+          config   = {
+            queue_size = 5,
+            http_endpoint = "http://" .. helpers.mock_upstream_host
+                                      .. ":"
+                                      .. helpers.mock_upstream_port
+                                      .. "/post_log/http_queue2"
+          }
+        }
+
+        assert(helpers.start_kong({
+          database = strategy,
+          nginx_worker_processes = workers,
+        }))
+
+        assert(helpers.start_kong({
+          database = strategy,
+          prefix = "servroot2",
+          admin_listen = "127.0.0.1:9010",
+          proxy_listen = "127.0.0.1:9011",
+          stream_listen = "off",
+          nginx_conf = "spec/fixtures/custom_nginx.template",
+          nginx_worker_processes = 1,
+        }))
+      end)
+
+      lazy_teardown(function()
+        helpers.stop_kong()
+        helpers.stop_kong("servroot2")
+      end)
+
+      before_each(function()
+        proxy_client = helpers.proxy_client()
+      end)
+
+      after_each(function()
+        if proxy_client then
+          proxy_client:close()
+        end
+      end)
+
+
+      local function reset_log(logname)
+        local client = assert(helpers.http_client(helpers.mock_upstream_host, helpers.mock_upstream_port))
+        assert(client:send {
+          method  = "GET",
+          path    = "/reset_log/" .. logname,
+          headers = {
+            Accept = "application/json"
+          }
+        })
+        client:close()
+      end
+
+
+      it("logs to HTTP with a buffer", function()
+        reset_log("http_queue")
+
+        local n = 1000
+        for i = 1, n do
+          local client = helpers.proxy_client()
+          local res = assert(client:send({
+            method = "GET",
+            path = "/status/" .. tostring(200 + (i % 10)),
+            headers = {
+              ["Host"] = "http_queue_logging.test"
+            }
+          }))
+          assert.res_status(200 + (i % 10), res)
+          client:close()
+        end
+
+        helpers.wait_until(function()
+          local client = assert(helpers.http_client(helpers.mock_upstream_host, helpers.mock_upstream_port))
+          local res = assert(client:send {
+            method  = "GET",
+            path    = "/read_log/http_queue",
+            headers = {
+              Accept = "application/json"
+            }
+          })
+          local raw = assert.res_status(200, res)
+          local body = cjson.decode(raw)
+
+          -- we only get an exact count with workers == 1
+          if workers == 1 and #body.log.entries ~= math.ceil(n / 5) then
+            return false
+          end
+
+          local i = 0
+          for _, entry in ipairs(body.log.entries) do
+            local json = cjson.decode(entry.request.postData.text)
+
+            -- we only get an exact split with workers == 1
+            if workers == 1 and #json ~= 5 then
+              return false
+            end
+            for _, item in ipairs(json) do
+              assert.same("127.0.0.1", item.client_ip)
+
+              -- we only get a deterministic order with workers == 1
+              if workers == 1 then
+                assert.same(200 + ((i + 1) % 10), item.response.status)
+              end
+              i = i + 1
+            end
+          end
+          return i == n
+        end, 15)
+      end)
+
+      it("does not mix buffers", function()
+        reset_log("http_queue")
+        reset_log("http_queue2")
+
+        local n = 1000
+        for i = 1, n do
+          local client = helpers.proxy_client()
+          local res = assert(client:send({
+            method = "GET",
+            path = "/status/" .. tostring(200 + (i % 10)),
+            headers = {
+              ["Host"] = "http_queue_logging.test"
+            }
+          }))
+          assert.res_status(200 + (i % 10), res)
+          client:close()
+
+          client = helpers.proxy_client()
+          res = assert(client:send({
+            method = "GET",
+            path = "/status/" .. tostring(300 + (i % 10)),
+            headers = {
+              ["Host"] = "http_queue_logging2.test"
+            }
+          }))
+          assert.res_status(300 + (i % 10), res)
+          client:close()
+        end
+
+        helpers.wait_until(function()
+          local client = assert(helpers.http_client(helpers.mock_upstream_host, helpers.mock_upstream_port))
+          local res = assert(client:send {
+            method  = "GET",
+            path    = "/read_log/http_queue",
+            headers = {
+              Accept = "application/json"
+            }
+          })
+          local raw = assert.res_status(200, res)
+          local body = cjson.decode(raw)
+
+          local client2 = assert(helpers.http_client(helpers.mock_upstream_host, helpers.mock_upstream_port))
+          local res2 = assert(client2:send {
+            method  = "GET",
+            path    = "/read_log/http_queue2",
+            headers = {
+              Accept = "application/json"
+            }
+          })
+          local raw2 = assert.res_status(200, res2)
+          local body2 = cjson.decode(raw2)
+
+          -- we only get an exact count with workers == 1
+          if workers == 1 and #body.log.entries ~= math.ceil(n / 5) then
+            return false
+          end
+
+          -- we only get an exact count with workers == 1
+          if workers == 1 and #body2.log.entries ~= math.ceil(n / 5) then
+            return false
+          end
+
+          local i = 0
+          for _, entry in ipairs(body.log.entries) do
+            local json = cjson.decode(entry.request.postData.text)
+
+            -- we only get an exact split with workers == 1
+            if workers == 1 and #json ~= 5 then
+              return false
+            end
+            for _, item in ipairs(json) do
+              assert.same("127.0.0.1", item.client_ip)
+
+              -- we only get a deterministic order with workers == 1
+              if workers == 1 then
+                assert.same(200 + ((i + 1) % 10), item.response.status)
+              end
+              i = i + 1
+            end
+          end
+          if i ~= n then
+            return false
+          end
+
+          local i = 0
+          for _, entry in ipairs(body2.log.entries) do
+            local json = cjson.decode(entry.request.postData.text)
+
+            -- we only get an exact split with workers == 1
+            if workers == 1 and #json ~= 5 then
+              return false
+            end
+            for _, item in ipairs(json) do
+              assert.same("127.0.0.1", item.client_ip)
+
+              -- we only get a deterministic order with workers == 1
+              if workers == 1 then
+                assert.same(300 + ((i + 1) % 10), item.response.status)
+              end
+              i = i + 1
+            end
+          end
+          if i ~= n then
+            return false
+          end
+
+          return true
+        end, 15)
+      end)
+    end)
+
+  end
+
+
+  describe("Plugin: http-log (log) enabled globally [#" .. strategy .. "]", function()
     local proxy_client
 
     lazy_setup(function()
