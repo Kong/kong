94bec158193bb8065562dedad21892bff9ebfd6e
eskerda <eskerda@gmail.com>
2019-04-26 14:40:29 +0200

    fix(migrations) add 0.13 to 0.14 migrations (pg)

--- kong/db/migrations/core/001_13_to_14.lua
+++ kong/db/migrations/core/001_13_to_14.lua
@@ -1,6 +1,38 @@
+local utils = require "kong.tools.utils"
+
+local fmt = string.format
+
 return {
   postgres = {
     up = [[
+      -- 2018-03-27-123400_prepare_certs_and_snis
+      DO $$
+      BEGIN
+        ALTER TABLE IF EXISTS ssl_certificates    RENAME TO certificates;
+        ALTER TABLE IF EXISTS ssl_servers_names   RENAME TO snis;
+      EXCEPTION WHEN duplicate_table THEN
+        -- Do nothing, accept existing state
+      END$$;
+
+      DO $$
+      BEGIN
+        ALTER TABLE snis RENAME COLUMN ssl_certificate_id TO certificate_id;
+        ALTER TABLE snis ADD    COLUMN id uuid;
+      EXCEPTION WHEN undefined_column THEN
+        -- Do nothing, accept existing state
+      END$$;
+
+      DO $$
+      BEGIN
+        ALTER TABLE snis ALTER COLUMN created_at TYPE timestamp with time zone
+          USING created_at AT time zone 'UTC';
+        ALTER TABLE certificates ALTER COLUMN created_at TYPE timestamp with time zone
+          USING created_at AT time zone 'UTC';
+      EXCEPTION WHEN undefined_column THEN
+        -- Do nothing, accept existing state
+      END$$;
+
+
       -- 2018-05-17-173100_hash_on_cookie
       DO $$
       BEGIN
@@ -13,6 +45,51 @@ return {
     ]],
 
     teardown = function(connector, helpers)
+      -- 2018-03-27-125400_fill_in_snis_ids
+      local rows, err = connector:query([[
+        SELECT * FROM snis;
+      ]])
+      if err then
+        return err
+      end
+      local sql_buffer = { "BEGIN;" }
+      local len = #rows
+      for i = 1, len do
+        sql_buffer[i + 1] = fmt("UPDATE snis SET id = '%s' WHERE name = '%s';",
+                                utils.uuid(),
+                                rows[i].name)
+      end
+      sql_buffer[len + 2] = "COMMIT;"
+
+      local _, err = connector:query(table.concat(sql_buffer))
+      if err then
+        return err
+      end
+
+      -- 2018-03-27-130400_make_ids_primary_keys_in_snis",
+      connector:query([[
+        ALTER TABLE snis
+          DROP CONSTRAINT IF EXISTS ssl_servers_names_pkey;
+
+        ALTER TABLE snis
+          DROP CONSTRAINT IF EXISTS ssl_servers_names_ssl_certificate_id_fkey;
+
+        DO $$
+        BEGIN
+          ALTER TABLE snis
+            ADD CONSTRAINT snis_name_unique UNIQUE(name);
+
+          ALTER TABLE snis
+            ADD PRIMARY KEY (id);
+
+          ALTER TABLE snis
+            ADD CONSTRAINT snis_certificate_id_fkey
+            FOREIGN KEY (certificate_id)
+            REFERENCES certificates;
+        EXCEPTION WHEN duplicate_table THEN
+          -- Do nothing, accept existing state
+        END$$;
+      ]])
     end,
   },
 
