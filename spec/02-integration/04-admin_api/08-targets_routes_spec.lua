4232f87645b40be53b11cbad027d6af1227ba516
Raimon Grau <raimonster@gmail.com>
2019-05-03 18:19:06 +0100

    chore(*) remove old dao references

--- spec/02-integration/04-admin_api/08-targets_routes_spec.lua
+++ spec/02-integration/04-admin_api/08-targets_routes_spec.lua
@@ -25,13 +25,12 @@ for _, strategy in helpers.each_strategy() do
 describe("Admin API #" .. strategy, function()
   local bp
   local db
-  local dao
   local client
   local weight_default, weight_min, weight_max = 100, 0, 1000
   local default_port = 8000
 
   lazy_setup(function()
-    bp, db, dao = helpers.get_db_utils(strategy, {
+    bp, db = helpers.get_db_utils(strategy, {
       "upstreams",
       "targets",
     })
@@ -47,7 +46,6 @@ describe("Admin API #" .. strategy, function()
 
   before_each(function()
     client = assert(helpers.admin_client())
-    ngx.ctx.workspaces = dao.workspaces:find_all({name = "default"})
   end)
 
   after_each(function()
