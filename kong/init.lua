740d108abfef098123b552437348b6d72da827e4
Enrique García Cota <kikito@gmail.com>
2019-05-07 00:16:54 +0200

    refactor(runloop) reorganize how loaded_plugins is calculated
    Reasons for this refactor:
    
    * The previous `sort_plugins_for_execution` "lied" because it did more than sorting - it also added the reports plugin
    * The initialization of loaded_plugins is now literally "as late as possible", since it is initialized right before it is used for the first time in build_new_plugins

--- kong/init.lua
+++ kong/init.lua
@@ -81,6 +81,7 @@ local plugins_iterator = require "kong.runloop.plugins_iterator"
 local balancer_execute = require("kong.runloop.balancer").execute
 local kong_error_handlers = require "kong.error_handlers"
 local BasePlugin   = require "kong.plugins.base_plugin"
+local reports = require "kong.reports"
 
 local kong             = kong
 local ngx              = ngx
@@ -132,7 +133,28 @@ local function plugin_protocols_match_current_subsystem(plugin)
 end
 
 
+local function get_loaded_plugins()
+  local loaded = assert(kong.db.plugins:get_handlers())
+
+  if kong.configuration.anonymous_reports then
+
+    reports.configure_ping(kong.configuration)
+    reports.add_ping_value("database_version", kong.db.infos.db_ver)
+    reports.toggle(true)
+
+    loaded[#loaded + 1] = {
+      name = "reports",
+      handler = reports,
+    }
+  end
+
+  return loaded
+end
+
+
 local function build_new_plugins(version)
+  loaded_plugins = loaded_plugins or get_loaded_plugins()
+
   local new_plugins = {
     version = version,
     map = {},
@@ -266,29 +288,6 @@ end
 
 local Kong = {}
 
-
-local function sort_plugins_for_execution(plugins)
-  -- sort plugins by priority
-  table.sort(plugins, function(a, b)
-    local priority_a = a.handler.PRIORITY or 0
-    local priority_b = b.handler.PRIORITY or 0
-    return priority_a > priority_b
-  end)
-
-  -- add reports plugin if not disabled
-  if kong.configuration.anonymous_reports then
-    local reports = require "kong.reports"
-
-    reports.configure_ping(kong.configuration)
-    reports.add_ping_value("database_version", kong.db.infos.db_ver)
-    reports.toggle(true)
-
-    plugins[#plugins + 1] = {
-      name = "reports",
-      handler = reports,
-    }
-  end
-end
 
 
 local function flush_delayed_response(ctx)
@@ -482,8 +481,6 @@ function Kong.init()
 
   -- Load plugins as late as possible so that everything is set up
   assert(db.plugins:load_plugin_schemas(config.loaded_plugins))
-  loaded_plugins = assert(db.plugins:get_handlers())
-  sort_plugins_for_execution(loaded_plugins)
 
   do
     local timeout = 60
