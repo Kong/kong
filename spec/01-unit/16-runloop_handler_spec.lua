d2a058eb51bbcd99c8e845565ae80514df497098
Enrique García Cota <kikito@gmail.com>
2019-05-23 16:25:26 -0300

    feat(runloop) rebuild router and plugins_iterator via a timer
    This change creates checks and rebuilds the plugins iterator and router via
    timers, once per second, so that the rebuilding is uncoupled from receiving a
    request.
    
    Extracted from #4488, with some differences:
    
    * two separate periodic timers are employed, one for the router and
      another for the plugins iterator, so that the rebuild of one does
      not delay the other, without having to use a timer that triggers
      another timer. This also allows us to continue using the
      `kong.concurrency` library to perform the concurrency checks, without
      having to handle semaphores directly.
    * several functions have been put directly inside `rebuild`
    * The `rebuild` utility function now returns `nil, err`, it is up
      to the caller to decide how to deal with errors.
    * Timer-based rebuilds explicitly set the `timeout` to 0 and
      ignore the case where `acquire_semaphore` returns `timeout`
    * `get_plugins_iterator` gets the iterator, while
      `get_updated_plugins_iterator` attempts to rebuild it first.
      In case of failure, it logs it and returns the old plugins iterator.
    * Added `if premature then return` to timer-based updates
    
    Co-Authored-By: Aapo Talvensaari <bungle@konghq.com>
    Co-Authored-By: Hisham Muhammad <hisham@gobolinux.org>

--- spec/01-unit/16-runloop_handler_spec.lua
+++ spec/01-unit/16-runloop_handler_spec.lua
@@ -6,12 +6,18 @@ local function setup_it_block()
   -- keep track of created semaphores
   local semaphores = {}
 
+  local my_cache = {}
+
   mocker.setup(finally, {
 
     ngx = {
       log = function()
         -- avoid stdout output during test
       end,
+      timer = {
+        at = function() end,
+        every = function() end,
+      }
     },
 
     kong = {
@@ -22,9 +28,23 @@ local function setup_it_block()
       response = {
         exit = function() end,
       },
+      worker_events = {
+        register = function() end,
+      },
+      cluster_events = {
+        subscribe = function() end,
+      },
+      configuration = {
+        database = "dummy",
+        async_rebuilds = false,
+      },
+      db = {
+        strategy = "dummy",
+      },
       cache = {
-        get = function()
-          return "1"
+        _cache = my_cache,
+        get = function(_, k)
+          return my_cache[k] or "1"
         end
       }
     },
@@ -75,6 +95,14 @@ local function setup_it_block()
   })
 end
 
+
+local mock_router = {
+  exec = function()
+    return nil
+  end
+}
+
+
 describe("runloop handler", function()
   describe("router rebuilds", function()
 
@@ -84,15 +112,18 @@ describe("runloop handler", function()
       local semaphores = require "ngx.semaphore"._semaphores
       local handler = require "kong.runloop.handler"
 
-      local build_router_spy = spy.new(function()
+      local update_router_spy = spy.new(function()
         return nil, "error injected by test (feel free to ignore :) )"
       end)
 
-      handler._set_build_router(build_router_spy)
+      handler.init_worker.before({})
+
+      handler._set_router(mock_router)
+      handler._set_update_router(update_router_spy)
 
       handler.access.before({})
 
-      assert.spy(build_router_spy).was_called(1)
+      assert.spy(update_router_spy).was_called(1)
 
       -- check semaphore
       assert.equal(1, semaphores[1].value)
@@ -104,19 +135,19 @@ describe("runloop handler", function()
       local semaphores = require "ngx.semaphore"._semaphores
       local handler = require "kong.runloop.handler"
 
-      local build_router_spy = spy.new(function()
-        return true
-      end)
+      handler.init_worker.before()
 
-      handler._set_build_router(build_router_spy)
+      local update_router_spy = spy.new(function() end)
+      handler._set_update_router(update_router_spy)
+      handler._set_router(mock_router)
 
+      -- call it once to create a semaphore
       handler.access.before({})
 
-      -- check semaphore
-      assert.equal(1, semaphores[1].value)
+      assert.spy(update_router_spy).was_called(1)
 
-      -- was called even if semaphore timed out on acquisition
-      assert.spy(build_router_spy).was_called(1)
+      -- force a router rebuild
+      handler._set_router_version("old")
 
       -- cause failure to acquire semaphore
       semaphores[1].wait = function()
@@ -126,7 +157,7 @@ describe("runloop handler", function()
       handler.access.before({})
 
       -- was called even if semaphore timed out on acquisition
-      assert.spy(build_router_spy).was_called(2)
+      assert.spy(update_router_spy).was_called(2)
 
       -- check semaphore
       assert.equal(1, semaphores[1].value)
