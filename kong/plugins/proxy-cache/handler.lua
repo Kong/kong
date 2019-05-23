3d3e6d1c77967fbd3e977931649438ad6adbba01
Vinicius Mignot <vinicius.mignot@gmail.com>
2019-05-23 11:20:56 -0300

    fix(proxy-cache) proxy-cache caches ngx.resp if available
    proxy-cache plugin was trying to always cache ngx.resp.get_headers()
    locally in handler.lua, but this function is not available for stream
    listeners. This change fixes that.

--- kong/plugins/proxy-cache/handler.lua
+++ kong/plugins/proxy-cache/handler.lua
@@ -9,6 +9,7 @@ local floor            = math.floor
 local get_method       = ngx.req.get_method
 local ngx_get_uri_args = ngx.req.get_uri_args
 local ngx_get_headers  = ngx.req.get_headers
+local resp_get_headers = ngx.resp and ngx.resp.get_headers
 local ngx_log          = ngx.log
 local ngx_now          = ngx.now
 local ngx_re_gmatch    = ngx.re.gmatch
@@ -413,7 +414,7 @@ function ProxyCacheHandler:header_filter(conf)
 
   -- if this is a cacheable request, gather the headers and mark it so
   if cacheable_response(ngx, conf, cc) then
-    ctx.res_headers = ngx.resp.get_headers(0, true)
+    ctx.res_headers = resp_get_headers(0, true)
     ctx.res_ttl = conf.cache_control and resource_ttl(cc) or conf.cache_ttl
     ngx.ctx.proxy_cache = ctx
 
