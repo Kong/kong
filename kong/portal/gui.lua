dfa41d96705aa3c4fc288c99dabcc88f534ec1b0
Henri Pietila <HenriPro@users.noreply.github.com>
2019-05-24 17:01:30 -0700

    feat(portal): adds redirect for odic login to prior page DX-563 (#1102)
    * feat(portal): adds redirect for odic login to prior page

--- kong/portal/gui.lua
+++ kong/portal/gui.lua
@@ -1,6 +1,8 @@
 local lapis = require "lapis"
+local ck = require "resty.cookie"
 local pl_file = require "pl.file"
 local auth    = require "kong.portal.auth"
+local workspaces  = require "kong.workspaces"
 local responses   = {} -- XXX EE: remove this placeholder
 local gui_helpers = require "kong.portal.gui_helpers"
 local EtluaWidget = require("lapis.etlua").EtluaWidget
@@ -34,6 +36,34 @@ app:enable("etlua")
 app:before_filter(function(self)
   local config = kong.configuration
   local headers = ngx.req.get_headers()
+
+  local cookie, err = ck:new()
+  if not cookie then
+    ngx.log(ngx.ERR, err)
+    return
+  end
+
+  local redirect = cookie:get("redirect")
+  if redirect then
+    local ok, err = cookie:set({
+      key = "redirect",
+      value = "",
+      path = "/",
+      max_age = 0,
+    })
+
+    if not ok then
+       ngx.log(ngx.ERR, err)
+       return
+    end
+
+    if config.portal_gui_use_subdomains then
+      return ngx.redirect('/' .. redirect)
+    end
+
+    local workspace_name = self.params.workspace_name or workspaces.DEFAULT_WORKSPACE
+    return ngx.redirect('/' .. workspace_name .. '/' .. redirect)
+  end
 
   self.is_admin = headers["Kong-Request-Type"] == "editor"
   self.path = ngx.unescape_uri(self.req.parsed_url.path)
