171fe5268645f7e3ffda40973a09daeb5f922399
eskerda <eskerda@gmail.com>
2019-05-29 16:32:04 +0200

    feat(dao) return ttl on pg select queries

--- kong/db/strategies/postgres/init.lua
+++ kong/db/strategies/postgres/init.lua
@@ -1309,6 +1309,9 @@ function _M.new(connector, schema, errors)
 
     upsert_expressions = concat(upsert_expressions, ", ")
 
+    local ttl_expression = "EXTRACT (EPOCH FROM (ttl - CURRENT_TIMESTAMP)) AS ttl"
+    select_expressions = concat({ select_expressions, ttl_expression }, ", ")
+
     create_statement = concat {
       "CREATE TABLE IF NOT EXISTS ", table_name_escaped, " (\n",
       "  ",   concat(create_expressions, ",\n  "), "\n",
