9160ef1fcbd279862a643fe637814275a7bc9372
Hisham Muhammad <hisham@gobolinux.org>
2019-05-31 11:44:31 -0300

    tests(balancer) disable https test due to OpenSSL crash
    This test launches a luaossl-based http server in a thread.
    Some data is being stored globally in the C side and being
    freed twice (by the main thread and the test-created thread),
    causing a crash:
    
    ```
    [ RUN      ] spec/02-integration/05-proxy/10-balancer_spec.lua @ 1235: Ring-balancer #postgres #ipv4 #healthchecks perform active health checks -- automatic recovery #https
    *** Error in `/home/travis/install-cache/e6da1a669af467e114689cac1893248c/openresty/nginx/sbin/nginx': double free or corruption (!prev): 0x00007f76bc00c370 ***
    ======= Backtrace: =========
    /lib/x86_64-linux-gnu/libc.so.6(+0x777e5)[0x7f76daacc7e5]
    /lib/x86_64-linux-gnu/libc.so.6(+0x8037a)[0x7f76daad537a]
    /lib/x86_64-linux-gnu/libc.so.6(cfree+0x4c)[0x7f76daad953c]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libcrypto.so.1.1(EVP_CIPHER_CTX_reset+0x49)[0x7f76db1b3a09]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libcrypto.so.1.1(EVP_CipherInit_ex+0x71)[0x7f76db1b4cc1]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libcrypto.so.1.1(+0x1b8a30)[0x7f76db1f1a30]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libcrypto.so.1.1(+0x1ba022)[0x7f76db1f3022]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libcrypto.so.1.1(RAND_DRBG_generate+0xe7)[0x7f76db1f5817]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libcrypto.so.1.1(RAND_DRBG_bytes+0x7a)[0x7f76db1f5a5a]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libcrypto.so.1.1(+0xcd041)[0x7f76db106041]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libcrypto.so.1.1(+0xcd256)[0x7f76db106256]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libcrypto.so.1.1(+0x11faa7)[0x7f76db158aa7]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libcrypto.so.1.1(EVP_PKEY_keygen+0x3f)[0x7f76db1bd5ef]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libssl.so.1.1(+0x2d0ff)[0x7f76db5560ff]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libssl.so.1.1(+0x5e7ff)[0x7f76db5877ff]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libssl.so.1.1(+0x509fb)[0x7f76db5799fb]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openssl/lib/libssl.so.1.1(SSL_do_handshake+0x58)[0x7f76db564938]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/luarocks/lib/lua/5.1/_cqueues.so(+0x2af13)[0x7f76c1400f13]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/luarocks/lib/lua/5.1/_cqueues.so(+0x1975b)[0x7f76c13ef75b]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/luarocks/lib/lua/5.1/_cqueues.so(+0x1b53c)[0x7f76c13f153c]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openresty/luajit/lib/libluajit-5.1.so.2(+0xa04a)[0x7f76dbd4004a]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/luarocks/lib/lua/5.1/_cqueues.so(+0x15467)[0x7f76c13eb467]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/luarocks/lib/lua/5.1/_cqueues.so(+0x169b1)[0x7f76c13ec9b1]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openresty/luajit/lib/libluajit-5.1.so.2(+0xa04a)[0x7f76dbd4004a]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/openresty/luajit/lib/libluajit-5.1.so.2(lua_pcall+0x2d)[0x7f76dbd509ad]
    /home/travis/install-cache/e6da1a669af467e114689cac1893248c/luarocks/lib/lua/5.1/llthreads2.so(+0x37ca)[0x7f76d899e7ca]
    /lib/x86_64-linux-gnu/libpthread.so.0(+0x76ba)[0x7f76dc1f06ba]
    /lib/x86_64-linux-gnu/libc.so.6(clone+0x6d)[0x7f76dab5c41d]
    ```
    
    This is causing consistent failures on Travis (possibly catching it since a
    base system library upgrade) as also on my machine. The plan
    is to move the test http server to a separate process to avoid
    this issue. In the meantime, we will disable this test.

--- spec/02-integration/05-proxy/10-balancer_spec.lua
+++ spec/02-integration/05-proxy/10-balancer_spec.lua
@@ -1231,7 +1231,7 @@ for _, strategy in helpers.each_strategy() do
             end
           end)
 
-          for _, protocol in ipairs({"http", "https"}) do
+          for _, protocol in ipairs({"http"}) do
             it("perform active health checks -- automatic recovery #" .. protocol, function()
               for nchecks = 1, 3 do
 
