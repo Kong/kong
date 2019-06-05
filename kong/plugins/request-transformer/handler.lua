eacac27ab27a29f1cbf7f0950ddce16c3a158311
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2019-06-05 19:09:31 +0300

    refactor(request-transformer) no need to to inherit from BasePlugin anymore

--- kong/plugins/request-transformer/handler.lua
+++ kong/plugins/request-transformer/handler.lua
@@ -1,19 +1,15 @@
-local BasePlugin = require "kong.plugins.base_plugin"
 local access = require "kong.plugins.request-transformer.access"
 
-local RequestTransformerHandler = BasePlugin:extend()
 
-function RequestTransformerHandler:new()
-  RequestTransformerHandler.super.new(self, "request-transformer")
-end
+local RequestTransformerHandler = {
+  VERSION  = "1.2.1",
+  PRIORITY = 801,
+}
 
 
 function RequestTransformerHandler:access(conf)
-  RequestTransformerHandler.super.access(self)
   access.execute(conf)
 end
 
-RequestTransformerHandler.PRIORITY = 801
-RequestTransformerHandler.VERSION = "1.2.0"
 
 return RequestTransformerHandler
