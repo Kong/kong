b59e9f8b582d55b34aad3b2d94a7fb6efb9bae90
Thijs Schreijer <thijs@thijsschreijer.nl>
2019-06-17 15:55:48 +0200

    fix(serverless-functions) only run them once when initializing

--- kong/plugins/pre-function/_handler.lua
+++ kong/plugins/pre-function/_handler.lua
@@ -14,14 +14,27 @@ return function(plugin_name, priority)
   function ServerlessFunction:access(config)
 
     local functions = config_cache[config]
+
     if not functions then
+      -- first call, go compile the functions
       functions = {}
       for _, fn_str in ipairs(config.functions) do
-        local func1 = loadstring(fn_str)
-        local _, func2 = pcall(func1)
-        insert(functions, type(func2) == "function" and func2 or func1)
+        local func1 = loadstring(fn_str)    -- load it
+        local _, func2 = pcall(func1)       -- run it
+        if type(func2) ~= "function" then
+          -- old style (0.1.0), without upvalues
+          insert(functions, func1)
+        else
+          -- this is a new function (0.2.0+), with upvalues
+          insert(functions, func2)
+
+          -- the first call to func1 above only initialized it, so run again
+          func2()
+        end
       end
+
       config_cache[config] = functions
+      return  -- must return since we allready executed them
     end
 
     for _, fn in ipairs(functions) do
