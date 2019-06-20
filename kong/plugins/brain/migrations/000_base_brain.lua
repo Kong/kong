c1175f30fef2b35c93721528bb09e44a38e8a90e
Caio Ramos Casimiro <caiorcasimiro@gmail.com>
2019-06-20 16:53:16 -0300

    added service map endpoints

---
+++ kong/plugins/brain/migrations/000_base_brain.lua
@@ -0,0 +1,23 @@
+return {
+  postgres = {
+    up = [[
+      CREATE TABLE IF NOT EXISTS brain_service_maps (
+        id UUID NOT NULL PRIMARY KEY,
+        created_at TIMESTAMP WITHOUT TIME ZONE,
+        service_map TEXT,
+        singleton TEXT UNIQUE
+      );
+    ]],
+  },
+
+  cassandra = {
+    up = [[
+      CREATE TABLE IF NOT EXISTS brain_service_maps (
+        id uuid PRIMARY KEY,
+        created_at timestamp,
+        service_map text,
+        singleton text,
+      );
+    ]],
+  },
+}
