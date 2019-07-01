3efca7c378e0bd6195e9371ba796bea4a80563dc
Caio Ramos Casimiro <caiorcasimiro@gmail.com>
2019-07-01 11:42:35 -0300

    service_token is never used, as we set it to foo in plugin configuration it's better to hide it from user and maybe delete it in future

--- kong/plugins/brain/schema.lua
+++ kong/plugins/brain/schema.lua
@@ -16,7 +16,7 @@ return {
           { queue_size = { type = "number", default = 1000 } },
           { flush_timeout = { type = "number", default = 2 } },
           { log_bodies = { type = "boolean", default = false } },
-          { service_token = { type = "string", required = true } },
+          { service_token = { type = "string", default = 'default' } },
           { connection_timeout = { type = "number", default = 30 } },
           { host = { type = "string", required = true, default = "collector.brain.kong.com" } },
           { port = { type = "number", required = true, default = 443 } },
