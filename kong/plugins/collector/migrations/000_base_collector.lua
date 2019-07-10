5c0c830dffbea6d897e7e7e01cc2bd8dcc582c48
Caio Ramos Casimiro <caiorcasimiro@gmail.com>
2019-07-10 19:53:05 -0300

    changes endpoint brain_service_maps to service_maps

--- kong/plugins/collector/migrations/000_base_collector.lua
+++ kong/plugins/collector/migrations/000_base_collector.lua
@@ -1,7 +1,7 @@
 return {
   postgres = {
     up = [[
-      CREATE TABLE IF NOT EXISTS brain_service_maps (
+      CREATE TABLE IF NOT EXISTS service_maps (
         id UUID NOT NULL PRIMARY KEY,
         created_at TIMESTAMP WITHOUT TIME ZONE,
         service_map TEXT,
@@ -12,7 +12,7 @@ return {
 
   cassandra = {
     up = [[
-      CREATE TABLE IF NOT EXISTS brain_service_maps (
+      CREATE TABLE IF NOT EXISTS service_maps (
         id uuid PRIMARY KEY,
         created_at timestamp,
         service_map text,
