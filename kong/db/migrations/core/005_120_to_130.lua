69a2dc5e3cab0fa301bf39547aa54728774e0f0e
Datong Sun <datong.sun@konghq.com>
2019-07-15 14:02:29 -0700

    feat(db) add new core entity "CA Certificates"
    Background
    ----------
    There has been cases were storing certificates without respective
    private key may be needed. For example, when validating upstream
    certificate.
    
    One may thinks that we can allow the existing "Certificate"
    entity to accept a `key = null`, but that will be problematic
    because we will have to write large amount of custom DAO
    methods (and respective tests) which will be hard to maintain as the
    usage grows.
    
    Solution
    --------
    We add a new entity type "CA Certificates", which is principally the same as the
    existing "Certificate" entity but without the `key` field. The new
    entity is stored in a new table called `"ca_certificates"`.

--- kong/db/migrations/core/005_120_to_130.lua
+++ kong/db/migrations/core/005_120_to_130.lua
@@ -8,12 +8,38 @@ return {
         -- Do nothing, accept existing state
       END;
       $$;
+
+      CREATE TABLE IF NOT EXISTS "ca_certificates" (
+        "id"          UUID                       PRIMARY KEY,
+        "created_at"  TIMESTAMP WITH TIME ZONE   DEFAULT (CURRENT_TIMESTAMP(0) AT TIME ZONE 'UTC'),
+        "cert"        TEXT NOT NULL,
+        "tags"        TEXT[]
+      );
+
+      DO $$
+      BEGIN
+        CREATE TRIGGER ca_certificates_sync_tags_trigger
+        AFTER INSERT OR UPDATE OF tags OR DELETE ON ca_certificates
+        FOR EACH ROW
+        EXECUTE PROCEDURE sync_tags();
+      EXCEPTION WHEN UNDEFINED_COLUMN OR UNDEFINED_TABLE THEN
+        -- Do nothing, accept existing state
+      END$$;
     ]],
   },
 
   cassandra = {
     up = [[
       ALTER TABLE upstreams ADD algorithm text;
+
+      CREATE TABLE IF NOT EXISTS ca_certificates(
+        partition text,
+        id uuid,
+        cert text,
+        created_at timestamp,
+        tags set<text>,
+        PRIMARY KEY (partition, id)
+      );
     ]],
   },
 }
