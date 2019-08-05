f5c17c91b7ed73187b332d229e62cfff24ec474e
Bao Mai <bao.mai@konghq.com>
2019-08-05 03:43:54 -0700

    feat(graphql-rate-limiting-advanced) API for cost decoration.
    Used `endpoints` module in kong.api.

---
+++ kong/plugins/gql-rate-limiting/api.lua
@@ -0,0 +1,17 @@
+
+local endpoints = require "kong.api.endpoints"
+
+local cost_decoration_schema = kong.db.gql_ratelimiting_cost_decoration.schema
+
+
+return {
+    ["/costs"] = {
+        schema = cost_decoration_schema,
+        methods = {
+            GET = endpoints.get_collection_endpoint(cost_decoration_schema),
+            -- TODO: Verify Type Path notation before updating or inserting
+            POST = endpoints.post_collection_endpoint(cost_decoration_schema),
+            PUT = endpoints.put_entity_endpoint(cost_decoration_schema)
+        }
+    }
+}
