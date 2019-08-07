87425172ddecb70f7e52d4acdc679fea31e195ed
Hisham Muhammad <hisham@gobolinux.org>
2019-08-07 11:52:13 -0300

    tests(balancer) do not assign ports in use
    When running a test on Linux (e.g. our Travis CI environment),
    ensure that a port is not in use when assigning it for a test.

--- spec/02-integration/05-proxy/10-balancer/01-ring-balancer_spec.lua
+++ spec/02-integration/05-proxy/10-balancer/01-ring-balancer_spec.lua
@@ -274,9 +274,24 @@ do
   end
 
   do
+    local os_name
+    do
+      local pd = io.popen("uname -s")
+      os_name = pd:read("*l")
+      pd:close()
+    end
+    local function port_in_use(port)
+      if os_name ~= "Linux" then
+        return false
+      end
+      return os.execute("netstat -n | grep -w " .. port)
+    end
+
     local port = FIRST_PORT
     gen_port = function()
-      port = port + 1
+      repeat
+        port = port + 1
+      until not port_in_use(port)
       return port
     end
   end
