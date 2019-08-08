5ee5fb57c0cc471d2bc6fa1aa06cc2c90cc2ccc1
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2019-08-08 11:40:07 +0300

    fix(plugins) foreign fields were not always required as they should
    The issue originates from converting the old schema to new one where
    some fields where marked as `default=ngx.null` instead of
    `required=true`. This PR changes those back to required.
    
    Fix #4875

--- kong/plugins/key-auth/daos.lua
+++ kong/plugins/key-auth/daos.lua
@@ -11,7 +11,7 @@ return {
     fields = {
       { id = typedefs.uuid },
       { created_at = typedefs.auto_timestamp_s },
-      { consumer = { type = "foreign", reference = "consumers", default = ngx.null, on_delete = "cascade", }, },
+      { consumer = { type = "foreign", reference = "consumers", required = true, on_delete = "cascade", }, },
       { key = { type = "string", required = false, unique = true, auto = true }, },
     },
   },
