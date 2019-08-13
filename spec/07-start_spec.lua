4e5d601beab45004155e91269b7563b2c098984d
eskerda <eskerda@gmail.com>
2019-08-13 15:33:58 +0200

    feat(test) try to start kong with stream enabled (FTI-845)

---
+++ spec/07-start_spec.lua
@@ -0,0 +1,33 @@
+local helpers = require "spec.helpers"
+
+describe("kong starts with proxy-cache plugin", function()
+
+  setup(function()
+    helpers.get_db_utils(nil, nil, {"proxy-cache"})
+  end)
+
+  before_each(function()
+    helpers.stop_kong(nil, true)
+  end)
+
+  teardown(function()
+    helpers.stop_kong(nil, true)
+  end)
+
+  it("starts with default conf", function()
+    assert(helpers.start_kong({
+      plugins = "bundled,proxy-cache",
+      nginx_conf = "spec/fixtures/custom_nginx.template",
+    }))
+  end)
+
+  it("starts with stream listen", function()
+    assert(helpers.start_kong({
+      plugins = "bundled,proxy-cache",
+      nginx_conf = "spec/fixtures/custom_nginx.template",
+      stream_listen = "0.0.0.0:5555",
+    }))
+  end)
+
+end)
+
