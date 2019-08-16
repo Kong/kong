9d1fa1f444073fda7045f9a8e874939e5be4b8e5
Nathan Bailey <nbailey0001@gmail.com>
2019-08-16 12:29:49 -0700

    feat(portal) copy old files table to legacy_files (#1227)
    * feat(portal) add migrations for legacy files
    
    * feat(migrations) move workspace_entities update after files table rename

--- kong/enterprise_edition/db/migrations/enterprise/004_036-1_to_037.lua
+++ kong/enterprise_edition/db/migrations/enterprise/004_036-1_to_037.lua
@@ -1,21 +1,8 @@
+local fmt = string.format
+
 return {
   postgres = {
     up = [[
-      ALTER TABLE files
-        ADD COLUMN checksum text;
-
-      ALTER TABLE files
-        ADD COLUMN path text UNIQUE NOT NULL;
-
-      CREATE TABLE IF NOT EXISTS legacy_files(
-        id uuid PRIMARY KEY,
-        auth boolean NOT NULL,
-        name text UNIQUE NOT NULL,
-        type text NOT NULL,
-        contents text,
-        created_at timestamp without time zone default (CURRENT_TIMESTAMP(0) at time zone 'utc')
-      );
-
       DO $$
       BEGIN
         ALTER TABLE developers ADD rbac_user_id uuid;
@@ -29,21 +16,44 @@ return {
     teardown = function(connector)
       assert(connector:connect_migrations())
 
-      -- remove unneccesssary columns in files
+      -- update old files workspace_entities.entity_type to 'legacy_files'
+      -- rename files to legacy_files
+      -- make new files table
       assert(connector:query([[
-        ALTER TABLE files DROP COLUMN IF EXISTS auth;
-        ALTER TABLE files DROP COLUMN IF EXISTS name;
-        ALTER TABLE files DROP COLUMN IF EXISTS type;
+        DO $$
+        BEGIN
+          ALTER TABLE IF EXISTS files
+            RENAME TO legacy_files;
+        EXCEPTION WHEN DUPLICATE_TABLE THEN
+          -- Do nothing, accept existing state
+        END;
+        $$;
+
+        DO $$
+        BEGIN
+          ALTER INDEX IF EXISTS "portal_files_name_idx" RENAME TO "legacy_files_name_idx";
+        EXCEPTION WHEN DUPLICATE_TABLE THEN
+          -- Do nothing, accept existing state
+        END;
+        $$;
+
+        UPDATE workspace_entities SET entity_type='legacy_files' WHERE entity_type='files';
+
+        CREATE TABLE IF NOT EXISTS files(
+          id uuid PRIMARY KEY,
+          path text UNIQUE NOT NULL,
+          checksum text,
+          contents text,
+          created_at timestamp without time zone default (CURRENT_TIMESTAMP(0) at time zone 'utc')
+        );
+
+        CREATE INDEX IF NOT EXISTS files_path_idx on files(path);
       ]]))
     end
   },
 
   cassandra = {
     up = [[
-      ALTER TABLE files ADD checksum text;
-      ALTER TABLE files ADD path text;
-      CREATE INDEX IF NOT EXISTS ON files(path);
-
       CREATE TABLE IF NOT EXISTS legacy_files(
         id uuid PRIMARY KEY,
         auth boolean,
@@ -59,15 +69,120 @@ return {
       ALTER TABLE developers ADD rbac_user_id uuid;
       CREATE INDEX IF NOT EXISTS ON developers(rbac_user_id);
     ]],
-    teardown = function(connector)
-      -- remove unneccesssary columns in files
+    teardown = function(connector, helpers)
+      local coordinator = connector:connect_migrations()
+
+      local _, err = connector:query([[ SELECT * FROM files LIMIT 1; ]])
+
+      if not err then
+
+        local files_def = {
+          name    = "files",
+          columns = {
+            id         = "uuid",
+            auth       = "boolean",
+            name       = "text",
+            type       = "text",
+            contents   = "text",
+            created_at = "timestamp",
+          },
+        }
+
+        local legacy_files_def = {
+          name    = "legacy_files",
+          columns = {
+            id         = "uuid",
+            auth       = "boolean",
+            name       = "text",
+            type       = "text",
+            contents   = "text",
+            created_at = "timestamp",
+          },
+        }
+
+        assert(helpers:copy_cassandra_records(files_def, legacy_files_def, {
+            id         = "id",
+            auth       = "auth",
+            name       = "name",
+            type       = "type",
+            contents   = "contents",
+            created_at = "created_at",
+        }))
+
+        local workspaces_map
+        for rows, err in coordinator:iterate("select * from files") do
+          for _, file in ipairs(rows) do
+            if err then
+              return nil, err
+            end
+
+            if not workspaces_map then
+              workspaces_map = {}
+              for rows, err in coordinator:iterate("select * from workspaces;") do
+                if err then
+                  return nil, err
+                end
+
+                for _, workspace in ipairs(rows) do
+                  workspaces_map[workspace.id] = workspace
+                end
+              end
+            end
+
+            local workspace_entity
+
+            for id, _ in pairs(workspaces_map) do
+              local workspace_entities, err = connector:query(
+                fmt("select * from workspace_entities where workspace_id = %s and entity_id = '%s' and unique_field_name = 'name';", id, file.id)
+              )
+              if err then
+                return nil, err
+              end
+
+              workspace_entity = workspace_entities[1]
+              if workspace_entity then
+                break
+              end
+            end
+
+            local workspace = workspace_entity and workspaces_map[workspace_entity.workspace_id]
+            if not workspace then
+              return nil, "not able to fetch workspace relation for file: " .. file.name
+            end
+
+            -- update files entity type to legacy_files for entity by name
+            local _, err = connector:query(fmt("update workspace_entities set entity_type = 'legacy_files' " ..
+              "where workspace_id = %s and entity_id = '%s' and unique_field_name = 'name';", workspace.id, file.id))
+            if err then
+              return nil, err
+            end
+
+            -- update files entity type to legacy_files for entity by id
+            local _, err = connector:query(fmt("update workspace_entities set entity_type = 'legacy_files' " ..
+              "where workspace_id = %s and entity_id = '%s' and unique_field_name = 'id';", workspace.id, file.id))
+            if err then
+              return nil, err
+            end
+          end
+        end
+      end
+
       assert(connector:query([[
         DROP INDEX IF EXISTS files_name_idx;
         DROP INDEX IF EXISTS files_type_idx;
+        DROP TABLE IF EXISTS files;
+      ]]))
 
-        ALTER TABLE files DROP auth;
-        ALTER TABLE files DROP name;
-        ALTER TABLE files DROP type;
+      assert(connector:query([[
+        CREATE TABLE IF NOT EXISTS files(
+          id uuid PRIMARY KEY,
+          path text,
+          checksum text,
+          contents text,
+          created_at timestamp
+        );
+
+        CREATE INDEX IF NOT EXISTS ON files(path);
       ]]))
     end
   },
