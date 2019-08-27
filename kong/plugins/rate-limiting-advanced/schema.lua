039b844d6d1f3d1273a0798f7989e030645a8b24
Murillo Paula <murillo@murillopaula.com>
2019-08-27 11:37:00 -0300

    fix(rate-limiting-advanced) move `check` logic to `entity_check`
    Prior to this, we had a failed import with the declarative YAML file via
    `kong config db_import`. By the CLI reference [1], the `kong config
    db_import` can be used to import a declarative config file into the Kong
    database. But when trying to import following settings, a trackeback was
    logged and the importing task had failed:
    
    <YAML>
    ```
    _format_version: "1.1"
    plugins:
      - name: rate-limiting-advanced
        config:
          limit: [10]
          window_size: [60]
          identifier: credential
          sync_rate: -1
    ```
    
    <log>
    ```
    2019/08/22 15:43:14 [verbose] Kong: 0.36-enterprise-edition
    ... snip ...
    2019/08/22 15:43:14 [verbose] prefix in use: /usr/local/kong
    Error:
    /usr/local/share/lua/5.1/kong/db/schema/init.lua:1169: attempt to call method 'each_field' (a nil value)
    stack traceback:
            /usr/local/share/lua/5.1/kong/db/schema/init.lua:1169: in function 'run_self_check'
            /usr/local/share/lua/5.1/kong/db/schema/init.lua:1252: in function 'run_entity_checks'
            /usr/local/share/lua/5.1/kong/db/schema/init.lua:1682: in function 'validate'
            /usr/local/share/lua/5.1/kong/db/schema/init.lua:851: in function 'validate_field'
            /usr/local/share/lua/5.1/kong/db/schema/init.lua:749: in function 'validate_elements'
            /usr/local/share/lua/5.1/kong/db/schema/init.lua:808: in function 'validate_field'
            /usr/local/share/lua/5.1/kong/db/schema/init.lua:1035: in function 'validate_fields'
            /usr/local/share/lua/5.1/kong/db/schema/init.lua:1672: in function 'validate'
            /usr/local/share/lua/5.1/kong/db/declarative/init.lua:147: in function 'parse_file'
            /usr/local/share/lua/5.1/kong/cmd/config.lua:72: in function 'cmd_exec'
            /usr/local/share/lua/5.1/kong/cmd/init.lua:87: in function </usr/local/share/lua/5.1/kong/cmd/init.lua:87>
            [C]: in function 'xpcall'
            /usr/local/share/lua/5.1/kong/cmd/init.lua:87: in function </usr/local/share/lua/5.1/kong/cmd/init.lua:44>
            /usr/local/bin/kong:7: in function 'file_gen'
            init_worker_by_lua:49: in function <init_worker_by_lua:47>
            [C]: in function 'xpcall'
            init_worker_by_lua:56: in function <init_worker_by_lua:54>
    ```
    
    For comparasion, there were no issues when importing the open source
    rate-limiting plugin with a declarative YAML file:
    
    <YAML>
    ```
    _format_version: "1.1"
    plugins:
      - name: rate-limiting
        config:
          second: 5
          hour: 10000
    ```
    
    <log>
    ```
    2019/08/22 15:48:02 [verbose] Kong: 0.36-enterprise-edition
    ... snip ...
    2019/08/22 15:48:02 [verbose] prefix in use: /usr/local/kong
    2019/08/22 15:48:02 [info] parse successful, beginning import
    2019/08/22 15:48:02 [info] import successful
    ```
    
    In this commit we're fixing this issue by moving the logic from the
    `check` function to the `entity_check` function.
    
    [1] https://docs.konghq.com/latest/cli/#kong-config

--- kong/plugins/rate-limiting-advanced/schema.lua
+++ kong/plugins/rate-limiting-advanced/schema.lua
@@ -72,38 +72,32 @@ return {
     }
   },
 
-  check = function(entity)
-    local config = entity.config
-
-    if not config.limit or not config.window_size then
-      return true
-    end
-
-    -- sort the window_size and limit arrays by limit
-    -- first we create a temp table, each element of which is a pair of
-    -- limit/window_size values. we then sort based on the limit element
-    -- of this array of pairs. finally, we re-assign the plugin_t configuration
-    -- elements directly based off the sorted temp table
-    local t = {}
-    for i, v in ipairs(config.limit) do
-      t[i] = { config.limit[i], config.window_size[i] }
-    end
-
-    table.sort(t, function(a, b) return tonumber(a[1]) < tonumber(b[1]) end)
-
-    for i = 1, #t do
-      config.limit[i] = tonumber(t[i][1])
-      config.window_size[i] = tonumber(t[i][2])
-    end
-
-    return true
-  end,
-
   entity_checks = {
     { custom_entity_check = {
       field_sources = { "config" },
       fn = function(entity)
         local config = entity.config
+
+        if not config.limit or not config.window_size then
+          return true
+        end
+
+        -- sort the window_size and limit arrays by limit
+        -- first we create a temp table, each element of which is a pair of
+        -- limit/window_size values. we then sort based on the limit element
+        -- of this array of pairs. finally, we re-assign the plugin_t configuration
+        -- elements directly based off the sorted temp table
+        local t = {}
+        for i, v in ipairs(config.limit) do
+          t[i] = { config.limit[i], config.window_size[i] }
+        end
+
+        table.sort(t, function(a, b) return tonumber(a[1]) < tonumber(b[1]) end)
+
+        for i = 1, #t do
+          config.limit[i] = tonumber(t[i][1])
+          config.window_size[i] = tonumber(t[i][2])
+        end
 
         if config.strategy == "memory" then
           local ok, err = check_shdict(config.dictionary_name)
