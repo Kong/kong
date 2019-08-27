811f2103bfac7028f4f2dbf48554d7a716656fb2
Jordan Lawler <jordanalawler@gmail.com>
2019-08-27 14:13:44 -0700

    feat(theme-helper) value/description support (#1262)
    * feat(theme-helper) add support for value/description for theme fonts/colors
    
    * chore(unit) linting

--- kong/portal/render_toolset/theme/init.lua
+++ kong/portal/render_toolset/theme/init.lua
@@ -1,14 +1,37 @@
 local helpers       = require "kong.portal.render_toolset.helpers"
 local singletons    = require "kong.singletons"
 
+local function get_val(attr)
+  local val
+  if type(attr) ~= "table" then
+    val = attr
+  end
+
+  if attr.value then
+    val = attr.value
+  end
+
+  return val
+end
+
 return function()
   local render_ctx = singletons.render_ctx
   local theme = helpers.tbl.deepcopy(render_ctx.theme or {})
 
+  theme.colors = theme.colors or {}
+  for k, color in pairs(theme.colors) do
+    theme.colors[k] = get_val(color)
+  end
+
+  theme.fonts = theme.fonts or {}
+  for k, font in pairs(theme.fonts or {}) do
+    theme.fonts[k] = get_val(font)
+  end
+
   theme.color = function(key)
     return theme.colors[key]
   end
-  
+
   theme.font = function (key)
     return theme.fonts[key]
   end
---
+++ spec-ee/01-unit/04-dev-portal/08-render_toolset/04-theme_spec.lua
@@ -0,0 +1,126 @@
+local handler = require "kong.portal.render_toolset.handler"
+
+describe("page", function()
+  local theme, snapshot, singletons, workspaces
+
+  lazy_setup(function()
+    singletons = require "kong.singletons"
+    workspaces = require "kong.workspaces"
+
+    singletons.db = {
+      files = {
+        select_all = function() return {} end
+      }
+    }
+
+    singletons.configuration = {
+      portal = "on",
+      portal_gui_listeners = {"127.0.0.1:8003"},
+      portal_api_listeners = {"127.0.0.1:8004"},
+      portal_gui_host = "localhost.com",
+      portal_gui_protocol = "http",
+      portal_api_url = "http://api.localhost.com",
+      portal_auth = "basic-auth",
+    }
+
+    singletons.render_ctx = {
+      route = "/default/hello-world",
+      content = {
+        title = "Hello World",
+        sidebar = {
+          show = true,
+          contents = {
+            "a", "b", "c",
+          }
+        },
+      },
+    }
+
+    workspaces.get_workspace = function()
+      return {
+        name = "default",
+        config = {},
+      }
+    end
+  end)
+
+  before_each(function()
+    snapshot = assert:snapshot()
+    theme = handler().theme
+  end)
+
+  after_each(function()
+    snapshot:revert()
+  end)
+
+  describe("empty color/font declerations", function()
+    lazy_setup(function()
+      singletons.render_ctx.theme = {}
+    end)
+
+    it('returns nil with no theme set', function()
+      assert.is_nil(theme.colors.green)
+      assert.is_nil(theme.colors.blue)
+      assert.is_nil(theme.colors.red)
+
+      assert.is_nil(theme.fonts.fancy)
+      assert.is_nil(theme.fonts.ugly)
+      assert.is_nil(theme.fonts.bold)
+    end)
+
+    it('returns nil from helper with no theme set', function()
+      assert.is_nil(theme.color("green"))
+      assert.is_nil(theme.color("blue"))
+      assert.is_nil(theme.color("red"))
+
+      assert.is_nil(theme.font("fancy"))
+      assert.is_nil(theme.font("ugly"))
+      assert.is_nil(theme.font("bold"))
+    end)
+  end)
+
+  describe("non-empty color/font declerations", function()
+    lazy_setup(function()
+      singletons.render_ctx.theme = {
+        colors = {
+          green = "#abcdef",
+          blue  = {
+            value = "#123456",
+          },
+          red   = {
+            dog = { "cat" }
+          }
+        },
+        fonts = {
+          fancy = "sofancy",
+          ugly  = {
+            value = "muchugly",
+          },
+          bold   = {
+            dog = { "cat" }
+          }
+        }
+      }
+    end)
+
+    it('normalizes color values', function()
+      assert.equals(theme.colors.green, "#abcdef")
+      assert.equals(theme.colors.blue, "#123456")
+      assert.is_nil(theme.colors.red)
+
+      assert.equals(theme.color("green"), "#abcdef")
+      assert.equals(theme.color("blue"), "#123456")
+      assert.is_nil(theme.color("red"))
+    end)
+  
+    it('normalizes font values', function()
+      assert.equals(theme.fonts.fancy, "sofancy")
+      assert.equals(theme.fonts.ugly, "muchugly")
+      assert.is_nil(theme.fonts.bold)
+
+      assert.equals(theme.font("fancy"), "sofancy")
+      assert.equals(theme.font("ugly"), "muchugly")
+      assert.is_nil(theme.font("bold"))
+    end)
+  end)
+end)
