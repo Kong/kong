62641cecb2a2d978476816ac43733246340ca74d
Harry Bagdi <harrybagdi@gmail.com>
2019-09-03 09:55:17 -0700

    feat(acl) add tags support

--- kong/plugins/acl/daos.lua
+++ kong/plugins/acl/daos.lua
@@ -12,6 +12,7 @@ return {
       { created_at = typedefs.auto_timestamp_s },
       { consumer = { type = "foreign", reference = "consumers", required = true, on_delete = "cascade" }, },
       { group = { type = "string", required = true } },
+      { tags  = typedefs.tags },
     },
   },
 }
