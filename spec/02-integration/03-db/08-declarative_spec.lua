7cd176849ff4cd2443a2c907d9f2003c0965a28f
Raimon Grau <raimonster@gmail.com>
2019-09-04 19:27:05 +0100

    chore(*) add EE tables to db export expected results

--- spec/02-integration/03-db/08-declarative_spec.lua
+++ spec/02-integration/03-db/08-declarative_spec.lua
@@ -194,7 +194,10 @@ for _, strategy in helpers.each_strategy() do
           "plugins",
           "routes",
           "services",
-          "snis"
+          "snis",
+          "workspace_entities", -- XXX EE
+          "workspace_entity_counters",   -- XXX EE
+          "workspaces",
         }, toplevel_keys)
 
         assert.equals("1.1", yaml._format_version)
@@ -247,5 +250,3 @@ for _, strategy in helpers.each_strategy() do
     end)
   end)
 end
-
-
