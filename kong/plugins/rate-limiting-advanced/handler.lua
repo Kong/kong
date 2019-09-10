2f74a11522fcfad808b4d9e74b8ad7026a7ba496
Travis Raines <traines@konghq.com>
2019-09-10 15:46:07 -0700

    chore(rate-limiting-advanced) bump version to 1.3.3

--- kong/plugins/rate-limiting-advanced/handler.lua
+++ kong/plugins/rate-limiting-advanced/handler.lua
@@ -18,7 +18,7 @@ local RATELIMIT_REMAINING = "X-RateLimit-Remaining"
 
 
 NewRLHandler.PRIORITY = 902
-NewRLHandler.VERSION = "1.3.2"
+NewRLHandler.VERSION = "1.3.3"
 
 
 local human_window_size_lookup = {
