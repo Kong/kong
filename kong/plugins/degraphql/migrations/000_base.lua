748c4397097ba118b5dbfddee6509f5575b93539
eskerda <eskerda@gmail.com>
2019-09-18 00:28:24 +0200

    fix(degraphql) add base cassandra migration

--- kong/plugins/degraphql/migrations/000_base.lua
+++ kong/plugins/degraphql/migrations/000_base.lua
@@ -21,7 +21,21 @@ return {
     ]],
   },
   cassandra = {
-    up = [[ ]],
+    up = [[
+      CREATE TABLE IF NOT EXISTS "degraphql_routes" (
+        "partition"         text,
+        "id"                uuid,
+        "service_id"        uuid,
+        "methods"           list<text>,
+        "uri"               text,
+        "query"             text,
+        "created_at"        timestamp,
+        "updated_at"        timestamp,
+        PRIMARY KEY         (partition, id)
+      );
+
+      CREATE INDEX IF NOT EXISTS degraphql_routes_service_id_idx ON degraphql_routes(service_id);
+    ]],
   },
 }
 
