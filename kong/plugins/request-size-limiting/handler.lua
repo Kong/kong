11ea5be97ceae86282544bf607bb49fbdbd00564
shashi ranjan <shashi@konghq.com>
2019-10-15 16:14:31 -0700

    feat(plugins/request-size-limiting) allow size unit (#1352)
    * feat(plugins/request-size-limiting) allow size unit
    
    * tests(plugins) test size unit

--- kong/plugins/request-size-limiting/handler.lua
+++ kong/plugins/request-size-limiting/handler.lua
@@ -3,15 +3,31 @@
 local strip = require("pl.stringx").strip
 local tonumber = tonumber
 
-local MB = 2^20
 
 local RequestSizeLimitingHandler = {}
 
 RequestSizeLimitingHandler.PRIORITY = 951
 RequestSizeLimitingHandler.VERSION = "2.0.0"
 
-local function check_size(length, allowed_size, headers)
-  local allowed_bytes_size = allowed_size * MB
+
+local size_units = {
+  "megabytes",
+  "kilobytes",
+  "bytes",
+}
+RequestSizeLimitingHandler.size_units = size_units
+
+
+local unit_multiplication_factor = {
+  ["bytes"]        = 1,
+  ["kilobytes"]    = 1024,    -- 2 ^ 10 bytes
+  ["megabytes"]    = 1048576, -- 2 ^ 20 bytes
+}
+RequestSizeLimitingHandler.unit_multiplication_factor = unit_multiplication_factor
+
+
+local function check_size(length, allowed_size, headers, unit)
+  local allowed_bytes_size = allowed_size * unit_multiplication_factor[unit]
   if length > allowed_bytes_size then
     if headers.expect and strip(headers.expect:lower()) == "100-continue" then
       return kong.response.exit(417, { message = "Request size limit exceeded" })
@@ -26,12 +42,12 @@ function RequestSizeLimitingHandler:access(conf)
   local cl = headers["content-length"]
 
   if cl and tonumber(cl) then
-    check_size(tonumber(cl), conf.allowed_payload_size, headers)
+    check_size(tonumber(cl), conf.allowed_payload_size, headers, conf.size_unit)
   else
     -- If the request body is too big, this could consume too much memory (to check)
     local data = kong.request.get_raw_body()
     if data then
-      check_size(#data, conf.allowed_payload_size, headers)
+      check_size(#data, conf.allowed_payload_size, headers, conf.size_unit)
     end
   end
 end
--- kong/plugins/request-size-limiting/schema.lua
+++ kong/plugins/request-size-limiting/schema.lua
@@ -1,4 +1,8 @@
 local typedefs = require "kong.db.schema.typedefs"
+local handler = require "kong.plugins.request-size-limiting.handler"
+
+
+local size_units = handler.size_units
 
 
 return {
@@ -10,6 +14,7 @@ return {
         type = "record",
         fields = {
           { allowed_payload_size = { type = "integer", default = 128 }, },
+          { size_unit = { type = "string", required = true, default = size_units[1], one_of = size_units }, },
         },
       },
     },
