a233e92ac715686f806cbde3b1472332f5a437b1
Darren Jennings <dmjenn02@gmail.com>
2019-10-16 08:09:59 -0400

    fix(nginx) admin api add no-cache header (#1379)

--- kong/templates/nginx_kong.lua
+++ kong/templates/nginx_kong.lua
@@ -465,7 +465,7 @@ server {
         default_type application/json;
         content_by_lua_block {
             Kong.serve_admin_api({
-                acah = "Content-Type, ${{RBAC_AUTH_HEADER}}, Kong-Request-Type",
+                acah = "Content-Type, ${{RBAC_AUTH_HEADER}}, Kong-Request-Type, Cache-Control",
             })
         }
 
