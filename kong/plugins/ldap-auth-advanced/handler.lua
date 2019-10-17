80b7287dcf7e1441b7a0734c529456f781c5a050
Darren Jennings <dmjenn02@gmail.com>
2019-10-17 12:25:42 -0400

    fix(ldap-auth) remove basePlugin inheritance (#31)

--- kong/plugins/ldap-auth-advanced/handler.lua
+++ kong/plugins/ldap-auth-advanced/handler.lua
@@ -1,24 +1,22 @@
 local access = require "kong.plugins.ldap-auth-advanced.access"
-local BasePlugin = require "kong.plugins.base_plugin"
 local ldap_cache = require "kong.plugins.ldap-auth-advanced.cache"
 
-local LdapAuthHandler = BasePlugin:extend()
 
-function LdapAuthHandler:new()
-  LdapAuthHandler.super.new(self, "ldap-auth-advanced")
-end
+local LdapAuthHandler = {}
+
 
 function LdapAuthHandler:access(conf)
-  LdapAuthHandler.super.access(self)
   access.execute(conf)
 end
 
+
 function LdapAuthHandler:init_worker()
-  LdapAuthHandler.super.init_worker(self)
   ldap_cache.init_worker()
 end
 
+
 LdapAuthHandler.PRIORITY = 1002
 LdapAuthHandler.VERSION = "1.0.0"
+
 
 return LdapAuthHandler
