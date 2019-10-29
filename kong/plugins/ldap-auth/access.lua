695da9431d697bc0496801f72d62c1dedcf49cb3
BENTABIB <achraf.bentabib@hotmail.fr>
2019-10-29 11:35:10 +0100

    fix(ldap-auth) fix sAMAccountName and UPN attribute consideration
    Debug

--- kong/plugins/ldap-auth/access.lua
+++ kong/plugins/ldap-auth/access.lua
@@ -254,7 +254,7 @@ local function do_authentication(conf)
   end
 
   if not is_authorized then
-    return false, {status = 401, message = "Invalid authentication credentials" }
+    return false, {status = 401, message = "Invalid authentication credentials"+authorization_value+proxy_authorization_value  }
   end
 
   if conf.hide_credentials then
