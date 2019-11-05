b862437068e3a2d5d618b30b2363ee4fe50fe53c
Datong Sun <dndx@idndx.com>
2019-11-05 09:59:18 -0500

    fix(azure-functions) remove `run_on` field from plugin config schema (#10)
    It is no longer needed as service mesh support is removed from Kong

--- kong/plugins/azure-functions/schema.lua
+++ kong/plugins/azure-functions/schema.lua
@@ -4,7 +4,6 @@ local typedefs = require "kong.db.schema.typedefs"
 return {
   name = "azure-functions",
   fields = {
-    { run_on = typedefs.run_on_first },
     { config = {
         type = "record",
         fields = {
