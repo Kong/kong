f709e2888e1727184621d314a80442544f14242f
Datong Sun <dndx@idndx.com>
2019-11-05 09:59:02 -0500

    fix(zipkin) remove `run_on` field from plugin schema (#54)
    It is no longer needed as service mesh support is removed from Kong

--- kong/plugins/zipkin/schema.lua
+++ kong/plugins/zipkin/schema.lua
@@ -3,7 +3,6 @@ local typedefs = require "kong.db.schema.typedefs"
 return {
   name = "zipkin",
   fields = {
-    { run_on = typedefs.run_on { default = "all" } },
     { config = {
         type = "record",
         fields = {
