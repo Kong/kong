0c713f5a58eb5219070568ec4b9527abfcf8ba03
Enrique García Cota <kikito@gmail.com>
2019-12-03 17:51:48 +0100

    feat(router) add routes.path_handling attribute
    This change introduces a new Routes attribute called path_handling.
    
    It complements #5216 and #5227 by making it possible to select the
    behavior the router will have when combining the Service Path, the Route
    Path and the Request path into a single path sent to the upstream.
    
    For now `path_handling` accepts two values:
    
    * "v0" makes paths in the router behave like in Kong 0.x (like they did
      before #3780)
    * "v1" makes paths behave like in Kong 1.x (like they did after #3780)
    
    Notice that in order to add this new attribute a lot of tests needed to
    be duplicated. The cases were v0 differs from v1 have been marked with
    `-- !` for easy identification.

--- kong/router.lua
+++ kong/router.lua
@@ -19,6 +19,7 @@ local sort          = table.sort
 local upper         = string.upper
 local lower         = string.lower
 local find          = string.find
+local byte          = string.byte
 local sub           = string.sub
 local tonumber      = tonumber
 local ipairs        = ipairs
@@ -29,6 +30,7 @@ local max           = math.max
 local band          = bit.band
 local bor           = bit.bor
 
+local SLASH         = byte("/")
 
 local ERR           = ngx.ERR
 local WARN          = ngx.WARN
@@ -1435,21 +1437,72 @@ function _M.new(routes)
               local request_postfix = matches.uri_postfix or sub(req_uri, 2, -1)
               local upstream_base = upstream_url_t.path or "/"
 
-              if matched_route.strip_uri then
-                -- we drop the matched part, replacing it with the upstream path
-                if sub(upstream_base, -1, -1) == "/" and
-                   sub(request_postfix, 1, 1) == "/" then
-                  -- double "/", so drop the first
-                  upstream_uri = sub(upstream_base, 1, -2) .. request_postfix
+              if matched_route.route.path_handling == "v1" then
+                if matched_route.strip_uri then
+                  -- we drop the matched part, replacing it with the upstream path
+                  if byte(upstream_base, -1) == SLASH and
+                     byte(request_postfix, 1) == SLASH then
+                    -- double "/", so drop the first
+                    upstream_uri = sub(upstream_base, 1, -2) .. request_postfix
+
+                  else
+                    upstream_uri = upstream_base .. request_postfix
+                  end
 
                 else
-                  upstream_uri = upstream_base .. request_postfix
+                  -- we retain the incoming path, just prefix it with the upstream
+                  -- path, but skip the initial slash
+                  upstream_uri = upstream_base .. sub(req_uri, 2, -1)
                 end
 
-              else
-                -- we retain the incoming path, just prefix it with the upstream
-                -- path, but skip the initial slash
-                upstream_uri = upstream_base .. sub(req_uri, 2, -1)
+              else -- matched_route.route.path_handling == "v0"
+                if byte(upstream_base, -1) == SLASH then
+                  -- ends with / and strip_uri = true
+                  if matched_route.strip_uri then
+                    if request_postfix == "" then
+                      if upstream_base == "/" then
+                        upstream_uri = "/"
+                      elseif byte(req_uri, -1) == SLASH then
+                        upstream_uri = upstream_base
+                      else
+                        upstream_uri = sub(upstream_base, 1, -2)
+                      end
+                    elseif byte(request_postfix, 1, 1) == SLASH then
+                      -- double "/", so drop the first
+                      upstream_uri = sub(upstream_base, 1, -2) .. request_postfix
+                    else -- ends with / and strip_uri = true, no double slash
+                      upstream_uri = upstream_base .. request_postfix
+                    end
+
+                  else -- ends with / and strip_uri = false
+                    -- we retain the incoming path, just prefix it with the upstream
+                    -- path, but skip the initial slash
+                    upstream_uri = upstream_base .. sub(req_uri, 2)
+                  end
+
+                else -- does not end with /
+                  -- does not end with / and strip_uri = true
+                  if matched_route.strip_uri then
+                    if request_postfix == "" then
+                      if #req_uri > 1 and byte(req_uri, -1) == SLASH then
+                        upstream_uri = upstream_base .. "/"
+                      else
+                        upstream_uri = upstream_base
+                      end
+                    elseif byte(request_postfix, 1, 1) == SLASH then
+                      upstream_uri = upstream_base .. request_postfix
+                    else
+                      upstream_uri = upstream_base .. "/" .. request_postfix
+                    end
+
+                  else -- does not end with / and strip_uri = false
+                    if req_uri == "/" then
+                      upstream_uri = upstream_base
+                    else
+                      upstream_uri = upstream_base .. req_uri
+                    end
+                  end
+                end
               end
 
               -- preserve_host header logic
