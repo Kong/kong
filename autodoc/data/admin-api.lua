bcd87e5892b25ea79cbb94317b2be351cf68cbd1
Enrique García Cota <kikito@gmail.com>
2019-12-04 11:26:38 +0100

    docs(admin-api) document Route.path_handling in autodoc

--- autodoc/data/admin-api.lua
+++ autodoc/data/admin-api.lua
@@ -611,6 +611,35 @@ return {
             strip the matching prefix from the upstream request URL.
           ]]
         },
+        path_handling = {
+          description = [[
+            Controls how the Service Path, Route Path and requested path are combined when sending a request to the
+            upstream.
+
+            `"v0"` is the behavior used in Kong 0.x. It treats `service.path`, `route.path` and request path as
+            *segments* of a url. It will always join them via slashes. Given a service path `/s`, route path `/r`
+            and request path `/re`, the concatenated path will be `/s/re`. If the resulting path is a single slash,
+            no further transformation is done to it. If it's longer, then the trailing slash is removed.
+
+            `"v1"` is the behavior used in Kong 1.x. It treats `service.path` as a *prefix*, and ignores the initial
+            slashes of the request and route paths. Given service path `/s`, route path `/r` and request path `/re`,
+            the concatenated path will be `/sre`.
+
+            Both versions of the algorithm detect "double slashes" when combining paths, replacing them by single
+            slashes.
+
+            | `service.path` | `route.path` | `route.strip_path` | `route.path_handling` | request path | proxied path  |
+            |----------------|--------------|--------------------|-----------------------|--------------|---------------|
+            | `/s`           | `/fv0`       | `false`            | `v0`                  | `/fv0req`    | `/s/fv0req`   |
+            | `/s`           | `/fv1`       | `false`            | `v1`                  | `/fv1req`    | `/sfv1req`    |
+            | `/s`           | `/tv0`       | `true`             | `v0`                  | `/tv0req`    | `/s/req`      |
+            | `/s`           | `/tv1`       | `true`             | `v1`                  | `/tv1req`    | `/sreq`       |
+            | `/s`           | `/fv0/`      | `false`            | `v0`                  | `/fv0/req`   | `/s/fv0/req`  |
+            | `/s`           | `/fv1/`      | `false`            | `v1`                  | `/fv1/req`   | `/sfv1/req`   |
+            | `/s`           | `/tv0/`      | `true`             | `v0`                  | `/tv0/req`   | `/s/req`      |
+            | `/s`           | `/tv1/`      | `true`             | `v1`                  | `/tv1/req    | `/sreq`       |
+          ]]
+        },
         preserve_host = {
           description = [[
             When matching a Route via one of the `hosts` domain names,
