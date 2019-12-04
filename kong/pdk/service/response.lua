668307c2bac67352bb0eb5f8ca1ba8fd22566b76
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2019-12-04 20:59:58 +0200

    feat(pdk) implement kong.service.response.get_raw_body and .get_body

--- kong/pdk/service/response.lua
+++ kong/pdk/service/response.lua
@@ -2,6 +2,9 @@
 -- Manipulation of the response from the Service
 -- @module kong.service.response
 
+
+local cjson = require "cjson.safe".new()
+local multipart = require "multipart"
 local phase_checker = require "kong.pdk.private.phases"
 
 
@@ -9,6 +12,7 @@ local ngx = ngx
 local sub = string.sub
 local fmt = string.format
 local gsub = string.gsub
+local find = string.find
 local type = type
 local error = error
 local lower = string.lower
@@ -16,6 +20,9 @@ local tonumber = tonumber
 local getmetatable = getmetatable
 local setmetatable = setmetatable
 local check_phase = phase_checker.check
+
+
+cjson.decode_array_with_array_mt(true)
 
 
 local PHASES = phase_checker.phases
@@ -63,6 +70,16 @@ end
 local function new(pdk, major_version)
   local response = {}
 
+
+  local MIN_POST_ARGS          = 1
+  local MAX_POST_ARGS_DEFAULT  = 100
+  local MAX_POST_ARGS          = 1000
+
+  local CONTENT_TYPE           = "Content-Type"
+
+  local CONTENT_TYPE_POST      = "application/x-www-form-urlencoded"
+  local CONTENT_TYPE_JSON      = "application/json"
+  local CONTENT_TYPE_FORM_DATA = "multipart/form-data"
 
   local MIN_HEADERS            = 1
   local MAX_HEADERS_DEFAULT    = 100
@@ -182,13 +199,101 @@ local function new(pdk, major_version)
   end
 
 
+  ---
+  -- Returns the raw buffered body.
+  --
+  -- @function kong.service.response.get_raw_body
+  -- @phases `header_filter`, `body_filter`, `log`
+  -- @treturn string body The raw buffered body
+  -- @usage
+  -- -- Plugin needs to call kong.service.request.enable_buffering() on `rewrite`
+  -- -- or `access` phase prior calling this function.
+  --
+  -- local body = kong.service.response.get_raw_body()
   function response.get_raw_body()
-    -- TODO: implement
+    check_phase(header_body_log)
+    if not pdk.ctx.core.buffered_proxying then
+      error("service body is only available with buffered proxying " ..
+            "(see: kong.service.request.enable_buffering function)", 2)
+    end
+
+    return pdk.ctx.core.buffered_body or ""
   end
 
 
-  function response.get_body()
-    -- TODO: implement
+  ---
+  -- Returns the decoded buffered body.
+  --
+  -- @function kong.service.response.get_body
+  -- @phases `header_filter`, `body_filter`, `log`
+  -- @tparam string mimetype The mime-type of the response (if known)
+  -- @tparam[opt] string mimetype the MIME type
+  -- @tparam[opt] number max_args set a limit on the maximum number of parsed
+  -- @treturn string body The raw buffered body
+  -- @usage
+  -- -- Plugin needs to call kong.service.request.enable_buffering() on `rewrite`
+  -- -- or `access` phase prior calling this function.
+  --
+  -- local body = kong.service.response.get_body()
+  function response.get_body(mimetype, max_args)
+    check_phase(header_body_log)
+    if not pdk.ctx.core.buffered_proxying then
+      error("service body is only available with buffered proxying " ..
+            "(see: kong.service.request.enable_buffering function)", 2)
+    end
+
+    local content_type = mimetype or response.get_header(CONTENT_TYPE)
+    if not content_type then
+      return nil, "missing content type"
+    end
+
+    local content_type_lower = lower(content_type)
+    do
+      local s = find(content_type_lower, ";", 1, true)
+      if s then
+        content_type_lower = sub(content_type_lower, 1, s - 1)
+      end
+    end
+
+    if find(content_type_lower, CONTENT_TYPE_POST, 1, true) == 1 then
+      if max_args ~= nil then
+        if type(max_args) ~= "number" then
+          error("max_args must be a number", 2)
+
+        elseif max_args < MIN_POST_ARGS then
+          error("max_args must be >= " .. MIN_POST_ARGS, 2)
+
+        elseif max_args > MAX_POST_ARGS then
+          error("max_args must be <= " .. MAX_POST_ARGS, 2)
+        end
+      end
+
+      local body = response.get_raw_body()
+      local pargs, err = ngx.decode_args(body, max_args or MAX_POST_ARGS_DEFAULT)
+      if not pargs then
+        return nil, err, CONTENT_TYPE_POST
+      end
+
+      return pargs, nil, CONTENT_TYPE_POST
+
+    elseif find(content_type_lower, CONTENT_TYPE_JSON, 1, true) == 1 then
+      local body = response.get_raw_body()
+      local json = cjson.decode(body)
+      if type(json) ~= "table" then
+        return nil, "invalid json body", CONTENT_TYPE_JSON
+      end
+
+      return json, nil, CONTENT_TYPE_JSON
+
+    elseif find(content_type_lower, CONTENT_TYPE_FORM_DATA, 1, true) == 1 then
+      local body = response.get_raw_body()
+
+      -- TODO: multipart library doesn't support multiple fields with same name
+      return multipart(body, content_type):get_all(), nil, CONTENT_TYPE_FORM_DATA
+
+    else
+      return nil, "unsupported content type '" .. content_type .. "'", content_type_lower
+    end
   end
 
 
