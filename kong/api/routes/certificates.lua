f05711f2c06062af8701508f2de088c5309b35de
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2019-12-10 20:57:30 +0200

    fix(admin-api) incorrect PUT behavior for /certificates
    ### Summary
    
    @hbagdi reported issue on `PUT` behavior on `/certificates` with #5309.
    
    This PR fixes the `PUT` behavior. It also fixes several other issues
    found while fixing the original issue.
    
    ### Issues Resolver
    
    Fix #5309

--- kong/api/routes/certificates.lua
+++ kong/api/routes/certificates.lua
@@ -1,72 +1,122 @@
 local endpoints   = require "kong.api.endpoints"
+local arguments   = require "kong.api.arguments"
 local utils       = require "kong.tools.utils"
-local Set         = require "pl.Set"
 
 
+local ngx = ngx
 local kong = kong
+local type = type
+local find = string.find
+local lower = string.lower
 local unescape_uri = ngx.unescape_uri
 
 
-local function get_cert_id_from_sni(self, db, helpers)
+local function prepare_params(self)
   local id = unescape_uri(self.params.certificates)
-  if utils.is_valid_uuid(id) then
-    return
+  local method = self.req.method
+  local name
+  if not utils.is_valid_uuid(id) then
+    name = id
+
+    local sni, _, err_t = kong.db.snis:select_by_name(name)
+    if err_t then
+      return endpoints.handle_error(err_t)
+    end
+
+    if sni then
+      id = sni.certificate.id
+
+    else
+      if method ~= "PUT" then
+        return kong.response.exit(404, { message = "SNI not found" })
+      end
+
+      id = utils.uuid()
+    end
   end
 
-  local sni, _, err_t = db.snis:select_by_name(id)
-  if err_t then
-    return endpoints.handle_error(err_t)
+  self.params.certificates = id
+  self.params.name = name
+end
+
+
+local function prepare_args(self)
+  local name_field
+  do
+    local content_type = ngx.var.content_type
+    if content_type then
+      content_type = lower(content_type)
+      if find(content_type, "application/x-www-form-urlencoded", 1, true) == 1 or
+         find(content_type, "multipart/form-data",               1, true) == 1 then
+        name_field = kong.db.snis.schema.fields.name
+      end
+    end
   end
 
-  if sni then
-    self.params.certificates = sni.certificate.id
-    return
+  local method = self.req.method
+  local snis = self.args.post.snis
+  local name = self.params.name
+
+  if type(snis) == "table" then
+    local count = #snis
+
+    if name and method == "PUT" then
+      count = count + 1
+      snis[count] = name
+    end
+
+    if name_field then
+      for i=1, count do
+        snis[i] = arguments.infer_value(snis[i], name_field)
+      end
+    end
+
+  elseif type(snis) == "string" then
+    if name_field then
+      snis = arguments.infer_value(snis, name_field)
+    end
+
+    if name and method == "PUT" and name ~= snis then
+      snis = { snis, name }
+    else
+      snis = { snis }
+    end
   end
 
-  if self.req.method == "PUT" then
-    return
+  if not snis and method == "PUT" then
+    snis = ngx.null
   end
 
-  return kong.response.exit(404, { message = "SNI not found" })
+  self.params.name = nil
+  self.args.post.snis = snis
 end
 
 
 return {
   ["/certificates/:certificates"] = {
-    before = get_cert_id_from_sni,
+    before = prepare_params,
 
     -- override to include the snis list when getting an individual certificate
     GET = endpoints.get_entity_endpoint(kong.db.certificates.schema,
                                         nil, nil, "select_with_name_list"),
 
-    -- override to create a new SNI in the PUT /certificates/foo.com (create) case
-    PUT = function(self, db, helpers)
-      local cert, err_t, _
-      local id = unescape_uri(self.params.certificates)
-
-      -- cert was found via id or sni inside `before` section
-      if utils.is_valid_uuid(id) then
-        cert, _, err_t = endpoints.upsert_entity(self, db, db.certificates.schema)
-
-      else -- create a new cert. Add extra sni if provided on url
-        self.args.post.snis = Set.values(Set(self.args.post.snis or {}) + id)
-        cert, _, err_t = endpoints.insert_entity(self, db, db.certificates.schema)
-      end
-
-      if err_t then
-        return endpoints.handle_error(err_t)
-      end
-
-      if not cert then
-        return kong.response.exit(404, { message = "Not found" })
-      end
-
-      return kong.response.exit(200, cert)
+    PUT = function(self, _, _, parent)
+      prepare_args(self)
+      return parent()
     end,
+
+    PATCH = function(self, _, _, parent)
+      prepare_args(self)
+      return parent()
+    end
   },
 
   ["/certificates/:certificates/snis"] = {
-    before = get_cert_id_from_sni,
+    before = prepare_params,
+  },
+
+  ["/certificates/:certificates/snis/:snis"] = {
+    before = prepare_params,
   },
 }
 
