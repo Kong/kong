7a4a996e711a93c56af396281833dbe2a349ca54
Colin Hutchinson <hutchic@users.noreply.github.com>
2019-12-10 14:27:06 -0500

    tests(plugins) add capability to test externally hosted but bundled Kong plugins (#5305)
    All bundled Kong plugins are inferred from the rockspec as always being `kong-` and `~`. From there we determine git tag from the result of `luarocks show`.
    
    Once we have the plugin repository and git tag we clone the plugin source code, copy the necessary fixtures and then run the plugin spec tests.
    
    * test(plugins) add capability to test externally hosted but bundled Kong plugins
    
    * chore(ci) enable azure-functions and report status if any failed
    
    * chore(ci) combine external plugins into the same CI job
    
    * chore(ci) nicer output for plugin tests
    
    * chore(ci) remove the added stage we no longer use
    
    * chore(dependency) bump the zipkin plugin
    
    * chore(tests) exclude zipkin from the CI tests until next release
    
    * chore(test) dont delete the plugin code we used for testing

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+function cyan() {
+    echo -e "\033[1;36m$*\033[0m"
+}
+function red() {
+    echo -e "\033[1;31m$*\033[0m"
+}
+
 export BUSTED_ARGS="-o gtest -v --exclude-tags=flaky,ipv6"
 
 if [ "$KONG_TEST_DATABASE" == "postgres" ]; then
@@ -23,7 +30,51 @@ if [ "$TEST_SUITE" == "dbless" ]; then
                      spec/02-integration/04-admin_api/15-off_spec.lua
 fi
 if [ "$TEST_SUITE" == "plugins" ]; then
-    eval "$TEST_CMD" spec/03-plugins/
+    set +e
+    rm -f .failed
+
+    for p in spec/03-plugins/*; do
+        echo
+        cyan "--------------------------------------"
+        cyan $(basename $p)
+        cyan "--------------------------------------"
+        echo
+
+        $TEST_CMD $p || echo "* $p" >> .failed
+    done
+
+    cat kong-*.rockspec | grep kong- | grep -v zipkin | grep -v sidecar | grep "~" | while read line ; do
+        REPOSITORY=`echo $line | sed "s/\"/ /g" | awk -F" " '{print $1}'`
+        VERSION=`luarocks show $REPOSITORY | grep $REPOSITORY | head -1 | awk -F" " '{print $2}' | cut -f1 -d"-"`
+        REPOSITORY=`echo $REPOSITORY | sed -e 's/kong-prometheus-plugin/kong-plugin-prometheus/g'`
+        REPOSITORY=`echo $REPOSITORY | sed -e 's/kong-proxy-cache-plugin/kong-plugin-proxy-cache/g'`
+
+        echo
+        cyan "--------------------------------------"
+        cyan $REPOSITORY $VERSION
+        cyan "--------------------------------------"
+        echo
+
+        git clone https://github.com/Kong/$REPOSITORY.git --branch $VERSION --single-branch /tmp/test-$REPOSITORY
+        cp -R /tmp/test-$REPOSITORY/spec/fixtures/* spec/fixtures/ || true
+        pushd /tmp/test-$REPOSITORY
+        luarocks make
+        popd
+
+        $TEST_CMD /tmp/test-$REPOSITORY/spec/ || echo "* $REPOSITORY" >> .failed
+
+    done
+
+    if [ -f .failed ]; then
+        echo
+        red "--------------------------------------"
+        red "Plugin tests failed:"
+        red "--------------------------------------"
+        cat .failed
+        exit 1
+    else
+        exit 0
+    fi
 fi
 if [ "$TEST_SUITE" == "pdk" ]; then
     TEST_NGINX_RANDOMIZE=1 prove -I. -j$JOBS -r t/01-pdk
