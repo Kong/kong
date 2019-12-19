838e54e9fe8660d13fc8c7180604e8b6c13b3aed
Hisham Muhammad <hisham@gobolinux.org>
2019-12-19 20:37:06 -0300

    feat(migrations) add path_handling with smart defaults
    If migrating from 0.14.1, initialize `path_handling` with `'v0'`.
    
    If migrating from 1.x, create `path_handling` it with a default of `'v1'` to
    fill existing rows, then change the default.

--- kong/db/migrations/core/007_140_to_200.lua
+++ kong/db/migrations/core/007_140_to_200.lua
@@ -1,13 +1,16 @@
 return {
   postgres = {
     up = [[
+      -- If migrating from 1.x, the "path_handling" column does not exist yet.
+      -- Create it with a default of 'v1' to fill existing rows, then change the default.
       DO $$
       BEGIN
-        ALTER TABLE IF EXISTS ONLY "routes" ADD "path_handling" TEXT;
+        ALTER TABLE IF EXISTS ONLY "routes" ADD "path_handling" TEXT DEFAULT 'v1';
       EXCEPTION WHEN DUPLICATE_COLUMN THEN
         -- Do nothing, accept existing state
       END;
       $$;
+      ALTER TABLE IF EXISTS ONLY "routes" ALTER COLUMN "path_handling" SET DEFAULT 'v0';
     ]],
 
     teardown = function(connector)
@@ -36,6 +39,22 @@ return {
     ]],
 
     teardown = function(connector)
+      local coordinator = assert(connector:connect_migrations())
+
+      for rows, err in coordinator:iterate([[SELECT * FROM routes]]) do
+        if err then
+          return nil, err
+        end
+
+        for _, row in ipairs(rows) do
+          if row.path_handling ~= "v0" then
+            assert(connector:query([[
+              UPDATE routes SET path_handling = 'v1'
+              WHERE partition = 'routes' AND id = ]] .. row.id))
+          end
+        end
+      end
+
       assert(connector:query([[
         DROP INDEX IF EXISTS plugins_run_on_idx;
         ALTER TABLE plugins DROP run_on;
