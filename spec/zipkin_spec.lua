bf0809240b48d70ce7084904494a2d309c956cad
Guilherme Salazar <gsz@acm.org>
2019-12-19 14:30:46 -0800

    tests(zipkin) refactor tests
    - Use an actual Zipkin server in tests
    - Remove dependency on lua-http and cqueues, by using Kong's existing
    helper HTTP client and Zipkin server

--- spec/zipkin_spec.lua
+++ spec/zipkin_spec.lua
@@ -1,11 +1,8 @@
-local TEST_TIMEOUT = 2
-
-local cqueues = require "cqueues"
 local helpers = require "spec.helpers"
-local http_request = require "http.request"
-local http_server = require "http.server"
-local new_headers = require "http.headers".new
 local cjson = require "cjson"
+local utils = require "kong.tools.utils"
+local to_hex = require "resty.string".to_hex
+
 
 -- Transform zipkin annotations into a hash of timestamps. It assumes no repeated values
 -- input: { { value = x, timestamp = y }, { value = x2, timestamp = y2 } }
@@ -19,48 +16,24 @@ local function annotations_to_hash(annotations)
   return hash
 end
 
+
 local function assert_is_integer(number)
   assert.equals("number", type(number))
   assert.equals(number, math.floor(number))
 end
 
 
+local function gen_trace_id()
+  return to_hex(utils.get_rand_bytes(8, true))
+end
+
 
 for _, strategy in helpers.each_strategy() do
-describe("integration tests with mock zipkin server [#" .. strategy .. "]", function()
-  local server
-
-  local cb
-  local proxy_port, proxy_host
-  local zipkin_port, zipkin_host
+describe("integration tests with zipkin server [#" .. strategy .. "]", function()
   local proxy_client_grpc
   local route, grpc_route
-
-  after_each(function()
-    cb = nil
-  end)
-
-  local with_server do
-    local function assert_loop(cq, timeout)
-      local ok, err, _, thd = cq:loop(timeout)
-      if not ok then
-        if thd then
-          err = debug.traceback(thd, err)
-        end
-        error(err, 2)
-      end
-    end
-
-    with_server = function(server_cb, client_cb)
-      cb = spy.new(server_cb)
-      local cq = cqueues.new()
-      cq:wrap(assert_loop, server)
-      cq:wrap(client_cb)
-      assert_loop(cq, TEST_TIMEOUT)
-      return (cb:called())
-    end
-  end
-
+  local zipkin_client
+  local proxy_client
 
   -- the following assertions should be true on any span list, even in error mode
   local function assert_span_invariants(request_span, proxy_span, expected_name)
@@ -75,7 +48,10 @@ describe("integration tests with mock zipkin server [#" .. strategy .. "]", func
     assert.same("string", type(request_span.traceId))
     assert.truthy(request_span.traceId:match("^%x+$"))
     assert_is_integer(request_span.timestamp)
-    assert.truthy(request_span.duration >= proxy_span.duration)
+
+    if request_span.duration and proxy_span.duration then
+      assert.truthy(request_span.duration >= proxy_span.duration)
+    end
 
     assert.equals(2, #request_span.annotations)
     local rann = annotations_to_hash(request_span.annotations)
@@ -83,7 +59,7 @@ describe("integration tests with mock zipkin server [#" .. strategy .. "]", func
     assert_is_integer(rann["krf"])
     assert.truthy(rann["krs"] <= rann["krf"])
 
-    assert.same(ngx.null, request_span.localEndpoint)
+    assert.is_nil(request_span.localEndpoint)
 
     -- proxy_span
     assert.same("table", type(proxy_span))
@@ -96,7 +72,10 @@ describe("integration tests with mock zipkin server [#" .. strategy .. "]", func
     assert.same("string", type(proxy_span.traceId))
     assert.truthy(proxy_span.traceId:match("^%x+$"))
     assert_is_integer(proxy_span.timestamp)
-    assert.truthy(proxy_span.duration >= 0)
+
+    if request_span.duration and proxy_span.duration then
+      assert.truthy(proxy_span.duration >= 0)
+    end
 
     assert.equals(6, #proxy_span.annotations)
     local pann = annotations_to_hash(proxy_span.annotations)
@@ -117,25 +96,6 @@ describe("integration tests with mock zipkin server [#" .. strategy .. "]", func
 
 
   setup(function()
-    -- create a mock zipkin server
-    server = assert(http_server.listen {
-      host = "127.0.0.1",
-      port = 0,
-      onstream = function(_, stream)
-        local req_headers = assert(stream:get_headers())
-        local res_headers = new_headers()
-        res_headers:upsert(":status", "500")
-        res_headers:upsert("connection", "close")
-        assert(cb, "test has not set callback")
-        local body = cb(req_headers, res_headers, stream)
-        assert(stream:write_headers(res_headers, false))
-        assert(stream:write_chunk(body or "", true))
-      end,
-    })
-    assert(server:listen())
-    local _
-    _, zipkin_host, zipkin_port = server:localname()
-
     local bp = helpers.get_db_utils(strategy, { "services", "routes", "plugins" })
 
     -- enable zipkin plugin globally pointing to mock server
@@ -143,7 +103,7 @@ describe("integration tests with mock zipkin server [#" .. strategy .. "]", func
       name = "zipkin",
       config = {
         sample_ratio = 1,
-        http_endpoint = string.format("http://%s:%d/api/v2/spans", zipkin_host, zipkin_port),
+        http_endpoint = "http://127.0.0.1:9411/api/v2/spans",
       }
     })
 
@@ -170,198 +130,213 @@ describe("integration tests with mock zipkin server [#" .. strategy .. "]", func
       nginx_conf = "spec/fixtures/custom_nginx.template",
     })
 
-    proxy_host = helpers.get_proxy_ip(false)
-    proxy_port = helpers.get_proxy_port(false)
+    proxy_client = helpers.proxy_client()
     proxy_client_grpc = helpers.proxy_client_grpc()
+    zipkin_client = helpers.http_client("127.0.0.1", 9411)
   end)
 
   teardown(function()
-    server:close()
     helpers.stop_kong()
   end)
 
   it("generates spans, tags and annotations for regular requests", function()
-    assert.truthy(with_server(function(_, _, stream)
-      local spans = cjson.decode((assert(stream:get_body_as_string())))
+    local trace_id = gen_trace_id()
 
-      assert.equals(3, #spans)
-      local balancer_span, proxy_span, request_span = spans[1], spans[2], spans[3]
-      -- common assertions for request_span and proxy_span
-      assert_span_invariants(request_span, proxy_span, "GET")
+    local r = assert(proxy_client:send {
+      method  = "GET",
+      path    = "/",
+      headers = {
+        ["x-b3-traceid"] = trace_id,
+        ["x-b3-sampled"] = "1",
+        host  = "mock-http-route",
+      },
+    })
+    assert.response(r).has.status(200)
 
-      -- specific assertions for request_span
-      local request_tags = request_span.tags
-      assert.truthy(request_tags["kong.node.id"]:match("^[%x-]+$"))
-      request_tags["kong.node.id"] = nil
-      assert.same({
-        ["http.method"] = "GET",
-        ["http.path"] = "/",
-        ["http.status_code"] = "200", -- found (matches server status)
-        lc = "kong"
-      }, request_tags)
-      local peer_port = request_span.remoteEndpoint.port
-      assert_is_integer(peer_port)
-      assert.same({ ipv4 = "127.0.0.1", port = peer_port }, request_span.remoteEndpoint)
+    local spans
+    helpers.wait_until(function()
+      local res = assert(zipkin_client:get("/api/v2/trace/" .. trace_id))
+      spans = cjson.decode(assert.response(res).has.status(200))
+      return #spans == 3
+    end)
 
-      -- specific assertions for proxy_span
-      assert.same(proxy_span.tags["kong.route"], route.id)
-      assert.same(proxy_span.tags["peer.hostname"], "127.0.0.1")
+    local balancer_span, proxy_span, request_span = spans[1], spans[2], spans[3]
+    -- common assertions for request_span and proxy_span
+    assert_span_invariants(request_span, proxy_span, "get")
 
-      assert.same({ ipv4 = helpers.mock_upstream_host, port = helpers.mock_upstream_port },
-        proxy_span.remoteEndpoint)
+    -- specific assertions for request_span
+    local request_tags = request_span.tags
+    assert.truthy(request_tags["kong.node.id"]:match("^[%x-]+$"))
+    request_tags["kong.node.id"] = nil
+    assert.same({
+      ["http.method"] = "GET",
+      ["http.path"] = "/",
+      ["http.status_code"] = "200", -- found (matches server status)
+      lc = "kong"
+    }, request_tags)
+    local peer_port = request_span.remoteEndpoint.port
+    assert.equals("number", type(peer_port))
+    assert.same({ ipv4 = "127.0.0.1", port = peer_port }, request_span.remoteEndpoint)
 
-      -- specific assertions for balancer_span
-      assert.equals(balancer_span.parentId, request_span.id)
-      assert.equals(request_span.name .. " (balancer try 1)", balancer_span.name)
-      assert_is_integer(balancer_span.timestamp)
-      assert_is_integer(balancer_span.duration)
+    -- specific assertions for proxy_span
+    assert.same(proxy_span.tags["kong.route"], route.id)
+    assert.same(proxy_span.tags["peer.hostname"], "127.0.0.1")
 
-      assert.same({ ipv4 = helpers.mock_upstream_host, port = helpers.mock_upstream_port },
-        balancer_span.remoteEndpoint)
-      assert.equals(ngx.null, balancer_span.localEndpoint)
-      assert.same({
-        error = "false",
-        ["kong.balancer.try"] = "1",
-      }, balancer_span.tags)
-    end, function()
-      -- regular request which matches the existing route
-      local req = http_request.new_from_uri("http://mock-http-route/")
-      req.host = proxy_host
-      req.port = proxy_port
-      assert(req:go())
-    end))
+    assert.same({ ipv4 = helpers.mock_upstream_host, port = helpers.mock_upstream_port },
+    proxy_span.remoteEndpoint)
+
+    -- specific assertions for balancer_span
+    assert.equals(balancer_span.parentId, request_span.id)
+    assert.equals(request_span.name .. " (balancer try 1)", balancer_span.name)
+    assert.equals("number", type(balancer_span.timestamp))
+
+    if balancer_span.duration then
+      assert.equals("number", type(balancer_span.duration))
+    end
+
+    assert.same({ ipv4 = helpers.mock_upstream_host, port = helpers.mock_upstream_port },
+    balancer_span.remoteEndpoint)
+    assert.is_nil(balancer_span.localEndpoint)
+    assert.same({
+      error = "false",
+      ["kong.balancer.try"] = "1",
+    }, balancer_span.tags)
   end)
 
   it("generates spans, tags and annotations for regular requests (#grpc)", function()
-    assert.truthy(with_server(function(_, _, stream)
-      local spans = cjson.decode((assert(stream:get_body_as_string())))
+    local trace_id = gen_trace_id()
 
-      --assert.equals(3, #spans)
-      local balancer_span, proxy_span, request_span = spans[1], spans[2], spans[3]
-      -- common assertions for request_span and proxy_span
-      assert_span_invariants(request_span, proxy_span, "POST")
+    local ok, resp = proxy_client_grpc({
+      service = "hello.HelloService.SayHello",
+      body = {
+        greeting = "world!"
+      },
+      opts = {
+        ["-H"] = "'x-b3-traceid: " .. trace_id .. "' -H 'x-b3-sampled: 1'",
+        ["-authority"] = "mock-grpc-route",
+      }
+    })
+    assert.truthy(ok)
+    assert.truthy(resp)
 
-      -- specific assertions for request_span
-      local request_tags = request_span.tags
-      assert.truthy(request_tags["kong.node.id"]:match("^[%x-]+$"))
-      request_tags["kong.node.id"] = nil
-      assert.same({
-        ["http.method"] = "POST",
-        ["http.path"] = "/hello.HelloService/SayHello",
-        ["http.status_code"] = "200", -- found (matches server status)
-        lc = "kong"
-      }, request_tags)
-      local peer_port = request_span.remoteEndpoint.port
-      assert_is_integer(peer_port)
-      assert.same({ ipv4 = "127.0.0.1", port = peer_port }, request_span.remoteEndpoint)
+    local spans
+    helpers.wait_until(function()
+      local res = assert(zipkin_client:get("/api/v2/trace/" .. trace_id))
+      spans = cjson.decode(assert.response(res).has.status(200))
+      return #spans == 3
+    end)
 
-      -- specific assertions for proxy_span
-      assert.same(proxy_span.tags["kong.route"], grpc_route.id)
-      assert.same(proxy_span.tags["peer.hostname"], "localhost")
+    local balancer_span, proxy_span, request_span = spans[1], spans[2], spans[3]
+    -- common assertions for request_span and proxy_span
+    assert_span_invariants(request_span, proxy_span, "post")
 
-      assert.same({ ipv4 = "127.0.0.1", port = 15002 },
-        proxy_span.remoteEndpoint)
+    -- specific assertions for request_span
+    local request_tags = request_span.tags
+    assert.truthy(request_tags["kong.node.id"]:match("^[%x-]+$"))
+    request_tags["kong.node.id"] = nil
+    assert.same({
+      ["http.method"] = "POST",
+      ["http.path"] = "/hello.HelloService/SayHello",
+      ["http.status_code"] = "200", -- found (matches server status)
+      lc = "kong"
+    }, request_tags)
+    local peer_port = request_span.remoteEndpoint.port
+    assert_is_integer(peer_port)
+    assert.same({ ipv4 = "127.0.0.1", port = peer_port }, request_span.remoteEndpoint)
 
-      -- specific assertions for balancer_span
-      assert.equals(balancer_span.parentId, request_span.id)
-      assert.equals(request_span.name .. " (balancer try 1)", balancer_span.name)
-      assert_is_integer(balancer_span.timestamp)
+    -- specific assertions for proxy_span
+    assert.same(proxy_span.tags["kong.route"], grpc_route.id)
+    assert.same(proxy_span.tags["peer.hostname"], "localhost")
+
+    assert.same({ ipv4 = "127.0.0.1", port = 15002 },
+    proxy_span.remoteEndpoint)
+
+    -- specific assertions for balancer_span
+    assert.equals(balancer_span.parentId, request_span.id)
+    assert.equals(request_span.name .. " (balancer try 1)", balancer_span.name)
+    assert_is_integer(balancer_span.timestamp)
+
+    if balancer_span.duration then
       assert_is_integer(balancer_span.duration)
+    end
 
-      assert.same({ ipv4 = "127.0.0.1", port = 15002 },
-        balancer_span.remoteEndpoint)
-      assert.equals(ngx.null, balancer_span.localEndpoint)
-      assert.same({
-        error = "false",
-        ["kong.balancer.try"] = "1",
-      }, balancer_span.tags)
-    end, function()
-      -- Making the request
-      local ok, resp = proxy_client_grpc({
-        service = "hello.HelloService.SayHello",
-        body = {
-          greeting = "world!"
-        },
-        opts = {
-          ["-authority"] = "mock-grpc-route",
-        }
-      })
-      assert.truthy(ok)
-      assert.truthy(resp)
-    end))
+    assert.same({ ipv4 = "127.0.0.1", port = 15002 },
+    balancer_span.remoteEndpoint)
+    assert.is_nil(balancer_span.localEndpoint)
+    assert.same({
+      error = "false",
+      ["kong.balancer.try"] = "1",
+    }, balancer_span.tags)
   end)
 
   it("generates spans, tags and annotations for non-matched requests", function()
-    assert.truthy(with_server(function(_, _, stream)
-      local spans = cjson.decode((assert(stream:get_body_as_string())))
-      assert.equals(2, #spans)
-      local proxy_span, request_span = spans[1], spans[2]
-      -- common assertions for request_span and proxy_span
-      assert_span_invariants(request_span, proxy_span, "GET")
+    local trace_id = gen_trace_id()
 
-      -- specific assertions for request_span
-      local request_tags = request_span.tags
-      assert.truthy(request_tags["kong.node.id"]:match("^[%x-]+$"))
-      request_tags["kong.node.id"] = nil
-      assert.same({
-        ["http.method"] = "GET",
-        ["http.path"] = "/",
-        ["http.status_code"] = "404", -- note that this was "not found"
-        lc = "kong"
-      }, request_tags)
-      local peer_port = request_span.remoteEndpoint.port
-      assert_is_integer(peer_port)
-      assert.same({ ipv4 = "127.0.0.1", port = peer_port }, request_span.remoteEndpoint)
+    local r = assert(proxy_client:send {
+      method  = "GET",
+      path    = "/foobar",
+      headers = {
+        ["x-b3-traceid"] = trace_id,
+        ["x-b3-sampled"] = "1",
+      },
+    })
+    assert.response(r).has.status(404)
 
-      -- specific assertions for proxy_span
-      assert.is_nil(proxy_span.tags)
+    local spans
+    helpers.wait_until(function()
+      local res = assert(zipkin_client:get("/api/v2/trace/" .. trace_id))
+      spans = cjson.decode(assert.response(res).has.status(200))
+      return #spans == 2
+    end)
 
-      assert.equals(ngx.null, proxy_span.remoteEndpoint)
-      assert.equals(ngx.null, proxy_span.localEndpoint)
-    end, function()
-      -- This request reaches the proxy, but doesn't match any route.
-      -- The plugin runs in "error mode": access phase doesn't run, but others, like header_filter, do run
-      local uri = string.format("http://%s:%d/", proxy_host, proxy_port)
-      local req = http_request.new_from_uri(uri)
-      assert(req:go())
-    end))
+    local proxy_span, request_span = spans[1], spans[2]
+
+    -- common assertions for request_span and proxy_span
+    assert_span_invariants(request_span, proxy_span, "get")
+
+    -- specific assertions for request_span
+    local request_tags = request_span.tags
+    assert.truthy(request_tags["kong.node.id"]:match("^[%x-]+$"))
+    request_tags["kong.node.id"] = nil
+    assert.same({
+      ["http.method"] = "GET",
+      ["http.path"] = "/foobar",
+      ["http.status_code"] = "404", -- note that this was "not found"
+      lc = "kong"
+    }, request_tags)
+    local peer_port = request_span.remoteEndpoint.port
+    assert_is_integer(peer_port)
+    assert.same({ ipv4 = "127.0.0.1", port = peer_port }, request_span.remoteEndpoint)
+
+    -- specific assertions for proxy_span
+    assert.is_nil(proxy_span.tags)
+    assert.is_nil(proxy_span.remoteEndpoint)
+    assert.is_nil(proxy_span.localEndpoint)
   end)
-
-  it("propagates b3 headers on routed request", function()
-    local trace_id = "1234567890abcdef"
-    assert.truthy(with_server(function(_, _, stream)
-      local spans = cjson.decode((assert(stream:get_body_as_string())))
-      for _, v in ipairs(spans) do
-        assert.same(trace_id, v.traceId)
-      end
-    end, function()
-      -- regular request, with extra headers
-      local req = http_request.new_from_uri("http://mock-http-route/")
-      req.host = proxy_host
-      req.port = proxy_port
-      req.headers:upsert("x-b3-traceid", trace_id)
-      req.headers:upsert("x-b3-sampled", "1")
-      assert(req:go())
-    end))
-  end)
-
-  -- TODO add grpc counterpart of above test case
 
   it("propagates b3 headers for non-matched requests", function()
-    local trace_id = "1234567890abcdef"
-    assert.truthy(with_server(function(_, _, stream)
-      local spans = cjson.decode((assert(stream:get_body_as_string())))
-      for _, v in ipairs(spans) do
-        assert.same(trace_id, v.traceId)
-      end
-    end, function()
-      -- This request reaches the proxy, but doesn't match any route. The trace_id should be respected here too
-      local uri = string.format("http://%s:%d/", proxy_host, proxy_port)
-      local req = http_request.new_from_uri(uri)
-      req.headers:upsert("x-b3-traceid", trace_id)
-      req.headers:upsert("x-b3-sampled", "1")
-      assert(req:go())
-    end))
+    local trace_id = gen_trace_id()
+
+    local r = assert(proxy_client:send {
+      method  = "GET",
+      path    = "/foobar",
+      headers = {
+        ["x-b3-traceid"] = trace_id,
+        ["x-b3-sampled"] = "1",
+      },
+    })
+    assert.response(r).has.status(404)
+
+    local spans
+    helpers.wait_until(function()
+      local res = assert(zipkin_client:get("/api/v2/trace/" .. trace_id))
+      spans = cjson.decode(assert.response(res).has.status(200))
+      return #spans == 2
+    end)
+
+    for _, v in ipairs(spans) do
+      assert.same(trace_id, v.traceId)
+    end
   end)
 end)
 end
