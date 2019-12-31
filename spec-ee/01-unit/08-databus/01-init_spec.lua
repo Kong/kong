12d348e442f6fff17dbad5ce9e7307290af7988c
eskerda <eskerda@gmail.com>
2019-12-31 17:21:51 +0100

    feat(dbus) add on_change and snooze specs
    * on_change specs
    * snooze specs
    * mock_cache specs

--- spec-ee/01-unit/08-databus/01-init_spec.lua
+++ spec-ee/01-unit/08-databus/01-init_spec.lua
@@ -1,4 +1,112 @@
--- local utils = require "kong.tools.utils"
+local function mock_cache()
+  local store = {}
+  local clock = 0
+
+  return {
+    get = function(self, key, opts, callback, ...)
+      local args = { ... }
+      local thing = store[key]
+      local hit_lvl
+
+      -- lol... invalidate
+      if thing and thing.ttl and thing.ttl < clock then
+        store[key] = nil
+        thing = nil
+      end
+
+      if not thing then
+        local value, opts, ttl = callback(unpack(args))
+        thing = {
+          value = value,
+          opts = opts,
+          ttl = ttl ~= nil and clock + ttl or nil,
+        }
+        store[key] = thing
+        hit_lvl = 3
+      else
+        hit_lvl = 1
+      end
+
+      return thing.value, nil, hit_lvl
+
+     end,
+     _travel = function(time)
+       clock = clock + time
+     end,
+     _store = store,
+     _nuke = function()
+       store = {}
+     end,
+  }
+end
+
+describe("mock_cache", function()
+  local cache = mock_cache()
+
+  before_each(function()
+    cache._nuke()
+  end)
+
+  it("pretends to be mlcache", function()
+    -- Miss
+    local val, _, hit_lvl = cache:get("some_key", nil, function(some, arg)
+      return { some, arg }
+    end, "hello", "world")
+    assert.same({ "hello", "world" }, val)
+    assert.equal(3, hit_lvl)
+
+    -- Hit
+    val, _, hit_lvl = cache:get("some_key", nil, function(some, arg)
+      return { some, arg }
+    end, "hello", "world")
+    assert.same({ "hello", "world" }, val)
+    assert.equal(1, hit_lvl)
+
+    -- Miss
+    val, _, hit_lvl = cache:get("another_key", nil, function()
+      return "hello!", nil, 20
+    end)
+    assert.same("hello!", val)
+    assert.equal(3, hit_lvl)
+
+    -- Hit
+    val, _, hit_lvl = cache:get("another_key", nil, function()
+      return "hello!", nil, 20
+    end)
+    assert.same("hello!", val)
+    assert.equal(1, hit_lvl)
+
+    -- Time travel 100 seconds
+    cache._travel(100)
+
+    -- Miss
+    val, _, hit_lvl = cache:get("another_key", nil, function()
+      return "hello!", nil, 20
+    end)
+    assert.same("hello!", val)
+    assert.equal(3, hit_lvl)
+
+    -- Time travel 10 seconds
+    cache._travel(10)
+
+    -- Hit
+    val, _, hit_lvl = cache:get("another_key", nil, function()
+      return "hello!", nil, 20
+    end)
+    assert.same("hello!", val)
+    assert.equal(1, hit_lvl)
+
+    -- Time travel 15 seconds
+    cache._travel(15)
+
+    -- Miss
+    val, _, hit_lvl = cache:get("another_key", nil, function()
+      return "hello!", nil, 20
+    end)
+    assert.same("hello!", val)
+    assert.equal(3, hit_lvl)
+  end)
+end)
 
 describe("databus", function()
 
@@ -11,6 +119,8 @@ describe("databus", function()
         databus_enabled = true,
       },
       worker_events = {},
+      cache = mock_cache(),
+      log = mock(setmetatable({}, { __index = function() return function() end end })),
     }
 
     for k, v in pairs(databus.events) do
@@ -278,6 +388,171 @@ describe("databus", function()
         }
 
         assert.stub(databus.queue.add).was.called_with(databus.queue, blob)
+      end)
+
+      describe("on_change", function()
+        it("when false, enqueues dbus job as many times as called", function()
+          entity.on_change = false
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(1)
+
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(2)
+        end)
+        it("when true, enqueues dbus job only if data signature has changed", function()
+          entity.on_change = true
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(1)
+
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(1)
+
+          worker_event.data = { different = "data" }
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(2)
+
+          worker_event.data = { different = "data" }
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(2)
+        end)
+      end)
+
+      describe("snooze", function()
+        it("when set, disables a databus event for 'snooze' seconds", function()
+          entity.snooze = 60
+
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(1)
+
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(1)
+
+          -- 50 seconds pass
+          kong.cache._travel(50)
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(1)
+
+          -- 20 more seconds pass (70 seconds)
+          kong.cache._travel(20)
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(2)
+
+          -- now it should be snoozed for 60 more seconds (130 seconds)
+          -- 30 seconds pass
+          kong.cache._travel(30)
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(2)
+
+          -- 31 seconds pass
+          kong.cache._travel(31)
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(3)
+        end)
+      end)
+
+      describe("on_change + snooze", function()
+        it("only events with different signatures get called during snooze time", function()
+          entity.on_change = true
+          entity.snooze = 60
+
+          local different_data = { different = "data" }
+
+          -- first event
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(1)
+
+          -- 10 seconds
+          kong.cache._travel(10)
+
+          -- same as first
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(1)
+
+          -- different data
+          databus.callback(entity)(different_data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(2)
+
+          -- 20 seconds
+          kong.cache._travel(10)
+
+          -- different data
+          databus.callback(entity)(different_data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(2)
+
+          -- 61 seconds
+          kong.cache._travel(41)
+
+          -- first event
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(3)
+
+          -- different data
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(3)
+
+          -- 71 seconds
+          kong.cache._travel(71)
+
+          databus.callback(entity)(worker_event.data,
+                                   worker_event.event,
+                                   worker_event.source,
+                                   worker_event.pid)
+          assert.stub(databus.queue.add).was.called(4)
+        end)
       end)
     end)
   end)
