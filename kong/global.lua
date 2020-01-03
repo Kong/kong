74c08239f51cb629b3fd0ca16e3bbc464f93fe1c
Edu Alonso <eduardo.alonsogarcia@telefonica.com>
2020-01-03 19:16:37 +0100

    feat(cache) Allow configuring neg_ttl for caching database entities
    The cache allows to configure ttl and neg_ttl but kong impose that the values are equal. With this change
    any can set both values when this is needed.
    For example any can configure a neg_ttl to be lower than ttl that is more useful in most of the cases.
    When the datase is down and a nil value is cached, this stay until normal ttl expires and this may lead
    to not recover quickly when database is up.
    Setting this neg_ttl to a lower value than ttl can make this recover quickly.
    Anyway, if the cache allows to configure the tow values they should be offered to customers

--- kong/global.lua
+++ kong/global.lua
@@ -151,9 +151,11 @@ do
 
   function _GLOBAL.init_cache(kong_config, cluster_events, worker_events)
     local db_cache_ttl = kong_config.db_cache_ttl
+    local db_cache_neg_ttl = kong_config.db_cache_neg_ttl
     local cache_pages = 1
     if kong_config.database == "off" then
       db_cache_ttl = 0
+      db_cache_neg_ttl = 0
       cache_pages = 2
     end
 
@@ -162,7 +164,7 @@ do
       worker_events     = worker_events,
       propagation_delay = kong_config.db_update_propagation,
       ttl               = db_cache_ttl,
-      neg_ttl           = db_cache_ttl,
+      neg_ttl           = db_cache_neg_ttl,
       resurrect_ttl     = kong_config.resurrect_ttl,
       cache_pages       = cache_pages,
       resty_lock_opts   = {
--- kong/templates/kong_defaults.lua
+++ kong/templates/kong_defaults.lua
@@ -77,6 +77,7 @@ declarative_config = NONE
 db_update_frequency = 5
 db_update_propagation = 0
 db_cache_ttl = 0
+db_cache_neg_ttl = 0
 db_resurrect_ttl = 30
 db_cache_warmup_entities = services, plugins
 
