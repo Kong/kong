840d8f18e21abe432fe8b9bae6a641e5f3d6d31f
eskerda <eskerda@gmail.com>
2020-01-08 17:08:15 +0100

    fix(dbus/lambda) do not pcall on validation

--- kong/db/schema/entities/dbus_subschemas.lua
+++ kong/db/schema/entities/dbus_subschemas.lua
@@ -74,26 +74,12 @@ local slack_schema = {
 }
 
 local function validate_function(fun)
-  local func1, err = loadstring(fun)
+  local _, err = loadstring(fun)
   if err then
     return false, "Error parsing function: " .. err
   end
 
-  local success, func2 = pcall(func1)
-
-  if not success or func2 == nil then
-    -- the code IS the handler function
-    return true
-  end
-
-  -- the code RETURNED the handler function
-  if type(func2) == "function" then
-    return true
-  end
-
-  -- the code returned something unknown
-  return false, "Bad return value from function, expected function type, got "
-                .. type(func2)
+  return true
 end
 
 
