db14e42de5e61d20004971e9173720894b1aa5c0
Vinicius Mignot <vinicius.mignot@gmail.com>
2020-01-10 12:27:45 -0300

    fix(router) split paths to properly sort for routing priority
    Unrelated longer paths were messing with the routing priority, so they
    must be evaluated alone when choosing by the longest path.

--- spec/02-integration/05-proxy/02-router_spec.lua
+++ spec/02-integration/05-proxy/02-router_spec.lua
@@ -1448,12 +1448,20 @@ for _, strategy in helpers.each_strategy() do
           {
             strip_path = true,
             methods    = { "GET" },
-            paths      = { "/root" },
+            paths      = { "/unrelated/longer/uri/that/should/not/match", "/root/fixture" },
+            hosts      = { "ahost.test" },
           },
           {
             strip_path = true,
             methods    = { "GET" },
-            paths      = { "/root/fixture" },
+            paths      = { "/root/fixture/get" },
+            hosts      = { "ahost.test" },
+          },
+          {
+            strip_path = true,
+            methods    = { "GET" },
+            paths      = { "/root/fixture/get" },
+            hosts      = { "anotherhost.test" },
           },
         })
       end)
@@ -1468,6 +1476,7 @@ for _, strategy in helpers.each_strategy() do
           path    = "/root/fixture/get",
           headers = {
             ["kong-debug"] = 1,
+            ["host"] = "ahost.test",
           }
         })
 
