a93b9f4d140c1dcffb6ba677f56cadc182e49dd4
Hisham Muhammad <hisham@gobolinux.org>
2020-01-10 16:20:55 -0300

    release: 2.0.0rc2

--- kong/meta.lua
+++ kong/meta.lua
@@ -2,7 +2,7 @@ local version = setmetatable({
   major = 2,
   minor = 0,
   patch = 0,
-  suffix = "rc1"
+  suffix = "rc2"
 }, {
   __tostring = function(t)
     return string.format("%d.%d.%d%s", t.major, t.minor, t.patch,
