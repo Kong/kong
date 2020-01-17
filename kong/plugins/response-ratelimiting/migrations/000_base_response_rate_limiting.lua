d66b9df6d7d5f63438cc1d17ed509f8b753eb150
Hisham Muhammad <hisham@gobolinux.org>
2020-01-16 21:03:57 -0300

    feat(response-ratelimiting) squash 002_15_to_10

--- kong/plugins/response-ratelimiting/migrations/000_base_response_rate_limiting.lua
+++ kong/plugins/response-ratelimiting/migrations/000_base_response_rate_limiting.lua
@@ -7,10 +7,9 @@ return {
         "period_date"  TIMESTAMP WITH TIME ZONE     NOT NULL,
         "service_id"   UUID                         NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000'::uuid,
         "route_id"     UUID                         NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000'::uuid,
-        "api_id"       UUID                         NOT NULL DEFAULT '00000000-0000-0000-0000-000000000000'::uuid,
         "value"        INTEGER,
 
-        PRIMARY KEY ("identifier", "period", "period_date", "service_id", "route_id", "api_id")
+        PRIMARY KEY ("identifier", "period", "period_date", "service_id", "route_id")
       );
     ]],
   },
@@ -20,12 +19,11 @@ return {
       CREATE TABLE IF NOT EXISTS response_ratelimiting_metrics(
         route_id    uuid,
         service_id  uuid,
-        api_id      uuid,
         period_date timestamp,
         period      text,
         identifier  text,
         value       counter,
-        PRIMARY KEY ((route_id, service_id, api_id, identifier, period_date, period))
+        PRIMARY KEY ((route_id, service_id, identifier, period_date, period))
       );
     ]],
   },
--- kong/plugins/response-ratelimiting/migrations/init.lua
+++ kong/plugins/response-ratelimiting/migrations/init.lua
@@ -1,4 +1,3 @@
 return {
   "000_base_response_rate_limiting",
-  "002_15_to_10",
 }
