7b957b9fd670faec5cd2b20768c027c3207e4e3d
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2020-01-29 18:50:54 +0200

    basic stream

--- kong/plugins/log-serializers/basic.lua
+++ kong/plugins/log-serializers/basic.lua
@@ -1,88 +1,146 @@
 local tablex = require "pl.tablex"
 local ngx_ssl = require "ngx.ssl"
+
+
 local gkong = kong
+local subsystem = ngx.config.subsystem
+local tonumber = tonumber
+
 
 local _M = {}
+
 
 local EMPTY = tablex.readonly({})
 local REDACTED_REQUEST_HEADERS = { "authorization", "proxy-authorization" }
 local REDACTED_RESPONSE_HEADERS = {}
 
-function _M.serialize(ngx, kong)
-  local ctx = ngx.ctx
-  local var = ngx.var
-  local req = ngx.req
+if subsystem == "http" then
+  function _M.serialize(ngx, kong)
+    local ctx = ngx.ctx
+    local var = ngx.var
+    local req = ngx.req
 
-  if not kong then
-    kong = gkong
-  end
+    if not kong then
+      kong = gkong
+    end
 
-  local authenticated_entity
-  if ctx.authenticated_credential ~= nil then
-    authenticated_entity = {
-      id = ctx.authenticated_credential.id,
-      consumer_id = ctx.authenticated_credential.consumer_id
+    local authenticated_entity
+    if ctx.authenticated_credential ~= nil then
+      authenticated_entity = {
+        id = ctx.authenticated_credential.id,
+        consumer_id = ctx.authenticated_credential.consumer_id
+      }
+    end
+
+    local request_tls
+    local request_tls_ver = ngx_ssl.get_tls1_version_str()
+    if request_tls_ver then
+      request_tls = {
+        version = request_tls_ver,
+        cipher = var.ssl_cipher,
+        client_verify = var.ssl_client_verify,
+      }
+    end
+
+    local request_uri = var.request_uri or ""
+
+    local req_headers = kong.request.get_headers()
+    for _, header in ipairs(REDACTED_REQUEST_HEADERS) do
+      if req_headers[header] then
+        req_headers[header] = "REDACTED"
+      end
+    end
+
+    local resp_headers = ngx.resp.get_headers()
+    for _, header in ipairs(REDACTED_RESPONSE_HEADERS) do
+      if resp_headers[header] then
+        resp_headers[header] = "REDACTED"
+      end
+    end
+
+    return {
+      request = {
+        uri = request_uri,
+        url = var.scheme .. "://" .. var.host .. ":" .. var.server_port .. request_uri,
+        querystring = kong.request.get_query(), -- parameters, as a table
+        method = kong.request.get_method(), -- http method
+        headers = req_headers,
+        size = var.request_length,
+        tls = request_tls,
+      },
+      upstream_uri = var.upstream_uri,
+      response = {
+        status = ngx.status,
+        headers = resp_headers,
+        size = var.bytes_sent,
+      },
+      tries = (ctx.balancer_data or EMPTY).tries,
+      latencies = {
+        kong = (ctx.KONG_REWRITE_TIME or 0) +
+               (ctx.KONG_ACCESS_TIME or 0) +
+               (ctx.KONG_BALANCER_TIME or 0) +
+               (ctx.KONG_RECEIVE_TIME or 0),
+        proxy = ctx.KONG_WAITING_TIME or -1,
+        request = var.request_time * 1000,
+      },
+      authenticated_entity = authenticated_entity,
+      route = ctx.route,
+      service = ctx.service,
+      consumer = ctx.authenticated_consumer,
+      client_ip = var.remote_addr,
+      started_at = req.start_time() * 1000,
     }
   end
 
-  local request_tls
-  local request_tls_ver = ngx_ssl.get_tls1_version_str()
-  if request_tls_ver then
-    request_tls = {
-      version = request_tls_ver,
-      cipher = var.ssl_cipher,
-      client_verify = var.ssl_client_verify,
+elseif subsystem == "stream" then
+  function _M.serialize(ngx, kong)
+    local ctx = ngx.ctx
+    local var = ngx.var
+
+    --if not kong then
+    --  kong = gkong
+    --end
+
+    local authenticated_entity
+    if ctx.authenticated_credential ~= nil then
+      authenticated_entity = {
+        id = ctx.authenticated_credential.id,
+        consumer_id = ctx.authenticated_credential.consumer_id
+      }
+    end
+
+    local request_tls
+    local request_tls_ver = ngx_ssl.get_tls1_version_str()
+    if request_tls_ver then
+      request_tls = {
+        version = request_tls_ver,
+        cipher = var.ssl_cipher,
+        client_verify = var.ssl_client_verify,
+      }
+    end
+
+    return {
+      session = {
+        upstream_bytes = tonumber(var.bytes_received, 10),
+        downstream_bytes = tonumber(var.bytes_sent, 10),
+        duration = tonumber(var.session_time, 10),
+        status = tonumber(var.status, 10),
+        tls = request_tls,
+      },
+      tries = (ctx.balancer_data or EMPTY).tries,
+      latencies = {
+        kong = (ctx.KONG_PREREAD_TIME or 0) +
+               (ctx.KONG_BALANCER_TIME or 0),
+      },
+      authenticated_entity = authenticated_entity,
+      route = ctx.route,
+      service = ctx.service,
+      consumer = ctx.authenticated_consumer,
+      client_ip = var.remote_addr,
+      started_at = ctx.KONG_PROCESSING_START,
     }
   end
-
-  local request_uri = var.request_uri or ""
-
-  local req_headers = kong.request.get_headers()
-  for _, header in ipairs(REDACTED_REQUEST_HEADERS) do
-    if req_headers[header] then
-      req_headers[header] = "REDACTED"
-    end
-  end
-
-  local resp_headers = ngx.resp.get_headers()
-  for _, header in ipairs(REDACTED_RESPONSE_HEADERS) do
-    if resp_headers[header] then
-      resp_headers[header] = "REDACTED"
-    end
-  end
-
-  return {
-    request = {
-      uri = request_uri,
-      url = var.scheme .. "://" .. var.host .. ":" .. var.server_port .. request_uri,
-      querystring = kong.request.get_query(), -- parameters, as a table
-      method = kong.request.get_method(), -- http method
-      headers = req_headers,
-      size = var.request_length,
-      tls = request_tls
-    },
-    upstream_uri = var.upstream_uri,
-    response = {
-      status = ngx.status,
-      headers = resp_headers,
-      size = var.bytes_sent
-    },
-    tries = (ctx.balancer_data or EMPTY).tries,
-    latencies = {
-      kong = (ctx.KONG_ACCESS_TIME or 0) +
-             (ctx.KONG_RECEIVE_TIME or 0) +
-             (ctx.KONG_REWRITE_TIME or 0) +
-             (ctx.KONG_BALANCER_TIME or 0),
-      proxy = ctx.KONG_WAITING_TIME or -1,
-      request = var.request_time * 1000
-    },
-    authenticated_entity = authenticated_entity,
-    route = ctx.route,
-    service = ctx.service,
-    consumer = ctx.authenticated_consumer,
-    client_ip = var.remote_addr,
-    started_at = req.start_time() * 1000
-  }
 end
+
 
 return _M
