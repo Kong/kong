53b15ddb82f9a1e270aab4d3d78bd04c12d3b1de
Datong Sun <datong.sun@konghq.com>
2020-01-30 06:12:35 -0800

    ci(github) use GitHub Actions as CI for Kong OSS
    This commit allows us to use GitHub Actions as CI solution. It provides
    more concurrency and provides ease for excessive run time and queue
    length we have been experiencing with Travis CI so far.
    
    This commit makes Kong able to run on GitHub Actions, but still kept the
    Travis CI files around so that we could use both at the same time during
    the transitional period. Eventually Travis CI support will be removed.
    
    This PR further splits Kong core integration tests into two halves on
    GitHub Actions to further reduce the run time by utilizing all the
    concurrency we have access to, as the core integration tests has
    consistently been the long tail of all the jobs, running 2x slower than
    the second slowest job. Splitting is not done on Travis as the
    concurrency limit makes doing so pointless.
    
    Co-Authored-By: Enrique García Cota <kikito@gmail.com>

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -21,8 +21,23 @@ else
 fi
 
 if [ "$TEST_SUITE" == "integration" ]; then
-    eval "$TEST_CMD" spec/02-integration/
+    if [[ "$TEST_SPLIT" == first* ]]; then
+        # GitHub Actions, run first batch of integration tests
+        eval "$TEST_CMD" $(ls -d spec/02-integration/* | head -n4)
+
+    elif [[ "$TEST_SPLIT" == second* ]]; then
+        # GitHub Actions, run second batch of integration tests
+        # Note that the split here is chosen carefully to result
+        # in a similar run time between the two batches, and should
+        # be adjusted if imbalance become significant in the future
+        eval "$TEST_CMD" $(ls -d spec/02-integration/* | tail -n+5)
+
+    else
+        # Non GitHub Actions
+        eval "$TEST_CMD" spec/02-integration/
+    fi
 fi
+
 if [ "$TEST_SUITE" == "dbless" ]; then
     eval "$TEST_CMD" spec/02-integration/02-cmd \
                      spec/02-integration/05-proxy \
@@ -77,5 +92,5 @@ if [ "$TEST_SUITE" == "plugins" ]; then
     fi
 fi
 if [ "$TEST_SUITE" == "pdk" ]; then
-    TEST_NGINX_RANDOMIZE=1 prove -I. -j$JOBS -r t/01-pdk
+    TEST_NGINX_RANDOMIZE=1 prove -I. -r t/01-pdk
 fi
