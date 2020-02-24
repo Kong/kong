7e622c83c25e5e395a3fcd3746a4266c14079757
Datong Sun <datong.sun@konghq.com>
2020-02-24 04:36:35 -0800

    add readonly user to `setup_env.sh`

--- .ci/setup_env.sh
+++ .ci/setup_env.sh
@@ -117,6 +117,14 @@ fi
 
 luarocks install busted-htest 1.0.0
 
+psql -v ON_ERROR_STOP=1 --username "$KONG_TEST_PG_USER" <<-EOSQL
+    CREATE user postgres_ro;
+
+    GRANT CONNECT ON DATABASE travis TO postgres_ro;
+    \c $KONG_TEST_PG_DATABASE;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO postgres_ro;
+EOSQL
+
 nginx -V
 resty -V
 luarocks --version
