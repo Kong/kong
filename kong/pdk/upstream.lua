2bc1dc6a9241d18cc56ce7f6cc326d44d7c859b2
taotao <20034838@qq.com>
2020-03-01 15:11:58 +0800

    New features：pdk.kong.upstream
    New features：
    
    pdk.kong.upstream
    
    https://discuss.konghq.com/t/data-center-aware-load-balancing/5667

---
+++ kong/pdk/upstream.lua
@@ -0,0 +1,93 @@
+
+-- @module kong.upstream
+
+local balancer = require "kong.runloop.balancer"
+local phase_checker = require "kong.pdk.private.phases"
+
+
+local ngx = ngx
+local check_phase = phase_checker.check
+
+local PHASES = phase_checker.phases
+
+local function new()
+  local upstream = {}
+
+  ---
+  -- Get upstream balancer health
+  --
+  -- @function kong.upstream.balancer_health
+  -- @phases access
+  -- @tparam string upstream_name
+  -- @treturn health_info|nil `health_info` on success, or `nil` if no health_info entities where found
+  -- @treturn string|nil An error message describing the error if there was one.
+  --
+  -- @usage
+  -- local ok, err = kong.upstream.balancer_health("upstream_name")
+  -- if not ok then
+  --   kong.log.err(err)
+  --   return
+  -- end
+  function upstream.balancer_health(upstream_name)
+    check_phase(PHASES.access)
+
+    if type(upstream_name) ~= "string" then
+      error("upstream_name must be a string", 2)
+    end
+
+    local upstream = balancer.get_upstream_by_name(upstream_name)
+    if not upstream then
+      return nil, "could not find an Upstream named '" .. upstream_name .. "'"
+    end
+    
+    local health_info, err = balancer.get_balancer_health(upstream.id)
+
+    if err then
+      return nil, "failed getting upstream health '" .. upstream_name .. "'"
+    end
+
+    return health_info
+  end
+
+
+    ---
+  -- Get upstream node health
+  --
+  -- @function kong.upstream.node_health
+  -- @phases access
+  -- @tparam string upstream_name
+  -- @treturn health_info|nil `health_info` on success, or `nil` if no health_info entities where found
+  -- @treturn string|nil An error message describing the error if there was one.
+  --
+  -- @usage
+  -- local ok, err = kong.upstream.node_health("upstream_name")
+  -- if not ok then
+  --   kong.log.err(err)
+  --   return
+  -- end
+  function upstream.node_health(upstream_name)
+    check_phase(PHASES.access)
+
+    if type(upstream_name) ~= "string" then
+      error("upstream_name must be a string", 2)
+    end
+
+    local upstream = balancer.get_upstream_by_name(upstream_name)
+    if not upstream then
+      return nil, "could not find an Upstream named '" .. upstream_name .. "'"
+    end
+
+    local health_info, err = balancer.get_upstream_health(upstream.id)
+    if err then
+      return nil, "failed getting upstream health '" .. upstream_name .. "'"
+    end
+
+    return health_info
+  end
+
+  return upstream
+end
+
+return {
+  new = new,
+}
