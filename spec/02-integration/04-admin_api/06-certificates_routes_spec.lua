Kong/kong

commit c9b62748fa67dc186663d7590cb6d187ea671537

fix(admin-api) incorrect PUT behavior for /certificates (#5321)
### Summary

@hbagdi reported issue on `PUT` behavior on `/certificates` with #5309.

This PR fixes the `PUT` behavior. It also fixes several other issues
found while fixing the original issue.

### Issues Resolver

Fix #5309

kong/api/routes/certificates.lua
@@ -1,72 +1,122 @@
 local endpoints   = require "kong.api.endpoints"
+local arguments   = require "kong.api.arguments"
 local utils       = require "kong.tools.utils"
-local Set         = require "pl.Set"
 
 
+local ngx = ngx
 local kong = kong
+local type = type
+local find = string.find
+local lower = string.lower
 local unescape_uri = ngx.unescape_uri
 
 
-local function get_cert_id_from_sni(self, db, helpers)
+local function prepare_params(self)
   local id = unescape_uri(self.params.certificates)
-  if utils.is_valid_uuid(id) then
-    return
+  local method = self.req.method
+  local name
+  if not utils.is_valid_uuid(id) then
+    name = id
+
+    local sni, _, err_t = kong.db.snis:select_by_name(name)
+    if err_t then
+      return endpoints.handle_error(err_t)
+    end
+
+    if sni then
+      id = sni.certificate.id
+
+    else
+      if method ~= "PUT" then
+        return kong.response.exit(404, { message = "SNI not found" })
+      end
+
+      id = utils.uuid()
+    end
   end
 
-  local sni, _, err_t = db.snis:select_by_name(id)
-  if err_t then
-    return endpoints.handle_error(err_t)
+  self.params.certificates = id
+  self.params.name = name
+end
+
+
+local function prepare_args(self)
+  local name_field
+  do
+    local content_type = ngx.var.content_type
+    if content_type then
+      content_type = lower(content_type)
+      if find(content_type, "application/x-www-form-urlencoded", 1, true) == 1 or
+         find(content_type, "multipart/form-data",               1, true) == 1 then
+        name_field = kong.db.snis.schema.fields.name
+      end
+    end
   end
 
-  if sni then
-    self.params.certificates = sni.certificate.id
-    return
+  local method = self.req.method
+  local snis = self.args.post.snis
+  local name = self.params.name
+
+  if type(snis) == "table" then
+    local count = #snis
+
+    if name and method == "PUT" then
+      count = count + 1
+      snis[count] = name
+    end
+
+    if name_field then
+      for i=1, count do
+        snis[i] = arguments.infer_value(snis[i], name_field)
+      end
+    end
+
+  elseif type(snis) == "string" then
+    if name_field then
+      snis = arguments.infer_value(snis, name_field)
+    end
+
+    if name and method == "PUT" and name ~= snis then
+      snis = { snis, name }
+    else
+      snis = { snis }
+    end
   end
 
-  if self.req.method == "PUT" then
-    return
+  if not snis and method == "PUT" then
+    snis = ngx.null
   end
 
-  return kong.response.exit(404, { message = "SNI not found" })
+  self.params.name = nil
+  self.args.post.snis = snis
 end
 
 
 return {
   ["/certificates/:certificates"] = {
-    before = get_cert_id_from_sni,
+    before = prepare_params,
 
     -- override to include the snis list when getting an individual certificate
     GET = endpoints.get_entity_endpoint(kong.db.certificates.schema,
                                         nil, nil, "select_with_name_list"),
 
-    -- override to create a new SNI in the PUT /certificates/foo.com (create) case
-    PUT = function(self, db, helpers)
-      local cert, err_t, _
-      local id = unescape_uri(self.params.certificates)
-
-      -- cert was found via id or sni inside `before` section
-      if utils.is_valid_uuid(id) then
-        cert, _, err_t = endpoints.upsert_entity(self, db, db.certificates.schema)
-
-      else -- create a new cert. Add extra sni if provided on url
-        self.args.post.snis = Set.values(Set(self.args.post.snis or {}) + id)
-        cert, _, err_t = endpoints.insert_entity(self, db, db.certificates.schema)
-      end
-
-      if err_t then
-        return endpoints.handle_error(err_t)
-      end
-
-      if not cert then
-        return kong.response.exit(404, { message = "Not found" })
-      end
-
-      return kong.response.exit(200, cert)
+    PUT = function(self, _, _, parent)
+      prepare_args(self)
+      return parent()
     end,
+
+    PATCH = function(self, _, _, parent)
+      prepare_args(self)
+      return parent()
+    end
   },
 
   ["/certificates/:certificates/snis"] = {
-    before = get_cert_id_from_sni,
+    before = prepare_params,
+  },
+
+  ["/certificates/:certificates/snis/:snis"] = {
+    before = prepare_params,
   },
 }
 

spec/02-integration/04-admin_api/06-certificates_routes_spec.lua
@@ -326,7 +326,7 @@ describe("Admin API: #" .. strategy, function()
         assert.same(json, in_db)
       end)
 
-      it("updates if found", function()
+      it("upserts if found", function()
         local res = client:put("/certificates/" .. certificate.id, {
           body = { cert = ssl_fixtures.cert_alt, key = ssl_fixtures.key_alt },
           headers = { ["Content-Type"] = "application/json" },
@@ -336,7 +336,7 @@ describe("Admin API: #" .. strategy, function()
         local json = cjson.decode(body)
         assert.same(ssl_fixtures.cert_alt, json.cert)
         assert.same(ssl_fixtures.key_alt, json.key)
-        assert.same({"bar.com", "foo.com"}, json.snis)
+        assert.same({}, json.snis)
 
         json.snis = nil
 
@@ -440,6 +440,20 @@ describe("Admin API: #" .. strategy, function()
         end
       end)
 
+      it_content_types("update by id returns full certificate", function(content_type)
+        return function()
+          local res = client:patch("/certificates/" .. cert_foo.id, {
+            body = {},
+            headers = { ["Content-Type"] = content_type }
+          })
+
+          local body = assert.res_status(200, res)
+          local json = cjson.decode(body)
+
+          assert.same(cert_foo, json)
+        end
+      end)
+
       it_content_types("updates a certificate by sni", function(content_type)
         return function()
           local body
@@ -464,6 +478,20 @@ describe("Admin API: #" .. strategy, function()
           local json = cjson.decode(body)
 
           assert.equal(ssl_fixtures.cert_alt, json.cert)
+        end
+      end)
+
+      it_content_types("update by sni returns full certificate", function(content_type)
+        return function()
+          local res = client:patch("/certificates/foo.com", {
+            body = {},
+            headers = { ["Content-Type"] = content_type }
+          })
+
+          local body = assert.res_status(200, res)
+          local json = cjson.decode(body)
+
+          assert.same(cert_foo, json)
         end
       end)
 

kong/db/dao/certificates.lua
@@ -99,14 +99,11 @@ function _Certificates:update(cert_pk, cert, options)
     end
   end
 
-  -- update certificate if necessary
-  if cert.key or cert.cert then
     cert.snis = nil
     cert, err, err_t = self.super.update(self, cert_pk, cert, options)
     if err then
       return nil, err, err_t
     end
-  end
 
   if name_list then
     cert.snis = name_list

commit 0ac91de74cb0bccf4fe7cbcc6d7396837e3acf60

fix(admin) PUT on certificates could error with sni is duplicated
### Summary

If you send multiple `SNIs` as body argument AND also an `SNI` on URL
matching one of the `SNIs` in body, it leads to `sni is duplicated`
error.

This bug was introduced with #5321, so it affects Kong 1.5+
(including 1.5).

This PR fixes the issue and the duplicate is not reported anymore as
an error (url SNI is only appended to `snis` if not already found on
`snis`).

### Issues Resolved

Fix #5652 (at least partly)

kong/api/routes/certificates.lua
@@ -16,7 +16,7 @@ local function prepare_params(self)
   local method = self.req.method
   local name
   if not utils.is_valid_uuid(id) then
-    name = id
+    name = arguments.infer_value(id, kong.db.snis.schema.fields.name)
 
     local sni, _, err_t = kong.db.snis:select_by_name(name)
     if err_t then
@@ -41,39 +41,45 @@ end
 
 
 local function prepare_args(self)
-  local name_field
+  local infer_snis
   do
     local content_type = ngx.var.content_type
     if content_type then
       content_type = lower(content_type)
-      if find(content_type, "application/x-www-form-urlencoded", 1, true) == 1 or
-         find(content_type, "multipart/form-data",               1, true) == 1 then
-        name_field = kong.db.snis.schema.fields.name
+
+      if find(content_type, "application/x-www-form-urlencoded", 1, true) == 1
+      or find(content_type, "multipart/form-data",               1, true) == 1
+      then
+        infer_snis = true
       end
     end
   end
 
   local method = self.req.method
+
   local snis = self.args.post.snis
   local name = self.params.name
 
   if type(snis) == "table" then
+    local found
     local count = #snis
+    for i=1, count do
+      if infer_snis then
+        snis[i] = arguments.infer_value(snis[i], kong.db.snis.schema.fields.name)
+      end
 
-    if name and method == "PUT" then
-      count = count + 1
-      snis[count] = name
-    end
-
-    if name_field then
-      for i=1, count do
-        snis[i] = arguments.infer_value(snis[i], name_field)
+      if not found and name and name == snis[i] then
+        found = true
       end
     end
 
+    if not found and name and method == "PUT" then
+      snis[count + 1] = name
+    end
+
   elseif type(snis) == "string" then
-    if name_field then
-      snis = arguments.infer_value(snis, name_field)
+    if infer_snis then
+      snis = arguments.infer_value(snis, kong.db.snis.schema.fields.name)
     end
 
     if name and method == "PUT" and name ~= snis then

spec/02-integration/04-admin_api/06-certificates_routes_spec.lua
@@ -330,6 +330,29 @@ describe("Admin API: #" .. strategy, function()
         assert.same(json, in_db)
       end)
 
+      it("creates a new sni when provided in the url (with sni duplicated in url and body)", function()
+        local n1 = get_name()
+        local n2 = get_name()
+        local res = client:put("/certificates/" .. n1, {
+          body = {
+            cert = ssl_fixtures.cert,
+            key = ssl_fixtures.key,
+            snis = { n1, n2 },
+          },
+          headers = { ["Content-Type"] = "application/json" },
+        })
+
+        local body = assert.res_status(200, res)
+        local json = cjson.decode(body)
+        assert.same(ssl_fixtures.cert, json.cert)
+
+        assert.same({ n1, n2 }, json.snis)
+        json.snis = nil
+
+        local in_db = assert(db.certificates:select({ id = json.id }, { nulls = true }))
+        assert.same(json, in_db)
+      end)
+
       it("upserts if found", function()
         local certificate = add_certificate()
 