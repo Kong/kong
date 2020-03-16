010c95241bd9232b37114cb828bffa02c57c12d1
Murillo Paula <murillo@murillopaula.com>
2020-03-16 17:09:09 -0300

    chore(handler) stop using kong.ctx.plugin

--- kong/plugins/ip-restriction/handler.lua
+++ kong/plugins/ip-restriction/handler.lua
@@ -17,25 +17,22 @@ function IpRestrictionHandler:access(conf)
   local block = false
   local err = false
   local binary_remote_addr = ngx.var.binary_remote_addr
+  local ip
 
   if not binary_remote_addr then
     return kong.response.exit(FORBIDDEN, { message = "Cannot identify the client IP address, unix domain sockets are not supported." })
   end
 
   if conf.blacklist and #conf.blacklist > 0 then
-    if not kong.ctx.plugin.blacklist then
-      kong.ctx.plugin.blacklist = ipmatcher.new(conf.blacklist)
-    end
+    ip = ipmatcher.new(conf.blacklist)
 
-    block, err = kong.ctx.plugin.blacklist:match_bin(binary_remote_addr)
+    block, err = ip:match_bin(binary_remote_addr)
   end
 
   if conf.whitelist and #conf.whitelist > 0 then
-    if not kong.ctx.plugin.whitelist then
-      kong.ctx.plugin.whitelist = ipmatcher.new(conf.whitelist)
-    end
+    ip = ipmatcher.new(conf.whitelist)
 
-    block, err = kong.ctx.plugin.whitelist:match_bin(binary_remote_addr)
+    block, err = ip:match_bin(binary_remote_addr)
     block = not block
   end
 
