9696cad50ed8dc1ff536048e67a9da5ba76d95ad
amberh <amberh@aweber.com>
2020-03-20 10:06:29 -0400

    refactor(oauth2) combine migration scripts
    Follow previous convention and use the
    version in the migration script name.
    
    https://github.com/Kong/kong/pull/5268

--- kong/plugins/oauth2/migrations/004_200_to_210.lua
+++ kong/plugins/oauth2/migrations/004_200_to_210.lua
@@ -14,6 +14,13 @@ return {
       EXCEPTION WHEN DUPLICATE_COLUMN THEN
         -- Do nothing, accept existing state
       END$$;
+
+      DO $$
+      BEGIN
+        ALTER TABLE IF EXISTS ONLY "oauth2_credentials" ADD "client_type" TEXT;
+      EXCEPTION WHEN DUPLICATE_COLUMN THEN
+        -- Do nothing, accept existing state
+      END$$;
     ]],
   },
 
@@ -21,6 +28,7 @@ return {
     up = [[
       ALTER TABLE oauth2_authorization_codes ADD challenge text;
       ALTER TABLE oauth2_authorization_codes ADD challenge_method text;
+      ALTER TABLE oauth2_credentials ADD client_type text;
     ]],
   },
 }
--- kong/plugins/oauth2/migrations/init.lua
+++ kong/plugins/oauth2/migrations/init.lua
@@ -1,6 +1,5 @@
 return {
   "000_base_oauth2",
   "003_130_to_140",
-  "004_code_challenges",
-  "005_application_type",
+  "004_200_to_210",
 }
