Kong/kong

commit 5e25921de2da56eeb45042c5f7c74822584b7ddf

fix(cmd) always set 'lua_package_[c]path' directives
This ensures that new master processes started via the USR2 signal
(binary reload) also have a complete `lua_package_path` directive,
including the contents of `LUA_PATH` (which is visible to the Kong CLI
process, but not to a newly spawned master process).

kong/cmd/utils/prefix_handler.lua
@@ -176,6 +176,10 @@ local function compile_conf(kong_config, conf_template)
 
   compile_env = pl_tablex.merge(compile_env, kong_config, true) -- union
   compile_env.dns_resolver = table.concat(compile_env.dns_resolver, " ")
+  compile_env.lua_package_path = (os.getenv("LUA_PATH") or "") .. ";" ..
+                                 (compile_env.lua_package_path or "")
+  compile_env.lua_package_cpath = (os.getenv("LUA_CPATH") or "") .. ";" ..
+                                  (compile_env.lua_package_cpath or "")
 
   local post_template, err = pl_template.substitute(conf_template, compile_env)
   if not post_template then

spec/01-unit/04-prefix_handler_spec.lua
@@ -55,7 +55,7 @@ describe("NGINX conf compiler", function()
   describe("compile_kong_conf()", function()
     it("compiles the Kong NGINX conf chunk", function()
       local kong_nginx_conf = prefix_handler.compile_kong_conf(helpers.test_conf)
-      assert.matches("lua_package_path%s+'%./spec/fixtures/custom_plugins/%?%.lua;;'", kong_nginx_conf)
+      assert.matches("lua_package_path%s+'.-%./spec/fixtures/custom_plugins/%?%.lua;;'", kong_nginx_conf)
       assert.matches("listen%s+0%.0%.0%.0:9000;", kong_nginx_conf)
       assert.matches("listen%s+127%.0%.0%.1:9001;", kong_nginx_conf)
       assert.matches("server_name%s+kong;", kong_nginx_conf)

commit 2170825d74716a396ceabc63d80a9266bb235016

fix(cmd) fix path order
The test mocks no longer get selected first because the wrong order
is being set.

fixes problem introduced in
https://github.com/Kong/kong/commit/5e25921de2da56eeb45042c5f7c74822584b7ddf

kong/cmd/utils/prefix_handler.lua
@@ -176,10 +176,10 @@ local function compile_conf(kong_config, conf_template)
 
   compile_env = pl_tablex.merge(compile_env, kong_config, true) -- union
   compile_env.dns_resolver = table.concat(compile_env.dns_resolver, " ")
-  compile_env.lua_package_path = (os.getenv("LUA_PATH") or "") .. ";" ..
-                                 (compile_env.lua_package_path or "")
-  compile_env.lua_package_cpath = (os.getenv("LUA_CPATH") or "") .. ";" ..
-                                  (compile_env.lua_package_cpath or "")
+  compile_env.lua_package_path = (compile_env.lua_package_path or "") .. ";" ..
+                                 (os.getenv("LUA_PATH") or "")
+  compile_env.lua_package_cpath = (compile_env.lua_package_cpath or "") .. ";" ..
+                                  (os.getenv("LUA_CPATH") or "")
 
   local post_template, err = pl_template.substitute(conf_template, compile_env)
   if not post_template then

spec/01-unit/04-prefix_handler_spec.lua
@@ -55,7 +55,7 @@ describe("NGINX conf compiler", function()
   describe("compile_kong_conf()", function()
     it("compiles the Kong NGINX conf chunk", function()
       local kong_nginx_conf = prefix_handler.compile_kong_conf(helpers.test_conf)
-      assert.matches("lua_package_path%s+'.-%./spec/fixtures/custom_plugins/%?%.lua;;'", kong_nginx_conf)
+      assert.matches("lua_package_path%s+'%./spec/fixtures/custom_plugins/%?%.lua;.+'", kong_nginx_conf)
       assert.matches("listen%s+0%.0%.0%.0:9000;", kong_nginx_conf)
       assert.matches("listen%s+127%.0%.0%.1:9001;", kong_nginx_conf)
       assert.matches("server_name%s+kong;", kong_nginx_conf)