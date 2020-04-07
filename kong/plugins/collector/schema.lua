94117507413f615c2277c2c57e7275f50c9db261
Caio Casimiro <caiorcasimiro@gmail.com>
2020-04-07 16:23:33 -0300

    (chore) removes unused configuration options

--- kong/plugins/collector/schema.lua
+++ kong/plugins/collector/schema.lua
@@ -9,8 +9,6 @@ return {
           { queue_size = { type = "number", default = 100 } },
           { flush_timeout = { type = "number", default = 2 } },
           { log_bodies = { type = "boolean", default = true } },
-          { clear_body_values = { type = "boolean", default = true } },
-          { connection_timeout = { type = "number", default = 120 } },
           { http_endpoint = { type = "string", required = true, default = "http://collector.com" } },
           { https_verify = { type = "boolean", default = false } },
         },
