658285c642fb1d2e770e683aaef082a7986af5be
Murillo Paula <murillo@murillopaula.com>
2020-04-07 07:31:48 -0300

    chore(kafka-log) bump plugin version to 0.1.0 (#13)

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.1.0
+
+### Added
+
+* Add support for the `api_version` property
--- kong/plugins/kafka-log/handler.lua
+++ kong/plugins/kafka-log/handler.lua
@@ -5,7 +5,7 @@ local cjson_encode = require("cjson").encode
 local KafkaLogHandler = {}
 
 KafkaLogHandler.PRIORITY = 5
-KafkaLogHandler.VERSION = "0.0.1"
+KafkaLogHandler.VERSION = "0.1.0"
 
 --- Publishes a message to Kafka.
 -- Must run in the context of `ngx.timer.at`.
