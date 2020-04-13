6631ebaac2dd5e3b4f857a7052060961e8106bce
bao <bao.mai@konghq.com>
2020-04-12 19:46:19 -0700

    test(handler) Integration tests using delay upstream

--- kong/plugins/upstream-timeout/handler.lua
+++ kong/plugins/upstream-timeout/handler.lua
@@ -1,28 +1,25 @@
+local BasePlugin = require "kong.plugins.base_plugin"
 
-local UpstreamTimeout = {}
+local UpstreamTimeout = BasePlugin:extend()
 
 
--- TODO: Integration test...
--- Config in-memory cache
--- Should be able to apply on consumer, route, or service
-
--- Custom errors?
 function UpstreamTimeout:access(conf)
-    -- TODO: Cache config
-    if not conf then end
+	UpstreamTimeout.super.access(self)
 
-    -- Needs option to revert to old timeout
-    if conf.read_timeout then
-        ngx.ctx.balancer_data.read_timeout = conf.read_timeout
-    end
-    if conf.write_timeout then
-        ngx.ctx.balancer_data.send_timeout = conf.write_timeout
-    end
-    if conf.connect_timeout then
-        ngx.ctx.balancer_data.connect_timeout = conf.connect_timeout
-    end
+	-- Needs option to revert to old timeout
+	if conf.read_timeout then
+		ngx.ctx.balancer_data.read_timeout = conf.read_timeout
+	end
+	if conf.send_timeout then
+		ngx.ctx.balancer_data.send_timeout = conf.send_timeout
+	end
+	if conf.connect_timeout then
+		ngx.ctx.balancer_data.connect_timeout = conf.connect_timeout
+	end
 
 end
 
+UpstreamTimeout.PRIORITY = 400
+UpstreamTimeout.VERSION = "1.0.0-0"
 
 return UpstreamTimeout
--- spec/03-plugins/01-timeout_spec.lua
+++ spec/03-plugins/01-timeout_spec.lua
@@ -1,29 +1,63 @@
 local helpers = require "spec.helpers"
 
 for _, strategy in helpers.each_strategy() do
-	describe("Plugin: upstream-timeout", function ()
+	describe("Plugin enabled on Service:", function ()
 		local admin_client
 		local proxy_client
-		local timeout_route
+		local no_delay_service, delay_service
 
-		setup(function ()
-			local bp = helpers.get_db_utils(strategy)
+		lazy_setup(function ()
+			local bp = helpers.get_db_utils(strategy, {
+				"services",
+				"routes",
+				"plugins"
+			})
 
-			-- Note there is already service to echo the request
-			timeout_route = bp.routes:insert({
-				hosts = { "test_upstream_timeout.adult" }
+			no_delay_service = assert(bp.services:insert({
+				name = "no-delay-service",
+				url = "http://httpbin.org/delay/0",
+			}))
+			delay_service = assert(bp.services:insert({
+				name = "delay-service",
+				url = "http://httpbin.org/delay/2" -- 2 second delay
+			}))
+
+			assert(bp.routes:insert({
+				hosts = { "no.delay.com" },
+				service = { id = no_delay_service.id }
+			}))
+			assert(bp.routes:insert({
+				hosts = { "twoseconds.delay.com" },
+				service = { id = delay_service.id }
+			}))
+
+
+			assert(bp.plugins:insert {
+				service = { id = no_delay_service.id },
+				name    = "upstream-timeout",
+				config  = {
+					read_timeout = 1000
+				}
+			})
+			assert(bp.plugins:insert {
+				service = { id = delay_service.id },
+				name    = "upstream-timeout",
+				config  =  {
+					read_timeout = 1000
+				}
 			})
 
 			assert(helpers.start_kong({
 				database = strategy,
-				nginx_conf = "spec/fixtures/custom_nginx.template"
+				nginx_conf = "spec/fixtures/custom_nginx.template",
+				plugins = "bundled, upstream-timeout"
 			}))
 
 			proxy_client = helpers.proxy_client()
 			admin_client = helpers.admin_client()
 		end)
 
-		teardown(function ()
+		lazy_teardown(function ()
 			if proxy_client and admin_client then
 				proxy_client:close()
 				admin_client:close()
@@ -31,40 +65,27 @@ for _, strategy in helpers.each_strategy() do
 			helpers.stop_kong()
 		end)
 
-		describe("POST", function ()
-			describe("config.send_timeout", function ()
-				local res = assert(admin_client:send {
-					method = "POST",
-					path   = "/plugins",
-					headers = {
-						["Content-Type"] = "application/json"
-					},
-					body   = {
-						name   = "key-auth",
-						config = {
-							route = { id = timeout_route.id }
-						}
-					}
-				})
-
-				res = assert(proxy_client:send {
+		describe("request upstream", function ()
+			local function upstream_request(host)
+				return proxy_client:send({
 					method = "GET",
-					path   = "/request",
+					path = "/",
 					headers = {
-						["Host"]  = "test_upstream_timeout.adult",
-						["Content-Type"] = "application/json"
-					},
-					body = {
-						data = { "ping", "pong" }
+						host = host
 					}
 				})
+			end
 
-				-- What is timeout status
-				assert.response(res).has.status(500)
+			it("will succeed if response below timeout", function ()
+				local res = assert(upstream_request("no.delay.com"))
+				assert.response(res).has.status(200)
 			end)
 
+			it("will fail if response exceeds timeout", function ()
+				local res = assert(upstream_request("twoseconds.delay.com"))
+				assert.response(res).has.status(504)
+			end)
 
 		end)
 	end)
-
 end
