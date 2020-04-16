9451c352c52d6826ee87f330394310dfb6e76b57
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2020-04-16 15:39:43 +0300

    chore(openid-connect) create oic_jwks row as part of the migration
    So that it does not need to be created on `init_worker`, which
    potentially may cause an issue (though unlikely) with Cassandra.

--- kong/plugins/openid-connect/migrations/002_200_to_210.lua
+++ kong/plugins/openid-connect/migrations/002_200_to_210.lua
@@ -1,3 +1,6 @@
+local jwks = require "kong.openid-connect.jwks"
+
+
 return {
   postgres = {
     up = [[
@@ -6,6 +9,26 @@ return {
         "jwks"  JSONB
       );
     ]],
+    teardown = function(connector)
+      local generated_jwks, err = jwks.new({ json = true })
+      if not generated_jwks then
+        return nil, err
+      end
+
+      assert(connector:connect_migrations())
+
+      local insert_query = string.format([[
+        INSERT INTO "oic_jwks" ("id", "jwks")
+             VALUES ('c3cfba2d-1617-453f-a416-52e6edb5f9a0', '%s')
+        ON CONFLICT DO NOTHING;
+      ]], generated_jwks)
+
+      local _, err = connector:query(insert_query)
+      if err then
+        return nil, err
+      end
+    end,
+
   },
 
   cassandra = {
@@ -15,5 +38,23 @@ return {
         jwks text
       );
     ]],
+    teardown = function(connector)
+      local generated_jwks, err = jwks.new({ json = true })
+      if not generated_jwks then
+        return nil, err
+      end
+
+      assert(connector:connect_migrations())
+
+      local insert_query = string.format([[
+        INSERT INTO oic_jwks (id, jwks)
+             VALUES (c3cfba2d-1617-453f-a416-52e6edb5f9a0, '%s') IF NOT EXISTS
+      ]], generated_jwks)
+
+      local _, err = connector:query(insert_query)
+      if err then
+        return nil, err
+      end
+    end,
   },
 }
