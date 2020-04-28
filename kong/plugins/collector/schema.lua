a1ed4bf6cf2c0bd0ca58ecb76c3dbe62e5165598
Amy Wang <awang1596@gmail.com>
2020-04-28 14:23:57 -0700

    Refactor(flush_timeout) Change flush_timeout default to 1

--- kong/plugins/collector/schema.lua
+++ kong/plugins/collector/schema.lua
@@ -7,7 +7,7 @@ return {
         fields = {
           { retry_count = { type = "number", default = 10 } },
           { queue_size = { type = "number", default = 100 } },
-          { flush_timeout = { type = "number", default = 2 } },
+          { flush_timeout = { type = "number", default = 1 } },
           { log_bodies = { type = "boolean", default = true } },
           { http_endpoint = { type = "string", required = true, default = "http://collector.com" } },
           { https_verify = { type = "boolean", default = false } },
