4627cf8d459308c8d6aad8cbf79caf51caa63b3b
Hisham Muhammad <hisham@gobolinux.org>
2020-06-04 22:14:36 -0300

    feat(migrations) migrations for workspace awareness
    Includes a migration for cache_key in plugins, including a DAO
    extension for blue-green support.
    
    Co-Authored-By: Raimon Grau <raimonster@gmail.com>

--- kong/db/dao/plugins.lua
+++ kong/db/dao/plugins.lua
@@ -326,4 +326,39 @@ function Plugins:get_handlers()
 end
 
 
+function Plugins:select_by_cache_key(key)
+
+  -- first try new way
+  local entity, new_err = self.super.select_by_cache_key(self, key)
+
+  if not new_err then -- the step above didn't fail
+    -- we still need to check whether the migration is done,
+    -- because the new table may be only partially full
+    local schema_state = assert(self.db:schema_state())
+
+    -- if migration is complete, disable this translator function and return
+    if schema_state:is_migration_executed("core", "009_200_to_210") then
+      Plugins.select_by_cache_key = self.super.select_by_cache_key
+      return entity
+    end
+  end
+
+  key = key:sub(1, -38) -- strip ":<ws_id>" from the end
+
+  -- otherwise, we either have not started migrating, or we're migrating but
+  -- the plugin identified by key doesn't have a cache_key yet
+  -- do things "the old way" in both cases
+  local row, old_err = self.super.select_by_cache_key(self, key)
+  if row then
+    return self:row_to_entity(row)
+  end
+
+  -- when both ways have failed, return the "new" error message.
+  -- otherwise, only return an error if the "old" version failed.
+  local err = (new_err and old_err) and new_err or old_err
+
+  return nil, err
+end
+
+
 return Plugins
