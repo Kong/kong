bc070c290827076c15ee421d561534bcb16cec5c
Raimon Grau (rgrau) <raimonster@gmail.com>
2020-06-23 17:58:12 +0100

    feat(key-auth-enc) add ws_id column FT-1360 (#1)
    * feat(key-auth-enc) add ws_id column
    
    * fix(key-auth-enc) use migration helper
    
    * fix(key-auth-enc) use new helper method for migration
    
    * chore(key-auth-enc) remomve run_on
    
    * refactor(key-auth-enc) rename migration to 200_to_210

---
+++ kong/plugins/key-auth-enc/migrations/001_200_to_210.lua
@@ -0,0 +1,12 @@
+local operations = require "kong.db.migrations.operations.200_to_210"
+
+local plugin_entities = {
+  {
+    name = "keyauth_enc_credentials",
+    primary_key = "id",
+    uniques = {"key"},
+    fks = {{name = "consumer", reference = "consumers", on_delete = "cascade"}},
+  }
+}
+
+return operations.ws_migrate_plugin(plugin_entities)
--- kong/plugins/key-auth-enc/migrations/init.lua
+++ kong/plugins/key-auth-enc/migrations/init.lua
@@ -1,3 +1,4 @@
 return {
   "000_base_key_auth_enc",
+  "001_200_to_210",
 }
