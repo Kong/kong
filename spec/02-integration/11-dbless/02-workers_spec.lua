4ae8b237d13691bfba7a0cfeaf4270e9fb949f81
Datong Sun <datong.sun@konghq.com>
2020-06-29 09:59:20 -0700

    fix test failure by not creating DB-less blueprints

--- spec/02-integration/11-dbless/02-workers_spec.lua
+++ spec/02-integration/11-dbless/02-workers_spec.lua
@@ -17,15 +17,9 @@ describe("Workers initialization #off", function()
   local admin_client, proxy_client
 
   lazy_setup(function()
-    helpers.get_db_utils("off", {
-      "routes",
-      "services",
-      "plugins",
-    })
-
     assert(helpers.start_kong({
       database   = "off",
-      nginx_worker_processes = 2,
+      nginx_worker_processes = 1,
     }))
 
     admin_client = assert(helpers.admin_client())
@@ -53,16 +47,21 @@ describe("Workers initialization #off", function()
     })
     assert.res_status(201, res)
 
-    -- make a request to ensure that proxying is working
-    -- (and to make some time for the worker to respawn)
-    res = assert(proxy_client:get("/1", { headers = { host = "example1.dev" } }))
-    assert.res_status(401, res)
+    helpers.signal_workers(nil, "-TERM")
+
+    proxy_client:close()
+    proxy_client = assert(helpers.proxy_client())
 
     local conf = helpers.get_running_conf()
     local _, code = helpers.execute("grep -F 'error building initial plugins iterator: plugins iterator was changed while rebuilding it' " ..
                                      conf.nginx_err_logs, true)
     local not_found = 1
     assert.equal(not_found, code)
+
+    -- make a request to ensure that proxying is working
+    -- (and to make some time for the worker to respawn)
+    res = assert(proxy_client:get("/1", { headers = { host = "example1.dev" } }))
+    assert.res_status(401, res)
   end)
 end)
 
