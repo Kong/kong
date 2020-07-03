195e6d66c33d96f4624234dcf0919e5abed160e0
jideel <jideelgh@free.fr>
2020-07-03 16:06:40 +0200

    Add facility configuration capability to syslog plugin (feat)

--- kong/plugins/syslog/handler.lua
+++ kong/plugins/syslog/handler.lua
@@ -26,9 +26,32 @@ local LOG_LEVELS = {
   emerg = 0
 }
 
-local function send_to_syslog(log_level, severity, message)
+local FACILITIES = {
+  FACILITY_AUTH = lsyslog.FACILITY_AUTH,
+  FACILITY_AUTHPRIV = lsyslog.FACILITY_AUTHPRIV,
+  FACILITY_CRON = lsyslog.FACILITY_CRON,
+  FACILITY_DAEMON = lsyslog.FACILITY_DAEMON,
+  FACILITY_FTP = lsyslog.FACILITY_FTP,
+  FACILITY_KERN = lsyslog.FACILITY_KERN,
+  FACILITY_LPR = lsyslog.FACILITY_LPR,
+  FACILITY_MAIL = lsyslog.FACILITY_MAIL,
+  FACILITY_NEWS = lsyslog.FACILITY_NEWS,
+  FACILITY_SYSLOG = lsyslog.FACILITY_SYSLOG,
+  FACILITY_USER = lsyslog.FACILITY_USER,
+  FACILITY_UUCP = lsyslog.FACILITY_UUCP,
+  FACILITY_LOCAL0 = lsyslog.FACILITY_LOCAL0,
+  FACILITY_LOCAL1 = lsyslog.FACILITY_LOCAL1,
+  FACILITY_LOCAL2 = lsyslog.FACILITY_LOCAL2,
+  FACILITY_LOCAL3 = lsyslog.FACILITY_LOCAL3,
+  FACILITY_LOCAL4 = lsyslog.FACILITY_LOCAL4,
+  FACILITY_LOCAL5 = lsyslog.FACILITY_LOCAL5,
+  FACILITY_LOCAL6 = lsyslog.FACILITY_LOCAL6,
+  FACILITY_LOCAL7 = lsyslog.FACILITY_LOCAL7
+}
+
+local function send_to_syslog(log_level, severity, message, facility)
   if LOG_LEVELS[severity] <= LOG_LEVELS[log_level] then
-    l_open(SENDER_NAME, lsyslog.FACILITY_USER)
+    l_open(SENDER_NAME, FACILITIES[facility])
     l_log(lsyslog["LOG_" .. string_upper(severity)], cjson.encode(message))
   end
 end
@@ -39,11 +62,11 @@ local function log(premature, conf, message)
   end
 
   if message.response.status >= 500 then
-    send_to_syslog(conf.log_level, conf.server_errors_severity, message)
+    send_to_syslog(conf.log_level, conf.server_errors_severity, message, conf.syslog_facility)
   elseif message.response.status >= 400 then
-    send_to_syslog(conf.log_level, conf.client_errors_severity, message)
+    send_to_syslog(conf.log_level, conf.client_errors_severity, message, conf.syslog_facility)
   else
-    send_to_syslog(conf.log_level, conf.successful_severity, message)
+    send_to_syslog(conf.log_level, conf.successful_severity, message, conf.syslog_facility)
   end
 end
 
--- kong/plugins/syslog/schema.lua
+++ kong/plugins/syslog/schema.lua
@@ -6,6 +6,16 @@ local severity = {
   one_of = { "debug", "info", "notice", "warning", "err", "crit", "alert", "emerg" },
 }
 
+local facility = {
+	type = "string",
+	default = "FACILITY_USER",
+	one_of = { "FACILITY_AUTH", "FACILITY_AUTHPRIV",  "FACILITY_CRON",  "FACILITY_DAEMON",
+			"FACILITY_FTP",  "FACILITY_KERN",  "FACILITY_LPR",  "FACILITY_MAIL",
+			"FACILITY_NEWS", "FACILITY_SYSLOG",  "FACILITY_USER",  "FACILITY_UUCP",
+			"FACILITY_LOCAL0",  "FACILITY_LOCAL1", "FACILITY_LOCAL2",  "FACILITY_LOCAL3",
+			"FACILITY_LOCAL4",  "FACILITY_LOCAL5",  "FACILITY_LOCAL6",  "FACILITY_LOCAL7" },
+}
+
 return {
   name = "syslog",
   fields = {
@@ -17,6 +27,7 @@ return {
           { successful_severity = severity },
           { client_errors_severity = severity },
           { server_errors_severity = severity },
+          { syslog_facility = facility },
     }, }, },
   },
 }
