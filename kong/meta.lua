99872d71441ee6a8444184425b694e0b43242e75
Guilherme Salazar <gsz@acm.org>
2020-07-09 12:58:32 -0300

    release: 2.1.0rc1

--- kong/meta.lua
+++ kong/meta.lua
@@ -2,7 +2,7 @@ local version = setmetatable({
   major = 2,
   minor = 1,
   patch = 0,
-  suffix = "beta.1"
+  suffix = "rc.1"
 }, {
   -- our Makefile during certain releases adjusts this line. Any changes to
   -- the format need to be reflected in both places
