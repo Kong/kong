83c8c60da69aad71cf9555f58e5c29d0f553b6e5
Shashank <shashank@Shashanks-MBP.localdomain>
2020-08-03 10:49:32 -0500

    Dir clean up as per recommendations

--- spec/01-access_spec.lua
+++ spec/01-access_spec.lua
@@ -2,27 +2,50 @@ local helpers = require "spec.helpers"
 local version = require("version").version or require("version")
 
 
-local PLUGIN_NAME = "myplugin"
+local PLUGIN_NAME = "mocking"
 
 
 for _, strategy in helpers.each_strategy() do
   describe(PLUGIN_NAME .. ": (access) [#" .. strategy .. "]", function()
     local client
+      
+      lazy_setup(function()
+        local users_apispec = assert(io.open("users_OASv2.yaml"))
+        local bp = helpers.get_db_utils(strategy, {
+          "routes",
+          "services",
+          "files",
+        })
 
-    lazy_setup(function()
-
-      local bp = helpers.get_db_utils(strategy, nil, { PLUGIN_NAME })
+        local anonymous_user = bp.files:insert {
+          path = "default:specs/users.yaml",
+          content = "",
+        }
 
       -- Inject a test route. No need to create a service, there is a default
       -- service which will echo the request.
       local route1 = bp.routes:insert({
         hosts = { "test1.com" },
       })
+    
+      local route2 = bp.routes:insert({
+        hosts = { "mock-error.com" },
+      })
       -- add the plugin to test to the route we created
       bp.plugins:insert {
         name = PLUGIN_NAME,
         route = { id = route1.id },
-        config = {},
+        config = {
+          api_specification_filename = "users.yaml",
+        },
+      }
+      
+      bp.plugins:insert {
+        name = PLUGIN_NAME,
+        route = { id = route2.id },
+        config = {
+          api_specification_filename = "",
+        },
       }
 
       -- start kong
@@ -50,8 +73,8 @@ for _, strategy in helpers.each_strategy() do
 
 
 
-    describe("request", function()
-      it("gets a 'hello-world' header", function()
+    describe(PLUGIN_NAME.." ", function()
+      it("gets a X-Kong-Mocking-Plugin header", function()
         local r = assert(client:send {
           method = "GET",
           path = "/request",  -- makes mockbin return the entire request
@@ -60,31 +83,28 @@ for _, strategy in helpers.each_strategy() do
           }
         })
         -- validate that the request succeeded, response status 200
-        assert.response(r).has.status(200)
-        -- now check the request (as echoed by mockbin) to have the header
-        local header_value = assert.request(r).has.header("hello-world")
-        -- validate the value of that header
-        assert.equal("this is on a request", header_value)
+        --assert.response(r).has.status(200)
+        local body = assert.res_status(200, res)
+        local json = cjson.decode(body)
+        local upStreamHeaderVal  = json.headers["X-Kong-Mocking-Plugin"]
+        assert.equal("true",upStreamHeaderVal)
       end)
     end)
 
 
 
-    describe("response", function()
-      it("gets a 'bye-world' header", function()
+    describe(PLUGIN_NAME.." ", function()
+      it("Empty spec filename check", function()
         local r = assert(client:send {
           method = "GET",
           path = "/request",  -- makes mockbin return the entire request
           headers = {
-            host = "test1.com"
+            host = "mock-error.com"
           }
         })
         -- validate that the request succeeded, response status 200
-        assert.response(r).has.status(200)
-        -- now check the response to have the header
-        local header_value = assert.response(r).has.header("bye-world")
-        -- validate the value of that header
-        assert.equal("this is on the response", header_value)
+        assert.res_status(404, res)
+      
       end)
     end)
 
