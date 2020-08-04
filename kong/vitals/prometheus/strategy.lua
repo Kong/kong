5d30df51f9e99f527c67f8717119c49eca72a6ed
Jack Kavanagh <jackkav@gmail.com>
2020-08-04 16:26:35 +0100

    feat(vitals) can resolve entity names

--- kong/vitals/prometheus/strategy.lua
+++ kong/vitals/prometheus/strategy.lua
@@ -12,6 +12,7 @@ local WARN       = ngx.WARN
 local singletons = require "kong.singletons"
 local pl_stringx   = require "pl.stringx"
 local statsd_handler = require "kong.vitals.prometheus.statsd.handler"
+local enums = require "kong.enterprise_edition.dao.enums"
 
 local http = require "resty.http"
 local cjson = require "cjson.safe"
@@ -729,6 +730,27 @@ function _M:select_consumer_stats(opts)
   end
 end
 
+-- @param entity: consumer or service DAO
+local function resolve_entity_metadata(entity)
+  local is_service = not not entity.name
+  if is_service then
+    return { name = entity.name }
+  end
+  if entity.type == enums.CONSUMERS.TYPE.APPLICATION then
+    return {
+      name = "",
+      app_id = entity.username:sub(0, entity.username:find("_") - 1),
+      app_name = entity.username:sub(entity.username:find("_") + 1)
+    }
+  end
+  return {
+    name = entity.username or entity.custom_id,
+    app_id = "",
+    app_name = "",
+  }
+end
+_M.resolve_entity_metadata = resolve_entity_metadata
+
 -- @param entity: consumer or service
 -- @param entity_id: UUID or null, signifies how each row is indexed
 -- @param interval: seconds, minutes, hours, days, weeks, months
@@ -738,6 +760,18 @@ function _M:status_code_report_by(entity, entity_id, interval, start_ts)
   interval = interval or 60
   local is_consumer = entity == "consumer"
   local is_timeseries_report = not not entity_id
+
+  local entities = {}
+  local plural_entity = entity .. 's'
+  if is_timeseries_report then
+    local row = kong.db[plural_entity]:select({ id = entity_id }, { workspace = null })
+    entities[row.id] = resolve_entity_metadata(row)
+  else
+    for row in kong.db[plural_entity]:each(nil, { workspace = null }) do
+      entities[row.id] = resolve_entity_metadata(row)
+    end
+  end
+
   local stats = {}
   local inspect = require "inspect"
   local all_services = "count by (service)(kong_status_code_per_workspace)"
@@ -762,9 +796,23 @@ function _M:status_code_report_by(entity, entity_id, interval, start_ts)
   kong.log.err(inspect(res))
   for _, series_list in ipairs(res) do
     for _, series in ipairs(series_list) do
-    kong.log.err("Entity ID!!!!!!! " .. series.metric[entity])
-    local index = series.metric[entity]
-    stats[index] = stats[index] or { ["total"] = 0, ["2XX"] = 0, ["4XX"] = 0, ["5XX"] = 0 }
+      local index, entity_metadata
+      -- if is_timeseries_report then
+      --   local timestamp = tostring(value[1])
+      --   index = timestamp
+      --   entity_metadata = entities[entity_id] or {}
+      -- else
+      index = series.metric[entity]
+      entity_metadata = entities[index] or {}
+      -- end
+      kong.log.err("Entity ID!!!!!!! " .. series.metric[entity])
+      local index = series.metric[entity]
+      stats[index] = stats[index] or { ["total"] = 0, ["2XX"] = 0, ["4XX"] = 0, ["5XX"] = 0 }
+      stats[index]["name"] = entity_metadata.name
+      if is_consumer then
+        stats[index]["app_id"] = entity_metadata.app_id
+        stats[index]["app_name"] = entity_metadata.app_name
+      end
     end
   end
   local meta = {
@@ -817,6 +865,7 @@ function _M:latency_report(hostname, interval, start_ts)
   
   return { stats=stats, meta=meta }
 end
+
 
 function _M:log()
   self.statsd_handler:log()
