9b485b0fd67e4a90545a04c791929ad313590aaa
taotao2014 <20034838@qq.com>
2020-08-05 15:35:39 +0800

    modify name to get_balancer_health

--- kong/pdk/upstream.lua
+++ kong/pdk/upstream.lua
@@ -14,21 +14,21 @@ local function new()
   local upstream = {}
 
   ---
-  -- Get upstream balancer health
+  -- Get balancer health
   --
-  -- @function kong.upstream.balancer_health
+  -- @function kong.upstream.get_balancer_health
   -- @phases access
   -- @tparam string upstream_name
   -- @treturn health_info|nil `health_info` on success, or `nil` if no health_info entities where found
   -- @treturn string|nil An error message describing the error if there was one.
   --
   -- @usage
-  -- local ok, err = kong.upstream.balancer_health("upstream_name")
+  -- local ok, err = kong.upstream.get_balancer_health("upstream_name")
   -- if not ok then
   --   kong.log.err(err)
   --   return
   -- end
-  function upstream.balancer_health(upstream_name)
+  function upstream.get_balancer_health(upstream_name)
     check_phase(PHASES.access)
 
     if type(upstream_name) ~= "string" then
