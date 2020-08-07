6c9051e961ff10e1f871168f69e67a798e673b22
Raimon Grau <raimonster@gmail.com>
2020-08-07 12:40:00 +0100

    fix(scripts) slack reminders bump to next/2.1.x.x

--- scripts/generate-slack-reminders.sh
+++ scripts/generate-slack-reminders.sh
@@ -5,11 +5,13 @@ shopt -s inherit_errexit
 reminder() {
   echo "/remind #team-fast-track scripts/sync-ce-ee -f $1 -t $2 every $3"
 }
+
 main() {
+  LATEST="next/2.1.x.x"
   reminder "next/1.3.0.x" "next/1.5.0.x" "Friday"
-  reminder "next/1.5.0.x" "next/2.1.0.x" "Tuesday"
-  reminder "next/2.1.0.x" "master" "Wednesday"
-  reminder "kong:master" "kong-ee:next/2.1.0.x" "Thursday"
+  reminder "next/1.5.0.x" "$LATEST" "Tuesday"
+  reminder "$LATEST" "master" "Wednesday"
+  reminder "kong:master" "kong-ee:$LATEST" "Thursday"
 }
 
 main "$@"
