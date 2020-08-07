f9ae3e2bee117cd754492429a664610762492729
eskerda <eskerda@gmail.com>
2020-08-07 19:11:26 +0200

    fix(migrations/hmac) fix plugin entities

---
+++ kong/plugins/hmac-auth/migrations/enterprise/001_1500_to_2100.lua
@@ -0,0 +1,13 @@
+local operations = require "kong.enterprise_edition.db.migrations.operations.1500_to_2100"
+
+local plugin_entities = {
+  {
+    name = "hmacauth_credentials",
+    primary_key = "id",
+    uniques = {"username"},
+    fks = {{name = "consumer", reference = "consumers", on_delete = "cascade"}},
+  }
+}
+
+
+return operations.ws_migrate_plugin(plugin_entities)
