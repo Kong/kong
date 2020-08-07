d226dbc90c4ed64e8a5e6db78f487f2d0b533de1
debu99 <chichibb199@gmail.com>
2020-08-07 17:15:52 +0800

    add enabled option to handler.lua

--- kong/plugins/request-termination/handler.lua
+++ kong/plugins/request-termination/handler.lua
@@ -21,27 +21,31 @@ local RequestTerminationHandler = {}
 
 
 RequestTerminationHandler.PRIORITY = 2
-RequestTerminationHandler.VERSION = "2.0.0"
+RequestTerminationHandler.VERSION = "2.0.1"
 
 
 function RequestTerminationHandler:access(conf)
   local status  = conf.status_code
   local content = conf.body
+  local enabled = conf.enabled
 
-  if content then
-    local headers = {
-      ["Content-Type"] = conf.content_type
-    }
+  
+  if enabled then
+    if content then
+      local headers = {
+        ["Content-Type"] = conf.content_type
+      }
 
-    if singletons.configuration.enabled_headers[constants.HEADERS.SERVER] then
-      headers[constants.HEADERS.SERVER] = server_header
+      if singletons.configuration.enabled_headers[constants.HEADERS.SERVER] then
+        headers[constants.HEADERS.SERVER] = server_header
+      end
+
+      return kong.response.exit(status, content, headers)
     end
 
-    return kong.response.exit(status, content, headers)
+    local message = conf.message or DEFAULT_RESPONSE[status]
+    return kong.response.exit(status, message and { message = message } or nil)
   end
-
-  local message = conf.message or DEFAULT_RESPONSE[status]
-  return kong.response.exit(status, message and { message = message } or nil)
 end
 
 
