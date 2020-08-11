185d3bd4203be55549fbbdaf1457803168f6161b
Thijs Schreijer <thijs@thijsschreijer.nl>
2020-08-11 11:02:13 +0200

    implement helper function to find fixtures

--- spec/01-access_spec.lua
+++ spec/01-access_spec.lua
@@ -1,5 +1,21 @@
 local helpers = require "spec.helpers"
 local version = require("version").version or require("version")
+local pl_path = require "pl.path"
+
+local fixture_path do
+  -- this code will get debug info and from that determine the file
+  -- location, so fixtures can be found based of this path
+  local info = debug.getinfo(function() end)
+  fixture_path = info.source
+  if fixture_path:sub(1,1) == "@" then
+    fixture_path = fixture_path:sub(2, -1)
+  end
+  fixture_path = pl_path.splitpath(fixture_path) .. "/resources/"
+end
+
+local function read_fixture(filename)
+  return assert(helpers.utils.readfile(fixture_path .. filename))
+end
 
 
 local PLUGIN_NAME = "mocking"
@@ -10,11 +26,11 @@ local WORKING_DIR = "/Users/steve.young/Documents/GitHub/kong-plugin-mocking"
 for _, strategy in helpers.each_strategy() do
   describe(PLUGIN_NAME .. ": (access) [#" .. strategy .. "]", function()
     local client
-      
+
       lazy_setup(function()
-        local f = assert(io.open("spec/resources/stock.json"))
-        local str = f:read("*a")
-        f:close()
+        --local f = assert(io.open(fixture_path .. "/resources/stock.json"))
+        --local str = f:read("*a")
+        --f:close()
 
         local bp = helpers.get_db_utils(strategy, {
           "routes",
@@ -24,7 +40,7 @@ for _, strategy in helpers.each_strategy() do
 
         local anonymous_user = bp.files:insert {
           path = "default:specs/stock.json",
-          content = str,
+          content = read_fixture("stock.json"),
         }
 
       -- Inject a test route. No need to create a service, there is a default
@@ -32,7 +48,7 @@ for _, strategy in helpers.each_strategy() do
       local route1 = bp.routes:insert({
         hosts = { "test1.com" },
       })
-    
+
       -- add the plugin to test to the route we created
       bp.plugins:insert {
         name = PLUGIN_NAME,
@@ -81,7 +97,7 @@ for _, strategy in helpers.each_strategy() do
         local body = assert.res_status(200, res)
         local json = cjson.decode(body)
         local upStreamHeaderVal  = json.headers["X-Kong-Mocking-Plugin"]
-        -- check mocking plugin 
+        -- check mocking plugin
         assert.equal("true",upStreamHeaderVal)
       end)
     end)
@@ -99,7 +115,7 @@ for _, strategy in helpers.each_strategy() do
         })
         -- validate that the request succeeded, response status 200
         assert.res_status(404, res)
-      
+
       end)
     end)
 
