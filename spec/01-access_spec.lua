d2fb84374d836403a9ed26fe7d8c478fa9da9db8
Shashank <shashank@Shashanks-MBP.localdomain>
2020-08-17 11:29:41 -0500

    Insert service and try to enable plugin on route

--- spec/01-access_spec.lua
+++ spec/01-access_spec.lua
@@ -2,6 +2,12 @@ local helpers = require "spec.helpers"
 local version = require("version").version or require("version")
 local pl_path = require "pl.path"
 
+local lyaml       = require "lyaml"
+local cjson       = require("cjson.safe").new()
+local kong = kong
+local ngx_log = ngx.log
+local ngx_WARN = ngx.WARN
+
 local fixture_path do
   -- this code will get debug info and from that determine the file
   -- location, so fixtures can be found based of this path
@@ -13,8 +19,13 @@ local fixture_path do
   fixture_path = pl_path.splitpath(fixture_path) .. "/resources/"
 end
 
+
 local function read_fixture(filename)
-  return assert(helpers.utils.readfile(fixture_path .. filename))
+  ngx_log(ngx_WARN, "fixture path: ", fixture_path)
+  local content  = assert(helpers.utils.readfile(fixture_path .. filename))
+  --ngx_log(ngx_WARN, "content", content)
+   return content
+  --return assert(helpers.utils.readfile(fixture_path .. filename))
 end
 
 
@@ -40,23 +51,51 @@ for _, strategy in helpers.each_strategy() do
 
         assert(db.files:insert {
           path = "default:specs/stock.json",
-          content = read_fixture("stock.json"),
+          contents = read_fixture("stock.json"),  
+          --ngx_log(ngx_WARN, "contents: ", read_fixture("stock.json"))
         })
 
+        assert(db.files:insert {
+          path = "defualt:specs/multipleexamples.json",
+          contents = read_fixture("multipleexamples.json"),  
+          --ngx_log(ngx_WARN, "contents: ", read_fixture("stock.json"))
+        })
+        
+        local service1 = bp.services:insert{
+          protocol = "http",
+          port     = 80,
+          host     = "mockbin.com",
+        }
+        
       -- Inject a test route. No need to create a service, there is a default
       -- service which will echo the request.
-      local route1 = bp.routes:insert({
+      local route1 = db.routes:insert({
         hosts = { "test1.com" },
+        service    = service1,
+
       })
 
       -- add the plugin to test to the route we created
-      bp.plugins:insert {
-        name = PLUGIN_NAME,
+      db.plugins:insert {
+        name = "mocking",
         route = { id = route1.id },
         config = {
-          api_specification_filename = "stock.json",
+          api_specification_filename = "multipleexamples.json",
         },
       }
+      local route2 = db.routes:insert({
+        hosts = { "test2.com" },
+      })
+
+      -- add the plugin to test to the route we created
+      db.plugins:insert {
+        name = PLUGIN_NAME,
+        route = { id = route2.id },
+        config = {
+          api_specification_filename = " ",
+        },
+      }
+
 
       -- start kong
       assert(helpers.start_kong({
@@ -87,15 +126,17 @@ for _, strategy in helpers.each_strategy() do
       it("gets a X-Kong-Mocking-Plugin header", function()
         local r = assert(client:send {
           method = "GET",
-          path = "/stock/historical",  -- makes mockbin return the entire request
+          path = "/pet/findByStatus/MultipleExamples",  -- makes mockbin return the entire request
           headers = {
             host = "test1.com"
           }
         })
         -- validate that the request succeeded, response status 200
         --assert.response(r).has.status(200)
-        local body = assert.res_status(200, res)
+        local body = assert.res_status(200, r)
+        ngx_log(ngx_WARN, "Body: ", body)
         local json = cjson.decode(body)
+        --ngx_log(ngx_WARN, "json value: ", json)
         local upStreamHeaderVal  = json.headers["X-Kong-Mocking-Plugin"]
         -- check mocking plugin
         assert.equal("true",upStreamHeaderVal)
@@ -108,13 +149,13 @@ for _, strategy in helpers.each_strategy() do
       it("Empty spec filename check", function()
         local r = assert(client:send {
           method = "GET",
-          path = "/no_path",  -- makes mockbin return the entire request
+          path = "/pet/findByStatus/MultipleExamples",  -- makes mockbin return the entire request
           headers = {
             host = "test1.com"
           }
         })
         -- validate that the request succeeded, response status 200
-        assert.res_status(404, res)
+        assert.res_status(404, r)
 
       end)
     end)
