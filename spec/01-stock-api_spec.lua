7ba0ac063187adb775bb1f4575a2b05d36c2d48e
KongSteve <60162013+KongSteve@users.noreply.github.com>
2020-08-20 09:17:40 +1000

    Add stock test suite

--- spec/01-stock-api_spec.lua
+++ spec/01-stock-api_spec.lua
@@ -1,6 +1,9 @@
 local helpers   = require "spec.helpers"
 local pl_path   = require "pl.path"
 local cjson     = require("cjson.safe").new()
+local ngx_log   = ngx.log
+local ngx_WARN  = ngx.WARN
+
 local PLUGIN_NAME = "mocking"
 
 local fixture_path do
@@ -16,7 +19,11 @@ end
 
 
 local function read_fixture(filename)
-  return assert(helpers.utils.readfile(fixture_path .. filename))
+  ngx_log(ngx_WARN, "fixture path: ", fixture_path)
+  local content  = assert(helpers.utils.readfile(fixture_path .. filename))
+  --ngx_log(ngx_WARN, "content", content)
+   return content
+  --return assert(helpers.utils.readfile(fixture_path .. filename))
 end
 
 for _, strategy in helpers.each_strategy() do
@@ -31,16 +38,16 @@ for _, strategy in helpers.each_strategy() do
         }, { PLUGIN_NAME })
 
         assert(db.files:insert {
-          path = "specs/T1-1748.json",
-          contents = read_fixture("T1-1748.json"),  
+          path = "specs/stock.json",
+          contents = read_fixture("stock.json"),  
         })
-
+        
         local service1 = bp.services:insert{
           protocol = "http",
           port     = 80,
           host     = "mocking.com",
         }
-
+        
       local route1 = db.routes:insert({
         hosts = { "mocking.com" },
         service    = service1,
@@ -52,7 +59,7 @@ for _, strategy in helpers.each_strategy() do
         name = PLUGIN_NAME,
         service = { id = service1.id },
         config = {
-          api_specification_filename = "T1-1748.json",
+          api_specification_filename = "stock.json",
           random_delay = false
         },
       }
@@ -80,25 +87,26 @@ for _, strategy in helpers.each_strategy() do
       if client then client:close() end
     end)
 
-    describe("T1-1748 Extract example in OpenApi V3 spec", function()
+    describe("Stock API Specification tests", function()
       it("/stock/historical happy path", function()
         local r = assert(client:send {
           method = "GET",
-          path = "/pet/findByStatus/MultipleExamples",  -- makes mockbin return the entire request
+          path = "/stock/historical",  -- makes mockbin return the entire request
           headers = {
             host = "mocking.com"
           }
         })
         -- validate that the request succeeded, response status 200
+        --assert.response(r).has.status(200)
         local body = assert.res_status(200, r)
-        kong.log.notice(body)
+        ngx_log(ngx_WARN, "Body: ", body)
         local header_value = assert.response(r).has.header("X-Kong-Mocking-Plugin")
         -- validate the value of that header
         assert.equal("true", header_value)
       end)
     end)
-
-    describe("T1-1748 Test not defined Random path", function()
+    
+    describe("Stock API Specification tests", function()
       it("/random_path Random path", function()
         local r = assert(client:send {
           method = "GET",
