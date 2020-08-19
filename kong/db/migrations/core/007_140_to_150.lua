c183965602c3e0888693418957875c0ca6a39ffe
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2020-08-19 16:40:31 +0300

    style(migrations) just makes our migrations more consistent
    ### Summary
    
    Just makes our migrations more uniform and consistent, and uses better
    names on some variables.

--- kong/db/migrations/core/007_140_to_150.lua
+++ kong/db/migrations/core/007_140_to_150.lua
@@ -19,6 +19,7 @@ return {
     ]],
 
     teardown = function(connector)
+      local cassandra = require "cassandra"
       local coordinator = assert(connector:connect_migrations())
 
       for rows, err in coordinator:iterate("SELECT id, path_handling FROM routes") do
@@ -26,11 +27,13 @@ return {
           return nil, err
         end
 
-        for _, row in ipairs(rows) do
-          if row.path_handling ~= "v0" and row.path_handling ~= "v1" then
-            local _, err = connector:query([[
-              UPDATE routes SET path_handling = 'v1'
-              WHERE partition = 'routes' AND id = ]] .. row.id)
+        for i = 1, #rows do
+          local route = rows[i]
+          if route.path_handling ~= "v0" and route.path_handling ~= "v1" then
+            local _, err = connector:query(
+              "UPDATE routes SET path_handling = 'v1' WHERE partition = 'routes' AND id = ?",
+              { cassandra.uuid(route.id) }
+            )
             if err then
               return nil, err
             end
