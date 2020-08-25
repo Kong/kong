2c8f1350c88a6f1182cef5c12f552c7e34447c11
Murillo Paula <murillo@murillopaula.com>
2020-08-25 10:57:49 -0300

    chore(response-transformer-advanced) release: 0.4.1

--- kong/plugins/response-transformer-advanced/handler.lua
+++ kong/plugins/response-transformer-advanced/handler.lua
@@ -92,6 +92,6 @@ function ResponseTransformerHandler:body_filter(conf)
 end
 
 ResponseTransformerHandler.PRIORITY = 800
-ResponseTransformerHandler.VERSION = "0.4.0"
+ResponseTransformerHandler.VERSION = "0.4.1"
 
 return ResponseTransformerHandler
