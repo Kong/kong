fb74ade4471c17f4f3a985e04fd8b4c30ba3bcb3
KongGuide <kongguide@outlook.com>
2020-08-28 08:42:50 +0800

    rebase to next limit by path

--- kong/plugins/rate-limiting/handler.lua
+++ kong/plugins/rate-limiting/handler.lua
@@ -67,6 +67,9 @@ local function get_identifier(conf)
 
   elseif conf.limit_by == "header" then
     identifier = kong.request.get_header(conf.header_name)
+
+  elseif conf.limit_by == "path" then
+    identifier = kong.request.get_path(conf.path)
   end
 
   return identifier or kong.client.get_forwarded_ip()
