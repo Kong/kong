ade199f0f7c9f93c66c3607733d47c15e7ea0409
Javier Guerra <javier.guerra@konghq.com>
2020-09-21 13:50:16 -0500

    slightly nicer "fake http" request object

--- kong/tools/stream_api.lua
+++ kong/tools/stream_api.lua
@@ -9,68 +9,101 @@ function stream_api.register_endpoint(path, handler)
 end
 
 
-function stream_api.serve()
-  local sock = assert(ngx.req.socket())
-  local line = sock:receive()
+local stream_mt = {}
+stream_mt.__index = stream_mt
 
-  local path = line:match("^%S+%s+(%S+)")
+function stream_api.serve()
+  local req = setmetatable({ socket = ngx.req.socket() }, stream_mt)
+
+  local path = req:get_path()
   local handler = path and _endpoints[path]
   if handler then
-    handler(sock, line)
-  else
-    stream_api.send_response(sock, "404 Not Found")
+    req:exit(handler(req))
   end
+
+  req:exit(req:response("404 Not Found"))
 end
 
 
-function stream_api.get_headers(socket)
-  local o = {}
+function stream_mt:get_line()
+  if not self.req_line then
+    self.req_line = self.socket:receive("*l")
+  end
 
-  while true do
-    local l = socket:receive("*l")
-    if l == "" then
-      return o
-    end
+  return self.req_line
+end
 
-    local k, v = l:match("^%s*(%S+)%s*:%s*(.*)$")
-    if k then
-      k = k:lower():gsub('-', '_')
-      if o[k] then
-        v = o[k] .. " " .. v
+function stream_mt:get_path()
+  if not self.path then
+    self.path = self:get_line():match("^%S+%s+(%S+)")
+  end
+
+  return self.path
+end
+
+function stream_mt:get_headers()
+  if not self.headers then
+    local headers = {}
+
+    while true do
+      local l = self.socket:receive("*l")
+      if l == "" then
+        self.headers = headers
+        break
       end
 
-      o[k] = v
+      local k, v = l:match("^%s*(%S+)%s*:%s*(.*)%s*$")
+      if k then
+        k = k:lower():gsub('-', '_')
+        if headers[k] then
+          v = headers[k] .. " " .. v
+        end
+        headers[k] = v
+      end
     end
+
+    self.headers = headers
   end
+
+  return self.headers
 end
 
-
-function stream_api:serialize_headers(headers)
-  local o = {}
-  for k, v in pairs(headers) do
-    o[#o + 1] = string.format("%s: %s", k, v)
+function stream_mt:get_body()
+  if not self.body then
+    if not self.content_length then
+      self.content_length = tonumber(self:get_headers().content_length)
+    end
+    self.body = self.socket:receive(self.content_length or "*a")
   end
+
+  return self.body
+end
+
+function stream_mt:response(status, headers, body)
+  local o = {"HTTP/1.1 " .. tostring(status)}
+
+  if headers then
+    if type(body) == "string" then
+      headers["Content-Length"] = #body
+    end
+
+    for k, v in pairs(headers or {}) do
+      o[#o + 1] = string.format("%s: %s", k, v)
+    end
+  end
+
+  o[#o + 1] = ""
+  o[#o + 1] = body or ""
+
   return table.concat(o, "\r\n")
 end
 
-
-function stream_api.send_response(socket, status, headers, body)
-  socket:send("HTTP/1.1 " .. tostring(status) .. "\r\n")
-
-  if headers then
-    if type(headers) ~= "string" then
-      headers = stream_api.serialize_headers(headers)
-    end
-
-    socket:send(headers)
-  end
-  socket:send("\r\n")
-
-  if body then
-    socket:send(body)
+function stream_mt:exit(payload)
+  if payload then
+    self.socket:send(payload)
   end
 
-  socket:shutdown("send")
+  self.socket:shutdown("send")
 end
 
 
--- spec/fixtures/custom_plugins/kong/plugins/stream-api-echo/handler.lua
+++ spec/fixtures/custom_plugins/kong/plugins/stream-api-echo/handler.lua
@@ -4,14 +4,11 @@ local echo_mod = {
   PRIORITY = 1000,
 }
 
-stream_api.register_endpoint("/echo", function(sock, reqline)
-  local headers = stream_api.get_headers(sock)
-  local body = sock:receive(tonumber(headers.content_length))
-  --print ("body: ", tostring(line))
+stream_api.register_endpoint("/echo", function(req)
+  local body = req:get_body()
 
-  stream_api.send_response(sock, 200, nil, body)
+  return req:response(200, nil, body)
 end)
-
 
 return echo_mod
 
