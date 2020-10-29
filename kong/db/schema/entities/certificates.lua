73735f4e6c48a53993780d797c119039dadddf16
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2020-10-29 21:29:52 +0200

    feat(db) certificates to have cert_alt and key_alt fields
    The PR #6509 added support for static configuration to be able to
    set multiple certificates and keys with:
    
    - `ssl_cert` and `ssl_cert_key`
    - `admin_ssl_cert` and `admin_ssl_cert_key`
    - `status_ssl_cert` and `status_ssl_cert_key`
    
    But it didn't yet allow `certificates entity` to contain multiple
    certificates. This PR changes `certificates entity` to contain two
    new fields:
    
    - `cert_alt`
    - `key_alt`
    
    Where you can configure alternative certificate and the matching key,
    this way you can use `cert` and `key` to store ECDSA certificate and
    `cert_alt` and `cert_key` to store RSA certificate.

--- kong/db/schema/entities/certificates.lua
+++ kong/db/schema/entities/certificates.lua
@@ -2,6 +2,10 @@ local typedefs = require "kong.db.schema.typedefs"
 local openssl_pkey = require "resty.openssl.pkey"
 local openssl_x509 = require "resty.openssl.x509"
 
+
+local type = type
+
+
 return {
   name        = "certificates",
   primary_key = { "id" },
@@ -13,10 +17,13 @@ return {
     { created_at     = typedefs.auto_timestamp_s },
     { cert           = typedefs.certificate { required = true }, },
     { key            = typedefs.key         { required = true }, },
+    { cert_alt       = typedefs.certificate { required = false }, },
+    { key_alt        = typedefs.key         { required = false }, },
     { tags           = typedefs.tags },
   },
 
   entity_checks = {
+    { mutually_required = { "cert_alt", "key_alt" } },
     { custom_entity_check = {
       field_sources = { "cert", "key" },
       fn = function(entity)
@@ -29,6 +36,39 @@ return {
 
         return true
       end,
-    } }
+    } },
+    { custom_entity_check = {
+      field_sources = { "cert_alt", "key_alt" },
+      fn = function(entity)
+        if type(entity.cert_alt) == "string" and type(entity.key_alt) == "string" then
+          local cert_alt = openssl_x509.new(entity.cert_alt)
+          local key_alt = openssl_pkey.new(entity.key_alt)
+
+          if cert_alt:get_pubkey():to_PEM() ~= key_alt:to_PEM("public") then
+            return nil, "alternative certificate does not match key"
+          end
+        end
+
+        return true
+      end,
+    } },
+    { custom_entity_check = {
+      field_sources = { "cert", "cert_alt" },
+      fn = function(entity)
+        if type(entity.cert) == "string" and type(entity.cert_alt) == "string" then
+          local cert = openssl_x509.new(entity.cert)
+          local cert_alt = openssl_x509.new(entity.cert_alt)
+          local cert_type = cert:get_pubkey():get_key_type()
+          local cert_alt_type = cert_alt:get_pubkey():get_key_type()
+          if cert_type.id == cert_alt_type.id then
+            return nil, "certificate and alternative certificate need to have " ..
+                        "different type (e.g. RSA and ECDSA), the provided " ..
+                        "certificates were both of the same type"
+          end
+        end
+
+        return true
+      end,
+    } },
   }
 }
