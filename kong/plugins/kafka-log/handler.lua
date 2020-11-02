99a8fe7b4e809d5a00b18762c55d52eba6073c38
Raimon Grau <raimonster@gmail.com>
2020-11-02 12:17:13 +0000

    chore(kafka-log) add copyright

--- kong/plugins/kafka-log/handler.lua
+++ kong/plugins/kafka-log/handler.lua
@@ -1,3 +1,10 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+
 local basic_serializer = require "kong.plugins.log-serializers.basic"
 local producers = require "kong.plugins.kafka-log.producers"
 local cjson_encode = require("cjson").encode
