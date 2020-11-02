ed075e3d8ee37e9af9c913c0d46ef0b9926e9332
Raimon Grau <raimonster@gmail.com>
2020-11-02 12:17:14 +0000

    chore(request-validator) add copyright

--- kong/plugins/request-validator/kong/metaschema.lua
+++ kong/plugins/request-validator/kong/metaschema.lua
@@ -1,3 +1,10 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+
 --- THIS MODULE WAS IMPORTED FROM KONG
 -- Contains a fix not currently in Kong EE's version:
 -- https://github.com/Kong/kong/commit/a2ed1da2c549d6c0131edab4e59b3486a8032784
