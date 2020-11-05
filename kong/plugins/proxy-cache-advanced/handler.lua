dc7d351634327136b4fc9357758f31e8ab79109b
eskerda <eskerda@gmail.com>
2020-11-05 17:43:14 +0100

    chore(*) add copyright

--- kong/plugins/proxy-cache-advanced/handler.lua
+++ kong/plugins/proxy-cache-advanced/handler.lua
@@ -1,3 +1,10 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+
 local require     = require
 local strategies  = require "kong.plugins.proxy-cache-advanced.strategies"
 local cache_key   = require "kong.plugins.proxy-cache-advanced.cache_key"
