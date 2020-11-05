e58d6870d4267ff5f4c81d890fe40ca39ab731fa
eskerda <eskerda@gmail.com>
2020-11-05 18:09:21 +0100

    chore(canary) add copyright

--- kong/plugins/canary/groups.lua
+++ kong/plugins/canary/groups.lua
@@ -1,3 +1,10 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+
 -- This file is a copy of the extracted file from PR https://github.com/Kong/kong/pull/3486
 -- so once that code lands somewhere, this file should be removed and replaced by
 -- calls to that relocated logic.
