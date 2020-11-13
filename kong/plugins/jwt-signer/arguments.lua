a6f7cc17a23a03d75b3925e997fca3b20b521d7b
Raimon Grau <raimonster@gmail.com>
2020-11-13 08:27:26 +0000

    chore(jwt-signer) add copyright (#10)

--- kong/plugins/jwt-signer/arguments.lua
+++ kong/plugins/jwt-signer/arguments.lua
@@ -1,3 +1,10 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+
 local sub           = string.sub
 local ngx           = ngx
 local get_headers   = ngx.req.get_headers
