3bb7f401dd1eb3786c3ed99bf3ba128bf07c4a30
Thijs Schreijer <thijs@thijsschreijer.nl>
2020-11-27 15:27:01 +0100

    chore(test) apply logfile assertions

--- spec/02-integration/05-proxy/06-ssl_spec.lua
+++ spec/02-integration/05-proxy/06-ssl_spec.lua
@@ -218,8 +218,7 @@ for _, strategy in helpers.each_strategy() do
     describe("proxy ssl verify", function()
       it("prevents requests to upstream that does not possess a trusted certificate", function()
         -- setup: cleanup logs
-        local test_error_log_path = helpers.test_conf.nginx_err_logs
-        os.execute(":> " .. test_error_log_path)
+        os.execute(":> " .. helpers.test_conf.nginx_err_logs)
 
         local res = assert(proxy_client:send {
           method  = "GET",
@@ -230,32 +229,9 @@ for _, strategy in helpers.each_strategy() do
         })
         local body = assert.res_status(502, res)
         assert.equal("An invalid response was received from the upstream server", body)
-
-        local pl_file = require("pl.file")
-
-        helpers.wait_until(function()
-          -- Assertion: there should be [error] resulting from
-          -- TLS handshake failure
-
-          local logs = pl_file.read(test_error_log_path)
-          local found = false
-
-          for line in logs:gmatch("[^\r\n]+") do
-            if line:find("upstream SSL certificate verify error: " ..
-                         "(20:unable to get local issuer certificate) " ..
-                         "while SSL handshaking to upstream", nil, true)
-            then
-              found = true
-
-            else
-              assert.not_match("[error]", line, nil, true)
-            end
-          end
-
-          if found then
-              return true
-          end
-        end, 2)
+        assert.logfile().has.line("upstream SSL certificate verify error: " ..
+                                  "(20:unable to get local issuer certificate) " ..
+                                  "while SSL handshaking to upstream", true, 2)
       end)
 
       it("trusted certificate, request goes through", function()
