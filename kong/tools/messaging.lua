1a19d5c96d6be3c52d5839965a356e6380c52bf4
Wangchong Zhou <fffonion@gmail.com>
2020-12-22 12:29:49 +0800

    fix(messaging) start the websocket client in http subsystem only (#2118)

--- kong/tools/messaging.lua
+++ kong/tools/messaging.lua
@@ -8,6 +8,7 @@
 local clustering = require "kong.clustering"
 
 local utils = require("kong.tools.utils")
+local subsystem = ngx.config.subsystem
 local msgpack = require "MessagePack"
 local mp_pack = msgpack.pack
 local mp_unpack = msgpack.unpack
@@ -210,7 +211,7 @@ function _M:register_for_messages()
 end
 
 function _M:start_client(server_name)
-  if ngx.worker.id() == 0 then
+  if ngx.worker.id() == 0 and subsystem == "http" then
     start_ws_client(self, server_name)
 
     assert(ngx.timer.at(self.buffer_ttl, flush_cp, self))
