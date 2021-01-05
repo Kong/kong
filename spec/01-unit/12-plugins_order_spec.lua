c05780b87b814a64874ff6b44057f6f81e3403c8
eskerda <eskerda@gmail.com>
2021-01-05 20:05:01 +0100

    Revert "chore(*) unbundle serverless plugin"
    This reverts commit 458f9f6cf647cae626a75e95988901f564b91e78.

--- spec/01-unit/12-plugins_order_spec.lua
+++ spec/01-unit/12-plugins_order_spec.lua
@@ -52,8 +52,7 @@ describe("Plugins", function()
     -- backwards-compatibility
 
     local order = {
-      -- XXX Bundled with KD
-      -- "pre-function",
+      "pre-function",
       "correlation-id",
       "zipkin",
       "bot-detection",
@@ -86,8 +85,7 @@ describe("Plugins", function()
       "loggly",
       "syslog",
       "request-termination",
-      -- XXX Bundled with KD
-      -- "post-function",
+      "post-function",
     }
 
     table.sort(plugins, function(a, b)
