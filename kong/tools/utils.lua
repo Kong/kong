9191fe3b885f851290d70b87f85f09c1f489e7fe
Enrique García Cota <kikito@gmail.com>
2021-03-09 12:12:45 +0100

    refactor(utils) abstract utils.topological_sort
    Extracts the "generic" topological sort to utils so it can be used to
    sort anything, not just schemas.

--- kong/tools/utils.lua
+++ kong/tools/utils.lua
@@ -1354,4 +1354,73 @@ end
 _M.get_mime_type = get_mime_type
 _M.get_error_template = get_error_template
 
+
+local topological_sort do
+
+  local function visit(current, neighbors_map, visited, marked, sorted)
+    if visited[current] then
+      return true
+    end
+
+    if marked[current] then
+      return nil, "Cycle detected, cannot sort topologically"
+    end
+
+    marked[current] = true
+
+    local schemas_pointing_to_current = neighbors_map[current]
+    if schemas_pointing_to_current then
+      local neighbor, ok, err
+      for i = 1, #schemas_pointing_to_current do
+        neighbor = schemas_pointing_to_current[i]
+        ok, err = visit(neighbor, neighbors_map, visited, marked, sorted)
+        if not ok then
+          return nil, err
+        end
+      end
+    end
+
+    marked[current] = false
+
+    visited[current] = true
+
+    insert(sorted, 1, current)
+
+    return true
+  end
+
+  topological_sort = function(items, get_neighbors)
+    local neighbors_map = {}
+    local source, destination
+    local neighbors
+    for i = 1, #items do
+      source = items[i] -- services
+      neighbors = get_neighbors(source)
+      for j = 1, #neighbors do
+        destination = neighbors[j] --routes
+        neighbors_map[destination] = neighbors_map[destination] or {}
+        insert(neighbors_map[destination], source)
+      end
+    end
+
+    local sorted = {}
+    local visited = {}
+    local marked = {}
+
+    local current, ok, err
+    for i = 1, #items do
+      current = items[i]
+      if not visited[current] and not marked[current] then
+        ok, err = visit(current, neighbors_map, visited, marked, sorted)
+        if not ok then
+          return nil, err
+        end
+      end
+    end
+
+    return sorted
+  end
+end
+_M.topological_sort = topological_sort
+
 return _M
