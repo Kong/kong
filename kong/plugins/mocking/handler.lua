4f02cbf5732d109d9f5b85ac59cf6caff2b7c05f
Alan Boudreault <alan@kovaro.ca>
2021-05-18 08:30:36 -0400

    chore(mocking) bump to 0.2.1

--- kong/plugins/mocking/handler.lua
+++ kong/plugins/mocking/handler.lua
@@ -5,7 +5,7 @@ local match       = string.match
 local ngx         = ngx
 local random      = math.random
 local plugin = {
-  VERSION  = "0.2.0",
+  VERSION  = "0.2.1",
   -- Mocking plugin should execute after all other plugins
   PRIORITY = -1,
 }
