7cda0379f1cfda5c68bb89be64edd4b6c528b7db
Murillo Paula <murillo@murillopaula.com>
2021-06-21 16:21:12 -0300

    tests(helpers) add forced_port to proxy_client (#7459)

--- spec/helpers.lua
+++ spec/helpers.lua
@@ -805,11 +805,13 @@ end
 --- returns a pre-configured `http_client` for the Kong proxy port.
 -- @function proxy_client
 -- @param timeout (optional, number) the timeout to use
-local function proxy_client(timeout)
+-- @param forced_port (optional, number) if provided will override the port in
+-- the Kong configuration with this port
+local function proxy_client(timeout, forced_port)
   local proxy_ip = get_proxy_ip(false)
   local proxy_port = get_proxy_port(false)
   assert(proxy_ip, "No http-proxy found in the configuration")
-  return http_client(proxy_ip, proxy_port, timeout or 60000)
+  return http_client(proxy_ip, forced_port or proxy_port, timeout or 60000)
 end
 
 
