705513d0f66e30680d40520d5fbfb242b6836c77
Falon Darville <falon.darville@konghq.com>
2021-06-22 14:33:08 -0700

    docs(admin-api) add note about host as case sensitive (#7478)
    DOCU-1562

--- autodoc/admin-api/data/admin-api.lua
+++ autodoc/admin-api/data/admin-api.lua
@@ -652,7 +652,7 @@ return {
           ]]
         },
         host = {
-          description = [[The host of the upstream server.]],
+          description = [[The host of the upstream server. Note that the host value is case sensitive.]],
           example = "example.com",
         },
         port = {
@@ -830,7 +830,7 @@ return {
         hosts = {
           kind = "semi-optional",
           description = [[
-            A list of domain names that match this Route.
+            A list of domain names that match this Route. Note that the hosts value is case sensitive.
           ]],
           examples = { {"example.com", "foo.test"}, nil },
           skip_in_example = true, -- hack so we get HTTP fields in the first example and Stream fields in the second
