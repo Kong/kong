6d25aa30d599276505145d5026c2dd73f9454e7e
James Hurst <james@reel.audio>
2021-07-22 16:08:37 +0100

    feat(jq) tests and implementation for response filter

--- kong/plugins/jq-filter/handler.lua
+++ kong/plugins/jq-filter/handler.lua
@@ -32,6 +32,18 @@ local function is_media_type_allowed(content_type, filter_conf)
 end
 
 
+local function is_status_code_allowed(status_code, filter_conf)
+  local status_codes = filter_conf.if_status_code
+  for _, code in ipairs(status_codes) do
+    if status_code == code then
+      return true
+    end
+  end
+
+  return false
+end
+
+
 function JqFilter:access(conf)
   local request_body = kong.request.get_raw_body()
   if not request_body then
@@ -62,7 +74,7 @@ function JqFilter:access(conf)
       local headers = cjson_decode(res)
 
       if type(headers) == "table" then
-        for name, value in pairs(headers or {}) do
+        for name, value in pairs(headers) do
           if type(name) == "string" and type(value) == "string" then
             new_headers[name] = value
           end
@@ -75,6 +87,54 @@ function JqFilter:access(conf)
 
   kong.service.request.set_headers(new_headers)
   kong.service.request.set_raw_body(request_body)
+end
+
+
+function JqFilter:response(conf)
+  local response_body = kong.service.response.get_raw_body()
+  if not response_body then
+    return
+  end
+
+  local new_headers = {}
+
+  local response_status = kong.response.get_status()
+  local response_content_type = kong.response.get_header("Content-Type")
+
+  for _, filter in ipairs(conf.filters) do
+    if filter.context ~= "response" or
+      not is_media_type_allowed(response_content_type, filter) or
+      not is_status_code_allowed(response_status, filter) then
+
+      goto next
+    end
+
+    local res = CACHE(
+      filter.program,
+      response_body,
+      filter.jq_options
+    )
+
+    if filter.target == "body" then
+      response_body = res
+
+    elseif filter.target == "headers" then
+      local headers = cjson_decode(res)
+
+      if type(headers) == "table" then
+        for name, value in pairs(headers) do
+          if type(name) == "string" and type(value) == "string" then
+            new_headers[name] = value
+          end
+        end
+      end
+    end
+
+    ::next::
+  end
+
+  kong.response.set_headers(new_headers)
+  return kong.response.exit(response_status, response_body)
 end
 
 
