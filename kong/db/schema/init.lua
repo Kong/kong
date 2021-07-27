b1fbc1a48143c395169e0e63e9839f092a345e91
Tyler Ball <tyler.ball@konghq.com>
2021-07-27 11:33:44 -0700

    fix(schema) Remove duplicates from constraints list
    It appears that Schema.new is called multiple times for each schema.
    This behavior was populating the 'constraints' list with duplicate
    entries, making it difficult to handle cascade delete constraints in the
    dao layer. I added set logic to remove these duplicates.
    
    In the future it would probably make more sense to instead to not
    initialize he schema entities multiple times. However, there does need
    to be a 2 phase pass on all the schemas to populate the constraints
    because they are an inverse relationship.
    
    Signed-off-by: Tyler Ball <tyler.ball@konghq.com>

--- kong/db/schema/init.lua
+++ kong/db/schema/init.lua
@@ -2017,7 +2017,11 @@ function Schema:get_constraints()
     return _workspaceable
   end
 
-  return _cache[self.name].constraints
+  local constraints = {}
+  for _, c in pairs(_cache[self.name].constraints) do
+    table.insert(constraints, c)
+  end
+  return constraints
 end
 
 
@@ -2148,11 +2152,12 @@ function Schema.new(definition, is_subschema)
       if not is_subschema then
         -- Store the inverse relation for implementing constraints
         local constraints = assert(_cache[field.reference]).constraints
-        table.insert(constraints, {
+        -- Set logic to prevent duplicates when Schema is initialized multiple times
+        constraints[self.name] = {
           schema     = self,
           field_name = key,
           on_delete  = field.on_delete,
-        })
+        }
       end
     end
   end
