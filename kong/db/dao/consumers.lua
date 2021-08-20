71def16ad823b6dcec1051e7af9038b435fe628b
Mierenga <mike.swierenga@konghq.com>
2021-08-20 09:48:52 -0400

    feat(consumers) add support for strategies besides postgres

--- kong/db/dao/consumers.lua
+++ kong/db/dao/consumers.lua
@@ -5,6 +5,10 @@
 -- at https://konghq.com/enterprisesoftwarelicense/.
 -- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
 
+local utils     = require "kong.tools.utils"
+
+local fmt = string.format
+
 local Consumers = {}
 
 
@@ -44,20 +48,110 @@ function Consumers:page_by_type(_, size, offset, options)
   return rows, nil, nil, next_offset
 end
 
-function Consumers:select_by_username_ignore_case(subject)
-  local consumers, err = kong.db.connector:query(table.concat({
-    "SELECT * FROM consumers WHERE LOWER(username) = LOWER(",
-    kong.db.connector:escape_literal(subject),
-    ") LIMIT 1",
-  }, ""))
+function Consumers:select_by_username_ignore_case(username)
+  local function log_multiple_matches(matches)
+    local match_info = {}
+
+    for i,match in pairs(matches) do
+      table.insert(match_info, fmt("%s (id: %s)", match.username, match.id))
+    end
+    kong.log.notice(fmt("multiple consumers match '%s' by username case-insensitively: %s", username, table.concat(match_info, ", ")))
+  end
+
+  local function upperChar(str, i)
+    if i == 0 then
+      return str
+    end
+    return str:sub(0,i-1) .. str:sub(i,i):upper() .. str:sub(i+1)
+  end
+
+  local function postgres_query()
+    local qs = fmt("SELECT * FROM consumers WHERE LOWER(username) = LOWER('%s');", username)
+    return kong.db.connector:query(qs)
+  end
+
+  local function permutation_query()
+    local permutations = {}
+
+    table.insert(permutations, username)
+
+    local split_domain_char = "@"
+    local local_part, domain = table.unpack(utils.split(username, split_domain_char))
+    if not domain then
+      split_domain_char = ""
+    end
+
+    -- gruceo.kong@kong.com
+    local lower_all = table.concat({local_part:lower(), domain}, split_domain_char)
+    -- GRUCEO.KONG@kong.com
+    local upper_all = table.concat({local_part:upper(), domain}, split_domain_char)
+    -- Gruceo.kong@kong.com
+    local upper_first_lower_rest = table.concat({upperChar(local_part:lower(), 1), domain}, split_domain_char)
+    local upper_first_keep_rest = table.concat({upperChar(local_part, 1), domain}, split_domain_char)
+
+    if username ~= lower_all then
+      table.insert(permutations, lower_all)
+    end
+
+    if username ~= upper_all then
+      table.insert(permutations, upper_all)
+    end
+
+    if username ~= upper_first_keep_rest then
+      table.insert(permutations, upper_first_keep_rest)
+    end
+
+    if upper_first_keep_rest ~= upper_first_lower_rest then
+      table.insert(permutations, upper_first_lower_rest)
+    end
+
+    -- make variants with each subpart capitalized (split by the following chars)
+    -- Gruceo.Kong@kong.com
+    local split_chars = {".", "-", "_"}
+    for i,char in pairs(split_chars) do
+      local local_subparts = utils.split(local_part, char)
+      if #local_subparts > 1 then
+        local capitalized_subparts = {}
+        for j,subpart in pairs(local_subparts) do
+          table.insert(capitalized_subparts, upperChar(subpart, 1))
+        end
+        table.insert(permutations, table.concat({table.concat(capitalized_subparts, char), domain}, split_domain_char))
+      end
+    end
+
+    local consumers = {}
+    local consumer, err
+    for _,permutation in pairs(permutations) do
+      consumer, err = kong.db.consumers:select_by_username(permutation)
+      if consumer then
+        table.insert(consumers, consumer)
+      end
+    end
+
+    table.sort(consumers, function(a,b)
+      return a.created_at < b.created_at
+    end)
+
+    return consumers, err
+  end
+
+  local consumers, err
+
+  if kong.db.strategy == "postgres" then
+    consumers, err = postgres_query()
+  else
+    consumers, err = permutation_query()
+  end
 
   if err then
     return nil, err
   end
 
-  local case_insensitive_match = consumers[1]
+  if #consumers > 1 then
+    log_multiple_matches(consumers)
+  end
 
-  return case_insensitive_match, nil
+  return consumers[1], nil
 end
 
 
