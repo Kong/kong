75c8f74fa27d4f7501a9723a1e704f8605732ee9
James Hurst <james@reel.audio>
2021-08-24 17:07:11 +0100

    refactor(jq) remove dynamic config and headers target

--- kong/plugins/jq/schema.lua
+++ kong/plugins/jq/schema.lua
@@ -8,24 +8,20 @@ return {
       config = {
         type = "record",
         fields = {
-          {
-            filters = {
-              required = true,
-              type = "array",
-              elements = {
-                required = true,
-                type = "record",
-                fields = {
-                  typedefs.context,
-                  typedefs.target,
-                  typedefs.program,
-                  typedefs.jq_options,
-                  typedefs.if_media_type,
-                  typedefs.if_status_code,
-                },
-              },
-            },
-          },
+          { request_jq_program = typedefs.program },
+          { request_jq_program_options = typedefs.jq_options },
+          { request_if_media_type = typedefs.if_media_type },
+
+          { response_jq_program = typedefs.program },
+          { response_jq_program_options = typedefs.jq_options },
+          { response_if_media_type = typedefs.if_media_type },
+          { response_if_status_code = typedefs.if_status_code },
+        },
+        entity_checks = {
+          { at_least_one_of = {
+            "request_jq_program",
+            "response_jq_program"
+          } },
         },
       },
     },
--- kong/plugins/jq/typedefs.lua
+++ kong/plugins/jq/typedefs.lua
@@ -4,94 +4,71 @@ return {
   protocols = {
     protocols = typedefs.protocols_http
   },
-  context = {
-    context = {
-      type = "string",
-      default = "response",
-      one_of = {
-        "request",
-        "response",
-      },
-    },
-  },
   program = {
-    program = {
-      required = true,
-      type = "string",
-    },
-  },
-  target = {
-    target = {
-      type = "string",
-      default = "body",
-      one_of = {
-        "body",
-        "headers",
-      },
-    },
+    required = false,
+    type = "string",
   },
   jq_options = {
-    jq_options = {
-      required = false,
-      type = "record",
-      default = {},
-      fields = {
-        {
-          compact_output = {
-            type = "boolean",
-            default = true,
-          },
+    required = false,
+    type = "record",
+    default = {},
+    fields = {
+      {
+        compact_output = {
+          required = true,
+          type = "boolean",
+          default = true,
         },
-        {
-          raw_output = {
-            type = "boolean",
-            default = false,
-          },
+      },
+      {
+        raw_output = {
+          required = true,
+          type = "boolean",
+          default = false,
         },
-        {
-          join_output = {
-            type = "boolean",
-            default = false,
-          },
+      },
+      {
+        join_output = {
+          required = true,
+          type = "boolean",
+          default = false,
         },
-        {
-          ascii_output = {
-            type = "boolean",
-            default = false,
-          },
+      },
+      {
+        ascii_output = {
+          required = true,
+          type = "boolean",
+          default = false,
         },
-        {
-          sort_keys = {
-            type = "boolean",
-            default = false,
-          },
+      },
+      {
+        sort_keys = {
+          required = true,
+          type = "boolean",
+          default = false,
         },
       },
     },
   },
   if_media_type = {
-    if_media_type = {
-      required = false,
-      type = "array",
-      elements = {
-        type = "string",
-      },
-      default = { "application/json" },
+    required = false,
+    type = "array",
+    elements = {
+      type = "string",
     },
+    default = { "application/json" },
   },
   if_status_code = {
-    if_status_code = {
-      required = false,
-      type = "array",
-      elements = {
-        type = "integer",
-        between = {
-          100,
-          599
-        },
+    required = false,
+    type = "array",
+    elements = {
+      type = "integer",
+      between = {
+        100,
+        599
       },
-      default = { 200 },
     },
+    default = { 200 },
   },
 }
 
