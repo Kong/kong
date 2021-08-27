940b1a3a24b8a5eb10a89e19485c4109c4178c65
Mierenga <mike.swierenga@konghq.com>
2021-08-27 18:54:42 -0400

    feat(admins) strategy-specific select_by_username_ignore_case()

---
+++ kong/db/strategies/off/admins.lua
@@ -0,0 +1,16 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+
+local Admins = {}
+
+
+function Admins:select_by_username_ignore_case(username)
+  -- selecting case-insensitively is not supported for the off strategy
+  return nil, nil
+end
+
+return Admins
