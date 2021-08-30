7ab4762f545dbe04ad24d42c5b284b84a55a58a2
mikefero <michael.fero@konghq.com>
2021-08-30 10:48:23 -0400

    chore(mocking) adding copyright headers

--- kong/plugins/mocking/schema.lua
+++ kong/plugins/mocking/schema.lua
@@ -1,3 +1,10 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+
 -- see: https://konghq.atlassian.net/browse/FT-1982
 -- local function validate_specification(given_value, given_config)
 --   -- TODO: how to enable it in current plugin development stack?
