40d29c63ee2751553195ec94fe00b9e271952883
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2021-09-21 21:27:11 +0300

    refactor(response-transformer) use kong.response.get_raw_body and kong.response.set_raw_body
    ### Summary
    
    Just refactors `proxy-cache` plugin to use the two new PDK functions.

--- kong/plugins/response-transformer/handler.lua
+++ kong/plugins/response-transformer/handler.lua
@@ -4,14 +4,12 @@ local header_transformer = require "kong.plugins.response-transformer.header_tra
 
 local is_body_transform_set = header_transformer.is_body_transform_set
 local is_json_body = header_transformer.is_json_body
-local concat = table.concat
 local kong = kong
-local ngx = ngx
 
 
 local ResponseTransformerHandler = {
   PRIORITY = 800,
-  VERSION = "2.0.1",
+  VERSION = "2.0.2",
 }
 
 
@@ -21,23 +19,15 @@ end
 
 
 function ResponseTransformerHandler:body_filter(conf)
-  if is_body_transform_set(conf) and is_json_body(kong.response.get_header("Content-Type")) then
-    local ctx = ngx.ctx
-    local chunk, eof = ngx.arg[1], ngx.arg[2]
+  if not is_body_transform_set(conf)
+    or not is_json_body(kong.response.get_header("Content-Type"))
+  then
+    return
+  end
 
-    ctx.rt_body_chunks = ctx.rt_body_chunks or {}
-    ctx.rt_body_chunk_number = ctx.rt_body_chunk_number or 1
-
-    if eof then
-      local chunks = concat(ctx.rt_body_chunks)
-      local body = body_transformer.transform_json_body(conf, chunks)
-      ngx.arg[1] = body or chunks
-
-    else
-      ctx.rt_body_chunks[ctx.rt_body_chunk_number] = chunk
-      ctx.rt_body_chunk_number = ctx.rt_body_chunk_number + 1
-      ngx.arg[1] = nil
-    end
+  local body = kong.response.get_raw_body()
+  if body then
+    return kong.response.set_raw_body(body_transformer.transform_json_body(conf, body))
   end
 end
 
