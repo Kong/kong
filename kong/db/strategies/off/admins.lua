0e8976d2094482846cb95000fbf891af6e862bff
Mierenga <mike.swierenga@konghq.com>
2021-09-27 10:54:47 -0400

    feat(consumers) implement select_by_username_ignore_case for off strategy

--- kong/db/strategies/off/admins.lua
+++ kong/db/strategies/off/admins.lua
@@ -9,8 +9,25 @@ local Admins = {}
 
 
 function Admins:select_by_username_ignore_case(username)
-  -- selecting case-insensitively is not supported for the off strategy
-  return nil, nil
+  local PAGE_SIZE = 100
+  local next_offset = nil
+  local rows, err
+  local matches = {}
+
+  repeat
+    rows, err, next_offset = self:page(PAGE_SIZE, next_offset)
+    if err then
+      return nil, err
+    end
+    for _, row in ipairs(rows) do
+      if type(row.username) == 'string' and row.username:lower() == username:lower() then
+        table.insert(matches, row)
+      end
+    end
+
+  until next_offset == nil
+
+  return matches, nil
 end
 
 return Admins
