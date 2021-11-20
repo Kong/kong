d3f0dda43074c3b760dd2e835ffb0f5ebcdb0caa
Sergey Bondari <sergey.bondari@konghq.com>
2021-11-19 18:45:51 -0800

    feat(conf) add dns_cache_size configuration parameter support

--- kong/tools/dns.lua
+++ kong/tools/dns.lua
@@ -31,6 +31,7 @@ local setup_client = function(conf)
     badTtl = conf.dns_error_ttl,       -- ttl in seconds for dns error responses (except 3 - name error)
     emptyTtl = conf.dns_not_found_ttl, -- ttl in seconds for empty and "(3) name error" dns responses
     staleTtl = conf.dns_stale_ttl,     -- ttl in seconds for records once they become stale
+    cacheSize = conf.dns_cache_size,   -- maximum number of records cached in memory
     order = conf.dns_order,            -- order of trying record types
     noSynchronisation = conf.dns_no_sync,
   }
