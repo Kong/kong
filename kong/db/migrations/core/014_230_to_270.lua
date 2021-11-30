3f77c94d0adc69cfc9c2e1fdb977f5241e5e1447
Henri Pietila <HenriPro@users.noreply.github.com>
2021-11-29 16:17:35 -0800

    refactor(services) remove required in favor of C* teardown
    Co-authored-by: Fero <6863207+mikefero@users.noreply.github.com>

--- kong/db/migrations/core/014_230_to_270.lua
+++ kong/db/migrations/core/014_230_to_270.lua
@@ -14,7 +14,32 @@ return {
     cassandra = {
       up = [[
         ALTER TABLE services ADD enabled boolean;
-      ]]
+      ]],
+      teardown = function(connector)
+        local coordinator = assert(connector:get_stored_connection())
+        local cassandra = require "cassandra"
+
+        for rows, err in coordinator:iterate("SELECT partition, id, enabled FROM services") do
+          if err then
+            return nil, err
+          end
+
+          for _, row in ipairs(rows) do
+            if not row.enabled then
+              local _, err = coordinator:execute("UPDATE services SET enabled = ? WHERE partition = ? AND id = ?", {
+                cassandra.boolean(true),
+                cassandra.text(row.partition),
+                cassandra.uuid(row.id),
+              })
+              if err then
+                return nil, err
+              end
+            end
+          end
+        end
+
+        return true
+      end,
     },
   }
   
