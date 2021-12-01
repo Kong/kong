a20b9d1836edc6b503e142cedd955df6d9b6bcd6
Yang Yi <yang.yi@konghq.com>
2021-11-30 17:55:30 -0800

    fix merge conflict issue

--- kong/enterprise_edition/db/migrations/enterprise/012_2600_to_2700.lua
+++ kong/enterprise_edition/db/migrations/enterprise/012_2600_to_2700.lua
@@ -5,14 +5,23 @@
 -- at https://konghq.com/enterprisesoftwarelicense/.
 -- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
 
-local operations_2600_2700 = require "kong.enterprise_edition.db.migrations.operations.2600_to_2700"
+local operations = require "kong.enterprise_edition.db.migrations.operations.1500_to_2100"
+local log        = require "kong.cmd.utils.log"
+local constants  = require "kong.constants"
+
+local cassandra_table_is_partitioned = operations.utils.cassandra_table_is_partitioned
+local render = operations.utils.render
+
+local ADMIN_CONSUMER_USERNAME_SUFFIX = constants.ADMIN_CONSUMER_USERNAME_SUFFIX
+
+local function string_ends(str, suffix)
+  return str:sub(-#(suffix)) == suffix
+end
 
 return {
   postgres = {
     up = [[
-      DO $$
-      BEGIN
-      UPDATE consumers
+      UPDATE consumers 
       SET
         username = CONCAT(username, '_ADMIN_'),
         username_lower = CONCAT(username_lower, '_admin_')
@@ -20,54 +29,58 @@ return {
         username !~ '_ADMIN_$'
       AND
         type = 2;
-      END;
-      $$;
-
-      DO $$
-      BEGIN
-        ALTER TABLE IF EXISTS ONLY "license_data" ADD "year" smallint;
-        ALTER TABLE IF EXISTS ONLY "license_data" ADD "month" smallint;
-      EXCEPTION WHEN DUPLICATE_COLUMN THEN
-        -- Do nothing, accept existing state
-      END;
-      $$;
-
-      DO $$
-      BEGIN
-        UPDATE "license_data" SET "year" = 0 WHERE "year" IS NULL;
-        UPDATE "license_data" SET "month" = 0 WHERE "month" IS NULL;
-        ALTER TABLE "license_data" ALTER COLUMN "year" SET NOT NULL;
-        ALTER TABLE "license_data" ALTER COLUMN "month" SET NOT NULL;
-        DROP INDEX IF EXISTS license_data_key_idx;
-        ALTER TABLE license_data DROP CONSTRAINT IF EXISTS license_data_pkey;
-        CREATE UNIQUE INDEX IF NOT EXISTS license_data_key_idx ON license_data(node_id, year, month);
-      END
-      $$;
-
-    ]]
+    ]], 
   },
 
   cassandra = {
     up = [[
-      /* Add temporary table for license data table */
-      CREATE TABLE IF NOT EXISTS license_data_tmp (
-        node_id                 uuid,
-        license_creation_date   timestamp,
-        req_cnt                 counter,
-        PRIMARY KEY (node_id, license_creation_date)
-      );
     ]],
-    teardown = function(connector)
-      local _, err = operations_2600_2700.cassandra_migrate_consumers(connector)
-      if err then
-        return nil, err
+    teardown = function (connector)
+      local coordinator = connector:connect_migrations()
+
+      -- find out all admin related consumers
+      local cql = render([[
+        SELECT id, username, username_lower FROM $(KEYSPACE).consumers
+        WHERE type = 2 ALLOW FILTERING $(PARTITION);
+      ]], {
+        KEYSPACE = connector.keyspace,
+        PARTITION = cassandra_table_is_partitioned(connector, "consumers")
+                      and [[ AND partition = 'consumers']]
+                      or "",
+      })
+
+      for rows, err in coordinator:iterate(cql) do
+
+        if err then
+          return log.debug(err)
+        end
+
+        for _, row in ipairs(rows) do
+
+          if row.username and not string_ends(row.username, ADMIN_CONSUMER_USERNAME_SUFFIX) then
+            local _, err = connector:query(render([[
+              UPDATE $(KEYSPACE).consumers
+              SET	username='$(USERNAME)', username_lower='$(USERNAME_LOWER)'
+              WHERE id=$(ID) $(PARTITION);
+            ]], {
+              KEYSPACE = connector.keyspace,
+              ID = row.id,
+              USERNAME = row.username .. ADMIN_CONSUMER_USERNAME_SUFFIX,
+              USERNAME_LOWER = (row.username_lower or "") .. ADMIN_CONSUMER_USERNAME_SUFFIX:lower(),
+              PARTITION = cassandra_table_is_partitioned(connector, "consumers")
+                        and [[ AND partition = 'consumers']]
+                        or "",
+            }))
+
+            if err then
+              return log.debug(err)
+            end
+          end
+
+        end
+
       end
 
-      local _, err = operations_2600_2700.cassandra_migrate_license_data(connector)
-      if err then
-        return nil, err
-      end
-
-    end,
-  },
+    end
+  }
 }
