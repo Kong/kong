b5f59ff742fb583228982ab262efa82fa6c41522
Yang Yi <yang.yi@konghq.com>
2021-12-01 20:41:27 -0800

    (fix) add cassandra secondary index for cache_key

--- kong/enterprise_edition/db/migrations/enterprise/012_2600_to_2700_1.lua
+++ kong/enterprise_edition/db/migrations/enterprise/012_2600_to_2700_1.lua
@@ -140,6 +140,7 @@ return {
 
         CREATE INDEX IF NOT EXISTS consumer_group_plugins_group_id_idx ON consumer_group_plugins(consumer_group_id);
         CREATE INDEX IF NOT EXISTS consumer_group_plugins_plugin_name_idx ON consumer_group_plugins(name);
+        CREATE INDEX IF NOT EXISTS consumer_group_plugins_cache_key_idx ON consumer_group_plugins(cache_key)
       ]] .. ws_migration_up(operations.cassandra.up),
       teardown = ws_migration_teardown(operations.cassandra.teardown),
     },
