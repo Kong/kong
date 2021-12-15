87354842394b9e74a0f7847584e7c5ea3d28d3d6
zhangshuaiNB <52234994+zhangshuaiNB@users.noreply.github.com>
2021-12-15 17:51:33 +0800

    fix: Support TLS SNI custom dynamic configuration

--- kong/templates/nginx_kong_stream.lua
+++ kong/templates/nginx_kong_stream.lua
@@ -121,10 +121,12 @@ server {
         Kong.ssl_certificate()
     }
 > end
-
+    
+    set $tls_sni_name 'kong_upstream';
     preread_by_lua_block {
         Kong.preread()
     }
+    proxy_ssl_name $tls_sni_name;
 
     proxy_ssl on;
     proxy_ssl_server_name on;
