07771c3d02e021e3c6a215e71cf58d5e18459f3f
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2022-02-10 03:10:58 +0200

    feat(db) make certificate entities keys and certs referenceable
    ### Summary
    
    Makes certificate entities `key`, `key_alt`, `cert` and `cert_alt`
    referenceable which means that vault references can be stored on
    those fields.

---
+++ spec/02-integration/13-vaults/01-vault_spec.lua
@@ -0,0 +1,64 @@
+local ssl_fixtures = require "spec.fixtures.ssl"
+local helpers = require "spec.helpers"
+local cjson = require "cjson"
+
+
+for _, strategy in helpers.each_strategy() do
+  describe("/certificates with DB: #" .. strategy, function()
+    local client
+
+    lazy_setup(function()
+      helpers.get_db_utils(strategy, {
+        "certificates",
+        "vaults",
+      })
+
+      assert(helpers.start_kong {
+        database = strategy,
+        vaults = "env",
+      })
+
+      client = assert(helpers.admin_client(10000))
+
+      local res = client:put("/vaults/test-vault", {
+        headers = { ["Content-Type"] = "application/json" },
+        body = {
+          name = "env",
+        },
+      })
+
+      assert.res_status(200, res)
+    end)
+
+    after_each(function()
+      if client then
+        client:close()
+      end
+    end)
+
+    lazy_teardown(function()
+      helpers.stop_kong()
+    end)
+
+    it("create certificates with cert and key as secret", function()
+      finally(function()
+        helpers.unsetenv("CERT")
+        helpers.unsetenv("KEY")
+      end)
+      helpers.setenv("CERT", ssl_fixtures.cert)
+      helpers.setenv("KEY", ssl_fixtures.key)
+      local res, err  = client:post("/certificates", {
+        body    = {
+          cert  = "{vault://test-vault/cert}",
+          key   = "{vault://test-vault/key}",
+        },
+        headers = { ["Content-Type"] = "application/json" },
+      })
+      assert.is_nil(err)
+      local body = assert.res_status(201, res)
+      local certificate = cjson.decode(body)
+      assert.not_nil(certificate.key)
+      assert.not_nil(certificate.cert)
+    end)
+  end)
+end
