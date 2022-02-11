15a0683058fe4af655e9f98aa205c3cd4aa203e8
James Hurst <james@reel.audio>
2022-02-11 13:00:08 +0000

    feat(forward-proxy) use certificate helpers

--- plugins-ee/forward-proxy/kong/plugins/forward-proxy/handler.lua
+++ plugins-ee/forward-proxy/kong/plugins/forward-proxy/handler.lua
@@ -9,8 +9,7 @@ local meta       = require "kong.meta"
 local http       = require "resty.http"
 local ee         = require "kong.enterprise_edition"
 local base64     = require "ngx.base64"
-local ssl        = require "ngx.ssl"
-
+local cert_utils = require "kong.enterprise_edition.cert_utils"
 
 local kong                = kong
 local ngx                 = ngx
@@ -202,26 +201,10 @@ function ForwardProxyHandler:access(conf)
 
   local proxy_uri = "http://"  .. conf.proxy_host .. ":" .. conf.proxy_port .. "/"
 
-  local ssl_client_cert, ssl_client_priv_key
+  local ssl_client_cert, ssl_client_priv_key, err
   if ctx.service.client_certificate then
-    local certificate_id = ctx.service.client_certificate.id
-
-    local db_cert, err = kong.db.certificates:select(
-      { id = certificate_id },
-      { nulls = true }
-    )
-
-    if not db_cert then
-      return kong.response.exit(500, err)
-    end
-
-    ssl_client_cert, err = ssl.parse_pem_cert(db_cert.cert)
-    if not ssl_client_cert then
-      return kong.response.exit(500, err)
-    end
-
-    ssl_client_priv_key, err = ssl.parse_pem_priv_key(db_cert.key)
-    if not ssl_client_priv_key then
+    ssl_client_cert, ssl_client_priv_key, err = cert_utils.load_certificate(ctx.service.client_certificate.id)
+    if not ssl_client_cert or not ssl_client_priv_key then
       return kong.response.exit(500, err)
     end
   end
@@ -232,6 +215,7 @@ function ForwardProxyHandler:access(conf)
     port = addr.port,
     proxy_opts = {
       http_proxy = proxy_uri,
+      https_proxy = proxy_uri,
     },
     ssl_verify = conf.https_verify,
     ssl_server_name = addr.host,
