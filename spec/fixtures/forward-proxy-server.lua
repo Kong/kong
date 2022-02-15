abee33fdd224b8174e3ee96197bc239072f97288
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-02-15 10:29:32 -0800

    tests(aws-lambda) update forward-proxy fixture

--- spec/fixtures/forward-proxy-server.lua
+++ spec/fixtures/forward-proxy-server.lua
@@ -1,116 +1,70 @@
 local _M = {}
 
-local function proxy(role, read, write)
-  coroutine.yield()
+local split = require("kong.tools.utils").split
 
-  while not ngx.worker.exiting() do
-    local data, err = read:receiveany(1024)
 
-    if data then
-      ngx.log(ngx.DEBUG, "got chunk from ", role, ", size: ", #data)
-      assert(write:send(data))
-
-    elseif err ~= "timeout" then
-      ngx.log(ngx.ERR, "got error reading from ", role, ": ", err)
-      break
-    end
-  end
-
-  return role
-end
-
---- poor man's CONNECT forward proxy
---
--- NGINX will throw a 400 at the HTTP CONNECT method, so this has to be
--- implemented in stream land.
+-- This is a very naive forward proxy, which accepts a CONNECT over HTTP, and
+-- then starts tunnelling the bytes blind (for end-to-end SSL).
 function _M.connect()
-  ngx.log(ngx.NOTICE, "incoming forward proxy request")
 
-  local client = assert(ngx.req.socket(true))
-  client:settimeouts(500, 500, 500)
+  local req_sock = ngx.req.socket(true)
+  req_sock:settimeouts(1000, 1000, 1000)
 
-  local reader = client:receiveuntil("\r\n\r\n")
-  local req = assert(reader())
+  -- receive request line
+  local req_line = req_sock:receive()
+  ngx.log(ngx.DEBUG, "request line: ", req_line)
 
-  local method, target, version
-  local headers = {}
-
-  for line in req:gmatch("([^\r\n]+)") do
-    if not method then
-      method, target, version = line:match("^([A-Z]+)%s+([^%s]+)%s+HTTP/(.+)")
-
-      if not method then
-        ngx.log(ngx.WARN, "failed parsing http request: ", req)
-        return ngx.exit(444)
-
-      elseif method ~= "CONNECT" then
-        ngx.log(ngx.WARN, "received invalid method: ", method)
-        return ngx.exit(444)
-      end
-
-    else
-      local header, value = line:match("([^:]+):%s*(.+)")
-      if not header then
-        ngx.log(ngx.WARN, "failed parsing header line: ", line)
-        return ngx.exit(444)
-      end
-
-      headers[header:lower()] = value
-    end
+  local method, host_port, version = unpack(split(req_line, " "))
+  if method ~= "CONNECT" then
+    return ngx.exit(400)
   end
 
-  ngx.log(ngx.ERR, "Got request: ", require("inspect")({
-    method = method,
-    target = target,
-    version = version,
-    headers = headers,
-  }))
+  local upstream_host, upstream_port = unpack(split(host_port, ":"))
 
+  -- receive and discard any headers
+  repeat
+    local line = req_sock:receive("*l")
+    ngx.log(ngx.DEBUG, "request header: ", line)
+  until ngx.re.find(line, "^\\s*$", "jo")
 
-  ngx.log(ngx.INFO, "Connecting to ", target)
-  local upstream = assert(ngx.socket.tcp())
-  upstream:settimeouts(500, 500, 500)
-
-  local host, port = target:match("([^:]+):(.+)")
-
-  local ok, err = upstream:connect(host, port)
+  -- Connect to requested upstream
+  local upstream_sock = ngx.socket.tcp()
+  upstream_sock:settimeouts(1000, 1000, 1000)
+  local ok, err = upstream_sock:connect(upstream_host, upstream_port)
   if not ok then
-    ngx.log(ngx.ERR, "failed connecting to upstream ", target, ": ", err)
-    client:send("HTTP/1.1 502 bad things happened\r\n\r\n")
-    return ngx.exit(444)
+    return ngx.exit(504)
   end
 
-  ngx.log(ngx.INFO, "Connected!")
+  -- Tell the client we are good to go
+  ngx.print("HTTP/1.1 200 OK\n\n")
+  ngx.flush()
 
-  assert(client:send("HTTP/1.1 200 Connection established\r\n\r\n"))
+  -- 10Kb in either direction should be plenty
+  local max_bytes = 10 * 1024
 
-  local ds = ngx.thread.spawn(proxy, "client", client, upstream)
-  local us = ngx.thread.spawn(proxy, "upstream", upstream, client)
+  repeat
+    local req_data = req_sock:receiveany(max_bytes)
+    if req_data then
+      ngx.log(ngx.DEBUG, "client RCV ", #req_data, " bytes")
 
-  local ok, res = ngx.thread.wait(us, ds)
-  if not ok then
-    ngx.log(ngx.ERR, "thread returned error: ", res)
-    ngx.thread.kill(us)
-    ngx.thread.kill(ds)
-
-  else
-    ngx.log(ngx.NOTICE, res, " thread has completed")
-
-    local wait
-    if res == "upstream" then
-      wait = ds
-    else
-      wait = us
+      local bytes, err = upstream_sock:send(req_data)
+      if bytes then
+        ngx.log(ngx.DEBUG, "upstream SND ", bytes, " bytes")
+      end
     end
 
-    ok, res = ngx.thread.wait(wait)
-    if not ok then
-      ngx.log(ngx.ERR, "thread returned error: ", res)
-    end
-  end
+    local res_data = upstream_sock:receiveany(max_bytes)
+    if res_data then
+      ngx.log(ngx.DEBUG, "upstream RCV ", #res_data, " bytes")
 
-  return ngx.exit(0)
+      local bytes, err = req_sock:send(res_data)
+      if bytes then
+        ngx.log(ngx.DEBUG, "client SND: ", bytes, " bytes")
+      end
+    end
+  until not req_data and not res_data -- request socket should be closed
+
+  upstream_sock:close()
 end
-
 
 return _M
