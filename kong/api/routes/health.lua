2da37f028c929829df7c2ca77b70b5a08bda408d
Zachary Hu <zachary.hu@konghq.com>
2022-04-18 15:10:45 +0800

    perf(admin) move ffi.cdef to module level and remove extra local var

--- kong/api/routes/health.lua
+++ kong/api/routes/health.lua
@@ -1,8 +1,9 @@
 local ffi = require("ffi")
-local C = ffi.C
 local utils = require "kong.tools.utils"
 local declarative = require "kong.db.declarative"
 
+local C = ffi.C
+local var = ngx.var
 local tonumber = tonumber
 local kong = kong
 local knode  = (kong and kong.node) and kong.node or
@@ -12,6 +13,23 @@ local knode  = (kong and kong.node) and kong.node or
 local dbless = kong.configuration.database == "off"
 local data_plane_role = kong.configuration.role == "data_plane"
 
+if ffi.arch == "x64" or ffi.arch == "arm64" then
+  ffi.cdef[[
+  uint64_t *ngx_stat_requests;
+  uint64_t *ngx_stat_accepted;
+  uint64_t *ngx_stat_handled;
+  ]]
+
+elseif ffi.arch == "x86" or ffi.arch == "arm" then
+  ffi.cdef[[
+  uint32_t *ngx_stat_requests;
+  uint32_t *ngx_stat_accepted;
+  uint32_t *ngx_stat_handled;
+  ]]
+
+else
+  kong.log.err("Unsupported arch: " .. ffi.arch)
+end
 
 return {
   ["/status"] = {
@@ -38,36 +56,6 @@ return {
       end
 
       -- nginx stats
-
-      if ffi.arch == "x64" or ffi.arch == "arm64" then
-        ffi.cdef[[
-        uint64_t *ngx_stat_requests;
-        uint64_t *ngx_stat_accepted;
-        uint64_t *ngx_stat_handled;
-        uint64_t *ngx_stat_active;
-        uint64_t *ngx_stat_reading;
-        uint64_t *ngx_stat_writing;
-        uint64_t *ngx_stat_waiting;
-        ]]
-      elseif ffi.arch == "x86" or ffi.arch == "arm" then
-        ffi.cdef[[
-        uint32_t *ngx_stat_requests;
-        uint32_t *ngx_stat_accepted;
-        uint32_t *ngx_stat_handled;
-        uint32_t *ngx_stat_active;
-        uint32_t *ngx_stat_reading;
-        uint32_t *ngx_stat_writing;
-        uint32_t *ngx_stat_waiting;
-        ]]
-      else
-        kong.log.err("Unsupported arch: " .. ffi.arch)
-      end
-
-      local accepted = C.ngx_stat_accepted[0]
-      local handled = C.ngx_stat_handled[0]
-      local total = C.ngx_stat_requests[0]
-
-      local var = ngx.var
       local status_response = {
         memory = knode.get_memory_stats(unit, scale),
         server = {
@@ -75,9 +63,9 @@ return {
           connections_reading = tonumber(var.connections_reading),
           connections_writing = tonumber(var.connections_writing),
           connections_waiting = tonumber(var.connections_waiting),
-          connections_accepted = tonumber(accepted),
-          connections_handled = tonumber(handled),
-          total_requests = tonumber(total)
+          connections_accepted = tonumber(C.ngx_stat_accepted[0]),
+          connections_handled = tonumber(C.ngx_stat_handled[0]),
+          total_requests = tonumber(C.ngx_stat_requests[0])
         },
         database = {
           reachable = true,
