93a79ae901e8004df68814a9c2852f0cc48cc529
zhong <zhong.chen@konghq.com>
2022-05-17 10:34:39 -0700

    feat(analytics): update comments

--- kong/analytics/init.lua
+++ kong/analytics/init.lua
@@ -41,7 +41,7 @@ local protoc = require "protoc"
 local p = protoc.new()
 p.include_imports = true
 -- the file is uploaded by the build job
--- https://github.com/Kong/kong-distributions/blob/99d49c5c79399e4b0700ed7f7ea220efcd39c771/kong-images/build-kong.sh#L759
+-- https://github.com/Kong/kong-build-tools/blob/049c65563130d5e8355692dc29943f5595e32006/build-kong.sh#L45
 p:addpath("/usr/local/kong/lib")
 -- path for unit tests
 p:addpath("kong/include")
