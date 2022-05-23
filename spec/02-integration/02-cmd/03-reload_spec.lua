e6cf7249aa134b784be664ae5cd78ffe6fc6c25e
chronolaw <chrono_cpp@me.com>
2022-05-23 14:56:18 +0800

    remove print log

--- spec/02-integration/02-cmd/03-reload_spec.lua
+++ spec/02-integration/02-cmd/03-reload_spec.lua
@@ -14,7 +14,6 @@ local function get_kong_workers()
       path = "/",
     }
     if not res or res.status ~= 200 then
-      os.execute("cat servroot/logs/error.log")
       return false
     end
     local body = assert.res_status(200, res)
