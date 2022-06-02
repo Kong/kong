659636cdb9f269d1f05f25424eaef3016c811518
chronolaw <chrono_cpp@me.com>
2022-06-02 14:09:57 +0800

    fix 03-consistent-hashing_spec.lua

--- spec/02-integration/05-proxy/10-balancer/03-consistent-hashing_spec.lua
+++ spec/02-integration/05-proxy/10-balancer/03-consistent-hashing_spec.lua
@@ -215,11 +215,19 @@ for _, strategy in helpers.each_strategy() do
           bu.end_testcase_setup(strategy, bp)
 
           local client = helpers.proxy_client()
-          local res = assert(client:request({
-            method = "GET",
-            path = uri,
-            headers = { host = api_host },
-          }))
+
+          local res
+          helpers.wait_until(function()
+            res = assert(client:request({
+              method = "GET",
+              path = uri,
+              headers = { host = api_host },
+            }))
+
+            return pcall(function()
+              assert.res_status(200, res)
+            end)
+          end, 5)
 
           -- Go hit them with our test requests
           local oks = bu.client_requests(requests, api_host, nil, nil, nil, uri)
