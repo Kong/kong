8be2308220831217904e3bf91a970a78b4ae2fa9
Alan Boudreault <alan@alanb.ca>
2022-06-03 10:56:45 -0400

    fix(ci) restore the ee matrix

--- distribution/post-install-license-library.sh
+++ distribution/post-install-license-library.sh
@@ -9,17 +9,21 @@ if [ -n "${DEBUG:-}" ]; then
 fi
 
 function main() {
-    echo '--- installing kong-licensing ---'
-    pushd /distribution/kong-licensing/lib
-        CFLAGS="-I/work/openssl/inc" \
-        LIBPATH=/tmp/build/usr/local/kong/lib \
-        LDFLAGS="-Wl,-rpath,/usr/local/kong/lib -L/work/openssl/inc" \
-        make -s \
-            -j2 \
-            liblicense_utils.so
-        cp liblicense_utils.so /tmp/build/usr/local/kong/lib
-    popd
-    echo '--- installed kong-licensing ---'
+    if [ "${ENABLE_KONG_LICENSING:-}" == "false" ]; then
+        echo '--- skipping kong-licensing installation ---'
+    else
+        echo '--- installing kong-licensing ---'
+        pushd /distribution/kong-licensing/lib
+            CFLAGS="-I/work/openssl/inc" \
+            LIBPATH=/tmp/build/usr/local/kong/lib \
+            LDFLAGS="-Wl,-rpath,/usr/local/kong/lib -L/work/openssl/inc" \
+            make -s \
+                -j2 \
+                liblicense_utils.so
+            cp liblicense_utils.so /tmp/build/usr/local/kong/lib
+        popd
+        echo '--- installed kong-licensing ---'
+    fi
 }
 
 main
