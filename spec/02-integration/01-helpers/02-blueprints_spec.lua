4aa2395df3184da220069ce88b498d0e34afadc3
Wangchong Zhou <fffonion@gmail.com>
2022-06-10 02:40:59 -0700

    tests(bp) remove _G.kong hack as it's not needed

--- spec/02-integration/01-helpers/02-blueprints_spec.lua
+++ spec/02-integration/01-helpers/02-blueprints_spec.lua
@@ -18,7 +18,6 @@ for _, strategy in helpers.each_strategy() do
 
     local bp
     lazy_setup(function()
-      _G.kong = {}
       local db = assert(DB.new(helpers.test_conf, strategy))
       kong.db = db
       assert(db:init_connector())
