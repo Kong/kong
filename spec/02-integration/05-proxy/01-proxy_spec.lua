2265c5862df48805c05a7bddf7f05920726719c2
Vinicius Mignot <vinicius.mignot@gmail.com>
2022-06-14 11:26:50 -0300

    tests(proxy) server blocks increased with resty-events

--- spec/02-integration/05-proxy/01-proxy_spec.lua
+++ spec/02-integration/05-proxy/01-proxy_spec.lua
@@ -50,8 +50,8 @@ describe("Proxy interface listeners", function()
       proxy_listen = "off",
       admin_listen = "0.0.0.0:9001",
     }))
-    -- XXX EE-only 2
-    assert.equals(2, count_server_blocks(helpers.test_conf.nginx_kong_conf))
+    -- XXX EE-only 3
+    assert.equals(3, count_server_blocks(helpers.test_conf.nginx_kong_conf))
     assert.is_nil(get_listeners(helpers.test_conf.nginx_kong_conf).kong)
   end)
 
@@ -61,8 +61,8 @@ describe("Proxy interface listeners", function()
       admin_listen = "0.0.0.0:9000",
     }))
 
-    -- XXX EE-only 3
-    assert.equals(3, count_server_blocks(helpers.test_conf.nginx_kong_conf))
+    -- XXX EE-only 4
+    assert.equals(4, count_server_blocks(helpers.test_conf.nginx_kong_conf))
     assert.same({
       ["127.0.0.1:9001"] = 1,
       ["127.0.0.1:9002"] = 2,
