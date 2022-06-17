bf80cafdbee389fbff3a3de3e83314804173da61
Qi <add_sp@outlook.com>
2022-06-17 14:10:20 +0800

    tests(*) fix `spec/02-integration/03-db/09-query-semaphore_spec.lua`

--- spec/02-integration/03-db/09-query-semaphore_spec.lua
+++ spec/02-integration/03-db/09-query-semaphore_spec.lua
@@ -33,8 +33,6 @@ describe("#postgres Postgres query locks", function()
   end)
 
   it("results in query error failing to acquire resource", function()
-    local wait_timers_ctx = helpers.wait_timers_begin()
-
     local res = assert(client:send {
       method = "GET",
       path = "/slow-resource?prime=true",
@@ -43,7 +41,7 @@ describe("#postgres Postgres query locks", function()
     assert.res_status(204 , res)
 
     -- wait for zero-delay timer
-    helpers.wait_timers_end(wait_timers_ctx, 0.5)
+    helpers.wait_timer("slow-query", true, true)
 
     res = assert(client:send {
       method = "GET",
