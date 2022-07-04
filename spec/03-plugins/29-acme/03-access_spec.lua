82acde67f75745e2b3137469c54c784e2f68d7dd
suika <xumin.zhou@konghq.com>
2022-07-04 14:10:54 +0800

    add test for new option

--- spec/03-plugins/29-acme/03-access_spec.lua
+++ spec/03-plugins/29-acme/03-access_spec.lua
@@ -2,13 +2,13 @@ local helpers = require "spec.helpers"
 
 local dummy_id = "ZR02iVO6PFywzFLj6igWHd6fnK2R07C-97dkQKC7vJo"
 
+local do_domain = "acme.noatld"
+local skip_domain = "notacme.noatld"
+
 for _, strategy in helpers.each_strategy() do
   describe("Plugin: acme (handler.access) [#" .. strategy .. "]", function()
     local bp, db
     local proxy_client
-
-    local do_domain = "acme.noatld"
-    local skip_domain = "notacme.noatld"
 
     lazy_setup(function()
       bp, db = helpers.get_db_utils(strategy, {
@@ -119,6 +119,94 @@ for _, strategy in helpers.each_strategy() do
     end)
 
     pending("serves default cert", function()
+    end)
+
+  end)
+
+  describe("Plugin: acme (handler.access) [#" .. strategy .. "]", function()
+    local bp, db
+    local proxy_client
+
+    lazy_setup(function()
+      bp, db = helpers.get_db_utils(strategy, {
+        "certificates",
+        "snis",
+        "services",
+        "routes",
+        "plugins",
+        "acme_storage",
+      }, { "acme", })
+
+      assert(bp.routes:insert {
+        paths = { "/" },
+      })
+
+      assert(bp.plugins:insert {
+        name = "acme",
+        config = {
+          account_email = "test@test.com",
+          api_uri = "https://api.acme.org",
+          storage = "kong",
+          skip_domain_verify = true,
+        },
+      })
+
+      assert(bp.plugins:insert {
+        name = "key-auth",
+      })
+
+      assert(db.acme_storage:insert {
+        key = dummy_id .. "#http-01",
+        value = "isme",
+      })
+
+      assert(helpers.start_kong({
+        plugins = "bundled,acme",
+        database = strategy,
+      }))
+
+      proxy_client = helpers.proxy_client()
+    end)
+
+    lazy_teardown(function()
+      if proxy_client then
+        proxy_client:close()
+      end
+
+      helpers.stop_kong()
+    end)
+
+    it("allow any domain", function()
+      local res = assert( proxy_client:send {
+        method  = "GET",
+        path    = "/.well-known/acme-challenge/" .. dummy_id,
+        headers =  { host = "a.subdomain." .. do_domain }
+      })
+
+      -- key-auth should not run
+      local body = assert.response(res).has.status(200)
+      assert.equal("isme", body)
+
+      res = assert( proxy_client:send {
+        method  = "GET",
+        path    = "/.well-known/acme-challenge/" .. dummy_id,
+        headers =  { host = "asdsubdomain." .. do_domain }
+      })
+
+      -- key-auth should not run
+      local body = assert.response(res).has.status(200)
+      assert.equal("isme", body)
+
+      res = assert( proxy_client:send {
+        method  = "GET",
+        path    = "/.well-known/acme-challenge/" .. dummy_id,
+        headers =  { skip_domain }
+      })
+
+      -- key-auth should not run
+      local body = assert.response(res).has.status(200)
+      assert.equal("isme", body)
+
     end)
 
   end)
