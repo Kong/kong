a689b743bd9d30704aadd9c17f2e9cd28dbef375
Qi <add_sp@outlook.com>
2022-07-26 17:38:33 +0800

    tests(perf) fix perf framework for `aws-ec2` provider (#3529)

--- spec/fixtures/perf/terraform/aws-ec2/ssh.tf
+++ spec/fixtures/perf/terraform/aws-ec2/ssh.tf
@@ -6,5 +6,5 @@ resource "tls_private_key" "key" {
 resource "local_sensitive_file" "key_priv" {
   content  = tls_private_key.key.private_key_pem
   filename = "./id_rsa"
-  file_permission = "0755"
+  file_permission = "0600"
 }
