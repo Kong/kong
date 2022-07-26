11adf1a804fb7edae9454960724087b4f5f0fc41
suika <xumin.zhou@konghq.com>
2022-07-26 13:55:17 +0800

    apply suggestions

--- kong/db/declarative/migrations/init.lua
+++ kong/db/declarative/migrations/init.lua
@@ -1,12 +1,12 @@
 local regex_route_path = require "kong.db.declarative.migrations.regex_route_path"
 
-return function (tbl)
-    if not tbl then
-        -- we can not migrate without version specified
-        return
-    end
+return function(tbl)
+  if not tbl then
+    -- we can not migrate without version specified
+    return
+  end
 
-    regex_route_path(tbl)
+  regex_route_path(tbl)
 
-    tbl._format_version = "3.0"
+  tbl._format_version = "3.0"
 end
