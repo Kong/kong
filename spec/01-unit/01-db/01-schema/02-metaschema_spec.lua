82e2077407a392cf31e75ef41511f9d6ed26d9c6
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-08-01 14:51:30 -0700

    tests(schema) add test case for metaschema table_name

--- spec/01-unit/01-db/01-schema/02-metaschema_spec.lua
+++ spec/01-unit/01-db/01-schema/02-metaschema_spec.lua
@@ -439,6 +439,32 @@ describe("metaschema", function()
     assert.truthy(MetaSchema:validate(s))
   end)
 
+  it("populates the 'table_name' field from 'name' if not supplied", function()
+    local s = {
+      name = "testing",
+      primary_key = { "id" },
+      fields = {
+        { id = { type = "string", }, },
+        { foo = { type = "string" }, },
+      },
+    }
+
+    assert.truthy(MetaSchema:validate(s))
+    local schema = Schema.new(s)
+
+    assert.not_nil(schema.table_name)
+    assert.equals("testing", schema.name)
+    assert.equals("testing", schema.table_name)
+
+    s.table_name = "explicit_table_name"
+    assert.truthy(MetaSchema:validate(s))
+    schema = Schema.new(s)
+
+    assert.not_nil(schema.table_name)
+    assert.equals("testing", schema.name)
+    assert.equals("explicit_table_name", schema.table_name)
+  end)
+
   describe("subschemas", function()
 
     it("supports declaring subschemas", function()
