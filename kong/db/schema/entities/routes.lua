cd09979ad72e46caa54292eecd55d319bb0b8a1c
windmgc <windmgc@gmail.com>
2022-08-25 11:51:22 +0800

    add missing require

--- kong/db/schema/entities/routes.lua
+++ kong/db/schema/entities/routes.lua
@@ -1,6 +1,7 @@
 local typedefs = require("kong.db.schema.typedefs")
 local atc = require("kong.router.atc")
 local router = require("resty.router.router")
+local deprecation = require("kong.deprecation")
 
 local kong_router_flavor = kong and kong.configuration and kong.configuration.router_flavor
 
