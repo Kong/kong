62256c4182fd259a8898c4d3d65b3edd3b8345ef
chronolaw <chrono_cpp@me.com>
2022-09-01 10:58:22 +0800

    assert(inst:remove_matcher(id))

--- kong/router/atc.lua
+++ kong/router/atc.lua
@@ -231,7 +231,8 @@ local function new_from_previous(routes, get_exp_priority, old_router)
       r.seen = nil
 
     else
-      inst:remove_matcher(id)
+      assert(inst:remove_matcher(id))
+
       old_routes[id] = nil
       old_services[id] = nil
     end
