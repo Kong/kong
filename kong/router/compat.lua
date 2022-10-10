44d168faa5d0e9891b922dcaeabef126132f6010
chronolaw <chrono_cpp@me.com>
2022-10-10 16:44:07 +0800

    use one line to process regex path

--- kong/router/compat.lua
+++ kong/router/compat.lua
@@ -133,11 +133,9 @@ local function get_expression(route)
   end, paths, function(op, p)
     if op == OP_REGEX then
       -- 1. strip leading `~`
-      p = sub(p, 2)
       -- 2. prefix with `^` to match the anchored behavior of the traditional router
-      p = "^" .. p
       -- 3. update named capture opening tag for rust regex::Regex compatibility
-      return p:gsub("?<", "?P<")
+      return "^" .. p:sub(2):gsub("?<", "?P<")
     end
 
     return p
