aa09d808743b78c43d69fd308d9aea0355442556
suika <xumin.zhou@konghq.com>
2022-10-14 16:26:35 +0800

    apply suggestions

--- kong/runloop/plugin_servers/init.lua
+++ kong/runloop/plugin_servers/init.lua
@@ -364,7 +364,6 @@ end
 function plugin_servers.start()
   local pluginserver_timer = proc_mgmt.pluginserver_timer
   local pluginserver_connection_check_timer = proc_mgmt.connection_check_timer
-  local native_timer_at = _G.native_timer_at
   local handler = worker_id() == 0 and pluginserver_timer or pluginserver_connection_check_timer
 
   for _, server_def in ipairs(proc_mgmt.get_server_defs()) do
