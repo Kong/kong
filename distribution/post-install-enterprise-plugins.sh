4a7199281bf14a6ad92726e9abff600540dbc1e2
Colin Hutchinson <hutchic@users.noreply.github.com>
2022-10-17 08:35:54 -0400

    fix(plugin-installation): plugin installation script wasn't capable of "upgrading" (#3859)

--- distribution/post-install-enterprise-plugins.sh
+++ distribution/post-install-enterprise-plugins.sh
@@ -9,10 +9,13 @@ if [ -n "${DEBUG:-}" ]; then
 fi
 
 function main() {
+    echo '--- installing kong-enterprise plugins ---'
     pushd /kong
         make install-plugins-ee #move the script to here someday
     popd
     cp -R /usr/local/lib /tmp/build/usr/local/
+    cp -R /usr/local/share/lua /tmp/build/usr/local/share/
     luarocks purge --tree=/tmp/build/usr/local --old-versions
+    echo '--- installed kong-enterprise plugins ---'
 }
 main
