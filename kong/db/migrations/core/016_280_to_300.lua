2c0db84bbe3c30f3ce75e8a594421809222a6207
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-10-24 16:25:23 -0700

    chore(*): fix luacheck warnings

--- kong/db/migrations/core/016_280_to_300.lua
+++ kong/db/migrations/core/016_280_to_300.lua
@@ -346,12 +346,7 @@ return {
         return nil, err
       end
 
-<<<<<<< HEAD
-      local _
-      _, err = p_update_cache_key(connector)
-=======
       local _, err = p_update_cache_key(connector)
->>>>>>> kong/master
       if err then
         return nil, err
       end
