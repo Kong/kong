f1e38b50e44ddb7dfa3fa4f57788844e5f5794ee
Wangchong Zhou <wangchong@konghq.com>
2022-11-06 19:34:43 -0800

    fix`

--- spec/helpers/perf/drivers/terraform.lua
+++ spec/helpers/perf/drivers/terraform.lua
@@ -550,14 +550,13 @@ function _M:get_start_load_cmd(stub, script, uri)
   script_path = script_path and ("-s " .. script_path) or ""
 
   local nproc, err
-  -- find the physical cores count, instead of counting hyperthreading
-  nproc, err = perf.execute(ssh_execute_wrap(self, self.kong_ip, [[grep '^cpu\\scores' /proc/cpuinfo | uniq |  awk '{print $4}']]))
+  nproc, err = perf.execute(ssh_execute_wrap(self, self.kong_ip, "nproc"))
   if not nproc or err then
-    return false, "failed to get core count: " .. (err or "")
+    return false, "failed to get nproc: " .. (err or "")
   end
 
   if not tonumber(nproc) then
-    return false, "failed to get core count: " .. (nproc or "")
+    return false, "failed to get nproc: " .. (nproc or "")
   end
   nproc = tonumber(nproc)
 
