f2f4e043ab9edca9e19997502140ca893099d4f5
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-11-10 16:50:23 -0800

    fix(ci): use default entrypoint for redis container

--- spec/fixtures/redis/docker-entrypoint.sh
+++ spec/fixtures/redis/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 
+echo "Kong CI redis container..."
+
 if [ -d /workspace ] ;  then
+    echo "Starting test server..."
+
     redis-server                                                    \
         --tls-port 6380                                             \
         --tls-cert-file /workspace/spec/fixtures/redis/server.crt   \
