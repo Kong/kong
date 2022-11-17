0e44b45938818b6ced895a628e14c2180e4f036c
Wangchong Zhou <wangchong@konghq.com>
2022-11-16 23:45:30 -0800

    chore(clustering): remove unused code check_protocol_support

--- kong/clustering/utils.lua
+++ kong/clustering/utils.lua
@@ -325,42 +325,6 @@ function _M.check_configuration_compatibility(obj, dp_plugin_map)
 end
 
 
---- Return the highest supported Hybrid mode protocol version.
-function _M.check_protocol_support(conf, cert, cert_key)
-  local params = {
-    scheme = "https",
-    method = "HEAD",
-
-    ssl_verify = true,
-    ssl_client_cert = cert,
-    ssl_client_priv_key = cert_key,
-  }
-
-  if conf.cluster_mtls == "shared" then
-    params.ssl_server_name = "kong_clustering"
-
-  else
-    -- server_name will be set to the host if it is not explicitly defined here
-    if conf.cluster_server_name ~= "" then
-      params.ssl_server_name = conf.cluster_server_name
-    end
-  end
-
-  local c = http.new()
-  local res, err = c:request_uri(
-    "https://" .. conf.cluster_control_plane .. "/v1/wrpc", params)
-  if not res then
-    return nil, err
-  end
-
-  if res.status == 404 then
-    return "v0"
-  end
-
-  return "v1"   -- wrpc
-end
-
-
 local WS_OPTS = {
   timeout = constants.CLUSTERING_TIMEOUT,
   max_payload_len = kong.configuration.cluster_max_payload,
