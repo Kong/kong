d1ddb7b10709d4284dc23e6584afbbe8e749d5fd
chronolaw <chrono_cpp@me.com>
2022-11-30 10:10:14 +0800

    optimize load_into_db()

--- kong/db/declarative/init.lua
+++ kong/db/declarative/init.lua
@@ -333,10 +333,12 @@ function declarative.load_into_db(entities, meta)
     local entity = db[entity_name]
     if entity then
       insert(schemas, entity.schema)
+
     else
       return nil, "unknown entity: " .. entity_name
     end
   end
+
   local sorted_schemas, err = schema_topological_sort(schemas)
   if not sorted_schemas then
     return nil, err
@@ -350,17 +352,20 @@ function declarative.load_into_db(entities, meta)
   local options = {
     transform = meta._transform,
   }
-  local schema, primary_key, ok, err, err_t
+
   for i = 1, #sorted_schemas do
-    schema = sorted_schemas[i]
-    for _, entity in pairs(entities[schema.name]) do
+    local schema = sorted_schemas[i]
+    local schema_name = schema.name
+
+    local primary_key, ok, err, err_t
+    for _, entity in pairs(entities[schema_name]) do
       entity = deepcopy(entity)
       entity._tags = nil
       entity.ws_id = nil
 
       primary_key = schema:extract_pk_values(entity)
 
-      ok, err, err_t = db[schema.name]:upsert(primary_key, entity, options)
+      ok, err, err_t = db[schema_name]:upsert(primary_key, entity, options)
       if not ok then
         return nil, err, err_t
       end
@@ -456,9 +461,11 @@ local function export_from_db(emitter, skip_ws, skip_disabled_entities, expand_f
       -- as well do not export plugins and routes of dsiabled services
       if skip_disabled_entities and name == "services" and not row.enabled then
         disabled_services[row.id] = true
+
       elseif skip_disabled_entities and name == "routes" and row.service and
         disabled_services[row.service ~= null and row.service.id] then
           disabled_routes[row.id] = true
+
       elseif skip_disabled_entities and name == "plugins" and not row.enabled then
         goto skip_emit
 
@@ -540,6 +547,7 @@ local table_emitter = {
   emit_entity = function(self, entity_name, entity_data)
     if not self.out[entity_name] then
       self.out[entity_name] = { entity_data }
+
     else
       insert(self.out[entity_name], entity_data)
     end
@@ -574,6 +582,7 @@ local function remove_nulls(tbl)
   for k,v in pairs(tbl) do
     if v == null then
       tbl[k] = nil
+
     elseif type(v) == "table" then
       tbl[k] = remove_nulls(v)
     end
@@ -595,6 +604,7 @@ local proto_emitter = {
 
     if not self.out[entity_name] then
       self.out[entity_name] = { entity_data }
+
     else
       insert(self.out[entity_name], entity_data)
     end
