27a22e3de4af3df998eefc1afaad697eb1cddd22
suika <xumin.zhou@konghq.com>
2022-12-05 16:52:35 +0800

    test

--- spec/03-plugins/26-prometheus/07-optional_fields_spec.lua
+++ spec/03-plugins/26-prometheus/07-optional_fields_spec.lua
@@ -1,4 +1,5 @@
 local helpers = require "spec.helpers"
+local UUID_PATTERN = "%x%x%x%x%x%x%x%x%-%x%x%x%x%-%x%x%x%x%-%x%x%x%x%-%x%x%x%x%x%x%x%x%x%x%x%x"
 
 local tcp_status_port = helpers.get_available_port()
 
@@ -15,6 +16,7 @@ for _, strategy in helpers.each_strategy() do
         cluster_cert_key = "spec/fixtures/ocsp_certs/kong_clustering.key",
         status_listen = "0.0.0.0:" .. tcp_status_port,
         db_update_propagation = 0.01, -- otherwise cassandra would complain
+        proxy_listen = "0.0.0.0:8000",
       })
     end)
 
@@ -40,12 +42,32 @@ for _, strategy in helpers.each_strategy() do
       })
       local body = assert.res_status(200, res)
 
-      -- local metrics outputs
       assert.matches('kong_memory_lua_shared_dict_bytes', body, nil, true)
-      assert.matches('kong_nginx_connections_total', body, nil, true)
 
-      -- non-local metrics
-      assert.not_matches('kong_nginx_metric_errors_total', body, nil, true)
+      local states = { "accepted", "active", "handled", "reading", "total", "waiting", "writing"}
+
+      for _, v in ipairs(states) do
+        assert.matches('kong_nginx_connections_total{node_id="' .. UUID_PATTERN .. '",subsystem="' .. ngx.config.subsystem .. '",state="' .. v .. '"} %d+', body)
+      end
+
+      local nonlocal = {
+        "http_requests_total",
+        "stream_sessions_total",
+        "kong_latency_ms",
+        "upstream_latency_ms",
+        "request_latency_ms",
+        "session_duration_ms",
+        "bandwidth_bytes",
+        "upstream_target_health",
+        "data_plane_last_seen",
+        "data_plane_config_hash",
+        "data_plane_version_compatible",
+        "data_plane_cluster_cert_expiry_timestamp",
+      }
+
+      for _, v in ipairs(nonlocal) do
+        assert.not_matches('kong_' .. v, body, nil, true)
+      end
     end)
   end)
 end
