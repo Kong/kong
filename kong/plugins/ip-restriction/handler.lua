e4ab98e5b557569900b1222100d51cec4c93515d
chronolaw <chrono_cpp@me.com>
2022-12-11 08:05:31 +0800

    code clean

--- kong/plugins/ip-restriction/handler.lua
+++ kong/plugins/ip-restriction/handler.lua
@@ -47,18 +47,20 @@ function IpRestrictionHandler:access(conf)
     return kong.response.error(403, "Cannot identify the client IP address, unix domain sockets are not supported.")
   end
 
+  local deny = conf.deny
+  local allow = conf.allow
   local status = conf.status or 403
   local message = conf.message or "Your IP address is not allowed"
 
-  if conf.deny and #conf.deny > 0 then
-    local blocked = match_bin(conf.deny, binary_remote_addr)
+  if deny and #deny > 0 then
+    local blocked = match_bin(deny, binary_remote_addr)
     if blocked then
       return kong.response.error(status, message)
     end
   end
 
-  if conf.allow and #conf.allow > 0 then
-    local allowed = match_bin(conf.allow, binary_remote_addr)
+  if allow and #allow > 0 then
+    local allowed = match_bin(allow, binary_remote_addr)
     if not allowed then
       return kong.response.error(status, message)
     end
