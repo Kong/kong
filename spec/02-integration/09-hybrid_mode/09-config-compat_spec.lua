9b668ac4fb515b52becb03b6eafa0d5e4c4828d1
chronolaw <chrono_cpp@me.com>
2022-12-12 11:14:47 +0800

    fix 09-config-compat_spec.lua

--- spec/02-integration/09-hybrid_mode/09-config-compat_spec.lua
+++ spec/02-integration/09-hybrid_mode/09-config-compat_spec.lua
@@ -24,9 +24,13 @@ local function cluster_client(opts)
     node_plugins_list = PLUGIN_LIST,
   })
 
-  if res and res.config then
-    local inflated = assert(utils.inflate_gzip(res.config))
-    res.config = cjson.decode(inflated)
+  --if res and res.config then
+  --  local inflated = assert(utils.inflate_gzip(res.config))
+  --  res.config = cjson.decode(inflated)
+  --end
+
+  if res and res.config_table then
+    res.config = res.config_table
   end
 
   return res
