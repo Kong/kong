40875f4f602dc2d806d565e672b451a90967ec22
Zachary Hu <zachary.hu@konghq.com>
2022-12-21 17:09:26 +0800

    tests(*): add wait_until timeout and reduce db_update_propagation

--- spec-ee/02-integration/03-vitals/08-hybrid_spec.lua
+++ spec-ee/02-integration/03-vitals/08-hybrid_spec.lua
@@ -25,6 +25,7 @@ for _, strategy in helpers.each_strategy() do
         lua_ssl_trusted_certificate = "spec/fixtures/kong_clustering.crt",
         database = strategy,
         db_update_frequency = 0.1,
+        db_update_propagation = 0.1,
         cluster_listen = "127.0.0.1:9005",
         cluster_telemetry_listen = "127.0.0.1:9006",
         nginx_conf = "spec/fixtures/custom_nginx.template",
@@ -165,7 +166,7 @@ for _, strategy in helpers.each_strategy() do
             end
           end
 
-        end, 10)
+        end, 30)
       end)
     end)
   end)
