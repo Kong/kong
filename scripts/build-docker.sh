da5bff6a71d4517a599fb7ae9a2ab8345069565e
Mayo <i@shoujo.io>
2022-12-26 09:19:52 +0000

    feat(bazel): add `packages` workflow for CD on Github Actions (#4157)
    - [x] Add `nfpm_pkg` build rule for packaging
    - [x] Support `deb`, `apk`,`el8` `el7`, `aws2`, and `aws2022` package types
    - [x] Build Packages and Images
      - [x] Debian 10
      - [x] Debian 11 (image `*-debian`)
      - [x] Ubuntu 18.04
      - [x] Ubuntu 20.04
      - [x] Ubuntu 22.04 (image `*-ubuntu`)
      - [x] Centos 7
      - [x] RHEL 7
      - [x] RHEL 8 (image `*-rhel`)
      - [ ] Alpine (image `*-alpine`)
    - [x] Integrate with PULP
    - [x] GPG signing for rpm packages

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,52 +2,45 @@
 
 set -e
 
+# This script is from the Kong/kong-build-tools repo, and is used to build the Kong Docker image.
+
 source .requirements
 source scripts/backoff.sh
 
-DOWNLOAD_CACHE=${DOWNLOAD_CACHE:-/tmp}
 KONG_VERSION=$(bash scripts/grep-kong-version.sh)
-DOCKER_REPOSITORY=${TESTING_DOCKER_REPOSITORY:-"kong/kong-gateway-internal-testing"}
+
+DOCKER_REPOSITORY=${DOCKER_REPOSITORY:-"kong/kong"}
 ARCHITECTURE=${ARCHITECTURE:-amd64}
 KONG_CONTAINER_TAG=${KONG_CONTAINER_TAG:-$KONG_VERSION}
 PACKAGE_TYPE=${PACKAGE_TYPE:-deb}
-DOCKER_KONG_VERSION=${DOCKER_KONG_VERSION:-master}
-BASE_IMAGE_NAME=${BASE_IMAGE_NAME:-"ubuntu:22.04"}
+KONG_BASE_IMAGE=${KONG_BASE_IMAGE:-}
 
+ARTIFACT_PREFIX=${ARTIFACT_PREFIX:-"bazel-bin/pkg"}
+ARTIFACT=${ARTIFACT:-"kong.deb"}
+
+KONG_ARTIFACT=$ARTIFACT_PREFIX/$ARTIFACT
 KONG_IMAGE_NAME=$DOCKER_REPOSITORY:$KONG_CONTAINER_TAG
 
-DOCKER_BUILD_ARGS=()
-
-if [ ! -d $DOWNLOAD_CACHE/docker-kong ]; then
-  git clone https://github.com/Kong/docker-kong.git $DOWNLOAD_CACHE/docker-kong
+BUILD_ARGS=()
+if [ "$EDITION" == 'enterprise' ]; then
+  BUILD_ARGS+=(--build-arg EE_PORTS="8002 8445 8003 8446 8004 8447")
 fi
 
-pushd $DOWNLOAD_CACHE/docker-kong
-  git fetch
-  git reset --hard $DOCKER_KONG_VERSION || git reset --hard origin/$DOCKER_KONG_VERSION
-  chmod -R 755 ./*.sh
-popd
-
-if [ "$PACKAGE_TYPE" == "deb" ]; then
-  cp bazel-bin/pkg/kong_${KONG_VERSION}_${ARCHITECTURE}.deb $DOWNLOAD_CACHE/docker-kong/kong.deb
+if [ -n "$KONG_BASE_IMAGE" ]; then
+  BUILD_ARGS+=(--build-arg KONG_BASE_IMAGE="$KONG_BASE_IMAGE")
 fi
 
-pushd $DOWNLOAD_CACHE/docker-kong
-  DOCKER_BUILD_ARGS+=(--pull)
-  DOCKER_BUILD_ARGS+=(--build-arg ASSET=local .)
-
-  if [[ "$EDITION" == 'enterprise' ]]; then
-    DOCKER_BUILD_ARGS+=(--build-arg EE_PORTS="8002 8445 8003 8446 8004 8447")
-  fi
-
-  sed -i.bak 's/^FROM .*/FROM '${BASE_IMAGE_NAME}'/' Dockerfile.$PACKAGE_TYPE
-
-  with_backoff docker build \
+docker_build () {
+  tar -czh $KONG_ARTIFACT build/dockerfiles LICENSE | docker build \
+    --pull \
     --progress=auto \
     -t $KONG_IMAGE_NAME \
-    -f Dockerfile.$PACKAGE_TYPE \
-    --build-arg KONG_VERSION=$KONG_VERSION \
-    "${DOCKER_BUILD_ARGS[@]}"
+    -f build/dockerfiles/$PACKAGE_TYPE.Dockerfile \
+    --build-arg KONG_VERSION="$KONG_VERSION" \
+    --build-arg KONG_ARTIFACT="$KONG_ARTIFACT" \
+    "${BUILD_ARGS[@]}" -
+}
 
-  echo "Kong image Name: $KONG_IMAGE_NAME"
-popd
+with_backoff docker_build
+
+echo "Kong image Name: $KONG_IMAGE_NAME"
