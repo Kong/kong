d5b0e551c2b500b2930ef5cd62d207eea91c3975
chronolaw <chrono_cpp@me.com>
2022-12-28 10:18:05 +0800

    fix schema check

--- kong/db/schema/metaschema.lua
+++ kong/db/schema/metaschema.lua
@@ -222,6 +222,7 @@ local entity_checkers = {
       fields = {
         { field_sources = { type = "array", elements = { type = "string" } } },
         { fn = { type = "function" } },
+        { run_with_missing_fields = { type = "boolean"} },
       }
     }
   },
