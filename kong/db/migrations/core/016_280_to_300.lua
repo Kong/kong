475032941f1f1794b2e8b9d5d4242c42b0ea0958
chronolaw <chrono_cpp@me.com>
2023-01-10 19:45:20 +0800

    c_validate_regex_path

--- kong/db/migrations/core/016_280_to_300.lua
+++ kong/db/migrations/core/016_280_to_300.lua
@@ -9,6 +9,28 @@ local encode_array  = arrays.encode_array
 local migrate_path = require "kong.db.migrations.migrate_path_280_300"
 
 
+local validate_atc_expression
+do
+  local router = require("resty.router.router")
+  local CACHED_SCHEMA = require("kong.router.atc").schema
+  local get_expression = require("kong.router.compat").get_expression
+
+  validate_atc_expression = function(route)
+    local r = router.new(CACHED_SCHEMA)
+    local exp = get_expression(route)
+
+    local res, err = r:add_matcher(0, route.id, exp)
+    if not res then
+      log.error("Regex path may not work with router flavor 'traditional_compatible', " ..
+                "route id: %s, err: %s", route.id, err)
+      return false
+    end
+
+    return true
+  end
+end
+
+
 -- remove repeated targets, the older ones are not useful anymore. targets with
 -- weight 0 will be kept, as we cannot tell which were deleted and which were
 -- explicitly set as 0.
@@ -140,6 +162,40 @@ local function c_copy_vaults_beta_to_sm_vaults(coordinator)
   end
 
   return true
+end
+
+
+local function c_validate_regex_path(coordinator)
+  local validate_ok = true
+
+  for rows, err in coordinator:iterate("SELECT id, paths FROM routes") do
+    if err then
+      return nil, err
+    end
+
+    for i = 1, #rows do
+      local route = rows[i]
+
+      if not route.paths then
+        goto continue
+      end
+
+      for idx, path in ipairs(route.paths) do
+        local normalized_path, current_changed = migrate_path(path)
+        if current_changed then
+          route.paths[idx] = normalized_path
+        end
+      end
+
+      if not validate_atc_expression(route) then
+        validate_ok = false
+      end
+
+      ::continue::
+    end
+  end
+
+  return validate_ok
 end
 
 
