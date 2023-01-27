6d23ed4dbb3531802c4bbedf16644624a54fdb88
Hans Hübner <hans.huebner@konghq.com>
2023-01-27 07:09:31 +0000

    fix(test): fix another test

--- spec/01-unit/01-db/01-schema/11-declarative_config/02-process_auto_fields_spec.lua
+++ spec/01-unit/01-db/01-schema/11-declarative_config/02-process_auto_fields_spec.lua
@@ -175,6 +175,17 @@ describe("declarative config: process_auto_fields", function()
                 queue_size = 1,
                 retry_count = 10,
                 timeout = 10000,
+                queue = {
+                  batch_max_size = 1,
+                  capacity = 10000,
+                  max_delay = 1,
+                  max_idle_time = 60,
+                  max_retry_delay = 60,
+                  max_retry_time = 60,
+                  name = null,
+                  poll_time = 1,
+                  string_capacity = null,
+                },
               }
             },
           }
@@ -227,6 +238,17 @@ describe("declarative config: process_auto_fields", function()
                 queue_size = 1,
                 retry_count = 10,
                 timeout = 10000,
+                queue = {
+                  batch_max_size = 1,
+                  capacity = 10000,
+                  max_delay = 1,
+                  max_idle_time = 60,
+                  max_retry_delay = 60,
+                  max_retry_time = 60,
+                  name = null,
+                  poll_time = 1,
+                  string_capacity = null,
+                },
               }
             },
           }
@@ -339,6 +361,17 @@ describe("declarative config: process_auto_fields", function()
                       queue_size = 1,
                       retry_count = 10,
                       timeout = 10000,
+                      queue = {
+                        batch_max_size = 1,
+                        capacity = 10000,
+                        max_delay = 1,
+                        max_idle_time = 60,
+                        max_retry_delay = 60,
+                        max_retry_time = 60,
+                        name = null,
+                        poll_time = 1,
+                        string_capacity = null,
+                      },
                     }
                   },
                 }
@@ -654,6 +687,17 @@ describe("declarative config: process_auto_fields", function()
                           queue_size = 1,
                           retry_count = 10,
                           timeout = 10000,
+                          queue = {
+                            batch_max_size = 1,
+                            capacity = 10000,
+                            max_delay = 1,
+                            max_idle_time = 60,
+                            max_retry_delay = 60,
+                            max_retry_time = 60,
+                            name = null,
+                            poll_time = 1,
+                            string_capacity = null,
+                          },
                         }
                       }
                     }
