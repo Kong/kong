cb3e39702e238df2c9ad146aa35d62bfe0e0cebc
Hisham Muhammad <hisham@gobolinux.org>
2023-02-10 11:13:28 +0000

    refactor(proxy-wasm): support ngx_wasm_module 'feat/gateway' branch

--- kong/plugins/proxy-wasm/handler.lua
+++ kong/plugins/proxy-wasm/handler.lua
@@ -1,11 +1,5 @@
-local kong = kong
 local kong_meta = require "kong.meta"
 local proxy_wasm = require "resty.http.proxy_wasm"
-local schema = require "kong.plugins.proxy-wasm.schema"
-
-
-local ipairs = ipairs
-local insert = table.insert
 
 
 local ProxyWasmHandler = {
@@ -14,42 +8,31 @@ local ProxyWasmHandler = {
 }
 
 
-function ProxyWasmHandler:init_worker()
-  --[[
-  -- WIP: internal ngx_wasm_module refactor for root ctx/req ctx distinction
-  local db = kong.db
-  local filters, seen = {}, {}
+local cache = setmetatable({}, { __mode = "k" })
 
-  for plugin, err in db.plugins:each(nil, { name = schema.name }) do
-    if err then
-      ngx.log(ngx.CRIT, "could not load routes: " .. err)
-      return
-    end
 
-    for _, filter in ipairs(plugin.config.filters) do
-      if not seen[filter.name] then
-        insert(filters, {
-          name = filter.name,
-          config = filter.config,
-        })
-
-        seen[filter.name] = true
-      end
-    end
+local function get_c_ops(conf)
+  if cache[conf] then
+    return cache[conf]
   end
 
-  local root_c_ops, err = proxy_wasm.new(filters, true)
-  if not root_c_ops then
-    ngx.log(ngx.CRIT, err)
+  local c_ops, err = proxy_wasm.new(conf.filters)
+  if not c_ops then
+    error(err)
   end
-  --]]
+
+  local ok, err = proxy_wasm.load(c_ops)
+  if not ok then
+    error(err)
+  end
+
+  cache[conf] = c_ops
+  return c_ops
 end
 
 
 function ProxyWasmHandler:access(conf)
-  ngx.log(ngx.INFO, "in access")
-
-  local c_ops, err = proxy_wasm.new(conf.filters)
+  local c_ops, err = get_c_ops(conf)
   if not c_ops then
     error(err)
   end
@@ -58,21 +41,6 @@ function ProxyWasmHandler:access(conf)
   if not ok then
     error(err)
   end
-end
-
-
-function ProxyWasmHandler:header_filter(conf)
-  ngx.log(ngx.INFO, "in header_filter")
-end
-
-
-function ProxyWasmHandler:body_filter(conf)
-  ngx.log(ngx.INFO, "in body_filter")
-end
-
-
-function ProxyWasmHandler:log(conf)
-  ngx.log(ngx.INFO, "in log")
 end
 
 
