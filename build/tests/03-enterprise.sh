ee92150139ab32fc0198ae9401af053ce2c64739
Isa Farnik <isa@konghq.com>
2023-02-14 14:16:22 -0800

    chore(tests): appease shellcheck/shfmt

--- build/tests/03-enterprise.sh
+++ build/tests/03-enterprise.sh
@@ -12,15 +12,17 @@ if [[ "$EDITION" != "enterprise" ]]; then
 fi
 
 KONG_LICENSE_URL="https://download.konghq.com/internal/kong-gateway/license.json"
-KONG_LICENSE_DATA=$(curl \
-  --silent \
-  --location \
-  --retry 3 \
-  --retry-delay 3 \
-  --user "$PULP_USERNAME:$PULP_PASSWORD" \
-  --url "$KONG_LICENSE_URL"
-)
+KONG_LICENSE_DATA="$(
+  curl \
+    --silent \
+    --location \
+    --retry 3 \
+    --retry-delay 3 \
+    --user "$PULP_USERNAME:$PULP_PASSWORD" \
+    --url "$KONG_LICENSE_URL"
+)"
 export KONG_LICENSE_DATA
+
 if [[ ! $KONG_LICENSE_DATA == *"signature"* || ! $KONG_LICENSE_DATA == *"payload"* ]]; then
   # the check above is a bit lame, but the best we can do without requiring
   # yet more additional dependenies like jq or similar.
@@ -29,9 +31,10 @@ if [[ ! $KONG_LICENSE_DATA == *"signature"* || ! $KONG_LICENSE_DATA == *"payload
 fi
 
 F=$(mktemp)
-echo "$KONG_LICENSE_DATA" > $F
+echo "$KONG_LICENSE_DATA" >$F
 
-assert_response "-F payload=@$F $KONG_ADMIN_URI/licenses" "201"
+assert_response "-F payload=@$F $KONG_ADMIN_URI/licenses" "201 409"
+
 rm $F
 
 sleep 1
