c33859a83ea402e0b86460f8ea89f3997cc76b28
Xumin <100666470+StarlightIbuki@users.noreply.github.com>
2023-02-24 01:58:50 +0800

    fix(db/migration): do migration before validation (#10348)
    Co-authored-by: Chrono <chrono_cpp@me.com>
    Co-authored-by: Aapo Talvensaari <aapo.talvensaari@gmail.com>
    Co-authored-by: Enrique García Cota <kikito@gmail.com>
    (cherry picked from commit 2f82cedaa248275a9e61e869331409b38d24d8d9)

--- kong/db/declarative/migrations/route_path.lua
+++ kong/db/declarative/migrations/route_path.lua
@@ -1,32 +1,47 @@
 local migrate_path = require "kong.db.migrations.migrate_path_280_300"
+local lyaml_null = require("lyaml").null
+local cjson_null = require("cjson").null
 
+local ngx_null = ngx.null
 local pairs  = pairs
 local ipairs = ipairs
-local null   = ngx.null
 
-return function(tbl, version)
-  if not tbl or not (version == "1.1" or version == "2.1") then
-    return
+local EMPTY = {}
+
+local function ensure_table(val)
+  if val == nil or val == ngx_null or val == lyaml_null or val == cjson_null or type(val) ~= "table" then
+    return EMPTY
   end
+  return val
+end
 
-  local routes = tbl.routes
-
-  if not routes then
-    -- no need to migrate
-    return
-  end
-
+local function migrate_routes(routes)
   for _, route in pairs(routes) do
-    local paths = route.paths
-    if not paths or paths == null then
-      -- no need to migrate
-      goto continue
-    end
+    local paths = ensure_table(route.paths)
 
     for idx, path in ipairs(paths) do
       paths[idx] = migrate_path(path)
     end
-
-    ::continue::
   end
+end
+
+return function(tbl)
+  local version = tbl._format_version
+  if not tbl or not (version == "1.1" or version == "2.1") then
+    return
+  end
+
+  -- migrate top-level routes
+  local routes = ensure_table(tbl.routes)
+  migrate_routes(routes)
+
+  -- migrate routes nested in top-level services
+  local services = ensure_table(tbl.services)
+  for _, service in ipairs(services) do
+    local nested_routes = ensure_table(service.routes)
+
+    migrate_routes(nested_routes)
+  end
+
+  tbl._format_version = "3.0"
 end
