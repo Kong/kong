4bc8ea925762b1b442d36b3a590e0a7880d38c1a
Hans Hübner <hans.huebner@gmail.com>
2023-02-25 09:40:22 +0100

    Revert "fix(key-auth): return unauthorized if api key includes non-ascii symbols (#10171)" (#10372)
    This reverts commit d10101955bc554e9e453d5968392babbdf1bf4f9 (#10171).
    
    The fix is too restrictive on the key syntax due to a misunderstanding
    on my end.  Before reimplementing, we need a reproduction case that
    demonstrates the problem, as we may need to fix the issue at another
    level.

--- spec/03-plugins/09-key-auth/02-access_spec.lua
+++ spec/03-plugins/09-key-auth/02-access_spec.lua
@@ -245,19 +245,6 @@ for _, strategy in helpers.each_strategy() do
         local json = cjson.decode(body)
         assert.same({ message = "No API key found in request" }, json)
       end)
-      it("returns Unauthorized on invalid symbol in key header", function()
-        local res = assert(proxy_client:send {
-          method  = "GET",
-          path    = "/status/200",
-          headers = {
-            ["Host"] = "key-auth1.com",
-            ["apikey"] = "5SRmk6gý",
-          }
-        })
-        local body = assert.res_status(401, res)
-        local json = cjson.decode(body)
-        assert.same({ message = "Invalid symbol(s) found in API key" }, json)
-      end)
       it("returns WWW-Authenticate header on missing credentials", function()
         local res = assert(proxy_client:send {
           method  = "GET",
