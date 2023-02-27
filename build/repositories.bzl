Kong/kong

commit 50e8ba2df5c6134407fef034b4b170316cf239ab

fix(bazel): fix intel detection (#10322)

build/repositories.bzl
@@ -70,6 +70,8 @@ def _github_release_impl(ctx):
 
     if os_arch == "aarch64":
         os_arch = "arm64"
+    if os_arch == "x86_64":
+        os_arch = "amd64"
     elif os_arch != "amd64":
         fail("Unsupported arch %s" % os_arch)
 

commit 5eadca866b9db9859ea8769f7a8187dc75b1628f

fix(build): fix intel detection with elif
regression from #10322

build/repositories.bzl
@@ -70,7 +70,7 @@ def _github_release_impl(ctx):
 
     if os_arch == "aarch64":
         os_arch = "arm64"
-    if os_arch == "x86_64":
+    elif os_arch == "x86_64":
         os_arch = "amd64"
     elif os_arch != "amd64":
         fail("Unsupported arch %s" % os_arch)