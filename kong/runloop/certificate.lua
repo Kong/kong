0bc0c8b7e062d0f0c429e7037fd4c3d5a360c864
Zhefeng Chen <chzf_zju@163.com>
2023-03-02 20:01:34 +0800

    optimize calls of string.find

--- kong/runloop/certificate.lua
+++ kong/runloop/certificate.lua
@@ -243,11 +243,12 @@ local function find_certificate(sni)
       -- we choose to not call typedefs.wildcard_host.custom_validator(sni)
       -- in the front to reduce the cost in normal flow.
       -- these error messages are from validate_wildcard_host()
-      local idx = err:find("must not have a port") or
-                  err:find("must not be an IP") or
-                  err:find("invalid value: ") or
-                  err:find("only one wildcard must be specified") or
-                  err:find("wildcard must be leftmost or rightmost character")
+      local idx = err:find("must not have a port", nil, true) or
+                  err:find("must not be an IP", nil, true) or
+                  err:find("invalid value: ", nil, true) or
+                  err:find("only one wildcard must be specified", nil, true) or
+                  err:find("wildcard must be leftmost or rightmost character",
+                           nil, true)
       if idx then
 
         kong.log.debug("invalid SNI '", sni, "', ", err:sub(idx),
