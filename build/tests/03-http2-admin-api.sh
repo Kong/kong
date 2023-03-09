1a8649bfba9f6ab7bfe95085e9a275dc638f8999
windmgc <windmgc@gmail.com>
2023-03-09 12:08:49 +0800

    change curl http2 support check from bypass to fail

--- build/tests/03-http2-admin-api.sh
+++ build/tests/03-http2-admin-api.sh
@@ -11,8 +11,7 @@ kong_ready
 
 msg_test "Check if cURL supports HTTP/2"
 if ! curl --version | grep -i "http2" > /dev/null; then
-  msg_yellow "local cURL does not support HTTP/2, bypass HTTP/2 tests"
-  exit 0
+  err_exit " local cURL does not support HTTP/2"
 fi
 
 msg_test "Check HTTP/2 Admin API response is valid"
