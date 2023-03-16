e90d327a5864cc47ad5f9cf28e4d6c153b37fb22
Zhefeng C <38037704+catbro666@users.noreply.github.com>
2023-03-16 20:30:18 +0800

    fix(runloop): ensure ssl request with an invalid sni is not logged as an error (#10404)
    Currently, a ssl request with invalid sni will cause an error log as follows:
    
    `2023/03/01 06:56:42 [error] 9398#0: *466 [lua] certificate.lua:51: log(): [ssl] failed to fetch SNI: failed to fetch '1.1.1.1' SNI: [postgres] must not be an IP, context: ssl_certificate_by_lua*, client: 127.0.0.1, server: 0.0.0.0:8443`
    
    This is confusing. Customers may think it's related to db.
    As it's just an invalid request, change the log to debug level.
    
    FTI-1716

--- kong/db/schema/typedefs.lua
+++ kong/db/schema/typedefs.lua
@@ -153,7 +153,7 @@ local function validate_sni(host)
     return nil, "invalid value: " .. host
   end
 
-  if res.type ~= "name" then
+  if res and res.type ~= "name" then
     return nil, "must not be an IP"
   end
 
@@ -186,7 +186,7 @@ local function validate_wildcard_host(host)
     return nil, "invalid value: " .. host
   end
 
-  if res.type ~= "name" then
+  if res and res.type ~= "name" then
     return nil, "must not be an IP"
   end
 
