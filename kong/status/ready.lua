104a2262bbaf9f586dba55783a27954f7a4609b5
pluveto <i@pluvet.com>
2023-04-11 11:11:19 +0800

    returns err

--- kong/status/ready.lua
+++ kong/status/ready.lua
@@ -8,7 +8,11 @@ local get_current_hash = declarative.get_current_hash
 
 
 local worker_count = ngx.worker.count()
-local kong_shm = ngx.shared.kong
+local kong_shm     = ngx.shared.kong
+
+local ngx_log        = ngx.log
+local ngx_WARN       = ngx.WARN
+
 local is_traditional = kong.configuration.database ~= "off"
 
 local DECLARATIVE_PLUGINS_REBUILD_COUNT_KEY = 
@@ -17,11 +21,17 @@ local DECLARATIVE_ROUTERS_REBUILD_COUNT_KEY =
                                 constants.DECLARATIVE_ROUTERS_REBUILD_COUNT_KEY
 local DECLARATIVE_EMPTY_CONFIG_HASH = constants.DECLARATIVE_EMPTY_CONFIG_HASH
 
+--[[
+Checks if Kong is ready to serve.
+
+@return boolean indicating if Kong is ready to serve.
+@return string|nil an error message if Kong is not ready, or nil otherwise.
+--]]
 local function is_ready()
 
   local ok = kong.db:connect()
   if not ok then
-    return false
+    return false, "failed to connect to database"
   end
 
   if is_traditional then
@@ -34,16 +44,22 @@ local function is_ready()
   local plugins_iterator_rebuilds = 
       tonumber(kong_shm:get(DECLARATIVE_PLUGINS_REBUILD_COUNT_KEY)) or 0
 
-  if router_rebuilds < worker_count 
-    or plugins_iterator_rebuilds < worker_count
-  then
-    return false
+  if router_rebuilds < worker_count then
+    return false, "router rebuilds not complete"
+  end
+
+  if plugins_iterator_rebuilds < worker_count then
+    return false, "plugins iterator rebuilds not complete"
   end
 
   local current_hash = get_current_hash()
 
-  if not current_hash or current_hash == DECLARATIVE_EMPTY_CONFIG_HASH then
-    return false
+  if not current_hash then
+    return false, "no configuration hash"
+  end
+
+  if current_hash == DECLARATIVE_EMPTY_CONFIG_HASH then
+    return false, "empty configuration hash"
   end
 
   return true
@@ -52,10 +68,12 @@ end
 return {
   ["/status/ready"] = {
     GET = function(self, dao, helpers)
-      if is_ready() then
-        return kong.response.exit(200, 'ready')
+      local ok, err = is_ready()
+      if ok then
+        return kong.response.exit(200, { message = "ready" })
       else
-        return kong.response.exit(503, 'not ready')
+        ngx_log(ngx_WARN, "not ready: ", err)
+        return kong.response.exit(503, { message = "not ready" })
       end
     end
   }
