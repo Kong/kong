3af9222b8f0d7c6194131ea388c8c04fc1f1bdc8
Vinicius Mignot <vinicius.mignot@gmail.com>
2023-04-14 18:05:01 -0300

    fix(conf_loader): allow to configure only what matters

--- kong/templates/kong_defaults.lua
+++ kong/templates/kong_defaults.lua
@@ -202,8 +202,5 @@ wasm_filters_path = NONE
 wasm_compiler = NONE
 wasm_shm_kv = NONE
 wasm_shm_queue = NONE
-wasm_socket_buffer_reuse = NONE 
-wasm_socket_buffer_size = NONE
 wasm_tls_no_verify_warn = NONE
-proxy_wasm_request_headers_in_access = NONE
 ]]
