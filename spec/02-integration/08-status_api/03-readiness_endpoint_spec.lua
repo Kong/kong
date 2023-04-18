7211745c3b34424bd9269730f574b21b315b6f43
pluveto <i@pluvet.com>
2023-04-18 11:07:32 +0800

    have a dedicated test for off and another one for traditional/hybrid

--- spec/02-integration/08-status_api/03-readiness_endpoint_spec.lua
+++ spec/02-integration/08-status_api/03-readiness_endpoint_spec.lua
@@ -27,53 +27,15 @@ describe("Status API - with strategy #" .. strategy, function()
   end)
  
   describe("status readiness endpoint", function()
-    it("should return 503 when no config, return 200 in db mode", function()
+    it("should return 200 in db mode", function()
       local res = assert(client:send {
         method = "GET",
         path = "/status/ready",
       })
 
-      assert.res_status(strategy == "off" and 503 or 200, res)
+      assert.res_status(200, res)
 
     end)
-
-    if strategy == "off" then
-      it("should return 503 when no config, and return 200 after a valid config is uploaded", function()
-        local res = assert(client:send {
-          method = "GET",
-          path = "/status/ready",
-        })
-
-        assert.res_status(503, res)
-
-        local res = assert(admin_client:send {
-          method = "POST",
-          path = "/config",
-          body = {
-            config = [[
-              _format_version: "3.0"
-              services:
-              - name: test
-                url: http://mockbin.org
-            ]]
-          },
-          headers = {
-            ["Content-Type"] = "multipart/form-data"
-          },
-        })
-
-        assert.res_status(201, res)
-
-        ngx.sleep(3)
-
-        local res = assert(client:send {
-          method = "GET",
-          path = "/status/ready",
-        })
-
-        assert.res_status(200, res)
-      end)
-    end
 
     it("should return 200 after loading an invalid config following a previously uploaded valid config.", function()
       local res = assert(client:send {
@@ -98,6 +60,8 @@ describe("Status API - with strategy #" .. strategy, function()
       
       assert.res_status(400, res)
 
+      ngx.sleep(3)
+
       local res = assert(client:send {
         method = "GET",
         path = "/status/ready",
@@ -109,3 +73,110 @@ describe("Status API - with strategy #" .. strategy, function()
 
 end)
 end
+
+describe("Status API - with strategy #off", function()
+  local client
+  local admin_client
+
+  lazy_setup(function()
+    helpers.get_db_utils(nil, {}) -- runs migrations
+    assert(helpers.start_kong {
+      status_listen = "127.0.0.1:9500",
+      plugins = "admin-api-method",
+      database = strategy,
+      nginx_worker_processes = 8,
+    })
+    client = helpers.http_client("127.0.0.1", 9500, 20000)
+
+    admin_client = helpers.admin_client()
+  end)
+
+  lazy_teardown(function()
+    if client then
+      client:close()
+    end
+    helpers.stop_kong()
+  end)
+ 
+  describe("status readiness endpoint", function()
+    it("should return 503 when no config in dbless mode", function()
+      local res = assert(client:send {
+        method = "GET",
+        path = "/status/ready",
+      })
+
+      assert.res_status(503, res)
+
+    end)
+
+    it("should return 503 when no config, and return 200 after a valid config is uploaded", function()
+      local res = assert(client:send {
+        method = "GET",
+        path = "/status/ready",
+      })
+      
+      assert.res_status(503, res)
+      
+      local res = assert(admin_client:send {
+        method = "POST",
+        path = "/config",
+        body = {
+          config = [[
+          _format_version: "3.0"
+          services:
+          - name: test
+          url: http://mockbin.org
+          ]]
+        },
+        headers = {
+          ["Content-Type"] = "multipart/form-data"
+        },
+      })
+      
+      assert.res_status(201, res)
+      
+      ngx.sleep(3)
+      
+      local res = assert(client:send {
+        method = "GET",
+        path = "/status/ready",
+      })
+      
+      assert.res_status(200, res)
+    end)
+
+    it("should return 200 after loading an invalid config following a previously uploaded valid config.", function()
+      local res = assert(client:send {
+        method = "GET",
+        path = "/status/ready",
+      })
+
+      assert.res_status(200, res)
+
+      local res = assert(admin_client:send {
+        method = "POST",
+        path = "/config",
+        body = {
+          config = [[
+            _format"\!@#$
+          ]]
+        },
+        headers = {
+          ["Content-Type"] = "multipart/form-data"
+        },
+      })
+      
+      assert.res_status(400, res)
+
+      ngx.sleep(3)
+
+      local res = assert(client:send {
+        method = "GET",
+        path = "/status/ready",
+      })
+
+      assert.res_status(200, res)
+    end)
+  end)
+
+end)
