9af3e390117300e6f729a93f6285e8f32aa3189e
pluveto <i@pluvet.com>
2023-04-18 11:10:15 +0800

    Add a test to prove that the DP returns 200 after CP is shut down.

--- spec/02-integration/09-hybrid_mode/11-status_spec.lua
+++ spec/02-integration/09-hybrid_mode/11-status_spec.lua
@@ -8,24 +8,45 @@ for _, strategy in helpers.each_strategy() do
 
   describe("Hybrid Mode - status ready #" .. strategy, function()
 
-    helpers.get_db_utils(strategy, {
-    }) -- runs migrations
+    helpers.get_db_utils(strategy, {}) -- runs migrations
 
-    assert(helpers.start_kong({
-      role = "data_plane",
-      database = "off",
-      prefix = "servroot2",
-      cluster_cert = "spec/fixtures/kong_clustering.crt",
-      cluster_cert_key = "spec/fixtures/kong_clustering.key",
-      cluster_control_plane = "127.0.0.1:9005",
-      proxy_listen = "127.0.0.1:9002",
-      nginx_worker_processes = 8,
-      status_listen = "127.0.0.1:" .. dp_status_port,
-    }))
+    local function start_kong_dp()
+      helpers.start_kong({
+        role = "data_plane",
+        database = "off",
+        prefix = "servroot_dp",
+        cluster_cert = "spec/fixtures/kong_clustering.crt",
+        cluster_cert_key = "spec/fixtures/kong_clustering.key",
+        cluster_control_plane = "127.0.0.1:9005",
+        proxy_listen = "127.0.0.1:9002",
+        nginx_worker_processes = 8,
+        status_listen = "127.0.0.1:" .. dp_status_port,
+      })
+    end
 
+    lazy_setup(function()
+      assert(start_kong_dp())
+    
+      assert(helpers.start_kong({
+          role = "control_plane",
+          cluster_cert = "spec/fixtures/kong_clustering.crt",
+          cluster_cert_key = "spec/fixtures/kong_clustering.key",
+          database = strategy,
+          prefix = "servroot",
+          cluster_listen = "127.0.0.1:9005",
+          nginx_conf = "spec/fixtures/custom_nginx.template",
+
+          status_listen = "127.0.0.1:" .. cp_status_port
+      }))
+    end)
+
+    lazy_teardown(function()
+        assert(helpers.stop_kong("servroot"))
+        assert(helpers.stop_kong("servroot_dp"))
+    end)
     -- now dp should be not ready
 
-    describe("status ready endpoint", function()
+    describe("dp status ready endpoint for no config", function()
 
       it("returns 503 on data plane", function()
         helpers.wait_until(function()
@@ -44,22 +65,10 @@ for _, strategy in helpers.each_strategy() do
         end, 5)
       end)
     end)
-    
-    assert(helpers.start_kong({
-        role = "control_plane",
-        cluster_cert = "spec/fixtures/kong_clustering.crt",
-        cluster_cert_key = "spec/fixtures/kong_clustering.key",
-        database = strategy,
-        prefix = "servroot",
-        cluster_listen = "127.0.0.1:9005",
-        nginx_conf = "spec/fixtures/custom_nginx.template",
-
-        status_listen = "127.0.0.1:" .. cp_status_port
-    }))
 
     -- now cp should be ready
 
-    describe("status ready endpoint", function()
+    describe("cp status ready endpoint", function()
 
       it("returns 200 on control plane", function()
         helpers.wait_until(function()
@@ -81,9 +90,9 @@ for _, strategy in helpers.each_strategy() do
 
     -- now dp receive config from cp, so dp should be ready
 
-    describe("status ready endpoint", function()
+    describe("cp/dp status ready endpoint", function()
 
-      it("returns 200 on data plane", function()
+      it("should return 200 on data plane after configuring", function()
         helpers.wait_until(function()
           local http_client = helpers.http_client('127.0.0.1', dp_status_port)
 
@@ -98,12 +107,30 @@ for _, strategy in helpers.each_strategy() do
             return true
           end
         end, 10)
+
+        assert(helpers.stop_kong("servroot"))
+
+        -- DP should keep return 200 after CP is shut down.
+
+        ngx.sleep(5)
+
+        local http_client = helpers.http_client('127.0.0.1', dp_status_port)
+
+        local res = http_client:send({
+          method = "GET",
+          path = "/status/ready",
+        })
+
+        local status = res and res.status
+        http_client:close()
+        if status == 200 then
+          return true
+        end
+
+        assert(start_kong_dp()) -- recovery state between tests
+
       end)
     end)
 
-    lazy_teardown(function()
-        helpers.stop_kong("servroot")
-        helpers.stop_kong("servroot2")
-    end)
   end)
 end
