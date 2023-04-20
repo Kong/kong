f091b1bd01b895362553595d3fef4cdf1bf6f899
Qirui(Keery) Nie <windmgc@gmail.com>
2023-04-20 12:35:13 +0800

    feat(aws-lambda): support scheme config on lambda service api endpoint (#9799)
    * feat(aws-lambda): support scheme config on lambda service endpoint
    
    * add more error log
    
    * add local sam related tests
    
    * add changelog
    
    * rename new field to disable_https
    
    * do not install sam in ci
    
    * Install SAM tool in github actions when matrix suite is plugin test
    
    * fix github action step
    
    * add sam tool for c*3 as well
    
    * add comment
    
    * use sudo to install SAM tool

---
+++ spec/fixtures/sam-app/hello_world/app.py
@@ -0,0 +1,42 @@
+import json
+
+# import requests
+
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
