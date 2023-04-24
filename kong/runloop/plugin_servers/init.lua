fd4ae3b8a7bb98c1c905bd005c3ac8f6b4a80db4
suika <xumin.zhou@konghq.com>
2023-04-24 15:47:15 +0800

    simplify the logic

--- kong/runloop/plugin_servers/init.lua
+++ kong/runloop/plugin_servers/init.lua
@@ -40,6 +40,9 @@ else
   resp_get_headers = NOOP
 end
 
+local SLEEP_STEP = 0.1
+local WAIT_TIME = 10
+local MAX_WAIT_STEPS = WAIT_TIME / SLEEP_STEP
 
 --- keep request data a bit longer, into the log timer
 local save_for_later = {}
@@ -192,18 +195,15 @@ function get_instance_id(plugin_name, conf)
   local key = type(conf) == "table" and kong.plugin.get_id() or plugin_name
   local instance_info = running_instances[key]
 
-  local sleep_time = 0
   local wait_count = 0
   while instance_info and not instance_info.id do
     -- some other thread is already starting an instance
     -- prevent busy-waiting
-    ngx_sleep(sleep_time)
+    ngx_sleep(SLEEP_STEP)
   
     -- to prevent a potential dead loop when someone failed to release the ID
     wait_count = wait_count + 1
-    if wait_count > 10 then
-      sleep_time = 1
-    elseif wait_count > 20 then
+    if wait_count > MAX_WAIT_STEPS then
       return nil, "Could not claim instance_id for " .. plugin_name .. " (key: " .. key .. ")"
     end
     instance_info = running_instances[key]
