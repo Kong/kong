e0aabbbbb00ee306d9d3e289ad776a4b9ecf2964
Zhefeng Chen <chzf_zju@163.com>
2023-05-06 15:27:50 +0800

    add lua_ssl_protocols and fix tests

--- kong/templates/nginx_stream_inject.lua
+++ kong/templates/nginx_stream_inject.lua
@@ -3,4 +3,5 @@ lua_ssl_verify_depth   ${{LUA_SSL_VERIFY_DEPTH}};
 > if lua_ssl_trusted_certificate_combined then
 lua_ssl_trusted_certificate '${{LUA_SSL_TRUSTED_CERTIFICATE_COMBINED}}';
 > end
+lua_ssl_protocols ${{LUA_SSL_PROTOCOLS}};
 ]]
