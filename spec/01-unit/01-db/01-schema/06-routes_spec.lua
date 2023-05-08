c29c11d0b619490414f190e886c45529395b65a1
chronolaw <chrono_cpp@me.com>
2023-05-08 15:09:16 +0800

    change case for rust.regex 1.8

--- spec/01-unit/01-db/01-schema/06-routes_spec.lua
+++ spec/01-unit/01-db/01-schema/06-routes_spec.lua
@@ -1356,7 +1356,7 @@ describe("routes schema (flavor = traditional_compatible)", function()
       id             = a_valid_uuid,
       name           = "my_route",
       protocols      = { "http" },
-      paths          = { "~/\\/*/user$" },
+      paths          = { "~/[abc/*/user$" },
       service        = { id = another_uuid },
     }
     route = Routes:process_auto_fields(route, "insert")
@@ -1365,5 +1365,19 @@ describe("routes schema (flavor = traditional_compatible)", function()
     assert.truthy(errs["@entity"])
     assert.matches("Router Expression failed validation", errs["@entity"][1],
                    nil, true)
+  end)
+
+  it("don't fail when rust.regex update to 1.8", function()
+    local route = {
+      id             = a_valid_uuid,
+      name           = "my_route",
+      protocols      = { "http" },
+      paths          = { "~/\\/*/user$" },
+      service        = { id = another_uuid },
+    }
+    route = Routes:process_auto_fields(route, "insert")
+    local ok, errs = Routes:validate_insert(route)
+    assert.truthy(ok)
+    assert.falsy(errs["@entity"])
   end)
 end)
