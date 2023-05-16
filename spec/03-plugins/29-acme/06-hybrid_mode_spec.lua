e05283d652aefbefbe71ca87779841377df9f53e
Yufu Zhao <ms2008vip@gmail.com>
2023-05-16 11:55:38 +0800

    fix(acme): sanity test can work with "kong" storage in Hybrid mode  (#5389)
    * fix(acme): sanity test can work with "kong" storage in Hybrid mode (#10852)
    
    Currently, there is a technical limitation in hybrid mode, that is, the
    DP side cannot perform any write database operation. However, when the
    sanity test is executed and the account identifier information is needed
    to write to storage, we will skip this operation and return 200 directly in this
    situation. This will not have any substantial impact on the
    functionality.
    
    Fix FTI-4909
    
    * chore(*): add missing copyright header

---
+++ spec/03-plugins/29-acme/06-hybrid_mode_spec.lua
@@ -0,0 +1,85 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+local helpers = require "spec.helpers"
+
+for _, strategy in helpers.each_strategy({"postgres"}) do
+  describe("Plugin: acme (handler.access) worked with [#" .. strategy .. "]", function()
+    local domain = "mydomain.com"
+
+    lazy_setup(function()
+      local bp = helpers.get_db_utils(strategy, {
+        "services",
+        "routes",
+        "plugins",
+      }, { "acme", })
+
+      assert(bp.routes:insert {
+        paths = { "/" },
+      })
+
+      assert(bp.plugins:insert {
+        name = "acme",
+        config = {
+          account_email = "test@test.com",
+          api_uri = "https://api.acme.org",
+          domains = { domain },
+          storage = "kong",
+          storage_config = {
+            kong = {},
+          },
+        },
+      })
+
+      assert(helpers.start_kong({
+        role = "control_plane",
+        database = strategy,
+        cluster_cert = "spec/fixtures/kong_clustering.crt",
+        cluster_cert_key = "spec/fixtures/kong_clustering.key",
+        lua_ssl_trusted_certificate = "spec/fixtures/kong_clustering.crt",
+        cluster_listen = "127.0.0.1:9005",
+        cluster_telemetry_listen = "127.0.0.1:9006",
+        nginx_conf = "spec/fixtures/custom_nginx.template",
+      }))
+
+      assert(helpers.start_kong({
+        role = "data_plane",
+        database = "off",
+        prefix = "servroot2",
+        cluster_cert = "spec/fixtures/kong_clustering.crt",
+        cluster_cert_key = "spec/fixtures/kong_clustering.key",
+        lua_ssl_trusted_certificate = "spec/fixtures/kong_clustering.crt",
+        cluster_control_plane = "127.0.0.1:9005",
+        cluster_telemetry_endpoint = "127.0.0.1:9006",
+        proxy_listen = "0.0.0.0:9002",
+      }))
+    end)
+
+    lazy_teardown(function()
+      helpers.stop_kong("servroot2")
+      helpers.stop_kong()
+    end)
+
+    it("sanity test works with \"kong\" storage in Hybrid mode", function()
+      local proxy_client = helpers.http_client("127.0.0.1", 9002)
+      helpers.wait_until(function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/.well-known/acme-challenge/x",
+          headers =  { host = domain }
+        })
+
+        if res.status ~= 404 then
+          return false
+        end
+
+        local body = res:read_body()
+        return body == "Not found\n"
+      end, 10)
+      proxy_client:close()
+    end)
+  end)
+end
