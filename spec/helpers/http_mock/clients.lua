fbb3a4d27b59784ac39ceaa88aae8915770995b1
suika <xumin.zhou@konghq.com>
2023-05-17 17:45:22 +0800

    test(*): implement new HTTP mocking
    Fix KAG-1148
    
    apply suggestions
    
    Co-authored-by: Chrono <chrono_cpp@me.com>
    Co-authored-by: Hans Hübner <hans.huebner@gmail.com>

---
+++ spec/helpers/http_mock/clients.lua
@@ -0,0 +1,22 @@
+local helpers = require "spec.helpers"
+local http_client = helpers.http_client
+
+local http_mock = {}
+
+-- we need to get rid of dependence to the "helpers"
+function http_mock:get_client()
+  local client = self.client
+  if not client then
+      client = http_client({
+        scheme = self.client_opts.tls and "https" or "http",
+        host = "localhost", 
+        port = self.client_opts.port,
+      })
+
+    self.client = client
+  end
+
+  return client
+end
+
+return http_mock
