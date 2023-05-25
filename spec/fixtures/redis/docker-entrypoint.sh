148602775947ffaea79412ec8dfa4e0f4f525714
Zachary Hu <6426329+outsinre@users.noreply.github.com>
2023-05-25 17:59:33 +0800

    tests(plugin): improve CE rate-limiting test (#10939) (#5527)

--- spec/fixtures/redis/docker-entrypoint.sh
+++ spec/fixtures/redis/docker-entrypoint.sh
@@ -7,6 +7,7 @@ if [ -d /workspace ] ;  then
     echo "Starting test server..."
 
     redis-server                                                    \
+        --port 6379                                                 \
         --tls-port 6380                                             \
         --tls-cert-file /workspace/spec/fixtures/redis/server.crt   \
         --tls-key-file /workspace/spec/fixtures/redis/server.key    \
