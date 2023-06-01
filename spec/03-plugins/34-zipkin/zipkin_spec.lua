Kong/kong

commit 020579731ef3bc5f905ced79cb4ee4c9eb3bc7f5

fix(otel): spans hierarchy + sampled flags and propagation (#10663)
* fix(otel): spans hierarchy

addresses span hierarchy for otel, similarly to what
https://github.com/Kong/kong-ee/pull/4904 did for dd

* fix(otel): sampled flags and propagation

address propagation of non-sampled spans, similarly to what
https://github.com/Kong/kong-ee/pull/5054 did for dd

* fix(otel): move some code from plugin to core

kong/tracing/propagation.lua
@@ -1,13 +1,12 @@
 local to_hex = require "resty.string".to_hex
 local table_merge = require "kong.tools.utils".table_merge
-local otlp = require "kong.plugins.opentelemetry.otlp"
 local unescape_uri = ngx.unescape_uri
 local char = string.char
 local match = string.match
 local gsub = string.gsub
 local fmt = string.format
 local concat = table.concat
-local to_ot_trace_id = otlp.to_ot_trace_id
+local to_ot_trace_id
 
 
 local baggage_mt = {
@@ -441,11 +440,33 @@ local function parse(headers, conf_header_type)
 end
 
 
-local function set(conf_header_type, found_header_type, proxy_span, conf_default_header_type)
+local function get_propagated()
+  return ngx.ctx.propagated_span
+end
+
+
+local function set_propagated(span)
+  ngx.ctx.propagated_span = span
+end
+
+
+-- set outgoing propagation headers
+-- 
+-- @tparam string conf_header_type type of tracing header to use
+-- @tparam string found_header_type type of tracing header found in request
+-- @tparam table proxy_span span to be propagated
+-- @tparam string conf_default_header_type used when conf_header_type=ignore
+-- @tparam bool reuse if true any existing propagated_span is reused instead of proxy_span
+local function set(conf_header_type, found_header_type, proxy_span, conf_default_header_type, reuse)
+  if reuse then
+    proxy_span = get_propagated() or proxy_span
+  end
+  -- proxy_span can be noop, in which case it should not be propagated.
   if proxy_span.is_recording == false then
     kong.log.debug("skipping propagation of noop span")
     return
   end
+  set_propagated(proxy_span)
 
   local set_header = kong.service.request.set_header
 
@@ -500,6 +521,7 @@ local function set(conf_header_type, found_header_type, proxy_span, conf_default
   end
 
   if conf_header_type == "ot" or found_header_type == "ot" then
+    to_ot_trace_id = to_ot_trace_id or require "kong.plugins.opentelemetry.otlp".to_ot_trace_id
     set_header("ot-tracer-traceid", to_hex(to_ot_trace_id(proxy_span.trace_id)))
     set_header("ot-tracer-spanid", to_hex(proxy_span.span_id))
     set_header("ot-tracer-sampled", proxy_span.should_sample and "1" or "0")
@@ -520,4 +542,5 @@ return {
   parse = parse,
   set = set,
   from_hex = from_hex,
+  get_propagated = get_propagated,
 }

kong/pdk/tracing.lua
@@ -203,6 +203,7 @@ local function link_span(tracer, span, name, options)
   span.start_time_ns = options.start_time_ns or ffi_time_unix_nano()
   span.attributes = options.attributes
   span.name = name
+  span.linked = true
 
   -- insert the span to ctx
   local ctx = ngx.ctx

kong/plugins/opentelemetry/handler.lua
@@ -93,10 +93,10 @@ end
 function OpenTelemetryHandler:access(conf)
   local headers = ngx_get_headers()
   local root_span = ngx.ctx.KONG_SPANS and ngx.ctx.KONG_SPANS[1]
+  local tracer = kong.tracing.new("otel")
 
   -- make propagation running with tracing instrumetation not enabled
   if not root_span then
-    local tracer = kong.tracing.new("otel")
     root_span = tracer.start_span("root")
 
     -- the span created only for the propagation and will be bypassed to the exporter
@@ -105,7 +105,7 @@ function OpenTelemetryHandler:access(conf)
 
   local header_type, trace_id, span_id, parent_id, should_sample, _ = propagation_parse(headers, conf.header_type)
   if should_sample == false then
-    root_span.should_sample = should_sample
+    tracer:set_should_sample(should_sample)
   end
 
   -- overwrite trace id
@@ -123,7 +123,14 @@ function OpenTelemetryHandler:access(conf)
     root_span.parent_id = parent_id
   end
 
-  propagation_set(conf.header_type, header_type, root_span, "w3c")
+  -- propagate the span that identifies the "last" balancer try
+  -- This has to happen "in advance". The span will be activated (linked)
+  -- after the OpenResty balancer results are available
+  local balancer_span = tracer.create_span(nil, {
+    span_kind = 3,
+    parent = root_span,
+  })
+  propagation_set(conf.header_type, header_type, balancer_span, "w3c", true)
 end
 
 

kong/tracing/instrumentation.lua
@@ -1,4 +1,5 @@
 local pdk_tracer = require "kong.pdk.tracing".new()
+local propagation = require "kong.tracing.propagation"
 local utils = require "kong.tools.utils"
 local tablepool = require "tablepool"
 local tablex = require "pl.tablex"
@@ -82,6 +83,15 @@ function _M.balancer(ctx)
   local try_count = balancer_data.try_count
   local upstream_connect_time = split(var.upstream_connect_time, ", ", "jo")
 
+  local last_try_balancer_span
+  do
+    local propagated = propagation.get_propagated()
+    -- pre-created balancer span was not linked yet
+    if propagated and not propagated.linked then
+      last_try_balancer_span = propagated
+    end
+  end
+
   for i = 1, try_count do
     local try = balancer_tries[i]
     local span_name = "balancer try #" .. i
@@ -95,7 +105,7 @@ function _M.balancer(ctx)
     }
 
     -- one of the unsuccessful tries
-    if i < try_count or try.state ~= nil or not ctx.last_try_balancer_span then
+    if i < try_count or try.state ~= nil or not last_try_balancer_span then
       span = tracer.start_span(span_name, span_options)
 
       if try.state then
@@ -112,7 +122,7 @@ function _M.balancer(ctx)
 
     else
       -- last try: load the last span (already created/propagated)
-      span = ctx.last_try_balancer_span
+      span = last_try_balancer_span
       tracer:link_span(span, span_name, span_options)
 
       if try.state then

spec/01-unit/26-tracing/01-tracer_pdk_spec.lua
@@ -176,6 +176,7 @@ describe("Tracer PDK", function()
         attributes = {
           "key1", "value1"
         },
+        linked = true,
       }
 
       span = c_tracer.start_span("meow", tpl)

spec/03-plugins/37-opentelemetry/03-propagation_spec.lua
@@ -22,9 +22,14 @@ describe("propagation tests #" .. strategy, function()
   local proxy_client
 
   lazy_setup(function()
-    local bp = helpers.get_db_utils(strategy, { "services", "routes", "plugins" })
+    local bp = helpers.get_db_utils(strategy, { "services", "routes", "plugins" }, { "trace-propagator" })
 
     service = bp.services:insert()
+
+    local multi_plugin_route = bp.routes:insert({
+      hosts = { "multi-plugin" },
+      service = service,
+    })
 
     bp.plugins:insert({
       name = "opentelemetry",
@@ -64,8 +69,23 @@ describe("propagation tests #" .. strategy, function()
       }
     })
 
+    bp.plugins:insert({
+      name = "trace-propagator",
+      route = multi_plugin_route,
+    })
+
+    bp.plugins:insert({
+      name = "opentelemetry",
+      route = multi_plugin_route,
+      config = {
+        endpoint = "http://localhost:8080/v1/traces",
+        header_type = "ignore",
+      }
+    })
+
     helpers.start_kong({
       database = strategy,
+      plugins = "bundled, trace-propagator",
       nginx_conf = "spec/fixtures/custom_nginx.template",
     })
 
@@ -132,6 +152,21 @@ describe("propagation tests #" .. strategy, function()
       local body = assert.response(r).has.status(200)
       local json = cjson.decode(body)
       assert.matches(trace_id .. "%-%x+%-1", json.headers.b3)
+    end)
+
+    it("with disabled sampling", function()
+      local trace_id = gen_trace_id()
+      local span_id = gen_span_id()
+
+      local r = proxy_client:get("/", {
+        headers = {
+          b3 = fmt("%s-%s-0", trace_id, span_id),
+          host = "http-route",
+        },
+      })
+      local body = assert.response(r).has.status(200)
+      local json = cjson.decode(body)
+      assert.matches(trace_id .. "%-%x+%-0", json.headers.b3)
     end)
   end)
 
@@ -212,7 +247,7 @@ describe("propagation tests #" .. strategy, function()
     local body = assert.response(r).has.status(200)
     local json = cjson.decode(body)
     -- Trace ID is left padded with 0 for assert
-    assert.matches( ('0'):rep(32-#trace_id) .. trace_id .. ":%x+:" .. span_id .. ":01", json.headers["uber-trace-id"])
+    assert.matches( ('0'):rep(32-#trace_id) .. trace_id .. ":%x+:%x+:01", json.headers["uber-trace-id"])
   end)
 
   it("propagates ot headers", function()
@@ -248,6 +283,25 @@ describe("propagation tests #" .. strategy, function()
     assert.same(32, #m[1])
     assert.same(16, #m[2])
     assert.same("01", m[3])
+  end)
+
+  it("reuses span propagated by another plugin", function()
+    local trace_id = gen_trace_id()
+
+    local r = proxy_client:get("/", {
+      headers = {
+        ["x-b3-sampled"] = "1",
+        ["x-b3-traceid"] = trace_id,
+        host = "multi-plugin",
+      },
+    })
+    local body = assert.response(r).has.status(200)
+    local json = cjson.decode(body)
+
+    -- trace-propagator parses incoming b3 headers, generates a span and
+    -- propagates it as b3. Opentelemetry ignores incoming type, reuses span
+    -- generated by the other plugin and propagates it as w3c.
+    assert.matches("00%-%x+-" .. json.headers["x-b3-spanid"] .. "%-01", json.headers.traceparent)
   end)
 end)
 end

spec/fixtures/custom_plugins/kong/plugins/trace-propagator/handler.lua
@@ -33,17 +33,12 @@ function _M:access(conf)
     root_span.parent_id = parent_id
   end
 
-  local new_span = ngx.ctx.last_try_balancer_span
-  if new_span == nil then
-    new_span = tracer.create_span(nil, {
-      span_kind = 3,
-      parent = root_span,
-    })
-    ngx.ctx.last_try_balancer_span = new_span
-  end
-
+  local balancer_span = tracer.create_span(nil, {
+    span_kind = 3,
+    parent = root_span,
+  })
   local type = header_type and "preserve" or "w3c"
-  propagation_set(type, header_type, new_span, "trace-propagator")
+  propagation_set(type, header_type, balancer_span, "w3c", true)
 end
 
 return _M

commit 9d704eb6145b54da26cb090e9bb36bc729691091

fix(zipkin): do not reuse `propagated_span` by default
Zipkin manages its spans and calls `finish` on them, and the result is
that `finish` is called twice if instrumentations are enabled. Then an
assertion error is generated, resulting in the span not being sent
correctly. This was an issue introduced with
https://github.com/Kong/kong/pull/10663

So my approach is not to store the propagation span if the reuse
parameter is not specified, in order to avoid the traces not being
generated correctly under that.

kong/tracing/propagation.lua
@@ -451,7 +451,7 @@ end
 
 
 -- set outgoing propagation headers
--- 
+--
 -- @tparam string conf_header_type type of tracing header to use
 -- @tparam string found_header_type type of tracing header found in request
 -- @tparam table proxy_span span to be propagated
@@ -466,7 +466,9 @@ local function set(conf_header_type, found_header_type, proxy_span, conf_default
     kong.log.debug("skipping propagation of noop span")
     return
   end
-  set_propagated(proxy_span)
+  if reuse then
+    set_propagated(proxy_span)
+  end
 
   local set_header = kong.service.request.set_header
 

spec/03-plugins/34-zipkin/zipkin_spec.lua
@@ -1474,3 +1474,69 @@ for _, strategy in helpers.each_strategy() do
 
   end)
 end
+
+for _, strategy in helpers.each_strategy() do
+  describe("can works with OTel", function()
+    local proxy_client, zipkin_client, service
+
+    setup(function()
+      local bp = helpers.get_db_utils(strategy, { "services", "routes", "plugins" })
+
+      service = bp.services:insert {
+        name = string.lower("http-" .. utils.random_string()),
+      }
+
+      -- kong (http) mock upstream
+      bp.routes:insert({
+        name = string.lower("route-" .. utils.random_string()),
+        service = service,
+        hosts = { "http-route" },
+        preserve_host = true,
+      })
+
+      -- enable zipkin plugin globally, with sample_ratio = 0
+      bp.plugins:insert({
+        name = "zipkin",
+        config = {
+          sample_ratio = 0,
+          http_endpoint = fmt("http://%s:%d/api/v2/spans", ZIPKIN_HOST, ZIPKIN_PORT),
+          default_header_type = "b3-single",
+        }
+      })
+
+      helpers.start_kong({
+        database = strategy,
+        nginx_conf = "spec/fixtures/custom_nginx.template",
+        tracing_instrumentations = "all",
+        tracing_sampling_rate = 1,
+      })
+
+      proxy_client = helpers.proxy_client()
+      zipkin_client = helpers.http_client(ZIPKIN_HOST, ZIPKIN_PORT)
+    end)
+
+    teardown(function()
+      helpers.stop_kong()
+    end)
+
+    it("generates traces when instrumentations are enabled", function()
+      local start_s = ngx.now()
+
+      local r = proxy_client:get("/", {
+        headers = {
+          ["x-b3-sampled"] = "1",
+          host  = "http-route",
+          ["zipkin-tags"] = "foo=bar; baz=qux"
+        },
+      })
+      assert.response(r).has.status(200)
+
+      local spans = wait_for_spans(zipkin_client, 3, service.name)
+      local request_span = assert(get_span("get", spans), "request span missing")
+      local proxy_span = assert(get_span("get (proxy)", spans), "proxy span missing")
+
+      -- common assertions for request_span and proxy_span
+      assert_span_invariants(request_span, proxy_span, 16 * 2, start_s, "kong")
+    end)
+  end)
+end