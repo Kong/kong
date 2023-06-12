16d4b1729ce84e90e8f8ed011e2d7ffba9d17505
Yusheng Li <leeys.top@gmail.com>
2023-06-12 10:58:24 +0800

    Update kong/tools/mime_type.lua
    Co-authored-by: Hans Hübner <hans.huebner@gmail.com>

--- kong/tools/mime_type.lua
+++ kong/tools/mime_type.lua
@@ -118,7 +118,7 @@ local function includes(this, other)
 
     -- considering included when this_subtype does not contain a suffix and is the suffix of other_subtype
     if not find(this.subtype, "+", nil, true) then -- this_subtype does not contain suffix
-      if match(other.subtype, this.subtype .. "$") then -- suffix match
+      if match(other.subtype, "+" .. this.subtype .. "$") then -- suffix match
         return true
       end
     end
