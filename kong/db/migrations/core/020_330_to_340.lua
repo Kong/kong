93a89916305eb003bb059dc1ecf37c1b85646ef1
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2023-06-13 16:31:40 +0300

    Revert "fix(migration): migrate queue parameters" (#11061)
    This reverts commit a7bf026a5cf91e376f8cb820e6d8923365f2d581.

--- kong/db/migrations/core/020_330_to_340.lua
+++
@@ -1,6 +0,0 @@
-local queue_parameter_migration_340 = require('kong.db.migrations.core.queue_parameter_migration_340')
-return {
-  postgres = {
-    up = queue_parameter_migration_340,
-  }
-}
--- spec/05-migration/db/migrations/core/020_330_to_340_spec.lua
+++
@@ -1,4 +0,0 @@
-
-describe("database migration", function()
-  -- This is a placeholder at this point.  The queue related data migration is tested using an integration test.
-end)
