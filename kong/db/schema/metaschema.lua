82908d119d6c10fd0e89189c62c55a9077d5fd70
Joshua Schmid <jaiks@posteo.de>
2023-06-29 10:22:29 +0200

    fix: revert description len back to original
    Signed-off-by: Joshua Schmid <jaiks@posteo.de>

--- kong/db/schema/metaschema.lua
+++ kong/db/schema/metaschema.lua
@@ -79,7 +79,7 @@ local field_schema = {
   { type = { type = "string", one_of = keys(Schema.valid_types), required = true }, },
   { required = { type = "boolean" }, },
   { reference = { type = "string" }, },
-  { description = { type = "string", len_min = 1, len_max = 5000}, },
+  { description = { type = "string", len_min = 10, len_max = 500}, },
   { auto = { type = "boolean" }, },
   { unique = { type = "boolean" }, },
   { unique_across_ws = { type = "boolean" }, },
