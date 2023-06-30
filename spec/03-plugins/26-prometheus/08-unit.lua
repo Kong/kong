5a0bd8308c495611443a86b320c80550f517d4e4
chronolaw <chrono_cpp@me.com>
2023-06-30 17:23:21 +0800

    check metric label names

--- spec/03-plugins/26-prometheus/08-unit.lua
+++ spec/03-plugins/26-prometheus/08-unit.lua
@@ -15,8 +15,9 @@ describe("Plugin: prometheus (unit)",function()
 
   it("check metric names", function()
     local prom = prometheus.init("prometheus_metrics", "kong_")
+    local m
 
-    local m = prom:counter("mem_used")
+    m = prom:counter("mem_used")
     assert.truthy(m)
 
     m = prom:counter("Mem_Used")
@@ -40,4 +41,37 @@ describe("Plugin: prometheus (unit)",function()
     m = prom:counter("name$")
     assert.falsy(m)
   end)
+
+  it("check metric label names", function()
+    local prom = prometheus.init("prometheus_metrics", "kong_")
+    local m
+
+    m = prom:counter("mem0", nil, {"LUA"})
+    assert.truthy(m)
+
+    m = prom:counter("mem1", nil, {"lua"})
+    assert.truthy(m)
+
+    m = prom:counter("mem2", nil, {"_lua_"})
+    assert.truthy(m)
+
+    m = prom:counter("mem3", nil, {":lua"})
+    assert.falsy(m)
+
+    m = prom:counter("mem4", nil, {"0lua"})
+    assert.falsy(m)
+
+    m = prom:counter("mem5", nil, {"lua*"})
+    assert.falsy(m)
+
+    m = prom:counter("mem6", nil, {"lua\\5.1"})
+    assert.falsy(m)
+
+    m = prom:counter("mem7", nil, {"lua\"5.1\""})
+    assert.falsy(m)
+
+    m = prom:counter("mem8", nil, {"lua-vm"})
+    assert.falsy(m)
+  end)
+
 end)
