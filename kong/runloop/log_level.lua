e0cf19322fe898e61a3f07c80d81729ebed83226
chronolaw <chrono_cpp@me.com>
2023-07-02 11:07:13 +0800

    log_level.lua

--- kong/runloop/log_level.lua
+++ kong/runloop/log_level.lua
@@ -13,9 +13,6 @@ local constants   = require("kong.constants")
 local kong_log    = require("resty.kong.log")
 
 
-local LOG_LEVELS  = require("kong.constants").LOG_LEVELS
-
-
 local ngx    = ngx
 local log    = ngx.log
 local ERR    = ngx.ERR
@@ -33,10 +30,14 @@ local function set_log_level(worker, level, timeout)
 end
 
 
-local function init()
-  local cur_log_level = kong_log.get_log_level(LOG_LEVELS[kong.configuration.log_level])
+local function init_handler()
   local shm_log_level = ngx.shared.kong:get(constants.DYN_LOG_LEVEL_KEY)
-  local timeout = (tonumber(ngx.shared.kong:get(constants.DYN_LOG_LEVEL_TIMEOUT_AT_KEY)) or 0) - ngx.time()
+
+  local cur_log_level = kong_log.get_log_level(
+                          constants.LOG_LEVELS[kong.configuration.log_level])
+  local timeout = (tonumber(
+                    ngx.shared.kong:get(constants.DYN_LOG_LEVEL_TIMEOUT_AT_KEY)) or 0)
+                  - ngx.time()
 
   if shm_log_level and cur_log_level ~= shm_log_level and timeout > 0 then
     set_log_level(ngx.worker.id(), shm_log_level, timeout)
@@ -63,7 +64,7 @@ local function cluster_handler(data)
 end
 
 
-local function node_handler(data)
+local function worker_handler(data)
   local worker = ngx.worker.id()
 
   log(NOTICE, "log level worker event received for worker ", worker)
@@ -73,11 +74,11 @@ end
 
 
 local function init_worker()
-  ngx.timer.at(0, init)
+  ngx.timer.at(0, init_handler)
 
   kong.cluster_events:subscribe("log_level", cluster_handler)
 
-  kong.worker_events.register(node_handler, "debug", "log_level")
+  kong.worker_events.register(worker_handler, "debug", "log_level")
 end
 
 
