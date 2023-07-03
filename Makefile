702674a16fb4e9ed749a67e491531ceca7335fdf
Hans Hübner <hans.huebner@gmail.com>
2023-07-03 16:46:41 +0200

    Revert "fix(ci): remove Lua cURL and CI modifications because of it (#10599)" (#11167)
    This reverts commit fa2c37192382582a8b2946e90564f509dd979b29.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 OS := $(shell uname | awk '{print tolower($$0)}')
 MACHINE := $(shell uname -m)
 
-DEV_ROCKS = "busted 2.1.2" "busted-htest 1.0.0" "luacheck 1.1.1" "lua-llthreads2 0.1.6" "http 0.4" "ldoc 1.5.0" "luacov 0.15.0"
+DEV_ROCKS = "busted 2.1.2" "busted-htest 1.0.0" "luacheck 1.1.1" "lua-llthreads2 0.1.6" "http 0.4" "ldoc 1.5.0" "luacov 0.15.0" "Lua-cURL 0.3.13"
 WIN_SCRIPTS = "bin/busted" "bin/kong" "bin/kong-health"
 BUSTED_ARGS ?= -v
 TEST_CMD ?= bin/busted $(BUSTED_ARGS)
@@ -12,10 +12,12 @@ ifeq ($(OS), darwin)
 OPENSSL_DIR ?= $(shell brew --prefix)/opt/openssl
 GRPCURL_OS ?= osx
 YAML_DIR ?= $(shell brew --prefix)/opt/libyaml
+CURL_INCDIR ?= $(shell brew --prefix)/opt/curl/include
 else
 OPENSSL_DIR ?= /usr
 GRPCURL_OS ?= $(OS)
 YAML_DIR ?= /usr
+CURL_INCDIR ?= /usr/include/x86_64-linux-gnu
 endif
 
 ifeq ($(MACHINE), aarch64)
@@ -84,7 +86,7 @@ install-dev-rocks: build-venv
 	  else \
 		echo $$rock not found, installing via luarocks... ; \
 		LIBRARY_PREFIX=$$(pwd)/bazel-bin/build/$(BUILD_NAME)/kong ; \
-		luarocks install $$rock OPENSSL_DIR=$$LIBRARY_PREFIX CRYPTO_DIR=$$LIBRARY_PREFIX YAML_DIR=$(YAML_DIR) || exit 1; \
+		luarocks install $$rock OPENSSL_DIR=$$LIBRARY_PREFIX CRYPTO_DIR=$$LIBRARY_PREFIX YAML_DIR=$(YAML_DIR) CURL_INCDIR=$(CURL_INCDIR) || exit 1; \
 	  fi \
 	done;
 
@@ -167,11 +169,11 @@ dependencies: bin/grpcurl
 		echo $$rock already installed, skipping ; \
 	  else \
 		echo $$rock not found, installing via luarocks... ; \
-		luarocks install $$rock OPENSSL_DIR=$(OPENSSL_DIR) CRYPTO_DIR=$(OPENSSL_DIR) YAML_DIR=$(YAML_DIR) || exit 1; \
+		luarocks install $$rock OPENSSL_DIR=$(OPENSSL_DIR) CRYPTO_DIR=$(OPENSSL_DIR) YAML_DIR=$(YAML_DIR) CURL_INCDIR=$(CURL_INCDIR) || exit 1; \
 	  fi \
 	done;
 
 install-legacy:
-	@luarocks make OPENSSL_DIR=$(OPENSSL_DIR) CRYPTO_DIR=$(OPENSSL_DIR) YAML_DIR=$(YAML_DIR)
+	@luarocks make OPENSSL_DIR=$(OPENSSL_DIR) CRYPTO_DIR=$(OPENSSL_DIR) YAML_DIR=$(YAML_DIR) CURL_INCDIR=$(CURL_INCDIR)
 
 dev-legacy: remove install-legacy dependencies
--- spec/02-integration/05-proxy/27-unbuffered_spec.lua
+++ spec/02-integration/05-proxy/27-unbuffered_spec.lua
@@ -1,6 +1,8 @@
 local helpers = require "spec.helpers"
 local random = require "resty.random"
 local rstring = require "resty.string"
+local curl = require("cURL")
+
 
 -- HTTP 1.1 Chunked Body (5 MB)
 local function chunked_random_body()
@@ -19,6 +21,55 @@ local function chunked_random_body()
 
     return chunk
   end
+end
+
+
+--- Curl HTTP Body (5 MB)
+local function ramdom_body()
+  return rstring.to_hex(random.bytes(5*1024*1024/2))
+end
+
+
+local HTTP_VERSIONS = {
+  CURL_HTTP_VERSION_NONE = 0,
+  CURL_HTTP_VERSION_1_0 = 1,
+  CURL_HTTP_VERSION_1_1 = 2,
+  CURL_HTTP_VERSION_2_0 = 3,
+  CURL_HTTP_VERSION_2TLS = 4,
+  CURL_HTTP_VERSION_2_PRIOR_KNOWLEDGE = 5
+}
+
+
+local function curl_post(url, body, http_version)
+  http_version = http_version or HTTP_VERSIONS["CURL_HTTP_VERSION_2_0"]
+
+  local c = curl.easy{
+    url = url,
+    ssl_verifypeer = false,
+    ssl_verifyhost = false,
+    post = true,
+    postfields = body,
+    --[curl.OPT_VERBOSE] = true,
+    [curl.OPT_HEADER] = true,
+    [curl.OPT_HTTP_VERSION] = http_version,
+  }
+  local response = {}
+  c:setopt_writefunction(table.insert, response)
+  c:perform()
+
+  local status  = c:getinfo_response_code()
+  local full_response = table.concat(response)
+  local raw_headers = string.sub(full_response, 1, c:getinfo(curl.INFO_HEADER_SIZE))
+  local return_body = string.sub(full_response, c:getinfo(curl.INFO_HEADER_SIZE))
+
+  --parse headers
+  local return_headers = {}
+  for header in string.gmatch(raw_headers, "[%w%-]+:[^\n]+") do
+    local index = string.find(header, ":")
+    return_headers[string.lower(string.sub(header, 1, index-1))] = string.sub(header, index+2)
+  end
+
+  return status, return_headers, return_body
 end
 
 
@@ -272,197 +323,142 @@ for _, strategy in helpers.each_strategy() do
 end
 
 
-if false then
-  -- TODO: needs Lua cURL and that needs curl headers
-  -- TODO: removed because it makes tests red on master
-  local curl = require("cURL") -- luacheck: ignore
+for _, version in pairs({ "CURL_HTTP_VERSION_1_1", "CURL_HTTP_VERSION_2_0" }) do
+  local http_version = HTTP_VERSIONS[version]
 
+  for _, strategy in helpers.each_strategy() do
+    describe("HTTP #" .. version .. " buffered requests/response [#" .. strategy .. "]", function()
+      local warmup_client
+      local base_url
+      local proxy_ip
+      local proxy_port
 
-  --- Curl HTTP Body (5 MB)
-  local function ramdom_body()
-    return rstring.to_hex(random.bytes(5*1024*1024/2))
-  end
+      lazy_setup(function()
+        local bp = helpers.get_db_utils(strategy, {
+          "routes",
+          "services"
+        })
 
+        local service = bp.services:insert()
 
-  local HTTP_VERSIONS = {
-    CURL_HTTP_VERSION_NONE = 0,
-    CURL_HTTP_VERSION_1_0 = 1,
-    CURL_HTTP_VERSION_1_1 = 2,
-    CURL_HTTP_VERSION_2_0 = 3,
-    CURL_HTTP_VERSION_2TLS = 4,
-    CURL_HTTP_VERSION_2_PRIOR_KNOWLEDGE = 5
-  }
+        bp.routes:insert({
+          protocols = { "http", "https" },
+          paths = { "/buffered" },
+          request_buffering = true,
+          response_buffering = true,
+          service = service,
+        })
 
+        bp.routes:insert({
+          protocols = { "http", "https" },
+          paths = { "/unbuffered" },
+          request_buffering = false,
+          response_buffering = false,
+          service = service,
+        })
 
-  local function curl_post(url, body, http_version)
-    http_version = http_version or HTTP_VERSIONS["CURL_HTTP_VERSION_2_0"]
+        bp.routes:insert({
+          protocols = { "http", "https" },
+          paths = { "/unbuffered-request" },
+          request_buffering = false,
+          response_buffering = true,
+          service = service,
+        })
 
-    local c = curl.easy{
-      url = url,
-      ssl_verifypeer = false,
-      ssl_verifyhost = false,
-      post = true,
-      postfields = body,
-      --[curl.OPT_VERBOSE] = true,
-      [curl.OPT_HEADER] = true,
-      [curl.OPT_HTTP_VERSION] = http_version,
-    }
-    local response = {}
-    c:setopt_writefunction(table.insert, response)
-    c:perform()
+        bp.routes:insert({
+          protocols = { "http", "https" },
+          paths = { "/unbuffered-response" },
+          request_buffering = true,
+          response_buffering = false,
+          service = service,
+        })
 
-    local status  = c:getinfo_response_code()
-    local full_response = table.concat(response)
-    local raw_headers = string.sub(full_response, 1, c:getinfo(curl.INFO_HEADER_SIZE))
-    local return_body = string.sub(full_response, c:getinfo(curl.INFO_HEADER_SIZE))
+        assert(helpers.start_kong {
+          database = strategy,
+          nginx_conf = "spec/fixtures/custom_nginx.template",
+        })
 
-    --parse headers
-    local return_headers = {}
-    for header in string.gmatch(raw_headers, "[%w%-]+:[^\n]+") do
-      local index = string.find(header, ":")
-      return_headers[string.lower(string.sub(header, 1, index-1))] = string.sub(header, index+2)
-    end
+      end)
 
-    return status, return_headers, return_body
-  end
+      lazy_teardown(function()
+        warmup_client:close()
+        helpers.stop_kong()
+      end)
 
+      before_each(function()
+        proxy_ip = helpers.get_proxy_ip(true, true)
+        proxy_port = helpers.get_proxy_port(true, true)
+        warmup_client = helpers.proxy_client()
+        base_url = "https://" .. proxy_ip .. ":" .. proxy_port
+      end)
 
-  for _, version in pairs({ "CURL_HTTP_VERSION_1_1", "CURL_HTTP_VERSION_2_0" }) do
-    local http_version = HTTP_VERSIONS[version]
-    for _, strategy in helpers.each_strategy() do
-      describe("HTTP #" .. version .. " buffered requests/response [#" .. strategy .. "]", function()
-        local warmup_client
-        local base_url
-        local proxy_ip
-        local proxy_port
+      describe("request latency", function()
+        local buffered_latency
+        local unbuffered_latency
+        local unbuffered_request_latency
+        local unbuffered_response_latency
+        local status, headers, _
 
-        lazy_setup(function()
-          local bp = helpers.get_db_utils(strategy, {
-            "routes",
-            "services"
-          })
-
-          local service = bp.services:insert()
-
-          bp.routes:insert({
-            protocols = { "http", "https" },
-            paths = { "/buffered" },
-            request_buffering = true,
-            response_buffering = true,
-            service = service,
-          })
-
-          bp.routes:insert({
-            protocols = { "http", "https" },
-            paths = { "/unbuffered" },
-            request_buffering = false,
-            response_buffering = false,
-            service = service,
-          })
-
-          bp.routes:insert({
-            protocols = { "http", "https" },
-            paths = { "/unbuffered-request" },
-            request_buffering = false,
-            response_buffering = true,
-            service = service,
-          })
-
-          bp.routes:insert({
-            protocols = { "http", "https" },
-            paths = { "/unbuffered-response" },
-            request_buffering = true,
-            response_buffering = false,
-            service = service,
-          })
-
-          assert(helpers.start_kong {
-            database = strategy,
-            nginx_conf = "spec/fixtures/custom_nginx.template",
-          })
-
+        it("is calculated for buffered", function()
+          warmup_client:post("/buffered/post", { body = "warmup" })
+          status, headers, _ = curl_post(
+            base_url .. "/buffered/post", ramdom_body(),
+            http_version
+          )
+          assert.equal(200, status)
+          buffered_latency = tonumber(headers["x-kong-proxy-latency"])
+          assert.is_number(buffered_latency)
         end)
 
-        lazy_teardown(function()
-          warmup_client:close()
-          helpers.stop_kong()
+        it("is calculated for unbuffered", function()
+          warmup_client:post("/unbuffered/post", { body = "warmup" })
+          status, headers, _ = curl_post(
+            base_url .. "/unbuffered/post", ramdom_body(),
+            http_version
+          )
+          assert.equal(200, status)
+          unbuffered_latency = tonumber(headers["x-kong-proxy-latency"])
+          assert.is_number(unbuffered_latency)
         end)
 
-        before_each(function()
-          proxy_ip = helpers.get_proxy_ip(true, true)
-          proxy_port = helpers.get_proxy_port(true, true)
-          warmup_client = helpers.proxy_client()
-          base_url = "https://" .. proxy_ip .. ":" .. proxy_port
+        it("is calculated for unbuffered request", function()
+          warmup_client:post("/unbuffered-request/post", { body = "warmup" })
+          status, headers, _ = curl_post(
+            base_url .. "/unbuffered-request/post", ramdom_body(),
+            http_version
+          )
+          assert.equal(200, status)
+          unbuffered_request_latency = tonumber(headers["x-kong-proxy-latency"])
+          assert.is_number(unbuffered_request_latency)
         end)
 
-        describe("request latency", function()
-          local buffered_latency
-          local unbuffered_latency
-          local unbuffered_request_latency
-          local unbuffered_response_latency
-          local status, headers, _
+        it("is calculated for unbuffered response", function()
+          warmup_client:post("/unbuffered-response/post", { body = "warmup" })
+          status, headers, _ = curl_post(
+            base_url .. "/unbuffered-response/post", ramdom_body(),
+            http_version
+          )
+          assert.equal(200, status)
+          unbuffered_response_latency = tonumber(headers["x-kong-proxy-latency"])
+          assert.is_number(unbuffered_response_latency)
+        end)
 
-          it("is calculated for buffered", function()
-            warmup_client:post("/buffered/post", { body = "warmup" })
-            status, headers, _ = curl_post(
-              base_url .. "/buffered/post", ramdom_body(),
-              http_version
-            )
-            assert.equal(200, status)
-            buffered_latency = tonumber(headers["x-kong-proxy-latency"])
-            assert.is_number(buffered_latency)
-          end)
+        it("is greater for buffered than unbuffered", function()
+          assert.equal(true, buffered_latency > unbuffered_latency)
+        end)
 
-          it("is calculated for unbuffered", function()
-            warmup_client:post("/unbuffered/post", { body = "warmup" })
-            status, headers, _ = curl_post(
-              base_url .. "/unbuffered/post", ramdom_body(),
-              http_version
-            )
-            assert.equal(200, status)
-            unbuffered_latency = tonumber(headers["x-kong-proxy-latency"])
-            assert.is_number(unbuffered_latency)
-          end)
+        it("is greater for buffered than unbuffered request", function()
+          assert.equal(true, buffered_latency > unbuffered_request_latency)
+        end)
 
-          it("is calculated for unbuffered request", function()
-            warmup_client:post("/unbuffered-request/post", { body = "warmup" })
-            status, headers, _ = curl_post(
-              base_url .. "/unbuffered-request/post", ramdom_body(),
-              http_version
-            )
-            assert.equal(200, status)
-            unbuffered_request_latency = tonumber(headers["x-kong-proxy-latency"])
-            assert.is_number(unbuffered_request_latency)
-          end)
+        it("is greater for unbuffered response than unbuffered", function()
+          assert.equal(true, unbuffered_response_latency > unbuffered_latency)
+        end)
 
-          it("is calculated for unbuffered response", function()
-            warmup_client:post("/unbuffered-response/post", { body = "warmup" })
-            status, headers, _ = curl_post(
-              base_url .. "/unbuffered-response/post", ramdom_body(),
-              http_version
-            )
-            assert.equal(200, status)
-            unbuffered_response_latency = tonumber(headers["x-kong-proxy-latency"])
-            assert.is_number(unbuffered_response_latency)
-          end)
-
-          it("is greater for buffered than unbuffered", function()
-            assert.equal(true, buffered_latency > unbuffered_latency)
-          end)
-
-          it("is greater for buffered than unbuffered request", function()
-            assert.equal(true, buffered_latency > unbuffered_request_latency)
-          end)
-
-          it("is greater for unbuffered response than unbuffered", function()
-            assert.equal(true, unbuffered_response_latency > unbuffered_latency)
-          end)
-
-          it("is greater for unbuffered response than unbuffered request", function()
-            assert.equal(true, unbuffered_response_latency > unbuffered_request_latency)
-          end)
+        it("is greater for unbuffered response than unbuffered request", function()
+          assert.equal(true, unbuffered_response_latency > unbuffered_request_latency)
         end)
       end)
-    end
+    end)
   end
 end
