6e23af2c420a42267ad91df5b78e18e3f1cf03e6
chronolaw <chrono_cpp@me.com>
2023-07-07 15:15:43 +0800

    26-udp_spec.lua

--- spec/02-integration/05-proxy/26-udp_spec.lua
+++ spec/02-integration/05-proxy/26-udp_spec.lua
@@ -1,12 +1,53 @@
 local helpers = require "spec.helpers"
+local atc_compat = require "kong.router.compat"
 
 
 local UDP_PROXY_PORT = 26001
 
 
+local function reload_router(flavor)
+  _G.kong = {
+    configuration = {
+      router_flavor = flavor,
+    },
+  }
+
+  helpers.setenv("KONG_ROUTER_FLAVOR", flavor)
+
+  package.loaded["spec.helpers"] = nil
+  package.loaded["kong.global"] = nil
+  package.loaded["kong.cache"] = nil
+  package.loaded["kong.db"] = nil
+  package.loaded["kong.db.schema.entities.routes"] = nil
+  package.loaded["kong.db.schema.entities.routes_subschemas"] = nil
+
+  helpers = require "spec.helpers"
+
+  helpers.unsetenv("KONG_ROUTER_FLAVOR")
+end
+
+
+local function gen_route(flavor, r)
+  if flavor ~= "expressions" then
+    return r
+  end
+
+  r.expression = atc_compat.get_expression(r)
+  r.priority = tonumber(atc_compat._get_priority(r))
+
+  r.sources = nil
+
+  return r
+end
+
+
+--for _, flavor in ipairs({ "traditional", "traditional_compatible", "expressions" }) do
+for _, flavor in ipairs({ "traditional" }) do
 for _, strategy in helpers.each_strategy() do
 
-  describe("UDP Proxying [#" .. strategy .. "]", function()
+  describe("UDP Proxying [#" .. strategy .. ", flavor = " .. flavor .. "]", function()
+    reload_router(flavor)
+
     lazy_setup(function()
       local bp = helpers.get_db_utils(strategy, {
         "routes",
@@ -18,11 +59,11 @@ for _, strategy in helpers.each_strategy() do
         url = "udp://127.0.0.1:" .. helpers.mock_upstream_stream_port,
       })
 
-      assert(bp.routes:insert {
+      assert(bp.routes:insert(gen_route(flavor, {
         protocols = { "udp" },
         service = service,
         sources = { { ip = "127.0.0.1", }, }
-      })
+      })))
 
       assert(helpers.start_kong {
         database = strategy,
@@ -47,3 +88,4 @@ for _, strategy in helpers.each_strategy() do
     end)
   end)
 end
+end   -- for flavor
