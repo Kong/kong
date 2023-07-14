4958e811f053eeffb3132348ac4dca4c59e3308f
chronolaw <chrono_cpp@me.com>
2023-07-14 17:40:32 +0800

    style lint

--- kong/global.lua
+++ kong/global.lua
@@ -178,25 +178,25 @@ function _GLOBAL.init_worker_events()
 
   -- `kong.configuration.prefix` is already normalized to an absolute path,
   -- but `ngx.config.prefix()` is not
-  local prefix = configuration
-                 and configuration.prefix
-                 or require("pl.path").abspath(ngx.config.prefix())
+  local prefix = configuration and
+                 configuration.prefix or
+                 require("pl.path").abspath(ngx.config.prefix())
 
-  local max_payload_len = configuration
-                           and configuration.worker_events_max_payload
-
-  local sock = ngx.config.subsystem == "stream"
-               and "stream_worker_events.sock"
-               or "worker_events.sock"
+  local sock = ngx.config.subsystem == "stream" and
+               "stream_worker_events.sock" or
+               "worker_events.sock"
 
   local listening = "unix:" .. prefix .. "/" .. sock
+
+  local max_payload_len = configuration and
+                          configuration.worker_events_max_payload
 
   opts = {
     unique_timeout = 5,                   -- life time of unique event data in lrucache
     broker_id = 0,                        -- broker server runs in nginx worker #0
     listening = listening,                -- unix socket for broker listening
-    max_payload_len = max_payload_len,    -- max payload size in bytes
     max_queue_len = 1024 * 50,            -- max queue len for events buffering
+    max_payload_len = max_payload_len,    -- max payload size in bytes
   }
 
   worker_events = require "resty.events.compat"
