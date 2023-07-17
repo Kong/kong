Msaroto/asw

feat(core): Admin GUI integration
Serve static assets from Canopy project

Changes include:
* new nginx config template for static file serving
* new configuration items under admin_gui_* namespace
* bundle canopy assets during the build stage
* more CORS support for Admin API
* tests and more tests

kong/templates/nginx_kong_gui_include.lua
@@ -1,0 +1,97 @@
+return [[
+> local admin_gui_rewrite = admin_gui_path ~= "/"
+> local admin_gui_path_prefix = admin_gui_path
+> if admin_gui_path == "/" then
+>   admin_gui_path_prefix = ""
+> end
+location = $(admin_gui_path_prefix)/robots.txt {
+    gzip on;
+    gzip_types text/plain text/css application/json application/javascript;
+
+    return 200 'User-agent: *\nDisallow: /';
+}
+
+location = $(admin_gui_path_prefix)/kconfig.js {
+    default_type application/javascript;
+
+    gzip on;
+    gzip_types application/javascript;
+    expires -1;
+
+    content_by_lua_block {
+        Kong.admin_gui_kconfig_content()
+    }
+}
+
+location = $(admin_gui_path_prefix)/favicon.ico {
+    root gui;
+
+    try_files /favicon.ico =404;
+
+    log_not_found off;
+
+    gzip on;
+    gzip_types text/plain text/css application/json application/javascript;
+
+    expires 90d;
+    add_header Cache-Control 'public';
+    add_header X-Frame-Options 'sameorigin';
+    add_header X-XSS-Protection '1; mode=block';
+    add_header X-Content-Type-Options 'nosniff';
+    add_header X-Permitted-Cross-Domain-Policies 'master-only';
+    etag off;
+}
+
+location ~* ^$(admin_gui_path_prefix)(?<path>/.*\.(jpg|jpeg|png|gif|svg|ico|css|ttf|js)(\?.*)?)$ {
+    root gui;
+
+    try_files $path =404;
+
+    log_not_found off;
+
+    gzip on;
+    gzip_types text/plain text/css application/json application/javascript;
+
+    expires 90d;
+    add_header Cache-Control 'public';
+    add_header X-Frame-Options 'sameorigin';
+    add_header X-XSS-Protection '1; mode=block';
+    add_header X-Content-Type-Options 'nosniff';
+    add_header X-Permitted-Cross-Domain-Policies 'master-only';
+    etag off;
+
+> if admin_gui_rewrite then
+    sub_filter '/__admin_gui_base__/' '$(admin_gui_path)/';
+> else
+    sub_filter '/__admin_gui_base__/' '/';
+> end
+    sub_filter_once off;
+    sub_filter_types *;
+}
+
+location ~* ^$(admin_gui_path_prefix)(?<path>/.*)?$ {
+    root gui;
+
+    try_files $path /index.html =404;
+
+    log_not_found off;
+
+    gzip on;
+    gzip_types text/plain text/css application/json application/javascript;
+
+    add_header Cache-Control 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0';
+    add_header X-Frame-Options 'sameorigin';
+    add_header X-XSS-Protection '1; mode=block';
+    add_header X-Content-Type-Options 'nosniff';
+    add_header X-Permitted-Cross-Domain-Policies 'master-only';
+    etag off;
+
+> if admin_gui_rewrite then
+    sub_filter '/__admin_gui_base__/' '$(admin_gui_path)/';
+> else
+    sub_filter '/__admin_gui_base__/' '/';
+> end
+    sub_filter_once off;
+    sub_filter_types *;
+}
+]]

spec/02-integration/17-admin_gui/01-admin-gui-path_spec.lua
@@ -1,0 +1,120 @@
+local lfs = require "lfs"
+local pl_path = require "pl.path"
+local helpers = require "spec.helpers"
+local test_prefix = helpers.test_conf.prefix
+
+local _
+
+describe("Admin GUI - admin_gui_path", function()
+  local client
+
+  after_each(function()
+    helpers.stop_kong()
+    if client then
+      client:close()
+    end
+  end)
+
+  it("should serve Admin GUI correctly when admin_gui_path is unset", function()
+    assert(helpers.start_kong({
+      database = "off",
+      admin_gui_listen = "127.0.0.1:9002",
+    }))
+    client = assert(helpers.admin_gui_client(nil, 9002))
+
+    local err, gui_dir_path, gui_index_file_path
+    gui_dir_path = pl_path.join(test_prefix, "gui")
+    os.execute("rm -rf " .. gui_dir_path)
+    _, err = lfs.mkdir(gui_dir_path)
+    assert.is_nil(err)
+
+    gui_index_file_path = pl_path.join(gui_dir_path, "index.html")
+
+    local gui_index_file
+    gui_index_file, err = io.open(gui_index_file_path, "w+")
+    assert.is_nil(err)
+    gui_index_file:write("TEST INDEX.HTML = /__admin_gui_base__/assets/image.png")
+    gui_index_file:close()
+
+    local res = assert(client:send {
+      method = "GET",
+      path = "/",
+    })
+    res = assert.res_status(200, res)
+    assert.matches("TEST INDEX%.HTML = /assets/image%.png", res)
+    client:close()
+
+    res = assert(client:send {
+      method = "GET",
+      path = "/kconfig.js",
+    })
+    res = assert.res_status(200, res)
+    assert.matches("'KONG_VERSION': '", res)
+  end)
+
+  it("should serve Admin GUI correctly when admin_gui_path is set", function()
+    assert(helpers.start_kong({
+      database = "off",
+      admin_gui_listen = "127.0.0.1:9002",
+      admin_gui_path = "/canopy",
+    }))
+    client = assert(helpers.admin_gui_client(nil, 9002))
+
+    local err, gui_dir_path, gui_index_file_path
+    gui_dir_path = pl_path.join(test_prefix, "gui")
+    os.execute("rm -rf " .. gui_dir_path)
+    _, err = lfs.mkdir(gui_dir_path)
+    assert.is_nil(err)
+
+    gui_index_file_path = pl_path.join(gui_dir_path, "index.html")
+
+    local gui_index_file
+    gui_index_file, err = io.open(gui_index_file_path, "w+")
+    assert.is_nil(err)
+    gui_index_file:write("TEST INDEX.HTML = /__admin_gui_base__/assets/image.png")
+    gui_index_file:close()
+
+    local res = assert(client:send {
+      method = "GET",
+      path = "/",
+    })
+    assert.res_status(404, res)
+    client:close()
+
+    local res = assert(client:send {
+      method = "GET",
+      path = "/any_other_test_path",
+    })
+    assert.res_status(404, res)
+    client:close()
+
+    res = assert(client:send {
+      method = "GET",
+      path = "/canopy",
+    })
+    res = assert.res_status(200, res)
+    assert.matches("TEST INDEX%.HTML = /canopy/assets/image%.png", res)
+    client:close()
+
+    local res = assert(client:send {
+      method = "GET",
+      path = "/kconfig.js",
+    })
+    assert.res_status(404, res)
+    client:close()
+
+    local res = assert(client:send {
+      method = "GET",
+      path = "/any_other_test_path/kconfig.js",
+    })
+    assert.res_status(404, res)
+    client:close()
+
+    res = assert(client:send {
+      method = "GET",
+      path = "/canopy/kconfig.js",
+    })
+    res = assert.res_status(200, res)
+    assert.matches("'KONG_VERSION': '", res)
+  end)
+end)

Kong/kong

kong/templates/nginx_kong_gui_include.lua
@@ -1,0 +1,97 @@
+return [[
+> local admin_gui_rewrite = admin_gui_path ~= "/"
+> local admin_gui_path_prefix = admin_gui_path
+> if admin_gui_path == "/" then
+>   admin_gui_path_prefix = ""
+> end
+location = $(admin_gui_path_prefix)/robots.txt {
+    gzip on;
+    gzip_types text/plain text/css application/json application/javascript;
+
+    return 200 'User-agent: *\nDisallow: /';
+}
+
+location = $(admin_gui_path_prefix)/kconfig.js {
+    default_type application/javascript;
+
+    gzip on;
+    gzip_types application/javascript;
+    expires -1;
+
+    content_by_lua_block {
+        Kong.admin_gui_kconfig_content()
+    }
+}
+
+location = $(admin_gui_path_prefix)/favicon.ico {
+    root gui;
+
+    try_files /favicon.ico =404;
+
+    log_not_found off;
+
+    gzip on;
+    gzip_types text/plain text/css application/json application/javascript;
+
+    expires 90d;
+    add_header Cache-Control 'public';
+    add_header X-Frame-Options 'sameorigin';
+    add_header X-XSS-Protection '1; mode=block';
+    add_header X-Content-Type-Options 'nosniff';
+    add_header X-Permitted-Cross-Domain-Policies 'master-only';
+    etag off;
+}
+
+location ~* ^$(admin_gui_path_prefix)(?<path>/.*\.(jpg|jpeg|png|gif|svg|ico|css|ttf|js)(\?.*)?)$ {
+    root gui;
+
+    try_files $path =404;
+
+    log_not_found off;
+
+    gzip on;
+    gzip_types text/plain text/css application/json application/javascript;
+
+    expires 90d;
+    add_header Cache-Control 'public';
+    add_header X-Frame-Options 'sameorigin';
+    add_header X-XSS-Protection '1; mode=block';
+    add_header X-Content-Type-Options 'nosniff';
+    add_header X-Permitted-Cross-Domain-Policies 'master-only';
+    etag off;
+
+> if admin_gui_rewrite then
+    sub_filter '/__km_base__/' '$(admin_gui_path)/';
+> else
+    sub_filter '/__km_base__/' '/';
+> end
+    sub_filter_once off;
+    sub_filter_types *;
+}
+
+location ~* ^$(admin_gui_path_prefix)(?<path>/.*)?$ {
+    root gui;
+
+    try_files $path /index.html =404;
+
+    log_not_found off;
+
+    gzip on;
+    gzip_types text/plain text/css application/json application/javascript;
+
+    add_header Cache-Control 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0';
+    add_header X-Frame-Options 'sameorigin';
+    add_header X-XSS-Protection '1; mode=block';
+    add_header X-Content-Type-Options 'nosniff';
+    add_header X-Permitted-Cross-Domain-Policies 'master-only';
+    etag off;
+
+> if admin_gui_rewrite then
+    sub_filter '/__km_base__/' '$(admin_gui_path)/';
+> else
+    sub_filter '/__km_base__/' '/';
+> end
+    sub_filter_once off;
+    sub_filter_types *;
+}
+]]

spec/02-integration/17-admin_gui/01-admin-gui-path_spec.lua
@@ -1,0 +1,120 @@
+local lfs = require "lfs"
+local pl_path = require "pl.path"
+local helpers = require "spec.helpers"
+local test_prefix = helpers.test_conf.prefix
+
+local _
+
+describe("Admin GUI - admin_gui_path", function()
+  local client
+
+  after_each(function()
+    helpers.stop_kong()
+    if client then
+      client:close()
+    end
+  end)
+
+  it("should serve Admin GUI correctly when admin_gui_path is unset", function()
+    assert(helpers.start_kong({
+      database = "off",
+      admin_gui_listen = "127.0.0.1:9002",
+    }))
+    client = assert(helpers.admin_gui_client(nil, 9002))
+
+    local err, gui_dir_path, gui_index_file_path
+    gui_dir_path = pl_path.join(test_prefix, "gui")
+    os.execute("rm -rf " .. gui_dir_path)
+    _, err = lfs.mkdir(gui_dir_path)
+    assert.is_nil(err)
+
+    gui_index_file_path = pl_path.join(gui_dir_path, "index.html")
+
+    local gui_index_file
+    gui_index_file, err = io.open(gui_index_file_path, "w+")
+    assert.is_nil(err)
+    gui_index_file:write("TEST INDEX.HTML = /__km_base__/assets/image.png")
+    gui_index_file:close()
+
+    local res = assert(client:send {
+      method = "GET",
+      path = "/",
+    })
+    res = assert.res_status(200, res)
+    assert.matches("TEST INDEX%.HTML = /assets/image%.png", res)
+    client:close()
+
+    res = assert(client:send {
+      method = "GET",
+      path = "/kconfig.js",
+    })
+    res = assert.res_status(200, res)
+    assert.matches("'KONG_VERSION': '", res)
+  end)
+
+  it("should serve Admin GUI correctly when admin_gui_path is set", function()
+    assert(helpers.start_kong({
+      database = "off",
+      admin_gui_listen = "127.0.0.1:9002",
+      admin_gui_path = "/manager",
+    }))
+    client = assert(helpers.admin_gui_client(nil, 9002))
+
+    local err, gui_dir_path, gui_index_file_path
+    gui_dir_path = pl_path.join(test_prefix, "gui")
+    os.execute("rm -rf " .. gui_dir_path)
+    _, err = lfs.mkdir(gui_dir_path)
+    assert.is_nil(err)
+
+    gui_index_file_path = pl_path.join(gui_dir_path, "index.html")
+
+    local gui_index_file
+    gui_index_file, err = io.open(gui_index_file_path, "w+")
+    assert.is_nil(err)
+    gui_index_file:write("TEST INDEX.HTML = /__km_base__/assets/image.png")
+    gui_index_file:close()
+
+    local res = assert(client:send {
+      method = "GET",
+      path = "/",
+    })
+    assert.res_status(404, res)
+    client:close()
+
+    local res = assert(client:send {
+      method = "GET",
+      path = "/any_other_test_path",
+    })
+    assert.res_status(404, res)
+    client:close()
+
+    res = assert(client:send {
+      method = "GET",
+      path = "/manager",
+    })
+    res = assert.res_status(200, res)
+    assert.matches("TEST INDEX%.HTML = /manager/assets/image%.png", res)
+    client:close()
+
+    local res = assert(client:send {
+      method = "GET",
+      path = "/kconfig.js",
+    })
+    assert.res_status(404, res)
+    client:close()
+
+    local res = assert(client:send {
+      method = "GET",
+      path = "/any_other_test_path/kconfig.js",
+    })
+    assert.res_status(404, res)
+    client:close()
+
+    res = assert(client:send {
+      method = "GET",
+      path = "/manager/kconfig.js",
+    })
+    res = assert.res_status(200, res)
+    assert.matches("'KONG_VERSION': '", res)
+  end)
+end)