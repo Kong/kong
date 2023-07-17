e823a9b8ee14ef4f16d92e96f3b0d901c6d3164d
Michael Martin <michael.martin@konghq.com>
2023-07-17 12:00:33 -0700

    chore(wasm): add prefix to init_worker error string

--- kong/init.lua
+++ kong/init.lua
@@ -864,6 +864,7 @@ function Kong.init_worker()
 
   ok, err = wasm.init_worker()
   if not ok then
+    err = "wasm nginx worker initialization failed: " .. tostring(err)
     stash_init_worker_error(err)
   end
 end
