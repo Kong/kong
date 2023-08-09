5024774d66830a3b924d97b5e108272c7dfab8c4
chronolaw <chrono_cpp@me.com>
2023-08-09 18:17:44 +0800

    fix headers_key|queries_key

--- kong/router/atc.lua
+++ kong/router/atc.lua
@@ -649,7 +649,7 @@ function _M:exec(ctx)
   local cache_key = (req_method or "") .. "|" ..
                     (req_uri    or "") .. "|" ..
                     (req_host   or "") .. "|" ..
-                    (sni        or "") .. (headers_key or "") .. (queries_key or "")
+                    (sni        or "") .. (headers_key or "|") .. (queries_key or "|")
 
   local match_t = self.cache:get(cache_key)
   if not match_t then
