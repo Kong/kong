004891ed0ed45b0ed9aea170992aba1b99ef9841
chronolaw <chrono_cpp@me.com>
2023-08-21 20:12:30 +0800

    clean exec

--- kong/router/atc.lua
+++ kong/router/atc.lua
@@ -621,8 +621,8 @@ local function get_http_params(func, name, max_config_option)
             string.format("router: not all request %s were read in order to determine the route as " ..
                           "the request contains more than %d %s, route selection " ..
                           "may be inaccurate, consider increasing the '%s' configuration value " ..
-                          "(currently at %d)"),
-                          name, max, name, max_config_option, max)
+                          "(currently at %d)",
+                          name, max, name, max_config_option, max))
   end
 
   return params
@@ -637,15 +637,7 @@ function _M:exec(ctx)
 
   local headers, headers_key
   if self.match_headers then
-    local err
-    headers, err = get_headers()
-    if err == "truncated" then
-      local lua_max_req_headers = kong and kong.configuration and kong.configuration.lua_max_req_headers or 100
-      ngx_log(ngx_ERR, "router: not all request headers were read in order to determine the route as ",
-                       "the request contains more than ", lua_max_req_headers, " headers, route selection ",
-                       "may be inaccurate, consider increasing the 'lua_max_req_headers' configuration value ",
-                       "(currently at ", lua_max_req_headers, ")")
-    end
+    headers = get_http_params(get_headers, "headers", "lua_max_req_headers")
 
     headers["host"] = nil
 
@@ -654,15 +646,7 @@ function _M:exec(ctx)
 
   local queries, queries_key
   if self.match_queries then
-    local err
-    queries, err = get_uri_args()
-    if err == "truncated" then
-      local lua_max_uri_args = kong and kong.configuration and kong.configuration.lua_max_uri_args or 100
-      ngx_log(ngx_ERR, "router: not all request queries were read in order to determine the route as ",
-                       "the request contains more than ", lua_max_uri_args, " queries, route selection ",
-                       "may be inaccurate, consider increasing the 'lua_max_uri_args' configuration value ",
-                       "(currently at ", lua_max_uri_args, ")")
-    end
+    queries = get_http_params(get_uri_args, "queries", "lua_max_uri_args")
 
     queries_key = get_queries_key(queries)
   end
