2395aac89de93bd0af3ca02383162d05348640b3
owl <ouyangjun1999@gmail.com>
2023-08-22 19:10:30 +0800

    fix(response): fix response status code as not same as real upstream status when using kong_buffered_http

--- kong/init.lua
+++ kong/init.lua
@@ -1296,7 +1296,7 @@ do
     local res = ngx.location.capture("/kong_buffered_http", options)
     if res.truncated and options.method ~= ngx.HTTP_HEAD then
       ctx.KONG_PHASE = PHASES.error
-      ngx.status = 502
+      ngx.status = res.status or 502
       return kong_error_handlers(ctx)
     end
 
