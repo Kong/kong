febb951032d278086a216fd9e5ad37e77bd988b4
Isa Farnik <isa@konghq.com>
2023-09-07 12:19:57 -0700

    wip

--- build/tests/04-bins.sh
+++ build/tests/04-bins.sh
@@ -12,3 +12,6 @@ assert_exec 0 'root' "test -x /usr/local/kong/bin/curl"
 
 msg_test "Check if Kong provided cURL runs correctly"
 assert_exec 0 'root' "/usr/local/kong/bin/curl --version"
+
+msg_test "Check if Kong provided cURL can do HTTP2"
+assert_exec 0 'root' "/usr/local/kong/bin/curl -sI https://curl.se -o/dev/null -w '%{http_version}\n' | grep 2"
