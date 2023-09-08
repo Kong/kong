a315ab2f55d3bd892ae86666ea21ca16a22e1fd9
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2023-09-08 16:30:31 +0300

    fix(vault): fix several issues in vault and refactor the vault code base (EE changes)
    Signed-off-by: Aapo Talvensaari <aapo.talvensaari@gmail.com>

--- spec-ee/02-integration/19-vaults/02-ttl_spec.lua
+++ spec-ee/02-integration/19-vaults/02-ttl_spec.lua
@@ -178,11 +178,11 @@ describe("vault ttl and rotation (#" .. attachment_point .. "_" .. vault.name ..
 
     -- Did we see the change from `init` to the newly set secret?
     vault:update_secret(secret, "updated_once", { ttl = 5, resurrect_ttl = 5 })
-    check_plugin_secret("updated_once", "updated_once", 11)
+    check_plugin_secret("updated_once", "updated_once", 21)
 
     -- Did we see the change from previsouly set secret to the newly set secret?
     vault:update_secret(secret, "updated_twice", { ttl = 5, resurrect_ttl = 5 })
-    check_plugin_secret("updated_twice", "updated_twice", 11)
+    check_plugin_secret("updated_twice", "updated_twice", 21)
 
     -- Does it disappear when we delete it from the vault?
     vault:delete_secret(secret)
@@ -193,20 +193,20 @@ describe("vault ttl and rotation (#" .. attachment_point .. "_" .. vault.name ..
     -- If we create a secret with a high resurrect time, we expect that it
     -- does not expire, even when we delete the secret from the vault
     -- inbetween
-    check_plugin_secret("init", nil, 11, NESTED_HEADER_2)
+    check_plugin_secret("init", nil, 21, NESTED_HEADER_2)
 
     -- We delete a secret
     vault:delete_secret(secret_2)
     -- Check if we can still see it (it should be valid for at least a couple of seconds.)
-    check_plugin_secret("init", nil, 11, NESTED_HEADER_2)
+    check_plugin_secret("init", nil, 21, NESTED_HEADER_2)
     -- re-create the secret
     vault:create_secret(secret_2, "created_again", { ttl = 5, resurrect_ttl = 5 })
     -- and check if this eventually changes to the new value
-    check_plugin_secret("created_again", nil, 11, NESTED_HEADER_2)
+    check_plugin_secret("created_again", nil, 21, NESTED_HEADER_2)
 
     -- If we create it again(with a different value), do we see the changes?
     vault:update_secret(secret_2, "updated_yet_again", {ttl = 5, resurrect_ttl = 5})
-    check_plugin_secret("updated_yet_again", nil, 11, NESTED_HEADER_2)
+    check_plugin_secret("updated_yet_again", nil, 21, NESTED_HEADER_2)
   end)
 end)
 
