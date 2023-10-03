a34bb4e7eeae378bf3f2fced3576b59a27645232
Enrique Garcia Cota <kikito@gmail.com>
2023-10-03 18:27:46 +0200

    chore(release): bump version numbers to 3.4.2

--- kong/meta.lua
+++ kong/meta.lua
@@ -1,7 +1,7 @@
 local version = setmetatable({
   major = 3,
   minor = 4,
-  patch = 1,
+  patch = 2,
   --suffix = "-alpha.13"
 }, {
   -- our Makefile during certain releases adjusts this line. Any changes to
