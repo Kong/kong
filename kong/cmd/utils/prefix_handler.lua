35588e433d09b1d6b9d6f5a3709f742d2f9ea5b6
Qi <add_sp@outlook.com>
2023-10-09 18:20:57 +0800

    fix(template): fix failure of starting if `proxy_access_log` is `off` (#6752)
    (cherry-picked from https://github.com/Kong/kong/commit/6b3d3c5a49f89e20f277136c1df2109e6e6e1df1)

--- kong/cmd/utils/prefix_handler.lua
+++ kong/cmd/utils/prefix_handler.lua
@@ -257,6 +257,7 @@ local function compile_conf(kong_config, conf_template, template_env_inject)
   -- computed config properties for templating
   local compile_env = {
     _escape = ">",
+    proxy_access_log_enabled = kong_config.proxy_access_log ~= "off",
     pairs = pairs,
     ipairs = ipairs,
     tostring = tostring,
