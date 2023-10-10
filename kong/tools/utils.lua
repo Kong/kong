5aad93600d913aa24c3039c27bacae1182bb970e
chronolaw <chrono_cpp@me.com>
2023-10-10 10:34:05 +0800

    replace_dashed in stream

--- kong/tools/utils.lua
+++ kong/tools/utils.lua
@@ -1849,17 +1849,29 @@ _M.get_start_time_ms  = get_start_time_ms
 _M.get_updated_monotonic_ms = get_updated_monotonic_ms
 
 
--- only available in http subsystem
 local replace_dashes
 do
-  -- 1,000,000 iterations with input of "my-header":
-  -- string.gsub:        81ms
-  -- ngx.re.gsub:        74ms
-  -- loop/string.buffer: 28ms
-  -- str_replace_char:   14ms
-  local str_replace_char = require("resty.core.utils").str_replace_char
-  replace_dashes = function(str)
-    return str_replace_char(str or "", "-", "_")
+  if ngx and ngx.config.subsystem == "http" then
+
+    -- 1,000,000 iterations with input of "my-header":
+    -- string.gsub:        81ms
+    -- ngx.re.gsub:        74ms
+    -- loop/string.buffer: 28ms
+    -- str_replace_char:   14ms
+    local str_replace_char = require("resty.core.utils").str_replace_char
+
+    replace_dashes = function(str)
+      return str_replace_char(str or "", "-", "_")
+    end
+
+  else    -- stream subsystem
+    replace_dashes = function(str)
+      if not find(str or "", "-", nil, true) then
+        return str
+      end
+
+      return gsub(str, "-", "_")
+    end
   end
 end
 _M.replace_dashes = replace_dashes
