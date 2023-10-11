c315e5ec1c6ae8e3215a9598dad4c39db603a201
chronolaw <chrono_cpp@me.com>
2023-10-11 17:52:32 +0800

    lint fix

--- kong/tools/string.lua
+++ kong/tools/string.lua
@@ -1,8 +1,14 @@
+local find          = string.find
+local gsub          = string.gsub
+
+
 local _M = {}
 
 
 local replace_dashes
 do
+  local str_replace_char
+
   if ngx and ngx.config.subsystem == "http" then
 
     -- 1,000,000 iterations with input of "my-header":
@@ -10,20 +16,20 @@ do
     -- ngx.re.gsub:        74ms
     -- loop/string.buffer: 28ms
     -- str_replace_char:   14ms
-    local str_replace_char = require("resty.core.utils").str_replace_char
-
-    replace_dashes = function(str)
-      return str_replace_char(str or "", "-", "_")
-    end
+    str_replace_char = require("resty.core.utils").str_replace_char
 
   else    -- stream subsystem
-    replace_dashes = function(str)
-      if not find(str or "", "-", nil, true) then
+    str_replace_char = function(str, f, replace)
+      if not find(str, f, nil, true) then
         return str
       end
 
-      return gsub(str, "-", "_")
+      return gsub(str, f, replace)
     end
+  end
+
+  replace_dashes = function(str)
+    return str_replace_char(str or "", "-", "_")
   end
 end
 _M.replace_dashes = replace_dashes
