1b9edc05f3f72d4f9771872df5c26f1bd2f9945a
Joshua Schmid <jaiks@posteo.de>
2023-10-11 09:41:34 +0200

    feat(core): add `/schemas/vaults/:name` endpoint (#11727)
    Co-authored-by: Joshua Schmid <jaiks@posteo.de>
    (cherry picked from commit 18de761b991ce98f450732f2d5cabb5f1e4d4a66)

--- kong/vaults/env/schema.lua
+++ kong/vaults/env/schema.lua
@@ -12,7 +12,7 @@ return {
       config = {
         type = "record",
         fields = {
-          { prefix = { type = "string", match = [[^[%a_-][%a%d_-]*$]] } },
+          { prefix = { type = "string", match = [[^[%a_-][%a%d_-]*$]], description = "The prefix for the environment variable that the value will be stored in." } },
         },
       },
     },
