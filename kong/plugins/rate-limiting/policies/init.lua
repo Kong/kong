f5691e757d3a51c6d60b354af4e8f391f8f6e1ee
Samuele <samuele@konghq.com>
2023-10-12 10:14:36 +0200

    fix(rate-limiting): revert request-aware-table usage (#11746)
    * the request-aware-table was incorrectly used in this plugin, these
      tables are expected to be shared across requests and should not be
      limited to a single request.

--- kong/plugins/rate-limiting/policies/init.lua
+++ kong/plugins/rate-limiting/policies/init.lua
@@ -9,7 +9,7 @@ local policy_cluster = require "kong.plugins.rate-limiting.policies.cluster"
 local timestamp = require "kong.tools.timestamp"
 local reports = require "kong.reports"
 local redis = require "resty.redis"
-local request_aware_table = require "kong.tools.request_aware_table"
+local table_clear = require "table.clear"
 
 local kong = kong
 local pairs = pairs
@@ -25,26 +25,23 @@ local EMPTY_UUID = "00000000-0000-0000-0000-000000000000"
 -- for `conf.sync_rate > 0`
 local auto_sync_timer
 
-local cur_usage = request_aware_table.new()
--- {
---     [[
---     [cache_key] = <integer>
---     ]]
--- }
+local cur_usage = {
+  --[[
+    [cache_key] = <integer>
+  --]]
+}
 
-local cur_usage_expire_at = request_aware_table.new()
--- {
---     [[
---     [cache_key] = <integer>
---     ]]
--- }
+local cur_usage_expire_at = {
+  --[[
+    [cache_key] = <integer>
+  --]]
+}
 
-local cur_delta = request_aware_table.new()
--- {
---     [[
---     [cache_key] = <integer>
---     ]]
--- }
+local cur_delta = {
+  --[[
+    [cache_key] = <integer>
+  --]]
+}
 
 
 local function is_present(str)
@@ -148,9 +145,9 @@ local function get_redis_connection(conf)
 end
 
 local function clear_local_counter()
-  cur_usage:clear()
-  cur_usage_expire_at:clear()
-  cur_delta:clear()
+  table_clear(cur_usage)
+  table_clear(cur_usage_expire_at)
+  table_clear(cur_delta)
 end
 
 local function sync_to_redis(premature, conf)
