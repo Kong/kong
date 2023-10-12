bb4e56c02bf2aa32c02f3965e15bc581c6484029
chronolaw <chrono_cpp@me.com>
2023-10-12 15:55:18 +0800

    use $kong_request_id

--- kong/tools/request_aware_table.lua
+++ kong/tools/request_aware_table.lua
@@ -35,7 +35,7 @@ local function enforce_sequential_access(table)
     return
   end
 
-  local curr_request_id = var.request_id
+  local curr_request_id = var.kong_request_id
   local allowed_request_id = rawget(table, ALLOWED_REQUEST_ID_K)
   if not allowed_request_id then
     -- first access. Set allowed request ID and allow access
