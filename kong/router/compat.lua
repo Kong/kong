a101659a20506fcc2a90e94db042516f1273563b
chronolaw <chrono_cpp@me.com>
2023-10-14 16:24:09 +0800

    compat.lua

--- kong/router/compat.lua
+++ kong/router/compat.lua
@@ -11,6 +11,7 @@ local uuid = require("resty.jit-uuid")
 
 
 local shallow_copy    = require("kong.tools.utils").shallow_copy
+local replace_dashes  = require("kong.tools.string").replace_dashes
 
 
 local is_regex_magic  = utils.is_regex_magic
@@ -251,7 +252,7 @@ local function get_expression(route)
       single_header_buf:reset():put("(")
 
       for i, value in ipairs(v) do
-        local name = "any(http.headers." .. h:gsub("-", "_"):lower() .. ")"
+        local name = "any(http.headers." .. replace_dashes(h):lower() .. ")"
         local op = OP_EQUAL
 
         -- value starts with "~*"
