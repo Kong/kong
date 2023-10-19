64f8d44ec8d605ab4231b4848cd3608d1a233a99
Xumin <100666470+StarlightIbuki@users.noreply.github.com>
2023-10-19 09:26:51 +0000

    tests(unit): fix flaky DP resilience tests (#6868)

--- spec-ee/01-unit/11-dp-resilience/02-strategies_s3_spec.lua
+++ spec-ee/01-unit/11-dp-resilience/02-strategies_s3_spec.lua
@@ -6,24 +6,13 @@
 -- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
 
 local helpers = require("spec.helpers")
+local http_mock = require("spec.helpers.http_mock")
 
 local FAKE_TIMESTAMP = 1667543171
+local S3PORT = helpers.get_available_port()
 local original_time = ngx.time
 
--- make an assertion of what response we expect to see.
--- handle the detail of parsing the response line and subtle differencies
-local function response_line_match(line, method, url)
-  local m = assert(ngx.re.match(line, [[(.+) (.+) HTTP/1.1]]))
-  assert.same(method, m[1])
-  local url_extracted = m[2]
-  -- a workaround for the subtle different behavior of different versions of `resty.http`:
-  -- empty query table may lead to an URL with trailing `?` in earlier versions.
-  if m[2]:sub(-1) == "?" then
-    url_extracted = m[2]:sub(1, -2)
-  end
-
-  assert.same(url, url_extracted)
-end
+local URL = "/test_bucket/test_prefix/test_version/config.json"
 
 local test_config = [[
 {
@@ -49,13 +38,13 @@ local function verify_aws_request(headers)
     headers.Authorization)
   assert(headers["X-Amz-Date"])
   assert(headers["X-Amz-Content-Sha256"])
-  assert.same("127.0.0.1:4566", headers.Host)
+  assert.same("127.0.0.1:" .. S3PORT, headers.Host)
 end
 
 describe("cp outage handling storage support: #s3", function()
-  local S3PORT = 4566
   local s3
   local s3_instance
+  local mock_s3_server
 
   lazy_setup(function()
     -- S3 environment variables
@@ -87,39 +76,55 @@ describe("cp outage handling storage support: #s3", function()
   end)
 
   before_each(function()
+    -- mocking s3 server
+    mock_s3_server = http_mock.new(S3PORT, {
+      [URL] = {
+        access = [=[
+          if ngx.req.get_method() == "GET" then
+            ngx.print([[]=] ..
+              test_config ..
+            [=[]])
+          end
+          ngx.exit(ngx.HTTP_OK)
+        ]=]
+      },
+    }, {
+      log_opts = { req_body = true }
+    })
+    mock_s3_server:start()
     s3_instance = s3.new("test_version", "s3://test_bucket/test_prefix")
   end)
 
-  it("upload", function ()
-    -- mocking s3 server
-    local mock_s3_server = helpers.http_server(S3PORT, { timeout = 10 })
-    s3_instance:backup_config(test_config)
-    local ok, lines, body, headers = mock_s3_server:join()
-    assert(ok)
-    response_line_match(lines[1], "PUT", "/test_bucket/test_prefix/test_version/config.json")
-    assert.same("application/json", headers["Content-Type"])
-    verify_aws_request(headers)
+  after_each(function()
+    mock_s3_server:stop(true)
+  end)
 
-    assert.equal(test_config, body)
+  it("upload", function ()
+    s3_instance:backup_config(test_config)
+
+    local req = mock_s3_server:get_request()
+
+    assert.same("PUT", req.method)
+    assert.same(URL, req.uri)
+    assert.same("application/json", req.headers["Content-Type"])
+    verify_aws_request(req.headers)
+
+    assert.equal(test_config, req.body)
   end)
 
   it("download", function ()
-    -- mocking s3 server
-    local mock_s3_server = helpers.http_server(S3PORT, { timeout = 10, response = [[
-HTTP/1.1 200 OK
-Content-Length: 224
-
-]] .. test_config })
     local result
     helpers.pwait_until(function()
       result = assert(s3_instance:fetch_config())
     end, 10)
-    local ok, lines, body, headers = mock_s3_server:join()
-    assert(ok)
-    response_line_match(lines[1], "GET", "/test_bucket/test_prefix/test_version/config.json")
-    verify_aws_request(headers)
 
-    assert.equal(body, nil)
+    local req = mock_s3_server:get_request()
+
+    assert.same("GET", req.method)
+    assert.same(URL, req.uri)
+    verify_aws_request(req.headers)
+    assert.equal("", req.body or "")
+
     assert.equal(test_config, result)
   end)
 end)
