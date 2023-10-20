6a6af6d095935ea8c941e00cd05f46d947ff4b52
Xiaochen Wang <wangxiaochen0@gmail.com>
2023-10-20 14:58:57 +0800

    perf(request-id): use `proxy_set_header` instead of `ngx.req.set_header` (#11788)
    Utilize the Nginx directive
    `proxy_set_header X-Kong-Request-Id $kong_request_id` instead of Lua
    call `set_header()` can enhance the RPS by ~2%
    in the testing scenario where no plugins are enabled.
    
    KAG-2814

--- kong/runloop/handler.lua
+++ kong/runloop/handler.lua
@@ -44,7 +44,6 @@ local log               = ngx.log
 local exit              = ngx.exit
 local exec              = ngx.exec
 local header            = ngx.header
-local set_header        = ngx.req.set_header
 local timer_at          = ngx.timer.at
 local get_phase         = ngx.get_phase
 local subsystem         = ngx.config.subsystem
@@ -1355,9 +1354,6 @@ return {
     end,
     -- Only executed if the `router` module found a route and allows nginx to proxy it.
     after = function(ctx)
-      local enabled_headers_upstream = kong.configuration.enabled_headers_upstream
-      local headers = constants.HEADERS
-
       -- Nginx's behavior when proxying a request with an empty querystring
       -- `/foo?` is to keep `$is_args` an empty string, hence effectively
       -- stripping the empty querystring.
@@ -1449,16 +1445,6 @@ return {
 
       if var.http_proxy_connection then
         clear_header("Proxy-Connection")
-      end
-
-      -- X-Kong-Request-Id upstream header
-      local rid, rid_get_err = request_id_get()
-      if not rid then
-        log(WARN, "failed to get Request ID: ", rid_get_err)
-      end
-
-      if enabled_headers_upstream[headers.REQUEST_ID] and rid then
-        set_header(headers.REQUEST_ID, rid)
       end
     end
   },
