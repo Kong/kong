c0a0816ac162daa657c874fa24511da8e9b01259
Guilherme Salazar <gsz@acm.org>
2023-10-23 10:44:31 -0300

    fix(plugin-servers): harden seq number generation
    Also, `get_instance_id` uses plugin cache key to fetch instance id.

--- kong/runloop/plugin_servers/pb_rpc.lua
+++ kong/runloop/plugin_servers/pb_rpc.lua
@@ -371,6 +371,9 @@ function Rpc:call_start_instance(plugin_name, conf)
     return nil, err
   end
 
+  kong.log.debug("started plugin server: seq ", conf.__seq__, ", worker ", ngx.worker.id(), ", instance id ",
+    status.instance_status.instance_id)
+
   return {
     id = status.instance_status.instance_id,
     conf = conf,
