6ca7d7d4bb63affe12ac65041f2d63e29406ee08
Andy Zhang <AndyZhang0707@users.noreply.github.com>
2023-10-31 10:47:43 +0800

    chore(conf): enable `dedicated_config_processing` by default (#11889)
    * chore: enable `dedicated_config_processing by default
    
    This reverts commit 6bccc872cbb3a8bb52389a4e7b18a06b59e05ac0.
    
    * docs(dcp): remove a trailing space

--- kong/templates/kong_defaults.lua
+++ kong/templates/kong_defaults.lua
@@ -274,7 +274,7 @@ dns_not_found_ttl = 30
 dns_error_ttl = 1
 dns_no_sync = on
 
-dedicated_config_processing = off
+dedicated_config_processing = on
 worker_consistency = eventual
 worker_state_update_frequency = 5
 
