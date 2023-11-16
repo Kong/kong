2e8e1f97bc3023d1be266aef93bb202de1f66005
tzssangglass <tzssangglass@gmail.com>
2023-11-16 10:38:02 +0800

    formatting
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- spec/02-integration/03-db/13-cluster_status_spec.lua
+++ spec/02-integration/03-db/13-cluster_status_spec.lua
@@ -74,14 +74,20 @@ for _, strategy in helpers.each_strategy() do
 
     describe("cert_details", function()
       it(":upsert()", function()
-        local p, err = db.clustering_data_planes:upsert({ id = "eb51145a-aaaa-bbbb-cccc-22087fb081db", },
-                                                 { config_hash = "a9a166c59873245db8f1a747ba9a80a7",
-                                                   hostname = "localhost",
-                                                   ip = "127.0.0.1",
-                                                   cert_details = {
-                                                    expiry_timestamp = 1897136778,
-                                                   }
-                                                 })
+        local p, err =
+          db.clustering_data_planes:upsert(
+            {
+              id = "eb51145a-aaaa-bbbb-cccc-22087fb081db",
+            },
+            {
+              config_hash = "a9a166c59873245db8f1a747ba9a80a7",
+              hostname = "localhost",
+              ip = "127.0.0.1",
+              cert_details = {
+                expiry_timestamp = 1897136778,
+              }
+            }
+          )
 
         assert.is_truthy(p)
         assert.is_nil(err)
@@ -89,10 +95,19 @@ for _, strategy in helpers.each_strategy() do
 
       it(":update()", function()
         -- this time update instead of insert
-        local p, err = db.clustering_data_planes:update({ id = "eb51145a-aaaa-bbbb-cccc-22087fb081db", },
-                                          { config_hash = "a9a166c59873245db8f1a747ba9a80a7",
-                                          cert_details = { expiry_timestamp = 1888983905, }
-                                          })
+        local p, err =
+          db.clustering_data_planes:update(
+            {
+              id = "eb51145a-aaaa-bbbb-cccc-22087fb081db",
+            },
+            {
+              config_hash = "a9a166c59873245db8f1a747ba9a80a7",
+              cert_details = {
+                expiry_timestamp = 1888983905,
+              }
+            }
+          )
+
         assert.is_truthy(p)
         assert.is_nil(err)
       end)
