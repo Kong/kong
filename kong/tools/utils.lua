75b76745a90f227115e0db63d3d4455494a34a10
Niklaus Schen <8458369+Water-Melon@users.noreply.github.com>
2023-11-20 16:42:42 +0800

    refactor(tools): move function validate_labels from tools.utils to conf_loader (#12051) (#7329)
    KAG-3094
    
    cherry-picked from Kong/kong#12051

--- kong/tools/utils.lua
+++ kong/tools/utils.lua
@@ -15,88 +15,13 @@
 -- @license [Apache 2.0](https://opensource.org/licenses/Apache-2.0)
 -- @module kong.tools.utils
 
-local type     = type
 local pairs    = pairs
 local ipairs   = ipairs
 local require  = require
-local pcall    = pcall
-local fmt      = string.format
 local match    = string.match
-local re_match = ngx.re.match
 
 
 local _M = {}
-
-
-local validate_labels
-do
-  local nkeys = require "table.nkeys"
-
-  local MAX_KEY_SIZE   = 63
-  local MAX_VALUE_SIZE = 63
-  local MAX_KEYS_COUNT = 10
-
-  -- validation rules based on Kong Labels AIP
-  -- https://kong-aip.netlify.app/aip/129/
-  local BASE_PTRN = "[a-z0-9]([\\w\\.:-]*[a-z0-9]|)$"
-  local KEY_PTRN  = "(?!kong)(?!konnect)(?!insomnia)(?!mesh)(?!kic)" .. BASE_PTRN
-  local VAL_PTRN  = BASE_PTRN
-
-  local function validate_entry(str, max_size, pattern)
-    if str == "" or #str > max_size then
-      return nil, fmt(
-        "%s must have between 1 and %d characters", str, max_size)
-    end
-    if not re_match(str, pattern, "ajoi") then
-      return nil, fmt("%s is invalid. Must match pattern: %s", str, pattern)
-    end
-    return true
-  end
-
-  -- Validates a label array.
-  -- Validates labels based on the kong Labels AIP
-  function validate_labels(raw_labels)
-    if nkeys(raw_labels) > MAX_KEYS_COUNT then
-      return nil, fmt(
-        "labels validation failed: count exceeded %d max elements",
-        MAX_KEYS_COUNT
-      )
-    end
-
-    for _, kv in ipairs(raw_labels) do
-      local del = kv:find(":", 1, true)
-      local k = del and kv:sub(1, del - 1) or ""
-      local v = del and kv:sub(del + 1) or ""
-
-      local ok, err = validate_entry(k, MAX_KEY_SIZE, KEY_PTRN)
-      if not ok then
-        return nil, "label key validation failed: " .. err
-      end
-      ok, err = validate_entry(v, MAX_VALUE_SIZE, VAL_PTRN)
-      if not ok then
-        return nil, "label value validation failed: " .. err
-      end
-    end
-
-    return true
-  end
-end
-_M.validate_labels = validate_labels
-
-
-function _M.get_request_id()
-  local ctx = ngx.ctx
-  if ctx.admin_api then
-    return ctx.admin_api.req_id
-  end
-
-  local ok, res = pcall(function() return ngx.var.set_request_id end)
-  if ok and type(res) == "string" and res ~= "" then
-    return res
-  end
-
-  return _M.random_string()
-end
 
 
 --- Extract the parent domain of CN and CN itself from X509 certificate
