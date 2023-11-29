8a5a1ba848d3e4d84f2e4111698fa8dbcb017b53
Brent Yarger <brent.yarger@konghq.com>
2023-11-29 13:14:28 -0800

    Add logic to clear dangling sockets in docker entrypoint

--- build/dockerfiles/entrypoint.sh
+++ build/dockerfiles/entrypoint.sh
@@ -44,6 +44,20 @@ if [[ "$1" == "kong" ]]; then
   if [[ "$2" == "docker-start" ]]; then
     kong prepare -p "$PREFIX" "$@"
 
+    # remove all dangling sockets in $PREFIX dir before starting Kong
+    LOGGED_SOCKET_WARNING=0
+    for localfile in $(find /var/run/kong -type s); do
+      if (( LOGGED_SOCKET_WARNING == 0 )); then
+        printf >&2 'WARN: found dangling unix sockets in the runtime directory '
+        printf >&2 '(%q) ' "/var/run/kong"
+        printf >&2 'while preparing to start Kong. This may be a sign that Kong '
+        printf >&2 'was previously shut down uncleanly or is in an unknown state '
+        printf >&2 'and could require further investigation.\n'
+        LOGGED_SOCKET_WARNING=1
+      fi
+      rm -f "$localfile"
+    done
+
     ln -sfn /dev/stdout $PREFIX/logs/access.log
     ln -sfn /dev/stdout $PREFIX/logs/admin_access.log
     ln -sfn /dev/stderr $PREFIX/logs/error.log
