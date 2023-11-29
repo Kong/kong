6d44e81235738a0466ec158dccfb73cb78af3f5a
Chrono <chrono_cpp@me.com>
2023-11-29 11:15:13 +0800

    feat(templates): add LMDB validation tag directive (#12026)
    This PR adds validation of LMDB cache by Kong's version (major + minor),
    wiping the content if tag mismatch to avoid compatibility issues
    during minor version upgrade.
    
    KAG-3093

--- kong/templates/nginx_inject.lua
+++ kong/templates/nginx_inject.lua
@@ -2,5 +2,10 @@ return [[
 > if database == "off" then
 lmdb_environment_path ${{LMDB_ENVIRONMENT_PATH}};
 lmdb_map_size         ${{LMDB_MAP_SIZE}};
+
+> if lmdb_validation_tag then
+lmdb_validation_tag   $(lmdb_validation_tag);
+> end
+
 > end
 ]]
