ac5b634a2c2df607c26ddd497b5fb8f67b851e95
Chrono <chrono_cpp@me.com>
2023-12-11 17:56:35 +0800

    refactor(tools): remove reference of sha256 from utils (#12095)
    KAG-3226

--- kong/plugins/hmac-auth/access.lua
+++ kong/plugins/hmac-auth/access.lua
@@ -1,7 +1,9 @@
 local constants = require "kong.constants"
-local sha256 = require "resty.sha256"
 local openssl_hmac = require "resty.openssl.hmac"
-local utils = require "kong.tools.utils"
+
+
+local sha256_base64 = require("kong.tools.sha256").sha256_base64
+local string_split = require("kong.tools.string").split
 
 
 local ngx = ngx
@@ -10,7 +12,6 @@ local error = error
 local time = ngx.time
 local abs = math.abs
 local decode_base64 = ngx.decode_base64
-local encode_base64 = ngx.encode_base64
 local parse_time = ngx.parse_http_time
 local re_gmatch = ngx.re.gmatch
 local hmac_sha1 = ngx.hmac_sha1
@@ -115,7 +116,7 @@ local function retrieve_hmac_fields(authorization_header)
     if m and #m >= 4 then
       hmac_params.username = m[1]
       hmac_params.algorithm = m[2]
-      hmac_params.hmac_headers = utils.split(m[3], " ")
+      hmac_params.hmac_headers = string_split(m[3], " ")
       hmac_params.signature = m[4]
     end
   end
@@ -231,9 +232,7 @@ local function validate_body()
     return body == ""
   end
 
-  local digest = sha256:new()
-  digest:update(body or '')
-  local digest_created = "SHA-256=" .. encode_base64(digest:final())
+  local digest_created = "SHA-256=" .. sha256_base64(body or '')
 
   return digest_created == digest_received
 end
--- kong/tools/utils.lua
+++ kong/tools/utils.lua
@@ -19,7 +19,6 @@ local _M = {}
 do
   local modules = {
     "kong.tools.table",
-    "kong.tools.sha256",
     "kong.tools.yield",
     "kong.tools.string",
     "kong.tools.uuid",
