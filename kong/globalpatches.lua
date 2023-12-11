170eaeaeb68f2b16cb5d18f571e14f6759993b63
Xiaochen Wang <wangxiaochen0@gmail.com>
2023-12-11 16:48:19 +0800

    fix(globalpatches): remove timer from SharedDict APIs
    1. It checks the expiration of an item when referring it instead of using timer.
    2. The API `set()` returns the item now, which will be used in the API
       `SharedDict:incr()`.
    3. It introduces a new internal API `get(data, key)` to check and retrieve
       non-expired items.
    4. It fixes the returned ttl value to align with the `ngx.shared.DICT:ttl()` API
       of lua-nginx-module.
    
    KAG-3309

--- kong/globalpatches.lua
+++ kong/globalpatches.lua
@@ -251,6 +251,15 @@ return function(options)
           value = value,
           info = {expire_at = expire_at}
         }
+        return data[key]
+      end
+      local function get(data, key)
+        local item = data[key]
+        if item and item.info.expire_at and item.info.expire_at <= ngx.now() then
+          data[key] = nil
+          item = nil
+        end
+        return item
       end
       function SharedDict:new()
         return setmetatable({data = {}}, {__index = self})
@@ -262,25 +271,18 @@ return function(options)
         return 0
       end
       function SharedDict:get(key)
-        return self.data[key] and self.data[key].value, nil
+        local item = get(self.data, key)
+        return item and item.value, nil
       end
       SharedDict.get_stale = SharedDict.get
       function SharedDict:set(key, value, exptime)
-        local expire_at = nil
-
-        if exptime then
-          ngx.timer.at(exptime, function()
-            self.data[key] = nil
-          end)
-          expire_at = ngx.now() + exptime
-        end
-
+        local expire_at = (exptime and exptime ~= 0) and (ngx.now() + exptime)
         set(self.data, key, value, expire_at)
         return true, nil, false
       end
       SharedDict.safe_set = SharedDict.set
       function SharedDict:add(key, value, exptime)
-        if self.data[key] ~= nil then
+        if get(self.data, key) then
           return false, "exists", false
         end
 
@@ -288,7 +290,7 @@ return function(options)
       end
       SharedDict.safe_add = SharedDict.add
       function SharedDict:replace(key, value)
-        if self.data[key] == nil then
+        if not get(key) then
           return false, "not found", false
         end
         set(self.data, key, value)
@@ -301,23 +303,17 @@ return function(options)
         return true
       end
       function SharedDict:incr(key, value, init, init_ttl)
-        if not self.data[key] then
+        local item = get(self.data, key)
+        if not item then
           if not init then
             return nil, "not found"
-          else
-            self.data[key] = { value = init, info = {} }
-            if init_ttl then
-              self.data[key].info.expire_at = ngx.now() + init_ttl
-              ngx.timer.at(init_ttl, function()
-                self.data[key] = nil
-              end)
-            end
           end
-        elseif type(self.data[key].value) ~= "number" then
+          item = set(self.data, key, init, init_ttl and ngx.now() + init_ttl)
+        elseif type(item.value) ~= "number" then
           return nil, "not a number"
         end
-        self.data[key].value = self.data[key].value + value
-        return self.data[key].value, nil
+        item.value = item.value + value
+        return item.value, nil
       end
       function SharedDict:flush_all()
         for _, item in pairs(self.data) do
@@ -344,11 +340,15 @@ return function(options)
         n = n or 1024
         local i = 0
         local keys = {}
-        for k in pairs(self.data) do
-          keys[#keys+1] = k
-          i = i + 1
-          if n ~= 0 and i == n then
-            break
+        for k, item in pairs(self.data) do
+          if item.info.expire_at and item.info.expire_at <= ngx.now() then
+            self.data[k] = nil
+          else
+            keys[#keys+1] = k
+            i = i + 1
+            if n ~= 0 and i == n then
+              break
+            end
           end
         end
         return keys
@@ -357,19 +357,16 @@ return function(options)
         local item = self.data[key]
         if item == nil then
           return nil, "not found"
-        else
-          local expire_at = item.info.expire_at
-          if expire_at == nil then
-            return 0
-          else
-            local remaining = expire_at - ngx.now()
-            if remaining < 0 then
-              return nil, "not found"
-            else
-              return remaining
-            end
-          end
         end
+        local expire_at = item.info.expire_at
+        if expire_at == nil then
+          return 0
+        end
+        local remaining = expire_at - ngx.now()
+        if remaining == 0 then
+          return nil, "not found"
+        end
+        return remaining
       end
 
       -- hack
