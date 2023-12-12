0ad5058b104716da8f0185ecb33300f4debbcba4
tzssangglass <tzssangglass@gmail.com>
2023-12-12 14:38:29 +0800

    fix ci
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- build/tests/04-uninstall.sh
+++ build/tests/04-uninstall.sh
@@ -11,18 +11,12 @@ remove_kong_command() {
     local pkg_name=""
     local remove_cmd=""
 
-    case "${PACKAGE_TYPE}" in
-        "deb")
-            pkg_name=$(dpkg -l | grep -i kong | awk '{print $2}')
-            remove_cmd="apt-get remove -y $pkg_name"
+    case "${BUILD_LABEL}" in
+        "ubuntu"| "debian")
+            remove_cmd="apt-get remove -y kong"
             ;;
-        "rpm")
-            pkg_name=$(rpm -qa | grep -i kong)
-            remove_cmd="yum remove -y $pkg_name"
-            ;;
-        "apk")
-            pkg_name=$(apk info | grep -i kong)
-            remove_cmd="apk del $pkg_name"
+        "rhel")
+            remove_cmd="yum remove -y kong"
             ;;
         *)
             return 1
@@ -35,10 +29,18 @@ msg_test '"kong" remove command'
 
 remove_command=$(remove_kong_command)
 if [ $? -eq 0 ]; then
-    assert_exec 0 'root' $remove_command
+    docker_exec root "$remove_command"
 else
     err_exit "can not find kong package"
 fi
 
-msg_test "/usr/local/kong does not exist after uninstall"
+msg_test "/usr/local/kong has been removed after uninstall"
 assert_exec 1 'kong' "test -d /usr/local/kong"
+
+# if /usr/local/share/lua/5.1 has other files, it will not be removed
+# only remove files which are installed by kong
+msg_test "/usr/local/share/lua/5.1 has been removed after uninstall"
+assert_exec 1 'kong' "test -d /usr/local/share/lua/5.1"
+
+msg_test "/usr/local/openresty has been removed after uninstall"
+assert_exec 1 'kong' "test -d /usr/local/openresty"
