78da63217d2e2cebd0ac4096ee709948fc94db37
Water-Melon <melon-c@hotmail.com>
2023-12-13 16:18:02 +0800

    fix(tests): execute all shell commands using resty.shell
    Due to the use of pl.utils.execute and os.execute,
    the execution time on both Lua code and the shell
    commands it launches exceeded the timeout set for
    the TCP connection.
    This resulted in abnormal data reception for some
    TCP connections.
    
    KAG-3157

--- spec/03-plugins/37-opentelemetry/05-otelcol_spec.lua
+++ spec/03-plugins/37-opentelemetry/05-otelcol_spec.lua
@@ -75,7 +75,8 @@ for _, strategy in helpers.each_strategy() do
 
       lazy_setup(function()
         -- clear file
-        os.execute("cat /dev/null > " .. OTELCOL_FILE_EXPORTER_PATH)
+        local shell = require "resty.shell"
+        shell.run("cat /dev/null > " .. OTELCOL_FILE_EXPORTER_PATH, nil, 0)
         setup_instrumentations("all")
       end)
 
