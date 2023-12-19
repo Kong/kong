58af785730b7beca229fb77657599a8b6b446889
chronolaw <chrono_cpp@me.com>
2023-12-19 15:09:45 +0800

    remove match_ctx_funcs

--- kong/router/fields.lua
+++ kong/router/fields.lua
@@ -12,14 +12,9 @@ local tb_concat = table.concat
 local replace_dashes_lower = require("kong.tools.string").replace_dashes_lower
 
 
-local is_http = ngx.config.subsystem == "http"
-
-
-local MATCH_CTX_FUNCS
---local CACHE_KEY_FUNCS
-
-
 local FIELDS_FUNCS = {
+    -- http.*
+
     ["http.method"] =
     function(v, params, cb)
       return cb("http.method", params.method)
@@ -73,10 +68,14 @@ local FIELDS_FUNCS = {
       return true
     end,
 
+    -- tls.*
+
     ["tls.sni"] =
     function(v, params, cb)
       return cb("tls.sni", params.sni)
     end,
+
+    -- net.*
 
     ["net.src.ip"] =
     function(v, params, cb)
@@ -101,162 +100,15 @@ local FIELDS_FUNCS = {
     -- below are atc context only
 
     ["net.protocol"] =
-    function(v, params, c)
+    function(v, params, cb)
       return cb("net.protocol", params.scheme)
     end,
 
     ["net.port"] =
-    function(v, params, c)
+    function(v, params, cb)
       return cb("net.port", params.port)
     end,
-
 }
-
-
-if is_http then
-
-
-MATCH_CTX_FUNCS = {
-    ["http.method"] =
-    function(v, params, c)
-      return c:add_value("http.method", params.method)
-    end,
-
-    ["http.path"] =
-    function(v, params, c)
-      return c:add_value("http.path", params.uri)
-    end,
-
-    ["http.host"] =
-    function(v, params, c)
-      return c:add_value("http.host", params.host)
-    end,
-
-    ["tls.sni"] =
-    function(v, params, c)
-      return c:add_value("tls.sni", params.sni)
-    end,
-
-    ["net.protocol"] =
-    function(v, params, c)
-      return c:add_value("net.protocol", params.scheme)
-    end,
-
-    ["net.port"] =
-    function(v, params, c)
-      return c:add_value("net.port", params.port)
-    end,
-
-    ["http.headers."] =
-    function(v, params, c)
-      local headers = params.headers
-      if not headers then
-        return true
-      end
-
-      for _, h in ipairs(v) do
-        local v = headers[h]
-        local f = "http.headers." .. h
-
-        if type(v) == "string" then
-          local res, err = c:add_value(f, v)
-          if not res then
-            return nil, err
-          end
-
-        elseif type(v) == "table" then
-          for _, v in ipairs(v) do
-            local res, err = c:add_value(f, v)
-            if not res then
-              return nil, err
-            end
-          end
-        end -- if type(v)
-      end   -- for ipairs(v)
-
-      return true
-    end,
-
-    ["http.queries."] =
-    function(v, params, c)
-      local queries = params.queries
-      if not queries then
-        return true
-      end
-
-      for _, n in ipairs(v) do
-        local v = queries[n]
-        local f = "http.queries." .. n
-
-        -- the query parameter has only one value, like /?foo=bar
-        if type(v) == "string" then
-          local res, err = c:add_value(f, v)
-          if not res then
-            return nil, err
-          end
-
-        -- the query parameter has no value, like /?foo,
-        -- get_uri_arg will get a boolean `true`
-        -- we think it is equivalent to /?foo=
-        elseif type(v) == "boolean" then
-          local res, err = c:add_value(f, "")
-          if not res then
-            return nil, err
-          end
-
-        -- multiple values for a single query parameter, like /?foo=bar&foo=baz
-        elseif type(v) == "table" then
-          for _, v in ipairs(v) do
-            local res, err = c:add_value(f, v)
-            if not res then
-              return nil, err
-            end
-          end
-        end -- if type(v)
-      end   -- for ipairs(v)
-
-      return true
-    end,
-}
-
-
-else -- stream subsystem
-
-
-MATCH_CTX_FUNCS = {
-    ["net.src.ip"] =
-    function(v, params, c)
-      return c:add_value("net.src.ip", params.src_ip)
-    end,
-
-    ["net.src.port"] =
-    function(v, params, c)
-      return c:add_value("net.src.port", params.src_port)
-    end,
-
-    ["net.dst.ip"] =
-    function(v, params, c)
-      return c:add_value("net.dst.ip", params.dst_ip)
-    end,
-
-    ["net.dst.port"] =
-    function(v, params, c)
-      return c:add_value("net.dst.port", params.dst_port)
-    end,
-
-    ["tls.sni"] =
-    function(v, params, c)
-      return c:add_value("tls.sni", params.sni)
-    end,
-
-    ["net.protocol"] =
-    function(v, params, c)
-      return c:add_value("net.protocol", params.scheme)
-    end,
-}
-
-
-end -- is_http
 
 
 -- cache key string
@@ -267,8 +119,7 @@ local function get_cache_key(fields, params)
   for field, value in pairs(fields) do
 
     -- these fields were not in cache key
-    if field == "http.scheme"  or
-       field == "net.protocol" or
+    if field == "net.protocol" or
        field == "net.port"
     then
       goto continue
@@ -319,15 +170,13 @@ local function get_atc_context(schema, fields, params)
   local c = context.new(schema)
 
   for field, value in pairs(fields) do
-    local func = MATCH_CTX_FUNCS[field]
-    --local func = FIELDS_FUNCS[field]
+    local func = FIELDS_FUNCS[field]
     if not func then  -- unknown field
       error("unknown router matching schema field: " .. field)
     end
 
     assert(value)
 
-    --[[
     local res, err = func(value, params, function(field, value)
       local headers_or_queries = field:sub(1, 13)
 
@@ -339,13 +188,27 @@ local function get_atc_context(schema, fields, params)
       end
 
       if headers_or_queries then
-        if type(value) == "table" then
-          tb_sort(value)
-          value = tb_concat(value, ",")
-        end
+        local v_type = type(value)
 
-        value = fmt("%s=%s", field, value)
-      end
+        -- multiple values for a single query parameter, like /?foo=bar&foo=baz
+        if v_type == "table" then
+          for _, v in ipairs(value) do
+            local res, err = c:add_value(field, v)
+            if not res then
+              return nil, err
+            end
+          end
+
+          return true
+
+        -- the query parameter has only one value, like /?foo=bar
+        -- the query parameter has no value, like /?foo,
+        -- get_uri_arg will get a boolean `true`
+        -- we think it is equivalent to /?foo=
+        elseif v_type == "boolean" then
+          value = ""
+        end -- if v_type
+      end   -- if headers_or_queries
 
       return c:add_value(field, value)
     end)
@@ -353,12 +216,7 @@ local function get_atc_context(schema, fields, params)
     if not res then
       return nil, err
     end
-    --]]
 
-    local res, err = func(value, params, c)
-    if not res then
-      return nil, err
-    end
   end -- for fields
 
   return c
