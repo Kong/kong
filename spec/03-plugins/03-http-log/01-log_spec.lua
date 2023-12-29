a7062f792a72b0042c45485c4b8ddb74d3c1e159
chronolaw <chrono_cpp@me.com>
2023-12-29 12:07:56 +0800

    tests(plugins): fix previous `master` test failures
    Fix `03-http-log/01-log_spec.lua`
    Fix `13-cors/01-access_spec.lua`
    Fix `spec/03-plugins/03-http-log/01-log_spec.lua`

--- spec/03-plugins/03-http-log/01-log_spec.lua
+++ spec/03-plugins/03-http-log/01-log_spec.lua
@@ -69,6 +69,7 @@ for _, strategy in helpers.each_strategy() do
 
       local route1 = bp.routes:insert {
         hosts   = { "http_logging.test" },
+        paths   = { "/" },
         service = service1
       }
 
@@ -673,6 +674,7 @@ for _, strategy in helpers.each_strategy() do
 
         local route = bp.routes:insert {
           hosts   = { "http_queue_logging.test" },
+          paths   = { "/" },
           service = service
         }
 
