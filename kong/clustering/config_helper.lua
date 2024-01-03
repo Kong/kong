ac319014badb112b9b0cce8cf767bd34e796a033
Michael Martin <michael.martin@konghq.com>
2024-01-02 16:31:46 -0800

    feat(clustering): report config reload errors to Konnect
    Data-plane nodes running will now report config reload failures such as
    invalid configuration or transient errors to the control-plane.
    
    (cherry picked from commit 3ad0280b6e5368c4f4e25a1a014aabd3ea02c931)

--- kong/clustering/config_helper.lua
+++ kong/clustering/config_helper.lua
@@ -12,6 +12,7 @@ local isempty = require("table.isempty")
 local isarray = require("table.isarray")
 local nkeys = require("table.nkeys")
 local buffer = require("string.buffer")
+local db_errors = require("kong.db.errors")
 
 
 local tostring = tostring
@@ -24,6 +25,7 @@ local sort = table.sort
 local yield = require("kong.tools.yield").yield
 local fetch_table = tablepool.fetch
 local release_table = tablepool.release
+local xpcall = xpcall
 
 
 local ngx_log = ngx.log
@@ -36,6 +38,7 @@ local ngx_DEBUG = ngx.DEBUG
 
 
 local DECLARATIVE_EMPTY_CONFIG_HASH = constants.DECLARATIVE_EMPTY_CONFIG_HASH
+local ERRORS = constants.CLUSTERING_DATA_PLANE_ERROR
 local _log_prefix = "[clustering] "
 
 
@@ -209,8 +212,96 @@ local function fill_empty_hashes(hashes)
   end
 end
 
-function _M.update(declarative_config, msg)
 
+--- Errors returned from _M.update() should have these fields
+---
+---@class kong.clustering.config_helper.update.err_t.base
+---
+---@field name        string # identifier that can be used to classify the error type
+---@field source      string # lua function that is responsible for this error
+---@field message     string # error description/contents
+---@field config_hash string
+
+
+--- Error returned when something causes an exception to be thrown
+---
+---@class kong.clustering.config_helper.update.err_t.exception : kong.clustering.config_helper.update.err_t.base
+---
+---@field exception any    # value that was passed to `error()`
+---@field traceback string # lua traceback of the exception
+
+
+--- Error returned when the configuration received from the control plane is
+--- not valid
+---
+---@class kong.clustering.config_helper.update.err_t.declarative : kong.clustering.config_helper.update.err_t.base
+---
+---@field flattened_errors table
+---@field fields           table
+---@field code?            integer
+
+
+--- Error returned when the act of reloading the local configuration failed
+---
+---@class kong.clustering.config_helper.update.err_t.reload : kong.clustering.config_helper.update.err_t.base
+
+
+---@alias kong.clustering.config_helper.update.err_t
+---| kong.clustering.config_helper.update.err_t.exception
+---| kong.clustering.config_helper.update.err_t.declarative
+---| kong.clustering.config_helper.update.err_t.reload
+
+
+---@param err_t kong.clustering.config_helper.update.err_t
+---@param msg kong.clustering.config_helper.update.msg
+local function format_error(err_t, msg)
+  err_t.source       = err_t.source      or "kong.clustering.config_helper.update"
+  err_t.name         = err_t.name        or ERRORS.GENERIC
+  err_t.message      = err_t.message     or "an unexpected error occurred"
+  err_t.config_hash  = msg.config_hash   or DECLARATIVE_EMPTY_CONFIG_HASH
+
+  -- Declarative config parse errors will include all the input entities in
+  -- the error table. Strip these out to keep the error payload size small.
+  local errors = err_t.flattened_errors
+  if type(errors) == "table" then
+    for i = 1, #errors do
+      local err = errors[i]
+      if type(err) == "table" then
+        err.entity = nil
+      end
+    end
+  end
+end
+
+
+---@param err any # whatever was passed to `error()`
+---@return kong.clustering.config_helper.update.err_t.exception err_t
+local function format_exception(err)
+  return {
+    name      = ERRORS.RELOAD,
+    source    = "kong.clustering.config_helper.update",
+    message   = "an exception was raised while updating the configuration",
+    exception = err,
+    traceback = debug.traceback(tostring(err), 1),
+  }
+end
+
+
+---@class kong.clustering.config_helper.update.msg : table
+---
+---@field config_table            table
+---@field config_hash             string
+---@field hashes                  table<string, string>
+---@field current_transaction_id? string|number
+
+
+---@param declarative_config table
+---@param msg kong.clustering.config_helper.update.msg
+---
+---@return boolean? success
+---@return string? err
+---@return kong.clustering.config_helper.update.err_t? err_t
+local function update(declarative_config, msg)
   local config_table = msg.config_table
   local config_hash = msg.config_hash
   local hashes = msg.hashes
@@ -219,6 +310,11 @@ function _M.update(declarative_config, msg)
 
   if not config_hash then
     config_hash, hashes = calculate_config_hash(config_table)
+
+    -- update the message in-place with the calculated hashes so that this
+    -- metadata can be used in error-reporting
+    msg.config_hash = config_hash
+    msg.hashes = hashes
   end
 
   if hashes then
@@ -232,10 +328,16 @@ function _M.update(declarative_config, msg)
     return true
   end
 
-  local entities, err, _, meta, new_hash =
-  declarative_config:parse_table(config_table, config_hash)
+  local entities, err, err_t, meta, new_hash =
+    declarative_config:parse_table(config_table, config_hash)
   if not entities then
-    return nil, "bad config received from control plane " .. err
+    ---@type kong.clustering.config_helper.update.err_t.declarative
+    err_t = db_errors:declarative_config_flattened(err_t, config_table)
+
+    err_t.name = ERRORS.CONFIG_PARSE
+    err_t.source = "kong.db.declarative.parse_table"
+
+    return nil, "bad config received from control plane " .. err, err_t
   end
 
   if current_hash == new_hash then
@@ -250,15 +352,50 @@ function _M.update(declarative_config, msg)
   local res
   res, err = declarative.load_into_cache_with_events(entities, meta, new_hash, hashes, msg.current_transaction_id)
   if not res then
-    return nil, err
+    ---@type kong.clustering.config_helper.update.err_t.reload
+    err_t = {
+      name = ERRORS.RELOAD,
+      source = "kong.db.declarative.load_into_cache_with_events",
+      message = err,
+    }
+
+    return nil, err, err_t
   end
 
-  if kong.configuration.log_level == "debug" and msg.current_transaction_id then
-     ngx_log(ngx.DEBUG, _log_prefix, "loaded configuration with transaction ID " .. msg.current_transaction_id)
+  if kong.configuration.log_level == "debug" then
+     ngx_log(ngx.DEBUG, _log_prefix, "loaded configuration with transaction ID ",
+             msg.current_transaction_id)
   end
 
   return true
 end
+
+
+---@param declarative_config table
+---@param msg kong.clustering.config_helper.update.msg
+---
+---@return boolean? success
+---@return string? err
+---@return kong.clustering.config_helper.update.err_t? err_t
+function _M.update(declarative_config, msg)
+  local pok, ok_or_err, err, err_t = xpcall(update, format_exception,
+                                            declarative_config, msg)
+
+  local ok = pok and ok_or_err
+
+  if not pok then
+    err_t = ok_or_err --[[@as kong.clustering.config_helper.update.err_t.exception]]--
+    -- format_exception() captures the original error in the .exception field
+    err = err_t.exception or "unknown error"
+  end
+
+  if not ok and err_t then
+    format_error(err_t, msg)
+  end
+
+  return ok, err, err_t
+end
+
 
 
 _M.calculate_config_hash = calculate_config_hash
