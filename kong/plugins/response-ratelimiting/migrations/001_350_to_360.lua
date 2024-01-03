58068918d2b93383bae38e377ff5df05268194bd
Mikołaj Nowak <mikolaj.nowak@konghq.com>
2024-01-03 18:43:09 +0100

    WIP9

--- kong/plugins/response-ratelimiting/migrations/001_350_to_360.lua
+++ kong/plugins/response-ratelimiting/migrations/001_350_to_360.lua
@@ -1,39 +1,36 @@
 return {
     postgres = {
       up = [[
-        DO $$
-        BEGIN
-          UPDATE plugins
-          SET config =
-            config::jsonb
-                - 'redis_host'
-                - 'redis_port'
-                - 'redis_password'
-                - 'redis_username'
-                - 'redis_ssl'
-                - 'redis_ssl_verify'
-                - 'redis_server_name'
-                - 'redis_timeout'
-                - 'redis_database'
-            || jsonb_build_object(
-                'redis',
-                jsonb_build_object(
-                    'base',
-                    jsonb_build_object(
-                        'host', config->'redis_host',
-                        'port', config->'redis_port',
-                        'password', config->'redis_password',
-                        'username', config->'redis_username',
-                        'ssl', config->'redis_ssl',
-                        'ssl_verify', config->'redis_ssl_verify',
-                        'server_name', config->'redis_server_name',
-                        'timeout', config->'redis_timeout',
-                        'database', config->'redis_database'
-                    )
-                )
-            )
-            WHERE name = 'rate-limiting';
-        END$$;
+        UPDATE plugins
+        SET config =
+          config::jsonb
+              - 'redis_host'
+              - 'redis_port'
+              - 'redis_password'
+              - 'redis_username'
+              - 'redis_ssl'
+              - 'redis_ssl_verify'
+              - 'redis_server_name'
+              - 'redis_timeout'
+              - 'redis_database'
+          || jsonb_build_object(
+              'redis',
+              jsonb_build_object(
+                  'base',
+                  jsonb_build_object(
+                      'host', config->'redis_host',
+                      'port', config->'redis_port',
+                      'password', config->'redis_password',
+                      'username', config->'redis_username',
+                      'ssl', config->'redis_ssl',
+                      'ssl_verify', config->'redis_ssl_verify',
+                      'server_name', config->'redis_server_name',
+                      'timeout', config->'redis_timeout',
+                      'database', config->'redis_database'
+                  )
+              )
+          )
+          WHERE name = 'rate-limiting';
       ]],
     },
 }
