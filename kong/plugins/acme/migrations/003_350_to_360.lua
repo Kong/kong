370f1a6223e482675a872737ac70aa7159fdd10d
Mikołaj Nowak <mikolaj.nowak@konghq.com>
2024-01-03 14:52:52 +0100

    WIP3

--- kong/plugins/acme/migrations/003_350_to_360.lua
+++ kong/plugins/acme/migrations/003_350_to_360.lua
@@ -1,43 +1,43 @@
 return {
-    postgres = {
-      up = [[
-        DO $$
-        BEGIN
-          UPDATE plugins
-          SET config =
-            config
-                #- '{storage_config,redis}'
+    -- postgres = {
+      -- up = [[
+      --   DO $$
+      --   BEGIN
+      --     UPDATE plugins
+      --     SET config =
+      --       config
+      --           #- '{storage_config,redis}'
 
-            || jsonb_build_object(
-                'storage_config',
-                (config -> 'storage_config') - 'redis'
-                || jsonb_build_object(
-                  'redis',
-                    jsonb_build_object(
-                        'base',
-                        jsonb_build_object(
-                            'host', config #> '{storage_config, redis, host}',
-                            'port', config #> '{storage_config, redis, port}',
-                            'password', config #> '{storage_config, redis, auth}',
-                            'username', config #> '{storage_config, redis, username}',
-                            'ssl', config #> '{storage_config, redis, ssl}',
-                            'ssl_verify', config #> '{storage_config, redis, ssl_verify}',
-                            'server_name', config #> '{storage_config, redis, ssl_server_name}',
-                            'timeout', config #> '{storage_config, redis, timeout}',
-                            'database', config #> '{storage_config, redis, database}'
-                        )
-                    )
-                  || jsonb_build_object(
-                      'extra_options',
-                      jsonb_build_object(
-                          'scan_count', config #> '{storage_config, redis, scan_count}',
-                          'namespace', config #> '{storage_config, redis, namespace}'
-                      )
-                  )
-                )
-              )
-            WHERE name = 'acme';
-        END$$;
-      ]],
-    },
+      --       || jsonb_build_object(
+      --           'storage_config',
+      --           (config -> 'storage_config') - 'redis'
+      --           || jsonb_build_object(
+      --             'redis',
+      --               jsonb_build_object(
+      --                   'base',
+      --                   jsonb_build_object(
+      --                       'host', config #> '{storage_config, redis, host}',
+      --                       'port', config #> '{storage_config, redis, port}',
+      --                       'password', config #> '{storage_config, redis, auth}',
+      --                       'username', config #> '{storage_config, redis, username}',
+      --                       'ssl', config #> '{storage_config, redis, ssl}',
+      --                       'ssl_verify', config #> '{storage_config, redis, ssl_verify}',
+      --                       'server_name', config #> '{storage_config, redis, ssl_server_name}',
+      --                       'timeout', config #> '{storage_config, redis, timeout}',
+      --                       'database', config #> '{storage_config, redis, database}'
+      --                   )
+      --               )
+      --             || jsonb_build_object(
+      --                 'extra_options',
+      --                 jsonb_build_object(
+      --                     'scan_count', config #> '{storage_config, redis, scan_count}',
+      --                     'namespace', config #> '{storage_config, redis, namespace}'
+      --                 )
+      --             )
+      --           )
+      --         )
+      --       WHERE name = 'acme';
+      --   END$$;
+      -- ]],
+    -- },
 }
