33479b4f8a97263a8b6059ecc7032e8eec661140
Xiaochen Wang <wangxiaochen0@gmail.com>
2024-01-08 21:06:53 +0800

    improve shared_dict.lua

--- spec/fixtures/shared_dict.lua
+++ spec/fixtures/shared_dict.lua
@@ -1,33 +1,41 @@
---- resty options executed by bin/busted
-return
+--- generate resty `--shdict` options executed by bin/busted
+
+local dicts = {
   -- http shared dicts
-  "--shdict 'kong 5m' " ..
-  "--shdict 'kong_locks 8m' " ..
-  "--shdict 'kong_healthchecks 5m' " ..
-  "--shdict 'kong_cluster_events 5m' " ..
-  "--shdict 'kong_rate_limiting_counters 12m' " ..
-  "--shdict 'kong_core_db_cache 16m' " ..
-  "--shdict 'kong_core_db_cache_miss 16m' " ..
-  "--shdict 'kong_db_cache 16m' " ..
-  "--shdict 'kong_db_cache_2 16m' " ..
-  "--shdict 'kong_db_cache_miss 12m' " ..
-  "--shdict 'kong_db_cache_miss_2 12m' " ..
-  "--shdict 'kong_mock_upstream_loggers 10m' " ..
-  "--shdict 'kong_secrets 5m' " ..
-  "--shdict 'test_vault 5m' " ..
-  "--shdict 'prometheus_metrics 5m' " ..
+  "kong 5m",
+  "kong_locks 8m",
+  "kong_healthchecks 5m",
+  "kong_cluster_events 5m",
+  "kong_rate_limiting_counters 12m",
+  "kong_core_db_cache 16m",
+  "kong_core_db_cache_miss 16m",
+  "kong_db_cache 16m",
+  "kong_db_cache_2 16m",
+  "kong_db_cache_miss 12m",
+  "kong_db_cache_miss_2 12m",
+  "kong_mock_upstream_loggers 10m",
+  "kong_secrets 5m",
+  "test_vault 5m",
+  "prometheus_metrics 5m",
 
   -- stream shared dicts
-  "--shdict 'stream_kong 5m' " ..
-  "--shdict 'stream_kong_locks 8m' " ..
-  "--shdict 'stream_kong_healthchecks 5m' " ..
-  "--shdict 'stream_kong_cluster_events 5m' " ..
-  "--shdict 'stream_kong_rate_limiting_counters 12m' " ..
-  "--shdict 'stream_kong_core_db_cache 16m' " ..
-  "--shdict 'stream_kong_core_db_cache_miss 16m' " ..
-  "--shdict 'stream_kong_db_cache 16m' " ..
-  "--shdict 'stream_kong_db_cache_2 16m' " ..
-  "--shdict 'stream_kong_db_cache_miss 12m' " ..
-  "--shdict 'stream_kong_db_cache_miss_2 12m' " ..
-  "--shdict 'stream_kong_secrets 5m' " ..
-  "--shdict 'stream_prometheus_metrics 5m' "
+  "stream_kong 5m",
+  "stream_kong_locks 8m",
+  "stream_kong_healthchecks 5m",
+  "stream_kong_cluster_events 5m",
+  "stream_kong_rate_limiting_counters 12m",
+  "stream_kong_core_db_cache 16m",
+  "stream_kong_core_db_cache_miss 16m",
+  "stream_kong_db_cache 16m",
+  "stream_kong_db_cache_2 16m",
+  "stream_kong_db_cache_miss 12m",
+  "stream_kong_db_cache_miss_2 12m",
+  "stream_kong_secrets 5m",
+  "stream_prometheus_metrics 5m",
+}
+
+for i, v in ipairs(dicts) do
+  dicts[i] = "--shdict '" .. v .. "'"
+end
+
+return table.concat(dicts, " ")
