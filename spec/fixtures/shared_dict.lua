7d0454886d84f43c096bfd04d9cb7acabdfb6e97
Xiaochen Wang <wangxiaochen0@gmail.com>
2024-01-08 15:47:43 +0800

    add missing shared dict into spec/fixtures/shared_dict.lua

--- spec/fixtures/shared_dict.lua
+++ spec/fixtures/shared_dict.lua
@@ -2,27 +2,32 @@
 return
   -- http shared dicts
   "--shdict 'kong 5m' " ..
+  "--shdict 'kong_locks 8m' " ..
+  "--shdict 'kong_healthchecks 5m' " ..
+  "--shdict 'kong_cluster_events 5m' " ..
+  "--shdict 'kong_rate_limiting_counters 12m' " ..
+  "--shdict 'kong_core_db_cache 16m' " ..
+  "--shdict 'kong_core_db_cache_miss 16m' " ..
   "--shdict 'kong_db_cache 16m' " ..
   "--shdict 'kong_db_cache_2 16m' " ..
   "--shdict 'kong_db_cache_miss 12m' " ..
   "--shdict 'kong_db_cache_miss_2 12m' " ..
-  "--shdict 'kong_secrets 5m' " ..
-  "--shdict 'kong_locks 8m' " ..
-  "--shdict 'kong_cluster_events 5m' " ..
-  "--shdict 'kong_healthchecks 5m' " ..
-  "--shdict 'kong_rate_limiting_counters 12m' " ..
   "--shdict 'kong_mock_upstream_loggers 10m' " ..
+  "--shdict 'kong_secrets 5m' " ..
   "--shdict 'test_vault 5m' " ..
   "--shdict 'prometheus_metrics 5m' " ..
 
   -- stream shared dicts
   "--shdict 'stream_kong 5m' " ..
+  "--shdict 'stream_kong_locks 8m' " ..
+  "--shdict 'stream_kong_healthchecks 5m' " ..
+  "--shdict 'stream_kong_cluster_events 5m' " ..
+  "--shdict 'stream_kong_rate_limiting_counters 12m' " ..
+  "--shdict 'stream_kong_core_db_cache 16m' " ..
+  "--shdict 'stream_kong_core_db_cache_miss 16m' " ..
   "--shdict 'stream_kong_db_cache 16m' " ..
   "--shdict 'stream_kong_db_cache_2 16m' " ..
   "--shdict 'stream_kong_db_cache_miss 12m' " ..
   "--shdict 'stream_kong_db_cache_miss_2 12m' " ..
-  "--shdict 'stream_kong_locks 8m' " ..
-  "--shdict 'stream_kong_cluster_events 5m' " ..
-  "--shdict 'stream_kong_healthchecks 5m' " ..
-  "--shdict 'stream_kong_rate_limiting_counters 12m' " ..
+  "--shdict 'stream_kong_secrets 5m' " ..
   "--shdict 'stream_prometheus_metrics 5m' "
