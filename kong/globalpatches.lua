773fe90191c1507335240b4d858b85630278205d
Yufu Zhao <ms2008vip@gmail.com>
2024-01-11 11:02:33 +0800

    fix(globalpatches): moved `require kong.constants` into the closure (#12328)
    This is necessary because EE has changed `package.path` in this closure,
    and we continue leave that at the top level of this file, EE's e2e
    test will report the following error:
    
    ```
    stack traceback:
            /usr/local/share/lua/5.1/kong/constants.lua:8: in main chunk
            [C]: in function 'require'
            /usr/local/share/lua/5.1/kong/globalpatches.lua:8: in main chunk
            [C]: in function 'require'
            /usr/local/bin/kong:6: in function 'file_gen'
            init_worker_by_lua(nginx.conf:118):46: in function <init_worker_by_lua(nginx.conf:118):44>
            [C]: in function 'xpcall'
            init_worker_by_lua(nginx.conf:118):53: in function <init_worker_by_lua(nginx.conf:118):51>
    ```

--- kong/globalpatches.lua
+++ kong/globalpatches.lua
@@ -5,8 +5,6 @@
 -- at https://konghq.com/enterprisesoftwarelicense/.
 -- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
 
-local constants = require "kong.constants"
-
 local ran_before
 
 
@@ -26,6 +24,7 @@ return function(options)
   package.path = package.path .. ";/usr/local/share/lua/5.1/?.ljbc;/usr/local/share/lua/5.1/?/init.ljbc"
 
   local meta = require "kong.meta"
+  local constants = require "kong.constants"
 
 
   local cjson_safe = require("cjson.safe")
