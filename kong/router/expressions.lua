560fdfb599da87666e3d5e86d1b7ac59eb64e479
Chrono <chrono_cpp@me.com>
2024-01-12 15:42:56 +0800

    feat(router/atc): support `net.src.*` and `net.dst.*` fields in HTTP expression routes (#11950)
    These fields are available in stream (L4) routes, making them also available in HTTP to reduce discrepancy between HTTP and stream routes.
    
    KAG-2963
    KAG-3032

--- kong/router/expressions.lua
+++ kong/router/expressions.lua
@@ -1,11 +1,16 @@
 local _M = {}
 
 
+local re_gsub = ngx.re.gsub
+
+
 local atc = require("kong.router.atc")
 local gen_for_field = atc.gen_for_field
 
 
 local OP_EQUAL    = "=="
+local NET_PORT_REG = [[(net\.port)(\s*)([=><!])]]
+local NET_PORT_REPLACE = [[net.dst.port$2$3]]
 
 
 local LOGICAL_AND = atc.LOGICAL_AND
@@ -19,8 +24,35 @@ local PROTOCOLS_OVERRIDE = {
 }
 
 
-local function get_exp_and_priority(route)
+-- net.port => net.dst.port
+local function transform_expression(route)
   local exp = route.expression
+
+  if not exp then
+    return nil
+  end
+
+  if not exp:find("net.port", 1, true) then
+    return exp
+  end
+
+  -- there is "net.port" in expression
+
+  local new_exp = re_gsub(exp, NET_PORT_REG, NET_PORT_REPLACE, "jo")
+
+  if exp ~= new_exp then
+    ngx.log(ngx.WARN, "The field 'net.port' of expression is deprecated " ..
+                      "and will be removed in the upcoming major release, " ..
+                      "please use 'net.dst.port' instead.")
+  end
+
+  return new_exp
+end
+_M.transform_expression = transform_expression
+
+
+local function get_exp_and_priority(route)
+  local exp = transform_expression(route)
   if not exp then
     ngx.log(ngx.ERR, "expecting an expression route while it's not (probably a traditional route). ",
                      "Likely it's a misconfiguration. Please check the 'router_flavor' config in kong.conf")
