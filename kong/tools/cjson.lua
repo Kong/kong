b8263863e92e87f6982ecc717e417252d978f7e6
Chrono <chrono_cpp@me.com>
2024-01-15 09:32:32 +0800

    style(tools): simplify the implementation of tools.cjson (#12316)
    Simplify the implementation of #12019.
    
    (cherry picked from commit 00bab1874ab4fcac91116144f00ce99a7aa5681b)

--- kong/tools/cjson.lua
+++ kong/tools/cjson.lua
@@ -6,23 +6,22 @@
 -- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
 
 local cjson = require "cjson.safe".new()
-local constants = require "kong.constants"
+local CJSON_MAX_PRECISION = require "kong.constants".CJSON_MAX_PRECISION
+
 
 cjson.decode_array_with_array_mt(true)
 cjson.encode_sparse_array(nil, nil, 2^15)
-cjson.encode_number_precision(constants.CJSON_MAX_PRECISION)
+cjson.encode_number_precision(CJSON_MAX_PRECISION)
+
 
 local _M = {}
 
 
-function _M.encode(json_text)
-  return cjson.encode(json_text)
-end
+_M.encode = cjson.encode
+_M.decode_with_array_mt = cjson.decode
 
-function _M.decode_with_array_mt(json_text)
-  return cjson.decode(json_text)
-end
 
 _M.array_mt = cjson.array_mt
+
 
 return _M
