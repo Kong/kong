3503f1550cb2dc2a8735a390976a40a73d37bb7a
chronolaw <chrono_cpp@me.com>
2024-01-17 17:42:10 +0800

    assert.not_match_re

--- spec/01-unit/04-prefix_handler_spec.lua
+++ spec/01-unit/04-prefix_handler_spec.lua
@@ -186,6 +186,8 @@ describe("NGINX conf compiler", function()
       assert.matches("listen%s+127%.0%.0%.1:8445 ssl;", kong_nginx_conf)
 
       assert.match_re(kong_nginx_conf, [[server_name kong;\n.+\n.+\n\n\s+http2 on;]])
+      assert.not_match_re(kong_nginx_conf, [[server_name kong_admin;\n.+\n.+\n\n\s+http2 on;]])
+      assert.not_match_re(kong_nginx_conf, [[server_name kong_gui;\n.+\n.+\n\n\s+http2 on;]])
 
       conf = assert(conf_loader(helpers.test_conf_path, {
         proxy_listen = "0.0.0.0:9000, 0.0.0.0:9443 ssl",
@@ -200,6 +202,8 @@ describe("NGINX conf compiler", function()
       assert.matches("listen%s+127%.0%.0%.1:8445 ssl;", kong_nginx_conf)
 
       assert.match_re(kong_nginx_conf, [[server_name kong_admin;\n.+\n.+\n\n\s+http2 on;]])
+      assert.not_match_re(kong_nginx_conf, [[server_name kong;\n.+\n.+\n\n\s+http2 on;]])
+      assert.not_match_re(kong_nginx_conf, [[server_name kong_gui;\n.+\n.+\n\n\s+http2 on;]])
 
       conf = assert(conf_loader(helpers.test_conf_path, {
         proxy_listen = "0.0.0.0:9000, 0.0.0.0:9443 ssl",
@@ -214,6 +218,8 @@ describe("NGINX conf compiler", function()
       assert.matches("listen%s+127%.0%.0%.1:8445 ssl;", kong_nginx_conf)
 
       assert.match_re(kong_nginx_conf, [[server_name kong_gui;\n.+\n.+\n\n\s+http2 on;]])
+      assert.not_match_re(kong_nginx_conf, [[server_name kong;\n.+\n.+\n\n\s+http2 on;]])
+      assert.not_match_re(kong_nginx_conf, [[server_name kong_admin;\n.+\n.+\n\n\s+http2 on;]])
     end)
     it("enables proxy_protocol", function()
       local conf = assert(conf_loader(helpers.test_conf_path, {
