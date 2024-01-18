dab212b350247c5adc48520b2ae7603b76f61368
chronolaw <chrono_cpp@me.com>
2024-01-18 17:37:15 +0800

    add tools.http.get_header

--- kong/tools/http.lua
+++ kong/tools/http.lua
@@ -527,4 +527,28 @@ do
 end
 
 
+do
+  local get_headers = ngx.req.get_headers
+
+  function _M.get_header(ctx, name)
+    if type(ctx) ~= "table" then
+      return nil, "ctx must be a table"
+    end
+
+    if type(name) ~= "string" then
+      return nil, "header name must be a string"
+    end
+
+    if not ctx.headers then
+      ctx.headers = get_headers()
+    end
+
+    local value = ctx.headers[name]
+
+    return type(value) == "table" and
+           value[1] or value
+  end
+end
+
+
 return _M
