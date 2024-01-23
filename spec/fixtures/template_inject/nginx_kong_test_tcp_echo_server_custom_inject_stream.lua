664d0d4018088483622d029f693559c1b781176c
Michael Martin <flrgh@protonmail.com>
2024-01-22 23:28:37 -0600

    [cherry-pick -> master] fix(tests): fix malloc assertion error by replacing thread tcp server (#12191) (#7871)
    * fix(tests): fix malloc assertion error by replacing thread tcp server (#12191)
    
    * fix(tests): fix malloc assertion error by replacing thread tcp server
    
    Due to many OpenSSL objects are not thread-safe and it causes
    intermittent crash when those objects are used in thread based tcp
    server. Replace it with process based tcp server resolves this error.
    
    This change only replaces the thread tcp server usage in
    11-reports_spec.lua file to keep the scope small.
    
    Before this fix, crash like "malloc_consolidate(): unaligned fastbin
    chunk detected." happens when running spec/01-unit/11-reports_spec.lua
    about 200 times.
    
    After this fix, there is no crash after running the same test suites
    more than 5000 times.
    
    * Use mock server in kong instead of using standalone nginx process.
    
    All tests in 11-reports_spec.lua passes. The single test case time
    becomes slow compared with standalone nginx process:
    
    thread tcp_server:         30ms
    standalone process nginx : 130ms
    kong mock server :         2000ms
    
    * Remove change log.
    
    * Share the tcp server between test cases in a test file.
    
    Before this change, the 11-reports_spec.lua runtime is about 40
    seconds. After this change, the runtime is 2.6 seconds. The current
    mainline runtime is 0.5 seconds.
    
    * Integrate the tcp echo server into default start_kong() config.
    
    * Update according to review comments.
    
    * chore(*): add copyright header to new file
    
    ---------
    
    Co-authored-by: Zhongwei Yao <zhongwei.yao@konghq.com>

---
+++ spec/fixtures/template_inject/nginx_kong_test_tcp_echo_server_custom_inject_stream.lua
@@ -0,0 +1,30 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+
+return [[
+server {
+    listen 8188;
+    listen 8189 ssl;
+
+> for i = 1, #ssl_cert do
+    ssl_certificate     $(ssl_cert[i]);
+    ssl_certificate_key $(ssl_cert_key[i]);
+> end
+    ssl_protocols TLSv1.1 TLSv1.2 TLSv1.3;
+
+    content_by_lua_block {
+        local sock = assert(ngx.req.socket())
+        local data = sock:receive()  -- read a line from downstream
+        if data then
+            sock:send(data.."\n") -- echo whatever was sent
+            ngx.log(ngx.INFO, "received data: " .. data)
+        else
+            ngx.log(ngx.WARN, "Nothing received")
+        end
+    }
+}
+]]
