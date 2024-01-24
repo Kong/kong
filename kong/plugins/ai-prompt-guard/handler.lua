5b1a853af8ce98f796d3b88e4ea4d103a81723be
Jack Tysoe <jack.tysoe@konghq.com>
2024-01-24 12:32:56 +0000

    fix(ai-prompt-guard): fixes from code review

--- kong/plugins/ai-prompt-guard/handler.lua
+++ kong/plugins/ai-prompt-guard/handler.lua
@@ -1,54 +1,68 @@
 local _M = {}
 
 -- imports
-local kong_meta = require "kong.meta"
-local fmt       = string.format
+local kong_meta   = require "kong.meta"
+local buffer      = require("string.buffer")
+local ngx_re_find = ngx.re.find
 --
 
 _M.PRIORITY = 771
 _M.VERSION = kong_meta.version
 
-local function bad_request(msg)
-  kong.log.warn(msg)
+local function bad_request(msg, reveal_msg_to_client)
+  -- don't let users know 'ai-prompt-guard' is in use
+  kong.log.info(msg)
+  if not reveal_msg_to_client then
+    msg = "bad request"
+  end
   return kong.response.exit(400, { error = { message = msg } })
 end
 
 function _M.execute(request, conf)
   local user_prompt
 
-  -- concat all 'user' prompts into one string, if allowed
+  -- concat all 'user' prompts into one string, if conversation history must be checked
   if request.messages and not conf.allow_all_conversation_history then
-    for k, v in ipairs(request.messages) do
+    local buf = buffer.new()
+
+    for _, v in ipairs(request.messages) do
       if v.role == "user" then
-        if not user_prompt then user_prompt = "" end
-        user_prompt = fmt("%s %s", user_prompt, v.content)
+        buf:put(v.content)
       end
     end
+
+    user_prompt = buf:get()
+
   elseif request.messages then
     -- just take the trailing 'user' prompt
-    for k, v in ipairs(request.messages) do
+    for _, v in ipairs(request.messages) do
       if v.role == "user" then
         user_prompt = v.content
       end
     end
+
   elseif request.prompt then
     user_prompt = request.prompt
+
   else
-    return false, "ai-prompt-guard only supports llm/v1/chat or llm/v1/completions prompts"
+    return nil, "ai-prompt-guard only supports llm/v1/chat or llm/v1/completions prompts"
   end
 
   if not user_prompt then
-    return false, "no 'prompt' or 'messages' received"
+    return nil, "no 'prompt' or 'messages' received"
   end
 
   -- check the prompt for explcit ban patterns
   if conf.deny_patterns and #conf.deny_patterns > 0 then
-    for i, v in ipairs(conf.deny_patterns) do
+    for _, v in ipairs(conf.deny_patterns) do
       -- check each denylist; if prompt matches it, deny immediately
-      local m, err = ngx.re.match(user_prompt, v)
-      if err then return false, "bad regex execution for: " .. v end
+      local m, _, err = ngx_re_find(user_prompt, v, "jo")
+      if err then
+        return nil, "bad regex execution for: " .. v
 
-      if m then return false, "prompt pattern is blocked" end
+      elseif m then
+        return nil, "prompt pattern is blocked"
+      end
     end
   end
 
@@ -56,16 +70,22 @@ function _M.execute(request, conf)
   if conf.allow_patterns and #conf.allow_patterns > 0 then
     local valid = false
 
-    for i, v in ipairs(conf.allow_patterns) do
+    for _, v in ipairs(conf.allow_patterns) do
       -- check each denylist; if prompt matches it, deny immediately
-      local m, err = ngx.re.match(user_prompt, v)
+      local m, _, err = ngx_re_find(user_prompt, v, "jo")
 
-      if err then return false, "bad regex execution for: " .. v end
+      if err then
+        return nil, "bad regex execution for: " .. v
 
-      if m then valid = true end
+      elseif m then
+        valid = true
+        break
+      end
     end
 
-    if not valid then return false, "prompt doesn't match any allowed pattern" end
+    if not valid then
+      return false, "prompt doesn't match any allowed pattern"
+    end
   end
 
   return true, nil
@@ -73,24 +93,19 @@ end
 
 function _M:access(conf)
   kong.service.request.enable_buffering()
-  kong.ctx.shared.prompt_guarded = true
+  kong.ctx.shared.ai_prompt_guarded = true -- future use
 
   -- if plugin ordering was altered, receive the "decorated" request
-  local request, err
-  if not kong.ctx.replacement_request then
-    request, err = kong.request.get_body("application/json")
+  local request, err = kong.request.get_body("application/json")
 
-    if err then
-      bad_request("ai-prompt-guard only supports application/json requests")
-    end
-  else
-    request = kong.ctx.replacement_request
+  if err then
+    return bad_request("this LLM route only supports application/json requests", true)
   end
 
   -- run access handler
   local ok, err = self.execute(request, conf)
   if not ok then
-    return bad_request(err)
+    return bad_request(err, false)
   end
 end
 
--- kong/plugins/ai-prompt-guard/schema.lua
+++ kong/plugins/ai-prompt-guard/schema.lua
@@ -11,12 +11,22 @@ return {
               description = "Array of valid patterns, or valid questions from the 'user' role in chat.",
               type = "array",
               default = {},
-              elements = { type = "string" } } },
+              len_max = 10,
+              elements = {
+                type = "string",
+                len_min = 1,
+                len_max = 50,
+              }}},
           { deny_patterns = {
               description = "Array of invalid patterns, or invalid questions from the 'user' role in chat.",
               type = "array",
               default = {},
-              elements = { type = "string" } } },
+              len_max = 10,
+              elements = {
+                type = "string",
+                len_min = 1,
+                len_max = 50,
+              }}},
           { allow_all_conversation_history = {
               description = "If true, will ignore all previous chat prompts from the conversation history.",
               type = "boolean",
@@ -28,23 +38,24 @@ return {
   },
   entity_checks = {
     {
-      custom_entity_check = {
-        field_sources = { "config" },
-        fn = function(entity)
-          local config = entity.config
+      at_least_one_of = { "config.allow_patterns", "config.deny_patterns" },
+      -- custom_entity_check = {
+      --   field_sources = { "config" },
+      --   fn = function(entity)
+      --     local config = entity.config
   
-          if config then
-            local allow_patterns_set = (config.allow_patterns) and (#config.allow_patterns > 0)
-            local deny_patterns_set = (config.deny_patterns) and (#config.deny_patterns > 0)
+      --     if config then
+      --       local allow_patterns_set = (config.allow_patterns) and (#config.allow_patterns > 0)
+      --       local deny_patterns_set = (config.deny_patterns) and (#config.deny_patterns > 0)
 
-            if (not allow_patterns_set) and (not deny_patterns_set) then
-              return nil, "must set one item in either [allow_patterns] or [deny_patterns]"
-            end
-          end
+      --       if (not allow_patterns_set) and (not deny_patterns_set) then
+      --         return nil, "must set one item in either [allow_patterns] or [deny_patterns]"
+      --       end
+      --     end
 
-          return true
-        end
-      }
+      --     return true
+      --   end
+      -- }
     }
   }
 }
