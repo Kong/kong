8c5d73dc28497e33fb9c8fb9798174ef243c1f6a
Mikołaj Nowak <mikolaj.nowak@konghq.com>
2024-02-02 15:16:22 +0100

    fix(key-auth): add missing www-authenticate headers
    When serve returns 401 Unauthorized response it should
    return WWW-Authenticate header as well with proper challenge.
    Not all key-auth 401 responses had this header.
    This commit also adds an option to configure the realm
    for protected resource. By default it is empty therefore it
    is not displayed but it can be configured to
    be present in www_authenticate header.
    
    Fix: #7772
    KAG-321

--- spec/01-unit/01-db/01-schema/11-declarative_config/03-flatten_spec.lua
+++ spec/01-unit/01-db/01-schema/11-declarative_config/03-flatten_spec.lua
@@ -320,6 +320,7 @@ describe("declarative config: flatten", function()
               config = {
                 anonymous = null,
                 hide_credentials = false,
+                realm = null,
                 key_in_header = true,
                 key_in_query = true,
                 key_in_body = false,
@@ -430,6 +431,7 @@ describe("declarative config: flatten", function()
               config = {
                 anonymous = null,
                 hide_credentials = false,
+                realm = null,
                 key_in_header = true,
                 key_in_query = true,
                 key_in_body = false,
@@ -628,6 +630,7 @@ describe("declarative config: flatten", function()
                 config = {
                   anonymous = null,
                   hide_credentials = false,
+                  realm = null,
                   key_in_header = true,
                   key_in_query = true,
                   key_in_body = false,
@@ -1144,6 +1147,7 @@ describe("declarative config: flatten", function()
                 config = {
                   anonymous = null,
                   hide_credentials = false,
+                  realm = null,
                   key_in_header = true,
                   key_in_query = true,
                   key_in_body = false,
