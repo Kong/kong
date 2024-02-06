97c25a05b5956d00ee4fe6ff7e7d6ca901cda894
Mikołaj Nowak <mikolaj.nowak@konghq.com>
2024-02-06 15:06:06 +0100

    chore(dao): refactor translate_backwards
    This commits simplifies translate_backwards feature by
    switching from function that retrieves value approach
    to a table with a path to the necessary key.
    
    It also adds tests for other paths (GET, POST, PUT, PATCH)
    for querying admin api.
    
    KAG-3677
    
    (cherry picked from commit 4e515833e63896cb5ded292dde884974d5bf4574)

--- kong/plugins/rate-limiting/schema.lua
+++ kong/plugins/rate-limiting/schema.lua
@@ -103,10 +103,7 @@ return {
           -- TODO: deprecated forms, to be removed in Kong 4.0
           { redis_host = {
             type = "string",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.host
-            end,
+            translate_backwards = {'redis', 'host'},
             func = function(value)
               deprecation("rate-limiting: config.redis_host is deprecated, please use config.redis.host instead",
                 { after = "4.0", })
@@ -115,10 +112,7 @@ return {
           } },
           { redis_port = {
             type = "integer",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.port
-            end,
+            translate_backwards = {'redis', 'port'},
             func = function(value)
               deprecation("rate-limiting: config.redis_port is deprecated, please use config.redis.port instead",
                 { after = "4.0", })
@@ -128,10 +122,7 @@ return {
           { redis_password = {
             type = "string",
             len_min = 0,
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.password
-            end,
+            translate_backwards = {'redis', 'password'},
             func = function(value)
               deprecation("rate-limiting: config.redis_password is deprecated, please use config.redis.password instead",
                 { after = "4.0", })
@@ -140,10 +131,7 @@ return {
           } },
           { redis_username = {
             type = "string",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.username
-            end,
+            translate_backwards = {'redis', 'username'},
             func = function(value)
               deprecation("rate-limiting: config.redis_username is deprecated, please use config.redis.username instead",
                 { after = "4.0", })
@@ -152,10 +140,7 @@ return {
           } },
           { redis_ssl = {
             type = "boolean",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.ssl
-            end,
+            translate_backwards = {'redis', 'ssl'},
             func = function(value)
               deprecation("rate-limiting: config.redis_ssl is deprecated, please use config.redis.ssl instead",
                 { after = "4.0", })
@@ -164,10 +149,7 @@ return {
           } },
           { redis_ssl_verify = {
             type = "boolean",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.ssl_verify
-            end,
+            translate_backwards = {'redis', 'ssl_verify'},
             func = function(value)
               deprecation("rate-limiting: config.redis_ssl_verify is deprecated, please use config.redis.ssl_verify instead",
                 { after = "4.0", })
@@ -176,10 +158,7 @@ return {
           } },
           { redis_server_name = {
             type = "string",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.server_name
-            end,
+            translate_backwards = {'redis', 'server_name'},
             func = function(value)
               deprecation("rate-limiting: config.redis_server_name is deprecated, please use config.redis.server_name instead",
                 { after = "4.0", })
@@ -188,10 +167,7 @@ return {
           } },
           { redis_timeout = {
             type = "integer",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.timeout
-            end,
+            translate_backwards = {'redis', 'timeout'},
             func = function(value)
               deprecation("rate-limiting: config.redis_timeout is deprecated, please use config.redis.timeout instead",
                 { after = "4.0", })
@@ -200,10 +176,7 @@ return {
           } },
           { redis_database = {
             type = "integer",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.database
-            end,
+            translate_backwards = {'redis', 'database'},
             func = function(value)
               deprecation("rate-limiting: config.redis_database is deprecated, please use config.redis.database instead",
                 { after = "4.0", })
--- kong/plugins/response-ratelimiting/schema.lua
+++ kong/plugins/response-ratelimiting/schema.lua
@@ -142,10 +142,7 @@ return {
           -- TODO: deprecated forms, to be removed in Kong 4.0
           { redis_host = {
             type = "string",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.host
-            end,
+            translate_backwards = {'redis', 'host'},
             func = function(value)
               deprecation("response-ratelimiting: config.redis_host is deprecated, please use config.redis.host instead",
                 { after = "4.0", })
@@ -154,10 +151,7 @@ return {
           } },
           { redis_port = {
             type = "integer",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.port
-            end,
+            translate_backwards = {'redis', 'port'},
             func = function(value)
               deprecation("response-ratelimiting: config.redis_port is deprecated, please use config.redis.port instead",
                 { after = "4.0", })
@@ -167,10 +161,7 @@ return {
           { redis_password = {
             type = "string",
             len_min = 0,
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.password
-            end,
+            translate_backwards = {'redis', 'password'},
             func = function(value)
               deprecation("response-ratelimiting: config.redis_password is deprecated, please use config.redis.password instead",
                 { after = "4.0", })
@@ -179,10 +170,7 @@ return {
           } },
           { redis_username = {
             type = "string",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.username
-            end,
+            translate_backwards = {'redis', 'username'},
             func = function(value)
               deprecation("response-ratelimiting: config.redis_username is deprecated, please use config.redis.username instead",
                 { after = "4.0", })
@@ -191,10 +179,7 @@ return {
           } },
           { redis_ssl = {
             type = "boolean",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.ssl
-            end,
+            translate_backwards = {'redis', 'ssl'},
             func = function(value)
               deprecation("response-ratelimiting: config.redis_ssl is deprecated, please use config.redis.ssl instead",
                 { after = "4.0", })
@@ -203,10 +188,7 @@ return {
           } },
           { redis_ssl_verify = {
             type = "boolean",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.ssl_verify
-            end,
+            translate_backwards = {'redis', 'ssl_verify'},
             func = function(value)
               deprecation("response-ratelimiting: config.redis_ssl_verify is deprecated, please use config.redis.ssl_verify instead",
                 { after = "4.0", })
@@ -215,10 +197,7 @@ return {
           } },
           { redis_server_name = {
             type = "string",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.server_name
-            end,
+            translate_backwards = {'redis', 'server_name'},
             func = function(value)
               deprecation("response-ratelimiting: config.redis_server_name is deprecated, please use config.redis.server_name instead",
                 { after = "4.0", })
@@ -227,10 +206,7 @@ return {
           } },
           { redis_timeout = {
             type = "integer",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.timeout
-            end,
+            translate_backwards = {'redis', 'timeout'},
             func = function(value)
               deprecation("response-ratelimiting: config.redis_timeout is deprecated, please use config.redis.timeout instead",
                 { after = "4.0", })
@@ -239,10 +215,7 @@ return {
           } },
           { redis_database = {
             type = "integer",
-            include_in_output = true,
-            translate_backwards = function(instance)
-              return instance.redis.database
-            end,
+            translate_backwards = {'redis', 'database'},
             func = function(value)
               deprecation("response-ratelimiting: config.redis_database is deprecated, please use config.redis.database instead",
                 { after = "4.0", })
