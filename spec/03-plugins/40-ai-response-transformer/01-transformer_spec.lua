b50f6cd307df9f1ec709506460846eb129af5bf9
Qi <qiqi.zhang@konghq.com>
2024-02-19 12:16:41 +0800

    tests(plugin/ai-response-transformer): replace mocking server by http_mock module

--- spec/03-plugins/40-ai-response-transformer/01-transformer_spec.lua
+++ spec/03-plugins/40-ai-response-transformer/01-transformer_spec.lua
@@ -8,8 +8,10 @@
 local llm_class = require("kong.llm")
 local helpers = require "spec.helpers"
 local cjson = require "cjson"
+local http_mock = require "spec.helpers.http_mock"
+local pl_path = require "pl.path"
 
-local MOCK_PORT = 62349
+local MOCK_PORT = helpers.get_available_port()
 local PLUGIN_NAME = "ai-response-transformer"
 
 local OPENAI_INSTRUCTIONAL_RESPONSE = {
@@ -20,7 +22,7 @@ local OPENAI_INSTRUCTIONAL_RESPONSE = {
     options = {
       max_tokens = 512,
       temperature = 0.5,
-      upstream_url = "http://"..helpers.mock_upstream_host..":"..MOCK_PORT.."/instructions"
+      upstream_url = "http://" .. helpers.mock_upstream_host .. ":" .. MOCK_PORT .. "/instructions"
     },
   },
   auth = {
@@ -62,98 +64,67 @@ local EXPECTED_RESULT = {
 }
 
 local SYSTEM_PROMPT = "You are a mathematician. "
-                   .. "Multiply all numbers in my JSON request, by 2. Return me this message: "
-                   .. "{\"status\": 400, \"headers: {\"content-type\": \"application/xml\"}, \"body\": \"OUTPUT\"} "
-                   .. "where 'OUTPUT' is the result but transformed into XML format."
+    .. "Multiply all numbers in my JSON request, by 2. Return me this message: "
+    .. "{\"status\": 400, \"headers: {\"content-type\": \"application/xml\"}, \"body\": \"OUTPUT\"} "
+    .. "where 'OUTPUT' is the result but transformed into XML format."
 
 
-local client
+describe(PLUGIN_NAME .. ": (unit)", function()
+  local mock
+  local mock_response_file = pl_path.abspath(
+    "spec/fixtures/ai-proxy/openai/request-transformer/response-with-instructions.json")
 
+  lazy_setup(function()
+    mock = http_mock.new(tostring(MOCK_PORT), {
+      ["/instructions"] = {
+        content = string.format([[
+            local pl_file = require "pl.file"
+            ngx.header["Content-Type"] = "application/json"
+            ngx.say(pl_file.read("%s"))
+          ]], mock_response_file),
+      },
+    }, {
+      hostname = "llm",
+    })
 
-for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
+    assert(mock:start())
+  end)
 
-  describe(PLUGIN_NAME .. ": (unit)", function()
+  lazy_teardown(function()
+    assert(mock:stop())
+  end)
 
-    lazy_setup(function()
-      -- set up provider fixtures
-      local fixtures = {
-        http_mock = {},
-      }
+  describe("openai transformer tests, specific response", function()
+    it("transforms request based on LLM instructions, with response transformation instructions format", function()
+      local llm = llm_class:new(OPENAI_INSTRUCTIONAL_RESPONSE, {})
+      assert.truthy(llm)
 
-      fixtures.http_mock.openai = [[
-        server {
-            server_name llm;
-            listen ]]..MOCK_PORT..[[;
-            
-            default_type 'application/json';
+      local result, err = llm:ai_introspect_body(
+        REQUEST_BODY,      -- request body
+        SYSTEM_PROMPT,     -- conf.prompt
+        {},                -- http opts
+        nil                -- transformation extraction pattern (loose json)
+      )
 
-            location ~/instructions {
-              content_by_lua_block {
-                local pl_file = require "pl.file"
-                ngx.print(pl_file.read("spec/fixtures/ai-proxy/openai/request-transformer/response-with-instructions.json"))
-              }
-            }
-        }
-      ]]
+      assert.is_nil(err)
 
-      -- start kong
-      assert(helpers.start_kong({
-        -- set the strategy
-        database   = strategy,
-        -- use the custom test template to create a local mock server
-        nginx_conf = "spec/fixtures/custom_nginx.template",
-        -- make sure our plugin gets loaded
-        plugins = "bundled," .. PLUGIN_NAME,
-        -- write & load declarative config, only if 'strategy=off'
-        declarative_config = strategy == "off" and helpers.make_yaml_file() or nil,
-      }, nil, nil, fixtures))
-    end)
-    
-    lazy_teardown(function()
-      helpers.stop_kong(nil, true)
-    end)
+      local table_result, err = cjson.decode(result)
+      assert.is_nil(err)
+      assert.same(EXPECTED_RESULT, table_result)
 
-    before_each(function()
-      client = helpers.proxy_client()
-    end)
+      -- parse in response string format
+      local headers, body, status, err = llm:parse_json_instructions(result)
+      assert.is_nil(err)
+      assert.same({ ["content-type"] = "application/xml" }, headers)
+      assert.same(209, status)
+      assert.same(EXPECTED_RESULT.body, body)
 
-    after_each(function()
-      if client then client:close() end
-    end)
-
-    describe("openai transformer tests, specific response", function()
-      it("transforms request based on LLM instructions, with response transformation instructions format", function()
-        local llm = llm_class:new(OPENAI_INSTRUCTIONAL_RESPONSE, {})
-        assert.truthy(llm)
-
-        local result, err = llm:ai_introspect_body(
-          REQUEST_BODY,  -- request body
-          SYSTEM_PROMPT, -- conf.prompt
-          {},            -- http opts
-          nil            -- transformation extraction pattern (loose json)
-        )
-
-        assert.is_nil(err)
-
-        local table_result, err = cjson.decode(result)
-        assert.is_nil(err)
-        assert.same(EXPECTED_RESULT, table_result)
-
-        -- parse in response string format
-        local headers, body, status, err = llm:parse_json_instructions(result)
-        assert.is_nil(err)
-        assert.same({ ["content-type"] = "application/xml"}, headers)
-        assert.same(209, status)
-        assert.same(EXPECTED_RESULT.body, body)
-
-        -- parse in response table format
-        headers, body, status, err = llm:parse_json_instructions(table_result)
-        assert.is_nil(err)
-        assert.same({ ["content-type"] = "application/xml"}, headers)
-        assert.same(209, status)
-        assert.same(EXPECTED_RESULT.body, body)
-      end)
-
+      -- parse in response table format
+      headers, body, status, err = llm:parse_json_instructions(table_result)
+      assert.is_nil(err)
+      assert.same({ ["content-type"] = "application/xml" }, headers)
+      assert.same(209, status)
+      assert.same(EXPECTED_RESULT.body, body)
     end)
   end)
-end end
+end)
