aba3f35c496d3edd0f56bb4a2d446494ad54ba93
Robin Xiang <liverpool8056@163.com>
2024-03-18 14:34:29 +0800

    fix(core): fix a bug that `X-Kong-Upstream-Status` header is not set when the response is returned from the cache of `proxy-cache` plugin.
    FTI-5827

--- spec/02-integration/05-proxy/15-upstream-status-header_spec.lua
+++ spec/02-integration/05-proxy/15-upstream-status-header_spec.lua
@@ -18,7 +18,7 @@ local function setup_db()
     protocol = helpers.mock_upstream_protocol,
   }
 
-  bp.routes:insert {
+  local route1 = bp.routes:insert {
     protocols = { "http" },
     paths = { "/status/200" },
     service = service,
@@ -49,6 +49,19 @@ local function setup_db()
     route = { id = route3.id },
   }
 
+  bp.plugins:insert {
+    name = "proxy-cache",
+    route = { id = route1.id },
+    config = {
+      response_code = { 200 },
+      request_method = { "GET" },
+      content_type = { "application/json" },
+      cache_ttl = 300,
+      storage_ttl = 300,
+      strategy = "memory",
+    }
+  }
+
   return bp
 end
 
@@ -65,8 +78,6 @@ describe(constants.HEADERS.UPSTREAM_STATUS .. " header", function()
         headers = "server_tokens,latency_tokens,x-kong-upstream-status",
         plugins = "bundled,dummy",
       })
-
-      client = helpers.proxy_client()
     end)
 
     lazy_teardown(function()
@@ -75,6 +86,11 @@ describe(constants.HEADERS.UPSTREAM_STATUS .. " header", function()
       end
 
       helpers.stop_kong()
+    end)
+
+    before_each(function()
+      if client then client:close() end
+      client = helpers.proxy_client()
     end)
 
     it("when no plugin changes status code", function()
@@ -99,6 +115,19 @@ describe(constants.HEADERS.UPSTREAM_STATUS .. " header", function()
         }
       })
       assert.res_status(500, res)
+      assert.equal("200", res.headers[constants.HEADERS.UPSTREAM_STATUS])
+    end)
+
+    it("should be set when proxy-cache is enabled", function()
+      local res = assert(client:send {
+        method  = "GET",
+        path    = "/status/200",
+        headers = {
+          host = helpers.mock_upstream_host,
+        }
+      })
+      assert.res_status(200, res)
+      assert.equal("Hit", res.headers["X-Cache-Status"])
       assert.equal("200", res.headers[constants.HEADERS.UPSTREAM_STATUS])
     end)
   end)
