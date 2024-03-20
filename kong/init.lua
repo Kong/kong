f6920fc412721ec1f6b9fefa0a8f8cab9b6a735d
tzssangglass <tzssangglass@gmail.com>
2024-03-20 16:37:27 +0800

    fix
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- kong/init.lua
+++ kong/init.lua
@@ -1278,8 +1278,10 @@ function Kong.balancer()
     -- record failure data
     local previous_try = tries[try_count - 1]
     previous_try.state, previous_try.code = get_last_failure()
-    local peer_conn = require "resty.kong.peer_conn"
-    previous_try.cached = peer_conn.get_last_peer_connection_cached()
+    if ngx.config.subsystem == "http" then
+      local peer_conn = require "resty.kong.peer_conn"
+      previous_try.cached = peer_conn.get_last_peer_connection_cached()
+    end
 
     -- Report HTTP status for health checks
     local balancer_instance = balancer_data.balancer
