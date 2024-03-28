7d88cb3e4202714894b15f21f2a47893977d20b0
Niklaus Schen <8458369+Water-Melon@users.noreply.github.com>
2024-03-28 17:00:44 +0800

    fix(plugins/acme): fix certificate renew failure issue (#12773)
    Using client.renew_certificate directly as the callback function in ngx_timer_at
    causes the parameter value to not be the plugin's config.
    
    KAG-4008
    
    (cherry picked from commit ca2e67cb5f8596f8e80527288b3c1dcb3d8ec064)

--- kong/plugins/acme/handler.lua
+++ kong/plugins/acme/handler.lua
@@ -82,6 +82,9 @@ local function renew(premature)
 end
 
 
+ACMEHandler.renew = renew
+
+
 function ACMEHandler:init_worker()
   local worker_id = ngx.worker.id()
   kong.log.info("acme renew timer started on worker ", worker_id)
