e301418d251b940fa55bd186b296f24c7bb16125
Jack Tysoe <91137069+tysoekong@users.noreply.github.com>
2024-04-15 10:28:17 +0100

    Update spec/03-plugins/38-ai-proxy/02-openai_integration_spec.lua
    Co-authored-by: Joshua Schmid <jaiks@posteo.de>

--- spec/03-plugins/38-ai-proxy/02-openai_integration_spec.lua
+++ spec/03-plugins/38-ai-proxy/02-openai_integration_spec.lua
@@ -1,7 +1,7 @@
 local helpers = require "spec.helpers"
 local cjson = require "cjson"
 local pl_file = require "pl.file"
-local pl_stringx    = require "pl.stringx"
+local pl_stringx = require "pl.stringx"
 
 local PLUGIN_NAME = "ai-proxy"
 local MOCK_PORT = helpers.get_available_port()
