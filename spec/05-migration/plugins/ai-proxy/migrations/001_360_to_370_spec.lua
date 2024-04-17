cd6e899fd86a45905b2cca89ce453c6a93e18f1e
Robin Xiang <liverpool8056@163.com>
2024-04-17 15:02:41 +0800

    test case

--- spec/05-migration/plugins/ai-proxy/migrations/001_360_to_370_spec.lua
+++ spec/05-migration/plugins/ai-proxy/migrations/001_360_to_370_spec.lua
@@ -1,43 +1,56 @@
 local uh = require "spec.upgrade_helpers"
 local helpers = require "spec.helpers"
+local pgmoon_json = require("pgmoon.json")
+local uuid = require "kong.tools.uuid"
+
+local function render(template, keys)
+  return (template:gsub("$%(([A-Z_]+)%)", keys))
+end
 
 for _, strategy in helpers.each_strategy() do
   describe("ai-proxy plugin migration", function()
     local db, ai_proxy_plugin
-    uh.setup(function()
-      _, db = helpers.get_db_utils(strategy, {"plugins"})
-      local plugin, err, err_t = db.plugins:insert {
-        name = "ai-proxy",
-        config = {
-          route_type = "llm/v1/completions",
-          auth = {
-            header_name = "x-api-key",
-            header_value = "anthropic-key",
-          },
-          model = {
-            name = "claude-2.1",
-            provider = "anthropic",
-            options = {
-              max_tokens = 256,
-              temperature = 1.0,
-              upstream_url = "http://example.com/llm/v1/completions/good",
-              anthropic_version = "2023-06-01",
-            },
-          },
-          logging = {
-            log_statistics = true,  -- anthropic does not support statistics
-          },
+    local id = uuid.uuid()
+    local plugin_name = "ai-proxy"
+    local plugin_config = {
+      route_type = "llm/v1/completions",
+      auth = {
+        header_name = "x-api-key",
+        header_value = "anthropic-key",
+      },
+      model = {
+        name = "claude-2.1",
+        provider = "anthropic",
+        options = {
+          max_tokens = 256,
+          temperature = 1.0,
+          upstream_url = "http://example.com/llm/v1/completions/good",
+          anthropic_version = "2023-06-01",
         },
-      }
+      },
+      logging = {
+        log_statistics = true,  -- anthropic does not support statistics
+      },
+    }
+
+    uh.setup(function()
+      local sql = render([[
+        INSERT INTO plugins (id, name, config, enabled) VALUES
+          ('$(ID)', '$(PLUGIN_NAME)', $(CONFIG)::jsonb, 1);
+      ]], {
+        ID = id,
+        PLUGIN_NAME = plugin_name,
+        CONFIG = pgmoon_json.encode_json(plugin_config),
+      })
+      _, db = helpers.get_db_utils(strategy, {"plugins"})
+      local res, err = db.connector:query(sql)
+      assert.is_not_nil(res)
       assert.is_nil(err)
-      assert.is_nil(err_t)
-      assert.is_not_nil(plugin)
-      ai_proxy_plugin = plugin
     end)
 
     uh.new_after_up("has updated ai-proxy plugin configuration", function ()
       -- local cache_key = db.plugins:cache_key("ai-proxy")
-      local plugin, err = db.plugins:select({ id = ai_proxy_plugin.id })
+      local plugin, err = db.plugins:select({ id = id })
       assert.is_nil(err)
       assert.is_not_nil(plugin)
       -- assert.equal(1, #rows)
