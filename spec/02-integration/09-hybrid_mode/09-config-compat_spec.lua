84700562632045223a05a077be74771bbd030f06
Jack Tysoe <91137069+tysoekong@users.noreply.github.com>
2024-05-06 12:14:01 +0100

    fix(ai proxy): 3.7 regression fixes rollup (#12974)
    * fix(ai-proxy): remove unsupported top_k parameter from openai format(s)
    
    * fix(ai-proxy): broken ollama-type streaming events
    
    * fix(ai-proxy): streaming setting moved ai-proxy only
    
    * fix(ai-proxy): anthropic token counts
    
    * fix(ai-proxy): wrong analytics format; missing azure extra analytics
    
    * fix(ai-proxy): correct plugin name in log serializer
    
    * fix(ai-proxy): store body string in case of regression
    
    * fix(ai-proxy): fix tests

--- spec/02-integration/09-hybrid_mode/09-config-compat_spec.lua
+++ spec/02-integration/09-hybrid_mode/09-config-compat_spec.lua
@@ -480,6 +480,7 @@ describe("CP/DP config compat transformations #" .. strategy, function()
           name = "ai-proxy",
           enabled = true,
           config = {
+            response_streaming = "allow", -- becomes nil
             route_type = "preserve", -- becomes 'llm/v1/chat'
             auth = {
               header_name = "header",
@@ -491,7 +492,6 @@ describe("CP/DP config compat transformations #" .. strategy, function()
               options = {
                 max_tokens = 512,
                 temperature = 0.5,
-                response_streaming = "allow", -- becomes nil
                 upstream_path = "/anywhere", -- becomes nil
               },
             },
@@ -500,7 +500,7 @@ describe("CP/DP config compat transformations #" .. strategy, function()
         -- ]]
 
         local expected_ai_proxy_prior_37 = utils.cycle_aware_deep_copy(ai_proxy)
-        expected_ai_proxy_prior_37.config.model.options.response_streaming = nil
+        expected_ai_proxy_prior_37.config.response_streaming = nil
         expected_ai_proxy_prior_37.config.model.options.upstream_path = nil
         expected_ai_proxy_prior_37.config.route_type = "llm/v1/chat"
 
