057ceb59392c50f2782ca5dbb681ff030b26f5a5
Mikołaj Nowak <mikolaj.nowak@konghq.com>
2024-05-06 12:59:35 +0200

    fix(migrations): split redis migrations into up and teardown
    "UP" phase should only contain non-destructive operations.
    "TEARDOWN" (or "FINISH") phase should be used to change/delete data.
    
    KAG-4419
    
    (cherry picked from commit 49aa2332b219826410c46cf207c9bb31ce25785d)

--- kong/plugins/acme/migrations/003_350_to_360.lua
+++ kong/plugins/acme/migrations/003_350_to_360.lua
@@ -12,30 +12,16 @@ return {
         BEGIN
           UPDATE plugins
           SET config =
-            config
-                #- '{storage_config,redis}'
-
-            || jsonb_build_object(
-                'storage_config',
-                (config -> 'storage_config') - 'redis'
+              jsonb_set(
+                config,
+                '{storage_config,redis}',
+                config #> '{storage_config, redis}'
                 || jsonb_build_object(
-                  'redis',
-                  jsonb_build_object(
-                      'host', config #> '{storage_config, redis, host}',
-                      'port', config #> '{storage_config, redis, port}',
-                      'password', config #> '{storage_config, redis, auth}',
-                      'username', config #> '{storage_config, redis, username}',
-                      'ssl', config #> '{storage_config, redis, ssl}',
-                      'ssl_verify', config #> '{storage_config, redis, ssl_verify}',
-                      'server_name', config #> '{storage_config, redis, ssl_server_name}',
-                      'timeout', config #> '{storage_config, redis, timeout}',
-                      'database', config #> '{storage_config, redis, database}'
-                  ) || jsonb_build_object(
-                      'extra_options',
-                      jsonb_build_object(
-                          'scan_count', config #> '{storage_config, redis, scan_count}',
-                          'namespace', config #> '{storage_config, redis, namespace}'
-                      )
+                  'password', config #> '{storage_config, redis, auth}',
+                  'server_name', config #> '{storage_config, redis, ssl_server_name}',
+                  'extra_options', jsonb_build_object(
+                    'scan_count', config #> '{storage_config, redis, scan_count}',
+                    'namespace', config #> '{storage_config, redis, namespace}'
                   )
                 )
               )
@@ -44,5 +30,24 @@ return {
           -- Do nothing, accept existing state
         END$$;
       ]],
+      teardown = function(connector, _)
+        local sql = [[
+          DO $$
+          BEGIN
+            UPDATE plugins
+            SET config =
+              config
+                #- '{storage_config,redis,auth}'
+                #- '{storage_config,redis,ssl_server_name}'
+                #- '{storage_config,redis,scan_count}'
+                #- '{storage_config,redis,namespace}'
+            WHERE name = 'acme';
+          EXCEPTION WHEN UNDEFINED_COLUMN OR UNDEFINED_TABLE THEN
+            -- Do nothing, accept existing state
+          END$$;
+        ]]
+        assert(connector:query(sql))
+        return true
+      end,
     },
 }
