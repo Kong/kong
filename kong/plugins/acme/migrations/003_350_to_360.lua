db11c6316c5fa98427e7a5b91f7ae883749a5e1a
Mikołaj Nowak <mikolaj.nowak93@gmail.com>
2024-05-08 11:02:31 +0200

    fix(migration): make redis schema migrations reentrant (#12992)
    (cherry picked from commit 328097a1225dbbb802c121c028b094fc6763cb5f)

--- kong/plugins/acme/migrations/003_350_to_360.lua
+++ kong/plugins/acme/migrations/003_350_to_360.lua
@@ -10,11 +10,11 @@ return {
                 '{storage_config,redis}',
                 config #> '{storage_config, redis}'
                 || jsonb_build_object(
-                  'password', config #> '{storage_config, redis, auth}',
-                  'server_name', config #> '{storage_config, redis, ssl_server_name}',
+                  'password', COALESCE(config #> '{storage_config, redis, auth}', config #> '{storage_config, redis, password}'),
+                  'server_name', COALESCE(config #> '{storage_config, redis, ssl_server_name}', config #> '{storage_config, redis, server_name}'),
                   'extra_options', jsonb_build_object(
-                    'scan_count', config #> '{storage_config, redis, scan_count}',
-                    'namespace', config #> '{storage_config, redis, namespace}'
+                    'scan_count', COALESCE(config #> '{storage_config, redis, scan_count}', config #> '{storage_config, redis, extra_options, scan_count}'),
+                    'namespace', COALESCE(config #> '{storage_config, redis, namespace}', config #> '{storage_config, redis, extra_options, namespace}')
                   )
                 )
               )
