c2e46d77c7f7343db442058cec367ea777822d7a
Qi <add_sp@outlook.com>
2024-05-10 03:54:06 +0000

    refactor(pdk): eliminate NYIs (UCLO) to be more JIT-friendly
    Currently, LuaJIT always close upvalues even if they are not
    actually be used, so we should eliminate unused upvalues
    manually to avoid bytecode UCLO, which is a NYI.

--- kong/enterprise_edition/pdk/response.lua
+++ kong/enterprise_edition/pdk/response.lua
@@ -14,43 +14,64 @@ local unpack = kong_table.unpack
 local check_phase = phase_checker.check
 local PHASES = phase_checker.phases
 
-local function new(self, module, major_version)
-  local _response = module.new(self, major_version)
 
-  local hooks = {}
-  local response = {
-    register_hook = function(method, hook_method, ctx)
-      check_phase(PHASES.init_worker)
+local function gen_func_register_hook(hooks)
+  return function(method, hook_method, ctx)
+    check_phase(PHASES.init_worker)
       local hook = { method = hook_method, ctx = ctx }
       if hooks[method] then
         table.insert(hooks[method], hook)
       else
         hooks[method] = { hook }
       end
-    end,
+  end
+end
+
+
+local function gen_mt_func__index_hook(hooks, response, k)
+  return function(...)
+    local arg = pack(...)
+    for _, hook in ipairs(hooks[k]) do
+      if hook.ctx then
+        arg = pack(hook.method(hook.ctx, unpack(arg)))
+      else
+        arg = pack(hook.method(unpack(arg)))
+      end
+    end
+    return response[k](unpack(arg))
+  end
+end
+
+
+local function gen_mt_func___index(hooks, response)
+  return function(_self, k)
+    if hooks[k] then
+      return gen_mt_func__index_hook(hooks, response, k)
+    else
+      return response[k]
+    end
+  end
+end
+
+
+local function gen_mt_func___newindex(response)
+  return function(_self, k, v)
+    response[k] = v
+  end
+end
+
+
+local function new(self, module, major_version)
+  local _response = module.new(self, major_version)
+
+  local hooks = {}
+  local response = {
+    register_hook = gen_func_register_hook(hooks),
   }
 
   local mt = {
-    __index = function(self, k)
-      if hooks[k] then
-        return function(...)
-          local arg = pack(...)
-          for _, hook in ipairs(hooks[k]) do
-            if hook.ctx then
-              arg = pack(hook.method(hook.ctx, unpack(arg)))
-            else
-              arg = pack(hook.method(unpack(arg)))
-            end
-          end
-          return _response[k](unpack(arg))
-        end
-      else
-        return _response[k]
-      end
-    end,
-    __newindex = function(self, k, v)
-      _response[k] = v
-    end,
+    __index = gen_mt_func___index(hooks, _response),
+    __newindex = gen_mt_func___newindex(_response),
   }
 
   setmetatable(response, mt)
