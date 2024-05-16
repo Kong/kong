3b97ce0f83b4945420eb58ee404f94cb20adb866
Datong Sun <dndx@idndx.com>
2024-05-16 00:35:32 -0700

    Revert "fix(rpc): disable cluster_rpc for 3.7"
    This reverts commit ddda6a1f2abbd1c8030a4325a807a17755a8bd19.

--- kong/templates/kong_defaults.lua
+++ kong/templates/kong_defaults.lua
@@ -41,7 +41,7 @@ cluster_ocsp = off
 cluster_max_payload = 16777216
 cluster_use_proxy = off
 cluster_dp_labels = NONE
-cluster_rpc = off
+cluster_rpc = on
 cluster_cjson = off
 
 lmdb_environment_path = dbless.lmdb
