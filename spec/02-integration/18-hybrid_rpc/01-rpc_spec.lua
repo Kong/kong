078ab242f54e0a9ae39ee3a22fff9f5e6a853842
Datong Sun <dndx@idndx.com>
2024-05-16 00:35:32 -0700

    Revert "fix(rpc): disable cluster_rpc for 3.7"
    This reverts commit ddda6a1f2abbd1c8030a4325a807a17755a8bd19.

--- spec/02-integration/18-hybrid_rpc/01-rpc_spec.lua
+++ spec/02-integration/18-hybrid_rpc/01-rpc_spec.lua
@@ -15,7 +15,6 @@ for _, strategy in helpers.each_strategy() do
         cluster_cert_key = "spec/fixtures/kong_clustering.key",
         database = strategy,
         cluster_listen = "127.0.0.1:9005",
-        cluster_rpc = "on",
         nginx_conf = "spec/fixtures/custom_nginx.template",
       }))
 
@@ -26,7 +25,6 @@ for _, strategy in helpers.each_strategy() do
         cluster_cert = "spec/fixtures/kong_clustering.crt",
         cluster_cert_key = "spec/fixtures/kong_clustering.key",
         cluster_control_plane = "127.0.0.1:9005",
-        cluster_rpc = "on",
         proxy_listen = "0.0.0.0:9002",
         nginx_conf = "spec/fixtures/custom_nginx.template",
       }))
@@ -38,32 +36,7 @@ for _, strategy in helpers.each_strategy() do
     end)
 
     describe("status API", function()
-      -- TODO: remove this test once cluster RPC is GA
-      it("no DR RPC capabilities exist", function()
-        -- This should time out, we expect no RPC capabilities
-        local status = pcall(helpers.wait_until, function()
-          local admin_client = helpers.admin_client()
-          finally(function()
-            admin_client:close()
-          end)
-
-          local res = assert(admin_client:get("/clustering/data-planes"))
-          local body = assert.res_status(200, res)
-          local json = cjson.decode(body)
-
-          for _, v in pairs(json.data) do
-            if v.ip == "127.0.0.1" and v.rpc_capabilities and #v.rpc_capabilities ~= 0 then
-              table.sort(v.rpc_capabilities)
-              assert.near(14 * 86400, v.ttl, 3)
-              assert.same({ "kong.debug.log_level.v1", }, v.rpc_capabilities)
-              return true
-            end
-          end
-        end, 10)
-        assert.is_false(status)
-      end)
-
-      pending("shows DP RPC capability status", function()
+      it("shows DP RPC capability status", function()
         helpers.wait_until(function()
           local admin_client = helpers.admin_client()
           finally(function()
