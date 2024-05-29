d51c894422732851a547979e56ee60e42ca42cf1
chronolaw <chrono_cpp@me.com>
2024-05-29 15:51:50 +0800

    revert change of utils.lua

--- kong/tools/utils.lua
+++ kong/tools/utils.lua
@@ -23,7 +23,7 @@ do
     "kong.tools.uuid",
     "kong.tools.rand",
     "kong.tools.time",
-    --"kong.tools.string",
+    "kong.tools.string",
     -- ]] keep it here for compatibility
     "kong.tools.ip",
     "kong.tools.http",
