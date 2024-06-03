70bbcf846ec0ce4041a92625c86601b8866936ee
Robin Xiang <liverpool8056@163.com>
2024-06-03 16:59:48 +0800

    Update kong/runloop/balancer/init.lua
    Co-authored-by: Yusheng Li <leeys.top@gmail.com>

--- kong/runloop/balancer/init.lua
+++ kong/runloop/balancer/init.lua
@@ -305,8 +305,8 @@ local function execute(balancer_data, ctx)
 
   if dns_cache_only then
     -- retry, so balancer is already set if there was one
-    balancer   = balancer_data.balancer
-    upstream   = balancer_data.upstream
+    balancer = balancer_data.balancer
+    upstream = balancer_data.upstream
 
   else
     -- first try, so try and find a matching balancer/upstream object
