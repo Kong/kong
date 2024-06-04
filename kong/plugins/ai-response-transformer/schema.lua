c682d1942efabdb4d09b7f970d7b1f86fe2342b5
Wangchong Zhou <wangchong@konghq.com>
2024-06-04 18:42:11 +0800

    feat(plugins): enable ai plugins to read request body from buffered file

--- kong/plugins/ai-response-transformer/schema.lua
+++ kong/plugins/ai-response-transformer/schema.lua
@@ -45,6 +45,13 @@ return {
             default = true,
         }},
 
+        { max_request_body_size = {
+            type = "integer",
+            default = 8 * 1024,
+            gt = 0,
+            description = "max allowed body size allowed to be introspected",}
+        },
+
         -- from forward-proxy
         { http_proxy_host = typedefs.host },
         { http_proxy_port = typedefs.port },
