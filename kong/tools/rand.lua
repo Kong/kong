ff8aea69cb351624c895f3de8ff1ca4edcb63579
chronolaw <chrono_cpp@me.com>
2024-06-04 08:20:21 +0800

    find before gsub

--- kong/tools/rand.lua
+++ kong/tools/rand.lua
@@ -120,10 +120,25 @@ do
     -- get 24 bytes, which will return a 32 char string after encoding
     -- this is done in attempt to maintain backwards compatibility as
     -- much as possible while improving the strength of this function
-    return encode_base64(get_rand_bytes(24, true))
-           :gsub("/", char(rand(48, 57)))  -- 0 - 10
-           :gsub("+", char(rand(65, 90)))  -- A - Z
-           :gsub("=", char(rand(97, 122))) -- a - z
+    local str = get_rand_bytes(24, true)
+
+    if str:find("/", 1, true) then
+      str = str:gsub("/", char(rand(48, 57)))  -- 0 - 10
+    end
+
+    if str:find("+", 1, true) then
+      str = str:gsub("+", char(rand(65, 90)))  -- A - Z
+    end
+
+    if str:find("=", 1, true) then
+      str = str:gsub("=", char(rand(97, 122))) -- a - z
+    end
+
+    return str
+    --return encode_base64(get_rand_bytes(24, true))
+    --       :gsub("/", char(rand(48, 57)))  -- 0 - 10
+    --       :gsub("+", char(rand(65, 90)))  -- A - Z
+    --       :gsub("=", char(rand(97, 122))) -- a - z
   end
 
 end
