63b26ccb4602b83253e5914cd20de3e654b490ad
tonydongkong <101685497+tonydongkong@users.noreply.github.com>
2024-06-13 12:35:33 -0400

    - add jwt test cases (#9426)
    Co-authored-by: Tony Dong <tony.dong@konghq.com@t-dong-mbp.mynetworksettings.com>

--- spec-ee/kong-api-tests/test/gateway/plugins/jwt-signer.spec.ts
+++ spec-ee/kong-api-tests/test/gateway/plugins/jwt-signer.spec.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { authDetails } from '@fixtures';
+import { jwtDecode } from 'jwt-decode';
 import {
   expect,
   Environment,
@@ -17,6 +18,8 @@ import {
   retryRequest,
   isGateway,
   waitForConfigRebuild,
+  postNegative,
+  deletePlugin,
 } from '@support';
 
 describe('Gateway Plugins: jwt-signer', function () {
@@ -34,6 +37,324 @@ describe('Gateway Plugins: jwt-signer', function () {
   const validTokenHeaders = {
     Authorization: `bearer ${authDetails.valid_token}`,
   };
+
+  const channelValidTokenHeaders = {
+    Authorization: `bearer ${authDetails.valid_token}`,
+    'Channel-Authorization': `bearer ${authDetails.valid_token}`
+  };
+
+  const testClaims = [
+    {
+      description: 'should be able to add extra claims in the resigned token',
+      config: {
+        add_claims: {
+          a: 'test_a',
+          b: '[true, false]',
+          c: '{ ccc: 123 }',
+          d: '12345.99',
+        }
+      },
+      expectedToken: {
+        a: 'test_a',
+        b: [ true, false ],
+        iss: 'kong',
+        c: '{ ccc: 123 }',
+        d: 12345.99,
+        iat: 1665610129,
+        original_iss: 'demo',
+        username: 'demo'
+      },
+      headerKey: 'Abc',
+      tokenHeaders: validTokenHeaders
+    },
+    {
+      description: 'should be able to add access token claims in the resigned token',
+      config: {
+        add_access_token_claims: {
+          e: 'test_e',
+          f: '[true, false]',
+          g: '{ ggg: 123 }',
+          d: '12345.99999999999999999999999999999',
+        }
+      },
+      expectedToken: {
+        f: [ true, false ],
+        c: '{ ccc: 123 }',
+        e: 'test_e',
+        username: 'demo',
+        g: '{ ggg: 123 }',
+        iss: 'kong',
+        a: 'test_a',
+        b: [ true, false ],
+        iat: 1665610129,
+        d: 12346,
+        original_iss: 'demo'
+      },
+      headerKey: 'Abc',
+      tokenHeaders: validTokenHeaders
+    },
+    {
+      description: 'should be able to set access token claims in the resigned token',
+      config: {
+        set_access_token_claims: {
+          i: 'test_i',
+          j: '[true, false]',
+          k: '{ kkk: 123 }',
+          l: '12345.999998888',
+        }
+      },
+      expectedToken: {
+        a: 'test_a',
+        k: '{ kkk: 123 }',
+        c: '{ ccc: 123 }',
+        b: [ true, false ],
+        e: 'test_e',
+        f: [ true, false ],
+        username: 'demo',
+        i: 'test_i',
+        g: '{ ggg: 123 }',
+        iss: 'kong',
+        d: 12346,
+        j: [ true, false ],
+        iat: 1665610129,
+        original_iss: 'demo',
+        l: 12345.999998888
+      },
+      headerKey: 'Abc',
+      tokenHeaders: validTokenHeaders
+    },
+    {
+      description: 'should be able to update claims with add_claims in the resigned token',
+      config: {
+          add_claims: {
+            a: 'test_a_new'
+          }
+      },
+      expectedToken: {
+        k: '{ kkk: 123 }',
+        a: 'test_a_new',
+        e: 'test_e',
+        f: [ true, false ],
+        username: 'demo',
+        i: 'test_i',
+        g: '{ ggg: 123 }',
+        iss: 'kong',
+        d: 12346,
+        j: [ true, false ],
+        iat: 1665610129,
+        original_iss: 'demo',
+        l: 12345.999998888
+      },
+      headerKey: 'Abc',
+      tokenHeaders: validTokenHeaders
+    },
+    {
+      description: 'should be able to update claims with set_claims in the resigned token',
+      config: {
+          set_claims: {
+            a: 'test_a_new_v2'
+          }
+      },
+      expectedToken: {
+        k: '{ kkk: 123 }',
+        a: 'test_a_new_v2',
+        e: 'test_e',
+        f: [ true, false ],
+        username: 'demo',
+        i: 'test_i',
+        g: '{ ggg: 123 }',
+        iss: 'kong',
+        d: 12346,
+        j: [ true, false ],
+        iat: 1665610129,
+        l: 12345.999998888,
+        original_iss: 'demo'
+      },
+      headerKey: 'Abc',
+      tokenHeaders: validTokenHeaders
+    },
+    {
+      description: 'should not be able to remove claims if added with add_claims or add_access_token_claims in the resigned token',
+      config: {
+          remove_access_token_claims: ['a', 'e']
+      },
+      expectedToken: {
+        k: '{ kkk: 123 }',
+        a: 'test_a_new_v2',
+        e: 'test_e',
+        f: [ true, false ],
+        username: 'demo',
+        i: 'test_i',
+        g: '{ ggg: 123 }',
+        iss: 'kong',
+        d: 12346,
+        j: [ true, false ],
+        iat: 1665610129,
+        l: 12345.999998888,
+        original_iss: 'demo'
+      },
+      headerKey: 'Abc',
+      tokenHeaders: validTokenHeaders
+    },
+    {
+      description: 'should be able to remove claims if it was pre-existed from the resigned token',
+      config: {
+          remove_access_token_claims: ['username']
+      },
+      expectedToken: {
+        k: '{ kkk: 123 }',
+        a: 'test_a_new_v2',
+        e: 'test_e',
+        f: [ true, false ],
+        i: 'test_i',
+        g: '{ ggg: 123 }',
+        iss: 'kong',
+        d: 12346,
+        j: [ true, false ],
+        iat: 1665610129,
+        original_iss: 'demo',
+        l: 12345.999998888
+      },
+      headerKey: 'Abc',
+      tokenHeaders: validTokenHeaders
+    },
+    {
+      description: 'should set_claims takes higher priority than add_claims',
+      config: {
+          add_claims: {
+            b: "{\"key\": 123}"
+          },
+          set_claims: {
+            b: "[\"str1\",\"str2\", true, 1234.009999]"
+          }
+      },
+      expectedToken: {
+        b: [ 'str1', 'str2', true, 1234.009999 ],
+        e: 'test_e',
+        f: [ true, false ],
+        d: 12346,
+        i: 'test_i',
+        g: '{ ggg: 123 }',
+        iss: 'kong',
+        original_iss: 'demo',
+        j: [ true, false ],
+        iat: 1665610129,
+        k: '{ kkk: 123 }',
+        l: 12345.999998888
+      },
+      headerKey: 'Abc',
+      tokenHeaders: validTokenHeaders
+    },
+    {
+      description: 'should set_access_token_claims takes higher priority than set_claims',
+      config: {
+          add_claims: {
+            b: "{\"key\": 123}"
+          },
+          set_claims: {
+            b: "[\"str1\",\"str2\", true, 1234.009999]"
+          }
+      },
+      expectedToken: {
+        b: [ 'str1', 'str2', true, 1234.009999 ],
+        e: 'test_e',
+        f: [ true, false ],
+        d: 12346,
+        i: 'test_i',
+        g: '{ ggg: 123 }',
+        iss: 'kong',
+        original_iss: 'demo',
+        j: [ true, false ],
+        iat: 1665610129,
+        k: '{ kkk: 123 }',
+        l: 12345.999998888
+      },
+      headerKey: 'Abc',
+      tokenHeaders: validTokenHeaders
+    },
+    {
+      description: 'should see original access token when set original_access_token_upstream_header',
+      config: {
+          original_access_token_upstream_header: "og_token"
+      },
+      expectedToken: { iat: 1665610129, iss: 'demo', username: 'demo' },
+      headerKey: 'Og-Token',
+      tokenHeaders: validTokenHeaders
+    },
+    {
+      description: 'should be able to add channel token',
+      config: {
+          channel_token_upstream_header: "DEF",
+          verify_channel_token_signature: false,
+          channel_token_request_header: "Channel-Authorization"
+      },
+      expectedToken: {
+        iss: 'kong',
+        b: [ 'str1', 'str2', true, 1234.009999 ],
+        iat: 1665610129,
+        original_iss: 'demo',
+        username: 'demo'
+      },
+      headerKey: 'Def',
+      tokenHeaders: channelValidTokenHeaders
+    },
+    {
+      description: 'should be able to add_claims and set_claims for channel token',
+      config: {
+          add_claims: {
+            c: "{ \"ccc\": 12309.9999 }"
+          },
+          set_claims: {
+            c: "{ \"ccc\": true }"
+          }
+      },
+      expectedToken: {
+        iss: 'kong',
+        c: { ccc: true },
+        iat: 1665610129,
+        original_iss: 'demo',
+        username: 'demo'
+      },
+      headerKey: 'Def',
+      tokenHeaders: channelValidTokenHeaders
+    },
+    {
+      description: 'should set_channel_token_claims take the higher priority for channel token',
+      config: {
+          add_claims: {
+            d: "{ \"ddd\": 12309.9999 }"
+          },
+          set_claims: {
+            d: "{ \"ddd\": true }"
+          },
+          add_access_token_claims: {
+            d: "1234"
+          },
+          set_channel_token_claims: {
+            d: "{\"dd\": 1234, \"ddd\": true}"
+          }
+      },
+      expectedToken: {
+        iss: 'kong',
+        d: { dd: 1234, ddd: true },
+        iat: 1665610129,
+        original_iss: 'demo',
+        username: 'demo'
+      },
+      headerKey: 'Def',
+      tokenHeaders: channelValidTokenHeaders
+    },
+    {
+      description: 'should set original channel token',
+      config: {
+          original_channel_token_upstream_header: "og-channel-token"
+      },
+      expectedToken: { iat: 1665610129, iss: 'demo', username: 'demo' },
+      headerKey: 'Og-Channel-Token',
+      tokenHeaders: channelValidTokenHeaders
+    }
+  ]
+
 
   let serviceId: string;
   let routeId: string;
@@ -215,9 +536,118 @@ describe('Gateway Plugins: jwt-signer', function () {
     expect(resp.status, 'Status should be 204').to.equal(204);
   });
 
+  it('should not create a jwt-signer plugin when original channel token and original access token headers are same', async function () {
+    basePayload = {
+      name: 'jwt-signer',
+      service: {
+        id: serviceId,
+      },
+      route: {
+        id: routeId,
+      },
+    };
+    const pluginPayload = {
+      ...basePayload,
+      config: {
+        original_channel_token_upstream_header: 'AAA',
+        verify_access_token_signature: false,
+        verify_channel_token_signature: false,
+        channel_token_optional: true,
+        original_access_token_upstream_header: 'AAA'
+      },
+    };
+
+    const resp = await postNegative(url, pluginPayload, 'post');
+
+    expect(resp.status, 'Status should be 400').to.equal(400);
+    expect(resp.data.name, 'Should indicate schema violation').to.equal(
+      'schema violation'
+    );
+    logResponse(resp);
+
+  });
+
+  it('should recreate jwt-signer plugin with the access token upstream header', async function () {
+    basePayload = {
+      name: 'jwt-signer',
+      service: {
+        id: serviceId,
+      },
+      route: {
+        id: routeId,
+      },
+    };
+    const pluginPayload = {
+      ...basePayload,
+      config: {
+        access_token_upstream_header: 'ABC',
+        verify_access_token_signature: false,
+        verify_channel_token_signature: false,
+        channel_token_optional: true,
+      },
+    };
+    const resp = await axios({ method: 'post', url, data: pluginPayload });
+    logResponse(resp);
+
+    expect(resp.status, 'Status should be 201').to.equal(201);
+    pluginId = resp.data.id;
+    await waitForConfigRebuild();
+  });
+
+  it('should proxy request with a valid token and verify the resigned token', async function () {
+    const expectedToken = {
+      username: 'demo',
+      iat: 1665610129,
+      iss: 'kong',
+      original_iss: 'demo'
+    }
+    const resp = await axios({
+      headers: validTokenHeaders,
+      url: `${proxyUrl}${path}`,
+    });
+    logResponse(resp);
+
+    expect(resp.status, 'Status should be 200').to.equal(200);
+    const decodedToken = jwtDecode(resp.data.headers['Abc']);
+    expect(
+      decodedToken, 'should see the decoded token in the response',
+    ).to.deep.equal(expectedToken);
+  });  
+
+  testClaims.forEach(({ description, config, expectedToken, headerKey, tokenHeaders }) => {
+    it(description, async function() {
+      let resp = await axios({
+        method: 'patch',
+        url: `${url}/${pluginId}`,
+        data: {
+          config: config
+        },
+      });
+
+      logResponse(resp);
+
+      expect(resp.status, 'Status should be 200').to.equal(200);
+
+      await waitForConfigRebuild()
+      
+      resp = await axios({
+        headers: tokenHeaders,
+        url: `${proxyUrl}${path}`,
+      });
+      logResponse(resp);
+
+      expect(resp.status, 'Status should be 200').to.equal(200);
+      const decodedToken = jwtDecode(resp.data.headers[headerKey]);
+      expect(
+        decodedToken, 'should see the decoded token in the response',
+      ).to.deep.equal(expectedToken);
+    });
+  });
+
   after(async function () {
     await deleteGatewayRoute(routeId);
     await deleteGatewayService(serviceId);
     await deleteConsumer(consumerId);
+    await deletePlugin(pluginId);
   });
 });
---
+++ spec-ee/kong-api-tests/test/gateway/plugins/jwt.spec.ts
@@ -0,0 +1,329 @@
+import axios from 'axios';
+import * as jwt from 'jsonwebtoken';
+import { authDetails } from '@fixtures';
+import {
+  expect,
+  Environment,
+  getBasePath,
+  createGatewayService,
+  deleteGatewayService,
+  createRouteForService,
+  deleteGatewayRoute,
+  logResponse,
+  createConsumer,
+  deleteConsumer,
+  getNegative,
+  retryRequest,
+  isGateway,
+  waitForConfigRebuild,
+} from '@support';
+
+describe('Gateway Plugins: jwt', function () {
+  const path = '/jwt';
+  const serviceName = 'jwt-service';
+  const algorithms = ['ES512', 'PS256', 'PS384', 'PS512', 'RS256', 'RS384', 'RS512', 'ES256', 'ES384'];
+
+  let serviceId: string;
+  let routeId: string;
+  let consumerId: string;
+  let iss: string;
+  let secret: string;
+  let basePayload: any;
+  let pluginId: string;
+
+  const url = `${getBasePath({
+    environment: isGateway() ? Environment.gateway.admin : undefined,
+  })}/plugins`;
+  const proxyUrl = `${getBasePath({
+    app: 'gateway',
+    environment: Environment.gateway.proxy,
+  })}`;
+  const consumerUrl = `${getBasePath({
+    environment: isGateway() ? Environment.gateway.admin : undefined,
+  })}/consumers`;
+
+
+  before(async function () {
+    const service = await createGatewayService(serviceName);
+    serviceId = service.id;
+    const route = await createRouteForService(serviceId, [path]);
+    routeId = route.id;
+    const consumerName = 'jwt-consumer';
+    const consumer = await createConsumer(consumerName);
+    consumerId = consumer.id;
+
+    basePayload = {
+      name: 'jwt',
+      service: {
+        id: serviceId,
+      },
+      route: {
+        id: routeId,
+      },
+    };
+  });
+
+  it('should create jwt plugin with default parameters', async function () {
+    basePayload = {
+      name: 'jwt',
+      service: {
+        id: serviceId,
+      },
+      route: {
+        id: routeId,
+      },
+    };
+    const pluginPayload = {
+      ...basePayload,
+      config: {},
+    };
+    const resp = await axios({ method: 'post', url, data: pluginPayload });
+    logResponse(resp);
+
+    expect(resp.status, 'Status should be 201').to.equal(201);
+
+    pluginId = resp.data.id;
+  });
+
+  it('should not proxy request without token', async function () {
+    await waitForConfigRebuild();
+
+    const req = () => getNegative(`${proxyUrl}${path}`);
+
+    const assertions = (resp) => {
+      logResponse(resp);
+      expect(resp.status, 'Status should be 401').to.equal(401);
+      expect(resp.data.message, 'Message should be unauthorized').to.equal(
+        'Unauthorized'
+      );
+    };
+
+    await retryRequest(req, assertions);
+  });
+
+  it('should not proxy request with iss claim absent from token', async function () {
+    let resp;
+
+    resp = await axios({
+        method: 'post',
+        url: `${consumerUrl}/${consumerId}/jwt`,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      });
+    
+    logResponse(resp);
+
+    expect(resp.status, 'Status should be 201').to.equal(201);
+
+    iss = resp.data.key;
+    secret = resp.data.secret;
+
+    const claims = {
+        'name': `${consumerId}`,
+    };
+    const jwt_token = jwt.sign(claims, secret, { algorithm: 'HS256' });
+
+    resp = await getNegative(`${proxyUrl}${path}`, {
+        'Authorization': `Bearer ${jwt_token}`
+    });
+
+    logResponse(resp);
+    expect(resp.data.message, 'Message should be unauthorized').to.equal(
+        "No mandatory 'iss' in claims"
+    );
+  
+    expect(resp.status, 'Status should be 401').to.equal(401);
+  });
+
+  it('should create HS256 JWT credential and proxy with the HS256 token', async function () {
+    let resp;
+
+    resp = await axios({
+        method: 'post',
+        url: `${consumerUrl}/${consumerId}/jwt`,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    });
+    
+    logResponse(resp);
+    expect(resp.status, 'Status should be 201').to.equal(201);
+
+    iss = resp.data.key;
+    secret = resp.data.secret;
+
+    const claims = {
+        'iss': iss,
+    };
+    const jwt_token = jwt.sign(claims, secret, { algorithm: 'HS256' });
+
+    await waitForConfigRebuild();
+
+    resp = await axios({
+        headers: {'Authorization': `Bearer ${jwt_token}`},
+        url: `${proxyUrl}${path}`,
+    });
+
+    logResponse(resp);
+  
+    expect(resp.status, 'Status should be 200').to.equal(200);
+  });
+
+  it('should be able to add exp to the claims', async function () {
+    const patchResp = await axios({
+        method: 'patch',
+        url: `${url}/${pluginId}`,
+        data: {
+            name: 'jwt',
+            config: {
+            claims_to_verify: ['exp'],
+            },
+        },
+    });
+    logResponse(patchResp);    
+    
+    expect(patchResp.status, 'Status should be 200').to.equal(200);
+    expect(
+        patchResp.data.config.claims_to_verify[0],
+        'Should add exp to the claims'
+    ).to.equal('exp');
+    await waitForConfigRebuild();
+  });
+    
+  it('should not proxy traffic if exp is absent from token', async function () {
+    const claims = {
+        'iss': `${iss}`,
+    };
+    const jwt_token = jwt.sign(claims, secret, { algorithm: 'HS256' });
+
+    const resp = await getNegative(`${proxyUrl}${path}`, {
+        'Authorization': `Bearer ${jwt_token}`
+    });
+
+    logResponse(resp);
+    expect(resp.data.exp, 'exp should be a number').to.equal(
+        'must be a number'
+    );
+
+    expect(resp.status, 'Status should be 401').to.equal(401); 
+  });
+
+  it('should not proxy traffic if invalid exp is present in token', async function () {
+    const now = new Date();
+    // Calculate the date 30 days away from now
+    const daysToSubstract = 30;
+    const futureDate = new Date(now.getTime() - daysToSubstract * 24 * 60 * 60 * 1000);   
+
+    const claims = {
+        'iss': `${iss}`,
+        'exp': Math.floor(futureDate.getTime() / 1000)
+    };
+    const jwt_token = jwt.sign(claims, secret, { algorithm: 'HS256' });
+
+    const resp = await getNegative(`${proxyUrl}${path}`, {
+        'Authorization': `Bearer ${jwt_token}`
+    });
+
+    logResponse(resp);
+    expect(resp.data.exp, 'Token is expired').to.equal('token expired');
+    expect(resp.status, 'Status should be 401').to.equal(401);   
+  });
+
+  it('should proxy traffic if valid exp is present in token', async function () {
+    const now = new Date();
+    // Calculate the date 30 days from now
+    const daysToAdd = 30;
+    const futureDate = new Date(now.getTime() + daysToAdd * 24 * 60 * 60 * 1000);   
+
+    const claims = {
+        'iss': `${iss}`,
+        'exp': Math.floor(futureDate.getTime() / 1000)
+    };
+
+    const jwt_token = jwt.sign(claims, secret, { algorithm: 'HS256' });
+
+    const resp = await axios({
+        headers: {'Authorization': `Bearer ${jwt_token}`},
+        url: `${proxyUrl}${path}`,
+    });
+    logResponse(resp);
+    
+    expect(resp.status, 'Status should be 200').to.equal(200);
+
+    await deleteConsumer(consumerId);
+  });
+
+  it('should be able to remove exp from the claims', async function () {
+    const patchResp = await axios({
+        method: 'patch',
+        url: `${url}/${pluginId}`,
+        data: {
+            name: 'jwt',
+            config: {
+            claims_to_verify: [],
+            },
+        },
+    });
+    logResponse(patchResp);    
+    
+    expect(patchResp.status, 'Status should be 200').to.equal(200);
+
+    await waitForConfigRebuild();
+  });
+
+  algorithms.forEach((algorithm) => {
+    it(`should proxy traffic with ${algorithm} algorithm`, async function () {
+        const publicKey = authDetails.jwt[`${algorithm}`]['public_key']
+        const privateKey = authDetails.jwt[`${algorithm}`]['private_key']
+
+        // create a consumer
+        const consumerName = `jwt-consumer-${algorithm}`;
+        const consumer = await createConsumer(consumerName);
+        consumerId = consumer.id;
+
+        // add the public key to consumer with the algorithm
+        let resp = await axios({
+            method: 'post',
+            url: `${consumerUrl}/${consumerId}/jwt`,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            data: {
+                algorithm: algorithm,
+                rsa_public_key: publicKey
+            },
+        });
+
+        logResponse(resp);
+
+        expect(resp.status, 'Status should be 201').to.equal(201);
+
+        iss = resp.data.key;
+        const claims = {
+            'iss': iss,
+        };
+        const jwt_token = jwt.sign(claims, privateKey, { algorithm: algorithm });
+
+        await waitForConfigRebuild();
+
+        resp = await axios({
+            headers: {'Authorization': `Bearer ${jwt_token}`},
+            url: `${proxyUrl}${path}`,
+        });
+
+        logResponse(resp);
+        expect(resp.status, 'Status should be 200').to.equal(200);
+        await deleteConsumer(consumerId);
+    });
+  });
+
+  it('should delete the jwt plugin', async function () {
+    const resp = await axios({
+      method: 'delete',
+      url: `${url}/${pluginId}`,
+    });
+    logResponse(resp);
+
+    expect(resp.status, 'Status should be 204').to.equal(204);
+  });
+
+  after(async function () {
+    await deleteGatewayRoute(routeId);
+    await deleteGatewayService(serviceId);
+  });
+});
