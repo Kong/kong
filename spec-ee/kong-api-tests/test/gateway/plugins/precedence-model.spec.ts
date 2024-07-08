bcec093c2145715d197e80a22ebb6a06cd172907
Jane Hitchcock <jane.hitchcock@konghq.com>
2024-07-08 11:31:58 -0500

    fix(tests): stabilizing precedence model test (#9657)
    * fix(tests): stabilizing precedence model test
    
    * Re-adding basic-auth

--- spec-ee/kong-api-tests/test/gateway/plugins/precedence-model.spec.ts
+++ spec-ee/kong-api-tests/test/gateway/plugins/precedence-model.spec.ts
@@ -8,20 +8,14 @@ import {
   addConsumerToConsumerGroup,
   createConsumer,
   createConsumerGroup,
-  deleteGatewayService,
-  deleteGatewayRoute,
-  deleteConsumer,
-  deleteConsumerGroup,
-  wait,
-  postNegative,
   logResponse,
   createPlugin,
-  deletePlugin,
-  getNegative,
   isGateway,
+  waitForConfigRebuild,
+  clearAllKongResources,
 } from '@support';
 
-describe.skip('Plugin Scope Precedence Model', () => {
+describe('Plugin Scope Precedence Model', () => {
   const url = `${getBasePath({
     environment: isGateway() ? Environment.gateway.admin : undefined,
   })}`;
@@ -29,51 +23,52 @@ describe.skip('Plugin Scope Precedence Model', () => {
     app: 'gateway',
     environment: Environment.gateway.proxy,
   });
-  let serviceId;
-  let routeId;
-  let consumerId;
-  let consumerGroupId;
-  let basicAuthPluginId;
-  let pluginIdList;
+  let serviceId
+  let routeId
+  let consumerId
+  let consumerGroupId
 
-  // array containing each scope array in order of precedence
-  const scopes = [
-    ['consumer', 'route', 'service'],
-    ['consumer_group', 'service', 'route'],
-    ['consumer', 'route'],
-    ['consumer', 'service'],
-    ['consumer_group', 'route'],
-    ['consumer_group', 'service'],
-    ['route', 'service'],
-    ['consumer'],
-    ['consumer_group'],
-    ['route'],
-    ['service'],
-    ['global'],
-  ];
-
-  const scopesConsumerGroups = scopes.filter((scope) => {
-    return scope.includes('consumer_group');
-  });
-
-  const scopesOther = scopes.filter((scope) => {
-    return !scope.includes('consumer_group');
-  });
+  function buildPayload(scope) {
+    const payload = {
+      name: 'request-transformer-advanced',
+      config: {
+        append: {
+          headers: [`x-test-header:${scope}`],
+        },
+      },
+    };
+    if (scope.includes('global')) {
+      return payload;
+    } else {
+      if (scope.includes('consumer')) {
+        payload['consumer'] = { id: consumerId };
+      }
+      if (scope.includes('consumer_group')) {
+        payload['consumer_group'] = { id: consumerGroupId };
+      }
+      if (scope.includes('route')) {
+        payload['route'] = { id: routeId };
+      }
+      if (scope.includes('service')) {
+        payload['service'] = { id: serviceId };
+      }
+      return payload
+    }
+  }
 
   before(async function () {
-    const service = await createGatewayService('PrecedenceService');
-    serviceId = service.id;
-    const route = await createRouteForService(serviceId, ['/precedence_test']);
-    routeId = route.id;
-    const consumer = await createConsumer();
-    consumerId = consumer.id;
-    const consumerGroup = await createConsumerGroup('PrecedenceGroup');
-    consumerGroupId = consumerGroup.id;
+    const service = await createGatewayService('PrecedenceService')
+    serviceId = service.id
+    const route = await createRouteForService(serviceId, ['/precedence_test'])
+    routeId = route.id
+    const consumer = await createConsumer()
+    consumerId = consumer.id
+    const consumerGroup = await createConsumerGroup('PrecedenceGroup')
+    consumerGroupId = consumerGroup.id
     await addConsumerToConsumerGroup(consumerId, consumerGroupId);
 
     // set up basic-auth plugin and add credentials for consumer
-    const basicAuthPlugin = await createPlugin({ name: 'basic-auth' });
-    basicAuthPluginId = basicAuthPlugin.id;
+    await createPlugin({ name: 'basic-auth' });
 
     await axios({
       url: `${url}/consumers/${consumerId}/basic-auth`,
@@ -83,138 +78,74 @@ describe.skip('Plugin Scope Precedence Model', () => {
         password: 'test',
       },
     });
-
-    await wait(3000); // eslint-disable-line no-restricted-syntax
+    
+    await waitForConfigRebuild()
   });
-
-  function buildPayload(scope) {
-    const payload = {
-      name: 'request-transformer-advanced',
-      config: {
-        add: {
-          headers: [`x-test-header:${scope}`],
-        },
-      },
-    };
-    if (scope.includes('global')) {
-      return payload;
-    } else {
-      if (scope.includes('consumer')) {
-        payload['consumer'] = {
-          id: consumerId,
-        };
-      }
-      if (scope.includes('consumer_group')) {
-        payload['consumer_group'] = {
-          id: consumerGroupId,
-        };
-      }
-      if (scope.includes('route')) {
-        payload['route'] = {
-          id: routeId,
-        };
-      }
-      if (scope.includes('service')) {
-        payload['service'] = {
-          id: serviceId,
-        };
-      }
-      return payload;
-    }
-  }
-
-  async function deletePlugins(idArray) {
-    if (!idArray) return;
-    idArray.map(async (id) => {
-      if (id) await deletePlugin(id);
-    });
-    // wait a couple seconds to ensure deletion
-    await wait(3000); // eslint-disable-line no-restricted-syntax
-  }
 
   it('should check that consumer group exists', async function () {
-    const resp = await getNegative(`${url}/consumer_groups/${consumerGroupId}`);
-    logResponse(resp);
-    expect(resp.status, 'Status should be 200').equal(200);
-    expect(resp.data.consumer_group.name).equal('PrecedenceGroup');
+    const resp = await axios.get(`${url}/consumer_groups/${consumerGroupId}`)
+    logResponse(resp)
+    expect(resp.status, 'Status should be 200').equal(200)
+    expect(resp.data.consumer_group.name).equal('PrecedenceGroup')
   });
 
-  //test each combination of consumer group and non-consumer group scopes
-  scopesConsumerGroups.forEach(async (scopeConsumer) => {
-    scopesOther.forEach(async (scopeOther) => {
-      const firstScope =
-        scopes.indexOf(scopeConsumer) < scopes.indexOf(scopeOther)
-          ? scopeConsumer
-          : scopeOther;
-      const secondScope =
-        firstScope === scopeConsumer ? scopeOther : scopeConsumer;
+  it(`should check that route scope takes precedence over global scope`, async function () {
+    await createPlugin(buildPayload(['route']))
+    await createPlugin(buildPayload(['global']))
+    await waitForConfigRebuild()
 
-      it(`should check that ${firstScope} takes precedence over ${secondScope}`, async function () {
-        const firstPayload = buildPayload(firstScope);
-        const secondPayload = buildPayload(secondScope);
+    const resp = await axios.get(`${proxyUrl}/precedence_test`, {headers: {authorization: `Basic ${Buffer.from('test:test').toString('base64')}`}})
 
-        const respFirst = await postNegative(`${url}/plugins`, firstPayload);
-        logResponse(respFirst);
-        expect(respFirst.status, 'Status should be 201').equal(201);
-        const firstPluginId = respFirst.data.id;
-
-        const respSecond = await postNegative(`${url}/plugins`, secondPayload);
-        logResponse(respSecond);
-        expect(respSecond.status, 'Status should be 201').equal(201);
-        const secondPluginId = respSecond.data.id;
-
-        pluginIdList = [firstPluginId, secondPluginId];
-
-        await wait(8000); // eslint-disable-line no-restricted-syntax
-
-        const resp = await getNegative(`${proxyUrl}/precedence_test`, {
-          authorization: `Basic ${Buffer.from('test:test').toString('base64')}`,
-        });
-        logResponse(resp);
-
-        expect(resp.status, 'Status should be 200').equal(200);
-
-        expect(resp.data.headers['X-Test-Header']).equal(firstScope.join(','));
-      });
-    });
+    logResponse(resp)
+    expect(resp.status, 'Status should be 200').equal(200)
+    expect(resp.data.headers['X-Test-Header']).contain('route')
   });
 
-  it('should create one plugin with each scope and ensure [consumer, route, service] takes precedence', async function () {
-    // create list comprehension of payloads for each scope
-    const payloads = scopes.map((scope) => {
-      return buildPayload(scope);
-    });
+  it('should check that consumer_group scope takes precedence over route scope', async function () {
+    // create consumer_group scoped plugin
+    await createPlugin(buildPayload(['consumer_group']))
+    await waitForConfigRebuild()
 
-    // create plugins for each scope
-    payloads.map(async (payload) => {
-      pluginIdList.push((await createPlugin(payload)).id);
-      console.log(pluginIdList);
-      const resp = await postNegative(`${url}/plugins`, payload);
-      logResponse(resp);
-      expect(resp.status, 'Status should be 201').equal(201);
-    });
+    const resp = await axios.get(`${proxyUrl}/precedence_test`, {headers: {authorization: `Basic ${Buffer.from('test:test').toString('base64')}`}})
 
-    await wait(8000); // eslint-disable-line no-restricted-syntax
+    expect(resp.status, 'Status should be 200').equal(200)
+    expect(resp.data.headers['X-Test-Header']).equal('consumer_group')
+  })
 
-    const resp = await getNegative(`${proxyUrl}/precedence_test`, {
-      authorization: `Basic ${Buffer.from('test:test').toString('base64')}`,
-    });
-    logResponse(resp);
+  it('should check that consumer scope takes precedence over consumer_group scope', async function () {
+    // create consumer scoped plugin
+    await createPlugin(buildPayload(['consumer']))
+    await waitForConfigRebuild()
 
-    expect(resp.status, 'Status should be 200').equal(200);
-    expect(resp.data.headers['X-Test-Header']).equal('consumer,route,service');
-  });
+    const resp = await axios.get(`${proxyUrl}/precedence_test`, {headers: {authorization: `Basic ${Buffer.from('test:test').toString('base64')}`}})
 
-  afterEach(async function () {
-    await deletePlugins(pluginIdList);
-    pluginIdList = [];
-  });
+    expect(resp.status, 'Status should be 200').equal(200)
+    expect(resp.data.headers['X-Test-Header']).equal('consumer')
+  })
+
+  it('should check that 2-entity scope takes precedence over single-entity scope', async function () {
+    // create consumer_group and service scoped plugin
+    await createPlugin(buildPayload(['consumer_group', 'service']))
+    await waitForConfigRebuild()
+
+    const resp = await axios.get(`${proxyUrl}/precedence_test`, {headers: {authorization: `Basic ${Buffer.from('test:test').toString('base64')}`}})
+
+    expect(resp.status, 'Status should be 200').equal(200)
+    expect(resp.data.headers['X-Test-Header']).equal('consumer_group,service')
+  })
+
+  it('should check that 3-entity scope takes precedence over 2-entity scope', async function () {
+    // create consumer, route, and service scoped plugin
+    await createPlugin(buildPayload(['consumer', 'route', 'service']))
+    await waitForConfigRebuild()
+
+    const resp = await axios.get(`${proxyUrl}/precedence_test`, {headers: {authorization: `Basic ${Buffer.from('test:test').toString('base64')}`}})
+
+    expect(resp.status, 'Status should be 200').equal(200)
+    expect(resp.data.headers['X-Test-Header']).equal('consumer,route,service')
+  })
 
   after(async function () {
-    await deleteGatewayRoute(routeId);
-    await deleteGatewayService(serviceId);
-    await deleteConsumer(consumerId);
-    await deleteConsumerGroup(consumerGroupId);
-    await deletePlugin(basicAuthPluginId);
-  });
-});
+    await clearAllKongResources();
+  })
+})
