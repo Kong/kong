989a13e9332d01ff392bc91fb61b4ed6279b2513
Zhefeng C <38037704+catbro666@users.noreply.github.com>
2024-07-11 15:31:07 +0800

    fix(runloop): the sni cache isn't invalidated when a sni is updated (#13165)
    Both `data.entity` and `data.old_entity` should be invalidated when
    a sni is updated. A non-existent sni may also have been cached.
    
    https://konghq.atlassian.net/browse/FTI-6009

--- kong/runloop/events.lua
+++ kong/runloop/events.lua
@@ -258,20 +258,29 @@ local function crud_plugins_handler(data)
 end
 
 
-local function crud_snis_handler(data)
-  log(DEBUG, "[events] SNI updated, invalidating cached certificates")
-
-  local sni = data.old_entity or data.entity
-  local sni_name = sni.name
+local function invalidate_snis(sni_name)
   local sni_wild_pref, sni_wild_suf = certificate.produce_wild_snis(sni_name)
   core_cache:invalidate("snis:" .. sni_name)
 
-  if sni_wild_pref then
+  if sni_wild_pref and sni_wild_pref ~= sni_name then
     core_cache:invalidate("snis:" .. sni_wild_pref)
   end
 
-  if sni_wild_suf then
+  if sni_wild_suf and sni_wild_suf ~= sni_name then
     core_cache:invalidate("snis:" .. sni_wild_suf)
+  end
+end
+
+
+local function crud_snis_handler(data)
+  log(DEBUG, "[events] SNI updated, invalidating cached certificates")
+
+  local new_name = data.entity.name
+  local old_name = data.old_entity and data.old_entity.name
+
+  invalidate_snis(new_name)
+  if old_name and old_name ~= new_name then
+    invalidate_snis(old_name)
   end
 end
 
