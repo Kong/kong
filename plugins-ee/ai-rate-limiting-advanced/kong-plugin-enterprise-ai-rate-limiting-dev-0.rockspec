9c86a093559f7bac242715218498b00d3e927b31
Mikołaj Nowak <mikolaj.nowak@konghq.com>
2024-07-11 15:03:45 +0200

    chore(migration): add ai-rate-limiting-advanced migrations for redis timeout
    The migrations for other plugins were added in commit: 48f49e0650b6e7c5dd64c9f21e904463bf201220
    
    KAG-2130

--- plugins-ee/ai-rate-limiting-advanced/kong-plugin-enterprise-ai-rate-limiting-dev-0.rockspec
+++ plugins-ee/ai-rate-limiting-advanced/kong-plugin-enterprise-ai-rate-limiting-dev-0.rockspec
@@ -16,5 +16,8 @@ build = {
   modules = {
     ["kong.plugins.ai-rate-limiting-advanced.handler"] = "kong/plugins/ai-rate-limiting-advanced/handler.lua",
     ["kong.plugins.ai-rate-limiting-advanced.schema"] = "kong/plugins/ai-rate-limiting-advanced/schema.lua",
+    ["kong.plugins.ai-rate-limiting-advanced.migrations"]     = "kong/plugins/ai-rate-limiting-advanced/migrations/init.lua",
+    ["kong.plugins.ai-rate-limiting-advanced.migrations.001_370_to_380"] =
+       "kong/plugins/ai-rate-limiting-advanced/migrations/001_370_to_380.lua",
   }
 }
---
+++ plugins-ee/ai-rate-limiting-advanced/kong/plugins/ai-rate-limiting-advanced/migrations/001_370_to_380.lua
@@ -0,0 +1,50 @@
+-- This software is copyright Kong Inc. and its licensors.
+-- Use of the software is subject to the agreement between your organization
+-- and Kong Inc. If there is no such agreement, use is governed by and
+-- subject to the terms of the Kong Master Software License Agreement found
+-- at https://konghq.com/enterprisesoftwarelicense/.
+-- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
+
+
+-- This migration updates plugin's config by removing timeout field as it's been deprecated (it then populates read_timeout, send_timeout and connect_timeout if they're not set)
+
+return {
+  postgres = {
+    up = [[
+      DO $$
+      BEGIN
+      UPDATE plugins
+      SET config =
+        jsonb_set(
+          config,
+          '{redis}',
+          (config -> 'redis') ||
+            jsonb_build_object(
+              'read_timeout', case when (config #>> '{redis,read_timeout}') IS NULL then (config #>> '{redis,timeout}')::integer else (config #>> '{redis,read_timeout}')::integer end,
+              'send_timeout', case when (config #>> '{redis,send_timeout}') IS NULL then (config #>> '{redis,timeout}')::integer else (config #>> '{redis,send_timeout}')::integer end,
+              'connect_timeout', case when (config #>> '{redis,connect_timeout}') IS NULL then (config #>> '{redis,timeout}')::integer else (config #>> '{redis,connect_timeout}')::integer end
+            )
+        )
+      WHERE name = 'ai-rate-limiting-advanced';
+      EXCEPTION WHEN UNDEFINED_COLUMN OR UNDEFINED_TABLE THEN
+        -- Do nothing, accept existing state
+      END$$;
+    ]],
+
+    teardown = function(connector, _)
+      local sql = [[
+        DO $$
+        BEGIN
+        UPDATE plugins
+        SET config =
+          jsonb_set(config, '{redis}', (config -> 'redis') - 'timeout')
+        WHERE name = 'ai-rate-limiting-advanced';
+        EXCEPTION WHEN UNDEFINED_COLUMN OR UNDEFINED_TABLE THEN
+          -- Do nothing, accept existing state
+        END$$;
+      ]]
+      assert(connector:query(sql))
+      return true
+    end,
+  },
+}
