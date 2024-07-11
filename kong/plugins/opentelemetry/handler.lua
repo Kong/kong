d229aa376492908cfd59a7dc8dc8c70ae319164c
Hans Hübner <hans.huebner@gmail.com>
2024-07-11 16:10:46 +0200

    feat(opentelemetry): send global metrics as logs

--- kong/plugins/opentelemetry/handler.lua
+++ kong/plugins/opentelemetry/handler.lua
@@ -13,12 +13,52 @@ local OpenTelemetryHandler = {
 }
 
 
+local function log_global_metrics(premature)
+  if (premature) then
+    return
+  end
+
+  -- Gather shm and worker metrics
+  local metrics = kong.node.get_memory_stats()
+  local attributes = {}
+  for name, data in pairs(metrics.lua_shared_dicts) do
+    attributes["lua_shared_dict." .. name .. ".allocated_slabs"] = data.allocated_slabs
+    attributes["lua_shared_dict." .. name .. ".capacity"] = data.capacity
+  end
+  for i, data in ipairs(metrics.workers_lua_vms) do
+    attributes["worker." .. tostring(i-1) .. ".http_allocated_gc"] = data.http_allocated_gc
+    attributes["worker." .. tostring(i-1) .. ".pid"] = data.pid
+  end
+  -- Gather nginx statistics
+  local nginx_statistics = kong.nginx.get_statistics()
+  local subsystem = ngx.config.subsystem
+  attributes[subsystem .. ".connections_active"] = nginx_statistics.connections_active
+  attributes[subsystem .. ".connections_accepted"] = nginx_statistics.connections_accepted
+  attributes[subsystem .. ".connections_handled"] = nginx_statistics.connections_handled
+  attributes[subsystem .. ".connections_reading"] = nginx_statistics.connections_reading
+  attributes[subsystem .. ".connections_writing"] = nginx_statistics.connections_writing
+  attributes[subsystem .. ".connections_waiting"] = nginx_statistics.connections_waiting
+  attributes[subsystem .. ".total"] = nginx_statistics.total_requests
+
+  -- Send log message with metrics data
+  kong.telemetry.log("kong", {}, "global_metrics", "global metrics", attributes)
+end
+
+
+function OpenTelemetryHandler:init_worker()
+  if ngx.worker.id() == 0 then
+    kong.timer:named_every("global metrics", 1, log_global_metrics)
+  end
+end
+
+
 function OpenTelemetryHandler:configure(configs)
   if configs then
     for _, config in ipairs(configs) do
       if config.logs_endpoint then
         dynamic_hook.hook("observability_logs", "push", o11y_logs.maybe_push)
         dynamic_hook.enable_by_default("observability_logs")
+        break
       end
     end
   end
