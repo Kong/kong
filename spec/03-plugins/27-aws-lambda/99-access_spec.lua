Kong/kong

commit 76f2612aaea6df0bc83fa8b485b7420279c177f9

feat(aws-lambda): add new configuration field `empty_arrays_mode` to control empty array decoding behavior (#13084)
Co-authored-by: Yusheng Li <leeys.top@gmail.com>
Co-authored-by: Zachary Hu <6426329+outsinre@users.noreply.github.com>

kong/plugins/aws-lambda/handler.lua
@@ -18,6 +18,7 @@ local VIA_HEADER_VALUE = meta._NAME .. "/" .. meta._VERSION
 local request_util = require "kong.plugins.aws-lambda.request-util"
 local build_request_payload = request_util.build_request_payload
 local extract_proxy_response = request_util.extract_proxy_response
+local remove_array_mt_for_empty_table = request_util.remove_array_mt_for_empty_table
 
 local aws = require("resty.aws")
 local AWS_GLOBAL_CONFIG
@@ -238,6 +239,18 @@ function AWSLambdaHandler:access(conf)
 
   if kong.configuration.enabled_headers[VIA_HEADER] then
     headers[VIA_HEADER] = VIA_HEADER_VALUE
+  end
+
+  -- TODO: remove this in the next major release
+  -- function to remove array_mt metatables from empty tables
+  -- This is just a backward compatibility code to keep a
+  -- long-lived behavior that Kong responsed JSON objects
+  -- instead of JSON arrays for empty arrays.
+  if conf.empty_arrays_mode == "legacy" then
+    local ct = headers["Content-Type"]
+    if ct and ct:lower():match("application/.*json") then
+      content = remove_array_mt_for_empty_table(content)
+    end
   end
 
   return kong.response.exit(status, content, headers)

spec/03-plugins/27-aws-lambda/99-access_spec.lua
@@ -176,6 +176,18 @@ for _, strategy in helpers.each_strategy() do
         service     = null,
       }
 
+      local route26 = bp.routes:insert {
+        hosts       = { "lambda26.test" },
+        protocols   = { "http", "https" },
+        service     = null,
+      }
+
+      local route27 = bp.routes:insert {
+        hosts       = { "lambda27.test" },
+        protocols   = { "http", "https" },
+        service     = null,
+      }
+
       bp.plugins:insert {
         name     = "aws-lambda",
         route    = { id = route1.id },
@@ -519,6 +531,32 @@ for _, strategy in helpers.each_strategy() do
         name = "http-log",
         config   = {
           http_endpoint = "http://localhost:" .. mock_http_server_port,
+        }
+      }
+
+      bp.plugins:insert {
+        name     = "aws-lambda",
+        route    = { id = route26.id },
+        config                 = {
+          port                 = 10001,
+          aws_key              = "mock-key",
+          aws_secret           = "mock-secret",
+          aws_region           = "us-east-1",
+          function_name        = "functionWithEmptyArray",
+          empty_arrays_mode    = "legacy",
+        }
+      }
+
+      bp.plugins:insert {
+        name     = "aws-lambda",
+        route    = { id = route27.id },
+        config                 = {
+          port                 = 10001,
+          aws_key              = "mock-key",
+          aws_secret           = "mock-secret",
+          aws_region           = "us-east-1",
+          function_name        = "functionWithEmptyArray",
+          empty_arrays_mode    = "correct",
         }
       }
 
@@ -921,6 +959,30 @@ for _, strategy in helpers.each_strategy() do
           local _, count = logs:gsub([[%[aws%-lambda%].+lambda%.ab%-cdef%-1%.amazonaws%.com.+name error"]], "")
           return count >= 1
         end, 10)
+      end)
+
+      it("invokes a Lambda function with empty array", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            ["Host"] = "lambda26.test"
+          }
+        })
+
+        local body = assert.res_status(200, res)
+        assert.matches("\"testbody\":{}", body)
+
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            ["Host"] = "lambda27.test"
+          }
+        })
+
+        local body = assert.res_status(200, res)
+        assert.matches("\"testbody\":%[%]", body)
       end)
 
       describe("config.is_proxy_integration = true", function()

spec/fixtures/aws-lambda.lua
@@ -65,6 +65,11 @@ local fixtures = {
                       ngx.sleep(2)
                       ngx.say("{\"statusCodge\": 200, \"body\": \"dGVzdA=\", \"isBase64Encoded\": false}")
 
+                    elseif string.match(ngx.var.uri, "functionWithEmptyArray") then
+                      ngx.header["Content-Type"] = "application/json"
+                      local str = "{\"statusCode\": 200, \"testbody\": [], \"isBase64Encoded\": false}"
+                      ngx.say(str)
+
                     elseif type(res) == 'string' then
                       ngx.header["Content-Length"] = #res + 1
                       ngx.say(res)

kong-3.8.0-0.rockspec
@@ -33,7 +33,7 @@ dependencies = {
   "lua-protobuf == 0.5.1",
   "lua-resty-healthcheck == 3.0.2",
   "lua-messagepack == 0.5.4",
-  "lua-resty-aws == 1.4.1",
+  "lua-resty-aws == 1.5.0",
   "lua-resty-openssl == 1.4.0",
   "lua-resty-counter == 0.2.1",
   "lua-resty-ipmatcher == 0.6.1",

kong/clustering/compat/removed_fields.lua
@@ -148,5 +148,8 @@ return {
     response_transformer = {
       "rename.json",
     },
+    aws_lambda = {
+      "empty_arrays_mode",
+    }
   },
 }

kong/plugins/aws-lambda/request-util.lua
@@ -9,6 +9,7 @@ local get_request_id = require("kong.tracing.request_id").get
 
 local EMPTY = {}
 
+local isempty = require "table.isempty"
 local split = pl_stringx.split
 local ngx_req_get_headers  = ngx.req.get_headers
 local ngx_req_get_uri_args = ngx.req.get_uri_args
@@ -325,10 +326,36 @@ local function build_request_payload(conf)
 end
 
 
+-- TODO: remove this in the next major release
+-- function to remove array_mt metatables from empty tables
+-- This is just a backward compatibility code to keep a
+-- long-lived behavior that Kong responsed JSON objects
+-- instead of JSON arrays for empty arrays.
+local function remove_array_mt_for_empty_table(tbl)
+  if type(tbl) ~= "table" then
+    return tbl
+  end
+
+  -- Check if the current table(array) is empty and has a array_mt metatable, and remove it
+  if isempty(tbl) and getmetatable(tbl) == cjson.array_mt then
+    setmetatable(tbl, nil)
+  end
+
+  for _, value in pairs(tbl) do
+    if type(value) == "table" then
+      remove_array_mt_for_empty_table(value)
+    end
+  end
+
+  return tbl
+end
+
+
 return {
   aws_serializer = aws_serializer,
   validate_http_status_code = validate_http_status_code,
   validate_custom_response = validate_custom_response,
   build_request_payload = build_request_payload,
   extract_proxy_response = extract_proxy_response,
+  remove_array_mt_for_empty_table = remove_array_mt_for_empty_table,
 }

kong/plugins/aws-lambda/schema.lua
@@ -116,6 +116,13 @@ return {
           default = "v1",
           one_of = { "v1", "v2" }
         } },
+        { empty_arrays_mode = { -- TODO: this config field is added for backward compatibility and will be removed in next major version
+          description = "An optional value that defines whether Kong should send empty arrays (returned by Lambda function) as `[]` arrays or `{}` objects in JSON responses. The value `legacy` means Kong will send empty arrays as `{}` objects in response",
+          type = "string",
+          required = true,
+          default = "legacy",
+          one_of = { "legacy", "correct" }
+        } },
       }
     },
   } },

commit 6f5684de30c2f8647a200a56f9d950027cbd74a9

fix(aws-lambda): fix an incompatibility with multi value header in proxy integration and legacy empty arrays mode (#13381)
This PR fixes a bug that introduced by #13084, that when is_proxy_integration mode is set to true with empty_arrays_mode set to legacy, and lambda function returns multiValueHeader that contains a Content-Type with single value array, the plugin cannot work correctly.

The multiValueHeaders field in the proxy integration response can be an array even if the header has only one value. I've tested AWS API gateway and the Kong gateway before #13084 and confirmed that both would work fine with Content-Type single value array inside the mutliValueHeader response field.

The reason of #13084 brings in this issue is that before #13084 we did not have any logic about reading Content-Type so no problem existed. The single value array will goes into kong.response.exit and normalized automatically.

This PR fixes the problem that when Content-Type returned by the lambda function is ["application-json"], the plugin cannot execute the if-block correctly due to not being able to call the lower function on the array(table) object.

Although there is a tiny possibility that the user will encounter this issue(because I think mostly people will define Content-Type inside the headers field instead of the mutliValueHeaders since it should be only one value), it is still good to fix this issue and let it behave like before.

https://konghq.atlassian.net/browse/FTI-6100

---------

Co-authored-by: Zachary Hu <6426329+outsinre@users.noreply.github.com>

kong/plugins/aws-lambda/handler.lua
@@ -4,6 +4,7 @@ local ngx_var = ngx.var
 local ngx_now = ngx.now
 local ngx_update_time = ngx.update_time
 local md5_bin = ngx.md5_bin
+local re_match = ngx.re.match
 local fmt = string.format
 local buffer = require "string.buffer"
 local lrucache = require "resty.lrucache"
@@ -252,7 +253,13 @@ function AWSLambdaHandler:access(conf)
   -- instead of JSON arrays for empty arrays.
   if conf.empty_arrays_mode == "legacy" then
     local ct = headers["Content-Type"]
-    if ct and ct:lower():match("application/.*json") then
+    -- If Content-Type is specified by multiValueHeader then
+    -- it will be an array, so we need to get the first element
+    if type(ct) == "table" and #ct > 0 then
+      ct = ct[1]
+    end
+
+    if ct and type(ct) == "string" and re_match(ct:lower(), "application/.*json", "jo") then
       content = remove_array_mt_for_empty_table(content)
     end
   end

spec/03-plugins/27-aws-lambda/99-access_spec.lua
@@ -188,6 +188,12 @@ for _, strategy in helpers.each_strategy() do
         service     = null,
       }
 
+      local route28 = bp.routes:insert {
+        hosts       = { "lambda28.test" },
+        protocols   = { "http", "https" },
+        service     = null,
+      }
+
       bp.plugins:insert {
         name     = "aws-lambda",
         route    = { id = route1.id },
@@ -557,6 +563,20 @@ for _, strategy in helpers.each_strategy() do
           aws_region           = "us-east-1",
           function_name        = "functionWithEmptyArray",
           empty_arrays_mode    = "correct",
+        }
+      }
+
+      bp.plugins:insert {
+        name     = "aws-lambda",
+        route    = { id = route28.id },
+        config                 = {
+          port                 = 10001,
+          aws_key              = "mock-key",
+          aws_secret           = "mock-secret",
+          aws_region           = "us-east-1",
+          function_name        = "functionWithArrayCTypeInMVHAndEmptyArray",
+          empty_arrays_mode    = "legacy",
+          is_proxy_integration = true,
         }
       }
 
@@ -983,6 +1003,19 @@ for _, strategy in helpers.each_strategy() do
 
         local body = assert.res_status(200, res)
         assert.matches("\"testbody\":%[%]", body)
+      end)
+
+      it("invokes a Lambda function with legacy empty array mode and mutlivalueheaders", function()
+        local res = assert(proxy_client:send {
+          method  = "GET",
+          path    = "/get",
+          headers = {
+            ["Host"] = "lambda28.test"
+          }
+        })
+
+        local _ = assert.res_status(200, res)
+        assert.equal("application/json+test", res.headers["Content-Type"])
       end)
 
       describe("config.is_proxy_integration = true", function()

spec/fixtures/aws-lambda.lua
@@ -70,6 +70,10 @@ local fixtures = {
                       local str = "{\"statusCode\": 200, \"testbody\": [], \"isBase64Encoded\": false}"
                       ngx.say(str)
 
+                    elseif string.match(ngx.var.uri, "functionWithArrayCTypeInMVHAndEmptyArray") then
+                      ngx.header["Content-Type"] = "application/json"
+                      ngx.say("{\"statusCode\": 200, \"isBase64Encoded\": true, \"body\": \"eyJrZXkiOiAidmFsdWUiLCAia2V5MiI6IFtdfQ==\", \"headers\": {}, \"multiValueHeaders\": {\"Content-Type\": [\"application/json+test\"]}}")
+
                     elseif type(res) == 'string' then
                       ngx.header["Content-Length"] = #res + 1
                       ngx.say(res)