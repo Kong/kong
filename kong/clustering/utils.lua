18babd638dca23bb81b3cf3abfbc05c9009eb9ee
chronolaw <chrono_cpp@me.com>
2024-07-24 13:45:38 +0800

    cluster_cjson

--- kong/clustering/utils.lua
+++ kong/clustering/utils.lua
@@ -168,7 +168,7 @@ end
 
 -- encode/decode json with cjson or simdjson
 local ok, simdjson = pcall(require, "resty.simdjson")
-if not ok or not kong.configuration.cluster_simd_json then
+if not ok or kong.configuration.cluster_cjson then
   local cjson = require("cjson.safe")
 
   _M.json_decode = cjson.decode
