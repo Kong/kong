443f2c56a2291cb727850bbb9a9e98636b49680b
Yufu Zhao <ms2008vip@gmail.com>
2024-07-26 11:14:07 +0800

    fix(opentelemetry): migration exception when upgrading from below version 3.3 to 3.7 (#13391)

--- spec/05-migration/plugins/opentelemetry/migrations/001_331_to_332_spec.lua
+++ spec/05-migration/plugins/opentelemetry/migrations/001_331_to_332_spec.lua
@@ -10,7 +10,56 @@ local uh = require "spec.upgrade_helpers"
 
 
 if uh.database_type() == 'postgres' then
-    local handler = uh.get_busted_handler("3.3.x.x", "3.6.x.x")
+    local handler = uh.get_busted_handler("3.0.x.x", "3.2.x.x")
+    handler("opentelemetry plugin migration", function()
+        lazy_setup(function()
+            assert(uh.start_kong())
+        end)
+
+        lazy_teardown(function ()
+            assert(uh.stop_kong())
+        end)
+
+        uh.setup(function ()
+            local admin_client = assert(uh.admin_client())
+
+            local res = assert(admin_client:send {
+                method = "POST",
+                path = "/plugins/",
+                body = {
+                    name = "opentelemetry",
+                    config = {
+                        endpoint = "http://localhost:8080/v1/traces",
+                    }
+                },
+                headers = {
+                    ["Content-Type"] = "application/json"
+                }
+            })
+            assert.res_status(201, res)
+            admin_client:close()
+        end)
+
+        uh.new_after_finish("has opentelemetry queue configuration", function ()
+            local admin_client = assert(uh.admin_client())
+            local res = assert(admin_client:send {
+                method = "GET",
+                path = "/plugins/"
+            })
+            local body = cjson.decode(assert.res_status(200, res))
+            assert.equal(1, #body.data)
+            assert.equal("opentelemetry", body.data[1].name)
+            local expected_config = {
+                queue = {
+                    max_batch_size = 200
+                },
+            }
+            assert.partial_match(expected_config, body.data[1].config)
+            admin_client:close()
+        end)
+    end)
+
+    handler = uh.get_busted_handler("3.3.x.x", "3.6.x.x")
     handler("opentelemetry plugin migration", function()
         lazy_setup(function()
             assert(uh.start_kong())
