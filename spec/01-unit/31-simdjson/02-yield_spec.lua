a4dcfcff00eac07b4a5f32f7b7d80374f0eeed4e
chronolaw <chrono_cpp@me.com>
2024-07-31 10:22:24 +0800

    disable/enable test

--- spec/01-unit/31-simdjson/02-yield_spec.lua
+++ spec/01-unit/31-simdjson/02-yield_spec.lua
@@ -1,8 +1,26 @@
-describe("[enable yield] ", function ()
-  local spy_ngx_sleep = spy.on(ngx, "sleep")
-  local simdjson = require("resty.simdjson")
+local orig_ngx_sleep = ngx.sleep
 
-  it("when encoding", function()
+
+local spy_ngx_sleep
+local simdjson
+
+
+describe("[yield] ", function ()
+  before_each(function()
+    spy_ngx_sleep = spy.on(ngx, "sleep")
+    simdjson = require("resty.simdjson")
+  end)
+
+
+  after_each(function()
+    ngx.sleep = orig_ngx_sleep
+    package.loaded["resty.simdjson"] = nil
+    package.loaded["resty.simdjson.decoder"] = nil
+    package.loaded["resty.simdjson.encoder"] = nil
+  end)
+
+
+  it("enabled when encoding", function()
 
     local parser = simdjson.new(true)
     assert(parser)
@@ -17,7 +35,22 @@ describe("[enable yield] ", function ()
   end)
 
 
-  it("when decoding", function()
+  it("disabled when encoding", function()
+
+    local parser = simdjson.new(false)
+    assert(parser)
+
+    local str = parser:encode({ str = string.rep("a", 2100) })
+
+    parser:destroy()
+
+    assert(str)
+    assert(type(str) == "string")
+    assert.spy(spy_ngx_sleep).was_called(0)
+  end)
+
+
+  it("enabled when decoding", function()
 
     local a = {}
     for i = 1, 1000 do
@@ -33,6 +66,26 @@ describe("[enable yield] ", function ()
 
     assert(obj)
     assert(type(obj) == "table")
-    assert.spy(spy_ngx_sleep).was_called(2)
+    assert.spy(spy_ngx_sleep).was_called(1)
+  end)
+
+
+  it("disabled when decoding", function()
+
+    local a = {}
+    for i = 1, 1000 do
+      a[i] = i
+    end
+
+    local parser = simdjson.new(false)
+    assert(parser)
+
+    local obj = parser:decode("[" .. table.concat(a, ",") .. "]")
+
+    parser:destroy()
+
+    assert(obj)
+    assert(type(obj) == "table")
+    assert.spy(spy_ngx_sleep).was_called(0)
   end)
 end)
