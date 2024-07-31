3c5934188fea1763d9a8ad62c911ccec854779fd
chronolaw <chrono_cpp@me.com>
2024-07-31 10:06:57 +0800

    tests(core): add test cases for simdjson yielding

---
+++ spec/01-unit/31-simdjson/02-yield_spec.lua
@@ -0,0 +1,48 @@
+local helpers = require("spec.helpers")
+
+
+describe("[enable yield] ", function ()
+  local simdjson
+  local spy_ngx_sleep = spy.on(ngx, "sleep")
+
+  lazy_setup(function()
+    simdjson = require("resty.simdjson")
+  end)
+
+  lazy_teardown(function()
+  end)
+
+  it("when encoding", function()
+
+    local parser = simdjson.new(true)
+    assert(parser)
+
+    local str = parser:encode({ str = string.rep("a", 2100) })
+
+    parser:destroy()
+
+    assert(str)
+    assert(type(str) == "string")
+    assert.spy(spy_ngx_sleep).was_called(1)
+  end)
+
+
+  it("when decoding", function()
+
+    local a = {}
+    for i = 1, 1000 do
+      a[i] = i
+    end
+
+    local parser = simdjson.new(true)
+    assert(parser)
+
+    local obj = parser:decode("[" .. table.concat(a, ",") .. "]")
+
+    parser:destroy()
+
+    assert(obj)
+    assert(type(obj) == "table")
+    assert.spy(spy_ngx_sleep).was_called(2)
+  end)
+end)
