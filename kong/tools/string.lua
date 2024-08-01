cffc4a9a3946e4861f7772f4cac72909bba1eb38
Niklaus Schen <8458369+Water-Melon@users.noreply.github.com>
2024-08-01 14:33:50 +0800

    feat(tools): add a function to parse Nginx size strings (#13432)
    This function will be used in Kong-EE.
    
    KAG-4698

--- kong/tools/string.lua
+++ kong/tools/string.lua
@@ -151,6 +151,35 @@ function _M.bytes_to_str(bytes, unit, scale)
 end
 
 
+local SCALES = {
+  k = 1024,
+  K = 1024,
+  m = 1024 * 1024,
+  M = 1024 * 1024,
+  g = 1024 * 1024 * 1024,
+  G = 1024 * 1024 * 1024,
+}
+
+function _M.parse_ngx_size(str)
+  assert(type(str) == "string", "Parameter #1 must be a string")
+
+  local len = #str
+  local unit = sub(str, len)
+  local scale = SCALES[unit]
+
+  if scale then
+    len = len - 1
+
+  else
+    scale = 1
+  end
+
+  local size = tonumber(sub(str, 1, len)) or 0
+
+  return size * scale
+end
+
+
 local try_decode_base64
 do
   local decode_base64    = ngx.decode_base64
