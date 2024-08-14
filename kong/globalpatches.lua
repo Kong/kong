1bd9468291867e1625ed03141f54ea78ac49dcd1
ProBrian <chenjingbo35@gmail.com>
2024-08-14 11:47:16 +0800

    refine annotations

--- kong/globalpatches.lua
+++ kong/globalpatches.lua
@@ -521,7 +521,7 @@ return function(options)
 
     local client = package.loaded["kong.resty.dns.client"]
     if not client then
-      -- dns initialized here, can't be eliminated due to test dependencies
+      -- dns initialization here is essential for busted tests.
       client = require("kong.tools.dns")()
     end
 
