0242a79d20181b95540d96bd92f82dbad2293e81
Niklaus Schen <8458369+Water-Melon@users.noreply.github.com>
2024-09-13 10:35:14 +0800

    chore(build): use gcc8 on amazonlinux2 (#10229)
    KAG-5335
    
    Cherry-picked from https://github.com/Kong/kong/pull/13637

--- spec-ee/kong-api-tests/test/gateway/plugins/app-dynamics.spec.ts
+++ spec-ee/kong-api-tests/test/gateway/plugins/app-dynamics.spec.ts
@@ -130,7 +130,7 @@ import {
 
     // send 150 requests to seed data for the plugin
     // it appears the Sigularityheader is not always present in the first 5 requests, so try 30 times
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 100; i++) {
       // eslint-disable-next-line no-restricted-syntax
       resp = await sendRequestsAndReturnLastResp(5, '')
 
