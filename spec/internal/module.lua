8dc5340c2e333dc1a11d538a25186c17477af9b0
chronolaw <chrono_cpp@me.com>
2024-09-19 13:37:18 +0800

    func reload_helpers()

--- spec/internal/module.lua
+++ spec/internal/module.lua
@@ -6,6 +6,36 @@ local function reload(name)
 end
 
 
+local reload_helpers
+do
+  local misc = require("spec.internal.misc")
+
+  -- flavor could be "traditional","traditional_compatible" or "expressions"
+  -- changing flavor will change db's schema
+  reload_helpers= function(flavor)
+    _G.kong = {
+      configuration = {
+        router_flavor = flavor,
+      },
+    }
+
+    misc.setenv("KONG_ROUTER_FLAVOR", flavor)
+
+    reload("kong.global")
+    reload("kong.cache")
+    reload("kong.db")
+    reload("kong.db.schema.entities.routes_subschemas")
+
+    local helpers = reload("spec.helpers")
+
+    misc.unsetenv("KONG_ROUTER_FLAVOR")
+
+    return helpers
+  end
+end
+
+
 return {
   reload = reload,
+  reload_helpers = reload_helpers,
 }
