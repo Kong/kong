2c0184e0655ffbdce029909c210a882de97850b7
chronolaw <chrono_cpp@me.com>
2024-09-26 18:38:02 +0800

    20-wasm/06-clustering_spec.lua add inc_sync

--- spec/02-integration/20-wasm/06-clustering_spec.lua
+++ spec/02-integration/20-wasm/06-clustering_spec.lua
@@ -72,7 +72,8 @@ local function new_wasm_filter_directory()
 end
 
 
-describe("#wasm - hybrid mode #postgres", function()
+for _, inc_sync in ipairs { "off"  } do
+describe("#wasm - hybrid mode #postgres" .. " inc_sync=" .. inc_sync, function()
   local cp_prefix = "cp"
   local cp_errlog = cp_prefix .. "/logs/error.log"
   local cp_filter_path
@@ -113,6 +114,7 @@ describe("#wasm - hybrid mode #postgres", function()
       wasm_filters        = "user", -- don't enable bundled filters for this test
       wasm_filters_path   = cp_filter_path,
       nginx_main_worker_processes = 2,
+      cluster_incremental_sync = inc_sync,
     }))
 
     assert.logfile(cp_errlog).has.line([[successfully loaded "response_transformer" module]], true, 10)
@@ -152,6 +154,7 @@ describe("#wasm - hybrid mode #postgres", function()
         wasm_filters_path     = dp_filter_path,
         node_id               = node_id,
         nginx_main_worker_processes = 2,
+        cluster_incremental_sync = inc_sync,
       }))
 
       assert.logfile(dp_errlog).has.line([[successfully loaded "response_transformer" module]], true, 10)
@@ -307,6 +310,7 @@ describe("#wasm - hybrid mode #postgres", function()
         nginx_conf            = "spec/fixtures/custom_nginx.template",
         wasm                  = "off",
         node_id               = node_id,
+        cluster_incremental_sync = inc_sync,
       }))
     end)
 
@@ -346,6 +350,7 @@ describe("#wasm - hybrid mode #postgres", function()
         wasm_filters          = "user", -- don't enable bundled filters for this test
         wasm_filters_path     = tmp_dir,
         node_id               = node_id,
+        cluster_incremental_sync = inc_sync,
       }))
     end)
 
@@ -364,3 +369,4 @@ describe("#wasm - hybrid mode #postgres", function()
     end)
   end)
 end)
+end -- for inc_sync
