a74640fa8e97c39553725dc6b6ff3cce16c6352f
chronolaw <chrono_cpp@me.com>
2024-09-27 18:16:35 +0800

    skip flaky 04-hybrid_mode_spec.lua

--- spec/03-plugins/09-key-auth/04-hybrid_mode_spec.lua
+++ spec/03-plugins/09-key-auth/04-hybrid_mode_spec.lua
@@ -1,7 +1,8 @@
 local helpers = require "spec.helpers"
 
 
-for _, inc_sync in ipairs { "on", "off"  } do
+-- XXX FIXME: inc_sync = on flaky
+for _, inc_sync in ipairs { "off" } do
 for _, strategy in helpers.each_strategy({"postgres"}) do
   describe("Plugin: key-auth (access) [#" .. strategy .. " inc_sync=" .. inc_sync .. "] auto-expiring keys", function()
     -- Give a bit of time to reduce test flakyness on slow setups
