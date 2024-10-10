ff5f3e9290042ca08edaccc3211c27da5f189747
Xiaochen Wang <wangxiaochen0@gmail.com>
2024-10-10 17:48:14 +0800

    dont return the default value

--- kong/db/dao/workspaces.lua
+++ kong/db/dao/workspaces.lua
@@ -29,7 +29,7 @@ end
 function Workspaces:select_by_name(key, options)
   if kong.configuration.database == "off" and key == "default" then
     -- it should be a table, not a single string
-    return { id = lmdb.get(DECLARATIVE_DEFAULT_WORKSPACE_KEY) or DECLARATIVE_DEFAULT_WORKSPACE_ID, }
+    return { id = lmdb.get(DECLARATIVE_DEFAULT_WORKSPACE_KEY), }
   end
 
   return self.super.select_by_name(self, key, options)
