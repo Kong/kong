e33930327215fb7780fa20024c096a32ea1e428b
Haoxuan <hhx.xxm@gmail.com>
2024-10-11 13:49:38 +0800

    feat(request-debugging): improve the time resolution and latency field (#10279)
    Using `time_ns` to measure the context duration and introduced a field
    `total_time_without_upstream` to represent total latency without upstream.
    
    KAG-4733
    
    Cherry-picked from https://github.com/Kong/kong/commit/ad758d7bd6680bdf6a98306fb8a8dfe094eed75b

--- kong/timing/context.lua
+++ kong/timing/context.lua
@@ -5,7 +5,7 @@
 -- at https://konghq.com/enterprisesoftwarelicense/.
 -- [ END OF LICENSE 0867164ffc95e54f04670b5169c09574bdbd9bba ]
 
-local cjson         = require("cjson.safe")
+local cjson         = require("cjson.safe").new()
 
 local ngx_get_phase = ngx.get_phase
 local ngx_re_gmatch = ngx.re.gmatch
@@ -15,13 +15,15 @@ local setmetatable  = setmetatable
 local table_insert  = table.insert
 local table_remove  = table.remove
 
-local get_cur_msec  = require("kong.tools.time").get_updated_monotonic_ms
+local time_ns       = require("kong.tools.time").time_ns
 
 local assert        = assert
 
 local _M            = {}
 local _MT           = { __index = _M }
 
+-- Set number precision smaller than 16 to avoid floating point errors
+cjson.encode_number_precision(14)
 
 function _M:enter_subcontext(name)
   assert(name ~= nil, "name is required")
@@ -36,14 +38,13 @@ function _M:enter_subcontext(name)
   end
 
   self.current_subcontext = self.current_subcontext.child[name]
-  self.current_subcontext.____start____ = get_cur_msec()
+  self.current_subcontext.____start____ = time_ns()
 end
 
 
 function _M:leave_subcontext(attributes)
   assert(#self.sub_context_stack > 0, "subcontext stack underflow")
-
-  local elapsed = get_cur_msec() - self.current_subcontext.____start____
+  local elapsed = (time_ns() - self.current_subcontext.____start____) / 1e6
   local old_total_time = self.current_subcontext.total_time or 0
   self.current_subcontext.total_time = old_total_time + elapsed
   self.current_subcontext.____start____ = nil
@@ -82,11 +83,37 @@ function _M:set_root_context_prop(k, v)
 end
 
 
+function _M:finalize(subcontext)
+  -- finalize total_time optionally rounding to the nearest integer
+  if subcontext.total_time then
+    -- round to 2 decimal places
+    subcontext.total_time = math_floor(subcontext.total_time * 100) / 100
+  end
+
+  if subcontext.child then
+    for _, child in pairs(subcontext.child) do
+      self:finalize(child)
+    end
+  end
+end
+
+
+function _M:get_total_time_without_upstream()
+  local total_time = 0
+  for k, child in pairs(self.root_context.child) do
+    if k ~= "upstream" and child.total_time then
+      total_time = total_time + child.total_time
+    end
+  end
+  return total_time
+end
+
+
 function _M:to_json()
   local dangling = nil
 
   -- `> 1` means we have at least one subcontext (the root context)
-  -- We always call this function at then end of the header_filter and 
+  -- We always call this function at then end of the header_filter and
   -- log phases, so we should always have at least one subcontext.
   while #self.sub_context_stack > 1 do
     self:set_context_prop("dangling", true)
@@ -99,6 +126,10 @@ function _M:to_json()
   end
 
   self:set_root_context_prop("dangling", dangling)
+  self:finalize(self.root_context)
+  if self.root_context.child ~= nil then
+    self.root_context.total_time_without_upstream = self:get_total_time_without_upstream()
+  end
   return assert(cjson.encode(self.root_context))
 end
 
