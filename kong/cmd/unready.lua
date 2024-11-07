d03b5927eb645667cfacbc251a4be50d2fd239f2
tzssangglass <tzssangglass@gmail.com>
2024-11-07 14:44:59 +0800

    fix unready in hybrid mode
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- kong/cmd/unready.lua
+++ kong/cmd/unready.lua
@@ -6,61 +6,79 @@ local conf_loader = require "kong.conf_loader"
 local pl_path = require "pl.path"
 local log = require "kong.cmd.utils.log"
 
-return {
-  execute = function(args)
-    log.disable()
+local function execute(args)
+  log.disable()
 
-    -- only to retrieve the default prefix or use given one
-    local conf = assert(conf_loader(args.conf, {
-      prefix = args.prefix
-    }))
+  -- only to retrieve the default prefix or use given one
+  local conf = assert(conf_loader(args.conf, {
+    prefix = args.prefix
+  }))
 
-    if pl_path.exists(conf.kong_env) then
-      -- load <PREFIX>/kong.conf containing running node's config
-      conf = assert(conf_loader(conf.kong_env))
-    end
-
-    log.enable()
-
-    if #conf.status_listeners == 0 then
-      print("No status listeners found in configuration.")
-      return
-    end
-
-    local status_listener = conf.status_listeners[1]
-
-    local scheme = "http"
-    if status_listener.ssl then
-      scheme = "https"
-    end
-
-    local url = scheme .. "://" .. status_listener.ip .. ":" .. status_listener.port .. "/status/unready"
-
-    local httpc = http.new()
-    httpc:set_timeout(1000)
-
-    local res, err = httpc:request_uri(url, {
-      method = "POST",
-      headers = {
-          ["Content-Type"] = "application/json"
-      },
-      body = "{}",
-      -- we don't need to verify the SSL certificate for this request
-      ssl_verify = false,
-    })
-
-    httpc:close()
-
-    if not res then
-      print(fmt("Failed to send request to %s: %s", url, err))
-      return
-    end
-
-    if res.status ~= 204 then
-      print(fmt("Unexpected response status from %s: %d", url, res.status))
-      return
-    end
-
-    print("Kong's status successfully changed to 'unready'")
+  if pl_path.exists(conf.kong_env) then
+    -- load <PREFIX>/kong.conf containing running node's config
+    conf = assert(conf_loader(conf.kong_env))
   end
+
+  log.enable()
+
+  if #conf.status_listeners == 0 then
+    print("No status listeners found in configuration.")
+    return
+  end
+
+  local status_listener = conf.status_listeners[1]
+
+  local scheme = "http"
+  if status_listener.ssl then
+    scheme = "https"
+  end
+
+  local url = scheme .. "://" .. status_listener.ip .. ":" .. status_listener.port .. "/status/unready"
+
+  local httpc = http.new()
+  httpc:set_timeout(1000)
+
+  local res, err = httpc:request_uri(url, {
+    method = "POST",
+    headers = {
+        ["Content-Type"] = "application/json"
+    },
+    body = "{}",
+    -- we don't need to verify the SSL certificate for this request
+    ssl_verify = false,
+  })
+
+  httpc:close()
+
+  if not res then
+    print(fmt("Failed to send request to %s: %s", url, err))
+    return
+  end
+
+  if res.status ~= 204 then
+    print(fmt("Unexpected response status from %s: %d", url, res.status))
+    return
+  end
+
+  print("Kong's status successfully changed to 'unready'")
+end
+
+
+local lapp = [[
+Usage: kong unready [OPTIONS]
+
+Make status listeners(`/status/ready`) return 503 Service Unavailable.
+
+Example usage:
+ kong unready
+
+Options:
+ -c,--conf    (optional string)  configuration file
+ -p,--prefix  (optional string)  override prefix directory
+]]
+
+
+return {
+  lapp = lapp,
+  execute = execute,
 }
--- spec/02-integration/02-cmd/17-unready_spec.lua
+++ spec/02-integration/02-cmd/17-unready_spec.lua
@@ -1,6 +1,7 @@
 local helpers = require "spec.helpers"
 local http = require "resty.http"
 
+local cp_status_port = helpers.get_available_port()
 local dp_status_port = 8100
 
 local function get_status_no_ssl_verify()
@@ -31,18 +32,172 @@ local function get_status_no_ssl_verify()
 end
 
 for _, strategy in helpers.each_strategy() do
-  describe("kong unready with #" .. strategy .. " backend", function()
+  if strategy ~= "off" then
+    -- skip the "off" strategy, as dbless has its own test suite
+    describe("kong unready with #" .. strategy .. " backend", function()
+      lazy_setup(function()
+        assert(helpers.start_kong({
+          database = strategy,
+          nginx_conf = "spec/fixtures/custom_nginx.template",
+          status_listen = "127.0.0.1:8100",
+          nginx_main_worker_processes = 8,
+        }))
+      end)
+
+      lazy_teardown(function()
+        helpers.stop_kong()
+      end)
+
+      it("should set Kong to 'unready'", function()
+        helpers.wait_until(function()
+          local http_client = helpers.http_client('127.0.0.1', dp_status_port)
+
+          local res = http_client:send({
+            method = "GET",
+            path = "/status/ready",
+          })
+
+          local status = res and res.status
+          http_client:close()
+          if status == 200 then
+            return true
+          end
+        end, 10)
+
+        local ok, err, msg = helpers.kong_exec("unready", {
+          prefix = helpers.test_conf.prefix,
+        })
+        assert.equal("", err)
+        assert.equal("Kong's status successfully changed to 'unready'\n", msg)
+        assert.equal(true, ok)
+
+        helpers.wait_until(function()
+          local http_client = helpers.http_client('127.0.0.1', dp_status_port)
+
+          local res = http_client:send({
+            method = "GET",
+            path = "/status/ready",
+          })
+
+          local status = res and res.status
+          http_client:close()
+          if status == 503 then
+            return true
+          end
+        end, 10)
+      end)
+
+    end)
+
+    describe("Kong without a status listener", function()
+      lazy_setup(function()
+        helpers.get_db_utils(strategy, {}) -- runs migrations
+
+        assert(helpers.start_kong({
+          database = strategy,
+          nginx_conf = "spec/fixtures/custom_nginx.template",
+        }))
+      end)
+
+      lazy_teardown(function()
+        helpers.stop_kong()
+      end)
+
+      it("should return an error when trying to set 'unready' without a status listener", function()
+        local ok, err, msg = helpers.kong_exec("unready", {
+          prefix = helpers.test_conf.prefix,
+        })
+        assert.equal("", err)
+        assert.equal("No status listeners found in configuration.\n", msg)
+        assert.equal(true, ok)
+      end)
+
+    end)
+
+    describe("Kong with SSL-enabled status listener", function()
+      lazy_setup(function()
+        helpers.get_db_utils(strategy, {}) -- runs migrations
+
+        assert(helpers.start_kong({
+          database = strategy,
+          nginx_conf = "spec/fixtures/custom_nginx.template",
+          status_listen = "127.0.0.1:8100 ssl",
+          nginx_main_worker_processes = 8,
+        }))
+      end)
+
+      lazy_teardown(function()
+        helpers.stop_kong()
+      end)
+
+      it("should set Kong to 'unready' with SSL-enabled status listener", function()
+        helpers.wait_until(function()
+          local status = get_status_no_ssl_verify()
+          if status == 200 then
+            return true
+          end
+        end, 10)
+
+        local ok, err, msg = helpers.kong_exec("unready", {
+          prefix = helpers.test_conf.prefix,
+        })
+        assert.equal("", err)
+        assert.equal("Kong's status successfully changed to 'unready'\n", msg)
+        assert.equal(true, ok)
+
+        helpers.wait_until(function()
+          local status = get_status_no_ssl_verify()
+          if status == 503 then
+            return true
+          end
+        end, 10)
+      end)
+    end)
+  end
+end
+
+for _, strategy in helpers.each_strategy() do
+  describe("kong unready in hybrid mode", function()
+    local bp = helpers.get_db_utils(strategy, {
+      "services",
+    })
+
+    -- insert some data to make sure the control plane is ready and send the configuration to dp
+    -- so that `current_hash` of dp wouldn't be DECLARATIVE_EMPTY_CONFIG_HASH, so that dp would be ready
+    assert(bp.services:insert {
+      name = "example",
+    })
+
     lazy_setup(function()
       assert(helpers.start_kong({
+        role = "data_plane",
+        database = "off",
+        cluster_cert = "spec/fixtures/kong_clustering.crt",
+        cluster_cert_key = "spec/fixtures/kong_clustering.key",
+        cluster_control_plane = "127.0.0.1:9005",
+        proxy_listen = "127.0.0.1:9002",
+        nginx_worker_processes = 8,
+        status_listen = "127.0.0.1:" .. dp_status_port,
+        prefix = "serve_dp",
+        log_level = "info",
+      }))
+
+      assert(helpers.start_kong({
+        role = "control_plane",
+        cluster_cert = "spec/fixtures/kong_clustering.crt",
+        cluster_cert_key = "spec/fixtures/kong_clustering.key",
         database = strategy,
+        prefix = "serve_cp",
+        cluster_listen = "127.0.0.1:9005",
         nginx_conf = "spec/fixtures/custom_nginx.template",
-        status_listen = "127.0.0.1:8100",
-        nginx_main_worker_processes = 8,
+
+        status_listen = "127.0.0.1:" .. cp_status_port
       }))
     end)
 
     lazy_teardown(function()
-      helpers.stop_kong()
+      helpers.stop_kong("serve_dp")
+      helpers.stop_kong("serve_cp")
     end)
 
     it("should set Kong to 'unready'", function()
@@ -61,7 +216,8 @@ for _, strategy in helpers.each_strategy() do
         end
       end, 10)
 
-      local ok, err, msg = helpers.kong_exec("unready", {
+      -- set dp to unready
+      local ok, err, msg = helpers.kong_exec("unready --prefix serve_dp", {
         prefix = helpers.test_conf.prefix,
       })
       assert.equal("", err)
@@ -82,60 +238,9 @@ for _, strategy in helpers.each_strategy() do
           return true
         end
       end, 10)
-    end)
 
-  end)
-
-  describe("Kong without a status listener", function()
-    lazy_setup(function()
-      assert(helpers.start_kong({
-        database = strategy,
-        nginx_conf = "spec/fixtures/custom_nginx.template",
-      }))
-    end)
-
-    lazy_teardown(function()
-      helpers.stop_kong()
-    end)
-
-    it("should return an error when trying to set 'unready' without a status listener", function()
-      local ok, err, msg = helpers.kong_exec("unready", {
-        prefix = helpers.test_conf.prefix,
-      })
-      assert.equal("", err)
-      assert.equal("No status listeners found in configuration.\n", msg)
-      assert.equal(true, ok)
-    end)
-
-  end)
-
-  describe("Kong with SSL-enabled status listener", function()
-    lazy_setup(function()
-      assert(helpers.start_kong({
-        database = strategy,
-        nginx_conf = "spec/fixtures/custom_nginx.template",
-        status_listen = "127.0.0.1:8100 ssl",
-        nginx_main_worker_processes = 8,
-      }))
-    end)
-
-    lazy_teardown(function()
-      helpers.stop_kong()
-    end)
-
-    it("should set Kong to 'unready' with SSL-enabled status listener", function()
-      helpers.wait_until(function()
-        local status, err = get_status_no_ssl_verify()
-        if status == 200 then
-          return true
-        end
-      end, 10)
-
-      local status, err = get_status_no_ssl_verify()
-      assert.equal(200, status)
-      assert.is_nil(err)
-
-      local ok, err, msg = helpers.kong_exec("unready", {
+      -- set cp to unready
+      local ok, err, msg = helpers.kong_exec("unready --prefix serve_cp", {
         prefix = helpers.test_conf.prefix,
       })
       assert.equal("", err)
@@ -143,7 +248,15 @@ for _, strategy in helpers.each_strategy() do
       assert.equal(true, ok)
 
       helpers.wait_until(function()
-        local status = get_status_no_ssl_verify()
+        local http_client = helpers.http_client('127.0.0.1', cp_status_port)
+
+        local res = http_client:send({
+          method = "GET",
+          path = "/status/ready",
+        })
+
+        local status = res and res.status
+        http_client:close()
         if status == 503 then
           return true
         end
