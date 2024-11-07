e3b5220b1123e78b56fd3a84604d2c78cfa2d504
Jun Ouyang <ouyangjun1999@gmail.com>
2024-11-08 04:30:39 +0800

    test(ai-proxy): fix red test due to other feature break http protocol behavior (#13848)

--- spec/03-plugins/38-ai-proxy/05-azure_integration_spec.lua
+++ spec/03-plugins/38-ai-proxy/05-azure_integration_spec.lua
@@ -5,9 +5,8 @@ local pl_file = require "pl.file"
 local PLUGIN_NAME = "ai-proxy"
 local MOCK_PORT = helpers.get_available_port()
 
-for _, client_protocol in ipairs({ "http", "https", "http2" }) do
 for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
-  describe(PLUGIN_NAME .. ": (access) [#" .. strategy .. "] [#" .. client_protocol .. "]", function()
+  describe(PLUGIN_NAME .. ": (access) [#" .. strategy  .. "]", function()
     local client
 
     lazy_setup(function()
@@ -408,13 +407,7 @@ for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
     end)
 
     before_each(function()
-      if client_protocol == "http" then
-        client = helpers.proxy_client()
-      elseif client_protocol == "https" then
-        client = helpers.proxy_ssl_client()
-      elseif client_protocol == "http2" then
-        client = helpers.proxy_ssl_client(nil, nil, 2)
-      end
+      client = helpers.proxy_client()
     end)
 
     after_each(function()
@@ -657,4 +650,3 @@ for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
   end)
 
 end end
-end -- for _, client_protocol
--- spec/03-plugins/38-ai-proxy/07-llama2_integration_spec.lua
+++ spec/03-plugins/38-ai-proxy/07-llama2_integration_spec.lua
@@ -5,10 +5,9 @@ local pl_file = require "pl.file"
 local PLUGIN_NAME = "ai-proxy"
 local MOCK_PORT = helpers.get_available_port()
 
-for _, client_protocol in ipairs({ "http", "https", "http2" }) do
 
 for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
-  describe(PLUGIN_NAME .. ": (access) [#" .. strategy .. "] [#" .. client_protocol .. "]", function()
+  describe(PLUGIN_NAME .. ": (access) [#" .. strategy  .. "]", function()
     local client
 
     lazy_setup(function()
@@ -193,13 +192,7 @@ for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
     end)
 
     before_each(function()
-      if client_protocol == "http" then
-        client = helpers.proxy_client()
-      elseif client_protocol == "https" then
-        client = helpers.proxy_ssl_client()
-      elseif client_protocol == "http2" then
-        client = helpers.proxy_ssl_client(nil, nil, 2)
-      end
+      client = helpers.proxy_client()
     end)
 
     after_each(function()
@@ -451,4 +444,3 @@ for _, strategy in helpers.all_strategies() do if strategy ~= "cassandra" then
   end)
 
 end end
-end -- for _, client_protocol
