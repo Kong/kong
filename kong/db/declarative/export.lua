f6580d5fd317b6323f2db5f263c2410d8263ddc6
chronolaw <chrono_cpp@me.com>
2024-11-11 17:42:18 +0800

    get_latest_version_real

--- kong/db/declarative/export.lua
+++ kong/db/declarative/export.lua
@@ -95,6 +95,23 @@ local function end_transaction(db)
 end
 
 
+local get_latest_version
+do
+  local strategy
+
+  local function get_latest_version_real()
+    return strategy:get_latest_version()
+  end
+
+  get_latest_version = function()
+    -- ensure we get the initialized kong.db
+    strategy = require("kong.clustering.services.sync.strategies.postgres").new(kong.db)
+    get_latest_version = get_latest_version_real
+    return get_latest_version()
+  end
+end
+
+
 local function export_from_db_impl(emitter, skip_ws, skip_disabled_entities, expand_foreigns)
   local schemas = {}
 
@@ -119,9 +136,7 @@ local function export_from_db_impl(emitter, skip_ws, skip_disabled_entities, exp
 
   local sync_version
   if emitter.want_sync_version then
-    local strategy = require("kong.clustering.services.sync.strategies.postgres").new(db)
-
-    sync_version = strategy:get_latest_version()
+    sync_version = get_latest_version()
   end
 
   emitter:emit_toplevel({
