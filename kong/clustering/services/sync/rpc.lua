6da33e31bafdc5181fc5ef44fc3d604ee91ae4da
chronolaw <chrono_cpp@me.com>
2024-11-16 08:43:47 +0800

    revert rpc call change

--- kong/clustering/services/sync/rpc.lua
+++ kong/clustering/services/sync/rpc.lua
@@ -62,7 +62,7 @@ function _M:init_cp(manager)
   -- CP
   -- Method: kong.sync.v2.get_delta
   -- Params: versions: list of current versions of the database
-  -- example: { default = { version = 1000 }, node = { version = "3.9.0" } }
+  -- example: { default = { version = 1000, } }
   manager.callbacks:register("kong.sync.v2.get_delta", function(node_id, current_versions)
     ngx_log(ngx_DEBUG, "[kong.sync.v2] config push (connected client)")
 
@@ -85,7 +85,7 @@ function _M:init_cp(manager)
       last_seen = ngx.time(),
       hostname = node_id,
       ip = kong.rpc:get_peer_ip(node_id),   -- try to get the correct ip
-      version = current_versions.node.version,    -- get from rpc call
+      version = "3.9.0",    -- TODO: get from rpc call
       sync_status = CLUSTERING_SYNC_STATUS.NORMAL,
       config_hash = fmt("%032d", default_namespace_version),
       rpc_capabilities = rpc_peers and rpc_peers[node_id] or {},
@@ -206,10 +206,6 @@ local function do_sync()
                  { version =
                    tonumber(declarative.get_current_hash()) or 0,
                  },
-                node =
-                 {
-                   version = kong.version, -- cp need this info
-                 }
                }
 
   local ns_deltas, err = kong.rpc:call("control_plane", "kong.sync.v2.get_delta", msg)
