102fd1b7d12ac44ee56547678b81170506060105
chronolaw <chrono_cpp@me.com>
2024-11-18 13:02:37 +0800

    change lmdb version value format

--- kong/clustering/services/sync/rpc.lua
+++ kong/clustering/services/sync/rpc.lua
@@ -56,6 +56,23 @@ local function full_sync_result()
 end
 
 
+-- version has a perfix "XX", like "XX000001"
+local function format_lmdb_version(ver)
+  return fmt("XX%030d", ver)
+end
+
+
+local function get_lmdb_version()
+  local str = declarative.get_current_hash()
+  if not str then
+    return 0
+  end
+
+  -- remove prefix "XX"
+  return tonumber(str:sub(3))
+end
+
+
 function _M:init_cp(manager)
   local purge_delay = manager.conf.cluster_data_plane_purge_delay
 
@@ -87,7 +104,8 @@ function _M:init_cp(manager)
       ip = kong.rpc:get_peer_ip(node_id),   -- try to get the correct ip
       version = "3.8.0.0",    -- XXX TODO: get from rpc call
       sync_status = CLUSTERING_SYNC_STATUS.NORMAL,
-      config_hash = fmt("%032d", default_namespace_version),
+      --config_hash = fmt("%032d", default_namespace_version),
+      config_hash = format_lmdb_version(default_namespace_version),
       rpc_capabilities = rpc_peers and rpc_peers[node_id] or {},
     }, { ttl = purge_delay, no_broadcast_crud_event = true, })
     if not ok then
@@ -162,7 +180,8 @@ function _M:init_dp(manager)
       return nil, "'new_version' key does not exist"
     end
 
-    local lmdb_ver = tonumber(declarative.get_current_hash()) or 0
+    --local lmdb_ver = tonumber(declarative.get_current_hash()) or 0
+    local lmdb_ver = get_lmdb_version()
     if lmdb_ver < version then
       return self:sync_once()
     end
@@ -184,8 +203,7 @@ end
 local function do_sync()
   local ns_deltas, err = kong.rpc:call("control_plane", "kong.sync.v2.get_delta",
                                        { default =
-                                         { version =
-                                           tonumber(declarative.get_current_hash()) or 0,
+                                         { version = get_lmdb_version(),
                                          },
                                        })
   if not ns_deltas then
@@ -321,7 +339,8 @@ local function do_sync()
   end -- for _, delta
 
   -- store current sync version
-  t:set(DECLARATIVE_HASH_KEY, fmt("%032d", version))
+  --t:set(DECLARATIVE_HASH_KEY, fmt("%032d", version))
+  t:set(DECLARATIVE_HASH_KEY, format_lmdb_version(version))
 
   -- store the correct default workspace uuid
   if default_ws_changed then
