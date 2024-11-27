ae420c601bb14f6a01d76f3ebc473b58829f70f5
chronolaw <chrono_cpp@me.com>
2024-11-27 11:29:29 +0800

    test page()

--- spec/02-integration/11-dbless/04-pagination_spec.lua
+++ spec/02-integration/11-dbless/04-pagination_spec.lua
@@ -22,8 +22,12 @@ plugins:
           return function(conf)
             local db = kong.db
 
+            -- check max_page_size
+
             assert(db.routes.pagination.max_page_size == 2048)
             assert(db.services.pagination.max_page_size == 2048)
+
+            -- check each()
 
             local r, err = db.routes:each(1000)
             assert(r and not err)
@@ -37,11 +41,27 @@ plugins:
             local r, err = db.routes:each(2049)
             assert(not r)
             assert(err == "[off] size must be an integer between 1 and 2048")
+
+            -- check page()
+
+            local entities, err = db.routes:page(1000)
+            assert(#entities == 1000 and not err)
+
+            local entities, err = db.routes:page(2047)
+            assert(#entities == 2047 and not err)
+
+            local entities, err = db.routes:page(2048)
+            assert(#entities == 2048 and not err)
+            ngx.log(ngx.INFO, "xxx #entities", #entities)
+
+            local entities, err = db.routes:page(2049)
+            assert(not entities)
+            assert(err == "[off] size must be an integer between 1 and 2048")
           end
 ]]
 
 
-local COUNT = 1001
+local COUNT = 3000
 
 
 describe("dbless pagination #off", function()
