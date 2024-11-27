35aa60546de8d313c792f32afcb3ec71fd179520
Zhefeng Chen <chzf_zju@163.com>
2024-11-27 13:12:03 +0800

    fix(plugins): Improved the error message when an anonymous consumer               was configured but did not exist
    https://konghq.atlassian.net/browse/FTI-5392

--- spec/03-plugins/09-key-auth/02-access_spec.lua
+++ spec/03-plugins/09-key-auth/02-access_spec.lua
@@ -9,6 +9,7 @@ for _, strategy in helpers.each_strategy() do
   describe("Plugin: key-auth (access) [#" .. strategy .. "]", function()
     local mock, proxy_client
     local kong_cred
+    local nonexisting_anonymous = uuid.uuid()  -- a nonexisting consumer id
 
     lazy_setup(function()
       mock = http_mock.new(MOCK_PORT)
@@ -117,7 +118,7 @@ for _, strategy in helpers.each_strategy() do
         name     = "key-auth",
         route = { id = route4.id },
         config   = {
-          anonymous = uuid.uuid(),  -- unknown consumer
+          anonymous = nonexisting_anonymous,  -- a nonexisting consumer id
         },
       }
 
@@ -803,7 +804,8 @@ for _, strategy in helpers.each_strategy() do
             ["Host"] = "key-auth4.test"
           }
         })
-        assert.response(res).has.status(500)
+        local body = cjson.decode(assert.res_status(500, res))
+        assert.same("anonymous consumer " .. nonexisting_anonymous .. " is configured but doesn't exist", body.message)
       end)
     end)
   end)
