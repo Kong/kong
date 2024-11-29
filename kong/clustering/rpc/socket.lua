e8749e0e3b4ad6311b346acba2be6bc469b30c4c
chronolaw <chrono_cpp@me.com>
2024-11-29 14:18:43 +0800

    notify new version

--- kong/clustering/rpc/socket.lua
+++ kong/clustering/rpc/socket.lua
@@ -169,7 +169,7 @@ function _M:start()
 
         -- call dispatch
         local res, err = kong.timer:named_at(string_format("JSON-RPC callback for node_id: %s, id: %d, method: %s",
-                                                           self.node_id, payload.id, payload.method),
+                                                           self.node_id, payload.id or 0, payload.method),
                                                            0, _M._dispatch, self, dispatch_cb, payload)
         if not res and payload.id then
           local reso, erro = self.outgoing:push(new_error(payload.id, jsonrpc.INTERNAL_ERROR))
