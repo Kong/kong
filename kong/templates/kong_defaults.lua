f345fb5ad9a23cfc065a1ac06fc77d8306d603a9
Chrono <chrono_cpp@me.com>
2024-12-02 14:02:20 +0800

    feat(clustering): full sync threshold should be configureable (#13893)

--- kong/templates/kong_defaults.lua
+++ kong/templates/kong_defaults.lua
@@ -43,6 +43,7 @@ cluster_use_proxy = off
 cluster_dp_labels = NONE
 cluster_rpc = off
 cluster_incremental_sync = off
+cluster_full_sync_threshold = 512
 cluster_cjson = off
 
 lmdb_environment_path = dbless.lmdb
