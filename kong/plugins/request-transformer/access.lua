083ebab90c9b47c8717d3c7f1a5cfde1b319be0d
ProBrian <chenjingbo35@gmail.com>
2024-12-04 11:28:24 +0800

    replace no ctx request.get_header in request-transformer by ngx.var.http_content_type

--- kong/plugins/request-transformer/access.lua
+++ kong/plugins/request-transformer/access.lua
@@ -8,7 +8,7 @@ local table_insert = table.insert
 local get_uri_args = kong.request.get_query
 local set_uri_args = kong.service.request.set_query
 local clear_header = kong.service.request.clear_header
-local get_header = kong.request.get_header
+-- local get_header = kong.request.get_header
 local set_header = kong.service.request.set_header
 local get_headers = kong.request.get_headers
 local set_headers = kong.service.request.set_headers
@@ -422,7 +422,9 @@ local function transform_multipart_body(conf, body, content_length, content_type
 end
 
 local function transform_body(conf, template_env)
-  local content_type_value = get_header(CONTENT_TYPE)
+  -- local content_type_value = get_header(CONTENT_TYPE)
+  -- content-type is a builtin single value header, use var to fetch is faster.
+  local content_type_value = ngx.var.http_content_type
   local content_type = get_content_type(content_type_value)
   if content_type == nil or #conf.rename.body < 1 and
      #conf.remove.body < 1 and #conf.replace.body < 1 and
@@ -456,7 +458,9 @@ local function transform_method(conf)
   if conf.http_method then
     set_method(conf.http_method:upper())
     if conf.http_method == "GET" or conf.http_method == "HEAD" or conf.http_method == "TRACE" then
-      local content_type_value = get_header(CONTENT_TYPE)
+      -- local content_type_value = get_header(CONTENT_TYPE)
+      -- content-type is a builtin single value header, use var to fetch is faster.
+      local content_type_value = ngx.var.http_content_type
       local content_type = get_content_type(content_type_value)
       if content_type == ENCODED then
         -- Also put the body into querystring
