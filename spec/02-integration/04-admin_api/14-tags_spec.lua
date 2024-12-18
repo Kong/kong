fff7b02af3fbc071c5ca823c72c92947a0742703
Xiaochen Wang <wangxiaochen0@gmail.com>
2024-12-18 14:57:10 +0800

    restore 14-tags_spec.lua (2)

--- spec/02-integration/04-admin_api/14-tags_spec.lua
+++ spec/02-integration/04-admin_api/14-tags_spec.lua
@@ -6,7 +6,7 @@ local cjson = require "cjson"
 -- This test we test on the correctness of the admin API response so that
 -- we can ensure the right function (page()) is executed.
 describe("Admin API - tags", function()
-   for _, strategy in helpers.all_strategies() do
+  for _, strategy in helpers.all_strategies() do
     describe("/entities?tags= with DB: #" .. strategy, function()
       local client, bp
 
