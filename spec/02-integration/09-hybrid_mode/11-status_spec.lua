a38018863901e9408ba7b6bcd63a9d0f108eefeb
lhanjian <lhjay1@gmail.com>
2024-12-19 17:54:54 +0800

    update

--- spec/02-integration/09-hybrid_mode/11-status_spec.lua
+++ spec/02-integration/09-hybrid_mode/11-status_spec.lua
@@ -74,9 +74,6 @@ for _, strategy in helpers.each_strategy() do
     end)
 
     describe("dp status ready endpoint for no config", function()
-      -- XXX FIXME
-      local skip_rpc_sync = rpc_sync == "on" and pending or it
-
       lazy_setup(function()
         assert(start_kong_cp())
         assert(start_kong_dp())
@@ -108,7 +105,7 @@ for _, strategy in helpers.each_strategy() do
 
       -- now dp receive config from cp, so dp should be ready
 
-      if rpc_sync == "off" then
+      if rpc == "on" and rpc_sync == "off" then
         it("should return 200 on data plane after configuring", function()
           helpers.wait_until(function()
             local http_client = helpers.http_client('127.0.0.1', dp_status_port)
