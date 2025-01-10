effa63ed92209991af6a3a7ff5fd0bd24db3a7b1
chronolaw <chrono_cpp@me.com>
2025-01-10 11:39:58 +0800

    Revert "clean prefix"
    This reverts commit 60ebe4e4dc1be48c6a5d3496d922f0f1fcb47239.

--- spec/02-integration/18-hybrid_rpc/06-batch-rpc_spec.lua
+++ spec/02-integration/18-hybrid_rpc/06-batch-rpc_spec.lua
@@ -36,8 +36,8 @@ for _, strategy in helpers.each_strategy() do
     end)
 
     lazy_teardown(function()
-      helpers.clean_prefix("servroot2")
-      helpers.clean_prefix()
+      helpers.stop_kong("servroot2")
+      helpers.stop_kong()
     end)
 
     describe("batch works", function()
@@ -82,10 +82,6 @@ for _, strategy in helpers.each_strategy() do
             return true
           end
         end, 10)
-
-        -- try to flush log files
-        helpers.stop_kong(nil, true)
-        helpers.stop_kong("servroot2", true)
 
         helpers.pwait_until(function()
           assert.logfile().has.line(
