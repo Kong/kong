a37a09e4903f6caf132958967badbb243022db94
Jack Tysoe <91137069+tysoekong@users.noreply.github.com>
2025-03-11 14:40:06 +0000

    fix(ai): do not replace chunked response with native formats
    Streaming was still coming back in OpenAI format for the new "native" bedrock and gemini llm_format config settings.
    
    AG-233

--- kong/llm/adapters/bedrock.lua
+++ kong/llm/adapters/bedrock.lua
@@ -229,6 +229,7 @@ function _BedrockAdapter:extract_model_and_stream(path, uri_captures)
 
   -- otherwise try raw parsing the path,
   --  in case the user has set this up incorrectly
+  -- TODO: also consider upstream_url?
   if (not model_name) or (not operation) then
     local t_model_name, t_operation = path:match("/model/([^/]+)/([^/]+)$")
 
