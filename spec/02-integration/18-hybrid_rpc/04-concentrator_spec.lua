aacc44de34debe86f1db97cb187ea5d2b967aa98
Chrono <chrono_cpp@me.com>
2025-05-20 09:35:11 +0800

    tests(clustering/rpc): add test cases for rpc concentrator (#14538)
    KAG-5749

--- spec/02-integration/18-hybrid_rpc/04-concentrator_spec.lua
+++ spec/02-integration/18-hybrid_rpc/04-concentrator_spec.lua
@@ -1,30 +1,44 @@
 local helpers = require "spec.helpers"
-local cjson = require("cjson.safe")
 
 
--- keep it for future usage
-local function obtain_dp_node_id()  -- luacheck: ignore
-  local dp_node_id
+local function start_cp(strategy, prefix,
+                        cluster_listen_port, admin_listen_port)
+  assert(helpers.start_kong({
+    role = "control_plane",
+    database = strategy,
+    prefix = prefix,
+    cluster_cert = "spec/fixtures/kong_clustering.crt",
+    cluster_cert_key = "spec/fixtures/kong_clustering.key",
+    cluster_listen = "127.0.0.1:" .. cluster_listen_port,
+    admin_listen = "127.0.0.1:" .. admin_listen_port,
+    nginx_conf = "spec/fixtures/custom_nginx.template",
 
-  helpers.wait_until(function()
-    local admin_client = helpers.admin_client()
-    finally(function()
-      admin_client:close()
-    end)
+    nginx_worker_processes = 1,
+    log_level = "debug",
+    cluster_rpc = "on",
+    plugins = "bundled,rpc-concentrator-test",
+    cluster_rpc_sync = "off",
+  }))
+end
 
-    local res = assert(admin_client:get("/clustering/data-planes"))
-    local body = assert.res_status(200, res)
-    local json = cjson.decode(body)
 
-    for _, v in pairs(json.data) do
-      if v.ip == "127.0.0.1" and ngx.time() - v.last_seen < 3 then
-        dp_node_id = v.id
-        return true
-      end
-    end
-  end, 10)
+local function start_dp(prefix, cluster_listen_port)
+  assert(helpers.start_kong({
+    role = "data_plane",
+    database = "off",
+    prefix = prefix,
+    cluster_cert = "spec/fixtures/kong_clustering.crt",
+    cluster_cert_key = "spec/fixtures/kong_clustering.key",
+    cluster_control_plane = "127.0.0.1:" .. cluster_listen_port,
+    proxy_listen = "0.0.0.0:9002",
+    nginx_conf = "spec/fixtures/custom_nginx.template",
 
-  return dp_node_id
+    nginx_worker_processes = 4,
+    log_level = "debug",
+    cluster_rpc = "on",
+    plugins = "bundled,rpc-concentrator-test",
+    cluster_rpc_sync = "off",
+  }))
 end
 
 
@@ -37,55 +51,58 @@ for _, strategy in helpers.each_strategy() do
         "clustering_data_planes",
       }) -- runs migrations
 
-      assert(helpers.start_kong({
-        role = "control_plane",
-        cluster_cert = "spec/fixtures/kong_clustering.crt",
-        cluster_cert_key = "spec/fixtures/kong_clustering.key",
-        database = strategy,
-        cluster_listen = "127.0.0.1:9005",
-        admin_listen = "127.0.0.1:" .. helpers.get_available_port(),
-        nginx_conf = "spec/fixtures/custom_nginx.template",
-        cluster_rpc = "on",
-        plugins = "bundled,rpc-hello-test",
-        cluster_rpc_sync = "off",
-      }))
+      -- 9005 is default cluster_listen_port
+      -- 9001 is default admin_listen_port
+      start_cp(strategy, "cp1",
+               9005, 9001)
 
-      assert(helpers.start_kong({
-        role = "control_plane",
-        cluster_cert = "spec/fixtures/kong_clustering.crt",
-        prefix = "servroot3",
-        cluster_cert_key = "spec/fixtures/kong_clustering.key",
-        database = strategy,
-        cluster_listen = "127.0.0.1:" .. helpers.get_available_port(),
-        nginx_conf = "spec/fixtures/custom_nginx.template",
-        cluster_rpc = "on",
-        plugins = "bundled,rpc-hello-test",
-        cluster_rpc_sync = "off",
-      }))
-
-      assert(helpers.start_kong({
-        role = "data_plane",
-        database = "off",
-        prefix = "servroot2",
-        cluster_cert = "spec/fixtures/kong_clustering.crt",
-        cluster_cert_key = "spec/fixtures/kong_clustering.key",
-        cluster_control_plane = "127.0.0.1:9005",
-        proxy_listen = "0.0.0.0:9002",
-        nginx_conf = "spec/fixtures/custom_nginx.template",
-        cluster_rpc = "on",
-        plugins = "bundled,rpc-hello-test",
-        cluster_rpc_sync = "off",
-      }))
+      start_dp("dp", 9005)
     end)
 
     lazy_teardown(function()
-      helpers.stop_kong("servroot2")
-      helpers.stop_kong("servroot3")
-      helpers.stop_kong()
+      helpers.stop_kong("dp")
+      helpers.stop_kong("cp1")
     end)
 
-    -- TODO: test with other rpc
-    --describe("XXX over RPC", function()
-    --end)
+    it("works well", function()
+      local dp_logfile = "dp/logs/error.log"
+
+      -- wait for rpc framework is ready
+      assert.logfile(dp_logfile).has.line(
+        "[kong.test.concentrator] rpc framework is ready.", true, 5)
+
+      -- start a new cp node then call rpc via concentrator
+      start_cp(strategy, "cp2",
+               helpers.get_available_port(),
+               helpers.get_available_port())
+
+      -- check dp's log
+      assert.logfile(dp_logfile).has.line(
+        "kong.test.concentrator: hello", true, 5)
+      assert.logfile(dp_logfile).has.no.line(
+        "[error]", true, 0)
+
+      -- check cp's log
+
+      local cp_logfile = "cp1/logs/error.log"
+
+      assert.logfile(cp_logfile).has.line(
+        "concentrator got 1 calls from database for node", true, 5)
+      assert.logfile(cp_logfile).has.no.line(
+        "assertion failed", true, 0)
+
+      local cp_logfile = "cp2/logs/error.log"
+
+      assert.logfile(cp_logfile).has.line(
+        "[kong.test.concentrator] node_id: ", true, 5)
+      assert.logfile(cp_logfile).has.line(
+        "via concentrator", true, 5)
+      assert.logfile(cp_logfile).has.line(
+        "[rpc] kong.test.concentrator succeeded", true, 5)
+      assert.logfile(cp_logfile).has.no.line(
+        "via local", true, 0)
+      assert.logfile(cp_logfile).has.no.line(
+        "assertion failed", true, 0)
+    end)
   end)
 end -- for _, strategy
