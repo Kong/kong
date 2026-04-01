984ead9da830633cb2a6cc758df6d007b3e05ea1
hanjian <hanjian.liu@konghq.com>
2026-04-01 18:05:05 +0800

    tests(vault): remove external dependency on mockbin.org (#17194)
    (cherry picked from commit e6eb39ebeee8006be30db44f471ab99ac0cb72a0)

--- spec/fixtures/custom_vaults/kong/vaults/mock/init.lua
+++ spec/fixtures/custom_vaults/kong/vaults/mock/init.lua
@@ -1,8 +1,6 @@
 local env = require "kong.vaults.env"
-local http = require "resty.http"
 
 
-local assert = assert
 local getenv = os.getenv
 
 
@@ -14,18 +12,6 @@ end
 
 
 local function get(conf, resource, version)
-  local client, err = http.new()
-  if not client then
-    return nil, err
-  end
-
-  client:set_timeouts(20000, 20000, 20000)
-  assert(client:request_uri("http://mockbin.org/headers", {
-    headers = {
-      Accept = "application/json",
-    },
-  }))
-
   return env.get(conf, resource, version)
 end
 
