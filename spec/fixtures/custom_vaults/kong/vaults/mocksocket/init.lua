65ac15f85ae4bd0645a6074e0dc276dac5b8af3b
Zachary Hu <zachary.hu@konghq.com>
2026-05-25 13:33:12 +0800

    tests(vault): match no request found

--- spec/fixtures/custom_vaults/kong/vaults/mocksocket/init.lua
+++ spec/fixtures/custom_vaults/kong/vaults/mocksocket/init.lua
@@ -21,7 +21,7 @@ local function get(conf, resource, version)
     httpc:set_timeout(50)
     httpc:request_uri("http://127.0.0.1:" .. test.PORT .. "/secret/dummy")
   end)
-  if not pok and fok and not fok:find("API disabled in", nil, true) then
+  if not pok and fok and not (fok:find("API disabled in", nil, true) or fok:find("no request found", nil, true)) then
     return nil, "unexpected error during HTTP request: " .. fok
   end
 
