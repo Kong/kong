f817cdc50a7450f2ea47eaa9068b892f5201f97b
Zachary Hu <6426329+outsinre@users.noreply.github.com>
2026-06-02 12:00:34 +0800

    Revert "fix(core): separate pluginserver runtime data from kong configuration (#14111)" (#14886)
    This reverts commit 76a46ba8db4e8e98414ad37897f2494d211d106c.

--- kong/runloop/plugin_servers/process.lua
+++ kong/runloop/plugin_servers/process.lua
@@ -10,7 +10,6 @@ local cjson_decode = cjson.decode
 local SIGTERM = 15
 
 
-local server_rt = {} -- store runtime of plugin server like proc
 local _M = {}
 
 
@@ -151,19 +150,16 @@ local function pluginserver_timer(premature, server_def)
     end
 
     kong.log.notice("[pluginserver] starting pluginserver process for ", server_def.name or "")
-    local proc = assert(ngx_pipe.spawn("exec " .. server_def.start_command, {
+    server_def.proc = assert(ngx_pipe.spawn("exec " .. server_def.start_command, {
       merge_stderr = true,
     }))
-    server_rt[server_def.name] = {
-      proc = proc,
-    }
     next_spawn = ngx.now() + 1
-    proc:set_timeouts(nil, nil, nil, 0)     -- block until something actually happens
-    kong.log.notice("[pluginserver] started, pid ", proc:pid())
+    server_def.proc:set_timeouts(nil, nil, nil, 0)     -- block until something actually happens
+    kong.log.notice("[pluginserver] started, pid ", server_def.proc:pid())
 
     while true do
-      grab_logs(proc, server_def.name)
-      local ok, reason, status = proc:wait()
+      grab_logs(server_def.proc, server_def.name)
+      local ok, reason, status = server_def.proc:wait()
 
       -- exited with a non 0 status
       if ok == false and reason == "exit" and status == 127 then
@@ -209,13 +205,12 @@ function _M.stop_pluginservers()
   -- only worker 0 manages plugin server processes
   if worker_id() == 0 then -- TODO move to privileged worker?
     for _, server_def in ipairs(kong_config.pluginservers) do
-      local server = server_rt[server_def.name]
-      if server and server.proc then
-        local ok, err = server.proc:kill(SIGTERM)
+      if server_def.proc then
+        local ok, err = server_def.proc:kill(SIGTERM)
         if not ok then
           kong.log.error("[pluginserver] failed to stop pluginserver '", server_def.name, ": ", err)
         end
-        kong.log.notice("[pluginserver] successfully stopped pluginserver '", server_def.name, "', pid ", server.proc:pid())
+        kong.log.notice("[pluginserver] successfully stopped pluginserver '", server_def.name, "', pid ", server_def.proc:pid())
       end
     end
   end
