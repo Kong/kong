c09fb7dd324feace4aa85afb67045d7621275a77
Zachary Hu <zachary.hu@konghq.com>
2026-09-23 14:41:48 +0800

    chore(cicd): remove unwanted skip_libsimdjson_ffi

--- scripts/explain_manifest/suites.py
+++ scripts/explain_manifest/suites.py
@@ -21,7 +21,7 @@ def read_requirements(path=None):
         lines = [re.findall("(.+)=([^# ]+)", d) for d in f.readlines()]
         return {l[0][0]: l[0][1].strip() for l in lines if l}
 
-def common_suites(expect, libxcrypt_no_obsolete_api: bool = False, skip_libsimdjson_ffi: bool = False):
+def common_suites(expect, libxcrypt_no_obsolete_api: bool = False):
     # file existence
     expect("/usr/local/kong/include/google/protobuf/**.proto",
            "includes Google protobuf headers").exists()
@@ -83,10 +83,9 @@ def common_suites(expect, libxcrypt_no_obsolete_api: bool = False, skip_libsimdj
         .functions \
         .contain("router_execute")
 
-    if not skip_libsimdjson_ffi:
-        expect("/usr/local/openresty/site/lualib/libsimdjson_ffi.so", "simdjson should have ffi module compiled") \
-            .functions \
-            .contain("simdjson_ffi_state_new")
+    expect("/usr/local/openresty/site/lualib/libsimdjson_ffi.so", "simdjson should have ffi module compiled") \
+        .functions \
+        .contain("simdjson_ffi_state_new")
 
     if libxcrypt_no_obsolete_api:
         expect("/usr/local/openresty/nginx/sbin/nginx", "nginx linked with libxcrypt.so.2") \
